e163f7bb2a6a2c7b05bde6ba324f529ea99b7c76
Vadim Zeitlin <vadim@wxwidgets.org>
1998-07-08 22:12:44 +0000

    1) keyboard handling added (OnNavigationKey) 2) removed default values from the old-style ctor to make it possible to    use the new one with default args
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@194 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/generic/panelg.h
+++ include/wx/generic/panelg.h
@@ -23,45 +23,52 @@ WXDLLEXPORT_DATA(extern const char*) wxPanelNameStr;
 // Dialog boxes
 class WXDLLEXPORT wxPanel: public wxWindow
 {
-  DECLARE_DYNAMIC_CLASS(wxPanel)
 public:
+  wxPanel();
 
-  wxPanel(void);
-
-  // Old-style constructor
+  // Old-style constructor (no default values for coordinates to avoid ambiguity
+  // with the new one)
   inline wxPanel(wxWindow *parent,
-           int x = -1, int y= -1, int width = 500, int height = 500,
-           long style = wxTAB_TRAVERSAL | wxNO_BORDER,
-           const wxString& name = wxPanelNameStr)
+                 int x, int y, int width, int height,
+                 long style = wxTAB_TRAVERSAL | wxNO_BORDER,
+                 const wxString& name = wxPanelNameStr)
   {
       Create(parent, -1, wxPoint(x, y), wxSize(width, height), style, name);
   }
 
   // Constructor
-  inline wxPanel(wxWindow *parent, wxWindowID id,
-           const wxPoint& pos = wxDefaultPosition,
-           const wxSize& size = wxDefaultSize,
-           long style = wxTAB_TRAVERSAL | wxNO_BORDER,
-           const wxString& name = wxPanelNameStr)
+  inline wxPanel(wxWindow *parent, wxWindowID id = -1,
+                 const wxPoint& pos = wxDefaultPosition,
+                 const wxSize& size = wxDefaultSize,
+                 long style = wxTAB_TRAVERSAL | wxNO_BORDER,
+                 const wxString& name = wxPanelNameStr)
   {
       Create(parent, id, pos, size, style, name);
   }
 
   bool Create(wxWindow *parent, wxWindowID id,
-           const wxPoint& pos = wxDefaultPosition,
-           const wxSize& size = wxDefaultSize,
-           long style = wxTAB_TRAVERSAL | wxNO_BORDER,
-           const wxString& name = wxPanelNameStr);
-
-  void OnPaint(wxPaintEvent& event);
+              const wxPoint& pos = wxDefaultPosition,
+              const wxSize& size = wxDefaultSize,
+              long style = wxTAB_TRAVERSAL | wxNO_BORDER,
+              const wxString& name = wxPanelNameStr);
 
   // Sends an OnInitDialog event, which in turns transfers data to
   // to the dialog via validators.
-  virtual void InitDialog(void);
+  virtual void InitDialog();
 
-  // Responds to colour changes
+  // overriden base class virtuals
+  virtual void SetFocus();
+
+  // callbacks
+    // Responds to colour changes
   void OnSysColourChanged(wxSysColourChangedEvent& event);
+    // Process a keyboard navigation message (Tab traversal)
+  void OnNavigationKey(wxNavigationKeyEvent& event);
 
+protected:
+  bool SetFocusToNextChild(wxNode *nodeCurrent = NULL, bool bForward = TRUE);
+
+DECLARE_DYNAMIC_CLASS(wxPanel)
 DECLARE_EVENT_TABLE()
 };
 
