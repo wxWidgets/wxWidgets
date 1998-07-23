904f68c7e8956b366aa48b3b5b28c2f65d678d41
Vadim Zeitlin <vadim@wxwidgets.org>
1998-07-23 17:54:24 +0000

    call to SetLabel in Create() added to strip "&" characters from the label
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@347 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/statbox.cpp
+++ src/gtk/statbox.cpp
@@ -5,7 +5,7 @@
 // Created:     01/02/97
 // Id:
 // Copyright:   (c) 1998 Robert Roebling, Julian Smart and Markus Holzem
-// Licence:   	wxWindows licence
+// Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
 #ifdef __GNUG__
@@ -24,26 +24,27 @@ wxStaticBox::wxStaticBox(void)
 {
 };
 
-wxStaticBox::wxStaticBox( wxWindow *parent, wxWindowID id, const wxString &label, 
-      const wxPoint &pos, const wxSize &size, 
+wxStaticBox::wxStaticBox( wxWindow *parent, wxWindowID id, const wxString &label,
+      const wxPoint &pos, const wxSize &size,
       long style, const wxString &name )
 {
   Create( parent, id, label, pos, size, style, name );
 };
 
-bool wxStaticBox::Create( wxWindow *parent, wxWindowID id, const wxString &label, 
-      const wxPoint &pos, const wxSize &size, 
+bool wxStaticBox::Create( wxWindow *parent, wxWindowID id, const wxString &label,
+      const wxPoint &pos, const wxSize &size,
       long style, const wxString &name )
 {
   m_needParent = TRUE;
-  
+
   PreCreation( parent, id, pos, size, style, name );
 
-  m_widget = gtk_frame_new( label );
-  
+  SetLabel(m_label);
+  m_widget = gtk_frame_new(m_label);
+
   PostCreation();
-  
+
   Show( TRUE );
-    
+
   return TRUE;
 };
