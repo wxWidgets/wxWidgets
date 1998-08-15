8f5f7c82d1ffa5124c55925062b4b350f07640d0
Vadim Zeitlin <vadim@wxwidgets.org>
1998-08-15 21:03:28 +0000

    added default values for ax size arguments of SetSizeHints (for msw compatibility)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@557 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/gtk1/frame.h
+++ include/wx/gtk1/frame.h
@@ -67,7 +67,9 @@ public:
   virtual void SetClientSize( int const width, int const height );
   
     // set minimal/maxmimal size for the frame
-  virtual void SetSizeHints( int minW, int minH, int maxW, int maxH, int incW = -1 );
+  virtual void SetSizeHints(int minW, int minH,
+                            int maxW = -1, int maxH = -1, 
+                            int incW = -1 );
 
   virtual wxStatusBar* CreateStatusBar(int number=1, long style = wxST_SIZEGRIP, wxWindowID id = 0,
     const wxString& name = "statusBar");
