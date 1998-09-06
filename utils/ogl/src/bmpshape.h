68278f98be1e296ce919e2a32c19ba3b298f67f6
Julian Smart <julian@anthemion.co.uk>
1998-09-06 14:35:53 +0000

    Renamed bitmap.cpp to bmpshape.cpp to aid debugging
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@678 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ utils/ogl/src/bmpshape.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        bmpshape.h
+// Purpose:     wxBitmapShape
+// Author:      Julian Smart
+// Modified by:
+// Created:     12/07/98
+// RCS-ID:      $Id$
+// Copyright:   (c) Julian Smart
+// Licence:   	wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _OGL_BITMAP_H_
+#define _OGL_BITMAP_H_
+
+#ifdef __GNUG__
+#pragma interface "bmpshape.h"
+#endif
+
+#include "basic.h"
+
+class wxBitmapShape: public wxRectangleShape
+{
+ DECLARE_DYNAMIC_CLASS(wxBitmapShape)
+ public:
+  wxBitmapShape();
+  ~wxBitmapShape();
+
+  void OnDraw(wxDC& dc);
+
+#ifdef PROLOGIO
+  // Prolog database stuff
+  char *GetFunctor();
+  void WritePrologAttributes(wxExpr *clause);
+  void ReadPrologAttributes(wxExpr *clause);
+#endif
+
+  // Does the copying for this object
+  void Copy(wxShape& copy);
+
+  void SetSize(double w, double h, bool recursive = TRUE);
+  inline wxBitmap& GetBitmap() const { return (wxBitmap&) m_bitmap; }
+  void SetBitmap(const wxBitmap& bm);
+  inline void SetFilename(const wxString& f) { m_filename = f; };
+  inline wxString GetFilename() const { return m_filename; }
+
+private:
+  wxBitmap      m_bitmap;
+  wxString      m_filename;
+};
+
+#endif
+  // _OGL_BITMAP_H_
+
+
