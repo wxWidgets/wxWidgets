5a43154b56ef5e41c88c0f313e90d72d72535e5e
Vadim Zeitlin <vadim@wxwidgets.org>
1998-10-28 21:32:29 +0000

    some old changes I forgot to check in
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@932 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/treectrl/treetest.h
+++ samples/treectrl/treetest.h
@@ -6,64 +6,14 @@
 // Created:     04/01/98
 // RCS-ID:      $Id$
 // Copyright:   (c) Julian Smart and Markus Holzem
-// Licence:   	wxWindows license
+// Licence:     wxWindows license
 /////////////////////////////////////////////////////////////////////////////
 
 // Define a new application type
-class MyApp: public wxApp
-{ public:
-    bool OnInit(void);
-
-    wxImageList *m_imageListNormal;
-};
-
-class MyTreeCtrl: public wxTreeCtrl
+class MyApp : public wxApp
 {
 public:
-	MyTreeCtrl(wxWindow *parent, const wxWindowID id, const wxPoint& pos,
-    const wxSize& size, long style);
-	~MyTreeCtrl();
-
-	void AddItemsRecursively(const wxTreeItemId& idParent,
-                                     size_t numChildren,
-                                     size_t depth);
-	void AddTestItemsToTree(size_t numChildren,
-                                    size_t depth);
-
-
-	void OnBeginDrag(wxTreeEvent& event);
-	void OnBeginRDrag(wxTreeEvent& event);
-	void OnBeginLabelEdit(wxTreeEvent& event);
-	void OnEndLabelEdit(wxTreeEvent& event);
-	void OnDeleteItem(wxTreeEvent& event);
-	void OnGetInfo(wxTreeEvent& event);
-	void OnSetInfo(wxTreeEvent& event);
-	void OnItemExpanded(wxTreeEvent& event);
-	void OnItemExpanding(wxTreeEvent& event);
-	void OnItemCollapsed(wxTreeEvent& event);
-	void OnItemCollapsing(wxTreeEvent& event);
-	void OnSelChanged(wxTreeEvent& event);
-	void OnSelChanging(wxTreeEvent& event);
-	void OnKeyDown(wxTreeEvent& event);
-
-   DECLARE_EVENT_TABLE()
-};
-
-// Define a new frame type
-class MyFrame: public wxFrame
-{ public:
-    MyTreeCtrl *m_treeCtrl;
-    wxTextCtrl *m_logWindow;
-
-    MyFrame(const wxString& title, int x, int y, int w, int h);
-    ~MyFrame(void);
-    
- public:
-    void OnQuit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-	bool OnClose(void) { return TRUE; }
-
-   DECLARE_EVENT_TABLE()
+  bool OnInit();
 };
 
 class MyTreeItemData : public wxTreeItemData
@@ -71,17 +21,79 @@ class MyTreeItemData : public wxTreeItemData
 public:
   MyTreeItemData(const wxString& desc) : m_desc(desc) { }
 
-	void ShowInfo(wxTreeCtrl *tree);
+  void ShowInfo(wxTreeCtrl *tree);
 
 private:
   wxString m_desc;
 };
 
-// ID for the menu quit command
-#define TREE_QUIT 	1
-#define TREE_ABOUT 	102
+class MyTreeCtrl : public wxTreeCtrl
+{
+public:
+  enum
+  {
+    TreeCtrlIcon_Folder,
+    TreeCtrlIcon_File
+  };
 
-#define TREE_CTRL   1000
+  MyTreeCtrl(wxWindow *parent, const wxWindowID id,
+             const wxPoint& pos, const wxSize& size,
+             long style);
+  virtual ~MyTreeCtrl();
+
+  void OnBeginDrag(wxTreeEvent& event);
+  void OnBeginRDrag(wxTreeEvent& event);
+  void OnBeginLabelEdit(wxTreeEvent& event);
+  void OnEndLabelEdit(wxTreeEvent& event);
+  void OnDeleteItem(wxTreeEvent& event);
+  void OnGetInfo(wxTreeEvent& event);
+  void OnSetInfo(wxTreeEvent& event);
+  void OnItemExpanded(wxTreeEvent& event);
+  void OnItemExpanding(wxTreeEvent& event);
+  void OnItemCollapsed(wxTreeEvent& event);
+  void OnItemCollapsing(wxTreeEvent& event);
+  void OnSelChanged(wxTreeEvent& event);
+  void OnSelChanging(wxTreeEvent& event);
+  void OnKeyDown(wxTreeEvent& event);
+
+private:
+  void AddItemsRecursively(const wxTreeItemId& idParent,
+                           size_t nChildren,
+                           size_t depth);
+
+  void AddTestItemsToTree(size_t numChildren,
+                          size_t depth);
+
+  wxImageList *m_imageListNormal;
+
+  DECLARE_EVENT_TABLE()
+};
+
+// Define a new frame type
+class MyFrame: public wxFrame
+{
+public:
+  // ctor and dtor
+  MyFrame(const wxString& title, int x, int y, int w, int h);
+  virtual ~MyFrame();
+
+  // menu callbacks
+  void OnQuit(wxCommandEvent& event);
+  void OnAbout(wxCommandEvent& event);
+
+private:
+  MyTreeCtrl *m_treeCtrl;
+
+  DECLARE_EVENT_TABLE()
+};
+
+// menu and control ids
+enum
+{
+  TreeTest_Quit,
+  TreeTest_About,
+  TreeTest_Ctrl = 100
+};
 
 enum
 {
