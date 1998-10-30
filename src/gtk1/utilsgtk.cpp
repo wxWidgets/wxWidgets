1950b38c35e4bed8cd2660d2178036b1b9e1b9bf
Karsten Ballüder <ballueder@usa.net>
1998-10-30 11:24:38 +0000

    wait4() now extern "C" :-)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@941 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk1/utilsgtk.cpp
+++ src/gtk1/utilsgtk.cpp
@@ -37,7 +37,11 @@
 
 #ifdef __SOLARIS__
 // somehow missing from sys/wait.h but in the system's docs
-pid_t wait4(pid_t pid, int *statusp, int options, struct rusage *rusage);   
+extern "C"
+{
+   pid_t wait4(pid_t pid, int *statusp, int options, struct rusage
+               *rusage);
+}
 #endif
 
 //------------------------------------------------------------------------
