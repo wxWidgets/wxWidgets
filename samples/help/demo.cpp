de5c0ba7451381eb3a261e962c0b8d569e2e77d0
Karsten Ballüder <ballueder@usa.net>
1998-11-10 21:22:22 +0000

    Added a sample to test wxExtHelpController. Should work with other wxHelpController implementations, too.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@982 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ samples/help/demo.cpp
@@ -0,0 +1,259 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        minimal.cpp
+// Purpose:     Minimal wxWindows sample
+// Author:      Julian Smart
+// Modified by:
+// Created:     04/01/98
+// RCS-ID:      $Id$
+// Copyright:   (c) Julian Smart and Markus Holzem
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+#ifdef __GNUG__
+    #pragma implementation "minimal.cpp"
+    #pragma interface "minimal.cpp"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWindows headers
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+#include   "wx/helpbase.h"
+#include   "wx/help.h"
+
+// ----------------------------------------------------------------------------
+// ressources
+// ----------------------------------------------------------------------------
+// the application icon
+#ifdef __WXGTK__
+    #include "mondrian.xpm"
+#endif
+
+// ----------------------------------------------------------------------------
+// private classes
+// ----------------------------------------------------------------------------
+
+// Define a new application type, each program should derive a class from wxApp
+class MyApp : public wxApp
+{
+public:
+    // override base class virtuals
+    // ----------------------------
+
+    // this one is called on application startup and is a good place for the app
+    // initialization (doing it here and not in the ctor allows to have an error
+    // return: if OnInit() returns false, the application terminates)
+    virtual bool OnInit();
+};
+
+// Define a new frame type: this is going to be our main frame
+class MyFrame : public wxFrame
+{
+public:
+    // ctor(s)
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+    // event handlers (these functions should _not_ be virtual)
+    void OnQuit(wxCommandEvent& event);
+    void OnHelp(wxCommandEvent& event);
+
+private:
+   wxHelpController  help;
+    // any class wishing to process wxWindows events must use this macro
+   DECLARE_EVENT_TABLE()
+};
+
+// ----------------------------------------------------------------------------
+// constants
+// ----------------------------------------------------------------------------
+
+// IDs for the controls and the menu commands
+enum
+{
+    // menu items
+    Minimal_Quit = 1,
+    Minimal_Help_Index,
+    Minimal_Help_Classes,
+    Minimal_Help_Functions,
+    Minimal_Help_Help,
+    Minimal_Help_KDE,
+    Minimal_Help_GNOME,
+    Minimal_Help_Netscape,
+    Minimal_Help_Search,
+    // controls start here (the numbers are, of course, arbitrary)
+    Minimal_Text = 1000,
+};
+
+// ----------------------------------------------------------------------------
+// event tables and other macros for wxWindows
+// ----------------------------------------------------------------------------
+
+// the event tables connect the wxWindows events with the functions (event
+// handlers) which process them. It can be also done at run-time, but for the
+// simple menu events like this the static method is much simpler.
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(Minimal_Quit,  MyFrame::OnQuit)
+    EVT_MENU(Minimal_Help_Index, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_Classes, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_Functions, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_Help, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_KDE, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_GNOME, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_Netscape, MyFrame::OnHelp)
+    EVT_MENU(Minimal_Help_Search, MyFrame::OnHelp)
+END_EVENT_TABLE()
+
+// Create a new application object: this macro will allow wxWindows to create
+// the application object during program execution (it's better than using a
+// static object for many reasons) and also declares the accessor function
+// wxGetApp() which will return the reference of the right type (i.e. MyApp and
+// not wxApp)
+IMPLEMENT_APP(MyApp)
+
+// ============================================================================
+// implementation
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// the application class
+// ----------------------------------------------------------------------------
+
+// `Main program' equivalent: the program execution "starts" here
+bool MyApp::OnInit()
+{
+    // Create the main application window
+    MyFrame *frame = new MyFrame("Minimal wxWindows App",
+                                 wxPoint(50, 50), wxSize(450, 340));
+
+    // Show it and tell the application that it's our main window
+    // @@@ what does it do exactly, in fact? is it necessary here?
+    frame->Show(TRUE);
+    SetTopWindow(frame);
+
+    // success: wxApp::OnRun() will be called which will enter the main message
+    // loop and the application will run. If we returned FALSE here, the
+    // application would exit immediately.
+    return TRUE;
+}
+
+// ----------------------------------------------------------------------------
+// main frame
+// ----------------------------------------------------------------------------
+
+// frame constructor
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+       : wxFrame((wxFrame *)NULL, -1, title, pos, size)
+{
+    // set the frame icon
+    SetIcon(wxICON(mondrian));
+
+    // create a menu bar
+    wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append(Minimal_Help_Index, "&Help Index...");
+    menuFile->Append(Minimal_Help_Classes, "&Help on Classes...");
+    menuFile->Append(Minimal_Help_Functions, "&Help on Functions...");
+    menuFile->Append(Minimal_Help_Help, "&About wxExtHelpController...");
+    menuFile->AppendSeparator();
+    menuFile->Append(Minimal_Help_Search, "&Search help...");
+    if(help.IsKindOf(CLASSINFO(wxExtHelpController)))
+       {
+          menuFile->AppendSeparator();
+          menuFile->Append(Minimal_Help_KDE, "Use &KDE");
+          menuFile->Append(Minimal_Help_GNOME, "Use &GNOME");
+          menuFile->Append(Minimal_Help_Netscape, "Use &Netscape");
+       }
+    menuFile->AppendSeparator();
+    menuFile->Append(Minimal_Quit, "E&xit");
+
+    // now append the freshly created menu to the menu bar...
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+
+    // ... and attach this menu bar to the frame
+    SetMenuBar(menuBar);
+
+    // create a status bar just for fun (by default with 1 pane only)
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWindows!");
+
+    // now create some controls
+
+    // a panel first - if there were several controls, it would allow us to
+    // navigate between them from the keyboard
+    wxPanel *panel = new wxPanel(this, -1, wxPoint(0, 0), wxSize(400, 200));
+
+    // and a static control whose parent is the panel
+    (void)new wxStaticText(panel, -1, "Hello, world!", wxPoint(10, 10));
+
+    // initialise the help system
+    help.Initialize("doc");
+   
+}
+
+
+// event handlers
+
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    // TRUE is to force the frame to close
+    Close(TRUE);
+}
+
+void MyFrame::OnHelp(wxCommandEvent& event)
+{
+   switch(event.GetId())
+   {
+   case Minimal_Help_Classes:
+      help.DisplaySection(1);
+      break;
+   case Minimal_Help_Functions:
+      help.DisplaySection(2);
+      break;
+   case Minimal_Help_Help:
+      help.DisplaySection(5);
+      break;
+   case Minimal_Help_KDE:
+      if(help.IsKindOf(CLASSINFO(wxExtHelpController)))
+         ((wxExtHelpController *)&help)->SetBrowser("kdehelp");
+      break;
+   case Minimal_Help_GNOME:
+      if(help.IsKindOf(CLASSINFO(wxExtHelpController)))
+         ((wxExtHelpController *)&help)->SetBrowser("gnome-help-browser");
+      break;
+   case Minimal_Help_Netscape:
+      if(help.IsKindOf(CLASSINFO(wxExtHelpController)))
+         ((wxExtHelpController *)&help)->SetBrowser("netscape",TRUE);
+      break;
+   case Minimal_Help_Search:
+   {
+      wxString key = wxGetTextFromUser("Search for?",
+                                       "Search help for keyword",
+                                       "",
+                                       this);
+      if(! key.IsEmpty())
+         help.KeywordSearch(key);
+   }
+   break;
+   case Minimal_Help_Index:
+   default:
+      help.DisplayContents();
+      break;
+   }
+}
