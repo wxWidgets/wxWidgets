fad893ed70126d47c9c1a3d27cb59eca103d40bb
Bryan Petty <bryan@ibaku.net>
1998-11-22 22:32:53 +0000

    This commit was manufactured by cvs2svn to create tag 'M_REFERENCE'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/M_REFERENCE@1021 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/fstream.h
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        fstream.h
+// Purpose:     File stream classes
+// Author:      Guilhem Lavaux
+// Modified by:
+// Created:     11/07/98
+// RCS-ID:      $Id$
+// Copyright:   (c) Guilhem Lavaux
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_WXFSTREAM_H__
+#define _WX_WXFSTREAM_H__
+
+#ifdef __GNUG__
+#pragma interface "fstream.h"
+#endif
+
+#include <wx/object.h>
+#include <wx/string.h>
+#include <wx/stream.h>
+#include <wx/file.h>
+
+class wxFileInputStream: public wxInputStream {
+ public:
+  wxFileInputStream(const wxString& ifileName);
+  wxFileInputStream(wxFile& file);
+  wxFileInputStream(int fd);
+  virtual ~wxFileInputStream();
+
+  virtual char Peek();
+
+  bool Ok() const { return m_file->IsOpened(); }
+
+ protected:
+  wxFileInputStream();
+
+  size_t OnSysRead(void *buffer, size_t size);
+  off_t OnSysSeek(off_t pos, wxSeekMode mode);
+  off_t OnSysTell() const;
+
+ protected:
+  wxFile *m_file;
+  bool m_file_destroy;
+};
+
+class wxFileOutputStream: public wxOutputStream {
+ public:
+  wxFileOutputStream(const wxString& fileName);
+  wxFileOutputStream(wxFile& file);
+  wxFileOutputStream(int fd);
+  virtual ~wxFileOutputStream();
+
+  // To solve an ambiguity on GCC
+  inline wxOutputStream& Write(const void *buffer, size_t size)
+     { return wxOutputStream::Write(buffer, size); }
+
+  void Sync();
+
+  bool Ok() const { return m_file->IsOpened(); }
+
+ protected:
+  wxFileOutputStream();
+
+  size_t OnSysWrite(const void *buffer, size_t size);
+  off_t OnSysSeek(off_t pos, wxSeekMode mode);
+  off_t OnSysTell() const;
+
+ protected:
+  wxFile *m_file;
+  bool m_file_destroy;
+};
+
+#endif
