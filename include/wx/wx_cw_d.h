a54d6c1bbb35f6adce30704c987496acc0337020
Stefan Csomor <csomor@advancedconcepts.ch>
1998-12-10 13:41:04 +0000

    CodeWarrior Support (no defines in project possible)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@1153 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/wx_cw_d.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx_cw_d.h
+// Purpose:     wxWindows definitions for CodeWarrior builds (Debug)
+// Author:      Stefan Csomor
+// Modified by:
+// Created:     12/10/98
+// RCS-ID:      $Id$
+// Copyright:   (c) Stefan Csomor
+// Licence:   	wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_CW__
+#define _WX_CW__
+
+#define MSL_USE_PRECOMPILED_HEADERS	1
+
+#if !defined( __MWERKS__ )
+	#error "this file is only for builds with Metrowerks CodeWarrior"
+#endif 
+
+#if (__MWERKS__ < 0x0900) || macintosh
+	#define __MAC__
+	#define __WXMAC__
+	#define	USE_PRECOMPILED_MAC_HEADERS	1  /*Set to 0 if you don't want to use precompiled MacHeaders*/
+	#include <ansi_prefix.mac.h>
+	#define WX_PRECOMP
+	// automatically includes MacHeaders
+#elif (__MWERKS__ >= 0x0900) && __INTEL__
+	#define __WXMSW__
+	#define __WINDOWS__
+	#define __WIN95__
+	#define STRICT
+	#define NOMINMAX
+	#include <ansi_prefix.win32.h>
+//		#include <Windows.h>
+//		#include <COMMCTRL.H>
+#elif __BEOS__
+	#include <ansi_prefix.be.h>
+	#include <Be.h>
+#else	
+	#error unknown MW compiler
+#endif
+
+#define __WXDEBUG__ 1
+#define WXDEBUG 1
+
+#endif
+    // _WX_CW__
