c52486391b86543e189e9991d6ccba3b9483f287
Julian Smart <julian@anthemion.co.uk>
1998-12-17 18:12:31 +0000

    Removed sometimes-ambiguous wxString operator
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@1222 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/string.h
+++ include/wx/string.h
@@ -304,11 +304,15 @@ public:
       { wxASSERT( !IsEmpty() ); CopyBeforeWrite(); return m_pchData[Len()-1]; }
 
     // on alpha-linux this gives overload problems:
+    // Also on Solaris, so removing for now (JACS)
+/*
 #if ! defined(__ALPHA__)
     /// operator version of GetChar
     char  operator[](size_t n) const
       { ASSERT_VALID_INDEX( n ); return m_pchData[n]; }
 #endif
+*/
+
     /// operator version of GetChar
     char  operator[](int n) const
       { ASSERT_VALID_INDEX( n ); return m_pchData[n]; }
