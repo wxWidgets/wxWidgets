d11de713deee92f6ecb3ba4aff54745c823ab0f0
Bryan Petty <bryan@ibaku.net>
1999-02-04 23:21:27 +0000

    This commit was manufactured by cvs2svn to create tag 'WORKING_KBD_NAVIGATION'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/WORKING_KBD_NAVIGATION@1605 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/choicdlg.h
+++
@@ -1,7 +0,0 @@
-#ifndef _WX_CHOICDLG_H_BASE_
-#define _WX_CHOICDLG_H_BASE_
-
-#include "wx/generic/choicdgg.h"
-
-#endif
-    // _WX_CHOICDLG_H_BASE_
--- utils/wxPython/distrib/zipit.bat
+++
@@ -1,8 +0,0 @@
-
-@echo off
-
-rem cd %WXWIN%\utils
-
-zip -@ -r wxPython\wxPython-%1.zip < wxPython\distrib\wxPython.rsp
-
-cd -
