7c39369e5a9b2298eed8adc64a229482d5d90dd1
Vadim Zeitlin <vadim@wxwidgets.org>
1999-02-23 18:57:01 +0000

    some feeble attempts at making wxBeginBusyCursor work
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@1760 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/cursor.cpp
+++ src/gtk/cursor.cpp
@@ -61,28 +61,34 @@ wxCursor::wxCursor( int cursorId )
         case wxCURSOR_CROSS:            gdk_cur = GDK_CROSSHAIR; break;
         case wxCURSOR_SIZEWE:           gdk_cur = GDK_SB_H_DOUBLE_ARROW; break;
         case wxCURSOR_SIZENS:           gdk_cur = GDK_SB_V_DOUBLE_ARROW; break;
-        case wxCURSOR_WAIT:                                        
+        case wxCURSOR_WAIT:
         case wxCURSOR_WATCH:            gdk_cur = GDK_WATCH; break;
         case wxCURSOR_SIZING:           gdk_cur = GDK_SIZING; break;
         case wxCURSOR_SPRAYCAN:         gdk_cur = GDK_SPRAYCAN; break;
         case wxCURSOR_IBEAM:            gdk_cur = GDK_XTERM; break;
         case wxCURSOR_PENCIL:           gdk_cur = GDK_PENCIL; break;
         case wxCURSOR_NO_ENTRY:         gdk_cur = GDK_PIRATE; break;
-        case wxCURSOR_SIZENWSE:                                     
+        case wxCURSOR_SIZENWSE:
         case wxCURSOR_SIZENESW:         gdk_cur = GDK_FLEUR; break;
         case wxCURSOR_QUESTION_ARROW:   gdk_cur = GDK_QUESTION_ARROW; break;
-	case wxCURSOR_PAINT_BRUSH:      gdk_cur = GDK_SPRAYCAN; break;
-	case wxCURSOR_MAGNIFIER:        gdk_cur = GDK_PLUS; break;
-	case wxCURSOR_CHAR:             gdk_cur = GDK_XTERM; break;
+        case wxCURSOR_PAINT_BRUSH:      gdk_cur = GDK_SPRAYCAN; break;
+        case wxCURSOR_MAGNIFIER:        gdk_cur = GDK_PLUS; break;
+        case wxCURSOR_CHAR:             gdk_cur = GDK_XTERM; break;
         case wxCURSOR_LEFT_BUTTON:      gdk_cur = GDK_LEFTBUTTON; break;
         case wxCURSOR_MIDDLE_BUTTON:    gdk_cur = GDK_MIDDLEBUTTON; break;
         case wxCURSOR_RIGHT_BUTTON:     gdk_cur = GDK_RIGHTBUTTON; break;
 /*
-	case wxCURSOR_DOUBLE_ARROW:     gdk_cur = GDK_DOUBLE_ARROW; break;
-	case wxCURSOR_CROSS_REVERSE:    gdk_cur = GDK_CROSS_REVERSE; break;
+        case wxCURSOR_DOUBLE_ARROW:     gdk_cur = GDK_DOUBLE_ARROW; break;
+        case wxCURSOR_CROSS_REVERSE:    gdk_cur = GDK_CROSS_REVERSE; break;
         case wxCURSOR_BASED_ARROW_UP:   gdk_cur = GDK_BASED_ARROW_UP; break;
         case wxCURSOR_BASED_ARROW_DOWN: gdk_cur = GDK_BASED_ARROW_DOWN; break;
 */
+        default:
+            wxFAIL_MSG("unsupported cursor type");
+            // will use the standard one
+
+        case wxCURSOR_ARROW:
+            break;
     }
 
     M_CURSORDATA->m_cursor = gdk_cursor_new( gdk_cur );
@@ -99,9 +105,11 @@ wxCursor::~wxCursor()
 
 wxCursor& wxCursor::operator = ( const wxCursor& cursor )
 {
-    if (*this == cursor) return (*this);
+    if (*this == cursor)
+        return (*this);
+
     Ref( cursor );
-    
+
     return *this;
 }
 
@@ -129,29 +137,41 @@ GdkCursor *wxCursor::GetCursor() const
 // busy cursor routines
 //-----------------------------------------------------------------------------
 
-bool g_isBusy = FALSE;
+extern wxCursor *g_globalCursor;
+
+static wxCursor *gs_savedCursor = NULL;
 
 void wxEndBusyCursor()
 {
-    g_isBusy = FALSE;
+    wxCHECK_RET( gs_savedCursor && gs_savedCursor->Ok(),
+                 "calling wxEndBusyCursor() without wxBeginBusyCursor()?" );
+
+    wxSetCursor(*gs_savedCursor);
+    delete gs_savedCursor;
+    gs_savedCursor = NULL;
 }
 
 void wxBeginBusyCursor( wxCursor *WXUNUSED(cursor) )
 {
-    g_isBusy = TRUE;
+    wxASSERT_MSG( !gs_savedCursor,
+                  "forgot to call wxEndBusyCursor, will leak memory" );
+
+    gs_savedCursor = new wxCursor;
+    if ( g_globalCursor && g_globalCursor->Ok() )
+        *gs_savedCursor = *g_globalCursor;
+    else
+        *gs_savedCursor = wxCursor(wxCURSOR_ARROW);
+    wxSetCursor(wxCursor(wxCURSOR_WATCH));
 }
 
 bool wxIsBusy()
 {
-    return g_isBusy;
+    return gs_savedCursor != NULL;
 }
 
 void wxSetCursor( const wxCursor& cursor )
 {
-    extern wxCursor *g_globalCursor;
     if (g_globalCursor) (*g_globalCursor) = cursor;
-
-    if (cursor.Ok()) {}
 }
 
 
