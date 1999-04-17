d207efe172bd6780b0dabc95a56dc46e7cad27e3
Ove Kaaven <ovek@arcticnet.no>
1999-04-17 13:21:38 +0000

    Adapted those asserts and debug messages for Unicode. All of wxGTK now compiles, and for my app, also links in Unicode mode. (Too bad it just segfaults though, guess it's debugging time... if only gdb wouldn't segfault even when just loading the core file)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@2215 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/unix/threadpsx.cpp
+++ src/unix/threadpsx.cpp
@@ -108,7 +108,7 @@ wxMutex::wxMutex()
 wxMutex::~wxMutex()
 {
     if (m_locked > 0)
-        wxLogDebug("Freeing a locked mutex (%d locks)", m_locked);
+        wxLogDebug(_T("Freeing a locked mutex (%d locks)"), m_locked);
 
     pthread_mutex_destroy( &(p_internal->p_mutex) );
     delete p_internal;
@@ -119,7 +119,7 @@ wxMutexError wxMutex::Lock()
     int err = pthread_mutex_lock( &(p_internal->p_mutex) );
     if (err == EDEADLK)
     {
-        wxLogDebug("Locking this mutex would lead to deadlock!");
+        wxLogDebug(_T("Locking this mutex would lead to deadlock!"));
 
         return wxMUTEX_DEAD_LOCK;
     }
@@ -155,7 +155,7 @@ wxMutexError wxMutex::Unlock()
     }
     else
     {
-        wxLogDebug("Unlocking not locked mutex.");
+        wxLogDebug(_T("Unlocking not locked mutex."));
 
         return wxMUTEX_UNLOCKED;
     }
@@ -304,7 +304,7 @@ void *wxThreadInternal::PthreadStart(void *ptr)
     // terminate the thread
     thread->Exit(status);
 
-    wxFAIL_MSG("wxThread::Exit() can't return.");
+    wxFAIL_MSG(_T("wxThread::Exit() can't return."));
 
     return NULL;
 }
@@ -334,7 +334,7 @@ wxThreadInternal::~wxThreadInternal()
 wxThreadError wxThreadInternal::Run()
 {
     wxCHECK_MSG( GetState() == STATE_NEW, wxTHREAD_RUNNING,
-                 "thread may only be started once after successful Create()" );
+                 _T("thread may only be started once after successful Create()") );
 
     // the mutex was locked on Create(), so we will be able to lock it again
     // only when the thread really starts executing and enters the wait -
@@ -353,7 +353,7 @@ wxThreadError wxThreadInternal::Run()
 
 void wxThreadInternal::Wait()
 {
-    wxCHECK_RET( WasCancelled(), "thread should have been cancelled first" );
+    wxCHECK_RET( WasCancelled(), _T("thread should have been cancelled first") );
 
     // if the thread we're waiting for is waiting for the GUI mutex, we will
     // deadlock so make sure we release it temporarily
@@ -391,7 +391,7 @@ void wxThreadInternal::Pause()
     // the state is set from the thread which pauses us first, this function
     // is called later so the state should have been already set
     wxCHECK_RET( m_state == STATE_PAUSED,
-                 "thread must first be paused with wxThread::Pause()." );
+                 _T("thread must first be paused with wxThread::Pause().") );
 
     // don't pause the thread which is being terminated - this would lead to
     // deadlock if the thread is paused after Delete() had called Resume() but
@@ -406,7 +406,7 @@ void wxThreadInternal::Pause()
 void wxThreadInternal::Resume()
 {
     wxCHECK_RET( m_state == STATE_PAUSED,
-                 "can't resume thread which is not suspended." );
+                 _T("can't resume thread which is not suspended.") );
 
     // we will be able to lock this mutex only when Pause() starts waiting
     wxMutexLocker lock(m_mutexSuspend);
@@ -519,7 +519,7 @@ void wxThread::SetPriority(unsigned int prio)
 {
     wxCHECK_RET( ((int)WXTHREAD_MIN_PRIORITY <= (int)prio) &&
                  ((int)prio <= (int)WXTHREAD_MAX_PRIORITY),
-                 "invalid thread priority" );
+                 _T("invalid thread priority") );
 
     wxCriticalSectionLocker lock(m_critsect);
 
@@ -548,7 +548,7 @@ void wxThread::SetPriority(unsigned int prio)
 
         case STATE_EXITED:
         default:
-            wxFAIL_MSG("impossible to set thread priority in this state");
+            wxFAIL_MSG(_T("impossible to set thread priority in this state"));
     }
 }
 
@@ -574,7 +574,7 @@ wxThreadError wxThread::Pause()
 
     if ( p_internal->GetState() != STATE_RUNNING )
     {
-        wxLogDebug("Can't pause thread which is not running.");
+        wxLogDebug(_T("Can't pause thread which is not running."));
 
         return wxTHREAD_NOT_RUNNING;
     }
@@ -596,7 +596,7 @@ wxThreadError wxThread::Resume()
     }
     else
     {
-        wxLogDebug("Attempt to resume a thread which is not paused.");
+        wxLogDebug(_T("Attempt to resume a thread which is not paused."));
 
         return wxTHREAD_MISC_ERROR;
     }
@@ -766,12 +766,12 @@ bool wxThreadModule::OnInit()
 
 void wxThreadModule::OnExit()
 {
-    wxASSERT_MSG( wxThread::IsMain(), "only main thread can be here" );
+    wxASSERT_MSG( wxThread::IsMain(), _T("only main thread can be here") );
 
     // terminate any threads left
     size_t count = gs_allThreads.GetCount();
     if ( count != 0u )
-        wxLogDebug("Some threads were not terminated by the application.");
+        wxLogDebug(_T("Some threads were not terminated by the application."));
 
     for ( size_t n = 0u; n < count; n++ )
     {
