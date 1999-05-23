c52d95b412d81372e2705d5e1c03a0417af79ade
Vadim Zeitlin <vadim@wxwidgets.org>
1999-05-23 23:45:47 +0000

    more MDI features tested
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@2546 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/mdi/mdi.cpp
+++ samples/mdi/mdi.cpp
@@ -6,107 +6,156 @@
 // Created:     04/01/98
 // RCS-ID:      $Id$
 // Copyright:   (c) Julian Smart and Markus Holzem
-// Licence:   	wxWindows license
+// Licence:     wxWindows license
 /////////////////////////////////////////////////////////////////////////////
+
+// ===========================================================================
+// declarations
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+// headers
+// ---------------------------------------------------------------------------
 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
 
 #ifdef __BORLANDC__
-#pragma hdrstop
+    #pragma hdrstop
 #endif
 
 #ifndef WX_PRECOMP
-#include "wx/wx.h"
-#include "wx/mdi.h"
+    #include "wx/wx.h"
+    #include "wx/mdi.h"
 #endif
 
 #include <wx/toolbar.h>
 
 #if defined(__WXGTK__) || defined(__WXMOTIF__)
-#include "mondrian.xpm"
-#include "bitmaps/new.xpm"
-#include "bitmaps/open.xpm"
-#include "bitmaps/save.xpm"
-#include "bitmaps/copy.xpm"
-#include "bitmaps/cut.xpm"
-#include "bitmaps/paste.xpm"
-#include "bitmaps/print.xpm"
-#include "bitmaps/help.xpm"
+    #include "mondrian.xpm"
+    #include "bitmaps/new.xpm"
+    #include "bitmaps/open.xpm"
+    #include "bitmaps/save.xpm"
+    #include "bitmaps/copy.xpm"
+    #include "bitmaps/cut.xpm"
+    #include "bitmaps/paste.xpm"
+    #include "bitmaps/print.xpm"
+    #include "bitmaps/help.xpm"
 #endif
 
-
 #include "mdi.h"
+
+IMPLEMENT_APP(MyApp)
+
+// ---------------------------------------------------------------------------
+// global variables
+// ---------------------------------------------------------------------------
 
 MyFrame *frame = (MyFrame *) NULL;
 wxList my_children;
 
-IMPLEMENT_APP(MyApp)
-
 // For drawing lines in a canvas
-long xpos = -1;
-long ypos = -1;
+static long xpos = -1;
+static long ypos = -1;
 
-int winNumber = 1;
+static int gs_nFrames = 0;
 
-// Initialise this in OnInit, not statically
-bool MyApp::OnInit(void)
-{
-  // Create the main frame window
-
-  frame = new MyFrame((wxFrame *) NULL, -1, (char *) "MDI Demo", wxPoint(-1, -1), wxSize(500, 400),
-   wxDEFAULT_FRAME_STYLE | wxHSCROLL | wxVSCROLL);
-
-  // Give it an icon
-#ifdef __WXMSW__
-  frame->SetIcon(wxIcon("mdi_icn"));
-#else
-  frame->SetIcon(wxIcon( mondrian_xpm ));
-#endif
-
-  // Make a menubar
-  wxMenu *file_menu = new wxMenu;
-
-  file_menu->Append(MDI_NEW_WINDOW, "&New window");
-  file_menu->Append(MDI_QUIT, "&Exit");
-
-  wxMenu *help_menu = new wxMenu;
-  help_menu->Append(MDI_ABOUT, "&About");
-
-  wxMenuBar *menu_bar = new wxMenuBar;
-
-  menu_bar->Append(file_menu, "&File");
-  menu_bar->Append(help_menu, "&Help");
-
-  // Associate the menu bar with the frame
-  frame->SetMenuBar(menu_bar);
-
-  frame->CreateStatusBar();
-
-  frame->Show(TRUE);
-
-  SetTopWindow(frame);
-
-  return TRUE;
-}
+// ---------------------------------------------------------------------------
+// event tables
+// ---------------------------------------------------------------------------
 
 BEGIN_EVENT_TABLE(MyFrame, wxMDIParentFrame)
     EVT_MENU(MDI_ABOUT, MyFrame::OnAbout)
     EVT_MENU(MDI_NEW_WINDOW, MyFrame::OnNewWindow)
-    EVT_SIZE(MyFrame::OnSize)
     EVT_MENU(MDI_QUIT, MyFrame::OnQuit)
+
+    EVT_CLOSE(MyFrame::OnClose)
+
+    EVT_SIZE(MyFrame::OnSize)
 END_EVENT_TABLE()
 
-// Define my frame constructor
-MyFrame::MyFrame(wxWindow *parent, const wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size,
-	const long style):
-  wxMDIParentFrame(parent, id, title, pos, size, style)
-{
-    textWindow = new wxTextCtrl(this, -1, "", wxDefaultPosition, wxDefaultSize,
-        wxTE_MULTILINE|wxSUNKEN_BORDER);
-    textWindow->SetValue("A help window");
+// Note that MDI_NEW_WINDOW and MDI_ABOUT commands get passed
+// to the parent window for processing, so no need to
+// duplicate event handlers here.
+BEGIN_EVENT_TABLE(MyChild, wxMDIChildFrame)
+    EVT_MENU(MDI_CHILD_QUIT, MyChild::OnQuit)
+    EVT_MENU(MDI_REFRESH, MyChild::OnRefresh)
 
-    CreateToolBar(wxNO_BORDER|wxTB_FLAT|wxTB_HORIZONTAL);
+    EVT_CLOSE(MyChild::OnClose)
+END_EVENT_TABLE()
+
+BEGIN_EVENT_TABLE(MyCanvas, wxScrolledWindow)
+    EVT_MOUSE_EVENTS(MyCanvas::OnEvent)
+END_EVENT_TABLE()
+
+// ===========================================================================
+// implementation
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+// MyApp
+// ---------------------------------------------------------------------------
+
+// Initialise this in OnInit, not statically
+bool MyApp::OnInit()
+{
+    // Create the main frame window
+
+    frame = new MyFrame((wxFrame *)NULL, -1, "MDI Demo",
+                        wxPoint(-1, -1), wxSize(500, 400),
+                        wxDEFAULT_FRAME_STYLE | wxHSCROLL | wxVSCROLL);
+
+    // Give it an icon
+#ifdef __WXMSW__
+    frame->SetIcon(wxIcon("mdi_icn"));
+#else
+    frame->SetIcon(wxIcon( mondrian_xpm ));
+#endif
+
+    // Make a menubar
+    wxMenu *file_menu = new wxMenu;
+
+    file_menu->Append(MDI_NEW_WINDOW, "&New window", "Create a new child window");
+    file_menu->Append(MDI_QUIT, "&Exit", "Quit the program");
+
+    wxMenu *help_menu = new wxMenu;
+    help_menu->Append(MDI_ABOUT, "&About");
+
+    wxMenuBar *menu_bar = new wxMenuBar;
+
+    menu_bar->Append(file_menu, "&File");
+    menu_bar->Append(help_menu, "&Help");
+
+    // Associate the menu bar with the frame
+    frame->SetMenuBar(menu_bar);
+
+    frame->CreateStatusBar();
+
+    frame->Show(TRUE);
+
+    SetTopWindow(frame);
+
+    return TRUE;
+}
+
+// ---------------------------------------------------------------------------
+// MyFrame
+// ---------------------------------------------------------------------------
+
+// Define my frame constructor
+MyFrame::MyFrame(wxWindow *parent,
+                 const wxWindowID id,
+                 const wxString& title,
+                 const wxPoint& pos,
+                 const wxSize& size,
+                 const long style)
+       : wxMDIParentFrame(parent, id, title, pos, size, style)
+{
+    textWindow = new wxTextCtrl(this, -1, "A help window",
+                                wxDefaultPosition, wxDefaultSize,
+                                wxTE_MULTILINE | wxSUNKEN_BORDER);
+
+    CreateToolBar(wxNO_BORDER | wxTB_FLAT | wxTB_HORIZONTAL);
     InitToolBar(GetToolBar());
 
     // Accelerators
@@ -118,164 +167,101 @@ MyFrame::MyFrame(wxWindow *parent, const wxWindowID id, const wxString& title, c
     SetAcceleratorTable(accel);
 }
 
-void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event) )
+void MyFrame::OnClose(wxCloseEvent& event)
 {
-      Close(TRUE);
+    if ( event.CanVeto() && (gs_nFrames > 0) )
+    {
+        wxString msg;
+        msg.Printf("%d windows still open, close anyhow?", gs_nFrames);
+        if ( wxMessageBox(msg, "Please confirm",
+                          wxICON_QUESTION | wxYES_NO) != wxYES )
+        {
+            event.Veto();
+
+            return;
+        }
+    }
+
+    event.Skip();
+}
+
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close();
 }
 
 void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event) )
 {
-      (void)wxMessageBox("wxWindows 2.0 MDI Demo\nAuthor: Julian Smart (c) 1997\nUsage: mdi.exe", "About MDI Demo");
+    (void)wxMessageBox("wxWindows 2.0 MDI Demo\n"
+                       "Author: Julian Smart (c) 1997\n"
+                       "Usage: mdi.exe", "About MDI Demo");
 }
 
 void MyFrame::OnNewWindow(wxCommandEvent& WXUNUSED(event) )
 {
-      // Make another frame, containing a canvas
-      MyChild *subframe = new MyChild(frame, "Canvas Frame", wxPoint(-1, -1), wxSize(-1, -1),
-                             wxDEFAULT_FRAME_STYLE);
+    // Make another frame, containing a canvas
+    MyChild *subframe = new MyChild(frame, "Canvas Frame",
+                                    wxPoint(-1, -1), wxSize(-1, -1),
+                                    wxDEFAULT_FRAME_STYLE);
 
-      wxString title;
-      title.Printf("Canvas Frame %d", winNumber);
-      subframe->SetTitle(title);
-      winNumber ++;
+    wxString title;
+    title.Printf("Canvas Frame %d", ++gs_nFrames);
 
-      // Give it an icon
+    subframe->SetTitle(title);
+
+    // Give it an icon
 #ifdef __WXMSW__
-      subframe->SetIcon(wxIcon("chrt_icn"));
+    subframe->SetIcon(wxIcon("chrt_icn"));
 #else
-      subframe->SetIcon(wxIcon( mondrian_xpm ));
+    subframe->SetIcon(wxIcon( mondrian_xpm ));
 #endif
 
-      // Make a menubar
-      wxMenu *file_menu = new wxMenu;
+    // Make a menubar
+    wxMenu *file_menu = new wxMenu;
 
-      file_menu->Append(MDI_NEW_WINDOW, "&New window");
-      file_menu->Append(MDI_CHILD_QUIT, "&Close child");
-      file_menu->Append(MDI_QUIT, "&Exit");
+    file_menu->Append(MDI_NEW_WINDOW, "&New window");
+    file_menu->Append(MDI_CHILD_QUIT, "&Close child", "Close this window");
+    file_menu->Append(MDI_QUIT, "&Exit");
 
-      wxMenu *option_menu = new wxMenu;
+    wxMenu *option_menu = new wxMenu;
 
-      // Dummy option
-      option_menu->Append(MDI_REFRESH, "&Refresh picture");
+    // Dummy option
+    option_menu->Append(MDI_REFRESH, "&Refresh picture");
 
-      wxMenu *help_menu = new wxMenu;
-      help_menu->Append(MDI_ABOUT, "&About");
+    wxMenu *help_menu = new wxMenu;
+    help_menu->Append(MDI_ABOUT, "&About");
 
-      wxMenuBar *menu_bar = new wxMenuBar;
+    wxMenuBar *menu_bar = new wxMenuBar;
 
-      menu_bar->Append(file_menu, "&File");
-      menu_bar->Append(option_menu, "&Options");
-      menu_bar->Append(help_menu, "&Help");
+    menu_bar->Append(file_menu, "&File");
+    menu_bar->Append(option_menu, "&Options");
+    menu_bar->Append(help_menu, "&Help");
 
-      // Associate the menu bar with the frame
-      subframe->SetMenuBar(menu_bar);
+    // Associate the menu bar with the frame
+    subframe->SetMenuBar(menu_bar);
 
-      int width, height;
-      subframe->GetClientSize(&width, &height);
-      MyCanvas *canvas = new MyCanvas(subframe, wxPoint(0, 0), wxSize(width, height));
-      canvas->SetCursor(wxCursor(wxCURSOR_PENCIL));
-      subframe->canvas = canvas;
+    int width, height;
+    subframe->GetClientSize(&width, &height);
+    MyCanvas *canvas = new MyCanvas(subframe, wxPoint(0, 0), wxSize(width, height));
+    canvas->SetCursor(wxCursor(wxCURSOR_PENCIL));
+    subframe->canvas = canvas;
 
-      // Give it scrollbars
-      canvas->SetScrollbars(20, 20, 50, 50);
+    // Give it scrollbars
+    canvas->SetScrollbars(20, 20, 50, 50);
 
-      subframe->Show(TRUE);
-}
+    subframe->CreateStatusBar();
+    subframe->SetStatusText(title);
 
-BEGIN_EVENT_TABLE(MyCanvas, wxScrolledWindow)
-	EVT_MOUSE_EVENTS(MyCanvas::OnEvent)
-END_EVENT_TABLE()
-
-// Define a constructor for my canvas
-MyCanvas::MyCanvas(wxWindow *parent, const wxPoint& pos, const wxSize& size):
- wxScrolledWindow(parent, -1, pos, size, wxSUNKEN_BORDER|wxVSCROLL|wxHSCROLL)
-{
-    SetBackgroundColour(wxColour("WHITE"));
-}
-
-// Define the repainting behaviour
-void MyCanvas::OnDraw(wxDC& dc)
-{
-  dc.SetFont(*wxSWISS_FONT);
-  dc.SetPen(*wxGREEN_PEN);
-  dc.DrawLine(0, 0, 200, 200);
-  dc.DrawLine(200, 0, 0, 200);
-
-  dc.SetBrush(*wxCYAN_BRUSH);
-  dc.SetPen(*wxRED_PEN);
-  dc.DrawRectangle(100, 100, 100, 50);
-  dc.DrawRoundedRectangle(150, 150, 100, 50, 20);
-
-  dc.DrawEllipse(250, 250, 100, 50);
-  dc.DrawSpline(50, 200, 50, 100, 200, 10);
-  dc.DrawLine(50, 230, 200, 230);
-  dc.DrawText("This is a test string", 50, 230);
-
-  wxPoint points[3];
-  points[0].x = 200; points[0].y = 300;
-  points[1].x = 100; points[1].y = 400;
-  points[2].x = 300; points[2].y = 400;
-  
-  dc.DrawPolygon(3, points);
-}
-
-// This implements a tiny doodling program! Drag the mouse using
-// the left button.
-void MyCanvas::OnEvent(wxMouseEvent& event)
-{
-  wxClientDC dc(this);
-  PrepareDC(dc);
-
-  wxPoint pt(event.GetLogicalPosition(dc));
-
-  if (xpos > -1 && ypos > -1 && event.Dragging())
-  {
-    dc.SetPen(*wxBLACK_PEN);
-    dc.DrawLine(xpos, ypos, pt.x, pt.y);
-  }
-  xpos = pt.x;
-  ypos = pt.y;
+    subframe->Show(TRUE);
 }
 
 void MyFrame::OnSize(wxSizeEvent& WXUNUSED(event) )
 {
     int w, h;
     GetClientSize(&w, &h);
-    
+
     textWindow->SetSize(0, 0, 200, h);
     GetClientWindow()->SetSize(200, 0, w - 200, h);
-}
-
-// Note that MDI_NEW_WINDOW and MDI_ABOUT commands get passed
-// to the parent window for processing, so no need to
-// duplicate event handlers here.
-
-BEGIN_EVENT_TABLE(MyChild, wxMDIChildFrame)
-  EVT_MENU(MDI_CHILD_QUIT, MyChild::OnQuit)
-END_EVENT_TABLE()
-
-MyChild::MyChild(wxMDIParentFrame *parent, const wxString& title, const wxPoint& pos, const wxSize& size,
-const long style):
-  wxMDIChildFrame(parent, -1, title, pos, size, style)
-{
-  canvas = (MyCanvas *) NULL;
-  my_children.Append(this);
-}
-
-MyChild::~MyChild(void)
-{
-  my_children.DeleteObject(this);
-}
-
-void MyChild::OnQuit(wxCommandEvent& WXUNUSED(event))
-{
-      Close(TRUE);
-}
-
-void MyChild::OnActivate(wxActivateEvent& event)
-{
-  if (event.GetActive() && canvas)
-    canvas->SetFocus();
 }
 
 void MyFrame::InitToolBar(wxToolBar* toolBar)
@@ -292,46 +278,160 @@ void MyFrame::InitToolBar(wxToolBar* toolBar)
     bitmaps[6] = new wxBitmap("icon7", wxBITMAP_TYPE_RESOURCE);
     bitmaps[7] = new wxBitmap("icon8", wxBITMAP_TYPE_RESOURCE);
 #else
-  bitmaps[0] = new wxBitmap( new_xpm );
-  bitmaps[1] = new wxBitmap( open_xpm );
-  bitmaps[2] = new wxBitmap( save_xpm );
-  bitmaps[3] = new wxBitmap( copy_xpm );
-  bitmaps[4] = new wxBitmap( cut_xpm );
-  bitmaps[5] = new wxBitmap( paste_xpm );
-  bitmaps[6] = new wxBitmap( print_xpm );
-  bitmaps[7] = new wxBitmap( help_xpm );
+    bitmaps[0] = new wxBitmap( new_xpm );
+    bitmaps[1] = new wxBitmap( open_xpm );
+    bitmaps[2] = new wxBitmap( save_xpm );
+    bitmaps[3] = new wxBitmap( copy_xpm );
+    bitmaps[4] = new wxBitmap( cut_xpm );
+    bitmaps[5] = new wxBitmap( paste_xpm );
+    bitmaps[6] = new wxBitmap( print_xpm );
+    bitmaps[7] = new wxBitmap( help_xpm );
 #endif
 
 #ifdef __WXMSW__
-  int width = 24;
+    int width = 24;
 #else
-  int width = 16;
+    int width = 16;
 #endif
-  int currentX = 5;
+    int currentX = 5;
 
-  toolBar->AddTool( MDI_NEW_WINDOW, *(bitmaps[0]), wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "New file");
-  currentX += width + 5;
-  toolBar->AddTool(1, *bitmaps[1], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Open file");
-  currentX += width + 5;
-  toolBar->AddTool(2, *bitmaps[2], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Save file");
-  currentX += width + 5;
-  toolBar->AddSeparator();
-  toolBar->AddTool(3, *bitmaps[3], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Copy");
-  currentX += width + 5;
-  toolBar->AddTool(4, *bitmaps[4], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Cut");
-  currentX += width + 5;
-  toolBar->AddTool(5, *bitmaps[5], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Paste");
-  currentX += width + 5;
-  toolBar->AddSeparator();
-  toolBar->AddTool(6, *bitmaps[6], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Print");
-  currentX += width + 5;
-  toolBar->AddSeparator();
-  toolBar->AddTool(7, *bitmaps[7], wxNullBitmap, TRUE, currentX, -1, (wxObject *) NULL, "Help");
+    toolBar->AddTool( MDI_NEW_WINDOW, *(bitmaps[0]), wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "New file");
+    currentX += width + 5;
+    toolBar->AddTool(1, *bitmaps[1], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Open file");
+    currentX += width + 5;
+    toolBar->AddTool(2, *bitmaps[2], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Save file");
+    currentX += width + 5;
+    toolBar->AddSeparator();
+    toolBar->AddTool(3, *bitmaps[3], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Copy");
+    currentX += width + 5;
+    toolBar->AddTool(4, *bitmaps[4], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Cut");
+    currentX += width + 5;
+    toolBar->AddTool(5, *bitmaps[5], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Paste");
+    currentX += width + 5;
+    toolBar->AddSeparator();
+    toolBar->AddTool(6, *bitmaps[6], wxNullBitmap, FALSE, currentX, -1, (wxObject *) NULL, "Print");
+    currentX += width + 5;
+    toolBar->AddSeparator();
+    toolBar->AddTool(7, *bitmaps[7], wxNullBitmap, TRUE, currentX, -1, (wxObject *) NULL, "Help");
 
-  toolBar->Realize();
+    toolBar->Realize();
 
-  int i;
-  for (i = 0; i < 8; i++)
-    delete bitmaps[i];
+    int i;
+    for (i = 0; i < 8; i++)
+        delete bitmaps[i];
 }
+
+// ---------------------------------------------------------------------------
+// MyCanvas
+// ---------------------------------------------------------------------------
+
+// Define a constructor for my canvas
+MyCanvas::MyCanvas(wxWindow *parent, const wxPoint& pos, const wxSize& size)
+        : wxScrolledWindow(parent, -1, pos, size,
+                           wxSUNKEN_BORDER|wxVSCROLL|wxHSCROLL)
+{
+    SetBackgroundColour(wxColour("WHITE"));
+
+    m_dirty = FALSE;
+}
+
+// Define the repainting behaviour
+void MyCanvas::OnDraw(wxDC& dc)
+{
+    dc.SetFont(*wxSWISS_FONT);
+    dc.SetPen(*wxGREEN_PEN);
+    dc.DrawLine(0, 0, 200, 200);
+    dc.DrawLine(200, 0, 0, 200);
+
+    dc.SetBrush(*wxCYAN_BRUSH);
+    dc.SetPen(*wxRED_PEN);
+    dc.DrawRectangle(100, 100, 100, 50);
+    dc.DrawRoundedRectangle(150, 150, 100, 50, 20);
+
+    dc.DrawEllipse(250, 250, 100, 50);
+    dc.DrawSpline(50, 200, 50, 100, 200, 10);
+    dc.DrawLine(50, 230, 200, 230);
+    dc.DrawText("This is a test string", 50, 230);
+
+    wxPoint points[3];
+    points[0].x = 200; points[0].y = 300;
+    points[1].x = 100; points[1].y = 400;
+    points[2].x = 300; points[2].y = 400;
+
+    dc.DrawPolygon(3, points);
+}
+
+// This implements a tiny doodling program! Drag the mouse using the left
+// button.
+void MyCanvas::OnEvent(wxMouseEvent& event)
+{
+    wxClientDC dc(this);
+    PrepareDC(dc);
+
+    wxPoint pt(event.GetLogicalPosition(dc));
+
+    if (xpos > -1 && ypos > -1 && event.Dragging())
+    {
+        dc.SetPen(*wxBLACK_PEN);
+        dc.DrawLine(xpos, ypos, pt.x, pt.y);
+
+        m_dirty = TRUE;
+    }
+
+    xpos = pt.x;
+    ypos = pt.y;
+}
+
+// ---------------------------------------------------------------------------
+// MyChild
+// ---------------------------------------------------------------------------
+
+MyChild::MyChild(wxMDIParentFrame *parent, const wxString& title,
+                 const wxPoint& pos, const wxSize& size,
+                 const long style)
+       : wxMDIChildFrame(parent, -1, title, pos, size, style)
+{
+    canvas = (MyCanvas *) NULL;
+    my_children.Append(this);
+}
+
+MyChild::~MyChild()
+{
+    my_children.DeleteObject(this);
+}
+
+void MyChild::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close(TRUE);
+}
+
+void MyChild::OnRefresh(wxCommandEvent& event)
+{
+    Refresh();
+}
+
+void MyChild::OnActivate(wxActivateEvent& event)
+{
+    if ( event.GetActive() && canvas )
+        canvas->SetFocus();
+}
+
+void MyChild::OnClose(wxCloseEvent& event)
+{
+    if ( canvas && canvas->IsDirty() )
+    {
+        if ( wxMessageBox("Really close?", "Please confirm",
+                          wxICON_QUESTION | wxYES_NO) != wxYES )
+        {
+            event.Veto();
+
+            return;
+        }
+    }
+
+    gs_nFrames--;
+
+    event.Skip();
+}
+
 
--- samples/mdi/mdi.h
+++ samples/mdi/mdi.h
@@ -6,35 +6,42 @@
 // Created:     04/01/98
 // RCS-ID:      $Id$
 // Copyright:   (c) Julian Smart and Markus Holzem
-// Licence:   	wxWindows license
+// Licence:     wxWindows license
 /////////////////////////////////////////////////////////////////////////////
 
 #include <wx/toolbar.h>
 
 // Define a new application
-class MyApp: public wxApp
+class MyApp : public wxApp
 {
-  public:
-    bool OnInit(void);
+public:
+    bool OnInit();
 };
 
-class MyCanvas: public wxScrolledWindow
+class MyCanvas : public wxScrolledWindow
 {
-  public:
+public:
     MyCanvas(wxWindow *parent, const wxPoint& pos, const wxSize& size);
     virtual void OnDraw(wxDC& dc);
+
+    bool IsDirty() const { return m_dirty; }
+
     void OnEvent(wxMouseEvent& event);
+
+private:
+    bool m_dirty;
 
     DECLARE_EVENT_TABLE()
 };
 
 // Define a new frame
-class MyFrame: public wxMDIParentFrame
+class MyFrame : public wxMDIParentFrame
 {
-  public:
+public:
     wxTextCtrl *textWindow;
-    
-    MyFrame(wxWindow *parent, const wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, const long style);
+
+    MyFrame(wxWindow *parent, const wxWindowID id, const wxString& title,
+            const wxPoint& pos, const wxSize& size, const long style);
 
     void InitToolBar(wxToolBar* toolBar);
 
@@ -42,24 +49,33 @@ class MyFrame: public wxMDIParentFrame
     void OnAbout(wxCommandEvent& event);
     void OnNewWindow(wxCommandEvent& event);
     void OnQuit(wxCommandEvent& event);
+    void OnClose(wxCloseEvent& event);
 
-DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 };
 
 class MyChild: public wxMDIChildFrame
 {
-  public:
+public:
     MyCanvas *canvas;
     MyChild(wxMDIParentFrame *parent, const wxString& title, const wxPoint& pos, const wxSize& size, const long style);
-    ~MyChild(void);
-    void OnActivate(wxActivateEvent& event);
-    void OnQuit(wxCommandEvent& event);
+    ~MyChild();
 
-DECLARE_EVENT_TABLE()
+    void OnActivate(wxActivateEvent& event);
+
+    void OnRefresh(wxCommandEvent& event);
+    void OnQuit(wxCommandEvent& event);
+    void OnClose(wxCloseEvent& event);
+
+    DECLARE_EVENT_TABLE()
 };
 
-#define MDI_QUIT        1
-#define MDI_NEW_WINDOW  2
-#define MDI_REFRESH     3
-#define MDI_CHILD_QUIT  4
-#define MDI_ABOUT       5
+// menu items ids
+enum
+{
+    MDI_QUIT = 100,
+    MDI_NEW_WINDOW,
+    MDI_REFRESH,
+    MDI_CHILD_QUIT,
+    MDI_ABOUT
+};
