39c9d5ac9dbde2d89f9f5998f9ff6f268760e909
Robert Roebling <robert@roebling.de>
1999-06-10 10:21:05 +0000

    wxWINE fixes.   Removed wxTextCtrl tests from controls.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@2737 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/controls/controls.cpp
+++ samples/controls/controls.cpp
@@ -26,12 +26,6 @@
 #include "wx/spinbutt.h"
 #include "wx/notebook.h"
 #include "wx/imaglist.h"
-#include "wx/spinbutt.h"
-
-#if wxUSE_CLIPBOARD
-    #include "wx/dataobj.h"
-    #include "wx/clipbrd.h"
-#endif
 
 #if wxUSE_TOOLTIPS
     #include "wx/tooltip.h"
@@ -69,26 +63,6 @@ public:
     bool OnInit();
 };
 
-// a text ctrl which allows to call different wxTextCtrl functions
-// interactively by pressing function keys in it
-class MyTextCtrl : public wxTextCtrl
-{
-public:
-    MyTextCtrl(wxWindow *parent, wxWindowID id, const wxString &value,
-               const wxPoint &pos, const wxSize &size, int style = 0)
-        : wxTextCtrl(parent, id, value, pos, size, style) { }
-
-    void OnKeyDown(wxKeyEvent& event);
-    void OnKeyUp(wxKeyEvent& event);
-    void OnChar(wxKeyEvent& event);
-
-private:
-    static inline wxChar GetChar(bool on, wxChar c) { return on ? c : _T('-'); }
-    void LogEvent(const wxChar *name, wxKeyEvent& event) const;
-
-    DECLARE_EVENT_TABLE()
-};
-
 class MyPanel: public wxPanel
 {
 public:
@@ -114,10 +88,6 @@ public:
     void OnUpdateShowProgress( wxUpdateUIEvent& event );
     void OnShowProgress( wxCommandEvent &event );
 #endif // wxUSE_SPINBUTTON
-    void OnPasteFromClipboard( wxCommandEvent &event );
-    void OnCopyToClipboard( wxCommandEvent &event );
-    void OnMoveToEndOfText( wxCommandEvent &event );
-    void OnMoveToEndOfEntry( wxCommandEvent &event );
 
     wxListBox     *m_listbox;
     wxChoice      *m_choice;
@@ -133,8 +103,6 @@ public:
     wxButton      *m_btnProgress;
 #endif
     wxTextCtrl    *m_spintext;
-    MyTextCtrl    *m_multitext;
-    MyTextCtrl    *m_textentry;
     wxCheckBox    *m_checkbox;
 
     wxTextCtrl    *m_text;
@@ -224,196 +192,6 @@ bool MyApp::OnInit()
 }
 
 //----------------------------------------------------------------------
-// MyTextCtrl
-//----------------------------------------------------------------------
-
-BEGIN_EVENT_TABLE(MyTextCtrl, wxTextCtrl)
-    EVT_KEY_DOWN(MyTextCtrl::OnKeyDown)
-    EVT_KEY_UP(MyTextCtrl::OnKeyUp)
-    EVT_CHAR(MyTextCtrl::OnChar)
-END_EVENT_TABLE()
-
-void MyTextCtrl::LogEvent(const wxChar *name, wxKeyEvent& event) const
-{
-    wxString key;
-    long keycode = event.KeyCode();
-    if ( wxIsprint((int)keycode) )
-        key.Printf( _T("'%c'") , (char)keycode);
-    else
-    {
-        switch ( keycode )
-        {
-            case WXK_BACK: key = "BACK"; break;
-            case WXK_TAB: key = "TAB"; break;
-            case WXK_RETURN: key = "RETURN"; break;
-            case WXK_ESCAPE: key = "ESCAPE"; break;
-            case WXK_SPACE: key = "SPACE"; break;
-            case WXK_DELETE: key = "DELETE"; break;
-            case WXK_START: key = "START"; break;
-            case WXK_LBUTTON: key = "LBUTTON"; break;
-            case WXK_RBUTTON: key = "RBUTTON"; break;
-            case WXK_CANCEL: key = "CANCEL"; break;
-            case WXK_MBUTTON: key = "MBUTTON"; break;
-            case WXK_CLEAR: key = "CLEAR"; break;
-            case WXK_SHIFT: key = "SHIFT"; break;
-            case WXK_ALT: key = "ALT"; break;
-            case WXK_CONTROL: key = "CONTROL"; break;
-            case WXK_MENU: key = "MENU"; break;
-            case WXK_PAUSE: key = "PAUSE"; break;
-            case WXK_CAPITAL: key = "CAPITAL"; break;
-            case WXK_PRIOR: key = "PRIOR"; break;
-            case WXK_NEXT: key = "NEXT"; break;
-            case WXK_END: key = "END"; break;
-            case WXK_HOME: key = "HOME"; break;
-            case WXK_LEFT: key = "LEFT"; break;
-            case WXK_UP: key = "UP"; break;
-            case WXK_RIGHT: key = "RIGHT"; break;
-            case WXK_DOWN: key = "DOWN"; break;
-            case WXK_SELECT: key = "SELECT"; break;
-            case WXK_PRINT: key = "PRINT"; break;
-            case WXK_EXECUTE: key = "EXECUTE"; break;
-            case WXK_SNAPSHOT: key = "SNAPSHOT"; break;
-            case WXK_INSERT: key = "INSERT"; break;
-            case WXK_HELP: key = "HELP"; break;
-            case WXK_NUMPAD0: key = "NUMPAD0"; break;
-            case WXK_NUMPAD1: key = "NUMPAD1"; break;
-            case WXK_NUMPAD2: key = "NUMPAD2"; break;
-            case WXK_NUMPAD3: key = "NUMPAD3"; break;
-            case WXK_NUMPAD4: key = "NUMPAD4"; break;
-            case WXK_NUMPAD5: key = "NUMPAD5"; break;
-            case WXK_NUMPAD6: key = "NUMPAD6"; break;
-            case WXK_NUMPAD7: key = "NUMPAD7"; break;
-            case WXK_NUMPAD8: key = "NUMPAD8"; break;
-            case WXK_NUMPAD9: key = "NUMPAD9"; break;
-            case WXK_MULTIPLY: key = "MULTIPLY"; break;
-            case WXK_ADD: key = "ADD"; break;
-            case WXK_SEPARATOR: key = "SEPARATOR"; break;
-            case WXK_SUBTRACT: key = "SUBTRACT"; break;
-            case WXK_DECIMAL: key = "DECIMAL"; break;
-            case WXK_DIVIDE: key = "DIVIDE"; break;
-            case WXK_F1: key = "F1"; break;
-            case WXK_F2: key = "F2"; break;
-            case WXK_F3: key = "F3"; break;
-            case WXK_F4: key = "F4"; break;
-            case WXK_F5: key = "F5"; break;
-            case WXK_F6: key = "F6"; break;
-            case WXK_F7: key = "F7"; break;
-            case WXK_F8: key = "F8"; break;
-            case WXK_F9: key = "F9"; break;
-            case WXK_F10: key = "F10"; break;
-            case WXK_F11: key = "F11"; break;
-            case WXK_F12: key = "F12"; break;
-            case WXK_F13: key = "F13"; break;
-            case WXK_F14: key = "F14"; break;
-            case WXK_F15: key = "F15"; break;
-            case WXK_F16: key = "F16"; break;
-            case WXK_F17: key = "F17"; break;
-            case WXK_F18: key = "F18"; break;
-            case WXK_F19: key = "F19"; break;
-            case WXK_F20: key = "F20"; break;
-            case WXK_F21: key = "F21"; break;
-            case WXK_F22: key = "F22"; break;
-            case WXK_F23: key = "F23"; break;
-            case WXK_F24: key = "F24"; break;
-            case WXK_NUMLOCK: key = "NUMLOCK"; break;
-            case WXK_SCROLL: key = "SCROLL"; break;
-            case WXK_PAGEUP: key = "PAGEUP"; break;
-            case WXK_PAGEDOWN: key = "PAGEDOWN"; break;
-            case WXK_NUMPAD_SPACE: key = "NUMPAD_SPACE"; break;
-            case WXK_NUMPAD_TAB: key = "NUMPAD_TAB"; break;
-            case WXK_NUMPAD_ENTER: key = "NUMPAD_ENTER"; break;
-            case WXK_NUMPAD_F1: key = "NUMPAD_F1"; break;
-            case WXK_NUMPAD_F2: key = "NUMPAD_F2"; break;
-            case WXK_NUMPAD_F3: key = "NUMPAD_F3"; break;
-            case WXK_NUMPAD_F4: key = "NUMPAD_F4"; break;
-            case WXK_NUMPAD_HOME: key = "NUMPAD_HOME"; break;
-            case WXK_NUMPAD_LEFT: key = "NUMPAD_LEFT"; break;
-            case WXK_NUMPAD_UP: key = "NUMPAD_UP"; break;
-            case WXK_NUMPAD_RIGHT: key = "NUMPAD_RIGHT"; break;
-            case WXK_NUMPAD_DOWN: key = "NUMPAD_DOWN"; break;
-            case WXK_NUMPAD_PRIOR: key = "NUMPAD_PRIOR"; break;
-            case WXK_NUMPAD_PAGEUP: key = "NUMPAD_PAGEUP"; break;
-            case WXK_NUMPAD_PAGEDOWN: key = "NUMPAD_PAGEDOWN"; break;
-            case WXK_NUMPAD_END: key = "NUMPAD_END"; break;
-            case WXK_NUMPAD_BEGIN: key = "NUMPAD_BEGIN"; break;
-            case WXK_NUMPAD_INSERT: key = "NUMPAD_INSERT"; break;
-            case WXK_NUMPAD_DELETE: key = "NUMPAD_DELETE"; break;
-            case WXK_NUMPAD_EQUAL: key = "NUMPAD_EQUAL"; break;
-            case WXK_NUMPAD_MULTIPLY: key = "NUMPAD_MULTIPLY"; break;
-            case WXK_NUMPAD_ADD: key = "NUMPAD_ADD"; break;
-            case WXK_NUMPAD_SEPARATOR: key = "NUMPAD_SEPARATOR"; break;
-            case WXK_NUMPAD_SUBTRACT: key = "NUMPAD_SUBTRACT"; break;
-            case WXK_NUMPAD_DECIMAL: key = "NUMPAD_DECIMAL"; break;
-
-            default:
-                key.Printf( _T("unknown (%ld)"), keycode);
-        }
-    }
-
-    wxLogMessage( _T("%s event: %s (flags = %c%c%c%c)"),
-                  name,
-                  key.c_str(),
-                  GetChar( event.ControlDown(), _T('C') ),
-                  GetChar( event.AltDown(), _T('A') ),
-                  GetChar( event.ShiftDown(), _T('S') ),
-                  GetChar( event.MetaDown(), _T('M') ) );
-
-}
-
-void MyTextCtrl::OnChar(wxKeyEvent& event)
-{
-    LogEvent( _T("Char"), event);
-
-    event.Skip();
-}
-
-void MyTextCtrl::OnKeyUp(wxKeyEvent& event)
-{
-    LogEvent( _("Key up"), event);
-
-    event.Skip();
-}
-
-void MyTextCtrl::OnKeyDown(wxKeyEvent& event)
-{
-    switch ( event.KeyCode() )
-    {
-        case WXK_F1:
-            // show current position and text length
-            {
-                long line, column, pos = GetInsertionPoint();
-                PositionToXY(pos, &column, &line);
-
-                wxLogMessage( _T("Current position: %ld\n"
-                        "Current line, column: (%ld, %ld)\n"
-                        "Number of lines: %ld\n"
-                        "Current line length: %ld\n"
-                        "Total text length: %ld"),
-                        pos,
-                        line, column,
-                        GetNumberOfLines(),
-                        GetLineLength(line),
-                        GetLastPosition());
-            }
-            break;
-
-        case WXK_F2:
-            // go to the end
-            SetInsertionPointEnd();
-            break;
-
-        case WXK_F3:
-            // go to position 10
-            SetInsertionPoint(10);
-            break;
-    }
-
-    LogEvent( _("Key down"), event);
-
-    event.Skip();
-}
-
-//----------------------------------------------------------------------
 // MyPanel
 //----------------------------------------------------------------------
 
@@ -445,12 +223,6 @@ const int  ID_COMBO_APPEND      = 144;
 const int  ID_COMBO_DELETE      = 145;
 const int  ID_COMBO_FONT        = 146;
 const int  ID_COMBO_ENABLE      = 147;
-
-const int  ID_TEXT              = 150;
-const int  ID_PASTE_TEXT        = 151;
-const int  ID_COPY_TEXT         = 152;
-const int  ID_MOVE_END_ENTRY    = 153;
-const int  ID_MOVE_END_ZONE     = 154;
 
 const int  ID_RADIOBOX          = 160;
 const int  ID_RADIOBOX_SEL_NUM  = 161;
@@ -510,10 +282,6 @@ EVT_SPIN      (ID_SPIN,                 MyPanel::OnSpinUpdate)
 EVT_UPDATE_UI (ID_BTNPROGRESS,          MyPanel::OnUpdateShowProgress)
 EVT_BUTTON    (ID_BTNPROGRESS,          MyPanel::OnShowProgress)
 #endif
-EVT_BUTTON    (ID_PASTE_TEXT,           MyPanel::OnPasteFromClipboard)
-EVT_BUTTON    (ID_COPY_TEXT,            MyPanel::OnCopyToClipboard)
-EVT_BUTTON    (ID_MOVE_END_ZONE,        MyPanel::OnMoveToEndOfText)
-EVT_BUTTON    (ID_MOVE_END_ENTRY,       MyPanel::OnMoveToEndOfEntry)
 END_EVENT_TABLE()
 
 MyPanel::MyPanel( wxFrame *frame, int x, int y, int w, int h )
@@ -642,36 +410,6 @@ MyPanel::MyPanel( wxFrame *frame, int x, int y, int w, int h )
     (void)new wxCheckBox( panel, ID_COMBO_ENABLE, "Disable", wxPoint(20,130), wxSize(140,30) );
     m_notebook->AddPage(panel, "wxComboBox", FALSE, Image_Combo);
 
-    panel = new wxPanel(m_notebook);
-    m_textentry = new MyTextCtrl( panel, -1, "Some text.", wxPoint(10,10), wxSize(320,28), //0);
-    wxTE_PROCESS_ENTER);
-    (*m_textentry) << " Appended.";
-    m_textentry->SetInsertionPoint(0);
-    m_textentry->WriteText( "Prepended. " );
-
-    m_multitext = new MyTextCtrl( panel, ID_TEXT, "Some text.", wxPoint(10,50), wxSize(320,70), wxTE_MULTILINE );
-    (*m_multitext) << " Appended.";
-    m_multitext->SetInsertionPoint(0);
-    m_multitext->WriteText( "Prepended. " );
-    m_multitext->AppendText( "\nPress function keys for test different tests." );
-
-    (*m_multitext) << "\nDoes it have cross cursor?";
-    m_multitext->SetCursor(*wxCROSS_CURSOR);
-
-    new MyTextCtrl( panel, -1, "This one is with wxTE_PROCESS_TAB style.", wxPoint(10,120), wxSize(320,70), wxTE_MULTILINE | wxTE_PROCESS_TAB);
-#if wxUSE_TOOLTIPS
-    m_multitext->AppendText( "\nThis ctrl has a tooltip. " );
-    m_multitext->SetToolTip("Press F1 here.");
-#endif // wxUSE_TOOLTIPS
-
-    (void)new wxStaticBox( panel, -1, "&Move cursor to the end of:", wxPoint(345, 0), wxSize(160, 100) );
-    (void)new wxButton( panel, ID_MOVE_END_ENTRY, "Text &entry", wxPoint(370, 20), wxSize(110, 30) );
-    (void)new wxButton( panel, ID_MOVE_END_ZONE, "Text &zone", wxPoint(370, 60), wxSize(110, 30) );
-    (void)new wxStaticBox( panel, -1, "wx&Clipboard", wxPoint(345,100), wxSize(160,100) );
-    (void)new wxButton( panel, ID_COPY_TEXT, "C&opy line 1", wxPoint(370,120), wxSize(110,30) );
-    (void)new wxButton( panel, ID_PASTE_TEXT, "&Paste text", wxPoint(370,160), wxSize(110,30) );
-    m_notebook->AddPage( panel, "wxTextCtrl" , FALSE, Image_Text );
-
     wxString choices2[] =
     {
         "First", "Second",
@@ -727,107 +465,6 @@ MyPanel::MyPanel( wxFrame *frame, int x, int y, int w, int h )
                                   wxPoint(208, 159) );
 #endif
     m_notebook->AddPage(panel, "wxGauge", FALSE, Image_Gauge);
-}
-
-void MyPanel::OnPasteFromClipboard( wxCommandEvent &WXUNUSED(event) )
-{
-    // We test for wxUSE_DRAG_AND_DROP also, because data objects
-    // may not be implemented for compilers that can't cope with the OLE
-    // parts in wxUSE_DRAG_AND_DROP.
-
-#if wxUSE_CLIPBOARD && wxUSE_DRAG_AND_DROP
-    if (!wxTheClipboard->Open())
-    {
-        *m_text << "Error opening the clipboard.\n";
-
-        return;
-    }
-    else
-    {
-        *m_text << "Successfully opened the clipboard.\n";
-    }
-
-    wxTextDataObject data;
-
-    if (wxTheClipboard->IsSupported( data.GetFormat() ))
-    {
-        *m_text << "Clipboard supports requested format.\n";
-
-        if (wxTheClipboard->GetData( &data ))
-        {
-            *m_text << "Successfully retrieved data from the clipboard.\n";
-            *m_multitext << data.GetText() << "\n";
-        }
-        else
-        {
-            *m_text << "Error getting data from the clipboard.\n";
-        }
-    }
-    else
-    {
-        *m_text << "Clipboard doesn't support requested format.\n";
-    }
-
-    wxTheClipboard->Close();
-
-    *m_text << "Closed the clipboard.\n";
-#else
-    wxLogError("Your version of wxWindows is compiled without clipboard support.");
-#endif
-}
-
-void MyPanel::OnCopyToClipboard( wxCommandEvent &WXUNUSED(event) )
-{
-#if wxUSE_CLIPBOARD && wxUSE_DRAG_AND_DROP
-    wxString text( m_multitext->GetLineText(0) );
-
-    if (text.IsEmpty())
-    {
-        *m_text << "No text to copy.\n";
-
-        return;
-    }
-
-    if (!wxTheClipboard->Open())
-    {
-        *m_text << "Error opening the clipboard.\n";
-
-        return;
-    }
-    else
-    {
-        *m_text << "Successfully opened the clipboard.\n";
-    }
-
-    wxTextDataObject *data = new wxTextDataObject( text );
-
-    if (!wxTheClipboard->SetData( data ))
-    {
-        *m_text << "Error while copying to the clipboard.\n";
-    }
-    else
-    {
-        *m_text << "Successfully copied data to the clipboard.\n";
-    }
-
-    wxTheClipboard->Close();
-
-    *m_text << "Closed the clipboard.\n";
-#else
-    wxLogError("Your version of wxWindows is compiled without clipboard support.");
-#endif
-}
-
-void MyPanel::OnMoveToEndOfText( wxCommandEvent &event )
-{
-    m_multitext->SetInsertionPointEnd();
-    m_multitext->SetFocus();
-}
-
-void MyPanel::OnMoveToEndOfEntry( wxCommandEvent &event )
-{
-    m_textentry->SetInsertionPointEnd();
-    m_textentry->SetFocus();
 }
 
 void MyPanel::OnSize( wxSizeEvent& WXUNUSED(event) )
--- samples/minimal/minimal.cpp
+++ samples/minimal/minimal.cpp
@@ -162,11 +162,9 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     // ... and attach this menu bar to the frame
     SetMenuBar(menuBar);
 
-#if wxUSE_STATUSBAR
     // create a status bar just for fun (by default with 1 pane only)
     CreateStatusBar(2);
     SetStatusText("Welcome to wxWindows!");
-#endif // wxUSE_STATUSBAR
 }
 
 
