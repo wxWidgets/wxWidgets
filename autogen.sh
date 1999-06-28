818ffd7cb55333a896cf402f733ecfa38a307083
Bryan Petty <bryan@ibaku.net>
1999-06-28 04:08:58 +0000

    This commit was manufactured by cvs2svn to create tag 'wxPython-2_1b1'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/wxPython-2_1b1@2916 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-aclocal -I .
-autoheader
-automake --foreign --verbose
-autoconf
--- include/wx/helpwin.h
+++
@@ -1,7 +0,0 @@
-#ifndef _WX_HELPWIN_H_BASE_
-#define _WX_HELPWIN_H_BASE_
-
-#include "wx/msw/helpwin.h"
-
-#endif
-    // _WX_HELPWIN_H_BASE_
