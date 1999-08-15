354616b0464a97f8956a4dd68f60eab31c585df5
Guilhem Lavaux <lavaux@easynet.fr>
1999-08-15 10:28:21 +0000

    Commited very sort samples
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@3386 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ utils/wxMMedia2/sample/test_med2.cpp
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------------------
+// Name: test_med.cpp
+// Purpose:
+// Date: 08/11/1999
+// Author: Guilhem Lavaux <lavaux@easynet.fr> (C) 1999
+// CVSID: $Id$
+// --------------------------------------------------------------------------
+#include <wx/app.h>
+#include <wx/wfstream.h>
+#include <wx/frame.h>
+#include "../lib/sndoss.h"
+#include "../lib/sndwav.h"
+#include "../lib/sndaiff.h"
+
+class MyApp: public wxApp {
+ bool OnInit() {
+   wxSoundStreamOSS *oss_dev = new wxSoundStreamOSS();
+   wxFileInputStream *f_input = new wxFileInputStream(argv[1]);
+   wxSoundWave *wav_file = new wxSoundWave(*f_input, *oss_dev);
+   wxFrame *frame = new wxFrame(NULL, -1, "My Frame");
+   wxSoundFormatPcm pcm;
+
+   if (oss_dev->GetError() != wxSOUND_NOERR) {
+     wxPrintf("No device\n");
+     return FALSE;
+   }
+
+   wav_file->Play();
+   frame->Show(TRUE);
+   return TRUE;
+ }
+};
+
+IMPLEMENT_APP(MyApp)
