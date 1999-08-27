d127d9360fd8603c66b45eaa4681950bfceb252b
Michael Bedward <mbedward@ozemail.com.au>
1999-08-27 08:22:32 +0000

    Fixes for compilation under AIX.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@3495 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/docview.cpp
+++ src/common/docview.cpp
@@ -282,10 +282,10 @@ bool wxDocument::OnSaveDocument(const wxString& file)
         msgTitle = wxString(_("File error"));
 
 #if wxUSE_STD_IOSTREAM
-    ofstream store(FNSTRINGCAST file.fn_str());
+    ofstream store(wxString(file.fn_str()));
     if (store.fail() || store.bad())
 #else
-    wxFileOutputStream store(FNSTRINGCAST file.fn_str());
+    wxFileOutputStream store(wxString(file.fn_str()));
     if (store.LastError() != 0)
 #endif
     {
@@ -318,10 +318,10 @@ bool wxDocument::OnOpenDocument(const wxString& file)
         msgTitle = wxString(_("File error"));
 
 #if wxUSE_STD_IOSTREAM
-    ifstream store(FNSTRINGCAST file.fn_str());
+    ifstream store(wxString(file.fn_str()));
     if (store.fail() || store.bad())
 #else
-    wxFileInputStream store(FNSTRINGCAST file.fn_str());
+    wxFileInputStream store(wxString(file.fn_str()));
     if (store.LastError() != 0)
 #endif
     {
