f3391ea5e0a7d67e1fc725c6353ff39daabf898c
Michael Bedward <mbedward@ozemail.com.au>
1999-08-29 06:30:50 +0000

    Script runs tmake for all template files in distrib/msw/tmake. Writes wxMSW makefiles to src/msw. Writes Makefile.in to top wxWindows dir.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@3514 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/tmake/makeall.sh
+++ distrib/msw/tmake/makeall.sh
@@ -1,40 +1,54 @@
 #!/bin/sh
 #
+# File:      makeall.sh
+# Purpose:   create wxWindows makefiles for all compilers
+# Author:    Michael Bedward
+# Created:   29 Aug 1999
+# Copyright: (c) 1999 Michael Bedward
+# Version:   $Id$
+#
+# This creates the makefiles for all compilers from the templates using
+# tmake. The tmake executable should be in the path.  
+
+# Assume we are in distrib/msw/tmake
+#
+topdir="../../.."
+mswdir="$topdir/src/msw"
 
 for tname in `ls *.t` 
 do
     case $tname in
 	b32.t)
-	    echo "Generating for Borland C++ (32 bits)..."
-	    tmake -t b32 wxwin.pro -o makefile.b32;;
+	    echo "Generating $mswdir/makefile.b32 for Borland C++ (32 bits)..."
+	    tmake -t b32 wxwin.pro -o $mswdir/makefile.b32 ;;
 
 	bcc.t)
-	    echo "Generating for Borland C++ (16 bits)..."
-	    tmake -t bcc wxwin.pro -o makefile.bcc;;
+	    echo "Generating $mswdir/makefile.bcc for Borland C++ (16 bits)..."
+	    tmake -t bcc wxwin.pro -o $mswdir/makefile.bcc;;
 
 	dos.t)
-	    echo "Generating for Visual C++ 1.52..."
-	    tmake -t dos wxwin.pro -o makefile.dos;;
+	    echo "Generating $mswdir/makefile.dos for Visual C++ 1.52..."
+	    tmake -t dos wxwin.pro -o $mswdir/makefile.dos;;
 
 	g95.t)
-	    echo "Generating for Cygwin/Mingw32..."
-	    tmake -t g95 wxwin.pro -o makefile.g95;;
+	    echo "Generating $mswdir/makefile.g95 for Cygwin/Mingw32..."
+	    tmake -t g95 wxwin.pro -o $mswdir/makefile.g95;;
 
 	sc.t)
-	    echo "Generating for Symantec C++..."
-	    tmake -t sc wxwin.pro -o makefile.sc;;
+	    echo "Generating $mswdir/makefile.sc for Symantec C++..."
+	    tmake -t sc wxwin.pro -o $mswdir/makefile.sc;;
 
 	vc.t) 
-	    echo "Generating for Visual C++ 4.0..."
-	    tmake -t vc wxwin.pro -o makefile.vc;;
+	    echo "Generating $mswdir/makefile.vc for Visual C++ 4.0..."
+	    tmake -t vc wxwin.pro -o $mswdir/makefile.vc;;
 
 	wat.t)
-	    echo "Generating for Watcom C++..."
-	    tmake -t wat wxwin.pro -o makefile.wat;;
+	    echo "Generating $mswdir/makefile.wat for Watcom C++..."
+	    tmake -t wat wxwin.pro -o $mswdir/makefile.wat;;
 
 	unx.t)
-	    echo "Generating for Unix and Configure..."
-	    tmake -t unx wxwin.pro -o Makefile.in;;
+	    echo "Generating $topdir/Makefile.in for Unix and Configure..."
+	    tmake -t unx wxwin.pro -o $topdir/Makefile.in;;
     esac
 done
 
