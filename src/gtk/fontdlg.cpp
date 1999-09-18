0b862e2069ae3f91f1b0ebf53e74602fa1e0b303
Vadim Zeitlin <vadim@wxwidgets.org>
1999-09-18 16:42:13 +0000

    font selector dialog doesn't work with GTK+ 1.0
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@3705 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/fontdlg.cpp
+++ src/gtk/fontdlg.cpp
@@ -33,7 +33,7 @@ extern bool g_isIdle;
 static
 bool gtk_fontdialog_delete_callback( GtkWidget *WXUNUSED(widget), GdkEvent *WXUNUSED(event), wxDialog *win )
 {
-    if (g_isIdle) 
+    if (g_isIdle)
         wxapp_install_idle_handler();
 
 /*
@@ -52,30 +52,32 @@ bool gtk_fontdialog_delete_callback( GtkWidget *WXUNUSED(widget), GdkEvent *WXUN
 // "clicked" for OK-button
 //-----------------------------------------------------------------------------
 
+#ifdef __WXGTK12__
 static
 void gtk_fontdialog_ok_callback( GtkWidget *WXUNUSED(widget), wxFontDialog *dialog )
 {
-    if (g_isIdle) 
+    if (g_isIdle)
         wxapp_install_idle_handler();
 
     GtkFontSelectionDialog *fontdlg = GTK_FONT_SELECTION_DIALOG(dialog->m_widget);
     GdkFont *gfont = gtk_font_selection_dialog_get_font(fontdlg);
-    
+
     if (!gfont)
     {
         wxMessageBox(_("Please choose a valid font."), _("Error"), wxOK);
         return;
     }
-    
+
     gchar *fontname = gtk_font_selection_dialog_get_font_name(fontdlg);
     wxFont font( gfont, fontname );
     g_free( fontname );
     dialog->m_fontData.SetChosenFont( font );
-    
+
     wxCommandEvent event(wxEVT_COMMAND_BUTTON_CLICKED, wxID_OK);
     event.SetEventObject( dialog );
     dialog->GetEventHandler()->ProcessEvent( event );
 }
+#endif // GTK+ 1.2 andlater only
 
 //-----------------------------------------------------------------------------
 // "clicked" for Cancel-button
@@ -84,7 +86,7 @@ void gtk_fontdialog_ok_callback( GtkWidget *WXUNUSED(widget), wxFontDialog *dial
 static
 void gtk_fontdialog_cancel_callback( GtkWidget *WXUNUSED(w), wxFontDialog *dialog )
 {
-    if (g_isIdle) 
+    if (g_isIdle)
         wxapp_install_idle_handler();
 
     wxCommandEvent event(wxEVT_COMMAND_BUTTON_CLICKED, wxID_CANCEL);
@@ -103,13 +105,15 @@ wxFontDialog::wxFontDialog( wxWindow *parent, wxFontData *data )
     m_needParent = FALSE;
 
     if (!PreCreation( parent, wxDefaultPosition, wxDefaultSize ) ||
-        !CreateBase( parent, -1, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, 
-	             wxDefaultValidator, _T("fontdialog") ))
+        !CreateBase( parent, -1, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE,
+                     wxDefaultValidator, _T("fontdialog") ))
     {
         wxFAIL_MSG( _T("wxXX creation failed") );
-	return;
+        return;
     }
-    
+#ifndef __WXGTK12__
+    wxFAIL_MSG( _T("TODO") );
+#else // GTK+ 1.2
     wxString m_message( _("Choose font") );
     m_widget = gtk_font_selection_dialog_new( m_message.mbc_str() );
 
@@ -118,7 +122,7 @@ wxFontDialog::wxFontDialog( wxWindow *parent, wxFontData *data )
     gtk_widget_set_uposition( m_widget, x, y );
 
     GtkFontSelectionDialog *sel = GTK_FONT_SELECTION_DIALOG(m_widget);
-    
+
     gtk_signal_connect( GTK_OBJECT(sel->ok_button), "clicked",
       GTK_SIGNAL_FUNC(gtk_fontdialog_ok_callback), (gpointer*)this );
 
@@ -127,12 +131,13 @@ wxFontDialog::wxFontDialog( wxWindow *parent, wxFontData *data )
 
     gtk_signal_connect( GTK_OBJECT(sel->cancel_button), "clicked",
       GTK_SIGNAL_FUNC(gtk_fontdialog_cancel_callback), (gpointer*)this );
-      
+
     // strange way to internationalize
     gtk_label_set( GTK_LABEL( GTK_BUTTON(sel->cancel_button)->child ), wxConvCurrent->cWX2MB(_("Cancel")) );
-    
+
     gtk_signal_connect( GTK_OBJECT(m_widget), "delete_event",
         GTK_SIGNAL_FUNC(gtk_fontdialog_delete_callback), (gpointer)this );
+#endif // GTK+ version
 }
 
 wxFontDialog::~wxFontDialog()
