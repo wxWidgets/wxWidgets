ccba72313c563d3544c38eadc7d012c690b7c6fc
Robin Dunn <robin@alldunn.com>
1999-10-07 22:28:44 +0000

    distribution updates
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@3885 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- utils/wxPython/distrib/zipit.bat
+++ utils/wxPython/distrib/zipit.bat
@@ -5,13 +5,19 @@
 zip -@ -r wxPython\wxPython-src-%1.zip < wxPython\distrib\wxPython.rsp
 
 mkdir wxPython\docs
-copy %WXWIN%\docs\html\wx\*.gif wxPython\docs
-copy %WXWIN%\docs\html\wx\*.htm wxPython\docs
-copy wxPython\docs\wx.htm wxPython\docs\index.htm
+mkdir wxPython\docs\wx
+copy %WXWIN%\docs\html\wx\*.gif wxPython\docs\wx
+copy %WXWIN%\docs\html\wx\*.htm wxPython\docs\wx
+copy wxPython\docs\wx.htm wxPython\docs\wx\index.htm
+
+mkdir wxPython\docs\ogl
+copy %WXWIN%\docs\html\ogl\*.gif wxPython\docs\ogl
+copy %WXWIN%\docs\html\ogl\*.htm wxPython\docs\ogl
+copy wxPython\docs\ogl.htm wxPython\docs\ogl\index.htm
+
 
 zip -r wxPython\wxPython-docs-%1.zip wxPython\docs
 
-del /y wxPython\docs\*.*
-rmdir wxPython\docs
+del /sxzy wxPython\docs
 
 move /R wxPython\*.zip wxPython\distrib
