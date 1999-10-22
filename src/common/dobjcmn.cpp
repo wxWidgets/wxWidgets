c32a8bd4dbc7ce0acb5e22e5558f66a566b23cc8
Vadim Zeitlin <vadim@wxwidgets.org>
1999-10-22 12:27:43 +0000

    another BC++ fix
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@4132 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/dobjcmn.cpp
+++ src/common/dobjcmn.cpp
@@ -97,7 +97,7 @@ wxDataObjectComposite::GetPreferredFormat(Direction WXUNUSED(dir)) const
 {
     wxSimpleDataObjectList::Node *node = m_dataObjects.Item( m_preferred );
 
-    wxCHECK_MSG( node, wxDF_INVALID, wxT("no preferred format") );
+    wxCHECK_MSG( node, wxFormatInvalid, wxT("no preferred format") );
 
     wxDataObjectSimple* dataObj = node->GetData();
 
