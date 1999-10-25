84a3fe2c9b1690df252be5b9553082e72d09a6f6
Julian Smart <julian@anthemion.co.uk>
1999-10-25 14:30:09 +0000

    Added wizard sample.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@4183 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- utils/projgen/makeproj.cpp
+++ utils/projgen/makeproj.cpp
@@ -270,6 +270,7 @@ void MyApp::GenerateSamples(const wxString& dir)
     GenerateSample("CaretVC", "caret", dir + wxString("/samples/caret"), wxStringList("caret.cpp", 0));
     GenerateSample("DrawingVC", "drawing", dir + wxString("/samples/drawing"), wxStringList("drawing.cpp", 0));
     GenerateSample("ScrollVC", "scroll", dir + wxString("/samples/scroll"), wxStringList("scroll.cpp", 0));
+    GenerateSample("WizardVC", "wizard", dir + wxString("/samples/wizard"), wxStringList("wiztest.cpp", 0));
 
     //// Utilities
 
