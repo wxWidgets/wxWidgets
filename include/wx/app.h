6a370f427a1c7ac3ca7b6b7ff8a29c3fc8734fcd
Jouk Jansen <joukj@hrem.nano.tudelft.nl>
1999-11-12 11:18:54 +0000

    Committing in .
     VMS patch
    
     Modified Files:
     	wxWindows/include/wx/app.h
    
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@4498 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/app.h
+++ include/wx/app.h
@@ -356,7 +356,7 @@ public:
 // be in your main program (e.g. hello.cpp). Now IMPLEMENT_APP should add this
 // code if required.
 
-#if defined(__AIX__) || defined(__HPUX__)
+#if defined(__AIX__) || defined(__HPUX__) || defined( __VMS__ )
     #define IMPLEMENT_WXWIN_MAIN \
         extern int wxEntry( int argc, char *argv[] ); \
         int main(int argc, char *argv[]) { return wxEntry(argc, argv); }
