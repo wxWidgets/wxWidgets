7a0d28d8e9b299c1e4e9572cd51f05532cfba62f
Stefan Neis <Stefan.Neis@t-online.de>
2000-01-14 22:50:09 +0000

    SN: Added #pragma implementation. Make it compile even if wxOwnerDrawn == 0
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@5407 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/os2/menuitem.cpp
+++ src/os2/menuitem.cpp
@@ -13,6 +13,10 @@
 // headers & declarations
 // ============================================================================
 
+#ifdef __GNUG__
+    #pragma implementation "menuitem.h"
+#endif
+
 // For compilers that support precompilation, includes "wx.h".
 #include "wx/wxprec.h"
 
@@ -76,9 +80,9 @@ wxMenuItem::wxMenuItem(wxMenu *pParentMenu,
                        const wxString& text,
                        const wxString& strHelp,
                        bool bCheckable,
-                       wxMenu *pSubMenu) :
+                       wxMenu *pSubMenu)
 #if wxUSE_OWNER_DRAWN
-                        wxOwnerDrawn(text, bCheckable)
+                      :  wxOwnerDrawn(text, bCheckable)
 #endif // owner drawn
 {
     wxASSERT_MSG( pParentMenu != NULL, wxT("a menu item should have a parent") );
