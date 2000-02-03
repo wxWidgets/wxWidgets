8f0b51916ec4ad8454eec508d7ca2f066e787341
Bart A.M. Jourquin <bart.jourquin@fucam.ac.be>
2000-02-03 13:17:32 +0000

    remove c++ comment in header
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@5826 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/iodbc/isqlext.h
+++ src/iodbc/isqlext.h
@@ -1079,7 +1079,7 @@ RETCODE SQL_API SQLColumns (HSTMT hstmt, UCHAR FAR * szTableQualifier,
     SWORD cbTableQualifier, UCHAR FAR * szTableOwner, SWORD cbTableOwner,
     UCHAR FAR * szTableName, SWORD cbTableName, UCHAR FAR * szColumnName,
     SWORD cbColumnName);
-RETCODE SQL_API SQLDriverConnect (HDBC hdbc, SQLHWND hwnd,  //glt
+RETCODE SQL_API SQLDriverConnect (HDBC hdbc, SQLHWND hwnd, 
     UCHAR FAR * szConnStrIn, SWORD cbConnStrIn, UCHAR FAR * szConnStrOut,
     SWORD cbConnStrOutMax, SWORD FAR * pcbConnStrOut, UWORD fDriverCompletion);
 RETCODE SQL_API SQLGetConnectOption (HDBC hdbc, UWORD fOption, PTR pvParam);
