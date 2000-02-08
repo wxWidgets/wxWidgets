fa1993cce56b3fbda1d30bcf61e78ac2014ebccd
Guillermo Rodriguez Garcia <guille@iies.es>
2000-02-08 02:09:54 +0000

    M_PI is not ANSI
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@5897 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/rotate/rotate.cpp
+++ samples/rotate/rotate.cpp
@@ -22,7 +22,14 @@
 
 #include "wx/image.h"
 
+/* GRG: This is not ANSI standard, define M_PI explicitly
 #include <math.h>       // M_PI
+*/
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
+
 
 class MyApp: public wxApp
 {
