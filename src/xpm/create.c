a7846525719dfd4949b0873f75fbfcb781185d52
Julian Smart <julian@anthemion.co.uk>
2000-02-15 19:26:20 +0000

    Typo corrected
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@6052 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xpm/create.c
+++ src/xpm/create.c
@@ -267,7 +267,7 @@ AllocColor(
 static int
 AllocColor(display, colormap, colorname, xcolor, closure)
     Display *display;
-    Colormap* colormap;
+    Colormap colormap;
     char *colorname;
     XColor *xcolor;
     void *closure;		/* not used */
