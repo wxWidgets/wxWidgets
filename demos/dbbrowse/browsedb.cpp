541418efad404d9ba69bf262833a8977e216d18c
Julian Smart <julian@anthemion.co.uk>
2000-03-18 13:19:54 +0000

    Corrected WXDLLEXPORT keyword position.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@6821 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demos/dbbrowse/browsedb.cpp
+++ demos/dbbrowse/browsedb.cpp
@@ -34,7 +34,7 @@
 // - darf nur einmal im Projekte definiert werden ?? Extra Databasse Klasse ?
 wxDbConnectInf ConnectInf;      // Für DBase
 //----------------------------------------------------------------------------------------
-extern wxDbList* WXDLLEXPORT PtrBegDbList;    /* from db.cpp, used in getting back error results from db connections */
+extern WXDLLEXPORT wxDbList* PtrBegDbList;    /* from db.cpp, used in getting back error results from db connections */
 //----------------------------------------------------------------------------------------
 char *GetExtendedDBErrorMsg(char *ErrFile, int ErrLine)
 {
