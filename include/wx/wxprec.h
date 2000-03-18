6040c9de7421d2c7770d7f54687a2413be5a79d1
Stefan Neis <Stefan.Neis@t-online.de>
2000-03-18 22:50:07 +0000

    No precompiled headers for gcc, even if compiling WXPM.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@6835 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/wxprec.h
+++ include/wx/wxprec.h
@@ -14,7 +14,7 @@
 
 // check if to use precompiled headers: do it for most Windows compilers unless
 // explicitly disabled by defining NOPCH
-#if ((defined(__BORLANDC__) || defined(__VISUALC__) || defined(__WATCOMC__)) && defined(__WXMSW__)) || defined(__WXPM__)
+#if ((defined(__BORLANDC__) || defined(__VISUALC__) || defined(__WATCOMC__)) && defined(__WXMSW__)) || defined(__VISAGECPP__)
     #if !defined(NOPCH)
         #define WX_PRECOMP
     #endif
