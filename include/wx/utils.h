04e158348ef94832828539c38cefe2c40debc778
Ron Lee <ron@debian.org>
2000-06-09 21:15:49 +0000

    fix for wxBusyCursorSuspender messing with wxIsBusy() state.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@7553 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/utils.h
+++ include/wx/utils.h
@@ -326,8 +326,16 @@ class WXDLLEXPORT wxBusyCursor
 public:
     wxBusyCursor(wxCursor* cursor = wxHOURGLASS_CURSOR)
         { wxBeginBusyCursor(cursor); }
-   ~wxBusyCursor()
+    ~wxBusyCursor()
         { wxEndBusyCursor(); }
+
+    // FIXME: These two methods are currently only implemented (and needed?)
+    //        in wxGTK.  BusyCursor handling should probably be moved to
+    //        common code since the wxGTK and wxMSW implementations are very
+    //        similar except for wxMSW using HCURSOR directly instead of
+    //        wxCursor..  -- RL.
+    static const wxCursor &GetStoredCursor();
+    static const wxCursor GetBusyCursor();
 };
 
 
