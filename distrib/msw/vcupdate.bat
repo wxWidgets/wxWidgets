cba254cf9a9f3ce7f42b61f1227d2054f77cfecf
Julian Smart <julian@anthemion.co.uk>
2000-07-23 15:24:03 +0000

    Added scripts to update .dsp files when version changes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@7810 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ distrib/msw/vcupdate.bat
@@ -0,0 +1,9 @@
+@echo off
+Rem Replace the DLL version number in the VC++ project files
+
+input Enter the old version name (e.g. wx22_0): %%oldName
+input Enter the new version name (e.g. wx22_1): %%newName
+
+call %wxwin\distrib\msw\filerepl.bat %oldName %newName %wxwin\distrib\msw\vc.rsp
+echo Done.
+
