211686199389c7c9d7001ca2ea5a8022548aa2f4
Robert Roebling <robert@roebling.de>
2000-08-19 08:56:44 +0000

    Added wxChoice::GetCount() and Select()
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@8137 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/choice.h
+++ include/wx/mac/choice.h
@@ -52,6 +52,7 @@ class WXDLLEXPORT wxChoice: public wxControl
   virtual void Clear();
   virtual int GetSelection() const ;
   virtual void SetSelection(int n);
+  virtual inline void Select( int n ) { SetSelection( n ); }
   virtual int FindString(const wxString& s) const;
   virtual wxString GetString(int n) const ;
   virtual void SetSize(int x, int y, int width, int height, int sizeFlags = wxSIZE_AUTO);
@@ -59,6 +60,7 @@ class WXDLLEXPORT wxChoice: public wxControl
   virtual bool SetStringSelection(const wxString& sel);
 
   virtual inline int Number() const { return m_strings.GetCount(); }
+  virtual inline int GetCount() const { return m_strings.GetCount(); }
   virtual void Command(wxCommandEvent& event);
 
   virtual inline void SetColumns(int WXUNUSED(n) = 1 ) { /* No effect */ } ;
