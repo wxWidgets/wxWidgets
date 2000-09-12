fcaf9e704e1a622af0a1d84583a63e0c1c9dd9b2
Robert Roebling <robert@roebling.de>
2000-09-12 08:17:23 +0000

    Added test for bitmap menu.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@8334 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/menu/menu.cpp
+++ samples/menu/menu.cpp
@@ -26,9 +26,10 @@
 
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
-
     #include <wx/log.h>
 #endif
+
+#include "copy.xpm"
 
 // ----------------------------------------------------------------------------
 // classes
@@ -232,6 +233,10 @@ MyFrame::MyFrame()
     // create the menubar
     wxMenu *fileMenu = new wxMenu;
     fileMenu->Append(Menu_File_Quit, "E&xit\tAlt-X", "Quit toolbar sample" );
+    
+    wxMenuItem *bitmap_menu_item = new wxMenuItem( fileMenu, Menu_File_Quit, "Quit with &bitmap" );
+    bitmap_menu_item->SetBitmap( wxBitmap( copy_xpm ) );
+    fileMenu->Append( bitmap_menu_item ); 
 
     wxMenu *menubarMenu = new wxMenu;
     menubarMenu->Append(Menu_MenuBar_Append, "&Append menu\tCtrl-A",
