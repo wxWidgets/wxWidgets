4c075b7090b8875b3810f5a8efcde1ecd4f35216
Robert Roebling <robert@roebling.de>
2001-01-25 18:27:44 +0000

    Beautified header.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@9168 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/datstrm.h
+++ include/wx/datstrm.h
@@ -20,59 +20,63 @@
 
 #if wxUSE_STREAMS
 
-class WXDLLEXPORT wxDataInputStream {
+class WXDLLEXPORT wxDataInputStream
+{
 public:
-  wxDataInputStream(wxInputStream& s);
-  ~wxDataInputStream();
+    wxDataInputStream(wxInputStream& s);
+    ~wxDataInputStream();
 
-  wxUint32 Read32();
-  wxUint16 Read16();
-  wxUint8 Read8();
-  double ReadDouble();
-  wxString ReadString();
+    wxUint32 Read32();
+    wxUint16 Read16();
+    wxUint8 Read8();
+    double ReadDouble();
+    wxString ReadString();
 
-  wxDataInputStream& operator>>(wxString& s);
-  wxDataInputStream& operator>>(wxInt8& c);
-  wxDataInputStream& operator>>(wxInt16& i);
-  wxDataInputStream& operator>>(wxInt32& i);
-  wxDataInputStream& operator>>(wxUint8& c);
-  wxDataInputStream& operator>>(wxUint16& i);
-  wxDataInputStream& operator>>(wxUint32& i);
-  wxDataInputStream& operator>>(double& i);
-  wxDataInputStream& operator>>(float& f);
+    wxDataInputStream& operator>>(wxString& s);
+    wxDataInputStream& operator>>(wxInt8& c);
+    wxDataInputStream& operator>>(wxInt16& i);
+    wxDataInputStream& operator>>(wxInt32& i);
+    wxDataInputStream& operator>>(wxUint8& c);
+    wxDataInputStream& operator>>(wxUint16& i);
+    wxDataInputStream& operator>>(wxUint32& i);
+    wxDataInputStream& operator>>(double& i);
+    wxDataInputStream& operator>>(float& f);
 
-  void BigEndianOrdered(bool be_order) { m_be_order = be_order; }
- protected:
-  wxInputStream *m_input;
-  bool m_be_order;
+    void BigEndianOrdered(bool be_order) { m_be_order = be_order; }
+  
+protected:
+    wxInputStream *m_input;
+    bool m_be_order;
 };
 
-class WXDLLEXPORT wxDataOutputStream {
- public:
-  wxDataOutputStream(wxOutputStream& s);
-  ~wxDataOutputStream();
+class WXDLLEXPORT wxDataOutputStream
+{
+public:
+    wxDataOutputStream(wxOutputStream& s);
+    ~wxDataOutputStream();
 
-  void Write32(wxUint32 i);
-  void Write16(wxUint16 i);
-  void Write8(wxUint8 i);
-  void WriteDouble(double d);
-  void WriteString(const wxString& string);
+    void Write32(wxUint32 i);
+    void Write16(wxUint16 i);
+    void Write8(wxUint8 i);
+    void WriteDouble(double d);
+    void WriteString(const wxString& string);
 
-  wxDataOutputStream& operator<<(const wxChar *string);
-  wxDataOutputStream& operator<<(wxString& string);
-  wxDataOutputStream& operator<<(wxInt8 c);
-  wxDataOutputStream& operator<<(wxInt16 i);
-  wxDataOutputStream& operator<<(wxInt32 i);
-  wxDataOutputStream& operator<<(wxUint8 c);
-  wxDataOutputStream& operator<<(wxUint16 i);
-  wxDataOutputStream& operator<<(wxUint32 i);
-  wxDataOutputStream& operator<<(double f);
-  wxDataOutputStream& operator<<(float f);
+    wxDataOutputStream& operator<<(const wxChar *string);
+    wxDataOutputStream& operator<<(wxString& string);
+    wxDataOutputStream& operator<<(wxInt8 c);
+    wxDataOutputStream& operator<<(wxInt16 i);
+    wxDataOutputStream& operator<<(wxInt32 i);
+    wxDataOutputStream& operator<<(wxUint8 c);
+    wxDataOutputStream& operator<<(wxUint16 i);
+    wxDataOutputStream& operator<<(wxUint32 i);
+    wxDataOutputStream& operator<<(double f);
+    wxDataOutputStream& operator<<(float f);
 
-  void BigEndianOrdered(bool be_order) { m_be_order = be_order; } 
- protected:
-  wxOutputStream *m_output;
-  bool m_be_order;
+    void BigEndianOrdered(bool be_order) { m_be_order = be_order; } 
+  
+protected:
+    wxOutputStream *m_output;
+    bool m_be_order;
 };
 
 #endif
