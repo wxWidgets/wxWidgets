97ffd370f4c7c52f8ccf6cdea73a86e17da5cb83
Vadim Zeitlin <vadim@wxwidgets.org>
2001-02-26 10:03:12 +0000

    applied patch 403956 (gcc warning fix)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@9428 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/html/htmlcell.h
+++ include/wx/html/htmlcell.h
@@ -314,7 +314,7 @@ class WXDLLEXPORT wxHtmlLinkInfo : public wxObject
                 { m_Href = m_Target = wxEmptyString; m_Event = NULL, m_Cell = NULL; }
         wxHtmlLinkInfo(const wxString& href, const wxString& target = wxEmptyString) : wxObject()
                 { m_Href = href; m_Target = target; m_Event = NULL, m_Cell = NULL; }
-        wxHtmlLinkInfo(const wxHtmlLinkInfo& l)
+        wxHtmlLinkInfo(const wxHtmlLinkInfo& l) : wxObject()
                 { m_Href = l.m_Href, m_Target = l.m_Target, m_Event = l.m_Event, m_Cell = l.m_Cell; }
         wxHtmlLinkInfo& operator=(const wxHtmlLinkInfo& l)
                 { m_Href = l.m_Href, m_Target = l.m_Target, m_Event = l.m_Event, m_Cell = l.m_Cell; return *this; }
