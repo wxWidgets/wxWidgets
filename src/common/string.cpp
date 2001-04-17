f7742addb130a6e21250268d77826d7590a18a56
Vadim Zeitlin <vadim@wxwidgets.org>
2001-04-17 22:34:02 +0000

    wxString::FormatV() fix (bug 416853)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@9780 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/string.cpp
+++ src/common/string.cpp
@@ -1143,7 +1143,7 @@ wxString wxString::Format(const wxChar *pszFormat, ...)
 wxString wxString::FormatV(const wxChar *pszFormat, va_list argptr)
 {
     wxString s;
-    s.Printf(pszFormat, argptr);
+    s.PrintfV(pszFormat, argptr);
     return s;
 }
 
