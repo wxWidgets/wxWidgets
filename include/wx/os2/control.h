d608dde1d73fdbacbe05ddf81c252da945c2e71a
David Webster <Dave.Webster@bhmi.com>
2001-04-18 20:46:23 +0000

    wxControl and wxDialog coded.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@9786 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/os2/control.h
+++ include/wx/os2/control.h
@@ -23,96 +23,126 @@ class WXDLLEXPORT wxControl : public wxControlBase
 
 public:
    wxControl();
-   wxControl(wxWindow *parent,
-             wxWindowID id,
-             const wxPoint& pos = wxDefaultPosition,
-             const wxSize& size = wxDefaultSize,
-             long style = 0,
+   wxControl( wxWindow*          pParent
+             ,wxWindowID         vId
+             ,const wxPoint&     rPos = wxDefaultPosition
+             ,const wxSize&      rSize = wxDefaultSize
+             ,long               lStyle = 0
 #if wxUSE_VALIDATORS
-             const wxValidator& validator = wxDefaultValidator,
+             ,const wxValidator& rValidator = wxDefaultValidator
 #endif
-             const wxString& name = wxControlNameStr)
+             ,const wxString&    rsName = wxControlNameStr
+            )
     {
-        Create(parent, id, pos, size, style, validator, name);
+        Create( pParent
+               ,vId
+               ,rPos
+               ,rSize
+               ,lStyle
+               ,rValidator
+               ,rsName
+              );
     }
-
-    bool Create(wxWindow *parent, wxWindowID id,
-            const wxPoint& pos = wxDefaultPosition,
-            const wxSize& size = wxDefaultSize, long style = 0,
-#if wxUSE_VALIDATORS
-            const wxValidator& validator = wxDefaultValidator,
-#endif
-            const wxString& name = wxControlNameStr);
     virtual ~wxControl();
 
-   // Simulates an event
-   virtual void Command(wxCommandEvent& event) { ProcessCommand(event); }
+    bool Create( wxWindow*          pParent
+                ,wxWindowID         vId
+                ,const wxPoint&     rPos = wxDefaultPosition
+                ,const wxSize&      rSize = wxDefaultSize
+                ,long               lStyle = 0
+#if wxUSE_VALIDATORS
+                ,const wxValidator& rValidator = wxDefaultValidator
+#endif
+                ,const wxString&    rsName = wxControlNameStr
+               );
 
-   // implementation from now on
-   // --------------------------
+    //
+    // Simulates an event
+    //
+    virtual void Command(wxCommandEvent& rEvent) { ProcessCommand(rEvent); }
 
-   // Calls the callback and appropriate event handlers
-   bool ProcessCommand(wxCommandEvent& event);
+    //
+    // Implementation from now on
+    // --------------------------
+    //
 
-   // OS2-specific
-   virtual bool OS2OnNotify(int idCtrl, WXLPARAM lParam, WXLPARAM *result);
+    //
+    // Calls the callback and appropriate event handlers
+    //
+    bool ProcessCommand(wxCommandEvent& rEvent);
 
-   // For ownerdraw items
-   virtual bool OS2OnDraw(WXDRAWITEMSTRUCT *WXUNUSED(item)) { return FALSE; };
-   virtual bool OS2OnMeasure(WXMEASUREITEMSTRUCT *WXUNUSED(item)) { return FALSE; };
+    //
+    // For ownerdraw items
+    //
+    virtual bool OS2OnDraw(WXDRAWITEMSTRUCT* WXUNUSED(pItem)) { return FALSE; };
+    virtual bool OS2OnMeasure(WXMEASUREITEMSTRUCT* WXUNUSED(pItem)) { return FALSE; };
 
-   wxArrayLong& GetSubcontrols() { return m_subControls; }
-
-   void OnEraseBackground(wxEraseEvent& event);
+    wxArrayLong&     GetSubcontrols() { return m_aSubControls; }
+    void             OnEraseBackground(wxEraseEvent& rEvent);
+    virtual WXHBRUSH OnCtlColor( WXHDC    hDC
+                                ,WXHWND   pWnd
+                                ,WXUINT   nCtlColor
+                                ,WXUINT   uMessage
+                                ,WXWPARAM wParam
+                                ,WXLPARAM lParam
+                               );
 
 #if WXWIN_COMPATIBILITY
-   virtual void SetButtonColour(const wxColour& WXUNUSED(col)) { }
-   wxColour* GetButtonColour() const { return NULL; }
+    virtual void SetButtonColour(const wxColour& WXUNUSED(rCol)) { }
+    wxColour*    GetButtonColour(void) const { return NULL; }
 
-   inline virtual void SetLabelFont(const wxFont& font);
-   inline virtual void SetButtonFont(const wxFont& font);
-   inline wxFont& GetLabelFont() const;
-   inline wxFont& GetButtonFont() const;
+    inline virtual void SetLabelFont(const wxFont& rFont);
+    inline virtual void SetButtonFont(const wxFont& rFont);
+    inline wxFont&      GetLabelFont(void) const;
+    inline wxFont&      GetButtonFont(void) const;
 
-   // Adds callback
-   inline void Callback(const wxFunction function);
-
-   wxFunction GetCallback() { return m_callback; }
+    //
+    // Adds callback
+    //
+    inline void Callback(const wxFunction function);
+    wxFunction  GetCallback(void) { return m_callback; }
 
 protected:
-   wxFunction       m_callback;     // Callback associated with the window
+    wxFunction                      m_callback;     // Callback associated with the window
 #endif // WXWIN_COMPATIBILITY
 
 protected:
-   // For controls like radiobuttons which are really composite
-   wxArrayLong m_subControls;
+    //
+    // For controls like radiobuttons which are really composite
+    //
+    wxArrayLong m_aSubControls;
 
-   virtual wxSize DoGetBestSize() const;
+    virtual wxSize DoGetBestSize(void) const;
 
-   // create the control of the given class with the given style, returns FALSE
-   // if creation failed
-   bool OS2CreateControl(const wxChar *classname, WXDWORD style,
-                         const wxPoint& pos = wxDefaultPosition,
-                         const wxSize& size = wxDefaultSize,
-                         const wxString& label = wxEmptyString,
-                         WXDWORD exstyle = (WXDWORD)-1);
+    //
+    // Create the control of the given class with the given style, returns FALSE
+    // if creation failed.
+    //
+    bool OS2CreateControl( const wxChar*   zClassname
+                          ,WXDWORD         dwStyle
+                          ,const wxPoint&  rPos = wxDefaultPosition
+                          ,const wxSize&   rSize = wxDefaultSize
+                          ,const wxString& rsLabel = wxEmptyString
+                          ,WXDWORD         dwExstyle = (WXDWORD)-1
+                         );
 
-   // determine the extended styles combination for this window (may slightly
-   // modify styl parameter)
-   WXDWORD GetExStyle(WXDWORD& style) const;
+    //
+    // Determine the extended styles combination for this window (may slightly
+    // modify styl parameter)
+    //
+    WXDWORD GetExStyle(WXDWORD& rStyle) const;
 
 private:
    DECLARE_EVENT_TABLE()
-};
-
+}; // end of wxControl
 
 #if WXWIN_COMPATIBILITY
     inline void wxControl::Callback(const wxFunction f) { m_callback = f; };
-    inline wxFont& wxControl::GetLabelFont() const { return GetFont(); }
-    inline wxFont& wxControl::GetButtonFont() const { return GetFont(); }
-    inline void wxControl::SetLabelFont(const wxFont& font) { SetFont(font); }
-    inline void wxControl::SetButtonFont(const wxFont& font) { SetFont(font); }
+    inline wxFont& wxControl::GetLabelFont(void) const { return GetFont(); }
+    inline wxFont& wxControl::GetButtonFont(void) const { return GetFont(); }
+    inline void wxControl::SetLabelFont(const wxFont& rFont) { SetFont(rFont); }
+    inline void wxControl::SetButtonFont(const wxFont& rFont) { SetFont(rFont); }
 #endif // WXWIN_COMPATIBILITY
 
-#endif
-    // _WX_CONTROL_H_
+#endif // _WX_CONTROL_H_
+
--- include/wx/os2/dialog.h
+++ include/wx/os2/dialog.h
@@ -16,126 +16,165 @@
 
 WXDLLEXPORT_DATA(extern const char*) wxDialogNameStr;
 
+//
 // Dialog boxes
+//
 class WXDLLEXPORT wxDialog: public wxDialogBase
 {
-  DECLARE_DYNAMIC_CLASS(wxDialog)
 public:
 
-  wxDialog();
+  inline wxDialog() { Init(); }
 
+  //
   // Constructor with a modal flag, but no window id - the old convention
-  inline wxDialog( wxWindow*       parent
-                  ,const wxString& title
-                  ,bool            modal
-                  ,int             x = -1
-                  ,int             y = -1
-                  ,int             width = 500
-                  ,int             height = 500
-                  ,long            style = wxDEFAULT_DIALOG_STYLE
-                  ,const wxString& name = wxDialogNameStr
+  //
+  inline wxDialog( wxWindow*       pParent
+                  ,const wxString& rsTitle
+                  ,bool            bModal
+                  ,int             nX = -1
+                  ,int             nY = -1
+                  ,int             nWidth = 500
+                  ,int             nHeight = 500
+                  ,long            lStyle = wxDEFAULT_DIALOG_STYLE
+                  ,const wxString& rsName = wxDialogNameStr
                  )
     {
-        long modalStyle = modal ? wxDIALOG_MODAL : wxDIALOG_MODELESS ;
-        Create(parent, -1, title, wxPoint(x, y), wxSize(width, height), style|modalStyle, name);
+        long                        lModalStyle = lStyle ? wxDIALOG_MODAL : wxDIALOG_MODELESS ;
+
+        Create( pParent
+               ,-1
+               ,rsTitle
+               ,wxPoint(nX, nY)
+               ,wxSize(nWidth, nHeight)
+               ,lStyle | lModalStyle
+               ,rsName
+              );
     }
 
+    //
     // Constructor with no modal flag - the new convention.
-    inline wxDialog( wxWindow*       parent
-                    ,wxWindowID      id
-                    ,const wxString& title
-                    ,const wxPoint&  pos = wxDefaultPosition
-                    ,const wxSize&   size = wxDefaultSize
-                    ,long            style = wxDEFAULT_DIALOG_STYLE
-                    ,const wxString& name = wxDialogNameStr
+    //
+    inline wxDialog( wxWindow*       pParent
+                    ,wxWindowID      vId
+                    ,const wxString& rsTitle
+                    ,const wxPoint&  rPos = wxDefaultPosition
+                    ,const wxSize&   rSize = wxDefaultSize
+                    ,long            lStyle = wxDEFAULT_DIALOG_STYLE
+                    ,const wxString& rsName = wxDialogNameStr
                    )
     {
-        Create(parent, id, title, pos, size, style, name);
+        Create( pParent
+               ,vId
+               ,rsTitle
+               ,rPos
+               ,rSize
+               ,lStyle
+               ,rsName
+              );
     }
 
-    bool Create( wxWindow*       parent
-                ,wxWindowID      id
-                ,const wxString& title
-                , // bool modal = FALSE, // TODO make this a window style?
-                 const wxPoint&  pos = wxDefaultPosition
-                ,const wxSize&   size = wxDefaultSize
-                ,long            style = wxDEFAULT_DIALOG_STYLE
-                ,const wxString& name = wxDialogNameStr
+    bool Create( wxWindow*       pParent
+                ,wxWindowID      vId
+                ,const wxString& rsTitle
+                ,const wxPoint&  rPos = wxDefaultPosition
+                ,const wxSize&   rSize = wxDefaultSize
+                ,long            lStyle = wxDEFAULT_DIALOG_STYLE
+                ,const wxString& rsName = wxDialogNameStr
                );
-
     ~wxDialog();
 
-    virtual bool Destroy();
+    virtual bool Destroy(void);
+    virtual bool Show(bool bShow);
+    virtual void Iconize(bool bIconize);
+    virtual bool IsIconized(void) const;
 
-    virtual void DoSetClientSize(int width, int height);
+    virtual bool IsTopLevel(void) const { return TRUE; }
 
-    virtual void GetPosition(int *x, int *y) const;
+    void         SetModal(bool bFlag);
+    virtual bool IsModal(void) const;
 
-    bool Show(bool show);
-    bool IsShown() const;
-    void Iconize(bool iconize);
+    //
+    // For now, same as Show(TRUE) but returns return code
+    //
+    virtual int  ShowModal(void);
+    virtual void EndModal(int nRetCode);
+
+    //
+    // Returns TRUE if we're in a modal loop
+    //
+    bool IsModalShowing() const;
 
 #if WXWIN_COMPATIBILITY
     bool Iconized() const { return IsIconized(); };
 #endif
 
-    virtual bool IsIconized() const;
-    void         Fit();
+    //
+    // Implementation only from now on
+    // -------------------------------
+    //
 
-    void     SetTitle(const wxString& title);
-    wxString GetTitle() const ;
+    //
+    // Event handlers
+    //
+    bool OnClose(void);
+    void OnCharHook(wxKeyEvent& rEvent);
+    void OnCloseWindow(wxCloseEvent& rEvent);
 
-    void OnSize(wxSizeEvent& event);
-    bool OnClose();
-    void OnCharHook(wxKeyEvent& event);
-    void OnPaint(wxPaintEvent& event);
-    void OnCloseWindow(wxCloseEvent& event);
+    //
+    // May be called to terminate the dialog with the given return code
+    //
 
-    void SetModal(bool flag);
-
-    virtual void Centre(int direction = wxBOTH);
-    virtual bool IsModal() const;
-
-    // For now, same as Show(TRUE) but returns return code
-    virtual int  ShowModal();
-    virtual void EndModal(int retCode);
-
+    //
     // Standard buttons
-    void     OnOK(wxCommandEvent& event);
-    void     OnApply(wxCommandEvent& event);
-    void     OnCancel(wxCommandEvent& event);
+    //
+    void     OnOK(wxCommandEvent& rEvent);
+    void     OnApply(wxCommandEvent& rEvent);
+    void     OnCancel(wxCommandEvent& rEvent);
 
+    //
     // Responds to colour changes
-    void     OnSysColourChanged(wxSysColourChangedEvent& event);
+    //
+    void     OnSysColourChanged(wxSysColourChangedEvent& rEvent);
 
-    // implementation
-    // --------------
-    virtual MRESULT OS2WindowProc(WXUINT message, WXWPARAM wParam, WXLPARAM lParam);
-
-    virtual WXHBRUSH OnCtlColor(WXHDC pDC, WXHWND pWnd, WXUINT nCtlColor,
-                                WXUINT message, WXWPARAM wParam, WXLPARAM lParam);
-
-    bool IsModalShowing() const { return m_modalShowing; }
-
-  // tooltip management
-#if wxUSE_TOOLTIPS
-    WXHWND GetToolTipCtrl() const { return m_hwndToolTip; }
-    void SetToolTipCtrl(WXHWND hwndTT) { m_hwndToolTip = hwndTT; }
-#endif // tooltips
-
+    //
+    // Callbacks
+    //
+    virtual MRESULT OS2WindowProc( WXUINT   uMessage
+                                  ,WXWPARAM wParam
+                                  ,WXLPARAM lParam
+                                 );
 protected:
-    bool   m_modalShowing;
-    WXHWND m_hwndOldFocus;  // the window which had focus before we were shown
+    //
+    // Override more base class virtuals
+    //
+    virtual void DoSetClientSize( int nWidth
+                                 ,int nHeight
+                                );
+    virtual void DoGetPosition( int* pnX
+                               ,int* pnY
+                              ) const;
+    //
+    // Show modal dialog and enter modal loop
+    //
+    void DoShowModal(void);
+
+    //
+    // Common part of all ctors
+    //
+    void Init();
 
 private:
-#if wxUSE_TOOLTIPS
-    WXHWND                m_hwndToolTip;
-#endif // tooltips
+    wxWindow*                       m_pOldFocus;
 
-private:
+    //
+    // While we are showing a modal dialog we disable the other windows using
+    // this object
+    //
+    class wxWindowDisabler*         m_pWindowDisabler;
 
-DECLARE_EVENT_TABLE()
-};
+    DECLARE_DYNAMIC_CLASS(wxDialog)
+    DECLARE_EVENT_TABLE()
+}; // end of CLASS wxDialog
 
-#endif
-    // _WX_DIALOG_H_
+#endif // _WX_DIALOG_H_
+
