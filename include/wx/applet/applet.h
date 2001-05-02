54921697ae6cb35c5865620dce494227b0df5e02
Kendall Bennett <KendallB@scitechsoft.com>
2001-05-02 20:59:28 +0000

    Initial check in of wxApplet code to CVS
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@9971 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/applet/applet.h
@@ -0,0 +1,83 @@
+/****************************************************************************
+*
+*						wxWindows HTML Applet Package
+*
+*               Copyright (C) 1991-2001 SciTech Software, Inc.
+*                            All rights reserved.
+*
+*  ======================================================================
+*  |REMOVAL OR MODIFICATION OF THIS HEADER IS STRICTLY PROHIBITED BY LAW|
+*  |                                                                    |
+*  |This copyrighted computer code is a proprietary trade secret of     |
+*  |SciTech Software, Inc., located at 505 Wall Street, Chico, CA 95928 |
+*  |USA (www.scitechsoft.com).  ANY UNAUTHORIZED POSSESSION, USE,       |
+*  |VIEWING, COPYING, MODIFICATION OR DISSEMINATION OF THIS CODE IS     |
+*  |STRICTLY PROHIBITED BY LAW.  Unless you have current, express       |
+*  |written authorization from SciTech to possess or use this code, you |
+*  |may be subject to civil and/or criminal penalties.                  |
+*  |                                                                    |
+*  |If you received this code in error or you would like to report      |
+*  |improper use, please immediately contact SciTech Software, Inc. at  |
+*  |530-894-8400.                                                       |
+*  |                                                                    |
+*  |REMOVAL OR MODIFICATION OF THIS HEADER IS STRICTLY PROHIBITED BY LAW|
+*  ======================================================================
+*
+* Language:		ANSI C++
+* Environment:	Any
+*
+* Description:  Header file for the wxApplet class
+*
+****************************************************************************/
+
+#ifndef __WX_APPLET_H
+#define __WX_APPLET_H
+
+#include "wx/panel.h"
+#include "wx/applet/window.h"
+
+/*--------------------------- Class Definitions ---------------------------*/
+
+/****************************************************************************
+REMARKS:
+Defines the abstract base class for wxApplet objects.
+****************************************************************************/
+class wxApplet : public wxPanel {
+private:
+    DECLARE_ABSTRACT_CLASS(wxApplet);
+    DECLARE_EVENT_TABLE();
+	
+protected:
+	wxHtmlAppletWindow	*m_Parent;
+
+			// Special handler for background erase messages 			
+			void OnEraseBackground(wxEraseEvent&);
+		
+public:
+			// Constructor (called during dynamic creation)
+			wxApplet() { m_Parent = NULL; }
+	
+			// Psuedo virtual constructor
+	virtual bool Create(
+				wxHtmlAppletWindow *parent,
+				const wxSize& size,
+				long style = wxTAB_TRAVERSAL | wxNO_BORDER);
+				
+			// Virtual destructor
+	virtual ~wxApplet();
+
+			// Handle HTML navigation to a new URL
+	virtual void OnLinkClicked(const wxHtmlLinkInfo& link) = 0;	
+			
+			// Handle HTML navigation forward command in applet
+	virtual void OnHistoryForward() = 0;
+			
+			// Handle HTML navigation back command in applet
+	virtual void OnHistoryBack() = 0;
+			
+			// Handle messages from the wxAppletManager and other applets
+	virtual void OnMessage(wxEvent& msg) = 0;
+	};
+	
+#endif // __WX_APPLET_H
+
---
+++ samples/applet/monitorapplet.h
@@ -0,0 +1,114 @@
+/****************************************************************************
+*
+*               Copyright (C) 1991-2001 SciTech Software, Inc.
+*                            All rights reserved.
+*
+*  ======================================================================
+*  |REMOVAL OR MODIFICATION OF THIS HEADER IS STRICTLY PROHIBITED BY LAW|
+*  |                                                                    |
+*  |This copyrighted computer code is a proprietary trade secret of     |
+*  |SciTech Software, Inc., located at 505 Wall Street, Chico, CA 95928 |
+*  |USA (www.scitechsoft.com).  ANY UNAUTHORIZED POSSESSION, USE,       |
+*  |VIEWING, COPYING, MODIFICATION OR DISSEMINATION OF THIS CODE IS     |
+*  |STRICTLY PROHIBITED BY LAW.  Unless you have current, express       |
+*  |written authorization from SciTech to possess or use this code, you |
+*  |may be subject to civil and/or criminal penalties.                  |
+*  |                                                                    |
+*  |If you received this code in error or you would like to report      |
+*  |improper use, please immediately contact SciTech Software, Inc. at  |
+*  |530-894-8400.                                                       |
+*  |                                                                    |
+*  |REMOVAL OR MODIFICATION OF THIS HEADER IS STRICTLY PROHIBITED BY LAW|
+*  ======================================================================
+*
+* Language:		ANSI C++
+* Environment:	Any
+*
+* Description:  Header file for the MonitorApplet class
+*
+****************************************************************************/
+
+#ifndef __WX_MONITORAPPLET_H
+#define __WX_MONITORAPPLET_H
+
+#include "wx/applet/applet.h"
+#include "combobox.h"
+#include "dialogs_wdr.h"
+
+/*--------------------------- Class Definitions ---------------------------*/
+
+/****************************************************************************
+REMARKS:
+Structure defining the simple monitor database records.
+****************************************************************************/
+struct MonitorEntry {
+	char	m_Mfr[60];
+	char	m_Model[60];
+	};
+
+/****************************************************************************
+REMARKS:
+Defines our wxMonitorData cookie object that is stored to maintain state
+information for this MonitorApplet.
+****************************************************************************/
+class MonitorData : public wxObject {
+public:
+	MonitorEntry	m_Monitor;
+	};
+
+// Name used to track the monitor data cookie 	
+#define MONITOR_COOKIE_NAME	"MonitorData"	
+	
+/****************************************************************************
+REMARKS:
+Defines our wxMonitor applet class
+****************************************************************************/
+class MonitorApplet : public wxApplet {
+private:
+    DECLARE_DYNAMIC_CLASS(MonitorApplet);
+    DECLARE_EVENT_TABLE();
+	
+protected:
+	ComboBox			*m_Mfr;	 		   	
+	ComboBox 			*m_Model;
+	MonitorData			*m_Data;	
+	static MonitorEntry	m_Monitors[];
+	
+			// Flush the current state to a cookie
+			void SaveCurrentState();
+			
+public:
+			// Constructor (called during dynamic creation)
+			MonitorApplet();
+
+			// Psuedo virtual constructor
+	virtual	bool Create(
+				wxHtmlAppletWindow *parent,
+				const wxSize& size,
+				long style);
+				
+			// Virtual destructor
+	virtual ~MonitorApplet();
+
+			// Handle HTML navigation to a new URL
+	virtual void OnLinkClicked(const wxHtmlLinkInfo& link);	
+			
+			// Handle HTML navigation forward command in applet
+	virtual void OnHistoryForward();
+			
+			// Handle HTML navigation back command in applet
+	virtual void OnHistoryBack();
+			
+			// Handle messages from the wxAppletManager and other applets
+	virtual void OnMessage(wxEvent& msg);
+
+			// Update the model and menufacturer lists
+			void ReadMfrList();
+			void ReadModelList(bool selectCurrent);
+
+			// Event handlers	
+			void OnChange(wxCommandEvent &event);
+	};
+	
+#endif // __WX_MONITORAPPLET_H
+
