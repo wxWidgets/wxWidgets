1c98149d129386266270253a5811f7bf1c376fa2
Julian Smart <julian@anthemion.co.uk>
2001-05-04 10:31:08 +0000

    Added mac.rsp, zipmac.bat
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@9992 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ distrib/msw/zipmac.bat
@@ -0,0 +1,36 @@
+@echo off
+Rem Zips up an archive of Mac and generic wxWindows files.
+set src=%wxwin%
+set dest=%src%\deliver
+
+Rem Set this to the required version
+set version=2.3.0
+
+if "%src%" == "" goto usage
+if "%dest%" == "" goto usage
+if "%1%" == "-help" goto usage
+if "%1%" == "--help" goto usage
+if "%1%" == "/?" goto usage
+echo About to archive a wxMac distribution:
+echo   From   %src%
+echo   To     %dest%
+echo CTRL-C if this is not correct.
+pause
+
+erase %dest%\wx*.zip
+
+cd %src%
+echo Zipping...
+
+zip -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\generic.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\mac.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\cw.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\tiff.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\jpeg.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\tex2rtf.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\dialoged.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\ogl.rsp
+zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\xml.rsp
+Rem zip -u -@ %dest%\wxMac-%version%.zip < %src%\distrib\msw\wx_html.rsp
+
+cd %dest%
