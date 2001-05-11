4def3b359546ee186b3db723db719f85b761f36a
Václav Slavík <vslavik@fastmail.fm>
2001-05-11 18:32:17 +0000

    some more src code reformatting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@10131 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/strconv.cpp
+++ src/common/strconv.cpp
@@ -93,34 +93,43 @@ WXDLLEXPORT_DATA(wxMBConv *) wxConvCurrent = &wxConvLibc;
 
 static size_t encode_utf16(wxUint32 input,wxUint16*output)
 {
-  if (input<=0xffff) {
-    if (output) *output++ = input;
-    return 1;
-  } else
-  if (input>=0x110000) {
-    return (size_t)-1;
-  } else {
-    if (output) {
-      *output++ = (input >> 10)+0xd7c0;
-      *output++ = (input&0x3ff)+0xdc00;
+    if (input<=0xffff) 
+    {
+        if (output) *output++ = input;
+        return 1;
+    } 
+    else if (input>=0x110000) 
+    {
+        return (size_t)-1;
+    } 
+    else 
+    {
+        if (output) 
+        {
+            *output++ = (input >> 10)+0xd7c0;
+            *output++ = (input&0x3ff)+0xdc00;
+        }
+        return 2;
     }
-    return 2;
-  }
 }
 
 static size_t decode_utf16(wxUint16*input,wxUint32&output)
 {
-  if ((*input<0xd800) || (*input>0xdfff)) {
-    output = *input;
-    return 1;
-  } else
-  if ((input[1]<0xdc00) || (input[1]>=0xdfff)) {
-    output = *input;
-    return (size_t)-1;
-  } else {
-    output = ((input[0] - 0xd7c0) << 10) + (input[1] - 0xdc00);
-    return 2;
-  }
+    if ((*input<0xd800) || (*input>0xdfff)) 
+    {
+        output = *input;
+        return 1;
+    } 
+    else if ((input[1]<0xdc00) || (input[1]>=0xdfff)) 
+    {
+        output = *input;
+        return (size_t)-1;
+    } 
+    else 
+    {
+        output = ((input[0] - 0xd7c0) << 10) + (input[1] - 0xdc00);
+        return 2;
+    }
 }
 
 #endif // WC_UTF16
@@ -200,26 +209,32 @@ WXDLLEXPORT_DATA(wxMBConvGdk) wxConvGdk;
 
 size_t wxMBConvGdk::MB2WC(wchar_t *buf, const char *psz, size_t n) const
 {
-  if (buf) {
-    return gdk_mbstowcs((GdkWChar *)buf, psz, n);
-  } else {
-    GdkWChar *nbuf = new GdkWChar[n=strlen(psz)];
-    size_t len = gdk_mbstowcs(nbuf, psz, n);
-    delete [] nbuf;
-    return len;
-  }
+    if (buf) 
+    {
+        return gdk_mbstowcs((GdkWChar *)buf, psz, n);
+    } 
+    else 
+    {
+        GdkWChar *nbuf = new GdkWChar[n=strlen(psz)];
+        size_t len = gdk_mbstowcs(nbuf, psz, n);
+        delete[] nbuf;
+        return len;
+    }
 }
 
 size_t wxMBConvGdk::WC2MB(char *buf, const wchar_t *psz, size_t n) const
 {
-  char *mbstr = gdk_wcstombs((GdkWChar *)psz);
-  size_t len = mbstr ? strlen(mbstr) : 0;
-  if (buf) {
-    if (len > n) len = n;
-    memcpy(buf, psz, len);
-    if (len < n) buf[len] = 0;
-  }
-  return len;
+    char *mbstr = gdk_wcstombs((GdkWChar *)psz);
+    size_t len = mbstr ? strlen(mbstr) : 0;
+    if (buf) 
+    {
+        if (len > n) 
+            len = n;
+        memcpy(buf, psz, len);
+        if (len < n) 
+            buf[len] = 0;
+    }
+    return len;
 }
 
 #endif // GTK > 1.0
@@ -261,86 +276,110 @@ size_t wxMBConvUTF7::WC2MB(char * WXUNUSED(buf),
 
 WXDLLEXPORT_DATA(wxMBConvUTF8) wxConvUTF8;
 
-static wxUint32 utf8_max[]={0x7f,0x7ff,0xffff,0x1fffff,0x3ffffff,0x7fffffff,0xffffffff};
+static wxUint32 utf8_max[]= 
+    { 0x7f, 0x7ff, 0xffff, 0x1fffff, 0x3ffffff, 0x7fffffff, 0xffffffff };
 
 size_t wxMBConvUTF8::MB2WC(wchar_t *buf, const char *psz, size_t n) const
 {
-  size_t len = 0;
+    size_t len = 0;
 
-  while (*psz && ((!buf) || (len<n))) {
-    unsigned char cc=*psz++, fc=cc;
-    unsigned cnt;
-    for (cnt=0; fc&0x80; cnt++) fc<<=1;
-    if (!cnt) {
-      // plain ASCII char
-      if (buf) *buf++=cc;
-      len++;
-    } else {
-      cnt--;
-      if (!cnt) {
-        // invalid UTF-8 sequence
-        return (size_t)-1;
-      } else {
-        unsigned ocnt=cnt-1;
-        wxUint32 res=cc&(0x3f>>cnt);
-        while (cnt--) {
-          cc = *psz++;
-          if ((cc&0xC0)!=0x80) {
-            // invalid UTF-8 sequence
-            return (size_t)-1;
-          }
-          res=(res<<6)|(cc&0x3f);
-        }
-        if (res<=utf8_max[ocnt]) {
-          // illegal UTF-8 encoding
-          return (size_t)-1;
-        }
+    while (*psz && ((!buf) || (len < n))) 
+    {
+        unsigned char cc = *psz++, fc = cc;
+        unsigned cnt;
+        for (cnt = 0; fc & 0x80; cnt++) 
+            fc <<= 1;
+        if (!cnt) 
+        {
+            // plain ASCII char
+            if (buf) 
+                *buf++ = cc;
+            len++;
+        } 
+        else 
+        {
+            cnt--;
+            if (!cnt) 
+            {
+                // invalid UTF-8 sequence
+                return (size_t)-1;
+            } 
+            else 
+            {
+                unsigned ocnt = cnt - 1;
+                wxUint32 res = cc & (0x3f >> cnt);
+                while (cnt--) 
+                {
+                    cc = *psz++;
+                    if ((cc & 0xC0) != 0x80) 
+                    {
+                        // invalid UTF-8 sequence
+                        return (size_t)-1;
+                    }
+                    res = (res << 6) | (cc & 0x3f);
+                }
+                if (res <= utf8_max[ocnt]) 
+                {
+                    // illegal UTF-8 encoding
+                    return (size_t)-1;
+                }
 #ifdef WC_UTF16
-        size_t pa = encode_utf16(res, buf);
-        if (pa == (size_t)-1)
-          return (size_t)-1;
-        if (buf) buf+=pa;
-        len+=pa;
+                size_t pa = encode_utf16(res, buf);
+                if (pa == (size_t)-1)
+                  return (size_t)-1;
+                if (buf) 
+                    buf += pa;
+                len += pa;
 #else
-        if (buf) *buf++=res;
-        len++;
+                if (buf) 
+                    *buf++ = res;
+                len++;
 #endif
-      }
+            }
+        }
     }
-  }
-  if (buf && (len<n)) *buf = 0;
-  return len;
+    if (buf && (len < n)) 
+        *buf = 0;
+    return len;
 }
 
 size_t wxMBConvUTF8::WC2MB(char *buf, const wchar_t *psz, size_t n) const
 {
-  size_t len = 0;
+    size_t len = 0;
 
-  while (*psz && ((!buf) || (len<n))) {
-    wxUint32 cc;
+    while (*psz && ((!buf) || (len < n))) 
+    {
+        wxUint32 cc;
 #ifdef WC_UTF16
-    size_t pa = decode_utf16(psz,cc);
-    psz += (pa == (size_t)-1) ? 1 : pa;
+        size_t pa = decode_utf16(psz,cc);
+        psz += (pa == (size_t)-1) ? 1 : pa;
 #else
-    cc=(*psz++)&0x7fffffff;
+        cc=(*psz++) & 0x7fffffff;
 #endif
-    unsigned cnt;
-    for (cnt=0; cc>utf8_max[cnt]; cnt++);
-    if (!cnt) {
-      // plain ASCII char
-      if (buf) *buf++=cc;
-      len++;
-    } else {
-      len+=cnt+1;
-      if (buf) {
-        *buf++=(-128>>cnt)|((cc>>(cnt*6))&(0x3f>>cnt));
-        while (cnt--)
-          *buf++=0x80|((cc>>(cnt*6))&0x3f);
-      }
+        unsigned cnt;
+        for (cnt = 0; cc > utf8_max[cnt]; cnt++) {}
+        if (!cnt) 
+        {
+            // plain ASCII char
+            if (buf) 
+                *buf++ = cc;
+            len++;
+        } 
+    
+        else 
+        {
+            len += cnt + 1;
+            if (buf) 
+            {
+                *buf++ = (-128 >> cnt) | ((cc >> (cnt * 6)) & (0x3f >> cnt));
+                while (cnt--)
+                    *buf++ = 0x80 | ((cc >> (cnt * 6)) & 0x3f);
+            }
+        }
     }
-  }
-  if (buf && (len<n)) *buf = 0;
-  return len;
+
+    if (buf && (len<n)) *buf = 0;
+    return len;
 }
 
 // ----------------------------------------------------------------------------
@@ -399,9 +438,9 @@ public:
         : cname(name) {}
     virtual ~wxCharacterSet()
         {}
-    virtual size_t MB2WC(wchar_t*buf, const char*psz, size_t n) 
+    virtual size_t MB2WC(wchar_t *buf, const char *psz, size_t n) 
         { return (size_t)-1; }
-    virtual size_t WC2MB(char*buf, const wchar_t*psz, size_t n)
+    virtual size_t WC2MB(char *buf, const wchar_t *psz, size_t n)
         { return (size_t)-1; }
     virtual bool usable()
         { return FALSE; }
@@ -412,13 +451,13 @@ public:
 class ID_CharSet : public wxCharacterSet
 {
 public:
-    ID_CharSet(const wxChar*name,wxMBConv*cnv)
+    ID_CharSet(const wxChar *name,wxMBConv *cnv)
         : wxCharacterSet(name), work(cnv) {}
         
-    size_t MB2WC(wchar_t*buf, const char*psz, size_t n)
+    size_t MB2WC(wchar_t *buf, const char *psz, size_t n)
         { return work ? work->MB2WC(buf,psz,n) : (size_t)-1; }
         
-    size_t WC2MB(char*buf, const wchar_t*psz, size_t n)
+    size_t WC2MB(char *buf, const wchar_t *psz, size_t n)
         { return work ? work->WC2MB(buf,psz,n) : (size_t)-1; }
         
     bool usable()
@@ -447,7 +486,7 @@ public:
 class IC_CharSet : public wxCharacterSet
 {
 public:
-    IC_CharSet(const wxChar*name) 
+    IC_CharSet(const wxChar *name) 
         : wxCharacterSet(name)
     {
         m2w = iconv_open(WC_NAME, wxConvLibc.cWX2MB(cname));
@@ -508,7 +547,7 @@ public:
         return res;
     }
     
-    size_t WC2MB(char*buf, const wchar_t*psz, size_t n)
+    size_t WC2MB(char *buf, const wchar_t *psz, size_t n)
     {
 #if defined(__BORLANDC__) && (__BORLANDC__ > 0x530)
         size_t inbuf = std::wcslen(psz) * SIZEOF_WCHAR_T;
@@ -523,7 +562,7 @@ public:
         // this absolutely doesn't rock!
         // (no, doing WC_BSWAP twice on the original buffer won't help, as it
         //  could be in read-only memory, or be accessed in some other thread)
-        wchar_t*tmpbuf=(wchar_t*)malloc((inbuf+1)*SIZEOF_WCHAR_T);
+        wchar_t *tmpbuf=(wchar_t*)malloc((inbuf+1)*SIZEOF_WCHAR_T);
         memcpy(tmpbuf,psz,(inbuf+1)*SIZEOF_WCHAR_T);
         WC_BSWAP(tmpbuf, inbuf)
         psz=tmpbuf;
@@ -578,20 +617,22 @@ public:
     CP_CharSet(const wxChar*name) 
         : wxCharacterSet(name), CodePage(CharsetToCodepage(name)) {}
         
-    size_t MB2WC(wchar_t*buf, const char*psz, size_t n)
+    size_t MB2WC(wchar_t *buf, const char *psz, size_t n)
     {
-        size_t len = MultiByteToWideChar(CodePage,0,psz,-1,buf,buf?n:0);
+        size_t len = 
+            MultiByteToWideChar(CodePage, 0, psz, -1, buf, buf ? n : 0);
         return len ? len : (size_t)-1;
     }
     
-    size_t WC2MB(char*buf, const wchar_t*psz, size_t n)
+    size_t WC2MB(char *buf, const wchar_t *psz, size_t n)
     {
-        size_t len = WideCharToMultiByte(CodePage,0,psz,-1,buf,buf?n:0,NULL,NULL);
+        size_t len = WideCharToMultiByte(CodePage, 0, psz, -1, buf,
+                                         buf ? n : 0, NULL, NULL);
         return len ? len : (size_t)-1;
     }
     
     bool usable()
-        { return CodePage!=-1; }
+        { return CodePage != -1; }
 
 public:
     long CodePage;
@@ -603,7 +644,8 @@ class EC_CharSet : public wxCharacterSet
 public:
     // temporarily just use wxEncodingConverter stuff,
     // so that it works while a better implementation is built
-    EC_CharSet(const wxChar*name) : wxCharacterSet(name), enc(wxFONTENCODING_SYSTEM)
+    EC_CharSet(const wxChar*name) : wxCharacterSet(name), 
+                                    enc(wxFONTENCODING_SYSTEM)
     {
         if (name)
             enc = wxTheFontMapper->CharsetToEncoding(name, FALSE);
@@ -611,14 +653,15 @@ public:
         w2m.Init(wxFONTENCODING_UNICODE, enc);
     }
     
-    size_t MB2WC(wchar_t*buf, const char*psz, size_t n)
+    size_t MB2WC(wchar_t *buf, const char *psz, size_t n)
     {
         size_t inbuf = strlen(psz);
-        if (buf) m2w.Convert(psz,buf);
+        if (buf) 
+            m2w.Convert(psz,buf);
         return inbuf;
     }
     
-    size_t WC2MB(char*buf, const wchar_t*psz, size_t n)
+    size_t WC2MB(char *buf, const wchar_t *psz, size_t n)
     {
 #if defined(__BORLANDC__) && (__BORLANDC__ > 0x530)
         size_t inbuf = std::wcslen(psz);
@@ -644,7 +687,7 @@ static wxCharacterSet *wxGetCharacterSet(const wxChar *name)
     wxCharacterSet *cset = NULL;
     if (name)
     {
-        if (!wxStricmp(name, wxT("UTF8")) || !wxStricmp(name, wxT("UTF-8")))
+        if (wxStricmp(name, wxT("UTF8")) == 0 || wxStricmp(name, wxT("UTF-8")) == 0)
         {
             cset = new ID_CharSet(name, &wxConvUTF8);
         }
@@ -703,7 +746,7 @@ void wxCSConv::LoadNow()
 #ifdef __UNIX__
 #if defined(HAVE_LANGINFO_H) && defined(CODESET)
             // GNU libc provides current character set this way
-            char*alang = nl_langinfo(CODESET);
+            char *alang = nl_langinfo(CODESET);
             if (alang)
             {
                 SetName(wxConvLibc.cMB2WX(alang));
@@ -715,10 +758,13 @@ void wxCSConv::LoadNow()
                 // try to see if it's in the environment variables
                 // (in most cases this won't work, but I was out of ideas)
                 wxChar *lang = wxGetenv(wxT("LC_ALL"));
-                if (!lang) lang = wxGetenv(wxT("LC_CTYPE"));
-                if (!lang) lang = wxGetenv(wxT("LANG"));
+                if (!lang) 
+                    lang = wxGetenv(wxT("LC_CTYPE"));
+                if (!lang) 
+                    lang = wxGetenv(wxT("LANG"));
                 wxChar *dot = lang ? wxStrchr(lang, wxT('.')) : (wxChar *)NULL;
-                if (dot) SetName(dot+1);
+                if (dot) 
+                    SetName(dot+1);
             }
 #endif
         }
@@ -735,11 +781,11 @@ size_t wxCSConv::MB2WC(wchar_t *buf, const char *psz, size_t n) const
         return m_cset->MB2WC(buf, psz, n);
 
     // latin-1 (direct)
-    size_t len=strlen(psz);
+    size_t len = strlen(psz);
     
     if (buf)
     {
-        for (size_t c=0; c<=len; c++)
+        for (size_t c = 0; c <= len; c++)
             buf[c] = (unsigned char)(psz[c]);
     }
     
@@ -761,8 +807,8 @@ size_t wxCSConv::WC2MB(char *buf, const wchar_t *psz, size_t n) const
 #endif
     if (buf)
     {
-        for (size_t c=0; c<=len; c++)
-            buf[c] = (psz[c]>0xff) ? '?' : psz[c];
+        for (size_t c = 0; c <= len; c++)
+            buf[c] = (psz[c] > 0xff) ? '?' : psz[c];
     }
     
     return len;
@@ -772,13 +818,19 @@ size_t wxCSConv::WC2MB(char *buf, const wchar_t *psz, size_t n) const
 class IC_CharSetConverter
 {
 public:
-    IC_CharSetConverter(IC_CharSet*from,IC_CharSet*to)
-        { cnv = iconv_open(wxConvLibc.cWX2MB(to->cname),wxConvLibc.cWX2MB(from->cname)); }
+    IC_CharSetConverter(IC_CharSet *from, IC_CharSet *to)
+    {
+        cnv = iconv_open(wxConvLibc.cWX2MB(to->cname), 
+                         wxConvLibc.cWX2MB(from->cname)); 
+    }
         
     ~IC_CharSetConverter()
-        { if (cnv!=(iconv_t)-1) iconv_close(cnv); }
+    { 
+        if (cnv != (iconv_t)-1) 
+            iconv_close(cnv); 
+    }
         
-    size_t Convert(char*buf, const char*psz, size_t n)
+    size_t Convert(char *buf, const char *psz, size_t n)
     {
         size_t inbuf = strlen(psz);
         size_t outbuf = n;
@@ -787,8 +839,9 @@ public:
 #else
         size_t res = iconv( cnv, &psz, &inbuf, &buf, &outbuf );
 #endif
-        if (res==(size_t)-1) return (size_t)-1;
-        return n-outbuf;
+        if (res == (size_t)-1) 
+            return (size_t)-1;
+        return (n - outbuf);
     }
 
 public:
