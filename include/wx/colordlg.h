7cdb542c4df079e47b2bc4859c822ea61066b016
Vadim Zeitlin <vadim@wxwidgets.org>
2001-07-05 01:26:15 +0000

    added wxUSE_COLOURDLG to the header too
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@10835 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/colordlg.h
+++ include/wx/colordlg.h
@@ -1,30 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/colrdlgg.h
+// Purpose:     wxColourDialog
+// Author:      Vadim Zeitiln
+// Modified by:
+// Created:     01/02/97
+// RCS-ID:      $Id$
+// Copyright:   (c) wxWindows team
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
 #ifndef _WX_COLORDLG_H_BASE_
 #define _WX_COLORDLG_H_BASE_
 
-#if defined(__WXMSW__)
-#include "wx/msw/colordlg.h"
-#elif defined(__WXMOTIF__)
-#include "wx/generic/colrdlgg.h"
-#elif defined(__WXGTK__)
-#include "wx/generic/colrdlgg.h"
-#elif defined(__WXQT__)
-#include "wx/generic/colrdlgg.h"
-#elif defined(__WXMAC__)
-#include "wx/generic/colrdlgg.h"
-#elif defined(__WXPM__)
-#include "wx/generic/colrdlgg.h"
-#elif defined(__WXSTUBS__)
-#include "wx/generic/colrdlgg.h"
-#endif
+#if wxUSE_COLOURDLG
 
-#if !defined(__WXMSW__)
-#define wxColourDialog wxGenericColourDialog
-#define sm_classwxColourDialog sm_classwxGenericColourDialog
+#if defined(__WXMSW__)
+    #include "wx/msw/colordlg.h"
+#else
+    #include "wx/generic/colrdlgg.h"
+
+    #define wxColourDialog wxGenericColourDialog
+    #define sm_classwxColourDialog sm_classwxGenericColourDialog
 #endif
 
 // get the colour from user and return it
-wxColour WXDLLEXPORT wxGetColourFromUser(wxWindow *parent = (wxWindow *)NULL,
-                                         const wxColour& colInit = wxNullColour);
+wxColour WXDLLEXPORT
+wxGetColourFromUser(wxWindow *parent = (wxWindow *)NULL,
+                    const wxColour& colInit = wxNullColour);
+
+#endif // wxUSE_COLOURDLG
 
 #endif
     // _WX_COLORDLG_H_BASE_
