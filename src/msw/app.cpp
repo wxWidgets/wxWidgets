4624defaf9bec9168ba6c23e768a5b82b6bc6d1c
Julian Smart <julian@anthemion.co.uk>
2001-07-13 14:21:42 +0000

    Fixed typo in cache management code
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@11002 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/app.cpp
+++ src/msw/app.cpp
@@ -1116,7 +1116,7 @@ void wxApp::OnIdle(wxIdleEvent& event)
     // automated DC cache management: clear the cached DCs and bitmap
     // if it's likely that the app has finished with them, that is, we
     // get an idle event and we're not dragging anything.
-    if (!::GetKeyState(MK_LBUTTON) && !::GetKeyState(MK_MBUTTON) && ::GetKeyState(MK_RBUTTON))
+    if (!::GetKeyState(MK_LBUTTON) && !::GetKeyState(MK_MBUTTON) && !::GetKeyState(MK_RBUTTON))
         wxDC::ClearCache();
 #endif // wxUSE_DC_CACHEING
 
