491b5be8305968871bc289453ed088a70c3de044
Vadim Zeitlin <vadim@wxwidgets.org>
2001-07-13 19:26:44 +0000

    added wxListView class: this is going to be a wxListCtrl with human (inter)face
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@11020 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/listctrl.h
+++ include/wx/listctrl.h
@@ -285,6 +285,55 @@ private:
 #endif
 
 // ----------------------------------------------------------------------------
+// wxListView: a class which provides a better API for list control
+// ----------------------------------------------------------------------------
+
+class WXDLLEXPORT wxListView : public wxListCtrl
+{
+public:
+    wxListView() { }
+    wxListView( wxWindow *parent,
+                wxWindowID id = -1,
+                const wxPoint& pos = wxDefaultPosition,
+                const wxSize& size = wxDefaultSize,
+                long style = wxLC_REPORT,
+                const wxValidator& validator = wxDefaultValidator,
+                const wxString &name = "listctrl" )
+    {
+        Create(parent, id, pos, size, style, wxDefaultValidator, name);
+    }
+
+    // [de]select an item
+    void Select(long n, bool on = true)
+    {
+        SetItemState(n, on ? wxLIST_STATE_SELECTED : 0, wxLIST_STATE_SELECTED);
+    }
+
+    // focus and show the given item
+    void Focus(long index)
+    {
+        SetItemState(index, wxLIST_STATE_FOCUSED, wxLIST_STATE_FOCUSED);
+        EnsureVisible(index);
+    }
+
+    // get the currently focused item or -1 if none
+    long GetFocusedItem() const
+    {
+        return GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_FOCUSED);
+    }
+
+    // get first and subsequent selected items, return -1 when no more
+    long GetNextSelected(long item) const
+        { return GetNextItem(item, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED); }
+    long GetFirstSelected() const
+        { return GetNextSelected(-1); }
+
+    // return TRUE if the item is selected
+    bool IsSelected(long index)
+        { return GetItemState(index, wxLIST_STATE_SELECTED) != 0; }
+};
+
+// ----------------------------------------------------------------------------
 // wxListEvent - the event class for the wxListCtrl notifications
 // ----------------------------------------------------------------------------
 
