fec026bdce39b87a334296b0eea766aa3d6e95ec
Julian Smart <julian@anthemion.co.uk>
2001-07-17 16:47:10 +0000

    Added icon index parsing
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@11083 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/gdiimage.cpp
+++ src/msw/gdiimage.cpp
@@ -400,7 +400,53 @@ bool wxICOFileHandler::LoadIcon(wxIcon *icon,
     wxSize size;
 
 #ifdef __WIN32__
-    HICON hicon = ::ExtractIcon(wxGetInstance(), name, 0 /* first */);
+    HICON hicon = NULL;
+
+    // Parse the filename: it may be of the form
+    // filename;n in order to specify the nth icon in the file.
+    // For the moment, ignore the issue of possible semicolons in the filename.
+    int iconIndex = 0;
+    wxString name1(name);
+    wxString strIconIndex = name.AfterLast(wxT(';'));
+    if (strIconIndex != name)
+    {
+        iconIndex = wxAtoi(strIconIndex);
+        name1 = name.BeforeLast(wxT(';'));
+    }
+
+    // were we asked for a large icon?
+    if ( desiredWidth == ::GetSystemMetrics(SM_CXICON) &&
+         desiredHeight == ::GetSystemMetrics(SM_CYICON) )
+    {
+        // get the specified large icon from file
+        if ( !::ExtractIconEx(name1, iconIndex, &hicon, NULL, 1) )
+        {
+            // it is not an error, but it might still be useful to be informed
+            // about it optionally
+            wxLogTrace(_T("iconload"),
+                       _T("No large icons found in the file '%s'."),
+                       name.c_str());
+        }
+    }
+    else if ( desiredWidth == ::GetSystemMetrics(SM_CXSMICON) &&
+              desiredHeight == ::GetSystemMetrics(SM_CYSMICON) )
+    {
+        // get the specified small icon from file
+        if ( !::ExtractIconEx(name1, iconIndex, NULL, &hicon, 1) )
+        {
+            wxLogTrace(_T("iconload"),
+                       _T("No small icons found in the file '%s'."),
+                       name.c_str());
+        }
+    }
+    //else: not standard size, load below
+
+    if ( !hicon )
+    {
+        // take any (the first one) icon from the file by default
+        hicon = ::ExtractIcon(wxGetInstance(), name1, 0 /* first */);
+    }
+
     if ( !hicon )
     {
         wxLogSysError(_T("Failed to load icon from the file '%s'"),
@@ -419,9 +465,10 @@ bool wxICOFileHandler::LoadIcon(wxIcon *icon,
     if ( (desiredWidth != -1 && desiredWidth != size.x) ||
          (desiredHeight != -1 && desiredHeight != size.y) )
     {
-        wxLogDebug(_T("Returning FALSE from wxICOFileHandler::Load because of the size mismatch: actual (%d, %d), requested (%d, %d)"),
-                      size.x, size.y,
-                      desiredWidth, desiredHeight);
+        wxLogTrace(_T("iconload"),
+                   _T("Returning FALSE from wxICOFileHandler::Load because of the size mismatch: actual (%d, %d), requested (%d, %d)"),
+                   size.x, size.y,
+                   desiredWidth, desiredHeight);
 
         ::DestroyIcon(hicon);
 
