a5940c66c79df48c37de7431250dcc0cd540d82a
Václav Slavík <vslavik@fastmail.fm>
2001-07-18 23:00:02 +0000

    Mingw Unicode fixes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@11092 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/wxchar.h
+++ include/wx/wxchar.h
@@ -63,8 +63,7 @@
         #define wxHAVE_TCHAR_FUNCTIONS
     #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x520)
         #define wxHAVE_TCHAR_FUNCTIONS
-    // w32api isn't yet tchar safe, will be soon, this is a placeholder
-    #elif defined(__GNUWIN32__) && wxCHECK_W32API_VERSION( 10, 0 )
+    #elif defined(__GNUWIN32__) && wxCHECK_W32API_VERSION( 1, 0 )
         #define wxHAVE_TCHAR_FUNCTIONS
         #include <stddef.h>
         #include <string.h>
