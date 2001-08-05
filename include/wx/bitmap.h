581acb9180edb54c60f56a29e2004387271f6092
Gilles Depeyrot <gilles_depeyrot@mac.com>
2001-08-05 16:32:48 +0000

    corrected conditional compilation for wxMac
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@11283 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/bitmap.h
+++ include/wx/bitmap.h
@@ -32,7 +32,7 @@ class WXDLLEXPORT wxImage;
 class WXDLLEXPORT wxMask;
 class WXDLLEXPORT wxPalette;
 
-#if defined(__WXMGL__)
+#if defined(__WXMGL__) || defined(__WXMAC__)
 // Only used by some ports
 // FIXME -- make all ports (but MSW which uses wxGDIImage) use these base classes
 
