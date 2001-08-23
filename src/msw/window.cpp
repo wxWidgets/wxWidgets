9e0996129e61a736ee557dc19ed1247bbc6e3e0c
Vadim Zeitlin <vadim@wxwidgets.org>
2001-08-23 23:28:06 +0000

    always pass WM_DESTROY to DefWindowProc() - this fixes memory/resource leak for wxListBox (backported from the trunk)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@11448 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/window.cpp
+++ src/msw/window.cpp
@@ -1871,7 +1871,11 @@ long wxWindow::MSWWindowProc(WXUINT message, WXWPARAM wParam, WXLPARAM lParam)
             break;
 
         case WM_DESTROY:
-            processed = HandleDestroy();
+            // never set processed to TRUE and *always* pass WM_DESTROY to
+            // DefWindowProc() as Windows may do some internal cleanup when
+            // processing it and failing to pass the message along may cause
+            // memory and resource leaks!
+            (void)HandleDestroy();
             break;
 
         case WM_MOVE:
