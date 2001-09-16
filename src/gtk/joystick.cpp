afe934e26a0754c8b83483a9b491fa7ae8422b70
Gilles Depeyrot <gilles_depeyrot@mac.com>
2001-09-16 09:29:04 +0000

    Since there was no #include before the #if wxUSE_JOYSTICK then nothing would ever be compiled in this module.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@11615 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/joystick.cpp
+++ src/gtk/joystick.cpp
@@ -12,7 +12,12 @@
 #ifdef __GNUG__
 #pragma implementation "joystick.h"
 #endif
+
+#include "wx/defs.h"
+
 #if wxUSE_JOYSTICK
+
+#include "wx/joystick.h"
 
 #include <linux/joystick.h>
 #include <sys/types.h>
@@ -21,9 +26,9 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 #include "wx/event.h"
 #include "wx/window.h"
-#include "wx/gtk/joystick.h"
 
 #define JOYSTICK_AXE_MAX 32767
 #define JOYSTICK_AXE_MIN -32767
