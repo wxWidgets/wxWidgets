cefd71d783b6a5547d5078c38cd88dc3b928d1af
Robin Dunn <robin@alldunn.com>
2001-09-17 22:08:35 +0000

    Fixed compile error.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@11626 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xrc/xh_unkwn.cpp
+++ src/xrc/xh_unkwn.cpp
@@ -7,7 +7,7 @@
 // Copyright:   (c) 2000 Vaclav Slavik
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
- 
+
 #ifdef __GNUG__
 #pragma implementation "xh_unkwn.h"
 #endif
@@ -23,6 +23,7 @@
 #include "wx/window.h"
 #include "wx/log.h"
 #include "wx/sizer.h"
+#include "wx/panel.h"
 
 
 class wxUnknownControlContainer : public wxPanel
@@ -35,14 +36,14 @@ public:
                               const wxSize& size = wxDefaultSize)
         : wxPanel(parent, id, pos, size, wxTAB_TRAVERSAL | wxNO_BORDER,
                   controlName + wxT("_container")),
-          m_controlName(controlName), m_controlAdded(FALSE) 
+          m_controlName(controlName), m_controlAdded(FALSE)
     {
         m_bg = GetBackgroundColour();
         SetBackgroundColour(wxColour(255, 0, 255));
     }
 
     virtual void AddChild(wxWindowBase *child);
-    
+
 protected:
     wxString m_controlName;
     bool m_controlAdded;
@@ -54,12 +55,12 @@ void wxUnknownControlContainer::AddChild(wxWindowBase *child)
     wxASSERT_MSG( !m_controlAdded, wxT("Couldn't add two unknown controls to the same container!") )
 
     wxPanel::AddChild(child);
-    
+
     SetBackgroundColour(m_bg);
     child->SetName(m_controlName);
     child->SetId(wxXmlResource::GetXMLID(m_controlName));
     m_controlAdded = TRUE;
-    
+
     wxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
     sizer->Add((wxWindow*)child, 1, wxEXPAND);
     SetSizer(sizer);
@@ -69,14 +70,14 @@ void wxUnknownControlContainer::AddChild(wxWindowBase *child)
 
 
 
-wxUnknownWidgetXmlHandler::wxUnknownWidgetXmlHandler() 
-: wxXmlResourceHandler() 
+wxUnknownWidgetXmlHandler::wxUnknownWidgetXmlHandler()
+: wxXmlResourceHandler()
 {
 }
 
 wxObject *wxUnknownWidgetXmlHandler::DoCreateResource()
-{ 
-    wxPanel *panel = 
+{
+    wxPanel *panel =
         new wxUnknownControlContainer(m_parentAsWindow,
                                       GetName(), -1,
                                       GetPosition(), GetSize());
