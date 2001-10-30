e6157a69950089f5fbd947b20c20ea437fd4f460
Robin Dunn <robin@alldunn.com>
2001-10-30 18:15:13 +0000

    Updated timer demo
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12218 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/wxTimer.py
+++ wxPython/demo/wxTimer.py
@@ -5,39 +5,50 @@ import time
 
 #---------------------------------------------------------------------------
 
-class TestTimer(wxTimer):
-    def __init__(self, log = None):
-        wxTimer.__init__(self)
-        self.log = log
-
-    def Notify(self):
-        wxBell()
-        if self.log:
-            self.log.WriteText('beep!\n')
+## class TestTimer(wxTimer):
+##     def __init__(self, log = None):
+##         wxTimer.__init__(self)
+##         self.log = log
+##     def Notify(self):
+##         wxBell()
+##         if self.log:
+##             self.log.WriteText('beep!\n')
 
 #---------------------------------------------------------------------------
 
-_timer = TestTimer()
-
+ID_Start = wxNewId()
+ID_Stop  = wxNewId()
+ID_Timer = wxNewId()
 
 class TestTimerWin(wxPanel):
     def __init__(self, parent, log):
-        _timer.log = log
         wxPanel.__init__(self, parent, -1)
+        self.log = log
 
         wxStaticText(self, -1, "This is a timer example",
                                wxPoint(15, 30))
 
-        wxButton(self, 11101, ' Start ', wxPoint(15, 75), wxDefaultSize)
-        wxButton(self, 11102, ' Stop ', wxPoint(115, 75), wxDefaultSize)
-        EVT_BUTTON(self, 11101, self.OnStart)
-        EVT_BUTTON(self, 11102, self.OnStop)
+        wxButton(self, ID_Start, ' Start ', wxPoint(15, 75), wxDefaultSize)
+        wxButton(self, ID_Stop, ' Stop ', wxPoint(115, 75), wxDefaultSize)
+
+        self.timer = wxTimer(self,      # object to send the event to
+                             ID_Timer)  # event id to use
+
+        EVT_BUTTON(self, ID_Start, self.OnStart)
+        EVT_BUTTON(self, ID_Stop,  self.OnStop)
+        EVT_TIMER(self,  ID_Timer, self.OnTimer)
+
 
     def OnStart(self, event):
-        _timer.Start(1000)
+        self.timer.Start(1000)
 
     def OnStop(self, event):
-        _timer.Stop()
+        self.timer.Stop()
+
+    def OnTimer(self, event):
+        wxBell()
+        if self.log:
+            self.log.WriteText('beep!\n')
 
 #---------------------------------------------------------------------------
 
