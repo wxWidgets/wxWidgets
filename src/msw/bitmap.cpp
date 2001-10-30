25acfd4ca34c7b1beb34eca73ee33c5297f5c021
Mattia Barbon <mbarbon@cpan.org>
2001-10-30 20:52:35 +0000

    Fixed wxMask::Create(bitmap,colour)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12220 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/bitmap.cpp
+++ src/msw/bitmap.cpp
@@ -1087,7 +1087,7 @@ bool wxMask::Create(const wxBitmap& bitmap, const wxColour& colour)
 
     // scan the bitmap for the transparent colour and set the corresponding
     // pixels in the mask to BLACK and the rest to WHITE
-    COLORREF maskColour = wxColourToRGB(colour);
+    COLORREF maskColour = RGB(colour.Red(), colour.Green(), colour.Blue());
     m_maskBitmap = (WXHBITMAP)::CreateBitmap(width, height, 1, 1, 0);
 
     HDC srcDC = ::CreateCompatibleDC(NULL);
