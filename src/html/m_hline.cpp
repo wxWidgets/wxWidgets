51072df23ffcf5bdd4651dbe0ad5143b1e360119
Václav Slavík <vslavik@fastmail.fm>
2001-11-20 23:50:44 +0000

    better <HR> tag handling in wxHTML
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12537 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/html/m_hline.cpp
+++ src/html/m_hline.cpp
@@ -41,17 +41,21 @@ FORCE_LINK_ME(m_hline)
 class wxHtmlLineCell : public wxHtmlCell
 {
     public:
-        wxHtmlLineCell(int size) : wxHtmlCell() {m_Height = size;}
+        wxHtmlLineCell(int size, bool shading) : wxHtmlCell() {m_Height = size; m_HasShading = shading;}
         void Draw(wxDC& dc, int x, int y, int view_y1, int view_y2);
         void Layout(int w)
             { m_Width = w; wxHtmlCell::Layout(w); }
+
+    private:
+        // Should we draw 3-D shading or not
+      bool m_HasShading;
 };
 
 
 void wxHtmlLineCell::Draw(wxDC& dc, int x, int y, int WXUNUSED(view_y1), int WXUNUSED(view_y2))
 {
-    wxBrush mybrush(wxT("BLACK"), wxSOLID);
-    wxPen mypen(wxT("BLACK"), 1, wxSOLID);
+    wxBrush mybrush(wxT("GREY"), (m_HasShading) ? wxTRANSPARENT : wxSOLID);
+    wxPen mypen(wxT("GREY"), 1, wxSOLID);
     dc.SetBrush(mybrush);
     dc.SetPen(mypen);
     dc.DrawRectangle(x + m_PosX, y + m_PosY, m_Width, m_Height);
@@ -71,6 +75,7 @@ TAG_HANDLER_BEGIN(HR, "HR")
     {
         wxHtmlContainerCell *c;
         int sz;
+        bool HasShading;
 
         m_WParser->CloseContainer();
         c = m_WParser->OpenContainer();
@@ -81,7 +86,8 @@ TAG_HANDLER_BEGIN(HR, "HR")
         c->SetWidthFloat(tag);
         sz = 1;
         tag.GetParamAsInt(wxT("SIZE"), &sz);
-        c->InsertCell(new wxHtmlLineCell((int)((double)sz * m_WParser->GetPixelScale())));
+        HasShading = !(tag.HasParam(wxT("NOSHADE")));
+        c->InsertCell(new wxHtmlLineCell((int)((double)sz * m_WParser->GetPixelScale()), HasShading));
 
         m_WParser->CloseContainer();
         m_WParser->OpenContainer();
