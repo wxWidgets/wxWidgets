279b1aea9dd6b57cee4db4375aa007d029db5ec0
Mattia Barbon <mbarbon@cpan.org>
2001-11-24 23:55:43 +0000

    Must honour wxUSE_TOOLTIPS in tooltip.h
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12653 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/tooltip.h
+++ include/wx/tooltip.h
@@ -1,6 +1,10 @@
 #ifndef _WX_TOOLTIP_H_BASE_
 #define _WX_TOOLTIP_H_BASE_
 
+#include "wx/setup.h"
+
+#if wxUSE_TOOLTIPS
+
 #if defined(__WXMSW__)
 #include "wx/msw/tooltip.h"
 #elif defined(__WXMOTIF__)
@@ -14,6 +18,9 @@
 #elif defined(__WXSTUBS__)
 // #include "wx/stubs/tooltip.h"
 #endif
+
+#endif
+    // wxUSE_TOOLTIPS
 
 #endif
     // _WX_TOOLTIP_H_BASE_
