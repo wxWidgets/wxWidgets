f2ebc38d2d3b919f4946b3469f27a4f5b6ea1553
Ron Lee <ron@debian.org>
2001-11-25 20:00:40 +0000

    oops, the size_t test actually is still needed in sndwav until we fix wxDataOutputStream to be 64 bit friendly.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_2_BRANCH@12695 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/mmedia/sndwav.cpp
+++ contrib/src/mmedia/sndwav.cpp
@@ -313,7 +313,13 @@ FAIL_WITH(s->Write(&signature, 4).LastWrite() != 4, wxSOUND_INVSTRM);
         delete frmt;
     }
 
+#ifdef wxSIZE_T_IS_ULONG
+    // FIXME: we still need this horrible cast until wxDataOutputStream
+    // is made 64 bit friendly.
+    data << (unsigned int)(fmt_data.GetSize() + m_sndformat->GetBytesFromTime(time));
+#else
     data << (fmt_data.GetSize() + m_sndformat->GetBytesFromTime(time));
+#endif
 
     // We, finally, copy the header block to the output stream 
     {
