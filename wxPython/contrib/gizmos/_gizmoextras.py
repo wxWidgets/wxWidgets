15c5a1043eb0ede6a59ed7c996edb197ea705da2
Bryan Petty <bryan@ibaku.net>
2001-11-28 18:22:16 +0000

    This commit was manufactured by cvs2svn to create tag 'BEFORE_VOLUME_CHANGES'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/BEFORE_VOLUME_CHANGES@12746 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/contrib/gizmos/_gizmoextras.py
+++
@@ -1,5 +0,0 @@
-# Stuff these names into the wx namespace so wxPyConstructObject can find them
-
-wx.wxDynamicSashSplitEventPtr       = wxDynamicSashSplitEventPtr
-wx.wxDynamicSashUnifyEventPtr       = wxDynamicSashUnifyEventPtr
-wx.wxDynamicSashWindowPtr           = wxDynamicSashWindowPtr
