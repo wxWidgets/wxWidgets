744d5712ce3c0e8d5b0bc3d4248ec5088350ccad
Robin Dunn <robin@alldunn.com>
2001-11-30 21:19:30 +0000

    Added the standard lines needed for compiling with PCH, and etc.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12766 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/gizmos/dynamicsash.cpp
+++ contrib/src/gizmos/dynamicsash.cpp
@@ -10,6 +10,23 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
+#ifdef __GNUG__
+    #pragma implementation "splittree.h"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWindows headers)
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
 #include "wx/gizmos/dynamicsash.h"
 
 /*
