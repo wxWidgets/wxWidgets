625e74a703585c33e5534a6a4cbde9c25ae0a037
Václav Slavík <vslavik@fastmail.fm>
2001-12-07 22:40:47 +0000

    compilation fix for Watcom
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12909 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mgl/icon.h
+++ include/wx/mgl/icon.h
@@ -39,9 +39,9 @@ public:
 
     // For compatibility with wxMSW where desired size is sometimes required to
     // distinguish between multiple icons in a resource.
-    wxIcon(const wxString& filename, wxBitmapType type = wxBITMAP_TYPE_ICO_RESOURCE,
+    wxIcon(const wxString& filename, int type = wxBITMAP_TYPE_ICO_RESOURCE,
            int WXUNUSED(desiredWidth)=-1, int WXUNUSED(desiredHeight)=-1 ) :
-           wxBitmap(filename, type) {}
+           wxBitmap(filename, (wxBitmapType)type) {}
     
     wxIcon& operator = (const wxIcon& icon);
     inline bool operator == (const wxIcon& icon) { return m_refData == icon.m_refData; }
