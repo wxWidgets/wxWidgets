8810d4312569c66ba192044604d1a29dab4fb21d
Mattia Barbon <mbarbon@cpan.org>
2001-12-08 22:22:00 +0000

    Applied #486410
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@12933 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mingw32.bat
+++ src/mingw32.bat
@@ -1,13 +1,15 @@
 REM
-REM replace c:\gcc-2.95 with whatever your installation root may be.
+REM replace c:\MINGW with whatever your installation root may be.
 REM GCC_EXEC_PREFIX is optional, and hardly ever needs to be set (read:
 REM leave it alone).
 REM
-PATH=c:\gcc-2.95\bin;%PATH%
-set BISON_SIMPLE=c:\gcc-2.95\share\bison.simple
-set BISON_HAIRY=c:\gcc-2.95\share\bison.hairy
+SET MINGWDIR=c:\gcc-2.95
+PATH=%MINGWDIR%\bin;%PATH%
+set BISON_SIMPLE=%MINGWDIR%\share\bison.simple
+set BISON_HAIRY=%MINGWDIR%\share\bison.hairy
 
-REM SET GCC_EXEC_PREFIX=c:\gcc-2.95\lib\gcc-lib\set LIBRARY_PATH=e:\mingw32\lib;e:\mingw32\lib\gcc-lib\i386-mingw32\2.8.1
+REM SET GCC_EXEC_PREFIX=%MINGWDIR%\lib\gcc-lib\
+REM set LIBRARY_PATH=%MINGWDIR%\lib;%MINGWDIR%\lib\gcc-lib\i386-mingw32\2.8.1
 
 rem 4DOS users only...
 unalias make
