9ed42c2d63731931bc4a68c072dd0769d271789d
Julian Smart <julian@anthemion.co.uk>
2001-12-19 23:32:48 +0000

    Patch mods
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13121 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/zippatch.bat
+++ distrib/msw/zippatch.bat
@@ -1,26 +1,32 @@
 @echo off
 rem Zip up a patch file
-set src=d:\wx2\wxWindows
-set dest=%wxwin\deliver
 
+if "%wxwin%" == "" goto usage
+
+set src=%wxwin%
+set dest=%src%\deliver
+
+Rem Set this to the required wxWindows version
+set wxversion=2.3.2
 Rem Set this to the required patch version
 set version=01
 
+set patchname=wxWindows-%wxversion%-patch-%version%.zip
+
 if "%src" == "" goto usage
 if "%dest" == "" goto usage
-echo About to archive patch%version%.zip
+echo About to create %dest%\%patchname%
 echo   From   %src
 echo   To     %dest
-if "%wise" == "1" echo with WISE setup creation.
 echo CTRL-C if this is not correct.
 pause
 
-erase %dest\patch*.zip
+erase /Q /E %dest%\wxWindows-*-patch*.zip
 
-cd %src
+cd %src%
 echo Zipping...
 
-zip32 -@ %dest\patch%version%.zip < %wxwin\distrib\msw\patch.rsp
+zip32 -@ %dest%\%patchname% < %src%\distrib\msw\patch.rsp
 echo wxWindows patch archived.
 
 cd %dest
@@ -28,8 +34,9 @@ cd %dest
 goto end
 
 :usage
-echo DOS wxWindows patch distribution.
+echo wxWindows patch distribution.
 echo.
 echo Usage: zippatch
+echo You must set WXWIN appropriately before calling this script.
 
 :end
