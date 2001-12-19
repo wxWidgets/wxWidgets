6ed63f8bbce8fa9eeccbc218c2fd874962416a85
Stefan Csomor <csomor@advancedconcepts.ch>
2001-12-19 21:32:27 +0000

    moved all code concerning toplevel code from the window file to toplevel
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13101 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/toplevel.h
+++ include/wx/mac/toplevel.h
@@ -63,6 +63,30 @@ public:
     // implementation from now on
     // --------------------------
 
+	virtual void MacCreateRealWindow( const wxString& title,
+           const wxPoint& pos,
+           const wxSize& size,
+           long style,
+           const wxString& name ) ;
+	static WindowRef MacGetWindowInUpdate() { return s_macWindowInUpdate ; }
+	virtual void MacGetPortParams(Point* localOrigin, Rect* clipRect, WindowRef *window , wxWindowMac** rootwin ) ;
+	virtual void MacDoGetPortClientParams(Point* localOrigin, Rect* clipRect, WindowRef *window  , wxWindowMac** rootwin) ;
+  virtual void Clear() ;
+  virtual ControlHandle MacGetContainerForEmbedding() ;
+  WindowRef MacGetWindowRef() { return m_macWindow ; }
+	virtual void MacActivate( EventRecord *ev , bool inIsActivating ) ;
+	virtual void MacUpdate( long timestamp ) ;
+	virtual void MacMouseDown( EventRecord *ev , short windowPart ) ;
+	virtual void MacMouseUp( EventRecord *ev , short windowPart ) ;
+	virtual void MacMouseMoved( EventRecord *ev , short windowPart ) ;
+	virtual void MacKeyDown( EventRecord *ev ) ;
+	virtual void MacFireMouseEvent( EventRecord *ev ) ;
+  virtual void Raise();
+  virtual void Lower();
+  virtual void SetTitle( const wxString& title);
+  virtual bool Show( bool show = TRUE );
+  virtual void DoMoveWindow(int x, int y, int width, int height);
+  void MacInvalidate( const Rect * rect, bool eraseBackground ) ;
 protected:
     // common part of all ctors
     void Init();
@@ -73,10 +97,26 @@ protected:
     // should the frame be maximized when it will be shown? set by Maximize()
     // when it is called while the frame is hidden
     bool m_maximizeOnShow;
+
+    SInt16 m_macWindowBackgroundTheme ;
+    WindowRef m_macWindow ;
+    ControlHandle m_macRootControl ;
+    wxWindowMac* m_macFocus ;
+    RgnHandle m_macNoEraseUpdateRgn ;
+    bool m_macNeedsErasing ;
+
+	  static WindowRef s_macWindowInUpdate ;
 };
 
 // list of all frames and modeless dialogs
 extern WXDLLEXPORT_DATA(wxWindowList) wxModelessWindows;
+
+// associate mac windows with wx counterparts
+
+wxTopLevelWindowMac* wxFindWinFromMacWindow( WindowRef inWindow ) ;
+void wxAssociateWinWithMacWindow(WindowRef inWindow, wxTopLevelWindowMac *win) ;
+void wxRemoveMacWindowAssociation(wxTopLevelWindowMac *win) ;
+
 
 #endif // _WX_MSW_TOPLEVEL_H_
 
