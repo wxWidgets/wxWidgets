bf9dfac51cefdcca1d678518f051ecb7318027d1
Václav Slavík <vslavik@fastmail.fm>
2001-12-31 11:53:00 +0000

    wxSystemSettings mods in wxGTK and wxMGL
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13266 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/settings.h
+++ include/wx/settings.h
@@ -170,9 +170,9 @@ public:
 #elif defined(__WXMOTIF__)
     #include "wx/motif/settings.h"
 #elif defined(__WXGTK__)
-    #include "wx/gtk/settings.h"
+    #define wxHAS_SS_NATIVE
 #elif defined(__WXMGL__)
-    #include "wx/mgl/settings.h"
+    #define wxHAS_SS_NATIVE
 #elif defined(__WXMAC__)
     #include "wx/mac/settings.h"
 #elif defined(__WXPM__)
