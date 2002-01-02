9d4ca3aa8f594848a8f68140b25ec86549e12e98
Václav Slavík <vslavik@fastmail.fm>
2002-01-02 17:08:49 +0000

    fixed missing wxprec.h
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13311 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/bmpbase.cpp
+++ src/common/bmpbase.cpp
@@ -12,6 +12,13 @@
 #pragma implementation "bitmapbase.h"
 #endif
 
+// For compilers that support precompilation, includes "wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
 #include "wx/wx.h"
 #include "wx/setup.h"
 #include "wx/utils.h"
