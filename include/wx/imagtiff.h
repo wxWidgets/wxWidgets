d394f4791d95f0c2fedb94aa9109e4800596558d
Václav Slavík <vslavik@fastmail.fm>
2002-01-03 23:08:59 +0000

    made wxImageHander's default values consistent with wxImage (doesn't matter, wxImage passes index explicitly)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13346 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/imagtiff.h
+++ include/wx/imagtiff.h
@@ -37,7 +37,7 @@ public:
   };
 
 #if wxUSE_STREAMS
-  virtual bool LoadFile( wxImage *image, wxInputStream& stream, bool verbose=TRUE, int index=0 );
+  virtual bool LoadFile( wxImage *image, wxInputStream& stream, bool verbose=TRUE, int index=-1 );
   virtual bool SaveFile( wxImage *image, wxOutputStream& stream, bool verbose=TRUE );
   virtual bool DoCanRead( wxInputStream& stream );
   virtual int GetImageCount( wxInputStream& stream );
