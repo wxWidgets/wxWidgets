847341dbebd752290cb450dbbbc4c2bc9b6a6ac8
Julian Smart <julian@anthemion.co.uk>
2002-01-18 14:33:01 +0000

    Updated VC++ files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13631 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/vcupdate.bat
+++ distrib/msw/vcupdate.bat
@@ -1,8 +1,8 @@
 @echo off
 Rem Replace the DLL version number in the VC++ project files
 
-input Enter the old version name (e.g. wx22_0): %%oldName
-input Enter the new version name (e.g. wx22_1): %%newName
+input Enter the old version name (e.g. msw232): %%oldName
+input Enter the new version name (e.g. msw233): %%newName
 
 call %wxwin\distrib\msw\filerepl.bat %oldName %newName %wxwin\distrib\msw\vc.rsp
 echo Done.
