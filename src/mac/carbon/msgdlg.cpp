d09487dd4eb610315dcb0e73266314eb817b03cd
Stefan Csomor <csomor@advancedconcepts.ch>
2002-01-20 16:32:42 +0000

    removed non appearance implementations
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13698 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/msgdlg.cpp
+++ src/mac/carbon/msgdlg.cpp
@@ -22,12 +22,6 @@
 IMPLEMENT_CLASS(wxMessageDialog, wxDialog)
 #endif
 
-#define kMacOKAlertResourceID 128
-#define kMacYesNoAlertResourceID 129
-#define kMacYesNoCancelAlertResourceID 130
-#define kMacNoYesAlertResourceID 131
-#define kMacNoYesCancelAlertResourceID 132
-
 short language = 0 ;
 
 void wxMacConvertNewlines( const char *source , char * destination ) ;
@@ -97,214 +91,136 @@ int wxMessageDialog::ShowModal()
 
 	wxASSERT_MSG( ( m_dialogStyle & 0x3F ) != wxYES , "this style is not supported on mac" ) ;
 
-	if ( !UMAHasAppearance() )
-	{
-		int resourceID ;
-		
-	  if (m_dialogStyle & wxYES_NO)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	      resourceID = kMacYesNoCancelAlertResourceID;
-	    else
-	      resourceID = kMacYesNoAlertResourceID + language * 10 ;
-	  }
-	  else if (m_dialogStyle & wxOK)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	      resourceID = kMacOKAlertResourceID; // wrong
-	    else
-	      resourceID = kMacOKAlertResourceID;
-	  }
-		else
-		{
-			return resultbutton ;
-		}
-	/*
-	  if (hWnd)
-	    msStyle |= MB_APPLMODAL;
-	  else
-	    msStyle |= MB_TASKMODAL;
-	*/
 
-		ParamText( pascalTitle , pascalText , NULL , NULL ) ;
+	AlertStdAlertParamRec	param;
 
-	  if (m_dialogStyle & wxICON_EXCLAMATION)
-				result = Alert( resourceID , NULL ) ;
-	  else if (m_dialogStyle & wxICON_HAND)
-			result = StopAlert( resourceID , NULL ) ;
-	  else if (m_dialogStyle & wxICON_INFORMATION)
-				result = NoteAlert( resourceID , NULL ) ;
-	  else if (m_dialogStyle & wxICON_QUESTION)
-				result = CautionAlert( resourceID , NULL ) ;
-		else
-				result = Alert( resourceID , NULL ) ;
-
-		resultbutton = result ;
-		
-	  if (m_dialogStyle & wxYES_NO)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	    {
-				switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_YES ;
-						break ;
-					case 2 :
-						resultbutton = wxID_NO ;
-						break ;
-					case 3 :
-						resultbutton = wxID_CANCEL ;
-						break ;
-				}
-	    }
-	    else
-	    {
-				switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_YES ;
-						break ;
-					case 2 :
-						resultbutton = wxID_NO ;
-						break ;
-					case 3 :
-						break ;
-				}
-			}
-		}
-	}
+	param.movable 		= true;
+	param.filterProc 	= NULL ;
+	
+  if (m_dialogStyle & wxYES_NO)
+  {
+    if (m_dialogStyle & wxCANCEL)
+    {
+			param.defaultText 	= yesPString ;
+			param.cancelText 	= (StringPtr) kAlertDefaultCancelText;
+			param.otherText 	= noPString ;
+			param.helpButton 	= false ;
+			param.defaultButton = kAlertStdAlertOKButton;
+			param.cancelButton 	= kAlertStdAlertCancelButton;
+    }
+    else
+    {
+			param.defaultText 	= yesPString ;
+			param.cancelText 	= NULL;
+			param.otherText 	= noPString ;
+			param.helpButton 	= false ;
+			param.defaultButton = kAlertStdAlertOKButton;
+			param.cancelButton 	= 0;
+    }
+  }
+  else if (m_dialogStyle & wxOK)
+  {
+    if (m_dialogStyle & wxCANCEL)
+    {
+    	// thats a cancel missing
+			param.defaultText 	= (StringPtr) kAlertDefaultOKText ;
+			param.cancelText 	= NULL;
+			param.otherText 	= NULL;
+			param.helpButton 	= false ;
+			param.defaultButton = kAlertStdAlertOKButton;
+			param.cancelButton 	= 0;
+    }
+    else
+    {
+			param.defaultText 	= (StringPtr) kAlertDefaultOKText ;
+			param.cancelText 	= NULL;
+			param.otherText 	= NULL;
+			param.helpButton 	= false ;
+			param.defaultButton = kAlertStdAlertOKButton;
+			param.cancelButton 	= 0;
+    }
+  }
 	else
 	{
-		AlertStdAlertParamRec	param;
+		return resultbutton ;
+	}
 
-		param.movable 		= true;
-		param.filterProc 	= NULL ;
-		
-	  if (m_dialogStyle & wxYES_NO)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	    {
-				param.defaultText 	= yesPString ;
-				param.cancelText 	= (StringPtr) kAlertDefaultCancelText;
-				param.otherText 	= noPString ;
-				param.helpButton 	= false ;
-				param.defaultButton = kAlertStdAlertOKButton;
-				param.cancelButton 	= kAlertStdAlertCancelButton;
-	    }
-	    else
-	    {
-				param.defaultText 	= yesPString ;
-				param.cancelText 	= NULL;
-				param.otherText 	= noPString ;
-				param.helpButton 	= false ;
-				param.defaultButton = kAlertStdAlertOKButton;
-				param.cancelButton 	= 0;
-	    }
-	  }
-	  else if (m_dialogStyle & wxOK)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	    {
-	    	// thats a cancel missing
-				param.defaultText 	= (StringPtr) kAlertDefaultOKText ;
-				param.cancelText 	= NULL;
-				param.otherText 	= NULL;
-				param.helpButton 	= false ;
-				param.defaultButton = kAlertStdAlertOKButton;
-				param.cancelButton 	= 0;
-	    }
-	    else
-	    {
-				param.defaultText 	= (StringPtr) kAlertDefaultOKText ;
-				param.cancelText 	= NULL;
-				param.otherText 	= NULL;
-				param.helpButton 	= false ;
-				param.defaultButton = kAlertStdAlertOKButton;
-				param.cancelButton 	= 0;
-	    }
-	  }
+	param.position 		= 0;
+
+  if (m_dialogStyle & wxICON_EXCLAMATION)
+			StandardAlert( kAlertNoteAlert, pascalTitle, pascalText, &param, &result );
+  else if (m_dialogStyle & wxICON_HAND)
+			StandardAlert( kAlertStopAlert, pascalTitle, pascalText, &param, &result );
+  else if (m_dialogStyle & wxICON_INFORMATION)
+		StandardAlert( kAlertNoteAlert, pascalTitle, pascalText, &param, &result );
+  else if (m_dialogStyle & wxICON_QUESTION)
+			StandardAlert( kAlertCautionAlert, pascalTitle, pascalText, &param, &result );
+	else
+			StandardAlert( kAlertPlainAlert, pascalTitle, pascalText, &param, &result );
+
+	if (m_dialogStyle & wxOK)
+  {
+    if (m_dialogStyle & wxCANCEL)				
+    {
+    	//TODO add Cancelbutton
+    	switch( result )
+			{
+				case 1 :
+					resultbutton = wxID_OK ;
+					break ;
+				case 2 :
+					break ;
+				case 3 :
+					break ;
+			}
+		}
 		else
 		{
-			return resultbutton ;
-		}
-
-		param.position 		= 0;
-
-	  if (m_dialogStyle & wxICON_EXCLAMATION)
-				StandardAlert( kAlertNoteAlert, pascalTitle, pascalText, &param, &result );
-	  else if (m_dialogStyle & wxICON_HAND)
-				StandardAlert( kAlertStopAlert, pascalTitle, pascalText, &param, &result );
-	  else if (m_dialogStyle & wxICON_INFORMATION)
-			StandardAlert( kAlertNoteAlert, pascalTitle, pascalText, &param, &result );
-	  else if (m_dialogStyle & wxICON_QUESTION)
-				StandardAlert( kAlertCautionAlert, pascalTitle, pascalText, &param, &result );
-		else
-				StandardAlert( kAlertPlainAlert, pascalTitle, pascalText, &param, &result );
-
-		if (m_dialogStyle & wxOK)
-	  {
-	    if (m_dialogStyle & wxCANCEL)				
-	    {
-	    	//TODO add Cancelbutton
-	    	switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_OK ;
-						break ;
-					case 2 :
-						break ;
-					case 3 :
-						break ;
-				}
-			}
-			else
+    	switch( result )
 			{
-	    	switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_OK ;
-						break ;
-					case 2 :
-						break ;
-					case 3 :
-						break ;
-				}
+				case 1 :
+					resultbutton = wxID_OK ;
+					break ;
+				case 2 :
+					break ;
+				case 3 :
+					break ;
 			}
 		}
-	  else if (m_dialogStyle & wxYES_NO)
-	  {
-	    if (m_dialogStyle & wxCANCEL)
-	    {
-				switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_YES ;
-						break ;
-					case 2 :
-						resultbutton = wxID_CANCEL ;
-						break ;
-					case 3 :
-						resultbutton = wxID_NO ;
-						break ;
-				}
-	    }
-	    else
-	    {
-				switch( result )
-				{
-					case 1 :
-						resultbutton = wxID_YES ;
-						break ;
-					case 2 :
-						break ;
-					case 3 :
-						resultbutton = wxID_NO ;
-						break ;
-				}
-			}
-		} 
 	}
+  else if (m_dialogStyle & wxYES_NO)
+  {
+    if (m_dialogStyle & wxCANCEL)
+    {
+			switch( result )
+			{
+				case 1 :
+					resultbutton = wxID_YES ;
+					break ;
+				case 2 :
+					resultbutton = wxID_CANCEL ;
+					break ;
+				case 3 :
+					resultbutton = wxID_NO ;
+					break ;
+			}
+    }
+    else
+    {
+			switch( result )
+			{
+				case 1 :
+					resultbutton = wxID_YES ;
+					break ;
+				case 2 :
+					break ;
+				case 3 :
+					resultbutton = wxID_NO ;
+					break ;
+			}
+		}
+	} 
+
 	return resultbutton ;
 }
 
