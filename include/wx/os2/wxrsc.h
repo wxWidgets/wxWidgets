98b67b810e2102b52701c52e03b2a5cc8f46afae
David Webster <Dave.Webster@bhmi.com>
2002-01-21 19:51:49 +0000

    More resource files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@13730 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/os2/wxrsc.h
@@ -0,0 +1,48 @@
+/*
+// Pointer ID's
+*/
+#define WXCURSOR_HAND                100
+#define WXCURSOR_BULLSEYE            101
+#define WXCURSOR_PENCIL              102
+#define WXCURSOR_MAGNIFIER           103
+#define WXCURSOR_NO_ENTRY            104
+#define WXCURSOR_SIZING              105
+#define WXCURSOR_ROLLER              106
+#define WXCURSOR_WATCH               107
+#define WXCURSOR_PBRUSH              108
+#define WXCURSOR_PLEFT               109
+#define WXCURSOR_PRIGHT              110
+#define WXCURSOR_QARROW              111
+#define WXCURSOR_BLANK               112
+
+/*
+// Icon Ids
+*/
+#define wxICON_TIP                   113
+#define wxICON_SMALL_CLOSED_FOLDER   114
+#define wxICON_SMALL_OPEN_FOLDER     115
+#define wxICON_SMALL_FILE            116
+#define wxICON_SMALL_COMPUTER        117
+#define wxICON_SMALL_DRIVE           118
+#define wxICON_SMALL_CDROM           119
+#define wxICON_SMALL_FLOPPY          120
+#define wxICON_SMALL_REMOVEABLE      121
+
+/*
+// Bitmap Ids
+*/
+#define wxDISABLE_BUTTON_BITMAP      122
+#define wxTICK_BITMAP                123
+#define wxCROSS_BITMAP               124
+#define wxCSQUERY_BITMAP             125
+#define wxBITMAP_STD_COLOURS         126
+
+/*
+// Dialog Ids -- must match enums in Toplevel.cpp
+*/
+#define ID_RESIZEABLEDIALOG          127
+#define ID_CAPTIONDIALOG             128
+#define ID_NOCAPTIONDIALOG           129
+
+#define ID_WINDDOW_MENU              130
+
