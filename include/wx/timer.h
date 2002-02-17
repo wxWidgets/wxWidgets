c331ac2ed75ba5c435d745e9282c27a339da4f18
Václav Slavík <vslavik@fastmail.fm>
2002-02-17 14:57:33 +0000

    use generic timer in wxMGL and wxX11
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@14276 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/timer.h
+++ include/wx/timer.h
@@ -107,10 +107,8 @@ protected:
     #include "wx/motif/timer.h"
 #elif defined(__WXGTK__)
     #include "wx/gtk/timer.h"
-#elif defined(__WXX11__)
-    #include "wx/x11/timer.h"
-#elif defined(__WXMGL__)
-    #include "wx/mgl/timer.h"
+#elif defined(__WXX11__) || defined(__WXMGL__)
+    #include "wx/generic/timer.h"
 #elif defined(__WXMAC__)
     #include "wx/mac/timer.h"
 #elif defined(__WXPM__)
