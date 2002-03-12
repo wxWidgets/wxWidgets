d5cf34f4dc5388b6791cf6abee76e63470fa8ede
Julian Smart <julian@anthemion.co.uk>
2002-03-12 12:45:52 +0000

    Added constructor for wxColourRefData, else CloneRefData did a binary copy which caused an assert in AllocExclusive (Add to Custom Colours in Colour Dialog)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@14564 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/x11/colour.cpp
+++ src/x11/colour.cpp
@@ -35,6 +35,13 @@ public:
         m_colormap = (WXColormap *) NULL;
         m_hasPixel = FALSE;
     }
+    wxColourRefData(const wxColourRefData& data):
+        wxObjectRefData()
+    {
+        m_color = data.m_color;
+        m_colormap = data.m_colormap;
+        m_hasPixel = data.m_hasPixel;
+    }
     
     ~wxColourRefData()
     {
