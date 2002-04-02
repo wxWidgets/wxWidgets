01cf2f9512e90d1c70e9423f5b099fe5624bcc1b
Vadim Zeitlin <vadim@wxwidgets.org>
2002-04-02 10:20:52 +0000

    fixed paper size calculation (convert from 10ths of mm to mm correctly)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@14904 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/cmndata.cpp
+++ src/common/cmndata.cpp
@@ -1512,12 +1512,9 @@ void wxPageSetupDialogData::CalculatePaperSizeFromId()
 
     wxSize sz = wxThePrintPaperDatabase->GetSize(m_printData.GetPaperId());
 
-    if (sz.x != 0)
-    {
-        // sz is in 10ths of a mm, so multiply by 10.
-        m_paperSize.x = sz.x * 10;
-        m_paperSize.y = sz.y * 10;
-    }
+    // sz is in 10ths of a mm, while paper size is in mm
+    m_paperSize.x = sz.x / 10;
+    m_paperSize.y = sz.y / 10;
 }
 
 #endif // wxUSE_PRINTING_ARCHITECTURE
