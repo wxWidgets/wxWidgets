62fa9712a0865b11e82b7dff6f0305d66bb6b98e
Vadim Zeitlin <vadim@wxwidgets.org>
2002-04-07 21:02:10 +0000

    added option to show the raw key codes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@15002 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/keyboard/keyboard.cpp
+++ samples/keyboard/keyboard.cpp
@@ -62,6 +62,7 @@ public:
     void OnAbout(wxCommandEvent& event);
     void OnClear(wxCommandEvent& event);
     void OnSkip(wxCommandEvent& event);
+    void OnShowRaw(wxCommandEvent& event);
 
     void OnSize(wxSizeEvent& event);
 
@@ -140,11 +141,13 @@ public:
                    wxRAISED_BORDER)
     {
         m_skip = TRUE;
+        m_showRaw = FALSE;
 
         SetBackgroundColour(*wxBLUE);
     }
 
     void SetSkip(bool skip) { m_skip = skip; }
+    void SetShowRaw(bool show) { m_showRaw = show; }
 
 protected:
     void OnKeyDown(wxKeyEvent& event) { LogEvent(_T("Key down"), event); }
@@ -164,6 +167,7 @@ private:
     void LogEvent(const wxChar *name, wxKeyEvent& event);
 
     bool m_skip;
+    bool m_showRaw;
 
     DECLARE_EVENT_TABLE()
 };
@@ -188,6 +192,7 @@ enum
 
     Keyboard_Clear,
     Keyboard_Skip,
+    Keyboard_ShowRaw,
 
     // it is important for the id corresponding to the "About" command to have
     // this standard value as otherwise it won't be handled properly under Mac
@@ -208,6 +213,7 @@ BEGIN_EVENT_TABLE(MyFrame, wxFrame)
 
     EVT_MENU(Keyboard_Clear, MyFrame::OnClear)
     EVT_MENU(Keyboard_Skip, MyFrame::OnSkip)
+    EVT_MENU(Keyboard_ShowRaw, MyFrame::OnShowRaw)
 
     EVT_SIZE(MyFrame::OnSize)
 END_EVENT_TABLE()
@@ -261,6 +267,8 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size, 
     menuFile->Append(Keyboard_Quit, _T("E&xit\tAlt-X"), _T("Quit this program"));
 
     wxMenu *menuKeys = new wxMenu;
+    menuKeys->AppendCheckItem(Keyboard_ShowRaw, _T("Show &raw keys\tCtrl-R"));
+    menuKeys->AppendSeparator();
     menuKeys->AppendCheckItem(Keyboard_Skip, _T("&Skip key down\tCtrl-S"));
 
     // the "About" item should be in the help menu
@@ -277,6 +285,10 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size, 
     SetMenuBar(menuBar);
 
     menuBar->Check(Keyboard_Skip, TRUE);
+
+#ifndef wxHAS_RAW_KEY_CODES
+    menuBar->Enable(Keyboard_ShowRaw, FALSE);
+#endif // !wxHAS_RAW_KEY_CODES
 #endif // wxUSE_MENUS
 
     m_winText = new TextWindow(this);
@@ -302,11 +314,10 @@ void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
 
 void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 {
-    wxString msg;
-    msg.Printf( _T("This is the about dialog of keyboard sample.\n")
-                _T("Welcome to %s"), wxVERSION_STRING);
+    wxString msg = _T("Demonstrates keyboard event processing in wxWindows\n")
+                   _T("© 2002 Vadim Zeitlin");
 
-    wxMessageBox(msg, _T("About Keyboard"), wxOK | wxICON_INFORMATION, this);
+    wxMessageBox(msg, _T("About wxKeyboard"), wxOK | wxICON_INFORMATION, this);
 }
 
 void MyFrame::OnClear(wxCommandEvent& event)
@@ -317,6 +328,11 @@ void MyFrame::OnClear(wxCommandEvent& event)
 void MyFrame::OnSkip(wxCommandEvent& event)
 {
     m_winText->SetSkip(event.IsChecked());
+}
+
+void MyFrame::OnShowRaw(wxCommandEvent& event)
+{
+    m_winText->SetShowRaw(event.IsChecked());
 }
 
 void MyFrame::OnSize(wxSizeEvent& WXUNUSED(event))
@@ -340,127 +356,138 @@ void TextWindow::LogEvent(const wxChar *name, wxKeyEvent& event)
     {
         switch ( keycode )
         {
-            case WXK_BACK: key = "BACK"; break;
-            case WXK_TAB: key = "TAB"; break;
-            case WXK_RETURN: key = "RETURN"; break;
-            case WXK_ESCAPE: key = "ESCAPE"; break;
-            case WXK_SPACE: key = "SPACE"; break;
-            case WXK_DELETE: key = "DELETE"; break;
-            case WXK_START: key = "START"; break;
-            case WXK_LBUTTON: key = "LBUTTON"; break;
-            case WXK_RBUTTON: key = "RBUTTON"; break;
-            case WXK_CANCEL: key = "CANCEL"; break;
-            case WXK_MBUTTON: key = "MBUTTON"; break;
-            case WXK_CLEAR: key = "CLEAR"; break;
-            case WXK_SHIFT: key = "SHIFT"; break;
-            case WXK_ALT: key = "ALT"; break;
-            case WXK_CONTROL: key = "CONTROL"; break;
-            case WXK_MENU: key = "MENU"; break;
-            case WXK_PAUSE: key = "PAUSE"; break;
-            case WXK_CAPITAL: key = "CAPITAL"; break;
-            case WXK_PRIOR: key = "PRIOR"; break;
-            case WXK_NEXT: key = "NEXT"; break;
-            case WXK_END: key = "END"; break;
-            case WXK_HOME: key = "HOME"; break;
-            case WXK_LEFT: key = "LEFT"; break;
-            case WXK_UP: key = "UP"; break;
-            case WXK_RIGHT: key = "RIGHT"; break;
-            case WXK_DOWN: key = "DOWN"; break;
-            case WXK_SELECT: key = "SELECT"; break;
-            case WXK_PRINT: key = "PRINT"; break;
-            case WXK_EXECUTE: key = "EXECUTE"; break;
-            case WXK_SNAPSHOT: key = "SNAPSHOT"; break;
-            case WXK_INSERT: key = "INSERT"; break;
-            case WXK_HELP: key = "HELP"; break;
-            case WXK_NUMPAD0: key = "NUMPAD0"; break;
-            case WXK_NUMPAD1: key = "NUMPAD1"; break;
-            case WXK_NUMPAD2: key = "NUMPAD2"; break;
-            case WXK_NUMPAD3: key = "NUMPAD3"; break;
-            case WXK_NUMPAD4: key = "NUMPAD4"; break;
-            case WXK_NUMPAD5: key = "NUMPAD5"; break;
-            case WXK_NUMPAD6: key = "NUMPAD6"; break;
-            case WXK_NUMPAD7: key = "NUMPAD7"; break;
-            case WXK_NUMPAD8: key = "NUMPAD8"; break;
-            case WXK_NUMPAD9: key = "NUMPAD9"; break;
-            case WXK_MULTIPLY: key = "MULTIPLY"; break;
-            case WXK_ADD: key = "ADD"; break;
-            case WXK_SEPARATOR: key = "SEPARATOR"; break;
-            case WXK_SUBTRACT: key = "SUBTRACT"; break;
-            case WXK_DECIMAL: key = "DECIMAL"; break;
-            case WXK_DIVIDE: key = "DIVIDE"; break;
-            case WXK_F1: key = "F1"; break;
-            case WXK_F2: key = "F2"; break;
-            case WXK_F3: key = "F3"; break;
-            case WXK_F4: key = "F4"; break;
-            case WXK_F5: key = "F5"; break;
-            case WXK_F6: key = "F6"; break;
-            case WXK_F7: key = "F7"; break;
-            case WXK_F8: key = "F8"; break;
-            case WXK_F9: key = "F9"; break;
-            case WXK_F10: key = "F10"; break;
-            case WXK_F11: key = "F11"; break;
-            case WXK_F12: key = "F12"; break;
-            case WXK_F13: key = "F13"; break;
-            case WXK_F14: key = "F14"; break;
-            case WXK_F15: key = "F15"; break;
-            case WXK_F16: key = "F16"; break;
-            case WXK_F17: key = "F17"; break;
-            case WXK_F18: key = "F18"; break;
-            case WXK_F19: key = "F19"; break;
-            case WXK_F20: key = "F20"; break;
-            case WXK_F21: key = "F21"; break;
-            case WXK_F22: key = "F22"; break;
-            case WXK_F23: key = "F23"; break;
-            case WXK_F24: key = "F24"; break;
-            case WXK_NUMLOCK: key = "NUMLOCK"; break;
-            case WXK_SCROLL: key = "SCROLL"; break;
-            case WXK_PAGEUP: key = "PAGEUP"; break;
-            case WXK_PAGEDOWN: key = "PAGEDOWN"; break;
-            case WXK_NUMPAD_SPACE: key = "NUMPAD_SPACE"; break;
-            case WXK_NUMPAD_TAB: key = "NUMPAD_TAB"; break;
-            case WXK_NUMPAD_ENTER: key = "NUMPAD_ENTER"; break;
-            case WXK_NUMPAD_F1: key = "NUMPAD_F1"; break;
-            case WXK_NUMPAD_F2: key = "NUMPAD_F2"; break;
-            case WXK_NUMPAD_F3: key = "NUMPAD_F3"; break;
-            case WXK_NUMPAD_F4: key = "NUMPAD_F4"; break;
-            case WXK_NUMPAD_HOME: key = "NUMPAD_HOME"; break;
-            case WXK_NUMPAD_LEFT: key = "NUMPAD_LEFT"; break;
-            case WXK_NUMPAD_UP: key = "NUMPAD_UP"; break;
-            case WXK_NUMPAD_RIGHT: key = "NUMPAD_RIGHT"; break;
-            case WXK_NUMPAD_DOWN: key = "NUMPAD_DOWN"; break;
-            case WXK_NUMPAD_PRIOR: key = "NUMPAD_PRIOR"; break;
-            case WXK_NUMPAD_PAGEUP: key = "NUMPAD_PAGEUP"; break;
-            case WXK_NUMPAD_PAGEDOWN: key = "NUMPAD_PAGEDOWN"; break;
-            case WXK_NUMPAD_END: key = "NUMPAD_END"; break;
-            case WXK_NUMPAD_BEGIN: key = "NUMPAD_BEGIN"; break;
-            case WXK_NUMPAD_INSERT: key = "NUMPAD_INSERT"; break;
-            case WXK_NUMPAD_DELETE: key = "NUMPAD_DELETE"; break;
-            case WXK_NUMPAD_EQUAL: key = "NUMPAD_EQUAL"; break;
-            case WXK_NUMPAD_MULTIPLY: key = "NUMPAD_MULTIPLY"; break;
-            case WXK_NUMPAD_ADD: key = "NUMPAD_ADD"; break;
-            case WXK_NUMPAD_SEPARATOR: key = "NUMPAD_SEPARATOR"; break;
-            case WXK_NUMPAD_SUBTRACT: key = "NUMPAD_SUBTRACT"; break;
-            case WXK_NUMPAD_DECIMAL: key = "NUMPAD_DECIMAL"; break;
+            case WXK_BACK: key = _T("BACK"); break;
+            case WXK_TAB: key = _T("TAB"); break;
+            case WXK_RETURN: key = _T("RETURN"); break;
+            case WXK_ESCAPE: key = _T("ESCAPE"); break;
+            case WXK_SPACE: key = _T("SPACE"); break;
+            case WXK_DELETE: key = _T("DELETE"); break;
+            case WXK_START: key = _T("START"); break;
+            case WXK_LBUTTON: key = _T("LBUTTON"); break;
+            case WXK_RBUTTON: key = _T("RBUTTON"); break;
+            case WXK_CANCEL: key = _T("CANCEL"); break;
+            case WXK_MBUTTON: key = _T("MBUTTON"); break;
+            case WXK_CLEAR: key = _T("CLEAR"); break;
+            case WXK_SHIFT: key = _T("SHIFT"); break;
+            case WXK_ALT: key = _T("ALT"); break;
+            case WXK_CONTROL: key = _T("CONTROL"); break;
+            case WXK_MENU: key = _T("MENU"); break;
+            case WXK_PAUSE: key = _T("PAUSE"); break;
+            case WXK_CAPITAL: key = _T("CAPITAL"); break;
+            case WXK_PRIOR: key = _T("PRIOR"); break;
+            case WXK_NEXT: key = _T("NEXT"); break;
+            case WXK_END: key = _T("END"); break;
+            case WXK_HOME: key = _T("HOME"); break;
+            case WXK_LEFT: key = _T("LEFT"); break;
+            case WXK_UP: key = _T("UP"); break;
+            case WXK_RIGHT: key = _T("RIGHT"); break;
+            case WXK_DOWN: key = _T("DOWN"); break;
+            case WXK_SELECT: key = _T("SELECT"); break;
+            case WXK_PRINT: key = _T("PRINT"); break;
+            case WXK_EXECUTE: key = _T("EXECUTE"); break;
+            case WXK_SNAPSHOT: key = _T("SNAPSHOT"); break;
+            case WXK_INSERT: key = _T("INSERT"); break;
+            case WXK_HELP: key = _T("HELP"); break;
+            case WXK_NUMPAD0: key = _T("NUMPAD0"); break;
+            case WXK_NUMPAD1: key = _T("NUMPAD1"); break;
+            case WXK_NUMPAD2: key = _T("NUMPAD2"); break;
+            case WXK_NUMPAD3: key = _T("NUMPAD3"); break;
+            case WXK_NUMPAD4: key = _T("NUMPAD4"); break;
+            case WXK_NUMPAD5: key = _T("NUMPAD5"); break;
+            case WXK_NUMPAD6: key = _T("NUMPAD6"); break;
+            case WXK_NUMPAD7: key = _T("NUMPAD7"); break;
+            case WXK_NUMPAD8: key = _T("NUMPAD8"); break;
+            case WXK_NUMPAD9: key = _T("NUMPAD9"); break;
+            case WXK_MULTIPLY: key = _T("MULTIPLY"); break;
+            case WXK_ADD: key = _T("ADD"); break;
+            case WXK_SEPARATOR: key = _T("SEPARATOR"); break;
+            case WXK_SUBTRACT: key = _T("SUBTRACT"); break;
+            case WXK_DECIMAL: key = _T("DECIMAL"); break;
+            case WXK_DIVIDE: key = _T("DIVIDE"); break;
+            case WXK_F1: key = _T("F1"); break;
+            case WXK_F2: key = _T("F2"); break;
+            case WXK_F3: key = _T("F3"); break;
+            case WXK_F4: key = _T("F4"); break;
+            case WXK_F5: key = _T("F5"); break;
+            case WXK_F6: key = _T("F6"); break;
+            case WXK_F7: key = _T("F7"); break;
+            case WXK_F8: key = _T("F8"); break;
+            case WXK_F9: key = _T("F9"); break;
+            case WXK_F10: key = _T("F10"); break;
+            case WXK_F11: key = _T("F11"); break;
+            case WXK_F12: key = _T("F12"); break;
+            case WXK_F13: key = _T("F13"); break;
+            case WXK_F14: key = _T("F14"); break;
+            case WXK_F15: key = _T("F15"); break;
+            case WXK_F16: key = _T("F16"); break;
+            case WXK_F17: key = _T("F17"); break;
+            case WXK_F18: key = _T("F18"); break;
+            case WXK_F19: key = _T("F19"); break;
+            case WXK_F20: key = _T("F20"); break;
+            case WXK_F21: key = _T("F21"); break;
+            case WXK_F22: key = _T("F22"); break;
+            case WXK_F23: key = _T("F23"); break;
+            case WXK_F24: key = _T("F24"); break;
+            case WXK_NUMLOCK: key = _T("NUMLOCK"); break;
+            case WXK_SCROLL: key = _T("SCROLL"); break;
+            case WXK_PAGEUP: key = _T("PAGEUP"); break;
+            case WXK_PAGEDOWN: key = _T("PAGEDOWN"); break;
+            case WXK_NUMPAD_SPACE: key = _T("NUMPAD_SPACE"); break;
+            case WXK_NUMPAD_TAB: key = _T("NUMPAD_TAB"); break;
+            case WXK_NUMPAD_ENTER: key = _T("NUMPAD_ENTER"); break;
+            case WXK_NUMPAD_F1: key = _T("NUMPAD_F1"); break;
+            case WXK_NUMPAD_F2: key = _T("NUMPAD_F2"); break;
+            case WXK_NUMPAD_F3: key = _T("NUMPAD_F3"); break;
+            case WXK_NUMPAD_F4: key = _T("NUMPAD_F4"); break;
+            case WXK_NUMPAD_HOME: key = _T("NUMPAD_HOME"); break;
+            case WXK_NUMPAD_LEFT: key = _T("NUMPAD_LEFT"); break;
+            case WXK_NUMPAD_UP: key = _T("NUMPAD_UP"); break;
+            case WXK_NUMPAD_RIGHT: key = _T("NUMPAD_RIGHT"); break;
+            case WXK_NUMPAD_DOWN: key = _T("NUMPAD_DOWN"); break;
+            case WXK_NUMPAD_PRIOR: key = _T("NUMPAD_PRIOR"); break;
+            case WXK_NUMPAD_PAGEUP: key = _T("NUMPAD_PAGEUP"); break;
+            case WXK_NUMPAD_PAGEDOWN: key = _T("NUMPAD_PAGEDOWN"); break;
+            case WXK_NUMPAD_END: key = _T("NUMPAD_END"); break;
+            case WXK_NUMPAD_BEGIN: key = _T("NUMPAD_BEGIN"); break;
+            case WXK_NUMPAD_INSERT: key = _T("NUMPAD_INSERT"); break;
+            case WXK_NUMPAD_DELETE: key = _T("NUMPAD_DELETE"); break;
+            case WXK_NUMPAD_EQUAL: key = _T("NUMPAD_EQUAL"); break;
+            case WXK_NUMPAD_MULTIPLY: key = _T("NUMPAD_MULTIPLY"); break;
+            case WXK_NUMPAD_ADD: key = _T("NUMPAD_ADD"); break;
+            case WXK_NUMPAD_SEPARATOR: key = _T("NUMPAD_SEPARATOR"); break;
+            case WXK_NUMPAD_SUBTRACT: key = _T("NUMPAD_SUBTRACT"); break;
+            case WXK_NUMPAD_DECIMAL: key = _T("NUMPAD_DECIMAL"); break;
+            case WXK_NUMPAD_DIVIDE: key = _T("NUMPAD_DIVIDE"); break;
 
             default:
             {
                if ( wxIsprint((int)keycode) )
                    key.Printf(_T("'%c'"), (char)keycode);
                else if ( keycode > 0 && keycode < 27 )
-                   key.Printf(_("Ctrl-%c"), _T('A') + keycode - 1);
+                   key.Printf(_T("Ctrl-%c"), _T('A') + keycode - 1);
                else
                    key.Printf(_T("unknown (%ld)"), keycode);
             }
         }
     }
 
-    wxLogMessage( _T("%s event: %s (flags = %c%c%c%c)"),
-                  name,
-                  key.c_str(),
-                  GetChar( event.ControlDown(), _T('C') ),
-                  GetChar( event.AltDown(), _T('A') ),
-                  GetChar( event.ShiftDown(), _T('S') ),
-                  GetChar( event.MetaDown(), _T('M') ) );
+    wxString msg;
+    msg.Printf(_T("%s event: %s (flags = %c%c%c%c)"),
+               name,
+               key.c_str(),
+               GetChar(event.ControlDown(), _T('C')),
+               GetChar(event.AltDown(), _T('A')),
+               GetChar(event.ShiftDown(), _T('S')),
+               GetChar(event.MetaDown(), _T('M')));
+
+    if ( m_showRaw )
+    {
+        msg += wxString::Format(_T(" (raw key code/flags: %lu and 0x%lx)"),
+                                event.GetRawKeyCode(),
+                                event.GetRawKeyFlags());
+    }
+
+    wxLogMessage(msg);
 
     if ( m_skip )
     {
