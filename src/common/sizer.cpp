247cdef5df07448be1137dd48c0f16fe4519d042
Julian Smart <julian@anthemion.co.uk>
2002-04-11 09:11:59 +0000

    wxSizer::Fit calls SetClientSize, not SetSize. This fixes the tool window in the Life demo.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@15088 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/sizer.cpp
+++ src/common/sizer.cpp
@@ -408,7 +408,7 @@ void wxSizer::Fit( wxWindow *window )
     else
         size = GetMinWindowSize( window );
 
-    window->SetSize( size );
+    window->SetClientSize( size );
 }
 
 void wxSizer::Layout()
