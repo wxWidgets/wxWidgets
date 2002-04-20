4b912ef220f5509b4d6a09a38c9e7839b5f11a6b
Vadim Zeitlin <vadim@wxwidgets.org>
2002-04-20 11:20:49 +0000

    made operator==() and \!=() const (still doesn't make them correct probably)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@15215 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/gtk1/icon.h
+++ include/wx/gtk1/icon.h
@@ -38,22 +38,22 @@ public:
 
     // For compatibility with wxMSW where desired size is sometimes required to
     // distinguish between multiple icons in a resource.
-    wxIcon( const wxString& filename, int type = wxBITMAP_TYPE_XPM, 
+    wxIcon( const wxString& filename, int type = wxBITMAP_TYPE_XPM,
             int WXUNUSED(desiredWidth)=-1, int WXUNUSED(desiredHeight)=-1 ) :
         wxBitmap(filename, type)
     {
     }
     wxIcon( char **bits, int width=-1, int height=-1 );
 
-    wxIcon& operator = (const wxIcon& icon);
-    inline bool operator == (const wxIcon& icon) { return m_refData == icon.m_refData; }
-    inline bool operator != (const wxIcon& icon) { return m_refData != icon.m_refData; }
+    wxIcon& operator=(const wxIcon& icon);
+    bool operator==(const wxIcon& icon) const { return m_refData == icon.m_refData; }
+    bool operator!=(const wxIcon& icon) const { return !(*this == icon); }
 
     // create from bitmap (which should have a mask unless it's monochrome):
     // there shouldn't be any implicit bitmap -> icon conversion (i.e. no
     // ctors, assignment operators...), but it's ok to have such function
     void CopyFromBitmap(const wxBitmap& bmp);
-  
+
 private:
     DECLARE_DYNAMIC_CLASS(wxIcon)
 };
