dfee19edbc2b2a27b2a8de39f29fe6555e7c6dc1
Vadim Zeitlin <vadim@wxwidgets.org>
2002-05-07 23:16:29 +0000

    fixed typos and tmakecmd/TMAKE/TM confusion
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@15414 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/tmake/makeall.bat
+++ distrib/msw/tmake/makeall.bat
@@ -11,11 +11,6 @@ rem tmake. The environment variable WX or WXWIN should be defined and contain
 rem the root directory of wxWindows installation. TMAKE program should be in
 rem path or, alternatively, TMAKE environment variable should be set.
 
-rem set tmakecmd=E:\Perl\5.00471\bin\MSWin32-x86\perl.exe g:\Unix\tmake\bin\tmake
-Rem set tmakecmd=d:\programs\tmake\bin\tmake
-Rem Let's assume that you have tmake in your PATH
-set tmakecmd=tmake
-
 if "x%WX%" == "x" goto skip
 set WXDIR=%WX%
 goto ok
@@ -30,6 +25,8 @@ echo "Please set WX environment variable!"
 goto end
 
 :ok
+rem Let's assume that you have tmake in your PATH but let override it with
+rem TMAKE env var
 set TM=tmake
 if "x%TMAKE%" == "x" goto skip2
 set TM=%TMAKE%
@@ -39,37 +36,36 @@ set TM=%TMAKE%
 rem this loop just doesn't want to work under command.com and cmd.exe and
 rem 4nt.exe, so I preferred to unroll it.
 rem for %%c in (b32 bcc dos g95 sc vc wat) %TM% -t %c% wxwin.pro -o %WXDIR%\src\msw\makefile.%c%
-
-echo Generating for for Visual C++ 6.0 wxBase C++ (32 bits)...
-%tmakecmd% -t basevc wxwin.pro -o %WXDIR%\src\msw\makebase.vc
-echo Generating for for Visual C++ 6.0 wxWindows.dsp
-%tmakecmd% -t vc6msw wxwin.pro -o %WXDIR%\src\wxWindows.dsp
-echo Generating for for Visual C++ 6.0 wxUniv.dsp
-%tmakecmd% -t vc6univ wxwin.pro -o %WXDIR%\src\wxUniv.dsp
-echo Generating for for Visual C++ 6.0 wxBase.dsp
-%tmakecmd% -t vc6base wxwin.pro -o %WXDIR%\src\wxBase.dsp
+echo Generating for Visual C++ 6.0 wxBase C++ (32 bits)...
+%TM% -t basevc wxwin.pro -o %WXDIR%\src\msw\makebase.vc
+echo Generating for Visual C++ 6.0 wxWindows.dsp
+%TM% -t vc6msw wxwin.pro -o %WXDIR%\src\wxWindows.dsp
+echo Generating for Visual C++ 6.0 wxUniv.dsp
+%TM% -t vc6univ wxwin.pro -o %WXDIR%\src\wxUniv.dsp
+echo Generating for Visual C++ 6.0 wxBase.dsp
+%TM% -t vc6base wxwin.pro -o %WXDIR%\src\wxBase.dsp
 echo Generating for Visual C++ 4.0...
-%tmakecmd% -t vc wxwin.pro -o %WXDIR%\src\msw\makefile.vc
+%TM% -t vc wxwin.pro -o %WXDIR%\src\msw\makefile.vc
 echo Generating for Borland C++ (32 bits)...
-%tmakecmd% -t b32 wxwin.pro -o %WXDIR%\src\msw\makefile.b32
+%TM% -t b32 wxwin.pro -o %WXDIR%\src\msw\makefile.b32
 echo Generating for Visual C++ 1.52...
-%tmakecmd% -t dos wxwin.pro -o %WXDIR%\src\msw\makefile.dos
+%TM% -t dos wxwin.pro -o %WXDIR%\src\msw\makefile.dos
 echo Generating for Borland C++ (16 bits)...
-%tmakecmd% -t bcc wxwin.pro -o %WXDIR%\src\msw\makefile.bcc
+%TM% -t bcc wxwin.pro -o %WXDIR%\src\msw\makefile.bcc
 echo Generating for Cygwin/Mingw32
-%tmakecmd% -t g95 wxwin.pro -o %WXDIR%\src\msw\makefile.g95
+%TM% -t g95 wxwin.pro -o %WXDIR%\src\msw\makefile.g95
 echo Generating for Symantec C++...
-%tmakecmd% -t sc wxwin.pro -o %WXDIR%\src\msw\makefile.sc
+%TM% -t sc wxwin.pro -o %WXDIR%\src\msw\makefile.sc
 echo Generating for Watcom C++...
-%tmakecmd% -t wat wxwin.pro -o %WXDIR%\src\msw\makefile.wat
+%TM% -t wat wxwin.pro -o %WXDIR%\src\msw\makefile.wat
 echo Generating for Unix and Configure...
-%tmakecmd% -t gtk wxwin.pro -o %WXDIR%\src\gtk\files.lst
-%tmakecmd% -t mgl wxwin.pro -o %WXDIR%\src\mgl\files.lst
-%tmakecmd% -t micro wxwin.pro -o %WXDIR%\src\microwin\files.lst
-%tmakecmd% -t univ wxwin.pro -o %WXDIR%\src\univ\files.lst
-%tmakecmd% -t msw wxwin.pro -o %WXDIR%\src\msw\files.lst
-%tmakecmd% -t mac wxwin.pro -o %WXDIR%\src\mac\files.lst
-%tmakecmd% -t motif wxwin.pro -o %WXDIR%\src\motif\files.lst
-%tmakecmd% -t x11 wxwin.pro -o %WXDIR%\src\x11\files.lst
-%tmakecmd% -t os2 wxwin.pro -o %WXDIR%\src\os2\files.lst
+%TM% -t gtk wxwin.pro -o %WXDIR%\src\gtk\files.lst
+%TM% -t mgl wxwin.pro -o %WXDIR%\src\mgl\files.lst
+%TM% -t micro wxwin.pro -o %WXDIR%\src\microwin\files.lst
+%TM% -t univ wxwin.pro -o %WXDIR%\src\univ\files.lst
+%TM% -t msw wxwin.pro -o %WXDIR%\src\msw\files.lst
+%TM% -t mac wxwin.pro -o %WXDIR%\src\mac\files.lst
+%TM% -t motif wxwin.pro -o %WXDIR%\src\motif\files.lst
+%TM% -t x11 wxwin.pro -o %WXDIR%\src\x11\files.lst
+%TM% -t os2 wxwin.pro -o %WXDIR%\src\os2\files.lst
 :end
