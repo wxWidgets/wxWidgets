311298e064ec6f1f6a52936fccc78efcd6e2ddae
Julian Smart <julian@anthemion.co.uk>
2002-07-04 09:08:59 +0000

    Applied patch from Mark Armstrong <marka@availsolutions.com> This patch fixes a debug assertion in tooltips.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16033 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/tooltip.cpp
+++ src/msw/tooltip.cpp
@@ -382,11 +382,9 @@ void wxToolTip::SetWindow(wxWindow *win)
     //     something like this where the derived class can do such things
     //     itself instead of wxToolTip "knowing" about them all
     wxComboBox *combo = wxDynamicCast(control, wxComboBox);
-    if ( combo )
+    if ( combo && !(combo->GetWindowStyle() & wxCB_READONLY))
     {
-        WXHWND hwndComboEdit = combo->GetWindowStyle() & wxCB_READONLY
-                                ? combo->GetHWND()
-                                : combo->GetEditHWND();
+        WXHWND hwndComboEdit = combo->GetEditHWND();
         if ( hwndComboEdit )
         {
             Add(hwndComboEdit);
