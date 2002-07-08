5fea3204c9e6d3c28ffabecca54d971b8205abe2
Robin Dunn <robin@alldunn.com>
2002-07-08 20:06:13 +0000

    Added generic static text control
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16089 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/wxStaticText.py
+++ wxPython/demo/wxStaticText.py
@@ -1,27 +1,36 @@
 
 from wxPython.wx import *
 
+USE_GENERIC = 1
+
+if USE_GENERIC:
+    from wxPython.lib.stattext import wxGenStaticText as wxStaticText
+
 #---------------------------------------------------------------------------
 
 class TestPanel(wxPanel):
     def __init__(self, parent):
         wxPanel.__init__(self, parent, -1)
 
-        wxStaticText(self, -1, "This is an example of static text",
-                     wxPoint(20, 10))
+        wxStaticText(self, -1, "This is an example of static text", (20, 10))
 
-        wxStaticText(self, -1, "using the wxStaticText Control.",
-                     wxPoint(20, 30))
+        wxStaticText(self, -1, "using the wxStaticText Control.", (20, 30))
 
-        wxStaticText(self, -1, "Is this yellow?",
-                     wxPoint(20, 70)).SetBackgroundColour('Yellow')
+        wxStaticText(self, -1, "Is this yellow?", (20, 70), (90, -1)).SetBackgroundColour('Yellow')
+
+        wxStaticText(self, -1, "align center", (120, 70), (90, -1), wxALIGN_CENTER).SetBackgroundColour('Yellow')
+
+        wxStaticText(self, -1, "align right", (220, 70), (90, -1), wxALIGN_RIGHT).SetBackgroundColour('Yellow')
 
         str = "This is a different font."
-        text = wxStaticText(self, -1, str, wxPoint(20, 100))
-        font = wxFont(20, wxSWISS, wxNORMAL, wxNORMAL, false, "Arial")
+        text = wxStaticText(self, -1, str, (20, 100))
+        font = wxFont(18, wxSWISS, wxNORMAL, wxNORMAL, false, "Arial")
         w, h, d, e = self.GetFullTextExtent(str, font)
         text.SetFont(font)
         text.SetSize(wxSize(w, h))
+
+        wxStaticText(self, -1, "Multi-line wxStaticText\nline 2\nline 3\n\nafter empty line", (20,150))
+        wxStaticText(self, -1, "Align right multi-line\nline 2\nline 3\n\nafter empty line", (220,150), style=wxALIGN_RIGHT)
 
 
 #---------------------------------------------------------------------------
@@ -35,14 +44,15 @@ def runTest(frame, nb, log):
 
 
 
-
-
-
-
-
 overview = '''\
 A static text control displays one or more lines of read-only text.
 
 '''
 
-#---------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import sys,os
+    import run
+    run.main(['', os.path.basename(sys.argv[0])])
+
