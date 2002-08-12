3fce70fbf5dc650636eb51a817c24e4fbb7ad5c8
Robin Dunn <robin@alldunn.com>
2002-08-12 19:16:53 +0000

    wxPySimpleApp calls wxInitAllImageHandlers
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16469 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/src/msw/wx.py
+++ wxPython/src/msw/wx.py
@@ -1736,6 +1736,7 @@ class wxPySimpleApp(wxApp):
     def __init__(self, flag=0):
         wxApp.__init__(self, flag)
     def OnInit(self):
+        wxInitAllImageHandlers()
         return true
 
 
