0c15fcc8144afb956192a4c57d71aae4fb6ff5b0
Robert Roebling <robert@roebling.de>
2002-08-13 19:55:14 +0000

    This way strconv copmiles again. I don't think     this is the intended use of ICONV_CONST, though.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16487 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/strconv.cpp
+++ src/common/strconv.cpp
@@ -506,7 +506,11 @@ public:
 #define ICONV_FAILED(cres, bufLeft)  (cres == (size_t)-1)
 #endif
 
-#define ICONV_CHAR_CAST(x)  ((ICONV_CONST char **)(x))
+#if ICONV_CONST
+#define ICONV_CHAR_CAST(x)  ((char **)(x))
+#else
+#define ICONV_CHAR_CAST(x)  ((const char **)(x))
+#endif
 
 // ----------------------------------------------------------------------------
 // IC_CharSet: encapsulates an iconv character set
