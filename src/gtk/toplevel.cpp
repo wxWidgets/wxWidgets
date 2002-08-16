508196798b9e693aab6aa9e223acc1a725567c8a
Julian Smart <julian@anthemion.co.uk>
2002-08-16 10:33:41 +0000

    Applied patch [ 594717 ] Implements wxFRAME_FLOAT_ON_PARENT By John Skiff
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16540 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/toplevel.cpp
+++ src/gtk/toplevel.cpp
@@ -362,8 +362,9 @@ bool wxTopLevelWindowGTK::Create( wxWindow *parent,
 
     m_widget = gtk_window_new( win_type );
 
-    if (m_parent && (GTK_IS_WINDOW(m_parent->m_widget)) &&
-        (GetExtraStyle() & wxTOPLEVEL_EX_DIALOG))
+    if (m_parent && (((GTK_IS_WINDOW(m_parent->m_widget)) &&
+		      (GetExtraStyle() & wxTOPLEVEL_EX_DIALOG)) ||
+		     (style & wxFRAME_FLOAT_ON_PARENT)))
     {
         gtk_window_set_transient_for( GTK_WINDOW(m_widget), GTK_WINDOW(m_parent->m_widget) );
     }
