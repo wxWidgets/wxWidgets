09740c31b9d87156893ea13def17bd2a31d8feeb
Robin Dunn <robin@alldunn.com>
2002-08-28 20:02:20 +0000

    Mode distrib scripts and such
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16846 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/makedev.bat
+++ wxPython/distrib/makedev.bat
@@ -1,47 +1,70 @@
 @echo off
 rem   Builds a zip containing stuff needed to link with the wxWindows DLL
-rem   shipped with wxPython.  This allows other developers to create apps
+rem   shipped with wxPython.  This should allow other developers to create apps
 rem   or extensions that can share the same DLL.
 
 setlocal
+set BASE=_distrib_zip\wxPython-%1
 
-set BASE=wxPython-devel
-
+rem **** Make a directory to build up a distribution tree
+mkdir _distrib_zip
 mkdir %BASE%
 
-mkdir %BASE%\ReleaseDLL
-mkdir %BASE%\HybridDLL
-copy /s %WXWIN%\ReleaseDLL %BASE%\ReleaseDLL
-copy /s %WXWIN%\HybridDLL %BASE%\HybridDLL
 
-mkdir %BASE%\lib
-copy %WXWIN%\lib\*.lib %BASE\lib
-del %BASE%\lib\*d.lib
-copy %WXWIN%\lib\*.dll %BASE\lib
-del %BASE%\lib\*d.dll
-copy %WXWIN%\lib\*.pdb %BASE\lib
-del %BASE%\lib\*d.pdb
-copy /s %WXWIN%\lib\mswdll %BASE\lib\mswdll
-copy /s %WXWIN%\lib\mswdllh %BASE\lib\mswdllh
-
-
-mkdir %BASE%\src
-copy %WXWIN%\src\makevc.env %BASE%\src
-copy %WXWIN%\src\*.vc %BASE%\src
-
-mkdir %BASE%\src\msw
-mkdir %BASE%\src\msw\ReleaseDLL
-copy %WXWIN%\src\msw\ReleaseDLL\dummy*.obj %BASE%\src\msw\ReleaseDLL
-mkdir %BASE%\src\msw\HybridDLL
-copy %WXWIN%\src\msw\HybridDLL\dummy*.obj %BASE%\src\msw\HybridDLL
+rem *** copy files
+copy distrib\README.devel.txt %BASE%\README.txt
 
 mkdir %BASE%\include
 mkdir %BASE%\include\wx
-copy /s %WXWIN%\include\wx\* %BASE%\include\wx
+copy /s %WXWIN%\include\wx\* 	%BASE%\include\wx
+
+mkdir %BASE%\lib
+mkdir %BASE%\lib\mswdllh
+mkdir %BASE%\lib\mswdlluh
+copy /s %WXWIN%\lib\mswdllh\* 	%BASE%\lib\mswdllh
+copy /s %WXWIN%\lib\mswdlluh\* 	%BASE%\lib\mswdlluh
+copy %WXWIN%\lib\wxmsw*h.lib 	%BASE%\lib
+copy %WXWIN%\lib\wxmsw*h.dll 	%BASE%\lib
+copy %WXWIN%\lib\mslu.txt 	%BASE%\lib
+copy %WXWIN%\lib\unicows.dll 	%BASE%\lib
+copy %WXWIN%\lib\unicows.lib 	%BASE%\lib
+
+mkdir %BASE%\src
+copy %WXWIN%\src\makevc.env 	%BASE%\src
+copy %WXWIN%\src\makelib.vc 	%BASE%\src
 
 
-zip -r dist\wxPython-devel-win32-%1.zip %BASE%
-del /sxzy %BASE%
+rem *** remove unneeded files
+del /sxzy %BASE%\include\wx\CVS
+del /sxzy %BASE%\include\wx\generic\CVS
+del /sxzy %BASE%\include\wx\gtk\CVS
+del /sxzy %BASE%\include\wx\html\CVS
+del /sxzy %BASE%\include\wx\mac\CVS
+del /sxzy %BASE%\include\wx\mgl\CVS
+del /sxzy %BASE%\include\wx\motif\CVS
+del /sxzy %BASE%\include\wx\msw\CVS
+del /sxzy %BASE%\include\wx\msw\ctl3d\CVS
+del /sxzy %BASE%\include\wx\msw\gnuwin32\CVS
+del /sxzy %BASE%\include\wx\msw\gnuwin32\gl\CVS
+del /sxzy %BASE%\include\wx\msw\ole\CVS
+del /sxzy %BASE%\include\wx\os2\CVS
+del /sxzy %BASE%\include\wx\protocol\CVS
+del /sxzy %BASE%\include\wx\univ\CVS
+del /sxzy %BASE%\include\wx\unix\CVS
+del /sxzy %BASE%\include\wx\x11\CVS
+del /sxzy %BASE%\include\wx\x11\nanox\CVS
+del /sxzy %BASE%\include\wx\x11\nanox\X11\CVS
+
+
+
+rem *** bundle it all up
+cd _distrib_zip
+tar cvf ..\dist\wxPythonWIN32-devel-%1.tar wxPython-%1
+gzip -9 ..\dist\wxPythonWIN32-devel-%1.tar
+
+rem *** cleanup
+cd ..
+del /sxzy _distrib_zip
 
 endlocal
 
