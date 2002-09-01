44379ce6fa20294cf83e5f65cc2006b1ea8be6c1
Julian Smart <julian@anthemion.co.uk>
2002-09-01 15:08:04 +0000

    Accidentally omitted code from previous patch
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@16908 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/ipcbase.cpp
+++ src/common/ipcbase.cpp
@@ -30,4 +30,54 @@ IMPLEMENT_CLASS(wxServerBase, wxObject)
 IMPLEMENT_CLASS(wxClientBase, wxObject)
 IMPLEMENT_CLASS(wxConnectionBase, wxObject)
 
+wxConnectionBase::wxConnectionBase(wxChar *buffer, int size)
+  : m_buffer(buffer), m_buffersize(size),
+    m_deletebufferwhendone(false), m_connected(true)
+{
+  if ( buffer == (wxChar *)NULL )
+  { // behave like next constructor
+    m_buffersize = 0;
+    m_deletebufferwhendone = true;
+  }
+}
+
+wxConnectionBase::wxConnectionBase()
+  : m_buffersize(0), m_buffer(NULL), m_deletebufferwhendone(true),
+    m_connected(true)
+{
+}
+
+wxConnectionBase::wxConnectionBase(wxConnectionBase& copy)
+  : m_buffer(copy.m_buffer), m_buffersize(copy.m_buffersize),
+    m_deletebufferwhendone(false), m_connected(copy.m_connected)
+{
+  // copy constructor would require ref-counted pointer to buffer
+  wxFAIL_MSG( _T("Copy constructor of wxConnectionBase not implemented") );
+}
+
+
+wxConnectionBase::~wxConnectionBase(void)
+{
+  if ( m_deletebufferwhendone && m_buffer )
+   delete m_buffer;
+}
+
+wxChar *wxConnectionBase::GetBufferAtLeast( size_t bytes )
+{
+  if ( m_buffersize >= bytes )
+    return m_buffer;
+  else
+  {  // need to resize buffer
+    if ( m_deletebufferwhendone )
+    { // we're in charge of buffer, increase it
+      if ( !m_buffer )
+        delete m_buffer;
+      m_buffer = new wxChar[bytes];
+      m_buffersize = bytes;
+      return m_buffer;
+    } // user-supplied buffer, fail
+    else
+      return NULL;
+  }
+}
 
