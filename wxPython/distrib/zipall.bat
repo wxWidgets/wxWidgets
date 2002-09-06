131cf9b036f0fe17569d66afabdfc7611f14cf25
Bryan Petty <bryan@ibaku.net>
2002-09-06 16:59:31 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.28.2'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/unlabeled-1.28.2@17020 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/zipall.bat
+++
@@ -1,4 +0,0 @@
-
-find . | grep -v "/CVS" | grep -v "./build/" | grep -v "./distrib/" | grep -v ".pyd" | grep -v ".pdb" | grep -v "contrib/ogl/contrib" | grep -v "contrib/stc/contrib" | zip -@ wxPython.zip
-
-
