d239112aab29a227f11e03b414f5d9196612ee3b
Bryan Petty <bryan@ibaku.net>
2002-09-11 01:00:07 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.19.2'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/unlabeled-1.19.2@17120 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/contrib/stc/_stcextras.py
+++
@@ -1,7 +0,0 @@
-# Stuff these names into the wx namespace so wxPyConstructObject can find them
-
-wx.wxStyledTextEventPtr       = wxStyledTextEventPtr
-wx.wxStyledTextCtrlPtr        = wxStyledTextCtrlPtr
-
-
-wxSTC_CARET_CENTER = wxSTC_CARET_STRICT
