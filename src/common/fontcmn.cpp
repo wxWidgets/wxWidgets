0a9f0ef7d0f4a71c697737ddd1ab0b5b71004df1
Julian Smart <julian@anthemion.co.uk>
2002-12-10 16:10:02 +0000

    FromString should not fail on wxMac if facename empty.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@18177 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/fontcmn.cpp
+++ src/common/fontcmn.cpp
@@ -323,8 +323,11 @@ bool wxNativeFontInfo::FromString(const wxString& s)
     underlined = l != 0;
 
     faceName = tokenizer.GetNextToken();
+
+#ifndef __WXMAC__
     if( !faceName )
         return FALSE;
+#endif
 
     token = tokenizer.GetNextToken();
     if ( !token.ToLong(&l) )
