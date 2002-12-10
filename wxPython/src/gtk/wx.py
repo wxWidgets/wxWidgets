6c85e4ea6c13577f03b66cf2d00b5c64af9291a5
Robin Dunn <robin@alldunn.com>
2002-12-10 22:45:08 +0000

    SWIGged updates for wxGTK
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@18191 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/src/gtk/wx.py
+++ wxPython/src/gtk/wx.py
@@ -1528,7 +1528,7 @@ wxColor      = wxColour
 wxNamedColor = wxNamedColour
 wxPen        = wxPyPen
 wxScrollbar  = wxScrollBar
-
+wxPoint2D    = wxPoint2DDouble
 
 # backwards compatibility
 wxNoRefBitmap       = wxBitmap
@@ -1541,6 +1541,7 @@ wxSystemSettings_GetSystemMetric = wxSystemSettings_GetMetric
 
 
 wxPyAssertionError = wxc.wxPyAssertionError
+
 
 #----------------------------------------------------------------------
 # wxGTK sets the locale when initialized.  Doing this at the Python
@@ -1602,6 +1603,18 @@ def wxPyTypeCast(obj, typeStr):
         theObj.thisown = obj.thisown
     return theObj
 
+#----------------------------------------------------------------------------
+# An isinstance for Pythons < 2.2 that can check a sequence of class objects
+# like the one in 2.2 can.
+
+def wxPy_isinstance(obj, klasses):
+    import types
+    if sys.version[:3] < "2.2" and type(klasses) in [types.TupleType, types.ListType]:
+        for klass in klasses:
+            if isinstance(obj, klass): return true
+        return false
+    else:
+        return isinstance(obj, klasses)
 
 #----------------------------------------------------------------------------
 _wxCallAfterId = None
