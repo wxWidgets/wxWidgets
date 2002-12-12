a8e8c0adf2722fab4769d7fcc46fdc5a547a45d2
Robin Dunn <robin@alldunn.com>
2002-12-12 04:59:17 +0000

    Win32 installer first prompts to uninstall old version
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@18197 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/make_installer.py
+++ wxPython/distrib/make_installer.py
@@ -7,7 +7,7 @@ will be created.
 """
 
 
-import sys, os, string
+import sys, os, string, time
 
 KEEP_TEMPS = 0
 ISCC = r"%s\InnoSetup2Ex\ISCC.exe %s"
@@ -32,7 +32,7 @@ OutputDir = dist
 WizardStyle = modern
 UninstallStyle = modern
 DisableStartupPrompt = true
-CompressLevel = 9
+Compression = bzip
 DirExistsWarning = no
 DisableReadyMemo = true
 DisableReadyPage = true
@@ -46,6 +46,8 @@ UninstallFilesDir = {app}\wxPython
 AppPublisherURL = http://wxPython.org/
 LicenseFile = licence\licence.txt
 CodeFile = %(IFSFILE)s
+
+;; WizardDebug = yes
 
 ;;------------------------------------------------------------
 
@@ -142,6 +144,7 @@ Source: "wxPython\tools\*.py";              DestDir: "{app}\wxPython\tools"; Com
 Source: "wxPython\tools\XRCed\CHANGES";     DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
 Source: "wxPython\tools\XRCed\TODO";        DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
 Source: "wxPython\tools\XRCed\README";      DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
+Source: "wxPython\tools\XRCed\sawfishrc";   DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
 Source: "wxPython\tools\XRCed\*.py";        DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
 Source: "wxPython\tools\XRCed\*.xrc";       DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
 Source: "wxPython\tools\XRCed\*.ico";       DestDir: "{app}\wxPython\tools\XRCed"; Components: tools
@@ -269,6 +272,7 @@ var
     PythonDir : String;
     InstallDir : String;
 
+
 function InitializeSetup(): Boolean;
 begin
     if not RegQueryStringValue(HKEY_LOCAL_MACHINE,
@@ -279,7 +283,8 @@ begin
                                    'Software\Python\PythonCore\%(PYTHONVER)s\InstallPath',
                                    '', PythonDir) then begin
 
-            MsgBox('No installation of Python %(PYTHONVER)s found in registry.\nBe sure to enter a pathname that places wxPython\non the PYTHONPATH',
+            MsgBox('No installation of Python %(PYTHONVER)s found in registry.' + #13 +
+                   'Be sure to enter a pathname that places wxPython on the PYTHONPATH',
                    mbConfirmation, MB_OK);
             PythonDir := 'C:\Put a directory on PYTHONPATH here\';
         end;
@@ -300,11 +305,33 @@ begin
     Result := InstallDir;
 end;
 
+
+
+function NextButtonClick(CurPage: Integer): Boolean;
+var
+    FileName: string;
+    ResultCode: Integer;
+begin
+    Result := True;
+    if CurPage <> wpSelectDir then Exit;
+    FileName := WizardDirValue() + '\wxPython\unins000.exe';
+    if FileExists(FileName) then begin
+        ResultCode := MsgBox('A prior wxPython installation was found in this directory.  It' + #13 +
+                             'is recommended that it be uninstalled first.' + #13#13 +
+                             'Should I do it?',
+                             mbConfirmation, MB_YESNO);
+        if ResultCode = IDYES then begin
+            InstExec(FileName, '/SILENT', WizardDirValue()+'\wxPython', True, False, SW_SHOWNORMAL, ResultCode);
+
+        end;
+    end;
+end;
+
+
 begin
 end.
 
 """
-
 
 #----------------------------------------------------------------------
 
@@ -370,6 +397,7 @@ def main():
     os.system(ISCC % (os.environ['TOOLS'], ISSFILE))
 
     if not KEEP_TEMPS:
+        time.sleep(1)
         os.remove(ISSFILE)
         os.remove(IFSFILE)
 
