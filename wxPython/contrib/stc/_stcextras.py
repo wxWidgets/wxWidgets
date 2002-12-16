0ff060feed3e54a58801621815f9d1e5702d403f
Bryan Petty <bryan@ibaku.net>
2002-12-16 21:11:04 +0000

    This commit was manufactured by cvs2svn to create tag 'wxPy_2_4_0'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/wxPy_2_4_0@18275 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/contrib/stc/_stcextras.py
+++
@@ -1,7 +0,0 @@
-# Stuff these names into the wx namespace so wxPyConstructObject can find them
-
-wx.wxStyledTextEventPtr       = wxStyledTextEventPtr
-wx.wxStyledTextCtrlPtr        = wxStyledTextCtrlPtr
-
-# This constant no longer exists in Scintilla, but I'll put it here for a while to avoid disrupting user code...
-wxSTC_CARET_CENTER = 0
