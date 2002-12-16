a5c0a4ca9c90439b912f6c024f3a6404ee186f8c
Robin Dunn <robin@alldunn.com>
2002-12-16 21:03:39 +0000

    Added Robb's email address
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@18272 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wxPython/lib/pubsub.py
+++ wxPython/wxPython/lib/pubsub.py
@@ -6,7 +6,7 @@
 #
 # Created:     12-December-2002
 # RCS-ID:      $Id$
-# Copyright:   (c) 2002 by Robb Shecter
+# Copyright:   (c) 2002 by Robb Shecter <robb@acm.org>
 # Licence:     wxWindows license
 #---------------------------------------------------------------------------
 """
