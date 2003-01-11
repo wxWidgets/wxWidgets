a969055e89e4bbf5e3ea280fcc7a536b1d35a7db
Mattia Barbon <mbarbon@cpan.org>
2003-01-11 14:42:38 +0000

    Removed unused, unimplemented and undocumented wxBitmapCheckBox stub. Minur cleanup in wxCheckBox::Create.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@18682 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/motif/checkbox.h
+++ include/wx/motif/checkbox.h
@@ -21,7 +21,6 @@
 WXDLLEXPORT_DATA(extern const char*) wxCheckBoxNameStr;
 
 // Checkbox item (single checkbox)
-class WXDLLEXPORT wxBitmap;
 class WXDLLEXPORT wxCheckBox: public wxCheckBoxBase
 {
     DECLARE_DYNAMIC_CLASS(wxCheckBox)
@@ -47,45 +46,8 @@ public:
     virtual void Command(wxCommandEvent& event);
     
     // Implementation
-    virtual void ChangeFont(bool keepOriginalSize = TRUE);
     virtual void ChangeBackgroundColour();
-    virtual void ChangeForegroundColour();
 };
 
-class WXDLLEXPORT wxBitmapCheckBox: public wxCheckBox
-{
-    DECLARE_DYNAMIC_CLASS(wxBitmapCheckBox)
-        
-public:
-    int checkWidth;
-    int checkHeight;
-    
-    inline wxBitmapCheckBox() { checkWidth = -1; checkHeight = -1; }
-    inline wxBitmapCheckBox(wxWindow *parent, wxWindowID id, const wxBitmap *label,
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize, long style = 0,
-        const wxValidator& validator = wxDefaultValidator,
-        const wxString& name = wxCheckBoxNameStr)
-    {
-        Create(parent, id, label, pos, size, style, validator, name);
-    }
-    
-    bool Create(wxWindow *parent, wxWindowID id, const wxBitmap *bitmap,
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize, long style = 0,
-        const wxValidator& validator = wxDefaultValidator,
-        const wxString& name = wxCheckBoxNameStr);
-    
-    virtual void SetValue(bool value);
-    virtual bool GetValue() const;
-    
-    virtual void SetLabel(const wxBitmap& bitmap);
-    virtual void SetLabel(const wxString& label) { wxControl::SetLabel(label); };
-    
-protected:
-    virtual void DoSetSize(int x, int y,
-        int width, int height,
-        int sizeFlags = wxSIZE_AUTO);
-};
 #endif
 // _WX_CHECKBOX_H_
--- src/motif/checkbox.cpp
+++ src/motif/checkbox.cpp
@@ -39,7 +39,6 @@ void wxCheckBoxCallback (Widget w, XtPointer clientData,
                          XtPointer ptr);
 
 IMPLEMENT_DYNAMIC_CLASS(wxCheckBox, wxControl)
-IMPLEMENT_DYNAMIC_CLASS(wxBitmapCheckBox, wxCheckBox)
 
 // Single check box item
 bool wxCheckBox::Create(wxWindow *parent, wxWindowID id, const wxString& label,
@@ -48,47 +47,32 @@ bool wxCheckBox::Create(wxWindow *parent, wxWindowID id, const wxString& label,
                         const wxValidator& validator,
                         const wxString& name)
 {
-    SetName(name);
-    SetValidator(validator);
-    m_windowStyle = style;
-    m_backgroundColour = parent->GetBackgroundColour();
-    m_foregroundColour = parent->GetForegroundColour();
-    m_font = parent->GetFont();
+    if( !wxControl::CreateControl( parent, id, pos, size, style, validator,
+                                   name ) )
+        return FALSE;
 
-    if (parent) parent->AddChild(this);
-
-    if ( id == -1 )
-        m_windowId = NewControlId();
-    else
-        m_windowId = id;
-
-#if 0  // gcc 2.95 doesn't like this apparently    
-    char* label1 = (label.IsNull() ? "" : (char*) (const char*) label);
-    XmString text = XmStringCreateSimple (label1);
-#endif
     wxString label1(wxStripMenuCodes(label));
-
     wxXmString text( label1 );
     
     Widget parentWidget = (Widget) parent->GetClientWidget();
-    XmFontList fontList = (XmFontList) m_font.GetFontList(1.0, XtDisplay(parentWidget));
+    XmFontList fontList =
+        (XmFontList) m_font.GetFontList(1.0, XtDisplay(parentWidget));
 
     m_mainWidget = (WXWidget) XtVaCreateManagedWidget ("toggle",
         xmToggleButtonWidgetClass, parentWidget,
         XmNfontList, fontList,
         XmNlabelString, text(),
         NULL);
-#if 0    
-    XmStringFree (text);
-#endif
     
-    XtAddCallback ((Widget) m_mainWidget, XmNvalueChangedCallback, (XtCallbackProc) wxCheckBoxCallback,
-        (XtPointer) this);
+    XtAddCallback( (Widget)m_mainWidget,
+                   XmNvalueChangedCallback, (XtCallbackProc)wxCheckBoxCallback,
+                   (XtPointer)this );
 
     XmToggleButtonSetState ((Widget) m_mainWidget, FALSE, TRUE);
 
     SetCanAddEventHandler(TRUE);
-    AttachWidget (parent, m_mainWidget, (WXWidget) NULL, pos.x, pos.y, size.x, size.y);
+    AttachWidget( parent, m_mainWidget, (WXWidget)NULL,
+                  pos.x, pos.y, size.x, size.y );
 
     ChangeBackgroundColour();
     return TRUE;
@@ -112,50 +96,6 @@ void wxCheckBox::Command (wxCommandEvent & event)
     ProcessCommand (event);
 }
 
-// Bitmap checkbox
-bool wxBitmapCheckBox::Create(wxWindow *parent, wxWindowID id, const wxBitmap *WXUNUSED(label),
-                              const wxPoint& WXUNUSED(pos),
-                              const wxSize& WXUNUSED(size), long style,
-                              const wxValidator& validator,
-                              const wxString& name)
-{
-    SetName(name);
-    SetValidator(validator);
-    m_windowStyle = style;
-
-    if (parent) parent->AddChild(this);
-
-    if ( id == -1 )
-        m_windowId = NewControlId();
-    else
-        m_windowId = id;
-
-    // TODO: Create the bitmap checkbox
-
-    return FALSE;
-}
-
-void wxBitmapCheckBox::SetLabel(const wxBitmap& WXUNUSED(bitmap))
-{
-    // TODO
-}
-
-void wxBitmapCheckBox::DoSetSize(int WXUNUSED(x), int WXUNUSED(y), int WXUNUSED(width), int WXUNUSED(height), int WXUNUSED(sizeFlags))
-{
-    // TODO
-}
-
-void wxBitmapCheckBox::SetValue(bool WXUNUSED(val))
-{
-    // TODO
-}
-
-bool wxBitmapCheckBox::GetValue() const
-{
-    // TODOD
-    return FALSE;
-}
-
 void wxCheckBoxCallback (Widget WXUNUSED(w), XtPointer clientData,
                          XtPointer WXUNUSED(ptr))
 {
@@ -168,11 +108,6 @@ void wxCheckBoxCallback (Widget WXUNUSED(w), XtPointer clientData,
     event.SetInt((int) item->GetValue ());
     event.SetEventObject(item);
     item->ProcessCommand (event);
-}
-
-void wxCheckBox::ChangeFont(bool keepOriginalSize)
-{
-    wxWindow::ChangeFont(keepOriginalSize);
 }
 
 void wxCheckBox::ChangeBackgroundColour()
@@ -192,12 +127,6 @@ void wxCheckBox::ChangeBackgroundColour()
     // Better to have the checkbox selection in black, or it's
     // hard to determine what state it is in.
     XtVaSetValues ((Widget) m_mainWidget,
-   //        XmNselectColor, g_itemColors[wxSELE_INDEX].pixel,
            XmNselectColor, selectPixel,
         NULL);
-}
-
-void wxCheckBox::ChangeForegroundColour()
-{
-    wxWindow::ChangeForegroundColour();
 }
