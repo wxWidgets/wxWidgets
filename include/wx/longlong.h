b448f30e9c884794601cecd87bb136d1edf5bb0f
Vadim Zeitlin <vadim@wxwidgets.org>
2003-01-17 13:27:20 +0000

    Borland apparently uses  for int64 in printf(), not
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@18790 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/longlong.h
+++ include/wx/longlong.h
@@ -60,7 +60,7 @@
 #elif defined(__BORLANDC__) && defined(__WIN32__) && (__BORLANDC__ >= 0x520)
     #define wxLongLong_t __int64
     #define wxLongLongSuffix i64
-    #define wxLongLongFmtSpec _T("I64")
+    #define wxLongLongFmtSpec _T("Ld")
 #elif (defined(SIZEOF_LONG_LONG) && SIZEOF_LONG_LONG >= 8)  || \
         defined(__MINGW32__) || \
         defined(__GNUC__) || \
