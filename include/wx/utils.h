4c2300c6a19594abf56832579ce3d8dba09d3834
Stefan Csomor <csomor@advancedconcepts.ch>
2003-01-29 14:55:21 +0000

    getting rid of wxBuffer
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19003 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/utils.h
+++ include/wx/utils.h
@@ -57,8 +57,8 @@ class WXDLLEXPORT wxPoint;
 // ----------------------------------------------------------------------------
 
 // Useful buffer (FIXME VZ: To be removed!!!)
-// Now only needed in Mac and MSW ports
-#if !defined(__WXMOTIF__) && !defined(__WXGTK__) && !defined(__WXX11__) && !defined(__WXMGL__)
+// Now only needed in MSW port
+#if !defined(__WXMOTIF__) && !defined(__WXGTK__) && !defined(__WXX11__) && !defined(__WXMGL__) && !defined(__WXMAC__)
 WXDLLEXPORT_DATA(extern wxChar*) wxBuffer;
 #endif
 
