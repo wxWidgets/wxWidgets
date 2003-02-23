7c6cd4a86b843afa877feaeb207b936dd7a4b1f3
Václav Slavík <vslavik@fastmail.fm>
2003-02-23 18:59:13 +0000

    fixed parsing of CDATA elements (<script> and <style>) (Bill Nalen)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19287 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/html/htmlpars.cpp
+++ src/html/htmlpars.cpp
@@ -127,6 +127,8 @@ void wxHtmlParser::CreateDOMTree()
     m_CurTextPiece = 0;
 }
 
+extern bool wxIsCDATAElement(const wxChar *tag);
+
 void wxHtmlParser::CreateDOMSubTree(wxHtmlTag *cur,
                                     int begin_pos, int end_pos,
                                     wxHtmlTagsCache *cache)
@@ -136,6 +138,15 @@ void wxHtmlParser::CreateDOMSubTree(wxHtmlTag *cur,
     wxChar c;
     int i = begin_pos;
     int textBeginning = begin_pos;
+
+    // If the tag contains CDATA text, we include the text between beginning
+    // and ending tag verbosely. Setting i=end_pos will skip to the very
+    // end of this function where text piece is added, bypassing any child
+    // tags parsing (CDATA element can't have child elements by definition):
+    if (cur != NULL && wxIsCDATAElement(cur->GetName().c_str()))
+    {
+        i = end_pos;
+    }
 
     while (i < end_pos)
     {
@@ -209,6 +220,7 @@ void wxHtmlParser::CreateDOMSubTree(wxHtmlTag *cur,
                 }
                 else
                     i = chd->GetBeginPos();
+                
                 textBeginning = i;
             }
 
