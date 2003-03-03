c0589c62a6b480c9f5505404df20fd8f94414e29
Vadim Zeitlin <vadim@wxwidgets.org>
2003-03-03 19:52:25 +0000

    added some macros to define version strings from version number (instead of having to manually update both)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19454 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/version.h
+++ include/wx/version.h
@@ -12,19 +12,30 @@
 #ifndef _WX_VERSION_H_
 #define _WX_VERSION_H_
 
-// Bump-up with each new version
+// the constants below must be changed with each new version
+// ----------------------------------------------------------------------------
+
+// NB: this file is parsed by Perl code in tmake templates in distrib/msw/tmake
+//     so don't change its format too much or they could break
 #define wxMAJOR_VERSION    2
 #define wxMINOR_VERSION    5
 #define wxRELEASE_NUMBER   0
 #define wxVERSION_STRING   _T("wxWindows 2.5.0")
 
-// These are used by src/msw/version.rc and should always be ASCII, not Unicode
-// and must be updated manually as well each time the version above changes
-#define wxVERSION_NUM_DOT_STRING   "2.5.0"
-#define wxVERSION_NUM_STRING       "250"
+// nothing to update below this line when updating the version
+// ----------------------------------------------------------------------------
 
-// nothing should be updated below this line when updating the version
+// helpers for wxVERSION_NUM_XXX
+#define wxMAKE_VERSION_STRING(x, y, z) #x #y #z
+#define wxMAKE_VERSION_DOT_STRING(x, y, z) #x "." #y "." #z
 
+// these are used by src/msw/version.rc and should always be ASCII, not Unicode
+#define wxVERSION_NUM_STRING \
+  wxMAKE_VERSION_STRING(wxMAJOR_VERSION, wxMINOR_VERSION, wxRELEASE_NUMBER)
+#define wxVERSION_NUM_DOT_STRING \
+  wxMAKE_VERSION_DOT_STRING(wxMAJOR_VERSION, wxMINOR_VERSION, wxRELEASE_NUMBER)
+
+// some more defines, not really sure if they're [still] useful
 #define wxVERSION_NUMBER (wxMAJOR_VERSION * 1000) + (wxMINOR_VERSION * 100) + wxRELEASE_NUMBER
 #define wxBETA_NUMBER      0
 #define wxVERSION_FLOAT wxMAJOR_VERSION + (wxMINOR_VERSION/10.0) + (wxRELEASE_NUMBER/100.0) + (wxBETA_NUMBER/10000.0)
