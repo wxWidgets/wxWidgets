c97cc71ff0762bcecaf30081a54c6778d119ff41
Robin Dunn <robin@alldunn.com>
2003-03-19 22:55:25 +0000

    SWIGged updates for wxMSW
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@19627 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/src/msw/gdi.py
+++ wxPython/src/msw/gdi.py
@@ -326,7 +326,7 @@ class wxColourPtr(wxObjectPtr):
         return "<C wxColour instance at %s>" % (self.this,)
     asTuple = Get
     def __str__(self):                  return str(self.asTuple())
-    def __repr__(self):                 return 'wxColour: ' + str(self.asTuple())
+    def __repr__(self):                 return 'wxColour:' + str(self.asTuple())
     def __nonzero__(self):              return self.Ok()
     def __getinitargs__(self):          return ()
     def __getstate__(self):             return self.asTuple()
