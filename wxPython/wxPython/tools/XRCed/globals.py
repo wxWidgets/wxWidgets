8187d73595cc1d799424ea2089223388d126e26e
Roman Rolinsky <rolinsky@femagsoft.com>
2003-03-20 20:24:17 +0000

    simplified font settings
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@19638 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wxPython/tools/XRCed/globals.py
+++ wxPython/wxPython/tools/XRCed/globals.py
@@ -10,14 +10,9 @@ from wxPython.xrc import *
 # Global constants
 
 sysFont = wxSystemSettings_GetFont(wxSYS_SYSTEM_FONT)
-if wxPlatform == '__WXGTK__':
-    labelFont = wxFont(sysFont.GetPointSize(), wxDEFAULT, wxNORMAL, wxBOLD)
-    modernFont = wxFont(sysFont.GetPointSize(), wxMODERN, wxNORMAL, wxNORMAL)
-    smallerFont = wxFont(sysFont.GetPointSize() - 2, wxDEFAULT, wxNORMAL, wxNORMAL)
-else:
-    labelFont = wxFont(10, wxDEFAULT, wxNORMAL, wxBOLD)
-    modernFont = wxFont(10, wxMODERN, wxNORMAL, wxNORMAL)
-    smallerFont = wxFont(8, wxDEFAULT, wxNORMAL, wxNORMAL)
+labelFont = wxFont(sysFont.GetPointSize(), wxDEFAULT, wxNORMAL, wxBOLD)
+modernFont = wxFont(sysFont.GetPointSize(), wxMODERN, wxNORMAL, wxNORMAL)
+smallerFont = wxFont(sysFont.GetPointSize() - 2, wxDEFAULT, wxNORMAL, wxNORMAL)
 
 progname = 'XRCed'
 version = '0.1.0'
