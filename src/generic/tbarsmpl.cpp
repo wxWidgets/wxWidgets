08af0d25f9d74cfd380975acfd3d61573d92c4c9
David Elliott <dfe@tgwbd.org>
2003-03-24 16:50:23 +0000

    !defined(wxUSE_TOOLBAR_NATIVE) changed to !wxUSE_TOOLBAR_NATIVE because the configure script does define it to 0, not undef it (wxCocoa is using this)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19752 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/tbarsmpl.cpp
+++ src/generic/tbarsmpl.cpp
@@ -86,7 +86,7 @@ public:
 
 IMPLEMENT_DYNAMIC_CLASS(wxToolBarSimple, wxToolBarBase)
 
-#if !defined(wxUSE_TOOLBAR_NATIVE) && !defined(__WXUNIVERSAL__)
+#if !wxUSE_TOOLBAR_NATIVE && !defined(__WXUNIVERSAL__)
     #include "wx/toolbar.h"
 
     IMPLEMENT_DYNAMIC_CLASS(wxToolBar, wxToolBarSimple)
