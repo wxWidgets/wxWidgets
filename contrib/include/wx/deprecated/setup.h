4a3bdee6a979146c426cd058e9bfe6b58135b124
Julian Smart <julian@anthemion.co.uk>
2003-03-30 19:47:55 +0000

    New location for some deprecated files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19890 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ contrib/include/wx/deprecated/setup.h
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/deprecated/setup.h
+// Purpose:     Configuration for deprecated features of the library
+// Author:      Julian Smart
+// Modified by:
+// Created:     2003-03-30
+// RCS-ID:      $Id$
+// Copyright:   (c) Julian Smart
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_DEPRECATED_SETUP_H_
+#define _WX_DEPRECATED_SETUP_H_
+
+// wxUSE_TIMEDATE enables compilation of the old wxDate and wxTime classes (not
+// the same as wxDateTime!). These classes are obsolete and shouldn't be used
+// in new code
+//
+// Default is 0
+//
+// Recommended setting: 0 unless you have legacy code which uses these classes
+#define wxUSE_TIMEDATE 0
+
+// wxProperty[Value/Form/List] classes, used by Dialog Editor
+#define wxUSE_PROPSHEET    1
+
+// wxTreeLayout class
+#define wxUSE_TREELAYOUT     1
+
+// use wxExpr (a.k.a. PrologIO)
+#define wxUSE_PROLOGIO          1
+
+// Use .wxr resource mechanism (requires PrologIO library)
+#define wxUSE_WX_RESOURCES      1
+
+#endif
+    // _WX_DEPRECATED_SETUP_H_
