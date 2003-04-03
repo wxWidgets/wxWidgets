72c30d65723db45f3e5cb9aa4ea50e502d37f5f4
Julian Smart <julian@anthemion.co.uk>
2003-04-03 13:01:59 +0000

    Applied patch [ 706329 ] [wxMac] Fixes wxGetLocalTimeMillis
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@19963 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/timercmn.cpp
+++ src/common/timercmn.cpp
@@ -346,20 +346,24 @@ wxLongLong wxGetLocalTimeMillis()
     return (val + tp.millitm);
 #elif defined(__WXMAC__)
     
-    UInt64 gMilliAtStart = 0 ;
-    Nanoseconds upTime = AbsoluteToNanoseconds( UpTime() ) ;
+    static UInt64 gMilliAtStart = 0;
+
+    Nanoseconds upTime = AbsoluteToNanoseconds( UpTime() );
+
     if ( gMilliAtStart == 0 )
     {
-        time_t start = time(NULL) ;
-        gMilliAtStart = ((UInt64) start) * 1000L ;
+        time_t start = time(NULL);
+        gMilliAtStart = ((UInt64) start) * 1000000L;
         gMilliAtStart -= upTime.lo / 1000 ;
-        gMilliAtStart -= ( ( (UInt64) upTime.hi ) << 32 ) / 1000 ;
+        gMilliAtStart -= ( ( (UInt64) upTime.hi ) << 32 ) / (1000 * 1000);
     }
-    UInt64 millival = gMilliAtStart ;
-    millival += upTime.lo / 1000 ;
-    millival += ( ( (UInt64) upTime.hi ) << 32 ) / 1000 ;
-    val = millival ;
-    return val ;
+
+    UInt64 millival = gMilliAtStart;
+    millival += upTime.lo / (1000 * 1000);
+    millival += ( ( (UInt64) upTime.hi ) << 32 ) / (1000 * 1000);
+    val = millival;
+
+    return val;
 #else // no gettimeofday() nor ftime()
     // We use wxGetLocalTime() to get the seconds since
     // 00:00:00 Jan 1st 1970 and then whatever is available
