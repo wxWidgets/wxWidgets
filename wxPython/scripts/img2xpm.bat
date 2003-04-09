5d5c8aa70e1e8efd38d5052011a22ff820b613ed
Robin Dunn <robin@alldunn.com>
2003-04-09 19:43:05 +0000

    Regenerated the default script batch files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@20111 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/scripts/img2xpm.bat
+++ wxPython/scripts/img2xpm.bat
@@ -1,3 +1,3 @@
 @echo off
 
-e:\tools\Python22\python.exe e:\PROJECTS\wx\wxPython\scripts\img2xpm %1 %2 %3 %4 %5 %6 %7 %8 %9
+E:\TOOLS\PYTHON22\PYTHON.EXE C:\PROJECTS\wx\wxPython\scripts\img2xpm %1 %2 %3 %4 %5 %6 %7 %8 %9
