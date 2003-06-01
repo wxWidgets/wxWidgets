5c9194275aea8149bfe0a6e6e2fe47db5b412151
Julian Smart <julian@anthemion.co.uk>
2003-06-01 19:23:16 +0000

    Fix (I hope) for bug [ 673862 ] printing zero length string with postscript does not work Now descent and externalLeading will be zeroed if the string is empty.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@20835 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/dcpsg.cpp
+++ src/generic/dcpsg.cpp
@@ -1995,6 +1995,8 @@ void wxPostScriptDC::DoGetTextExtent(const wxString& string,
     {
         if (x) (*x) = 0;
         if (y) (*y) = 0;
+        if (descent) (*descent) = 0;
+        if (externalLeading) (*externalLeading) = 0;
         return;
     }
     
