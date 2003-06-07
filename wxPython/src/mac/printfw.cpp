2381ea4581873634b5134c67aeac109bc5423880
Robin Dunn <robin@alldunn.com>
2003-06-07 05:34:58 +0000

    SWIGged updates for wxMac
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@20986 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/src/mac/printfw.cpp
+++ wxPython/src/mac/printfw.cpp
@@ -178,7 +178,14 @@ public:
     PYPRIVATE;
 };
 
-IMPLEMENT_CLASS( wxPyPrintPreview, wxPrintPreview );
+// Stupid renamed classes...  Fix this in 2.5...
+#if defined(__WXMSW__)
+IMPLEMENT_CLASS( wxPyPrintPreview, wxWindowsPrintPreview );
+#elif defined(__WXMAC__)
+IMPLEMENT_CLASS( wxPyPrintPreview, wxMacPrintPreview );
+#else
+IMPLEMENT_CLASS( wxPyPrintPreview, wxPostScriptPrintPreview );
+#endif
 
 IMP_PYCALLBACK_BOOL_INT    (wxPyPrintPreview, wxPrintPreview, SetCurrentPage);
 IMP_PYCALLBACK_BOOL_WXWINDC(wxPyPrintPreview, wxPrintPreview, PaintPage);
