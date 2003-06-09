cbe13a1f09becec432dc9c4a519a767bca348ed4
David Elliott <dfe@tgwbd.org>
2003-06-09 18:33:02 +0000

    Apple #pragma implementation fix (Yes, some of us do compile wxGTK on OS X)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@21025 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/unix/taskbarx11.cpp
+++ src/unix/taskbarx11.cpp
@@ -9,7 +9,7 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////
 
-#ifdef __GNUG__
+#if defined(__GNUG__) && !defined(__APPLE__)
 #pragma implementation "taskbarx11.h"
 #endif
 
