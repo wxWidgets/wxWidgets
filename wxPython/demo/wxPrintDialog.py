0097dbcc26553be2d2f6a1fb78bd9a13a4123a8e
Robin Dunn <robin@alldunn.com>
2003-06-16 23:40:05 +0000

    SetAllPages needs to be used if the Min and Max pages are set.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@21188 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/wxPrintDialog.py
+++ wxPython/demo/wxPrintDialog.py
@@ -5,11 +5,14 @@ from wxPython.wx import *
 
 def runTest(frame, nb, log):
     data = wxPrintDialogData()
+
+    data.EnableSelection(True)
     data.EnablePrintToFile(True)
     data.EnablePageNumbers(True)
-    data.EnableSelection(True)
     data.SetMinPage(1)
     data.SetMaxPage(5)
+    data.SetAllPages(True)
+
     dlg = wxPrintDialog(frame, data)
     if dlg.ShowModal() == wxID_OK:
         data = dlg.GetPrintDialogData()
