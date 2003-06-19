2ec55dc0bee338e32aa0612b36328cc82b9e9194
David Elliott <dfe@tgwbd.org>
2003-06-19 22:09:27 +0000

    Added wxSlider stub
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@21257 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ src/cocoa/slider.mm
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        cocoa/slider.mm
+// Purpose:     wxSlider
+// Author:      David Elliott
+// Modified by:
+// Created:     2003/06/19
+// RCS-ID:      $Id$
+// Copyright:   (c) 2003 David Elliott
+// Licence:   	wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include "wx/app.h"
+#include "wx/statline.h"
+
+#import <AppKit/NSView.h>
+
+IMPLEMENT_DYNAMIC_CLASS(wxSlider, wxControl)
+BEGIN_EVENT_TABLE(wxSlider, wxSliderBase)
+END_EVENT_TABLE()
+// WX_IMPLEMENT_COCOA_OWNER(wxSlider,NSSlider,NSControl,NSView)
+
+bool wxSlider::Create(wxWindow *parent, wxWindowID winid,
+            int value, int minValue, int maxValue,
+            const wxPoint& pos, const wxSize& size, long style,
+            const wxValidator& validator, const wxString& name)
+{
+    if(!CreateControl(parent,winid,pos,size,style,validator,name))
+        return false;
+    SetNSView([[NSView alloc] initWithFrame: NSMakeRect(10,10,20,20)]);
+    [m_cocoaNSView release];
+    if(m_parent)
+        m_parent->CocoaAddChild(this);
+    return true;
+}
+
+wxSlider::~wxSlider()
+{
+    CocoaRemoveFromParent();
+    SetNSView(NULL);
+}
+
