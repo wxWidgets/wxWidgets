7c79b1598fc391c618bd04ce04b27f528e450a78
Vadim Zeitlin <vadim@wxwidgets.org>
2003-06-22 16:13:47 +0000

    fix VC 7.x release build problems
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@21296 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/object.cpp
+++ src/common/object.cpp
@@ -5,7 +5,7 @@
 // Modified by: Ron Lee
 // Created:     04/01/98
 // RCS-ID:      $Id$
-// Copyright:   (c) 1998 Julian Smart and Markus Holzem
+// Copyright:   (c) 1998 Julian Smart
 //              (c) 2001 Ron Lee <ron@debian.org>
 // Licence:     wxWindows license
 /////////////////////////////////////////////////////////////////////////////
@@ -43,10 +43,21 @@
     #endif
 #endif // __WXDEBUG__ || wxUSE_DEBUG_CONTEXT
 
+// we must disable optimizations for VC.NET because otherwise its too eager
+// linker discards wxClassInfo objects in release build thus breaking many,
+// many things
+#if defined _MSC_VER && _MSC_VER >= 1300
+    #pragma optimize("", off)
+#endif
 
 wxClassInfo wxObject::sm_classwxObject( wxT("wxObject"), 0, 0,
                                         (int) sizeof(wxObject),
                                         (wxObjectConstructorFn) 0 );
+
+// restore optimizations
+#if defined _MSC_VER && _MSC_VER >= 1300
+    #pragma optimize("", on)
+#endif
 
 wxClassInfo* wxClassInfo::sm_first = NULL;
 wxHashTable* wxClassInfo::sm_classTable = NULL;
