4f356748c628f4c9e2a1e062ef76789ec221cb25
Vadim Zeitlin <vadim@wxwidgets.org>
2003-06-25 00:08:31 +0000

    removed #pragma interface because it results in link problems when building wxBase/wxGUI
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@21374 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/fontmap.h
+++ include/wx/fontmap.h
@@ -12,10 +12,6 @@
 #ifndef _WX_FONTMAPPER_H_
 #define _WX_FONTMAPPER_H_
 
-#if defined(__GNUG__) && !defined(__APPLE__)
-    #pragma interface "fontmap.h"
-#endif
-
 // ----------------------------------------------------------------------------
 // headers
 // ----------------------------------------------------------------------------
