8c7e3f14fb2ef30a9876e940ef0f44891254886c
Vadim Zeitlin <vadim@wxwidgets.org>
2003-07-10 12:24:15 +0000

    more Unicode fixes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@21860 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/gizmos/multicell.cpp
+++ contrib/src/gizmos/multicell.cpp
@@ -632,20 +632,13 @@ wxMultiCellCanvas :: wxMultiCellCanvas(wxWindow *par, int numRows, int numCols)
 	m_minCellSize = wxSize(5, 5);
 }
 //---------------------------------------------------------------------------
-wxString itoa(int x)
-{
-/*
-	char    bfr[255];
-	sprintf(bfr, "%d", x);
-	return bfr;
-*/
-	return wxString::Format(wxT("%d"), x);
-}
-//---------------------------------------------------------------------------
 void wxMultiCellCanvas :: Add(wxWindow *win, unsigned int row, unsigned int col)
 {
-	wxASSERT_MSG(row >= 0 && row < m_maxRows, wxString("Row ") + itoa(row) + " out of bounds (" + itoa(m_maxRows) + ")");
-	wxASSERT_MSG(col >= 0 && col < m_maxCols, wxString("Column ") + itoa(col) + " out of bounds (" + itoa(m_maxCols) + ")");
+	wxASSERT_MSG(row >= 0 && row < m_maxRows,
+                 wxString::Format(_T("Row %d out of bounds (0..%d)"), row, m_maxRows) );
+	wxASSERT_MSG(col >= 0 && col < m_maxCols,
+                 wxString::Format(_T("Column %d out of bounds (0..%d)"), col, m_maxCols) );
+
 	wxASSERT_MSG(m_cells[CELL_LOC(row, col)] == NULL, wxT("Cell already occupied"));
 
 	wxCell *newCell = new wxCell(win);
@@ -670,3 +663,4 @@ void wxMultiCellCanvas :: CalculateConstraints()
 }
 
 /*** End of File ***/
+
