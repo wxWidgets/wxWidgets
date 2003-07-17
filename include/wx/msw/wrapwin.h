2f552802877a78b58e6c251466b70a10653aa585
Vadim Zeitlin <vadim@wxwidgets.org>
2003-07-17 09:46:49 +0000

    useh higher value of _WIN32_IE
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@22039 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/wrapwin.h
+++ include/wx/msw/wrapwin.h
@@ -1,8 +1,10 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wrapwin.h
 // Purpose:     Wrapper around <windows.h>, to be included instead of it
+// Author:      Vaclav Slavik
 // Created:     2003/07/22
 // RCS-ID:      $Id$
+// Copyright:   (c) 2003 Vaclav Slavik
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
@@ -16,22 +18,23 @@
 #endif
 
 // define _WIN32_IE to a high value because we always check for the version
-// of installed DLLs at runtime anyway:
+// of installed DLLs at runtime anyway unless the user really doesn't want it
 #ifndef _WIN32_IE
     // for compilers that use w32api headers: w32api must be >= 1.1:
     #if defined( __GNUWIN32__ ) || defined( __MINGW32__ ) || \
         defined( __CYGWIN__ ) || \
         (defined(__WATCOMC__) && __WATCOMC__ >= 1200) || \
-        defined(__DIGITALMARS__)
-        #if wxCHECK_W32API_VERSION(1,1)
-            #define _WIN32_IE 0x400
-        #endif
+        defined(__DIGITALMARS__) && \
+        !wxCHECK_W32API_VERSION(1,1)
+        #define _WIN32_IE 0x300
     #else
-        #define _WIN32_IE 0x400
+        // highest known value at the time of this writing (Windows Server 2003)
+        #define _WIN32_IE 0x502
     #endif
-#endif
+#endif // !defined(_WIN32_IE)
 
 #include <windows.h>
 #include "wx/msw/winundef.h"
 
-#endif
+#endif // _WX_WRAPWIN_H_
+
