346dd7d7565882f2da7e0fb109e32b4fa9e5aeee
Václav Slavík <vslavik@fastmail.fm>
2003-07-19 11:52:47 +0000

    fix filenames handling
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@22100 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xrc/xmlres.cpp
+++ src/xrc/xmlres.cpp
@@ -115,6 +115,9 @@ bool wxXmlResource::Load(const wxString& filemask)
                 fn.Normalize(wxPATH_NORM_DOTS | wxPATH_NORM_ABSOLUTE);
                 fnd = fn.GetFullPath();
             }
+#if wxUSE_FILESYSTEM
+            fnd = wxFileSystem::FileNameToURL(fnd);
+#endif
         }
         
 #if wxUSE_FILESYSTEM
@@ -328,9 +331,9 @@ void wxXmlResource::UpdateResources()
 #if !wxUSE_UNICODE && wxUSE_INTL
     if ( (GetFlags() & wxXRC_USE_LOCALE) == 0 )
     {
-        // In case we are not using wxLocale to translate strings, convert the strings
-        // GUI's charset. This must not be done when wxXRC_USE_LOCALE is on, because
-        // it could break wxGetTranslation lookup.
+        // In case we are not using wxLocale to translate strings, convert the
+        // strings GUI's charset. This must not be done when wxXRC_USE_LOCALE
+        // is on, because it could break wxGetTranslation lookup.
         encoding = wxLocale::GetSystemEncodingName();
     }
 #endif
