a9d3371aec4154b69b46115c2b62d57306f26048
Julian Smart <julian@anthemion.co.uk>
2003-07-31 11:31:39 +0000

    Added back the copy ctor and assignment warning suppression
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@22409 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -60,6 +60,8 @@
 #   pragma warning(disable:4244)    // conversion from double to float
 #   pragma warning(disable:4710)    // function not inlined
 #   pragma warning(disable:4097)    // typedef used as class
+#   pragma warning(disable:4511)    // copy ctor couldn't be generated
+#   pragma warning(disable:4512)    // operator=() couldn't be generated
 #ifndef WIN32
 #   pragma warning(disable:4135)    // conversion between different integral types
 #   pragma warning(disable:4769)    // assignment of near pointer to long integer
