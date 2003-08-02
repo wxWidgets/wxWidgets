85f582bda7cff60e81f16a75551a8e0ee79867e1
Václav Slavík <vslavik@fastmail.fm>
2003-08-02 13:50:49 +0000

    wxJoystick and wxWave are now available in all Unix ports, the code was not GTK specific
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@22485 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/joystick.h
+++ include/wx/joystick.h
@@ -6,11 +6,11 @@
 #if defined(__WXMSW__)
 #include "wx/msw/joystick.h"
 #elif defined(__WXMOTIF__)
-#include "wx/motif/joystick.h"
+#include "wx/unix/joystick.h"
 #elif defined(__WXGTK__)
-#include "wx/gtk/joystick.h"
+#include "wx/unix/joystick.h"
 #elif defined(__WXX11__)
-#include "wx/x11/joystick.h"
+#include "wx/unix/joystick.h"
 #elif defined(__WXMAC__)
 #include "wx/mac/joystick.h"
 #elif defined(__WXPM__)
--- include/wx/wave.h
+++ include/wx/wave.h
@@ -4,13 +4,15 @@
 #if defined(__WXMSW__)
 #include "wx/msw/wave.h"
 #elif defined(__WXGTK__)
-#include "wx/gtk/wave.h"
+#include "wx/unix/wave.h"
+#elif defined(__WXX11__)
+#include "wx/unix/wave.h"
+#elif defined(__WXMOTIF__)
+#include "wx/unix/wave.h"
 #elif defined(__WXMAC__)
 #include "wx/mac/wave.h"
 #elif defined(__WXPM__)
 #include "wx/os2/wave.h"
-#elif defined(__WXMAC__)
-#include "wx/mac/wave.h"
 #endif
 
 #endif
