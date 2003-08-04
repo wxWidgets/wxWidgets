1bf49fb99a354b47a920329679eade9ad4981fd4
Vadim Zeitlin <vadim@wxwidgets.org>
2003-08-04 16:44:05 +0000

    don't include MDI headers if !wxUSE_MDI
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@22585 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mdi.h
+++ include/wx/mdi.h
@@ -1,21 +1,25 @@
 #ifndef _WX_MDI_H_BASE_
 #define _WX_MDI_H_BASE_
 
+#if wxUSE_MDI_ARCHITECTURE
+
 #if defined(__WXUNIVERSAL__)
-#include "wx/generic/mdig.h"
+    #include "wx/generic/mdig.h"
 #elif defined(__WXMSW__)
-#include "wx/msw/mdi.h"
+    #include "wx/msw/mdi.h"
 #elif defined(__WXMOTIF__)
-#include "wx/motif/mdi.h"
+    #include "wx/motif/mdi.h"
 #elif defined(__WXGTK__)
-#include "wx/gtk/mdi.h"
+    #include "wx/gtk/mdi.h"
 #elif defined(__WXMAC__)
-#include "wx/mac/mdi.h"
+    #include "wx/mac/mdi.h"
 #elif defined(__WXCOCOA__)
-#include "wx/generic/mdig.h"
+    #include "wx/generic/mdig.h"
 #elif defined(__WXPM__)
-#include "wx/generic/mdig.h"
+    #include "wx/generic/mdig.h"
 #endif
+
+#endif // wxUSE_MDI_ARCHITECTURE
 
 #endif
     // _WX_MDI_H_BASE_
