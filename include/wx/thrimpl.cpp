f0f044dcbe59a8085359073abcb41d250c18dac7
Stefan Neis <Stefan.Neis@t-online.de>
2003-08-24 14:08:44 +0000

    Compilation fixes for OS/2.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@23166 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/thrimpl.cpp
+++ include/wx/thrimpl.cpp
@@ -64,12 +64,12 @@ wxMutexError wxMutex::Unlock()
 // wxConditionInternal
 // --------------------------------------------------------------------------
 
-#if defined(__WXMSW__) || defined(__WXPM__)
+#if defined(__WXMSW__) || defined(__WXPM__) || defined(__EMX__)
 // Win32 and OS/2 don't have explicit support for the POSIX condition
 // variables and their events/event semaphores have quite different semantics,
 // so we reimplement the conditions from scratch using the mutexes and
 // semaphores
-#ifdef __WXPM__
+#if defined(__WXPM__) || defined(__EMX__)
 void InterlockedIncrement(LONG *num)
 {
   ::DosEnterCritSec();
