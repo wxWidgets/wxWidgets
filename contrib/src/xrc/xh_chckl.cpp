c14dc56ac059381d6fd32c00719a7646d183864f
Václav Slavík <vslavik@fastmail.fm>
2003-08-28 09:36:54 +0000

    wxCheckList -> wxCheckListBox
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@23269 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/xrc/xh_chckl.cpp
+++ contrib/src/xrc/xh_chckl.cpp
@@ -24,18 +24,22 @@
 #include "wx/xrc/xh_chckl.h"
 #include "wx/checklst.h"
 #include "wx/intl.h"
+#include "wx/log.h"
 
-wxCheckListXmlHandler::wxCheckListXmlHandler() 
+wxCheckListBoxXmlHandler::wxCheckListBoxXmlHandler() 
 : wxXmlResourceHandler(), m_insideBox(FALSE)
 {
     // no styles
     AddWindowStyles();
 }
 
-wxObject *wxCheckListXmlHandler::DoCreateResource()
+wxObject *wxCheckListBoxXmlHandler::DoCreateResource()
 { 
-    if (m_class == wxT("wxCheckList"))
+    if (m_class == wxT("wxCheckListBox") || m_class == wxT("wxCheckList"))
     {
+        if (m_class == wxT("wxCheckList"))
+            wxLogDebug(wxT("'wxCheckList' name is deprecated, use 'wxCheckListBox' instead."));
+
         // need to build the list of strings from children
         m_insideBox = TRUE;
         CreateChildrenPrivately(NULL, GetParamNode(wxT("content")));
@@ -101,9 +105,10 @@ wxObject *wxCheckListXmlHandler::DoCreateResource()
     }
 }
 
-bool wxCheckListXmlHandler::CanHandle(wxXmlNode *node)
+bool wxCheckListBoxXmlHandler::CanHandle(wxXmlNode *node)
 {
-    return (IsOfClass(node, wxT("wxCheckList")) ||
+    return (IsOfClass(node, wxT("wxCheckListBox")) ||
+            IsOfClass(node, wxT("wxCheckList")) /*backward compatibility*/ ||
            (m_insideBox && node->GetName() == wxT("item")));
 }
 
