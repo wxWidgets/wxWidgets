8c14bc9170c549c44cef259546e6c80541be28e5
Robin Dunn <robin@alldunn.com>
2003-09-01 19:54:41 +0000

    Fixed style setting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@23352 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/wxSingleChoiceDialog.py
+++ wxPython/demo/wxSingleChoiceDialog.py
@@ -6,7 +6,7 @@ from wxPython.wx import *
 def runTest(frame, nb, log):
     dlg = wxSingleChoiceDialog(frame, 'Test Single Choice', 'The Caption',
                                ['zero', 'one', 'two', 'three', 'four', 'five',
-                                'six', 'seven', 'eight'], wxOK|wxCANCEL)
+                                'six', 'seven', 'eight'], wxDEFAULT_DIALOG_STYLE|wxOK|wxCANCEL)
     if dlg.ShowModal() == wxID_OK:
         log.WriteText('You selected: %s\n' % dlg.GetStringSelection())
     dlg.Destroy()
