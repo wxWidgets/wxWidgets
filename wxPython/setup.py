4326f7b7b26b255ed657dffdce4fa8a459c07a36
Robin Dunn <robin@alldunn.com>
2003-09-19 16:44:43 +0000

    Updated version number
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@23713 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/setup.py
+++ wxPython/setup.py
@@ -15,7 +15,7 @@ from distutils.command.install_data import install_data
 
 VER_MAJOR        = 2      # The first three must match wxWindows
 VER_MINOR        = 5
-VER_RELEASE      = 0
+VER_RELEASE      = 1
 VER_SUBREL       = 0      # wxPython release num for x.y.z release of wxWindows
 VER_FLAGS        = "p1"   # release flags, such as prerelease num, unicode, etc.
 
