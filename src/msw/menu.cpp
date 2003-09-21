4a1c207cf1172a3d4292e4e753d277c7fb8d3160
Vadim Zeitlin <vadim@wxwidgets.org>
2003-09-21 15:22:15 +0000

    don't return & in strings returned by GetLabelTop() (closes 687905)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@23775 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/menu.cpp
+++ src/msw/menu.cpp
@@ -888,7 +888,7 @@ wxString wxMenuBar::GetLabelTop(size_t pos) const
     wxCHECK_MSG( pos < GetMenuCount(), wxEmptyString,
                  wxT("invalid menu index in wxMenuBar::GetLabelTop") );
 
-    return m_titles[pos];
+    return wxMenuItem::GetLabelFromText(m_titles[pos]);
 }
 
 // ---------------------------------------------------------------------------
