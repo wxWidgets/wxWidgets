0b3fcc0a593aabd27d9d2cde2c5e0fd65c88cc5b
Dimitri Schoolwerth <dimitri.schoolwerth@gmail.com>
2003-11-14 12:59:03 +0000

    Fixed wxUniv+OpenGL compilation for MSW; Cosmetic cleanup.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@24551 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/glcanvas.h
+++ include/wx/msw/glcanvas.h
@@ -34,22 +34,22 @@
 
 enum
 {
-  WX_GL_RGBA=1,          /* use true color palette */
-  WX_GL_BUFFER_SIZE,     /* bits for buffer if not WX_GL_RGBA */
-  WX_GL_LEVEL,           /* 0 for main buffer, >0 for overlay, <0 for underlay */
-  WX_GL_DOUBLEBUFFER,    /* use doublebuffer */
-  WX_GL_STEREO,          /* use stereoscopic display */
-  WX_GL_AUX_BUFFERS,     /* number of auxiliary buffers */
-  WX_GL_MIN_RED,         /* use red buffer with most bits (> MIN_RED bits) */
-  WX_GL_MIN_GREEN,       /* use green buffer with most bits (> MIN_GREEN bits) */
-  WX_GL_MIN_BLUE,        /* use blue buffer with most bits (> MIN_BLUE bits) */
-  WX_GL_MIN_ALPHA,       /* use blue buffer with most bits (> MIN_ALPHA bits) */
-  WX_GL_DEPTH_SIZE,      /* bits for Z-buffer (0,16,32) */
-  WX_GL_STENCIL_SIZE,    /* bits for stencil buffer */
-  WX_GL_MIN_ACCUM_RED,   /* use red accum buffer with most bits (> MIN_ACCUM_RED bits) */
-  WX_GL_MIN_ACCUM_GREEN, /* use green buffer with most bits (> MIN_ACCUM_GREEN bits) */
-  WX_GL_MIN_ACCUM_BLUE,  /* use blue buffer with most bits (> MIN_ACCUM_BLUE bits) */
-  WX_GL_MIN_ACCUM_ALPHA  /* use blue buffer with most bits (> MIN_ACCUM_ALPHA bits) */
+    WX_GL_RGBA=1,          /* use true color palette */
+    WX_GL_BUFFER_SIZE,     /* bits for buffer if not WX_GL_RGBA */
+    WX_GL_LEVEL,           /* 0 for main buffer, >0 for overlay, <0 for underlay */
+    WX_GL_DOUBLEBUFFER,    /* use doublebuffer */
+    WX_GL_STEREO,          /* use stereoscopic display */
+    WX_GL_AUX_BUFFERS,     /* number of auxiliary buffers */
+    WX_GL_MIN_RED,         /* use red buffer with most bits (> MIN_RED bits) */
+    WX_GL_MIN_GREEN,       /* use green buffer with most bits (> MIN_GREEN bits) */
+    WX_GL_MIN_BLUE,        /* use blue buffer with most bits (> MIN_BLUE bits) */
+    WX_GL_MIN_ALPHA,       /* use blue buffer with most bits (> MIN_ALPHA bits) */
+    WX_GL_DEPTH_SIZE,      /* bits for Z-buffer (0,16,32) */
+    WX_GL_STENCIL_SIZE,    /* bits for stencil buffer */
+    WX_GL_MIN_ACCUM_RED,   /* use red accum buffer with most bits (> MIN_ACCUM_RED bits) */
+    WX_GL_MIN_ACCUM_GREEN, /* use green buffer with most bits (> MIN_ACCUM_GREEN bits) */
+    WX_GL_MIN_ACCUM_BLUE,  /* use blue buffer with most bits (> MIN_ACCUM_BLUE bits) */
+    WX_GL_MIN_ACCUM_ALPHA  /* use blue buffer with most bits (> MIN_ACCUM_ALPHA bits) */
 };
 
 class WXDLLIMPEXP_GL wxGLCanvas;     /* forward reference */
@@ -57,73 +57,99 @@ class WXDLLIMPEXP_GL wxGLCanvas;     /* forward reference */
 class WXDLLIMPEXP_GL wxGLContext: public wxObject
 {
 public:
-   wxGLContext(bool isRGB, wxGLCanvas *win, const wxPalette& palette = wxNullPalette);
-   wxGLContext(
-               bool isRGB, wxGLCanvas *win,
-               const wxPalette& WXUNUSED(palette),
-               const wxGLContext *other        /* for sharing display lists */
-    );
-   ~wxGLContext();
+    wxGLContext(bool isRGB, wxGLCanvas *win, const wxPalette& palette = wxNullPalette);
 
-   void SetCurrent();
-   void SetColour(const wxChar *colour);
-   void SwapBuffers();
+    wxGLContext( bool isRGB, wxGLCanvas *win,
+        const wxPalette& WXUNUSED(palette),
+        const wxGLContext *other /* for sharing display lists */ );
+
+    ~wxGLContext();
 
 
-   inline wxWindow* GetWindow() const { return m_window; }
-   inline WXHDC GetHDC() const { return m_hDC; }
-   inline HGLRC GetGLRC() const { return m_glContext; }
+    void SetCurrent();
+
+    void SetColour(const wxChar *colour);
+
+    void SwapBuffers();
+
+
+    inline wxWindow* GetWindow() const { return m_window; }
+
+    inline WXHDC GetHDC() const { return m_hDC; }
+
+    inline HGLRC GetGLRC() const { return m_glContext; }
 
 public:
-   HGLRC            m_glContext;
-   WXHDC            m_hDC;
-   wxWindow*        m_window;
+    HGLRC            m_glContext;
+    WXHDC            m_hDC;
+    wxWindow*        m_window;
 };
 
 class WXDLLIMPEXP_GL wxGLCanvas: public wxWindow
 {
-   DECLARE_CLASS(wxGLCanvas)
- public:
-   wxGLCanvas(wxWindow *parent, wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition,
+    DECLARE_CLASS(wxGLCanvas)
+public:
+    wxGLCanvas(wxWindow *parent, wxWindowID id = wxID_ANY,
+        const wxPoint& pos = wxDefaultPosition,
         const wxSize& size = wxDefaultSize, long style = 0,
-        const wxString& name = wxGLCanvasName, int *attribList = 0, const wxPalette& palette = wxNullPalette);
-   wxGLCanvas( wxWindow *parent, const wxGLContext *shared = (wxGLContext *)NULL,
-        wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize, long style = 0, const wxString&
-        name = wxGLCanvasName,
-        int *attribList = (int*) NULL, const wxPalette& palette = wxNullPalette );
+        const wxString& name = wxGLCanvasName, int *attribList = 0,
+        const wxPalette& palette = wxNullPalette);
 
-   wxGLCanvas( wxWindow *parent, const wxGLCanvas *shared = (wxGLCanvas *)NULL, wxWindowID id = -1,
-        const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = 0,
-        const wxString& name = wxGLCanvasName, int *attribList = 0, const wxPalette& palette = wxNullPalette );
+    wxGLCanvas(wxWindow *parent,
+        const wxGLContext *shared = (wxGLContext *) NULL,
+        wxWindowID id = wxID_ANY,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        long style = 0,
+        const wxString& name = wxGLCanvasName,
+        int *attribList = (int *) NULL,
+        const wxPalette& palette = wxNullPalette);
 
-   ~wxGLCanvas();
+    wxGLCanvas(wxWindow *parent,
+        const wxGLCanvas *shared = (wxGLCanvas *)NULL,
+        wxWindowID id = wxID_ANY,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        long style = 0,
+        const wxString& name = wxGLCanvasName,
+        int *attribList = 0,
+        const wxPalette& palette = wxNullPalette);
 
-   // Replaces wxWindow::Create functionality, since we need to use a different window class
-   bool Create(wxWindow *parent, wxWindowID id,
-          const wxPoint& pos, const wxSize& size, long style, const wxString& name);
+    ~wxGLCanvas();
 
-   void SetCurrent();
-   #ifdef __WXUNIVERSAL__
-   virtual bool SetCurrent(bool doit = true){ return wxWindow::SetCurrent(doit); }; 
-   #endif
+    // Replaces wxWindow::Create functionality, since
+    // we need to use a different window class
+    bool Create(wxWindow *parent, wxWindowID id,
+        const wxPoint& pos, const wxSize& size,
+        long style, const wxString& name);
 
-   void SetColour(const wxChar *colour);
-   void SwapBuffers();
+    void SetCurrent();
 
-   void OnSize(wxSizeEvent& event);
+#ifdef __WXUNIVERSAL__
+    virtual bool SetCurrent(bool doit) { return wxWindow::SetCurrent(doit); };
+#endif
 
-   void OnQueryNewPalette(wxQueryNewPaletteEvent& event);
-   void OnPaletteChanged(wxPaletteChangedEvent& event);
+    void SetColour(const wxChar *colour);
 
-   inline wxGLContext* GetContext() const { return m_glContext; }
+    void SwapBuffers();
 
-   inline WXHDC GetHDC() const { return m_hDC; }
-   void SetupPixelFormat(int *attribList = (int*) NULL);
-   void SetupPalette(const wxPalette& palette);
-   wxPalette CreateDefaultPalette();
+    void OnSize(wxSizeEvent& event);
 
-   inline wxPalette* GetPalette() const { return (wxPalette*) & m_palette; }
+    void OnQueryNewPalette(wxQueryNewPaletteEvent& event);
+
+    void OnPaletteChanged(wxPaletteChangedEvent& event);
+
+    inline wxGLContext* GetContext() const { return m_glContext; }
+
+    inline WXHDC GetHDC() const { return m_hDC; }
+
+    void SetupPixelFormat(int *attribList = (int *) NULL);
+
+    void SetupPalette(const wxPalette& palette);
+
+    wxPalette CreateDefaultPalette();
+
+    inline wxPalette* GetPalette() const { return (wxPalette *) &m_palette; }
 
 protected:
     wxGLContext*   m_glContext;  // this is typedef-ed ptr, in fact
