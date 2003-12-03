3fe5f176be5ec2a3ea30ea8ecdd2b343fa89f27c
Robin Dunn <robin@alldunn.com>
2003-12-03 04:10:28 +0000

    regenned tools batch files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@24706 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/scripts/img2png.bat
+++ wxPython/scripts/img2png.bat
@@ -1,3 +1,3 @@
 @echo off
 
-E:\TOOLS\PYTHON22\PYTHON.EXE C:\PROJECTS\wx\wxPython\scripts\img2png %1 %2 %3 %4 %5 %6 %7 %8 %9
+E:\tools\python23\python.exe E:\PROJECTS\wx2.5\wxPython\scripts\img2png %1 %2 %3 %4 %5 %6 %7 %8 %9
--- wxPython/scripts/pyalacarte.bat
+++ wxPython/scripts/pyalacarte.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start E:\TOOLS\PYTHON22\pythonw.exe C:\PROJECTS\wx\wxPython\scripts\pyalacarte %1 %2 %3 %4 %5 %6 %7 %8 %9
+start E:\tools\python23\pythonw.exe E:\PROJECTS\wx2.5\wxPython\scripts\pyalacarte %1 %2 %3 %4 %5 %6 %7 %8 %9
