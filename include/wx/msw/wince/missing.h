504de6d140b2b93eb894cc5448848b7ee1bf060f
Julian Smart <julian@anthemion.co.uk>
2003-12-27 17:17:21 +0000

    Some further missing symbols for WinCE 4
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25016 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/wince/missing.h
+++ include/wx/msw/wince/missing.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wince/missing.h
-// Purpose:     Missing things in WinCE 3.0
+// Purpose:     Missing things in WinCE
 // Author:      Marco Cavallini
 // Modified by:
 // Created:     16/11/2002
@@ -11,6 +11,9 @@
 
 #ifndef _WX_CE_MISSING_H_
 #define _WX_CE_MISSING_H_
+
+#include "wx/msw/private.h"
+#include "shellapi.h"
 
 inline BOOL IsIconic( HWND hWnd )
 {
@@ -25,6 +28,63 @@ inline BOOL IsIconic( HWND hWnd )
 #ifdef WIN32_PLATFORM_PSPC
 #define SM_CXCURSOR             13
 #define SM_CYCURSOR             14
+#endif
+
+// Missing from eVC 4 for some reason
+#ifndef I_IMAGENONE
+#define I_IMAGENONE (-2)
+#endif
+
+#ifndef TBSTYLE_NO_DROPDOWN_ARROW
+#define TBSTYLE_NO_DROPDOWN_ARROW 0x0080
+#endif
+
+#if _WIN32_WCE >= 400
+// aygshell.h missing from eVC 4 for some reason
+#define SHCMBM_GETMENU      (WM_USER + 402)
+#define SHCMBM_SETSUBMENU   (WM_USER + 400) // wparam == id of button, lParam == hmenu, return is old hmenu
+#define SHCMBM_GETSUBMENU   (WM_USER + 401) // lParam == ID
+#define SHCMBM_GETMENU      (WM_USER + 402) // get the owning hmenu (as specified in the load resource)
+
+#define SHIDIF_DONEBUTTON           0x0001
+#define SHIDIF_SIZEDLG              0x0002
+#define SHIDIF_SIZEDLGFULLSCREEN    0x0004
+#define SHIDIF_SIPDOWN              0x0008
+#define SHIDIF_FULLSCREENNOMENUBAR  0x0010
+#define SHIDIF_EMPTYMENU            0x0020
+
+#define SHFS_SHOWTASKBAR            0x0001
+#define SHFS_HIDETASKBAR            0x0002
+#define SHFS_SHOWSIPBUTTON          0x0004
+#define SHFS_HIDESIPBUTTON          0x0008
+#define SHFS_SHOWSTARTICON          0x0010
+#define SHFS_HIDESTARTICON          0x0020
+
+typedef struct tagSHMENUBARINFO
+{
+    DWORD cbSize;               // IN  - Indicates which members of struct are valid
+	HWND hwndParent;            // IN
+    DWORD dwFlags;              // IN  - Some features we want
+    UINT nToolBarId;            // IN  - Which toolbar are we using
+    HINSTANCE hInstRes;         // IN  - Instance that owns the resources
+    int nBmpId;
+    int cBmpImages;             // IN  - Count of bitmap images
+	HWND hwndMB;                // OUT
+    COLORREF clrBk;             // IN  - background color of the menu bar (excluding sip)
+} SHMENUBARINFO, *PSHMENUBARINFO;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BOOL SHFullScreen(HWND hwndRequester, DWORD dwState);
+
+WINSHELLAPI BOOL  SHCreateMenuBar(SHMENUBARINFO *pmbi);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
 
 /*
