c0546c3ea73ecfab583e54290bcd8a9d55c8ae96
Robin Dunn <robin@alldunn.com>
2003-12-29 23:50:56 +0000

    Compatibility modules for a couple that have gone AWOL in the new namespace
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25036 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ wxPython/wxPython/htmlhelp.py
@@ -0,0 +1,30 @@
+# The contents of the old wxPython.htmlhelp module have been moved
+# into the wx.html module in the new scheme.  This module will help
+# with backwards compatibility by making those symbols visible in
+# wxPython.htmlhelp again.
+
+
+import wxPython.wx
+import wxPython.html
+
+wxHtmlBookRecordPtr = wxPython.html.wxHtmlBookRecordPtr
+wxHtmlBookRecord = wxPython.html.wxHtmlBookRecord
+wxHtmlContentsItemPtr = wxPython.html.wxHtmlContentsItemPtr
+wxHtmlContentsItem = wxPython.html.wxHtmlContentsItem
+wxHtmlSearchStatusPtr = wxPython.html.wxHtmlSearchStatusPtr
+wxHtmlSearchStatus = wxPython.html.wxHtmlSearchStatus
+wxHtmlHelpDataPtr = wxPython.html.wxHtmlHelpDataPtr
+wxHtmlHelpData = wxPython.html.wxHtmlHelpData
+wxHtmlHelpFramePtr = wxPython.html.wxHtmlHelpFramePtr
+wxHtmlHelpFrame = wxPython.html.wxHtmlHelpFrame
+wxHtmlHelpControllerPtr = wxPython.html.wxHtmlHelpControllerPtr
+wxHtmlHelpController = wxPython.html.wxHtmlHelpController
+wxHF_TOOLBAR = wxPython.html.wxHF_TOOLBAR
+wxHF_FLATTOOLBAR = wxPython.html.wxHF_FLATTOOLBAR
+wxHF_CONTENTS = wxPython.html.wxHF_CONTENTS
+wxHF_INDEX = wxPython.html.wxHF_INDEX
+wxHF_SEARCH = wxPython.html.wxHF_SEARCH
+wxHF_BOOKMARKS = wxPython.html.wxHF_BOOKMARKS
+wxHF_OPENFILES = wxPython.html.wxHF_OPENFILES
+wxHF_PRINT = wxPython.html.wxHF_PRINT
+wxHF_DEFAULTSTYLE = wxPython.html.wxHF_DEFAULTSTYLE
