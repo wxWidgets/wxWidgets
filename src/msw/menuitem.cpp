51f8b24631798a393e6a1e57eaf06998329fb6db
Julian Smart <julian@anthemion.co.uk>
2004-01-17 17:05:35 +0000

    Applied [ 876737 ] menu item with bitmap uses wrong font
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25211 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/menuitem.cpp
+++ src/msw/menuitem.cpp
@@ -135,7 +135,7 @@ wxMenuItem::wxMenuItem(wxMenu *pParentMenu,
                        wxMenu *pSubMenu)
           : wxMenuItemBase(pParentMenu, id, text, strHelp, kind, pSubMenu)
 #if wxUSE_OWNER_DRAWN
-            , wxOwnerDrawn(text, kind == wxITEM_CHECK)
+            , wxOwnerDrawn(text, kind == wxITEM_CHECK, true)
 #endif // owner drawn
 {
     Init();
