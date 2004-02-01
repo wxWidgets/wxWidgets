9ea7b86fc855764e6e849dc327804aba98237e57
Václav Slavík <vslavik@fastmail.fm>
2004-02-01 10:48:30 +0000

    prefix symbols with wx, for smaller risk of collision
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25445 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/html/forcelnk.h
+++ include/wx/html/forcelnk.h
@@ -44,8 +44,8 @@ See mod_*.cpp and htmlwin.cpp for example :-)
 
 // This must be part of the module you want to force:
 #define FORCE_LINK_ME(module_name)                                    \
-                int _link_dummy_func_##module_name ();                \
-                int _link_dummy_func_##module_name ()                 \
+                int _wx_link_dummy_func_##module_name ();             \
+                int _wx_link_dummy_func_##module_name ()              \
                 {                                                     \
                     return 1;                                         \
                 }
@@ -53,9 +53,9 @@ See mod_*.cpp and htmlwin.cpp for example :-)
 
 // And this must be somewhere where it certainly will be linked:
 #define FORCE_LINK(module_name)                                       \
-                extern int _link_dummy_func_##module_name ();         \
-                static int _link_dummy_var_##module_name =            \
-                               _link_dummy_func_##module_name ();
+                extern int _wx_link_dummy_func_##module_name ();      \
+                static int _wx_link_dummy_var_##module_name =         \
+                               _wx_link_dummy_func_##module_name ();
 
 #define FORCE_WXHTML_MODULES() \
     FORCE_LINK(m_layout) \
