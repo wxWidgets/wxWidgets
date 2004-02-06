9e00cf4691dc305b0c468b4b7b2ae9d9a024daaa
Bryan Petty <bryan@ibaku.net>
2004-02-06 00:03:13 +0000

    This commit was manufactured by cvs2svn to create tag 'scintilla-b4-extrafont'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/tags/scintilla-b4-extrafont@25524 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/zipwince.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-Rem Zip up WinCE project files
-
-erase %WXWIN%\wxWinCE.zip
-zip -@ %WXWIN%\wxWinCE.zip < %WXWIN%\distrib\msw\wince.rsp
-
-echo Done.
--- wxPython/scripts/img2png.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-E:\tools\python23\python.exe E:\PROJECTS\wx2.5\wxPython\scripts\img2png %1 %2 %3 %4 %5 %6 %7 %8 %9
