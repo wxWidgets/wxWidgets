f94380234878ab30799ccce3d1cf8cced364df37
Vadim Zeitlin <vadim@wxwidgets.org>
2004-02-12 01:02:41 +0000

    define DWORD_PTR &c for Win32 compilation whatever headers we use
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25756 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/wrapwin.h
+++ include/wx/msw/wrapwin.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        wrapwin.h
+// Name:        msw/wrapwin.h
 // Purpose:     Wrapper around <windows.h>, to be included instead of it
 // Author:      Vaclav Slavik
 // Created:     2003/07/22
@@ -19,6 +19,20 @@
 
 #include <windows.h>
 #include "wx/msw/winundef.h"
+
+// types DWORD_PTR, ULONG_PTR and so on might be not defined in old headers but
+// unfortunately I don't know of any standard way to test for this (as they're
+// typedefs and not #defines), so simply overwrite them in any case in Win32
+// mode -- and if compiling for Win64 they'd better have new headers anyhow
+//
+// this is ugly but what else can we do? even testing for compiler version
+// wouldn't help as you can perfectly well be using an older compiler (VC6)
+// with newer SDK headers
+#ifndef __WIN64__
+    #define UINT_PTR unsigned int
+    #define ULONG_PTR unsigned long
+    #define DWORD_PTR unsigned long
+#endif // !__WIN64__
 
 #endif // _WX_WRAPWIN_H_
 
