86132a69bcd8008660a962e7399492990ceb9278
Vadim Zeitlin <vadim@wxwidgets.org>
2004-02-15 02:58:56 +0000

    added wrapper header for CppUnit headers
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@25812 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/cppunit.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/cppunit.h
+// Purpose:     wrapper header for CppUnit headers
+// Author:      Vadim Zeitlin
+// Created:     15.02.04
+// RCS-ID:      $Id$
+// Copyright:   (c) 2004 Vadim Zeitlin
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_CPPUNIT_H_
+#define _WX_CPPUNIT_H_
+
+// using CPPUNIT_TEST() macro results in this warning, disable it as there is
+// no other way to get rid of it and it's not very useful anyhow
+#ifdef __VISUALC__
+    // typedef-name 'foo' used as synonym for class-name 'bar'
+    #pragma warning(disable:4097)
+
+    // unreachable code: we don't care about warnings in CppUnit headers
+    #pragma warning(disable:4702)
+
+    // 'id': identifier was truncated to 'num' characters in the debug info
+    #pragma warning(disable:4786)
+#endif // __VISUALC__
+
+#include "wx/beforestd.h"
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "wx/afterstd.h"
+
+#ifdef _MSC_VER
+  #pragma warning(default:4702)
+#endif // _MSC_VER
+
+// for VC++ automatically link in cppunit library
+#ifdef __VISUALC__
+  #ifdef NDEBUG
+    #pragma comment(lib, "cppunit.lib")
+  #else // Debug
+    #pragma comment(lib, "cppunitd.lib")
+  #endif // Release/Debug
+#endif
+
+#endif // _WX_CPPUNIT_H_
+
