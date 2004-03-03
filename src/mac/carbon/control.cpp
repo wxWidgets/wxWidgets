d206654397fc26f1d34032b1eaa53f5226288cb6
Stefan Csomor <csomor@advancedconcepts.ch>
2004-03-03 09:39:02 +0000

    tentative fix for http://sourceforge.net/tracker/index.php?func=detail&aid=908712&group_id=9863&atid=109863
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26053 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/control.cpp
+++ src/mac/carbon/control.cpp
@@ -196,6 +196,8 @@ wxControl::~wxControl()
     }
     if ( (ControlHandle) m_macControl )
     {
+        // in case the callback might be called during destruction
+        ::SetControlColorProc( m_macControl , NULL ) ;
         ::DisposeControl( (ControlHandle) m_macControl ) ;
         m_macControl = NULL ;
     }
