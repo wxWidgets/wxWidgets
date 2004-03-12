951aa70eb410423ef37c0d0572e062f2491f84e1
Robin Dunn <robin@alldunn.com>
2004-03-12 20:17:22 +0000

    distrib tweaks
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26193 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/makedev.bat
+++ wxPython/distrib/makedev.bat
@@ -14,57 +14,66 @@ mkdir %BASE%
 rem *** copy files
 copy distrib\README.devel.txt %BASE%\README.txt
 
+
 mkdir %BASE%\include
 mkdir %BASE%\include\wx
-copy /s %WXWIN%\include\wx\* 	%BASE%\include\wx
+copy /s %WXWIN%\include\wx\*                 %BASE%\include\wx
+
+copy /s %WXWIN%\contrib\include\wx\gizmos    %BASE%\include\wx\gizmos
+copy /s %WXWIN%\contrib\include\wx\ogl       %BASE%\include\wx\ogl
+copy /s %WXWIN%\contrib\include\wx\stc       %BASE%\include\wx\stc
+copy /s %WXWIN%\contrib\include\wx\xrc       %BASE%\include\wx\xrc
+
+copy /s %WXWIN%\wxPython\include\wx\*        %BASE%\include\wx
+mkdir %BASE%\include\wx\wxPython\i_files
+copy  %WXWIN%\wxPython\src\*.i               %BASE%\include\wx\wxPython\i_files
 
 mkdir %BASE%\lib
-mkdir %BASE%\lib\mswdllh
-mkdir %BASE%\lib\mswdlluh
-copy /s %WXWIN%\lib\mswdllh\* 	%BASE%\lib\mswdllh
-copy /s %WXWIN%\lib\mswdlluh\* 	%BASE%\lib\mswdlluh
-copy %WXWIN%\lib\wxmsw*h.lib 	%BASE%\lib
-copy %WXWIN%\lib\wxmsw*h.dll 	%BASE%\lib
-copy %WXWIN%\lib\mslu.txt 	%BASE%\lib
-copy %WXWIN%\lib\unicows.dll 	%BASE%\lib
-copy %WXWIN%\lib\unicows.lib 	%BASE%\lib
+mkdir %BASE%\lib\vc_dll
+mkdir %BASE%\lib\vc_dll\mswh
+mkdir %BASE%\lib\vc_dll\mswuh
 
-mkdir %BASE%\src
-copy %WXWIN%\src\makevc.env 	%BASE%\src
-copy %WXWIN%\src\makelib.vc 	%BASE%\src
+copy /s %WXWIN%\lib\vc_dll\mswh\*            %BASE%\lib\vc_dll\mswh
+copy /s %WXWIN%\lib\vc_dll\mswuh\*           %BASE%\lib\vc_dll\mswuh
+copy %WXWIN%\lib\vc_dll\*                    %BASE%\lib\vc_dll
 
 
 rem *** remove unneeded files
-del /sxzy %BASE%\include\wx\CVS
-del /sxzy %BASE%\include\wx\generic\CVS
-del /sxzy %BASE%\include\wx\gtk\CVS
-del /sxzy %BASE%\include\wx\html\CVS
-del /sxzy %BASE%\include\wx\mac\CVS
-del /sxzy %BASE%\include\wx\mgl\CVS
-del /sxzy %BASE%\include\wx\motif\CVS
-del /sxzy %BASE%\include\wx\msw\CVS
-del /sxzy %BASE%\include\wx\msw\ctl3d\CVS
-del /sxzy %BASE%\include\wx\msw\gnuwin32\CVS
-del /sxzy %BASE%\include\wx\msw\gnuwin32\gl\CVS
-del /sxzy %BASE%\include\wx\msw\ole\CVS
-del /sxzy %BASE%\include\wx\os2\CVS
-del /sxzy %BASE%\include\wx\protocol\CVS
-del /sxzy %BASE%\include\wx\univ\CVS
-del /sxzy %BASE%\include\wx\unix\CVS
-del /sxzy %BASE%\include\wx\x11\CVS
-del /sxzy %BASE%\include\wx\x11\nanox\CVS
-del /sxzy %BASE%\include\wx\x11\nanox\X11\CVS
+cd _distrib_zip
+
+ffind /SB wx*d_*.*		>  del-files
+ffind /SB wx*d.*		>> del-files
+ffind /SB .#*			>> del-files
+ffind /SB .cvsignore		>> del-files
+
+for %f in (@del-files) do 	rm -fv %f
+rem del /y @del-files
 
 
+ffind /SB /A:D	CVS			>  del-dirs
+echo  wxPython-%1\include\wx\cocoa 	>> del-dirs
+echo  wxPython-%1\include\wx\gtk 	>> del-dirs
+echo  wxPython-%1\include\wx\mac 	>> del-dirs
+echo  wxPython-%1\include\wx\mgl 	>> del-dirs
+echo  wxPython-%1\include\wx\motif 	>> del-dirs
+echo  wxPython-%1\include\wx\os2 	>> del-dirs
+echo  wxPython-%1\include\wx\univ 	>> del-dirs
+echo  wxPython-%1\include\wx\unix 	>> del-dirs
+echo  wxPython-%1\include\wx\x11 	>> del-dirs
+
+for %d in (@del-dirs) do 	rm -rfv %d
+rem del /sxzy @del-dirs
+
+rm del-files
+rm del-dirs
 
 rem *** bundle it all up
-cd _distrib_zip
 tar cvf ../dist/wxPythonWIN32-devel-%1.tar wxPython-%1
 gzip -9 ../dist/wxPythonWIN32-devel-%1.tar
 
 rem *** cleanup
 cd ..
-del /sxzy _distrib_zip
+rm -rf _distrib_zip
 
 endlocal
 
