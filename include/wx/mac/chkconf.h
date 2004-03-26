9ea759d4498f89b30a6233b2809a97bcede2dfa9
Stefan Csomor <csomor@advancedconcepts.ch>
2004-03-26 06:28:38 +0000

    dispatch for header files in order to keep old way of includes working
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26356 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/chkconf.h
+++ include/wx/mac/chkconf.h
@@ -1,30 +1,5 @@
-/*
- * Name:        wx/mac/chkconf.h
- * Purpose:     Compiler-specific configuration checking
- * Author:      Julian Smart
- * Modified by:
- * Created:     01/02/97
- * RCS-ID:      $Id$
- * Copyright:   (c) Julian Smart
- * Licence:     wxWindows licence
- */
-
-#ifndef _WX_MAC_CHKCONF_H_
-#define _WX_MAC_CHKCONF_H_
-
-/*
- * disable the settings which don't work for some compilers
- */
-
-/*
- * wxUSE_DEBUG_NEW_ALWAYS doesn't work with CodeWarrior
- */
-
-#if defined(__MWERKS__)
-    #undef wxUSE_DEBUG_NEW_ALWAYS
-    #define wxUSE_DEBUG_NEW_ALWAYS      0
+#if __WXMAC_CLASSIC__
+#include "wx/mac/classic/chkconf.h"
+#else
+#include "wx/mac/carbon/chkconf.h"
 #endif
-
-#endif
-    /* _WX_MAC_CHKCONF_H_ */
-
