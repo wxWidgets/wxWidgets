d9692c12b2b752340e0722b04cd4a494b92779f7
Stefan Csomor <csomor@advancedconcepts.ch>
2004-03-27 05:26:42 +0000

    defines necessary for unicode support
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26391 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx_pb.h
+++ include/wx_pb.h
@@ -12,3 +12,9 @@
 #define HAVE_USLEEP 1
 #define HAVE_FCNTL  1
 #define SIZEOF_INT 4
+#define SIZEOF_WCHAR_T 4
+#define TARGET_CARBON 1
+#define HAVE_WCHAR_H 1
+#define HAVE_WPRINTF 1
+#define HAVE_VSWPRINTF 1
+#define HAVE_STRCASECMP_IN_STRING_H 1
