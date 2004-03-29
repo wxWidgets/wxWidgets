8155488b3143e972da919de992c7bba2e29d63a1
Julian Smart <julian@anthemion.co.uk>
2004-03-29 08:12:04 +0000

    Wince fix
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26447 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/wrapcctl.h
+++ include/wx/msw/wrapcctl.h
@@ -46,6 +46,7 @@
 // Set Unicode format for a common control
 inline void wxSetCCUnicodeFormat(HWND hwnd)
 {
+#ifndef __WXWINCE__
     SendMessage(hwnd, CCM_SETUNICODEFORMAT,
 #if wxUSE_UNICODE
                 TRUE
@@ -53,6 +54,8 @@ inline void wxSetCCUnicodeFormat(HWND hwnd)
                 FALSE
 #endif
                 , 0);
+#endif
+  // __WXWINCE__
 }
 
 #endif // _WX_MSW_WRAPCCTL_H_
