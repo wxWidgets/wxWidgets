fa8114de44a45d992e45912818307d8b1a5004ba
David Elliott <dfe@tgwbd.org>
2004-04-02 17:43:59 +0000

    Added wxObjcAutoRef* classes for automatic retain/release of Objective-C objects
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26580 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ src/cocoa/ObjcRef.mm
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        cocoa/ObjcRef.mm
+// Purpose:     wxObjcAutoRefBase implementation
+// Author:      David Elliott
+// Modified by: 
+// Created:     2004/03/28
+// RCS-ID:      $Id$
+// Copyright:   (c) 2004 David Elliott <dfe@cox.net>
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include "wx/cocoa/ObjcRef.h"
+
+#include <Foundation/NSObject.h>
+
+/*static*/ struct objc_object* wxObjcAutoRefBase::ObjcRetain(struct objc_object* obj)
+{
+    return [obj retain];
+}
+
+/*static*/ struct objc_object* wxObjcAutoRefBase::ObjcRelease(struct objc_object* obj)
+{
+    return [obj release];
+}
+
