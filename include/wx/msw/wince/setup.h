fbaddf44820481d8f6af849847d6b094316b27e1
Vadim Zeitlin <vadim@wxwidgets.org>
2004-04-08 12:11:37 +0000

    we can now use png/jpeg/gif
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26671 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/wince/setup.h
+++ include/wx/msw/wince/setup.h
@@ -951,16 +951,16 @@
 #define wxUSE_IMAGE         1
 
 // Set to 1 for PNG format support (requires libpng). Also requires wxUSE_ZLIB.
-#define wxUSE_LIBPNG        0
+#define wxUSE_LIBPNG        1
 
 // Set to 1 for JPEG format support (requires libjpeg)
-#define wxUSE_LIBJPEG       0
+#define wxUSE_LIBJPEG       1
 
 // Set to 1 for TIFF format support (requires libtiff)
 #define wxUSE_LIBTIFF       0
 
 // Set to 1 for GIF format support
-#define wxUSE_GIF           0
+#define wxUSE_GIF           1
 
 // Set to 1 for PNM format support
 #define wxUSE_PNM           0
