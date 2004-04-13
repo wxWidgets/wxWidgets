53dbce233842a3d986e33e6b83a36ce34fd5ae23
David Elliott <dfe@tgwbd.org>
2004-04-13 19:45:02 +0000

    wxCocoa: wxRegion using wxRegionGeneric
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@26754 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ src/cocoa/region.mm
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        cocoa/region.mm
+// Purpose:     wxRegion class
+// Author:      David Elliott
+// Modified by: 
+// Created:     2004/04/12
+// RCS-ID:      $Id$
+// Copyright:   (c) 2004 David Elliott
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include "wx/region.h"
+
+IMPLEMENT_DYNAMIC_CLASS(wxRegion, wxGDIObject);
+
