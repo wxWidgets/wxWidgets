4b890be8bb7829e7c77776abd48acdad2dfac2dc
Václav Slavík <vslavik@fastmail.fm>
2004-04-25 08:06:40 +0000

    imported zlib 1.2.1
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/ZLIB@26952 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/zlib/compress.c
+++ src/zlib/compress.c
@@ -5,6 +5,7 @@
 
 /* @(#) $Id$ */
 
+#define ZLIB_INTERNAL
 #include "zlib.h"
 
 /* ===========================================================================
@@ -65,4 +66,14 @@ int ZEXPORT compress (dest, destLen, source, sourceLen)
     uLong sourceLen;
 {
     return compress2(dest, destLen, source, sourceLen, Z_DEFAULT_COMPRESSION);
+}
+
+/* ===========================================================================
+     If the default memLevel or windowBits for deflateInit() is changed, then
+   this function needs to be updated.
+ */
+uLong ZEXPORT compressBound (sourceLen)
+    uLong sourceLen;
+{
+    return sourceLen + (sourceLen >> 12) + (sourceLen >> 14) + 11;
 }
