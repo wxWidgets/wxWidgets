05d89f2c0b85eb33c4818430e6cbf5355b6ac50f
Włodzimierz Skiba <abx@abx.art.pl>
2004-05-18 17:42:22 +0000

    Patch 950002 after agreement from Markus Sinner
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@27342 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/html/chm.cpp
+++ src/html/chm.cpp
@@ -557,11 +557,11 @@ wxChmInputStream::CreateHHPStream()
     const char *tmp;
 
     // Try to open the #SYSTEM-File and create the HHP File out of it
-    // see http://bonedaddy.net/pabs3/chmspec/0.1/ch05s03.html#SYSTEM
-    if ( ! m_chm->Contains(_("/#SYSTEM")) )
+    // see http://bonedaddy.net/pabs3/chmspec/0.1.2/Internal.html#SYSTEM
+    if ( ! m_chm->Contains(_T("/#SYSTEM")) )
     {
 #ifdef DEBUG
-        wxLogDebug("Archive doesnt contain #SYSTEM file");
+        wxLogDebug(_("Archive doesnt contain #SYSTEM file"));
 #endif
         return;
     }
