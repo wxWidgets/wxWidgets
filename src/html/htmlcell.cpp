b5e656ae0259211cf0beb02d19d87a651435625f
Václav Slavík <vslavik@fastmail.fm>
2004-05-27 16:25:07 +0000

    fixed iteration over cells
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@27466 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/html/htmlcell.cpp
+++ src/html/htmlcell.cpp
@@ -800,7 +800,7 @@ void wxHtmlContainerCell::Layout(int w)
                     {
                         prev =
                         next = NULL;
-                        for ( int n = 0; line != cell; line = line->GetNext() )
+                        for ( int n = 0; line != cell; prev = line, line = next )
                         {
                             line->SetPos(line->GetPosX() + s_indent +
                                            ((n * step) / total),
