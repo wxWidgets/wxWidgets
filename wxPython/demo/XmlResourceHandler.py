33b7017158309d0ffba37843f99bd7e53265dbc0
Robin Dunn <robin@alldunn.com>
2004-06-17 05:16:30 +0000

    XRC style names use the C++ style names
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@27844 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/XmlResourceHandler.py
+++ wxPython/demo/XmlResourceHandler.py
@@ -71,10 +71,10 @@ class MyCustomPanelXmlHandler(xrc.XmlResourceHandler):
     def __init__(self):
         xrc.XmlResourceHandler.__init__(self)
         # Specify the styles recognized by objects of this type
-        self.AddStyle("wx.NO_3D", wx.NO_3D);
-        self.AddStyle("wx.TAB_TRAVERSAL", wx.TAB_TRAVERSAL);
-        self.AddStyle("wx.WS_EX_VALIDATE_RECURSIVELY", wx.WS_EX_VALIDATE_RECURSIVELY);
-        self.AddStyle("wx.CLIP_CHILDREN", wx.CLIP_CHILDREN);
+        self.AddStyle("wxNO_3D", wx.NO_3D);
+        self.AddStyle("wxTAB_TRAVERSAL", wx.TAB_TRAVERSAL);
+        self.AddStyle("wxWS_EX_VALIDATE_RECURSIVELY", wx.WS_EX_VALIDATE_RECURSIVELY);
+        self.AddStyle("wxCLIP_CHILDREN", wx.CLIP_CHILDREN);
         self.AddWindowStyles();
 
     # This method and the next one are required for XmlResourceHandlers
