d6375c69831f8cdb1beff91c67bb9c1a5f8095cb
Robin Dunn <robin@alldunn.com>
2004-06-24 04:45:20 +0000

    tweaks for testing
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@27990 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/Notebook.py
+++ wxPython/demo/Notebook.py
@@ -13,11 +13,11 @@ import  images
 
 class TestNB(wx.Notebook):
     def __init__(self, parent, id, log):
-        wx.Notebook.__init__(self, parent, id, style=
-                            #wxNB_TOP
+        wx.Notebook.__init__(self, parent, id, size=(21,21), style=
+                            #wx.NB_TOP # | wx.NB_MULTILINE
                             wx.NB_BOTTOM
-                            #wxNB_LEFT
-                            #wxNB_RIGHT
+                            #wx.NB_LEFT
+                            #wx.NB_RIGHT
                             )
         self.log = log
 
