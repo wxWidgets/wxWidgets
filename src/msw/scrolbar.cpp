1e63e750766f9c6313a08a79e625cb0d9fa74294
Robin Dunn <robin@alldunn.com>
2004-06-24 04:45:53 +0000

    removed a SetFont(parent->GetFont())
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@27991 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/scrolbar.cpp
+++ src/msw/scrolbar.cpp
@@ -152,8 +152,6 @@ bool wxScrollBar::Create(wxWindow *parent, wxWindowID id,
     ::SetScrollPos(scroll_bar, SB_CTL, 0, FALSE);
     ShowWindow(scroll_bar, SW_SHOW);
 
-    SetFont(parent->GetFont());
-
     m_hWnd = (WXHWND)scroll_bar;
 
     // Subclass again for purposes of dialog editing mode
