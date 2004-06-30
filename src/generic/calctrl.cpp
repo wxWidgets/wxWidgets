5f89b54341d5b81fec29a9524eba02f659e14260
Robin Dunn <robin@alldunn.com>
2004-06-30 21:55:10 +0000

    fixed typo
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@28113 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/calctrl.cpp
+++ src/generic/calctrl.cpp
@@ -33,7 +33,7 @@
     #include "wx/settings.h"
     #include "wx/brush.h"
     #include "wx/combobox.h"
-    #include "wx/listbox"
+    #include "wx/listbox.h"
     #include "wx/stattext.h"
     #include "wx/textctrl.h"
 #endif //WX_PRECOMP
