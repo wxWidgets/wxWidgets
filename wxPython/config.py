9fa2073f081e3ec6693a126e1025eb3432d3e785
Robin Dunn <robin@alldunn.com>
2004-08-07 03:43:59 +0000

    version number update
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@28690 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/config.py
+++ wxPython/config.py
@@ -38,7 +38,7 @@ import distutils.command.clean
 VER_MAJOR        = 2      # The first three must match wxWidgets
 VER_MINOR        = 5
 VER_RELEASE      = 2
-VER_SUBREL       = 3      # wxPython release num for x.y.z release of wxWidgets
+VER_SUBREL       = 4      # wxPython release num for x.y.z release of wxWidgets
 VER_FLAGS        = ""     # release flags, such as prerelease num, unicode, etc.
 
 DESCRIPTION      = "Cross platform GUI toolkit for Python"
