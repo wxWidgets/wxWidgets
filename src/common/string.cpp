597ee16421847ac338b3b2cfcd0e319398e9b54a
Václav Slavík <vslavik@fastmail.fm>
2004-08-11 08:18:37 +0000

    assert if conversion ctors are used incorrectly
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@28760 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/string.cpp
+++ src/common/string.cpp
@@ -968,6 +968,7 @@ wxString::wxString(const char *psz, wxMBConv& conv, size_t nLength)
     wxCharBuffer inBuf((const char *)NULL);
     if (nLength != npos)
     {
+        wxASSERT_MSG( psz != NULL );
         wxCharBuffer tmp(nLength);
         memcpy(tmp.data(), psz, nLength);
         tmp.data()[nLength] = '\0';
@@ -1025,6 +1026,7 @@ wxString::wxString(const wchar_t *pwz, wxMBConv& conv, size_t nLength)
     wxWCharBuffer inBuf((const wchar_t *)NULL);
     if (nLength != npos)
     {
+        wxASSERT_MSG( pwz != NULL );
         wxWCharBuffer tmp(nLength);
         memcpy(tmp.data(), pwz, nLength * sizeof(wchar_t));
         tmp.data()[nLength] = '\0';
