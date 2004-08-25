ac1c82d736d9b8bdb9d38d865212cc0db7903ed4
Robin Dunn <robin@alldunn.com>
2004-08-25 22:05:59 +0000

    comments
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@28905 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wx/lib/sheet.py
+++ wxPython/wx/lib/sheet.py
@@ -64,6 +64,8 @@ class CCellEditor(wx.grid.PyGridCellEditor):
     def Show(self, show, attr):
         """ Show or hide the edit control.  Use the attr (if not None)
             to set colors or fonts for the control.
+            NOTE: There is no need to everride this if you don't need
+                  to do something out of the ordingary.
         """
         self.base_Show(show, attr)
 
@@ -71,6 +73,8 @@ class CCellEditor(wx.grid.PyGridCellEditor):
         """ Draws the part of the cell not occupied by the edit control.  The
             base class version just fills it with background colour from the
             attribute.
+            NOTE: There is no need to everride this if you don't need
+                  to do something out of the ordingary.
         """
         # Call base class method.
         self.base_PaintBackground(self, rect, attr)
@@ -146,7 +150,10 @@ class CCellEditor(wx.grid.PyGridCellEditor):
         pass
 
     def Destroy(self):
-        """ Final cleanup """
+        """ Final cleanup
+            NOTE: There is no need to everride this if you don't need
+                  to do something out of the ordingary.
+        """
         self.base_Destroy()
 
     def Clone(self):
