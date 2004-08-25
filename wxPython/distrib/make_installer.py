609b563c8b9ade4fcde9160f11c7deaf16a9f7b6
Robin Dunn <robin@alldunn.com>
2004-08-25 16:59:16 +0000

    "wxWindows" --> "wxWidgets"
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@28889 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/make_installer.py
+++ wxPython/distrib/make_installer.py
@@ -257,7 +257,7 @@ Name: "{group}\Resource Editor";       Filename: "{code:GetPythonDir}\pythonw.ex
 
 Name: "{group}\Sample Apps";           Filename: "{app}\wx\samples"; Components: samples
 
-Name: "{group}\wxWindows Reference";   Filename: "{app}\wx\docs\wx.chm";                Components: docs
+Name: "{group}\wxWidgets Reference";   Filename: "{app}\wx\docs\wx.chm";                Components: docs
 Name: "{group}\Migration Guide";       Filename: "{app}\wx\docs\MigrationGuide.html";   Components: docs
 Name: "{group}\Recent Changes";        Filename: "{app}\wx\docs\CHANGES.html";          Components: docs
 Name: "{group}\Other Docs";            Filename: "{app}\wx\docs";                       Components: docs
