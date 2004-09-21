49d55bdc6bf261d75c679ab7878c656034542543
Robin Dunn <robin@alldunn.com>
2004-09-21 22:30:09 +0000

    Bump subrelease to 2.5.2.9.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@29246 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/version.h
+++ include/wx/version.h
@@ -22,7 +22,7 @@
 #define wxMAJOR_VERSION      2
 #define wxMINOR_VERSION      5
 #define wxRELEASE_NUMBER     2
-#define wxSUBRELEASE_NUMBER  8
+#define wxSUBRELEASE_NUMBER  9
 #define wxVERSION_STRING   _T("wxWidgets 2.5.2")
 
 /*  nothing to update below this line when updating the version */
