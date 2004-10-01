10c5f652b9604accb5981b59515d93c64df63f2c
Ryan Norton <wxprojects@comcast.net>
2004-10-01 12:40:59 +0000

    fix const to normal  warning
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@29591 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/cocoa/dc.mm
+++ src/cocoa/dc.mm
@@ -636,7 +636,7 @@ void wxDC::ComputeScaleAndOrigin(void)
   {
     // this is a bit artificial, but we need to force wxDC to think
     // the pen has changed
-    wxPen* pen = & GetPen();
+    const wxPen* pen = & GetPen();
     wxPen tempPen;
     m_pen = tempPen;
     SetPen(* pen);
