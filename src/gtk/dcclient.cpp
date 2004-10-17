f53df133a6e45a59bbec73cc412d1c10a9b49912
Robert Roebling <robert@roebling.de>
2004-10-17 14:32:42 +0000

    Applied (part of) small patch from Chris Bogolte which     corrects a test in the Blit code.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@29942 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/dcclient.cpp
+++ src/gtk/dcclient.cpp
@@ -1308,7 +1308,7 @@ bool wxWindowDC::DoBlit( wxCoord xdest, wxCoord ydest,
 
         // Scale bitmap if required
         wxBitmap use_bitmap;
-        if ((bm_width != bm_ww) || (bm_height != bm_hh))
+        if ((memDC->m_selected.GetWidth()!= bm_ww) || ( memDC->m_selected.GetHeight()!= bm_hh))
         {
             // This indicates that the blitting code below will get
             // a clipped bitmap and therefore needs to move the origin
@@ -1426,7 +1426,7 @@ bool wxWindowDC::DoBlit( wxCoord xdest, wxCoord ydest,
     }
     else // use_bitmap_method
     {
-        if ((width != ww) || (height != hh))
+        if ((memDC->m_selected.GetWidth() != ww) || (memDC->m_selected.GetHeight() != hh))
         {
             // get clip coords
             wxRegion tmp( xx,yy,ww,hh );
