18a82b9cd35b9c91ff381b6b34922788a8d9b897
Stefan Csomor <csomor@advancedconcepts.ch>
2004-10-17 10:52:37 +0000

    don't restrict to OS9 API for Mac-Builds
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@29938 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/morefilex/MoreFilesX.cpp
+++ src/mac/carbon/morefilex/MoreFilesX.cpp
@@ -72,9 +72,11 @@
 #if defined(__MACH__)
 	#include <Carbon/Carbon.h>
 	#include <string.h>
+	#define BuildingMoreFilesXForMacOS9 0
 #else
 	#include <Carbon.h>
 	#include <string.h>
+	#define BuildingMoreFilesXForMacOS9 1
 #endif
 
 #include "MoreFilesX.h"
