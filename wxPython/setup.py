7875e5ff98026892cca756bb7efd82b32ec192fc
Kevin Ollivier <kevino@theolliviers.com>
2004-10-19 06:07:13 +0000

    Additions for webkit wrapper support.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@29989 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/setup.py
+++ wxPython/setup.py
@@ -354,7 +354,18 @@ ext = Extension('_html', swig_sources,
                 )
 wxpExtensions.append(ext)
 
-
+swig_sources = run_swig(['webkit.i'], 'src', GENDIR, PKGDIR,
+                        USE_SWIG, swig_force, swig_args, swig_deps)
+ext = Extension('_webkit', swig_sources,
+                include_dirs =  includes,
+                define_macros = defines,
+                library_dirs = libdirs,
+                libraries = libs,
+                extra_compile_args = cflags,
+                extra_link_args = lflags,
+                **depends
+                )
+wxpExtensions.append(ext)
 
 swig_sources = run_swig(['wizard.i'], 'src', GENDIR, PKGDIR,
                         USE_SWIG, swig_force, swig_args, swig_deps)
