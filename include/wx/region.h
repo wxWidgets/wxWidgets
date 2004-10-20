c87beb79f2593f758de2f5dfa0f399013c7a703a
Vadim Zeitlin <vadim@wxwidgets.org>
2004-10-20 17:29:21 +0000

    include colour.h (fixes wxX11 compilation under Unix)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30039 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/region.h
+++ include/wx/region.h
@@ -1,24 +1,24 @@
 #ifndef _WX_REGION_H_BASE_
 #define _WX_REGION_H_BASE_
 
+#include "wx/colour.h"
+
 #if defined(__PALMOS__)
-#include "wx/palmos/region.h"
+    #include "wx/palmos/region.h"
 #elif defined(__WXMSW__)
-#include "wx/msw/region.h"
-#elif defined(__WXMOTIF__)
-#include "wx/x11/region.h"
+    #include "wx/msw/region.h"
 #elif defined(__WXGTK__)
-#include "wx/gtk/region.h"
-#elif defined(__WXX11__)
-#include "wx/x11/region.h"
+    #include "wx/gtk/region.h"
+#elif defined(__WXMOTIF__) || defined(__WXX11__)
+    #include "wx/x11/region.h"
 #elif defined(__WXMGL__)
-#include "wx/mgl/region.h"
+    #include "wx/mgl/region.h"
 #elif defined(__WXMAC__)
-#include "wx/mac/region.h"
+    #include "wx/mac/region.h"
 #elif defined(__WXCOCOA__)
-#include "wx/cocoa/region.h"
+    #include "wx/cocoa/region.h"
 #elif defined(__WXPM__)
-#include "wx/os2/region.h"
+    #include "wx/os2/region.h"
 #endif
 
 #endif
