c25b742aec59fd4b141fd6b355713a6427451b07
Václav Slavík <vslavik@fastmail.fm>
2004-10-28 11:20:44 +0000

    FL's floating windows render borders themselves, don't use small titlebars
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30142 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/fl/controlbar.cpp
+++ contrib/src/fl/controlbar.cpp
@@ -909,7 +909,7 @@ void wxFrameLayout::DoSetBarState( cbBarInfo* pBar )
                           wxPoint( 50,50 ),
                           wxSize ( 0, 0  ),
                           wxFRAME_FLOAT_ON_PARENT |
-                          wxFRAME_TOOL_WINDOW |
+                          wxNO_BORDER |
                           wxFRAME_NO_TASKBAR
                         );
 
