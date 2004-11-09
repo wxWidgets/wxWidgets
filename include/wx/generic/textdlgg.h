55b46fd164e36b063ee924e8a433f6b0f876dc70
Kevin Hock <hockkn@yahoo.com>
2004-11-09 19:13:15 +0000

    [1001483] Added wxPasswordEntryDialog analagous to wxTextEntryDialog, allows detecting entering an empty string vs. cancel unlike the wxGetPasswordFromUser dialog function.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30398 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/generic/textdlgg.h
+++ include/wx/generic/textdlgg.h
@@ -28,6 +28,7 @@
 class WXDLLEXPORT wxTextCtrl;
 
 WXDLLEXPORT_DATA(extern const wxChar*) wxGetTextFromUserPromptStr;
+WXDLLEXPORT_DATA(extern const wxChar*) wxGetPasswordFromUserPromptStr;
 
 #define wxTextEntryDialogStyle (wxOK | wxCANCEL | wxCENTRE | wxWS_EX_VALIDATE_RECURSIVELY)
 
@@ -70,6 +71,24 @@ private:
 };
 
 // ----------------------------------------------------------------------------
+// wxPasswordEntryDialog: dialog with password control, [ok] and [cancel]
+// ----------------------------------------------------------------------------
+
+class WXDLLEXPORT wxPasswordEntryDialog : public wxTextEntryDialog
+{
+public:
+    wxPasswordEntryDialog(wxWindow *parent,
+                      const wxString& message,
+                      const wxString& caption = wxGetPasswordFromUserPromptStr,
+                      const wxString& value = wxEmptyString,
+                      long style = wxTextEntryDialogStyle,
+                      const wxPoint& pos = wxDefaultPosition);
+private:
+    DECLARE_DYNAMIC_CLASS(wxPasswordEntryDialog)
+    DECLARE_NO_COPY_CLASS(wxPasswordEntryDialog)
+};
+
+// ----------------------------------------------------------------------------
 // function to get a string from user
 // ----------------------------------------------------------------------------
 
@@ -84,7 +103,7 @@ wxGetTextFromUser(const wxString& message,
 
 wxString WXDLLEXPORT
 wxGetPasswordFromUser(const wxString& message,
-                      const wxString& caption = wxGetTextFromUserPromptStr,
+                      const wxString& caption = wxGetPasswordFromUserPromptStr,
                       const wxString& default_value = wxEmptyString,
                       wxWindow *parent = (wxWindow *) NULL,
                       wxCoord x = wxDefaultCoord,
