cbfb8fe75add9ee7a20d06b468506d2764c7d82c
Ryan Norton <wxprojects@comcast.net>
2004-11-12 10:05:13 +0000

    explanation of getdefaultattributes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30486 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/textctrl.cpp
+++ src/msw/textctrl.cpp
@@ -2177,17 +2177,18 @@ bool wxTextCtrl::MSWOnNotify(int idCtrl, WXLPARAM lParam, WXLPARAM *result)
 }
 
 // ----------------------------------------------------------------------------
-// default colors for MSW text control
+// Default colors for MSW text control
+//
+// Set default background color to the native white instead of
+// the default wxSYS_COLOUR_BTNFACE (is triggered with wxNullColour). 
 // ----------------------------------------------------------------------------
 
 wxVisualAttributes wxTextCtrl::GetDefaultAttributes() const
 {
-    // it is important to return valid values for all attributes from here,
-    // GetXXX() below rely on this
     wxVisualAttributes attrs;
     attrs.font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
     attrs.colFg = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWTEXT);
-    attrs.colBg = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW);
+    attrs.colBg = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW); //white
 
     return attrs;
 }
