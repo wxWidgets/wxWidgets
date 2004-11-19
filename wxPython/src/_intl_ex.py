cde9174af0b266cb21723bc75dc5fb0418a7cc3f
Robin Dunn <robin@alldunn.com>
2004-11-19 19:23:23 +0000

    Use static method for AddCatalogLookupPathPrefix instead of global function
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30638 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/src/_intl_ex.py
+++ wxPython/src/_intl_ex.py
@@ -3,8 +3,8 @@
 # to the default catalog path.
 if wx.Platform == "__WXMSW__":
     import os
-    localedir = os.path.join(os.path.split(__file__)[0], "locale")
-    Locale_AddCatalogLookupPathPrefix(localedir)
+    _localedir = os.path.join(os.path.split(__file__)[0], "locale")
+    Locale.AddCatalogLookupPathPrefix(_localedir)
     del os
 
 #----------------------------------------------------------------------------
