36b79d445d08e08776e721d71a832f38daa7fa8c
Dimitri Schoolwerth <dimitri.schoolwerth@gmail.com>
2004-11-23 12:53:57 +0000

    fixed #includes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30729 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/bookctrl.h
+++ include/wx/bookctrl.h
@@ -241,7 +241,7 @@ private:
 
 // make a default book control for given platform
 #if defined(__WXMSW__) && defined(__SMARTPHONE__)
-    #include "wx\choicebook.h"
+    #include "wx/choicebook.h"
     #define wxBookCtrl                             wxChoicebook
     #define wxBookCtrlEvent                        wxChoicebookEvent
     #define wxEVT_COMMAND_BOOKCTRL_PAGE_CHANGED    wxEVT_COMMAND_CHOICEBOOK_PAGE_CHANGED
@@ -254,7 +254,7 @@ private:
     #define wxBC_RIGHT                             wxCHB_RIGHT
     #define wxBC_DEFAULT                           wxCHB_DEFAULT
 #else
-    #include "wx\notebook.h"
+    #include "wx/notebook.h"
     #define wxBookCtrl                             wxNotebook
     #define wxBookCtrlEvent                        wxNotebookEvent
     #define wxEVT_COMMAND_BOOKCTRL_PAGE_CHANGED    wxEVT_COMMAND_NOTEBOOK_PAGE_CHANGED
