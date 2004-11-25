524065c14ec6c77eec0fb0975418be0fabdd1d3a
Vadim Zeitlin <vadim@wxwidgets.org>
2004-11-25 14:53:08 +0000

    added test for wxUSE_SCROLLBAR
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@30782 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xrc/xh_scrol.cpp
+++ src/xrc/xh_scrol.cpp
@@ -19,7 +19,7 @@
     #pragma hdrstop
 #endif
 
-#if wxUSE_XRC
+#if wxUSE_XRC && wxUSE_SCROLLBAR
 
 #include "wx/xrc/xh_scrol.h"
 #include "wx/scrolbar.h"
@@ -61,4 +61,4 @@ bool wxScrollBarXmlHandler::CanHandle(wxXmlNode *node)
     return IsOfClass(node, wxT("wxScrollBar"));
 }
 
-#endif // wxUSE_XRC
+#endif // wxUSE_XRC && wxUSE_SCROLLBAR
