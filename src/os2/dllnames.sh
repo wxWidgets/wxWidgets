687da5395f3fc712c40183ba258d4c35ddeda0c0
Stefan Neis <Stefan.Neis@t-online.de>
2004-12-18 16:12:00 +0000

    Added support for debug libraries.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@31056 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/os2/dllnames.sh
+++ src/os2/dllnames.sh
@@ -10,6 +10,15 @@ case $dllFile in
 *wx_base_*)
     dllFile=`echo $dllFile | sed 's/base_\(...\)/b\1/'`
     ;;
+*wx_based_*)
+    dllFile=`echo $dllFile | sed 's/base_\(...\)/d\1/'`
+    ;;
+*wx_based-*)
+    dllFile=`echo $dllFile | sed 's/based/basd/'`
+    ;;
+*wx_*d_*)
+    dllFile=`echo $dllFile | sed 's/_\(.\)[^_]*_\(..\)[^-]*-/\1d\2/'`
+    ;;
 *wx_*_*)
     dllFile=`echo $dllFile | sed 's/_\(..\)[^_]*_\(..\)[^-]*-/\1\2/'`
     ;;
