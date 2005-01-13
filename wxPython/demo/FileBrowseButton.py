a91fbc8e34b7b4bf203b03ac66f129e85cdfa53b
Robin Dunn <robin@alldunn.com>
2005-01-13 20:46:17 +0000

    Use sizers for layout
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@31338 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/FileBrowseButton.py
+++ wxPython/demo/FileBrowseButton.py
@@ -16,18 +16,26 @@ class TestPanel(wx.Panel):
         self.log = log
 
         self.fbb = filebrowse.FileBrowseButton(
-            self, -1, (20, 20), (450, -1), changeCallback = self.fbbCallback
+            self, -1, size=(450, -1), changeCallback = self.fbbCallback
             )
 
         self.fbbh = filebrowse.FileBrowseButtonWithHistory(
-            self, -1, (20, 50), (450, -1),  changeCallback = self.fbbhCallback
+            self, -1, size=(450, -1),  changeCallback = self.fbbhCallback
             )
             
         self.dbb = filebrowse.DirBrowseButton(
-            self, -1, (20, 80), (450, -1), changeCallback = self.dbbCallback
+            self, -1, size=(450, -1), changeCallback = self.dbbCallback
             )
 
         self.fbbh.SetHistory(['You', 'can', 'put', 'some', 'filenames', 'here'])
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.fbb, 0, wx.ALL, 5)
+        sizer.Add(self.fbbh, 0, wx.ALL, 5)
+        sizer.Add(self.dbb, 0, wx.ALL, 5)
+        box = wx.BoxSizer()
+        box.Add(sizer, 0, wx.ALL, 20)
+        self.SetSizer(box)
 
 
     def fbbCallback(self, evt):
