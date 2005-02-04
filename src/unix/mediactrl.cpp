ddc90a8d52e91ca4b6a2d7643824aede1395067c
Ryan Norton <wxprojects@comcast.net>
2005-02-04 22:47:48 +0000

    unix mediactrl stubs...
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@31760 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ src/unix/mediactrl.cpp
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        mac/carbon/mediactrl.cpp
+// Purpose:     Built-in Media Backends for Unix (stub)
+// Author:      Ryan Norton <wxprojects@comcast.net>
+// Modified by:
+// Created:     02/04/05
+// RCS-ID:      $Id$
+// Copyright:   (c) 2004-2005 Ryan Norton
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+//===========================================================================
+//  DECLARATIONS
+//===========================================================================
+
+//---------------------------------------------------------------------------
+// Pre-compiled header stuff
+//---------------------------------------------------------------------------
+
+#if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
+#pragma implementation "mediactrl.h"
+#endif
+
+// For compilers that support precompilation, includes "wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "wx/mediactrl.h"
+
+//---------------------------------------------------------------------------
+// Compilation guard
+//---------------------------------------------------------------------------
+#if wxUSE_MEDIACTRL
+
+//in source file that contains stuff you don't directly use
+#include <wx/html/forcelnk.h>
+FORCE_LINK_ME(basewxmediabackends);
+
+#endif //wxUSE_MEDIACTRL
+
+
+
+
+
