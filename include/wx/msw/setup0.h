a4567ae7fe2d9f3400dfc14d469545937faed902
Ryan Norton <wxprojects@comcast.net>
2005-02-26 10:23:39 +0000

    I'm assuming this was meant to be included
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@32385 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/setup0.h
+++ include/wx/msw/setup0.h
@@ -120,7 +120,12 @@
 // Default is 1
 //
 // Recommended setting: 1 if your compiler supports it.
-#define wxUSE_ON_FATAL_EXCEPTION 1
+#if defined(_MSC_VER) || \
+    (defined(__BORLANDC__) && __BORLANDC__ >= 0x0550)
+    #define wxUSE_ON_FATAL_EXCEPTION 1
+#else
+    #define wxUSE_ON_FATAL_EXCEPTION 0
+#endif
 
 // Set this to 1 to be able to generate a human-readable (unlike
 // machine-readable minidumop created by wxCrashReport::Generate()) stack back
