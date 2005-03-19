83c84d59086838d33192cadbfb7f1aabbe9bf52e
Robin Dunn <robin@alldunn.com>
2005-03-19 00:38:46 +0000

    Bumped subrelease number
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@32907 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/config.py
+++ wxPython/config.py
@@ -39,8 +39,8 @@ import distutils.command.clean
 VER_MAJOR        = 2      # The first three must match wxWidgets
 VER_MINOR        = 5
 VER_RELEASE      = 4
-VER_SUBREL       = 1      # wxPython release num for x.y.z release of wxWidgets
-VER_FLAGS        = ""     # release flags, such as prerelease num, unicode, etc.
+VER_SUBREL       = 2      # wxPython release num for x.y.z release of wxWidgets
+VER_FLAGS        = "pre"     # release flags, such as prerelease num, unicode, etc.
 
 DESCRIPTION      = "Cross platform GUI toolkit for Python"
 AUTHOR           = "Robin Dunn"
