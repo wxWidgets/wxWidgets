ee64d4f33e7e539af47c2a3dc24fc3e6a39f10b2
Michael Wetherell <mike.wetherell@ntlworld.com>
2005-05-14 21:16:45 +0000

    Add wxUSE_CRASHREPORT
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@34064 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/univ/setup0.h
+++ include/wx/univ/setup0.h
@@ -1161,6 +1161,14 @@
 // Recommended setting: 1, set to 0 for a small library size reduction
 #define wxUSE_OWNER_DRAWN 0
 
+// Set this to 1 to be able to use wxCrashReport::Generate() to create mini
+// dumps of your program when it crashes (or at any other moment)
+//
+// Default is 1 if supported by the compiler (VC++ and recent BC++ only).
+//
+// Recommended setting: 1, set to 0 if your programs never crash
+#define wxUSE_CRASHREPORT 1
+
 #endif
 
 // ----------------------------------------------------------------------------
