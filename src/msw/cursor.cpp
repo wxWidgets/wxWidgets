65c7bef95160494c6109e4c63b464b9b756a838b
Julian Smart <julian@anthemion.co.uk>
2005-05-15 16:43:12 +0000

    Removed rarely-used cursor; replaced other cursors
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_4_BRANCH@34072 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/cursor.cpp
+++ src/msw/cursor.cpp
@@ -325,7 +325,7 @@ wxCursor::wxCursor(int cursor_type)
     }
     case wxCURSOR_NO_ENTRY:
     {
-      refData->m_hCursor = (WXHCURSOR) LoadCursor(wxGetInstance(), wxT("wxCURSOR_NO_ENTRY"));
+      refData->m_hCursor = (WXHCURSOR) LoadCursor((HINSTANCE) NULL, IDC_NO);
       break;
     }
     case wxCURSOR_LEFT_BUTTON:
@@ -350,7 +350,7 @@ wxCursor::wxCursor(int cursor_type)
     }
     case wxCURSOR_WATCH:
     {
-      refData->m_hCursor = (WXHCURSOR) LoadCursor(wxGetInstance(), wxT("wxCURSOR_WATCH"));
+      refData->m_hCursor = (WXHCURSOR) LoadCursor((HINSTANCE) NULL, IDC_WAIT);
       break;
     }
     case wxCURSOR_SPRAYCAN:
