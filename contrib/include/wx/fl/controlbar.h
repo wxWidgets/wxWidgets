abd20d3013277f639fe205c2e0aefc439c16ee26
Włodzimierz Skiba <abx@abx.art.pl>
2005-05-19 20:04:42 +0000

    Export correction (blind fix for error found in Tinderbox logs).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@34179 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/include/wx/fl/controlbar.h
+++ contrib/include/wx/fl/controlbar.h
@@ -904,10 +904,10 @@ public:
     bool          mHasLeftHandle;
     bool          mHasRightHandle;
 
-    // determines if this bar can float. The layout's setting as priority. For 
+    // determines if this bar can float. The layout's setting as priority. For
     // example, if the layout's mFloatingOn is false, this setting is irrelevant
     // since nothing will float at all. If the layout's floating is on, use this
-    // setting to prevent specific bars from floating. In other words, all bars 
+    // setting to prevent specific bars from floating. In other words, all bars
     // float by default and floating can be turned off on individual bars.
     bool          mFloatingOn;    // default: ON (which is also the layout's mFloatingOn default setting)
 
@@ -1036,11 +1036,11 @@ class cbCommonPaneProperties : public wxObject
     cbCommonPaneProperties(void);
 
         // Copy constructor
-    
+
     cbCommonPaneProperties(const cbCommonPaneProperties&);
 
         // Assignment operator
-    
+
     cbCommonPaneProperties& operator=(const cbCommonPaneProperties&);
 };
 
@@ -1461,7 +1461,7 @@ areas of frame layout that actually need to be updated. This should be extended 
 to implement a custom updating strategy.
 */
 
-class cbUpdatesManagerBase : public wxObject
+class WXDLLIMPEXP_FL cbUpdatesManagerBase : public wxObject
 {
     DECLARE_ABSTRACT_CLASS( cbUpdatesManagerBase )
 
