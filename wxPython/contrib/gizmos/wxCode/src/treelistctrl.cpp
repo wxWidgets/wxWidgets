0a3d6509d40f8017c3b0b501393c4f8c499a8b04
Dimitri Schoolwerth <dimitri.schoolwerth@gmail.com>
2005-05-22 01:48:22 +0000

    removed redundant preprocessor check
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@34233 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/contrib/gizmos/wxCode/src/treelistctrl.cpp
+++ wxPython/contrib/gizmos/wxCode/src/treelistctrl.cpp
@@ -1891,7 +1891,7 @@ void wxTreeListMainWindow::Init()
 
     m_underMouse = NULL;
 
-#if defined( __WXMAC__ ) && defined(__WXMAC_CARBON__)
+#ifdef __WXMAC_CARBON__
     m_normalFont.MacCreateThemeFont( kThemeViewsFont ) ;
 #else
     m_normalFont = wxSystemSettings::GetFont( wxSYS_DEFAULT_GUI_FONT );
