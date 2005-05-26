254e485eee4e1145b5c3fbb3f5662f68a2a5b9fd
Bryan Petty <bryan@ibaku.net>
2005-05-26 18:07:43 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.30.2'.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/unlabeled-1.30.2@34352 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wx/py/version.py
+++
@@ -1,9 +0,0 @@
-"""Provides an object representing the current 'version' or 'release'
-of Py as a whole.  Individual classes, such as the shell, filling and
-interpreter, each have a revision property based on the CVS Revision."""
-
-__author__ = "Patrick K. O'Brien <pobrien@orbtech.com>"
-__cvsid__ = "$Id$"
-__revision__ = "$Revision$"[11:-2]
-
-VERSION = '0.9.4'
