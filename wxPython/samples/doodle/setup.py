f0e8780701496eefa0877643cd015b7609b1023c
Robin Dunn <robin@alldunn.com>
2005-05-30 18:31:31 +0000

    Include msvcr71.dll if appropriate
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@34421 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/samples/doodle/setup.py
+++ wxPython/samples/doodle/setup.py
@@ -7,11 +7,19 @@
 #
 
 
+import sys, os
 from distutils.core import setup
 import py2exe
 
+if sys.platform == "win32" and sys.version_info > (2, 4):
+    DATA =  [("", os.path.join(sys.exec_prefix, 'msvcr71.dll'))]
+else:
+    DATA = []
+    
+
 setup( name = "superdoodle",
        #console = ["superdoodle.py"]
-       windows = ["superdoodle.py"]
+       windows = ["superdoodle.py"],
+       data_files = DATA,
        )
 
