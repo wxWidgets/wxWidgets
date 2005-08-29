bd118d35c67a840f94e7c369a4096f978054154c
Vadim Zeitlin <vadim@wxwidgets.org>
2005-08-29 13:48:56 +0000

    (blind) compilation fix for Borland after last commit
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@35363 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/datetime.cpp
+++ src/common/datetime.cpp
@@ -2851,7 +2851,7 @@ const wxChar *wxDateTime::ParseRfc822Date(const wxChar* date)
 
     // the spec was correct, construct the date from the values we found
     Set(day, mon, year, hour, min, sec);
-    MakeFromTimezone(offset*SEC_PER_MIN);
+    MakeFromTimezone(TimeZone(offset*SEC_PER_MIN));
 
     return p;
 }
