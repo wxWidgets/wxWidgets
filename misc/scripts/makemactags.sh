e426ff2972ea9927b683077696d4f767703f93b9
Vadim Zeitlin <vadim@wxwidgets.org>
2005-08-31 14:04:00 +0000

    create tags file for wxMac/Carbon
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@35381 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ misc/scripts/makemactags.sh
@@ -0,0 +1,2 @@
+ctags --totals src/common/*.cpp src/mac/corefoundation/*.cpp src/mac/carbon/*.cpp src/unix/*.cpp src/generic/*.cpp src/html/*.cpp src/xrc/*.cpp src/xml/*.cpp include/wx/*.h include/wx/mac/carbon/*.h include/wx/mac/*.h include/wx/generic/*.h include/wx/html/*.h include/wx/protocol/*.h include/wx/xml/*.h include/wx/xrc/*.h include/wx/unix/*.h include/wx/mac/corefoundation/*.h
+
