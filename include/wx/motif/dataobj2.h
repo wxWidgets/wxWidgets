3f62badb5f6c8b08d1cfdaf330d86f2d0eddeba7
Vadim Zeitlin <vadim@wxwidgets.org>
2005-09-13 16:37:11 +0000

    unihde base class virtual functions
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@35497 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/motif/dataobj2.h
+++ include/wx/motif/dataobj2.h
@@ -33,6 +33,16 @@ public:
     virtual size_t GetDataSize() const;
     virtual bool GetDataHere(void *buf) const;
     virtual bool SetData(size_t len, const void *buf);
+
+    // unhide base class virtual functions
+    virtual size_t GetDataSize(const wxDataFormat& WXUNUSED(format)) const
+        { return GetDataSize(); }
+    virtual bool GetDataHere(const wxDataFormat& WXUNUSED(format),
+                             void *buf) const
+        { return GetDataHere(buf); }
+    virtual bool SetData(const wxDataFormat& WXUNUSED(format),
+                         size_t len, const void *buf)
+        { return SetData(len, buf); }
 };
 
 #endif // _WX_MOTIF_DATAOBJ2_H_
