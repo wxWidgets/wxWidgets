6961b6be291462a041602d28dc8c93b6ad75d342
Kevin Ollivier <kevino@theolliviers.com>
2005-10-03 01:14:56 +0000

    Backporting build script changes on HEAD to WX_2_6_BRANCH
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@35773 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/distrib/build_packages.sh
+++ wxPython/distrib/build_packages.sh
@@ -3,6 +3,16 @@
 PY_VERSION=$1
 shift
 
+unicode=no
+debug=no
+
+for flag in $*; do
+    case ${flag} in
+      debug)       debug=yes              ;;
+      unicode)     unicode=yes            ;;
+    esac
+done
+
 if [ "$WXWIN" = "" ]; then
   export WXWIN=`pwd`/../..
 fi
@@ -19,20 +29,29 @@ if [ "$OSTYPE" = "cygwin" ]; then
     export SWIGDIR=$TOOLS/SWIG-1.3.24
   fi
 
+  DEBUG_FLAG=
+  UNICODE_FLAG=
+  if [ $debug = yes ]; then
+    DEBUG_FLAG=--debug
+  fi
+  if [ $unicode = yes ]; then
+    UNICODE_FLAG="UNICODE=1"
+  fi
+
   # copy wxPython build scripts
   cp $WXWIN/wxPython/distrib/msw/.m* $WXWIN/build/msw
   
   # setup wxPython defines
   cp $WXWIN/include/wx/msw/setup0.h $WXWIN/include/wx/msw/setup.h
-  $TOOLS/Python$PY_VERSION/python `cygpath -d $WXWIN/wxPython/distrib/create_setup.h.py` $@
+  $TOOLS/Python$PY_VERSION/python `cygpath -d $WXWIN/wxPython/distrib/create_setup.h.py` $UNICODE_FLAG
   
-  export PATH=${PATH}:${WXWIN}/lib/vc_dll
+  export PATH=${PATH}:${WXWIN}/lib/vc_dll:${TOOLS}/Python${PY_VERSION}
   
   cd $WXWIN/build/msw
   # remove old build files
   rm -rf vc_msw*
   UNI=
-  if [ "$UNICODE" != "" ]; then
+  if [ $unicode = yes ]; then
       UNI=-uni
   fi
   ./.make hybrid$UNI
@@ -44,47 +63,51 @@ if [ "$OSTYPE" = "cygwin" ]; then
   cd $WXWIN/locale
   make allmo
   
-  # TODO: Make the documentation
-  cd $WXWIN/wxPython
-  #distrib/makedocs
+  $TOOLS/Python$PY_VERSION/python `cygpath -d $WXWIN/wxPython/distrib/makemo.py`
 
-  $TOOLS/Python$PY_VERSION/python `cygpath -d distrib/makemo.py`
+  cd $WXWIN/wxPython
 
   rm -rf build build.unicode
   rm -rf wx/*.pyd
   
   # re-generate SWIG files
-  b $PY_VERSION t
+  $WXWIN/wxPython/b $PY_VERSION t
   
   # build the hybrid extension
   # NOTE: Win Python needs Windows-style pathnames, so we 
   # need to convert
-  export WXWIN=`cygpath -d $WXWIN`
-  export SWIGDIR=`cygpath -d $SWIGDIR`
+  export WXWIN=`cygpath -w $WXWIN`
+  export SWIGDIR=`cygpath -w $SWIGDIR`
   
-  DEBUG_FLAG=
-  UNICODE_FLAG=
-  if [ "$DEBUG" != "" ]; then
-    DEBUG_FLAG=--debug
-  fi
-  if [ "$UNICODE" != "" ]; then
-    UNICODE_FLAG="UNICODE=1"
-  fi
-  b $PY_VERSION h $DEBUG_FLAG $UNICODE_FLAG
+  $WXWIN/wxPython/b $PY_VERSION h $DEBUG_FLAG $UNICODE_FLAG
   
   # make the dev package
-  #distrib/makedev
+  $WXWIN/wxPython/distrib/makedev
   
-  $TOOLS/Python$PY_VERSION/python distrib/make_installer_inno4.py $UNICODE_FLAG
-  exit
+  $TOOLS/Python$PY_VERSION/python `cygpath -d $WXWIN/wxPython/distrib/make_installer_inno4.py` $UNICODE_FLAG
 elif [ "$OSTYPE" = "darwin" ]; then
   cd $WXWIN/wxPython
   
   # re-generate SWIG files
-  ./b $PY_VERSION t
+  $WXWIN/wxPython/b $PY_VERSION t
   
-  sudo distrib/mac/wxPythonOSX/build 2.3 panther inplace
-  exit
+  PY_DOT_VER=2.3
+  if [ "$PY_VERSION" = "24" ]; then
+    PY_DOT_VER=2.4
+  fi
+  
+  UNICODE_OPT=
+  if [ $unicode = yes ]; then
+    UNICODE_OPT=unicode
+  fi 
+  
+  sudo distrib/mac/wxPythonOSX/build $PY_DOT_VER panther inplace $UNICODE_OPT
 else
   echo "OSTYPE $OSTYPE not yet supported by this build script."
 fi
+
+# Now make the demo and docs tarballs
+cd $WXWIN/wxPython
+$WXWIN/wxPython/distrib/makedocs
+$WXWIN/wxPython/distrib/makedemo
+
