b4a12e485bd5baad8e14ec963b75586d2ee745c7
Michael Wetherell <mike.wetherell@ntlworld.com>
2005-11-03 00:40:45 +0000

    Remove bufferoverflowu.lib since it's not present with some versions of VC++ 8
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36066 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/libraries.h
+++ include/wx/msw/libraries.h
@@ -17,21 +17,4 @@
 // can be used where libs must be added conditionally, for those compilers that
 // support a way to do that.
 
-// Newer versions of VC++ support the /GS option which add buffer overflow
-// checks. This adds into the generated code calls to support functions in in
-// the runtime library.
-//
-// Starting with VC++ 8 this option is on by default, and code such as the
-// runtime library that we need to link is compiled with it. Some versions of
-// the runtime library don't include the support code, and a separate library
-// bufferoverflowu.lib must be linked. If all versions of VC++ 8 that can
-// compile wxWidgets have this lib we can link it here, if not then this will
-// have to be removed.
-//
-// More information: http://support.microsoft.com/?id=894573
-
-#if defined __VISUALC__ && __VISUALC__ >= 1400
-    #pragma comment(lib,"bufferoverflowu.lib")
-#endif
-
 #endif // _WX_MSW_LIBRARIES_H_
