2f1834d9f272e29a87303c66495a9ed80a392e38
Michael Wetherell <mike.wetherell@ntlworld.com>
2005-11-29 17:48:10 +0000

    Remove pragma interface
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36287 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/xrc/xh_grid.h
+++ include/wx/xrc/xh_grid.h
@@ -11,10 +11,6 @@
 #ifndef _WX_XH_GRD_H_
 #define _WX_XH_GRD_H_
 
-#if defined(__GNUG__) && !defined(__APPLE__)
-#pragma interface "xh_grid.cpp"
-#endif
-
 #include "wx/xrc/xmlres.h"
 
 #if wxUSE_XRC && wxUSE_GRID
