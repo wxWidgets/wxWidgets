1fbfc5edf6a94d8ed0e2e73290f75090f68d34ae
Michael Wetherell <mike.wetherell@ntlworld.com>
2005-12-14 17:03:47 +0000

    Compile Fix
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36389 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/ipc/client.h
+++ samples/ipc/client.h
@@ -87,7 +87,7 @@ public:
     virtual bool OnAdvise(const wxString& topic, const wxString& item, wxChar *data, int size, wxIPCFormat format);
     virtual bool OnDisconnect();
 protected:    
-    void MyConnection::Log(const wxString& command, const wxString& topic,
+    void Log(const wxString& command, const wxString& topic,
         const wxString& item, wxChar *data, int size, wxIPCFormat format);
 };
 
