b26e7b714d281fb5084a4b8865d36a4f69a08ed4
Julian Smart <julian@anthemion.co.uk>
2006-01-02 14:28:37 +0000

    Removed no longer needed code to fix combo sizing (causes layout problems on Windows)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36624 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/dirctrlg.cpp
+++ src/generic/dirctrlg.cpp
@@ -1190,22 +1190,6 @@ void wxGenericDirCtrl::DoResize()
         wxSize filterSz ;
         if (m_filterListCtrl)
         {
-#ifdef __WXMSW__
-            // For some reason, this is required in order for the
-            // correct control height to always be returned, rather
-            // than the drop-down list height which is sometimes returned.
-            wxSize oldSize = m_filterListCtrl->GetSize();
-            m_filterListCtrl->SetSize(wxDefaultCoord,
-                                      wxDefaultCoord,
-                                      oldSize.x+10,
-                                      wxDefaultCoord,
-                                      wxSIZE_USE_EXISTING);
-            m_filterListCtrl->SetSize(wxDefaultCoord,
-                                      wxDefaultCoord,
-                                      oldSize.x,
-                                      wxDefaultCoord,
-                                      wxSIZE_USE_EXISTING);
-#endif
             filterSz = m_filterListCtrl->GetSize();
             sz.y -= (filterSz.y + verticalSpacing);
         }
