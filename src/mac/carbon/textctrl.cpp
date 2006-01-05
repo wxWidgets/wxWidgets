81919a1e15246ca6cfc8fed1bfffac22a332ecee
Kevin Ollivier <kevino@theolliviers.com>
2006-01-05 06:46:19 +0000

    Comment out code that causes serious drawing issues under OS X 10.4. (Do we even need to show a grow icon when the text ctrl is not at the bottom right of the screen?)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@36698 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/textctrl.cpp
+++ src/mac/carbon/textctrl.cpp
@@ -1596,8 +1596,11 @@ TXNFrameOptions wxMacMLTEControl::FrameOptionsFromWXStyle( long wxStyle )
         if ( !(wxStyle & wxTE_NO_VSCROLL ) )
         {
             frameOptions |= kTXNWantVScrollBarMask ;
-            if ( frameOptions & kTXNWantHScrollBarMask )
-                frameOptions |= kTXNDrawGrowIconMask ;
+            // The following code causes drawing problems on 10.4. Perhaps it can be restored for
+            // older versions of the OS, but I'm not sure it's appropriate to put a grow icon here
+            // anyways, as AFAIK users can't actually use it to resize the text ctrl.
+            //if ( frameOptions & kTXNWantHScrollBarMask )
+            //    frameOptions |= kTXNDrawGrowIconMask ;
         }
     }
     else
