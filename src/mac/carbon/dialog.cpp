570586d2c8c0cf9cf76ff0f5ce805b7185ebec3e
Robin Dunn <robin@alldunn.com>
2006-01-05 18:26:25 +0000

    backport of "transparent grow box for dialogs" change from HEAD
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@36709 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/dialog.cpp
+++ src/mac/carbon/dialog.cpp
@@ -63,6 +63,11 @@ bool wxDialog::Create(wxWindow *parent, wxWindowID id,
     if ( !wxTopLevelWindow::Create(parent, id, title, pos, size, style & ~(wxYES|wxOK|wxNO /*|wxCANCEL*/) , name) )
         return FALSE;
 
+    HIViewRef growBoxRef = 0 ;
+    OSStatus err = HIViewFindByID( HIViewGetRoot( (WindowRef) m_macWindow ) , kHIViewWindowGrowBoxID , &growBoxRef );
+    if ( err == noErr && growBoxRef != 0 )
+        HIGrowBoxViewSetTransparent( growBoxRef , true ) ;
+    
     return TRUE;
 }
 
