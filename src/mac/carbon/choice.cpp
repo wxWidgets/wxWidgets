d34cca534cde8acf4406c0276ad6b5f76e7b1ce7
David Surovell <davids@osafoundation.org>
2006-01-06 03:17:29 +0000

    cleanup - reformat; added assignment of initial selection to match other platform behavior
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36723 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/choice.cpp
+++ src/mac/carbon/choice.cpp
@@ -17,9 +17,10 @@
 #include "wx/menu.h"
 #include "wx/mac/uma.h"
 
+extern MenuHandle NewUniqueMenu() ;
+
 IMPLEMENT_DYNAMIC_CLASS(wxChoice, wxControl)
 
-extern MenuHandle NewUniqueMenu() ;
 
 wxChoice::~wxChoice()
 {
@@ -28,71 +29,82 @@ wxChoice::~wxChoice()
         size_t i, max = GetCount();
 
         for ( i = 0; i < max; ++i )
-            delete GetClientObject(i);
+            delete GetClientObject( i );
     }
 
-        // DeleteMenu( m_macPopUpMenuId ) ;
+    // DeleteMenu( m_macPopUpMenuId ) ;
     // DisposeMenu( m_macPopUpMenuHandle ) ;
 }
 
-bool wxChoice::Create(wxWindow *parent, wxWindowID id,
-           const wxPoint& pos,
-           const wxSize& size,
-           const wxArrayString& choices,
-           long style,
-           const wxValidator& validator,
-           const wxString& name)
+bool wxChoice::Create(wxWindow *parent,
+    wxWindowID id,
+    const wxPoint& pos,
+    const wxSize& size,
+    const wxArrayString& choices,
+    long style,
+    const wxValidator& validator,
+    const wxString& name )
 {
-    wxCArrayString chs(choices);
+    wxCArrayString chs( choices );
 
-    return Create(parent, id, pos, size, chs.GetCount(), chs.GetStrings(),
-                  style, validator, name);
+    return Create(
+        parent, id, pos, size, chs.GetCount(), chs.GetStrings(),
+        style, validator, name );
 }
 
-bool wxChoice::Create(wxWindow *parent, wxWindowID id,
-           const wxPoint& pos,
-           const wxSize& size,
-           int n, const wxString choices[],
-           long style,
-           const wxValidator& validator,
-           const wxString& name)
+bool wxChoice::Create(wxWindow *parent,
+    wxWindowID id,
+    const wxPoint& pos,
+    const wxSize& size,
+    int n,
+    const wxString choices[],
+    long style,
+    const wxValidator& validator,
+    const wxString& name )
 {
-    m_macIsUserPane = false ;
+    m_macIsUserPane = false;
 
-    if ( !wxChoiceBase::Create(parent, id, pos, size, style, validator, name) )
+    if ( !wxChoiceBase::Create( parent, id, pos, size, style, validator, name ) )
         return false;
 
-    Rect bounds = wxMacGetBoundsForControl( this , pos , size ) ;
+    Rect bounds = wxMacGetBoundsForControl( this , pos , size );
 
-    m_peer = new wxMacControl(this) ;
-    verify_noerr ( CreatePopupButtonControl( MAC_WXHWND(parent->MacGetTopLevelWindowRef()) , &bounds , CFSTR("") ,
-        -12345 , false /* no variable width */ , 0 , 0 , 0 , m_peer->GetControlRefAddr() ) );
+    m_peer = new wxMacControl( this ) ;
+    OSStatus err = CreatePopupButtonControl(
+        MAC_WXHWND(parent->MacGetTopLevelWindowRef()) , &bounds , CFSTR("") ,
+        -12345 , false /* no variable width */ , 0 , 0 , 0 , m_peer->GetControlRefAddr() );
+    verify_noerr( err );
 
-
-    m_macPopUpMenuHandle =  NewUniqueMenu() ;
+    m_macPopUpMenuHandle = NewUniqueMenu() ;
     m_peer->SetData<MenuHandle>( kControlNoPart , kControlPopupButtonMenuHandleTag , (MenuHandle) m_macPopUpMenuHandle ) ;
-    m_peer->SetValueAndRange( n > 0 ? 1 : 0 , 0 , 0 ) ;
-    MacPostControlCreate(pos,size) ;
+    m_peer->SetValueAndRange( n > 0 ? 1 : 0 , 0 , 0 );
+    MacPostControlCreate( pos, size );
 
 #if !wxUSE_STL
     if ( style & wxCB_SORT )
-    {
-        m_strings = wxArrayString(1) ; // autosort
-    }
+        // autosort
+        m_strings = wxArrayString( 1 );
 #endif
 
     for ( int i = 0; i < n; i++ )
     {
-        Append(choices[i]);
+        Append( choices[i] );
     }
-    SetBestSize(size);   // Needed because it is a wxControlWithItems
+
+    // Set the first item as being selected
+    if (n > 0)
+        SetSelection( 0 );
+
+    // Needed because it is a wxControlWithItems
+    SetBestSize( size );
+
     return true;
 }
 
 // ----------------------------------------------------------------------------
 // adding/deleting items to/from the list
 // ----------------------------------------------------------------------------
-int wxChoice::DoAppend(const wxString& item)
+int wxChoice::DoAppend( const wxString& item )
 {
 #if wxUSE_STL
     wxArrayString::iterator insertPoint;
@@ -111,39 +123,42 @@ int wxChoice::DoAppend(const wxString& item)
 
     m_strings.insert( insertPoint, item );
 #else
-    size_t index = m_strings.Add( item ) ;
+    size_t index = m_strings.Add( item );
 #endif
-    m_datas.Insert( NULL , index ) ;
-    UMAInsertMenuItem(MAC_WXHMENU( m_macPopUpMenuHandle ) , item, m_font.GetEncoding() , index );
-    DoSetItemClientData( index , NULL ) ;
-    m_peer->SetMaximum( GetCount() ) ;
-    return index ;
+
+    m_datas.Insert( NULL , index );
+    UMAInsertMenuItem( MAC_WXHMENU( m_macPopUpMenuHandle ), item, m_font.GetEncoding(), index );
+    DoSetItemClientData( index, NULL );
+    m_peer->SetMaximum( GetCount() );
+
+    return index;
 }
 
-int wxChoice::DoInsert(const wxString& item, int pos)
+int wxChoice::DoInsert( const wxString& item, int pos )
 {
-    wxCHECK_MSG(!(GetWindowStyle() & wxCB_SORT), -1, wxT("can't insert into sorted list"));
-    wxCHECK_MSG((pos>=0) && (pos<=GetCount()), -1, wxT("invalid index"));
+    wxCHECK_MSG( !(GetWindowStyle() & wxCB_SORT), -1, wxT("wxChoice::DoInsert: can't insert into sorted list") );
+    wxCHECK_MSG( (pos >= 0) && (pos <= GetCount()), -1, wxT("wxChoice::DoInsert: invalid index") );
 
     if (pos == GetCount())
-        return DoAppend(item);
+        return DoAppend( item );
 
-    UMAInsertMenuItem(MAC_WXHMENU( m_macPopUpMenuHandle ) , item, m_font.GetEncoding() , pos );
-    m_strings.Insert( item, pos ) ;
-    m_datas.Insert( NULL, pos ) ;
-    DoSetItemClientData( pos , NULL ) ;
-    m_peer->SetMaximum( GetCount() ) ;
-    return pos ;
+    UMAInsertMenuItem( MAC_WXHMENU( m_macPopUpMenuHandle ), item, m_font.GetEncoding(), pos );
+    m_strings.Insert( item, pos );
+    m_datas.Insert( NULL, pos );
+    DoSetItemClientData( pos, NULL );
+    m_peer->SetMaximum( GetCount() );
+
+    return pos;
 }
 
-void wxChoice::Delete(int n)
+void wxChoice::Delete( int n )
 {
-    wxCHECK_RET( n < GetCount(), wxT("invalid item index in wxChoice::Delete") );
+    wxCHECK_RET( n < GetCount(), wxT("wxChoice::Delete: invalid index") );
+
     if ( HasClientObjectData() )
-    {
-        delete GetClientObject(n);
-    }
-    ::DeleteMenuItem( MAC_WXHMENU(m_macPopUpMenuHandle) , n + 1) ;
+        delete GetClientObject( n );
+
+    ::DeleteMenuItem( MAC_WXHMENU(m_macPopUpMenuHandle) , n + 1 ) ;
     m_strings.RemoveAt( n ) ;
     m_datas.RemoveAt( n ) ;
     m_peer->SetMaximum( GetCount() ) ;
@@ -156,6 +171,7 @@ void wxChoice::Clear()
     {
         ::DeleteMenuItem( MAC_WXHMENU(m_macPopUpMenuHandle) , 1 ) ;
     }
+
     m_strings.Empty() ;
     m_datas.Empty() ;
     m_peer->SetMaximum( 0 ) ;
@@ -168,7 +184,7 @@ void wxChoice::FreeData()
         size_t count = GetCount();
         for ( size_t n = 0; n < count; n++ )
         {
-            delete GetClientObject(n);
+            delete GetClientObject( n );
         }
     }
 }
@@ -178,10 +194,10 @@ void wxChoice::FreeData()
 // ----------------------------------------------------------------------------
 int wxChoice::GetSelection() const
 {
-    return m_peer->GetValue() -1 ;
+    return m_peer->GetValue() - 1 ;
 }
 
-void wxChoice::SetSelection(int n)
+void wxChoice::SetSelection( int n )
 {
     m_peer->SetValue( n + 1 ) ;
 }
@@ -195,22 +211,26 @@ int wxChoice::GetCount() const
     return m_strings.GetCount() ;
 }
 
-int wxChoice::FindString(const wxString& s, bool bCase ) const
+int wxChoice::FindString( const wxString& s, bool bCase ) const
 {
     return m_strings.Index( s , bCase ) ;
 }
 
-void wxChoice::SetString(int n, const wxString& s)
+void wxChoice::SetString( int n, const wxString& s )
 {
+    wxCHECK_RET( n >= 0 && (size_t)n < m_strings.GetCount(),
+                    wxT("wxChoice::SetString(): invalid index") );
+
     m_strings[n] = s ;
+
     // apple menu pos is 1-based
     UMASetMenuItemText( MAC_WXHMENU(m_macPopUpMenuHandle) , n + 1 , s , wxFont::GetDefaultEncoding() ) ;
 }
 
-wxString wxChoice::GetString(int n) const
+wxString wxChoice::GetString( int n ) const
 {
     wxCHECK_MSG( n >= 0 && (size_t)n < m_strings.GetCount(), wxEmptyString,
-                    _T("wxChoice::GetString(): invalid index") );
+                    wxT("wxChoice::GetString(): invalid index") );
 
     return m_strings[n] ;
 }
@@ -221,44 +241,49 @@ wxString wxChoice::GetString(int n) const
 void wxChoice::DoSetItemClientData( int n, void* clientData )
 {
     wxCHECK_RET( n >= 0 && (size_t)n < m_datas.GetCount(),
-                 wxT("invalid index in wxChoice::SetClientData") );
+                 wxT("wxChoice::DoSetItemClientData: invalid index") );
 
-    m_datas[n] = (char*) clientData ;
+    m_datas[n] = (char*)clientData ;
 }
 
-void *wxChoice::DoGetItemClientData(int n) const
+void * wxChoice::DoGetItemClientData( int n ) const
 {
     wxCHECK_MSG( n >= 0 && (size_t)n < m_datas.GetCount(), NULL,
-                 wxT("invalid index in wxChoice::GetClientData") );
+                 wxT("wxChoice::DoGetClientData: invalid index") );
+
     return (void *)m_datas[n];
 }
 
 void wxChoice::DoSetItemClientObject( int n, wxClientData* clientData )
 {
-    DoSetItemClientData(n, clientData);
+    DoSetItemClientData( n, clientData ) ;
 }
 
 wxClientData* wxChoice::DoGetItemClientObject( int n ) const
 {
-    return (wxClientData *)DoGetItemClientData(n);
+    return (wxClientData*)DoGetItemClientData( n ) ;
 }
 
-wxInt32 wxChoice::MacControlHit(WXEVENTHANDLERREF WXUNUSED(handler) , WXEVENTREF WXUNUSED(event) )
+wxInt32 wxChoice::MacControlHit( WXEVENTHANDLERREF WXUNUSED(handler) , WXEVENTREF WXUNUSED(event) )
 {
-    wxCommandEvent event(wxEVT_COMMAND_CHOICE_SELECTED, m_windowId );
-    int n = GetSelection();
+    wxCommandEvent event( wxEVT_COMMAND_CHOICE_SELECTED, m_windowId );
+
     // actually n should be made sure by the os to be a valid selection, but ...
+    int n = GetSelection();
     if ( n > -1 )
     {
         event.SetInt( n );
-        event.SetString(GetStringSelection());
-        event.SetEventObject(this);
+        event.SetString( GetStringSelection() );
+        event.SetEventObject( this );
+
         if ( HasClientObjectData() )
-            event.SetClientObject( GetClientObject(n) );
+            event.SetClientObject( GetClientObject( n ) );
         else if ( HasClientUntypedData() )
-            event.SetClientData( GetClientData(n) );
-        ProcessCommand(event);
+            event.SetClientData( GetClientData( n ) );
+
+        ProcessCommand( event );
     }
+
     return noErr ;
 }
 
@@ -267,11 +292,14 @@ wxSize wxChoice::DoGetBestSize() const
     int lbWidth = GetCount() > 0 ? 20 : 100;  // some defaults
     int lbHeight = 20;
     int wLine;
+
 #if TARGET_CARBON
     long metric ;
-    GetThemeMetric(kThemeMetricPopupButtonHeight , &metric );
+
+    GetThemeMetric( kThemeMetricPopupButtonHeight , &metric );
     lbHeight = metric ;
 #endif
+
     {
         wxMacPortStateHelper st( UMAGetWindowPort( (WindowRef) MacGetTopLevelWindowRef() ) ) ;
         if ( m_font.Ok() )
@@ -283,35 +311,43 @@ wxSize wxChoice::DoGetBestSize() const
         else
         {
             ::TextFont( kFontIDMonaco ) ;
-            ::TextSize( 9  );
+            ::TextSize( 9 ) ;
             ::TextFace( 0 ) ;
         }
+
         // Find the widest line
-        for(int i = 0; i < GetCount(); i++) {
-            wxString str(GetString(i));
-        #if wxUSE_UNICODE
-            Point bounds={0,0} ;
+        for(int i = 0; i < GetCount(); i++)
+        {
+            wxString str( GetString( i ) );
+
+#if wxUSE_UNICODE
+            Point bounds = { 0, 0 } ;
             SInt16 baseline ;
+
             ::GetThemeTextDimensions( wxMacCFStringHolder( str , m_font.GetEncoding() ) ,
                 kThemeCurrentPortFont,
                 kThemeStateActive,
                 false,
                 &bounds,
                 &baseline );
+
             wLine = bounds.h ;
-        #else
+#else
             wLine = ::TextWidth( str.c_str() , 0 , str.Length() ) ;
-        #endif
-            lbWidth = wxMax(lbWidth, wLine);
+#endif
+
+            lbWidth = wxMax( lbWidth, wLine ) ;
         }
+
         // Add room for the popup arrow
         lbWidth += 2 * lbHeight ;
+
         // And just a bit more
         int cx = ::TextWidth( "X" , 0 , 1 ) ;
         lbWidth += cx ;
-
     }
-    return wxSize(lbWidth, lbHeight);
+
+    return wxSize( lbWidth, lbHeight );
 }
 
 #endif // wxUSE_CHOICE
