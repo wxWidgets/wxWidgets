1feb5443ff11ea45e9f21b1ea9a6f58caf2b6b4f
Jamie Gadd <jrgadd2@cs.latrobe.edu.au>
2006-01-08 15:33:30 +0000

    Return correct values for GetFieldRect() when using themes.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36779 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/statbr95.cpp
+++ src/msw/statbr95.cpp
@@ -35,6 +35,10 @@
 // include <commctrl.h> "properly"
 #include "wx/msw/wrapcctl.h"
 
+#if wxUSE_UXTHEME
+    #include "wx/msw/uxtheme.h"
+#endif
+
 // ----------------------------------------------------------------------------
 // macros
 // ----------------------------------------------------------------------------
@@ -266,6 +270,23 @@ bool wxStatusBar95::GetFieldRect(int i, wxRect& rect) const
     {
         wxLogLastError(wxT("SendMessage(SB_GETRECT)"));
     }
+
+#if wxUSE_UXTHEME
+    wxUxThemeHandle theme((wxStatusBar95 *)this, L"Status"); // const_cast
+    if ( theme )
+    {
+        // by default Windows has a 2 pixel border to the right of the left
+        // divider (or it could be a bug) but it looks wrong so remove it
+        if ( i != 0 )
+        {
+            r.left -= 2;
+        }
+
+        wxUxThemeEngine::Get()->GetThemeBackgroundContentRect(theme, NULL,
+                                                              1 /* SP_PANE */, 0,
+                                                              &r, &r);
+    }
+#endif
 
     wxCopyRECTToRect(r, rect);
 
