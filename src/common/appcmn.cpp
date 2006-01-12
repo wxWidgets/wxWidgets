71e86b08be014102efd9029e70920ca1180e2778
Vadim Zeitlin <vadim@wxwidgets.org>
2006-01-12 23:10:29 +0000

    wxEventLoop::ms_activeLoop is now defined in its own file
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36849 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/appcmn.cpp
+++ src/common/appcmn.cpp
@@ -63,9 +63,6 @@ WX_CHECK_BUILD_OPTIONS("wxCore")
 // this defines wxEventLoopPtr
 wxDEFINE_TIED_SCOPED_PTR_TYPE(wxEventLoop)
 
-// define it here as we don't have common/evtloopcmn.cpp for now
-wxEventLoop *wxEventLoopBase::ms_activeLoop = NULL;
-
 // ============================================================================
 // wxAppBase implementation
 // ============================================================================
