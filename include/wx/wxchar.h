ac6178d747f7b3c50a61627c65d6a73ea6dcee5b
Julian Smart <julian@anthemion.co.uk>
2006-01-18 12:46:11 +0000

    Applied patch [ 1381895 ] remove circular include dependendcy in wxchar.h
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@36962 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/wxchar.h
+++ include/wx/wxchar.h
@@ -14,7 +14,9 @@
 #ifndef _WX_WXCHAR_H_
 #define _WX_WXCHAR_H_
 
-#include "wx/defs.h"        /* for wxUSE_UNICODE */
+/* defs.h indirectly includes this file, so don't include it here */
+#include "wx/platform.h"
+#include "wx/dlimpexp.h"
 
 #if defined(HAVE_STRTOK_R) && defined(__DARWIN__) && defined(_MSL_USING_MW_C_HEADERS) && _MSL_USING_MW_C_HEADERS
     char *strtok_r(char *, const char *, char **);
@@ -48,7 +50,7 @@
 /*
     Standard headers we need here.
 
-    NB: don't include any wxWidgets headers here because almost of them include
+    NB: don't include any wxWidgets headers here because almost all of them include
         this one!
  */
 
