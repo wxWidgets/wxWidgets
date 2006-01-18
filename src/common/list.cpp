7d78f6bda205354fdcfc566f501e9a9d1eebcdd1
Julian Smart <julian@anthemion.co.uk>
2006-01-18 11:58:00 +0000

    Applied patch [ 1386163 ] misquoted #include (CVS 2.6) Jose Cruanyes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@36957 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/list.cpp
+++ src/common/list.cpp
@@ -752,7 +752,7 @@ wxNode *wxStringList::Prepend(const wxChar *s)
 
 #else // wxUSE_STL = 1
 
-    #include <wx/listimpl.cpp>
+    #include "wx/listimpl.cpp"
     WX_DEFINE_LIST(wxObjectList);
 
 // with wxUSE_STL wxStringList contains wxString objects, not pointers
