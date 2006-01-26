8498a2858b50bb96947f367b495835793107e5cc
Włodzimierz Skiba <abx@abx.art.pl>
2006-01-26 17:00:19 +0000

    Include order is wxprec.h=>defs.h=>platform.h=>setup.h so remove explicit setup.h inclusion not touched by chckconf.h.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37163 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/cocoa/utils.cpp
+++ src/cocoa/utils.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        utils.cpp
+// Name:        src/cocoa/utils.cpp
 // Purpose:     Various utilities
 // Author:      AUTHOR
 // Modified by:
@@ -9,7 +9,8 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
-#include "wx/setup.h"
+#include "wx/wxprec.h"
+
 #include "wx/utils.h"
 #include "wx/app.h"
 #include "wx/apptrait.h"
