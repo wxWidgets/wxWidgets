065fa73213c49c01489cf6c44f788c7486cf4f8d
Robert Roebling <robert@roebling.de>
2006-01-26 12:28:19 +0000

    GTK2 RGBA has 32 bits.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37153 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/rawbmp.h
+++ include/wx/rawbmp.h
@@ -165,7 +165,7 @@ typedef wxPixelFormat<unsigned char, 24, 0, 1, 2> wxImagePixelFormat;
     #define wxPIXEL_FORMAT_ALPHA 3
 #elif defined(__WXGTK__)
     // Under GTK+ 2.X we use GdkPixbuf, which should be RGBA
-    typedef wxPixelFormat<unsigned char, 24, 0, 1, 2> wxNativePixelFormat;
+    typedef wxPixelFormat<unsigned char, 32, 0, 1, 2> wxNativePixelFormat;
 
     #define wxPIXEL_FORMAT_ALPHA 3
 #endif
