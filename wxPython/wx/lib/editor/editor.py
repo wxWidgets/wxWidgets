bf56f4ece08443e35a810916a59775e4e574dc8e
Robin Dunn <robin@alldunn.com>
2006-01-29 02:36:29 +0000

    Ensure the window is still valid before adjusting the scrollbars
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37207 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wx/lib/editor/editor.py
+++ wxPython/wx/lib/editor/editor.py
@@ -552,12 +552,13 @@ class Editor(wx.ScrolledWindow):
 
 
     def AdjustScrollbars(self):
-        for i in range(2):
-            self.SetCharDimensions()
-            self.scroller.SetScrollbars(
-                self.fw, self.fh,
-                self.CalcMaxLineLen()+3, max(self.LinesInFile()+1, self.sh),
-                self.sx, self.sy)
+        if self:
+            for i in range(2):
+                self.SetCharDimensions()
+                self.scroller.SetScrollbars(
+                    self.fw, self.fh,
+                    self.CalcMaxLineLen()+3, max(self.LinesInFile()+1, self.sh),
+                    self.sx, self.sy)
 
 #------------ backspace, delete, return
 
