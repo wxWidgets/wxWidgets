0857d1d65963341bede59588b84e996c6c3d0c36
Mart Raudsepp <leio@gentoo.org>
2006-02-04 02:10:53 +0000

    GTK2: Add a GTK_WIDGET_DRAWABLE check to the gtk_widget_draw replacement to make it call identical worker functions as before, and not spew warnings
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37299 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/frame.cpp
+++ src/gtk/frame.cpp
@@ -490,9 +490,12 @@ void wxFrame::GtkOnSize( int WXUNUSED(x), int WXUNUSED(y),
         gtk_pizza_set_size( GTK_PIZZA(m_wxwindow),
                             m_frameStatusBar->m_widget,
                             xx, yy, ww, hh );
-        gtk_widget_queue_draw (m_frameStatusBar->m_widget);
-        // FIXME: Do we really want to force an immediate redraw?
-        gdk_window_process_updates (m_frameStatusBar->m_widget->window, TRUE);
+        if (GTK_WIDGET_DRAWABLE (m_frameStatusBar->m_widget))
+        {
+            gtk_widget_queue_draw (m_frameStatusBar->m_widget);
+            // FIXME: Do we really want to force an immediate redraw?
+            gdk_window_process_updates (m_frameStatusBar->m_widget->window, TRUE);
+        }
     }
 #endif // wxUSE_STATUSBAR
 
