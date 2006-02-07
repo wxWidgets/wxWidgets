9b26622dc12128c13790c608571d5f9bfa2d892a
David Surovell <davids@osafoundation.org>
2006-02-07 21:54:48 +0000

    fixed DoDrawRoundedRect to use corner rounding radius arguments
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37378 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/dccg.cpp
+++ src/mac/carbon/dccg.cpp
@@ -1584,7 +1584,7 @@ void wxDC::DoDrawRoundedRectangle(wxCoord x, wxCoord y,
 
     wxMacCGContext* mctx = ((wxMacCGContext*) m_graphicContext) ;
     CGContextRef ctx = mctx->GetNativeContext() ;
-    AddRoundedRectToPath( ctx , CGRectMake( xx , yy , ww , hh ) , 16 ,16 ) ;
+    AddRoundedRectToPath( ctx , CGRectMake( xx , yy , ww , hh ) , radius , radius ) ;
     CGContextDrawPath( ctx , mctx->GetDrawingMode() ) ;
 }
 
