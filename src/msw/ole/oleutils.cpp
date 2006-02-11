812aed5deb0e4568efccd0ceee27cdc881d44fc8
Julian Smart <julian@anthemion.co.uk>
2006-02-11 09:19:26 +0000

    Added WXDLLEXPORT keywords
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@37483 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/ole/oleutils.cpp
+++ src/msw/ole/oleutils.cpp
@@ -71,7 +71,7 @@ bool IsIidFromList(REFIID riid, const IID *aIids[], size_t nCount)
   return false;
 }
 
-BSTR wxConvertStringToOle(const wxString& str)
+WXDLLEXPORT BSTR wxConvertStringToOle(const wxString& str)
 {
 /*
     unsigned int len = strlen((const char*) str);
@@ -85,7 +85,7 @@ BSTR wxConvertStringToOle(const wxString& str)
     return bstr.Get();
 }
 
-wxString wxConvertStringFromOle(BSTR bStr)
+WXDLLEXPORT wxString wxConvertStringFromOle(BSTR bStr)
 {
 #if wxUSE_UNICODE
     wxString str(bStr);
