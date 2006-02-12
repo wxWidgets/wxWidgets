7e81aa035ccbe28af54822e40607779d57d1b004
Stefan Csomor <csomor@advancedconcepts.ch>
2006-02-12 09:00:44 +0000

    intel fixes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37520 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/cursor.cpp
+++ src/mac/carbon/cursor.cpp
@@ -195,6 +195,13 @@ CursHandle wxGetStockCursor( int number )
     CursHandle c = (CursHandle) NewHandle( sizeof(Cursor) ) ;
     memcpy( *c, &gMacCursors[number], sizeof(Cursor) ) ;
 
+#ifndef WORDS_BIGENDIAN
+    short *sptr = (short*) *c ;
+    for ( int i = 0 ; i < 2 * 16 /* image and mask */ ; ++i, ++sptr )
+    {
+        *sptr = CFSwapInt16( *sptr ) ;
+    }
+#endif
     return c ;
 }
 
@@ -418,9 +425,13 @@ void wxCursor::CreateFromImage(const wxImage & image)
             *((*(**ch).crsrData) + y * bytesPerRow + x) =
                 GetCTabIndex( newColors , &col) ;
         }
-
+#ifdef WORDS_BIGENDIAN
         (**ch).crsr1Data[y] = rowbits ;
         (**ch).crsrMask[y] = maskbits ;
+#else
+        (**ch).crsr1Data[y] = CFSwapInt16(rowbits) ;
+        (**ch).crsrMask[y] = CFSwapInt16(maskbits) ;
+#endif
     }
 
     if ( !bHasMask )
