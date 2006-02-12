0ec80ebe6f64c51ac16de064b462427899e37303
David Surovell <davids@osafoundation.org>
2006-02-12 23:37:07 +0000

    cleanup - reformatting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37541 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/timer.cpp
+++ src/mac/carbon/timer.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        timer.cpp
+// Name:        sec/mac/carbon/timer.cpp
 // Purpose:     wxTimer implementation
 // Author:      Stefan Csomor
 // Modified by:
@@ -13,16 +13,17 @@
 
 #include "wx/timer.h"
 
-IMPLEMENT_ABSTRACT_CLASS(wxTimer, wxEvtHandler)
-
 #ifdef __WXMAC__
 #include "wx/mac/private.h"
 #endif
+
+#include "wx/dynarray.h"
+
 #ifndef __DARWIN__
 #include <Timer.h>
 #endif
 
-#include "wx/dynarray.h"
+IMPLEMENT_ABSTRACT_CLASS(wxTimer, wxEvtHandler)
 
 #define wxMAC_USE_CARBON_TIMER 1
 
@@ -30,74 +31,79 @@ IMPLEMENT_ABSTRACT_CLASS(wxTimer, wxEvtHandler)
 
 struct MacTimerInfo
 {
-    wxTimer* m_timer ;
-    EventLoopTimerUPP m_proc ;
-    EventLoopTimerRef   m_timerRef ;
-} ;
+    wxTimer* m_timer;
+    EventLoopTimerUPP m_proc;
+    EventLoopTimerRef   m_timerRef;
+};
 
-static pascal void wxProcessTimer( EventLoopTimerRef  theTimer , void *data ) ;
-static pascal void wxProcessTimer( EventLoopTimerRef theTimer , void *data )
+static pascal void wxProcessTimer( EventLoopTimerRef theTimer, void *data );
+static pascal void wxProcessTimer( EventLoopTimerRef theTimer, void *data )
 {
-    if ( !data )
-        return ;
+    if ( data == NULL )
+        return;
 
-    wxTimer* timer = (wxTimer*) data ;
+    wxTimer* timer = (wxTimer*)data;
 
     if ( timer->IsOneShot() )
-        timer->Stop() ;
+        timer->Stop();
 
     timer->Notify();
 }
 
 void wxTimer::Init()
 {
-    m_info = new MacTimerInfo() ;
-    m_info->m_timer = this ;
-    m_info->m_proc = NULL ;
-    m_info->m_timerRef = kInvalidID ;
+    m_info = new MacTimerInfo();
+    m_info->m_timer = this;
+    m_info->m_proc = NULL;
+    m_info->m_timerRef = kInvalidID;
 }
 
 bool wxTimer::IsRunning() const
 {
-    return ( m_info->m_timerRef != kInvalidID ) ;
+    return ( m_info->m_timerRef != kInvalidID );
 }
 
 wxTimer::~wxTimer()
 {
     Stop();
-    if (m_info != NULL) {
-        delete m_info ;
-        m_info = NULL ;
+    if (m_info != NULL)
+    {
+        delete m_info;
+        m_info = NULL;
     }
 }
 
-bool wxTimer::Start(int milliseconds,bool mode)
+bool wxTimer::Start( int milliseconds, bool mode )
 {
     (void)wxTimerBase::Start(milliseconds, mode);
 
     wxCHECK_MSG( m_milli > 0, false, wxT("invalid value for timer timeout") );
-    wxCHECK_MSG( m_info->m_timerRef == NULL , false, wxT("attempting to restart a timer") );
+    wxCHECK_MSG( m_info->m_timerRef == NULL, false, wxT("attempting to restart a timer") );
 
-    m_info->m_timer = this ;
-    m_info->m_proc = NewEventLoopTimerUPP( &wxProcessTimer);
-    verify_noerr( InstallEventLoopTimer (
-        GetMainEventLoop() ,
+    m_info->m_timer = this;
+    m_info->m_proc = NewEventLoopTimerUPP( &wxProcessTimer );
+
+    OSStatus err = InstallEventLoopTimer(
+        GetMainEventLoop(),
         m_milli*kEventDurationMillisecond,
-        IsOneShot() ? 0 : m_milli*kEventDurationMillisecond ,
+        IsOneShot() ? 0 : m_milli * kEventDurationMillisecond,
         m_info->m_proc,
         this,
-        &m_info->m_timerRef) ) ;
+        &m_info->m_timerRef );
+    verify_noerr( err );
+
     return true;
 }
 
 void wxTimer::Stop()
 {
     if (m_info->m_timerRef)
-        RemoveEventLoopTimer( m_info->m_timerRef ) ;
+        RemoveEventLoopTimer( m_info->m_timerRef );
     if (m_info->m_proc)
-        DisposeEventLoopTimerUPP(m_info->m_proc) ;
-    m_info->m_proc = NULL ;
-    m_info->m_timerRef = kInvalidID ;
+        DisposeEventLoopTimerUPP( m_info->m_proc );
+
+    m_info->m_proc = NULL;
+    m_info->m_timerRef = kInvalidID;
 }
 
 #else
@@ -105,96 +111,93 @@ void wxTimer::Stop()
 typedef struct MacTimerInfo
 {
     TMTask m_task;
-    wxMacNotifierTableRef m_table ;
-    wxTimer* m_timer ;
-} ;
+    wxMacNotifierTableRef m_table;
+    wxTimer* m_timer;
+};
 
-static void wxProcessTimer( unsigned long event , void *data ) ;
+static void wxProcessTimer( unsigned long event, void *data );
 
 static pascal void MacTimerProc( TMTask * t )
 {
-    MacTimerInfo * tm = (MacTimerInfo*)  t ;
-    wxMacAddEvent( tm->m_table , wxProcessTimer, 0 , (void*) tm->m_timer , TRUE ) ;
+    MacTimerInfo * tm = (MacTimerInfo*) t;
+    wxMacAddEvent( tm->m_table, wxProcessTimer, 0, (void*) tm->m_timer, true );
 }
 
 // we need this array to track timers that are being deleted within the Notify procedure
 // adding the timer before the Notify call and checking after whether it still is in there
 // as the destructor would have removed it from the array
 
-wxArrayPtrVoid gTimersInProcess ;
+wxArrayPtrVoid gTimersInProcess;
 
-static void wxProcessTimer( unsigned long event , void *data )
+static void wxProcessTimer( unsigned long event, void *data )
 {
-    if ( !data )
-        return ;
+    if ( data == NULL )
+        return;
 
-    wxTimer* timer = (wxTimer*) data ;
-
+    wxTimer* timer = (wxTimer*) data;
     if ( timer->IsOneShot() )
-        timer->Stop() ;
+        timer->Stop();
 
-    gTimersInProcess.Add( timer ) ;
-
+    gTimersInProcess.Add( timer );
     timer->Notify();
 
-    int index = gTimersInProcess.Index( timer ) ;
-
+    int index = gTimersInProcess.Index( timer );
     if ( index != wxNOT_FOUND )
     {
-        gTimersInProcess.RemoveAt( index ) ;
+        gTimersInProcess.RemoveAt( index );
 
         if ( !timer->IsOneShot() && timer->m_info->m_task.tmAddr )
-        {
-            PrimeTime( (QElemPtr)  &timer->m_info->m_task , timer->GetInterval() ) ;
-        }
-
+            PrimeTime( (QElemPtr) &timer->m_info->m_task, timer->GetInterval() );
     }
 }
 
 void wxTimer::Init()
 {
-    m_info = new MacTimerInfo() ;
-    m_info->m_task.tmAddr = NULL ;
-    m_info->m_task.tmWakeUp = 0 ;
-    m_info->m_task.tmReserved = 0 ;
-    m_info->m_task.qType = 0 ;
-    m_info->m_table = wxMacGetNotifierTable() ;
-    m_info->m_timer = this ;
+    m_info = new MacTimerInfo();
+    m_info->m_task.tmAddr = NULL;
+    m_info->m_task.tmWakeUp = 0;
+    m_info->m_task.tmReserved = 0;
+    m_info->m_task.qType = 0;
+    m_info->m_table = wxMacGetNotifierTable();
+    m_info->m_timer = this;
 }
 
 bool wxTimer::IsRunning() const
 {
     // as the qType may already indicate it is elapsed, but it
     // was not handled internally yet
-    return ( m_info->m_task.tmAddr != NULL ) ;
+    return ( m_info->m_task.tmAddr != NULL );
 }
 
 wxTimer::~wxTimer()
 {
     Stop();
-    if (m_info != NULL) {
-        delete m_info ;
-        m_info = NULL ;
+    if (m_info != NULL)
+    {
+        delete m_info;
+        m_info = NULL;
     }
-    int index = gTimersInProcess.Index( this ) ;
+
+    int index = gTimersInProcess.Index( this );
     if ( index != wxNOT_FOUND )
-        gTimersInProcess.RemoveAt( index ) ;
+        gTimersInProcess.RemoveAt( index );
 }
 
-bool wxTimer::Start(int milliseconds,bool mode)
+bool wxTimer::Start( int milliseconds, bool mode )
 {
-    (void)wxTimerBase::Start(milliseconds, mode);
+    (void)wxTimerBase::Start( milliseconds, mode );
 
     wxCHECK_MSG( m_milli > 0, false, wxT("invalid value for timer timeout") );
-    wxCHECK_MSG( m_info->m_task.tmAddr == NULL , false, wxT("attempting to restart a timer") );
+    wxCHECK_MSG( m_info->m_task.tmAddr == NULL, false, wxT("attempting to restart a timer") );
 
-    m_info->m_task.tmAddr = NewTimerUPP( MacTimerProc ) ;
-    m_info->m_task.tmWakeUp = 0 ;
-    m_info->m_task.tmReserved = 0 ;
-    m_info->m_task.qType = 0 ;
-    m_info->m_timer = this ;
-    InsXTime((QElemPtr) &m_info->m_task ) ;
-    PrimeTime( (QElemPtr) &m_info->m_task , m_milli ) ;
+    m_info->m_task.tmAddr = NewTimerUPP( MacTimerProc );
+    m_info->m_task.tmWakeUp = 0;
+    m_info->m_task.tmReserved = 0;
+    m_info->m_task.qType = 0;
+    m_info->m_timer = this;
+    InsXTime( (QElemPtr) &m_info->m_task );
+    PrimeTime( (QElemPtr) &m_info->m_task, m_milli );
+
     return true;
 }
 
@@ -202,13 +205,12 @@ void wxTimer::Stop()
 {
     if ( m_info->m_task.tmAddr )
     {
-        RmvTime(  (QElemPtr) &m_info->m_task ) ;
-        DisposeTimerUPP(m_info->m_task.tmAddr) ;
-        m_info->m_task.tmAddr = NULL ;
+        RmvTime( (QElemPtr) &m_info->m_task );
+        DisposeTimerUPP( m_info->m_task.tmAddr );
+        m_info->m_task.tmAddr = NULL;
     }
-    wxMacRemoveAllNotifiersForData( wxMacGetNotifierTable() , this ) ;
+
+    wxMacRemoveAllNotifiersForData( wxMacGetNotifierTable(), this );
 }
 
 #endif
-
-
