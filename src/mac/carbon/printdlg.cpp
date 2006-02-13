cc224d6e7974d699855b4e75143412867c537261
David Surovell <davids@osafoundation.org>
2006-02-13 08:03:37 +0000

    cleanup - reformatting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37553 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/printdlg.cpp
+++ src/mac/carbon/printdlg.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        printdlg.cpp
+// Name:        src/mac/carbon/printdlg.cpp
 // Purpose:     wxPrintDialog, wxPageSetupDialog
 // Author:      Stefan Csomor
 // Modified by:
@@ -20,46 +20,49 @@
 #include "wx/msgdlg.h"
 #include "wx/mac/private/print.h"
 
+
 // Use generic page setup dialog: use your own native one if one exists.
 
 IMPLEMENT_DYNAMIC_CLASS(wxMacPrintDialog, wxPrintDialogBase)
+
 
 wxMacPrintDialog::wxMacPrintDialog()
 {
     m_dialogParent = NULL;
     m_printerDC = NULL;
-    m_destroyDC = TRUE;
+    m_destroyDC = true;
 }
 
-wxMacPrintDialog::wxMacPrintDialog(wxWindow *p, wxPrintDialogData* data)
+wxMacPrintDialog::wxMacPrintDialog( wxWindow *p, wxPrintDialogData *data )
 {
-    Create(p, data);
+    Create( p, data );
 }
 
-wxMacPrintDialog::wxMacPrintDialog(wxWindow *p, wxPrintData* data)
+wxMacPrintDialog::wxMacPrintDialog( wxWindow *p, wxPrintData *data )
 {
     wxPrintDialogData data2;
-    if ( data )
+    if (data != NULL)
         data2 = *data;
-    
-    Create(p, &data2);
+
+    Create( p, &data2 );
 }
 
-bool wxMacPrintDialog::Create(wxWindow *p, wxPrintDialogData* data)
+bool wxMacPrintDialog::Create( wxWindow *p, wxPrintDialogData *data )
 {
     m_dialogParent = p;
     m_printerDC = NULL;
-    m_destroyDC = TRUE;
-    
-    if ( data )
+    m_destroyDC = true;
+
+    if (data != NULL)
         m_printDialogData = *data;
-    
-    return TRUE;
+
+    return true;
 }
 
 wxMacPrintDialog::~wxMacPrintDialog()
 {
-    if (m_destroyDC && m_printerDC) {
+    if (m_destroyDC && m_printerDC)
+    {
         delete m_printerDC;
         m_printerDC = NULL;
     }
@@ -68,69 +71,68 @@ wxMacPrintDialog::~wxMacPrintDialog()
 int wxMacPrintDialog::ShowModal()
 {
     m_printDialogData.GetPrintData().ConvertToNative();
-    ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->TransferFrom( &m_printDialogData ) ;
-    
-    int result = wxID_CANCEL ;
-    OSErr err = noErr ;
-    wxString message ;
-    
-    Boolean        accepted;
-    
-    err = PMSessionPrintDialog( ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->m_macPrintSession,
+    ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->TransferFrom( &m_printDialogData );
+
+    int result = wxID_CANCEL;
+    OSErr err = noErr;
+    Boolean accepted;
+
+    err = PMSessionPrintDialog(
+        ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->m_macPrintSession,
         ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->m_macPrintSettings,
         ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->m_macPageFormat,
-        &accepted);
+        &accepted );
+
     if ((err == noErr) && !accepted)
     {
-        err = kPMCancel; // user clicked Cancel button
+        // user clicked Cancel button
+        err = kPMCancel;
     }
 
-    if  ( err == noErr )
+    if (err == noErr)
     {
-        result = wxID_OK ;
+        result = wxID_OK;
     }
 
     if ((err != noErr) && (err != kPMCancel))
     {
-        message.Printf( wxT("Print Error %d"), err ) ;
-        wxMessageDialog dialog( NULL , message  , wxEmptyString, wxICON_HAND | wxOK) ;
+        wxString message;
+
+        message.Printf( wxT("Print Error %d"), err );
+        wxMessageDialog dialog( NULL, message, wxEmptyString, wxICON_HAND | wxOK );
         dialog.ShowModal();
     }
 
-    if ( result == wxID_OK )
+    if (result == wxID_OK)
     {
         m_printDialogData.GetPrintData().ConvertFromNative();
-        ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->TransferTo( &m_printDialogData ) ;
+        ((wxMacCarbonPrintData*)m_printDialogData.GetPrintData().GetNativeData())->TransferTo( &m_printDialogData );
     }
-    
-    return result ;
+
+    return result;
 }
 
-wxDC *wxMacPrintDialog::GetPrintDC()
+wxDC * wxMacPrintDialog::GetPrintDC()
 {
-    return new wxPrinterDC( m_printDialogData.GetPrintData() ) ;
+    return new wxPrinterDC( m_printDialogData.GetPrintData() );
 }
-
-/*
-* wxMacPageSetupDialog
-*/
 
 IMPLEMENT_CLASS(wxMacPageSetupDialog, wxPageSetupDialogBase)
 
-wxMacPageSetupDialog::wxMacPageSetupDialog(wxWindow *p, wxPageSetupDialogData *data) :
-wxPageSetupDialogBase()
+wxMacPageSetupDialog::wxMacPageSetupDialog( wxWindow *p, wxPageSetupDialogData *data )
+    : wxPageSetupDialogBase()
 {
-    Create(p, data);
+    Create( p, data );
 }
 
-bool wxMacPageSetupDialog::Create(wxWindow *p, wxPageSetupDialogData *data)
+bool wxMacPageSetupDialog::Create( wxWindow *p, wxPageSetupDialogData *data )
 {
     m_dialogParent = p;
-    
-    if (data)
+
+    if (data != NULL)
         m_pageSetupData = (*data);
-    
-    return TRUE;
+
+    return true;
 }
 
 wxMacPageSetupDialog::~wxMacPageSetupDialog()
@@ -138,50 +140,54 @@ wxMacPageSetupDialog::~wxMacPageSetupDialog()
 }
 
 wxPageSetupData& wxMacPageSetupDialog::GetPageSetupDialogData()
-{ 
+{
     return m_pageSetupData;
 }
 
 int wxMacPageSetupDialog::ShowModal()
 {
     m_pageSetupData.GetPrintData().ConvertToNative();
-    ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->TransferFrom( &m_pageSetupData ) ;
+    ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->TransferFrom( &m_pageSetupData );
 
-    int      result = wxID_CANCEL ;
-    OSErr    err = noErr ;
-    wxString message ;
-    
-    Boolean        accepted;
+    int result = wxID_CANCEL;
+    OSErr err = noErr;
+    Boolean accepted;
 
-    err = PMSessionPageSetupDialog( ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->m_macPrintSession,
+    err = PMSessionPageSetupDialog(
+        ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->m_macPrintSession,
         ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->m_macPageFormat,
-        &accepted);
+        &accepted );
+
     if ((err == noErr) && !accepted)
     {
-        err = kPMCancel; // user clicked Cancel button
+        // user clicked Cancel button
+        err = kPMCancel;
     }
 
-    //  If the user did not cancel, flatten and save the PageFormat object
-    //  with our document.
-    if (err == noErr) 
+    // If the user did not cancel, flatten and save the PageFormat object
+    // with our document.
+    if (err == noErr)
     {
-        result = wxID_OK ;
+        result = wxID_OK;
     }
+
     if ((err != noErr) && (err != kPMCancel))
     {
-        message.Printf( wxT("Print Error %d"), err ) ;
-        wxMessageDialog dialog( NULL , message , wxEmptyString, wxICON_HAND | wxOK) ;
+        wxString message;
+
+        message.Printf( wxT("Print Error %d"), err );
+        wxMessageDialog dialog( NULL, message, wxEmptyString, wxICON_HAND | wxOK );
         dialog.ShowModal();
     }
 
-    if (result == wxID_OK )
+    if (result == wxID_OK)
     {
         m_pageSetupData.GetPrintData().ConvertFromNative();
-        m_pageSetupData.SetPaperSize( m_pageSetupData.GetPrintData().GetPaperSize() ) ;
-        ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->TransferTo( &m_pageSetupData ) ;
-    }   
+        m_pageSetupData.SetPaperSize( m_pageSetupData.GetPrintData().GetPaperSize() );
+        ((wxMacCarbonPrintData*)m_pageSetupData.GetPrintData().GetNativeData())->TransferTo( &m_pageSetupData );
+    }
+
     return result;
 }
 
 #endif
-
