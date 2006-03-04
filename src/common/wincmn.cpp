5b7df27e4cf3d41c4fb4e2805462be1821989da5
Włodzimierz Skiba <abx@abx.art.pl>
2006-03-04 20:34:12 +0000

    More informative comment about previous commit.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37799 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/wincmn.cpp
+++ src/common/wincmn.cpp
@@ -446,8 +446,9 @@ void wxWindowBase::InvalidateBestSize()
     m_bestSizeCache = wxDefaultSize;
 
     // parent's best size calculation may depend on its children's
-    // best sizes, so let's invalidate it as well to be safe
-    // (but don't influence other wxTLWs)
+    // as long as child window we are in is not top level window itself
+    // (because the TLW size is never resized automatically)
+    // so let's invalidate it as well to be safe:
     if (m_parent && !IsTopLevel())
         m_parent->InvalidateBestSize();
 }
