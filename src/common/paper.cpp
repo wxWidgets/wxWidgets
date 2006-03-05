8a5434c1a50389fe83549c73af5d8c46e428213e
Vadim Zeitlin <vadim@wxwidgets.org>
2006-03-05 12:40:46 +0000

    corrected typo in PRC Envelope paper definition (used m instead of mm)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37816 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/paper.cpp
+++ src/common/paper.cpp
@@ -212,7 +212,7 @@ void wxPrintPaperDatabase::CreateDatabase()
     WXADDPAPER(wxPAPER_PENV_7_ROTATED,      115,                        wxTRANSLATE("PRC Envelope #7 Rotated 230 x 160 mm"), 2300, 1600);
     WXADDPAPER(wxPAPER_PENV_8_ROTATED,      116,                        wxTRANSLATE("PRC Envelope #8 Rotated 309 x 120 mm"), 3090, 1200);
     WXADDPAPER(wxPAPER_PENV_9_ROTATED,      117,                        wxTRANSLATE("PRC Envelope #9 Rotated 324 x 229 mm"), 3240, 2290);
-    WXADDPAPER(wxPAPER_PENV_10_ROTATED,     118,                        wxTRANSLATE("PRC Envelope #10 Rotated 458 x 324 m"), 4580, 3240);
+    WXADDPAPER(wxPAPER_PENV_10_ROTATED,     118,                        wxTRANSLATE("PRC Envelope #10 Rotated 458 x 324 mm"), 4580, 3240);
 }
 
 void wxPrintPaperDatabase::ClearDatabase()
