e1026179ef66aff49c44b5c6cdcbe68c938f33a3
Vadim Zeitlin <vadim@wxwidgets.org>
2006-03-07 23:16:58 +0000

    fixed a memory leak in case server name couldn't be resolved in MakeConnection() (coverity checker CID 56)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@37877 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/sckipc.cpp
+++ src/common/sckipc.cpp
@@ -163,14 +163,14 @@ wxConnectionBase *wxTCPClient::MakeConnection (const wxString& host,
                                                const wxString& serverName,
                                                const wxString& topic)
 {
+  wxSockAddress *addr = GetAddressFromName(serverName, host);
+  if ( !addr )
+      return NULL;
+
   wxSocketClient *client = new wxSocketClient(SCKIPC_FLAGS);
   wxSocketStream *stream = new wxSocketStream(*client);
   wxDataInputStream *data_is = new wxDataInputStream(*stream);
   wxDataOutputStream *data_os = new wxDataOutputStream(*stream);
-
-  wxSockAddress *addr = GetAddressFromName(serverName, host);
-  if ( !addr )
-      return NULL;
 
   bool ok = client->Connect(*addr);
   delete addr;
