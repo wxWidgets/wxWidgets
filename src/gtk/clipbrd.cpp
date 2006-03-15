b068aaf0e7396871aea0df827c1496d838fd0f62
Mart Raudsepp <leio@gentoo.org>
2006-03-15 08:49:36 +0000

    compilo for gtk1
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@38093 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/clipbrd.cpp
+++ src/gtk/clipbrd.cpp
@@ -497,7 +497,13 @@ bool wxClipboard::AddData( wxDataObject *data )
     gtk_signal_connect( GTK_OBJECT(m_clipboardWidget),
                         "selection_get",
                         GTK_SIGNAL_FUNC(selection_handler),
-                        GUINT_TO_POINTER( gtk_get_current_event_time() ) );
+                        GUINT_TO_POINTER(
+#ifdef __WXGTK20__
+                                gtk_get_current_event_time()
+#else
+                                gdk_event_get_time(gtk_get_current_event())
+#endif
+                                        ) );
 
 #if wxUSE_THREADS
     /* disable GUI threads */
