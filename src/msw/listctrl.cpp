612bbde52f394e5bbb57131834f1bceba172a190
Włodzimierz Skiba <abx@abx.art.pl>
2006-03-15 10:03:44 +0000

    wxTinderbox warning fix.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@38100 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/listctrl.cpp
+++ src/msw/listctrl.cpp
@@ -2491,7 +2491,7 @@ static void HandleItemPaint(LPNMLVCUSTOMDRAW pLVCD, HFONT hfont)
 
     // same thing for CDIS_FOCUS (except simpler as there is only one of them)
     if ( ::GetFocus() == hwndList &&
-            ListView_GetNextItem(hwndList, -1, LVNI_FOCUSED) == item )
+            ListView_GetNextItem(hwndList, (WPARAM)-1, LVNI_FOCUSED) == item )
     {
         nmcd.uItemState |= CDIS_FOCUS;
     }
