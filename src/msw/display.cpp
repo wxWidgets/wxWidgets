e557031896b65ff2052d8836028b19a7faa36f10
Julian Smart <julian@anthemion.co.uk>
2006-03-23 17:15:47 +0000

    Added missing declarations required for e.g. VC++ 6
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@38309 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/display.cpp
+++ src/msw/display.cpp
@@ -60,6 +60,22 @@
     // is only defined in winuser.h if WINVER >= 0x0500
     #if !defined(HMONITOR_DECLARED) && !defined(MNS_NOCHECK)
         DECLARE_HANDLE(HMONITOR);
+        typedef BOOL(CALLBACK * MONITORENUMPROC )(HMONITOR, HDC, LPRECT, LPARAM);
+        typedef struct tagMONITORINFO
+        {
+            DWORD   cbSize;
+            RECT    rcMonitor;
+            RECT    rcWork;
+            DWORD   dwFlags;
+        } MONITORINFO, *LPMONITORINFO;
+        typedef struct tagMONITORINFOEX : public tagMONITORINFO
+        {
+            TCHAR       szDevice[CCHDEVICENAME];
+        } MONITORINFOEX, *LPMONITORINFOEX;
+        #define MONITOR_DEFAULTTONULL       0x00000000
+        #define MONITOR_DEFAULTTOPRIMARY    0x00000001
+        #define MONITOR_DEFAULTTONEAREST    0x00000002
+        #define MONITORINFOF_PRIMARY        0x00000001        
         #define HMONITOR_DECLARED
     #endif
 #endif // !__WXWINCE__
