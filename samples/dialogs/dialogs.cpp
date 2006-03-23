acd8510fd2d8ec0bccc7f6881a090c653c02917f
Julian Smart <julian@anthemion.co.uk>
2006-03-23 11:35:56 +0000

    Don't centre the frame on WinCE
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@38296 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/dialogs/dialogs.cpp
+++ samples/dialogs/dialogs.cpp
@@ -383,7 +383,9 @@ bool MyApp::OnInit()
     myCanvas = new MyCanvas(frame);
     myCanvas->SetBackgroundColour(*wxWHITE);
 
+#ifndef __WXWINCE__
     frame->Centre(wxBOTH);
+#endif
 
     // Show the frame
     frame->Show(true);
