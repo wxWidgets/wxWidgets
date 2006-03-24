8313b86bf9b9dc51e928ed3f73c445059b63de3a
Julian Smart <julian@anthemion.co.uk>
2006-03-24 13:01:19 +0000

    Reduced deprecation warning frequency
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@38343 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/utils.cpp
+++ src/mac/carbon/utils.cpp
@@ -1580,5 +1580,16 @@ CGColorSpaceRef wxMacGetGenericRGBColorSpace()
 }
 #endif
 
+wxMacPortSaver::wxMacPortSaver( GrafPtr port )
+{
+    ::GetPort( &m_port ) ;
+    ::SetPort( port ) ;
+}
+
+wxMacPortSaver::~wxMacPortSaver()
+{
+    ::SetPort( m_port ) ;
+}
+
 #endif // wxUSE_GUI
 
