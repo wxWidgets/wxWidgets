93a57d19fe85793ba13e077e06849536b2b98775
Vadim Zeitlin <vadim@wxwidgets.org>
2006-03-31 14:26:59 +0000

    added tests for UTF8 and UTF16 files
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@38467 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/textfile/textfiletest.cpp
+++ tests/textfile/textfiletest.cpp
@@ -42,6 +42,10 @@ private:
         CPPUNIT_TEST( ReadUnix );
         CPPUNIT_TEST( ReadMac );
         CPPUNIT_TEST( ReadMixed );
+#if wxUSE_UNICODE
+        CPPUNIT_TEST( ReadUTF8 );
+        CPPUNIT_TEST( ReadUTF16 );
+#endif // wxUSE_UNICODE
     CPPUNIT_TEST_SUITE_END();
 
     void ReadEmpty();
@@ -49,12 +53,23 @@ private:
     void ReadUnix();
     void ReadMac();
     void ReadMixed();
+#if wxUSE_UNICODE
+    void ReadUTF8();
+    void ReadUTF16();
+#endif // wxUSE_UNICODE
 
     // return the name of the test file we use
     static const char *GetTestFileName() { return "textfiletest.txt"; }
 
     // create the test file with the given contents
-    static void CreateTestFile(const char *contents);
+    static void CreateTestFile(const char *contents)
+    {
+        return CreateTestFile(strlen(contents), contents);
+    }
+
+    // create the test file with the given contents (version must be used if
+    // contents contains NULs)
+    static void CreateTestFile(size_t len, const char *contents);
 
 
     DECLARE_NO_COPY_CLASS(TextFileTestCase)
@@ -66,12 +81,12 @@ CPPUNIT_TEST_SUITE_REGISTRATION( TextFileTestCase );
 // also include in it's own registry so that these tests can be run alone
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TextFileTestCase, "TextFileTestCase" );
 
-void TextFileTestCase::CreateTestFile(const char *contents)
+void TextFileTestCase::CreateTestFile(size_t len, const char *contents)
 {
     FILE *f = fopen(GetTestFileName(), "wb");
     CPPUNIT_ASSERT( f );
 
-    CPPUNIT_ASSERT( fputs(contents, f) >= 0 );
+    CPPUNIT_ASSERT( fwrite(contents, 1, len, f) >= 0 );
     CPPUNIT_ASSERT( fclose(f) == 0 );
 }
 
@@ -142,6 +157,44 @@ void TextFileTestCase::ReadMixed()
     CPPUNIT_ASSERT_EQUAL( wxString(_T("bar")), f.GetLine(1) );
     CPPUNIT_ASSERT_EQUAL( wxString(_T("baz")), f.GetLastLine() );
 }
+
+#if wxUSE_UNICODE
+
+void TextFileTestCase::ReadUTF8()
+{
+    CreateTestFile("\xd0\x9f\n"
+                   "\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82");
+
+    wxTextFile f;
+    CPPUNIT_ASSERT( f.Open(wxString::FromAscii(GetTestFileName()), wxConvUTF8) );
+
+    CPPUNIT_ASSERT_EQUAL( 2u, f.GetLineCount() );
+    CPPUNIT_ASSERT_EQUAL( wxTextFileType_Unix, f.GetLineType(0) );
+    CPPUNIT_ASSERT_EQUAL( wxTextFileType_None, f.GetLineType(1) );
+    CPPUNIT_ASSERT_EQUAL( wxString(L"\u041f"), f.GetFirstLine() );
+    CPPUNIT_ASSERT_EQUAL( wxString(L"\u0440\u0438\u0432\u0435\u0442"),
+                          f.GetLastLine() );
+}
+
+void TextFileTestCase::ReadUTF16()
+{
+    CreateTestFile(16,
+                   "\x1f\x04\x0d\x00\x0a\x00"
+                   "\x40\x04\x38\x04\x32\x04\x35\x04\x42\x04");
+
+    wxTextFile f;
+    wxMBConvUTF16 conv;
+    CPPUNIT_ASSERT( f.Open(wxString::FromAscii(GetTestFileName()), conv) );
+
+    CPPUNIT_ASSERT_EQUAL( 2u, f.GetLineCount() );
+    CPPUNIT_ASSERT_EQUAL( wxTextFileType_Dos, f.GetLineType(0) );
+    CPPUNIT_ASSERT_EQUAL( wxTextFileType_None, f.GetLineType(1) );
+    CPPUNIT_ASSERT_EQUAL( wxString(L"\u041f"), f.GetFirstLine() );
+    CPPUNIT_ASSERT_EQUAL( wxString(L"\u0440\u0438\u0432\u0435\u0442"),
+                          f.GetLastLine() );
+}
+
+#endif // wxUSE_UNICODE
 
 #endif // wxUSE_TEXTFILE
 
