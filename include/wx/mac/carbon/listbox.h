12cb75919071a7cda9e7896ee6d937ccf08a8153
David Surovell <davids@osafoundation.org>
2006-04-10 14:58:15 +0000

    cleanup - reformatting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@38655 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/carbon/listbox.h
+++ include/wx/mac/carbon/listbox.h
@@ -22,11 +22,11 @@
 class wxArrayInt;
 
 // forward decl for GetPeer() 
-class wxMacListControl ;
+class wxMacListControl;
 
 // List box item
 
-WX_DEFINE_ARRAY( char * , wxListDataArray ) ;
+WX_DEFINE_ARRAY( char* , wxListDataArray );
 
 // ----------------------------------------------------------------------------
 // List box control
@@ -37,46 +37,58 @@ class WXDLLEXPORT wxListBox : public wxListBoxBase
 public:
     // ctors and such
     wxListBox();
-    wxListBox(wxWindow *parent, wxWindowID id,
-            const wxPoint& pos = wxDefaultPosition,
-            const wxSize& size = wxDefaultSize,
-            int n = 0, const wxString choices[] = NULL,
-            long style = 0,
-            const wxValidator& validator = wxDefaultValidator,
-            const wxString& name = wxListBoxNameStr)
+
+    wxListBox(
+        wxWindow *parent,
+        wxWindowID id,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        int n = 0, const wxString choices[] = NULL,
+        long style = 0,
+        const wxValidator& validator = wxDefaultValidator,
+        const wxString& name = wxListBoxNameStr)
     {
         Create(parent, id, pos, size, n, choices, style, validator, name);
     }
-    wxListBox(wxWindow *parent, wxWindowID id,
-            const wxPoint& pos,
-            const wxSize& size,
-            const wxArrayString& choices,
-            long style = 0,
-            const wxValidator& validator = wxDefaultValidator,
-            const wxString& name = wxListBoxNameStr)
+
+    wxListBox(
+        wxWindow *parent,
+        wxWindowID id,
+        const wxPoint& pos,
+        const wxSize& size,
+        const wxArrayString& choices,
+        long style = 0,
+        const wxValidator& validator = wxDefaultValidator,
+        const wxString& name = wxListBoxNameStr)
     {
         Create(parent, id, pos, size, choices, style, validator, name);
     }
 
-    bool Create(wxWindow *parent, wxWindowID id,
-                const wxPoint& pos = wxDefaultPosition,
-                const wxSize& size = wxDefaultSize,
-                int n = 0, const wxString choices[] = NULL,
-                long style = 0,
-                const wxValidator& validator = wxDefaultValidator,
-                const wxString& name = wxListBoxNameStr);
-    bool Create(wxWindow *parent, wxWindowID id,
-                const wxPoint& pos,
-                const wxSize& size,
-                const wxArrayString& choices,
-                long style = 0,
-                const wxValidator& validator = wxDefaultValidator,
-                const wxString& name = wxListBoxNameStr);
+    bool Create(
+        wxWindow *parent,
+        wxWindowID id,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        int n = 0,
+        const wxString choices[] = NULL,
+        long style = 0,
+        const wxValidator& validator = wxDefaultValidator,
+        const wxString& name = wxListBoxNameStr);
+
+    bool Create(
+        wxWindow *parent,
+        wxWindowID id,
+        const wxPoint& pos,
+        const wxSize& size,
+        const wxArrayString& choices,
+        long style = 0,
+        const wxValidator& validator = wxDefaultValidator,
+        const wxString& name = wxListBoxNameStr);
 
     virtual ~wxListBox();
-    virtual void Refresh(bool eraseBack = true, const wxRect *rect = NULL);
 
     // implement base class pure virtuals
+    virtual void Refresh(bool eraseBack = true, const wxRect *rect = NULL);
     virtual void Clear();
     virtual void Delete(unsigned int n);
 
@@ -93,14 +105,17 @@ public:
     static wxVisualAttributes
     GetClassDefaultAttributes(wxWindowVariant variant = wxWINDOW_VARIANT_NORMAL);
 
-    wxArrayString   m_stringArray ;
-    wxListDataArray m_dataArray ;
-    
-    wxMacListControl*         GetPeer() const { return (wxMacListControl*) m_peer ; }
+    wxMacListControl * GetPeer() const { return (wxMacListControl*)m_peer; }
 
+    wxArrayString m_stringArray;
+    wxListDataArray m_dataArray;
+    
 protected:
+    // common creation for all databrowser list implementations
+    wxMacListControl* CreateMacListControl(const wxPoint& pos, const wxSize& size, long style);
+    
     // internal storage for line n has changed, issue a redraw
-    void  MacUpdateLine( int n ) ;
+    void MacUpdateLine(int n);
     
     virtual void DoSetSelection(int n, bool select);
     virtual int DoAppend(const wxString& item);
@@ -113,18 +128,14 @@ protected:
     virtual wxClientData* DoGetItemClientObject(unsigned int n) const;
     virtual void DoSetSize(int x, int y, int width, int height, int sizeFlags = wxSIZE_AUTO);
     virtual int DoListHitTest(const wxPoint& point) const;
+    virtual wxSize DoGetBestSize() const;
 
     // free memory (common part of Clear() and dtor)
     // prevent collision with some BSD definitions of macro Free()
     void FreeData();
 
     unsigned int m_noItems;
-    
-    virtual wxSize DoGetBestSize() const;
 
-    // common creation for all databrowser list implementations
-    wxMacListControl* CreateMacListControl(const wxPoint& pos, const wxSize& size, long style) ;
-    
 private:
     DECLARE_DYNAMIC_CLASS(wxListBox)
     DECLARE_EVENT_TABLE()
