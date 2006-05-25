1c2d839fbfe4683d762754f0d9a727b15f5c4300
Paul Cornett <paulcor@bullseye.com>
2006-05-25 02:40:58 +0000

    don't crash on zero length string in DoDrawRotatedText
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@39325 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/dcclient.cpp
+++ src/gtk/dcclient.cpp
@@ -1586,15 +1586,16 @@ void wxWindowDC::DoDrawText( const wxString &text, wxCoord x, wxCoord y )
 
 void wxWindowDC::DoDrawRotatedText( const wxString &text, wxCoord x, wxCoord y, double angle )
 {
+    if (!m_window || text.empty())
+        return;
+
+    wxCHECK_RET( Ok(), wxT("invalid window dc") );
+
     if ( wxIsNullDouble(angle) )
     {
         DrawText(text, x, y);
         return;
     }
-
-    wxCHECK_RET( Ok(), wxT("invalid window dc") );
-
-    if (!m_window) return;
 
     wxCoord w;
     wxCoord h;
