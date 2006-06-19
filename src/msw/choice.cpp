9b11300d9bfc3ac561ec69335beca74243ad5ea8
Robin Dunn <robin@alldunn.com>
2006-06-19 16:31:57 +0000

    Compile fix
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@39794 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/choice.cpp
+++ src/msw/choice.cpp
@@ -504,6 +504,8 @@ void wxChoice::DoSetSize(int x, int y,
                          int width, int height,
                          int sizeFlags)
 {
+    heightOrig = height;
+    
     // the height which we must pass to Windows should be the total height of
     // the control including the drop down list while the height given to us
     // is, of course, just the height of the permanently visible part of it
