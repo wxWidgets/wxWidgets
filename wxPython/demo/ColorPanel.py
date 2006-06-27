6eed24bf7854795e4903f6ee6c48c5a68e2da419
Robin Dunn <robin@alldunn.com>
2006-06-27 22:39:31 +0000

    Don't use custom bg style except on wxGTK
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@39868 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/ColorPanel.py
+++ wxPython/demo/ColorPanel.py
@@ -12,7 +12,8 @@ class ColoredPanel(wx.Window):
     def __init__(self, parent, color):
         wx.Window.__init__(self, parent, -1, style = wx.SIMPLE_BORDER)
         self.SetBackgroundColour(color)
-        self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+        if wx.Platform == '__WXGTK__':
+            self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
 
 
 #---------------------------------------------------------------------------
