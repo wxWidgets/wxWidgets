e43e81a1ca733d97434f277add1305810f9b8379
Vadim Zeitlin <vadim@wxwidgets.org>
2006-07-16 23:10:50 +0000

    fix Open(wxDbConnectInf) when using connection string (patch 1494705) (backport from HEAD)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@40149 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/db.cpp
+++ src/common/db.cpp
@@ -900,7 +900,7 @@ bool wxDb::Open(wxDbConnectInf *dbConnectInf, bool failOnDataTypeUnsupported)
 
     // Use the connection string if one is present
     if (dbConnectInf->UseConnectionStr())
-        return Open(GetConnectionInStr(), failOnDataTypeUnsupported);
+        return Open(dbConnectInf->GetConnectionStr(), failOnDataTypeUnsupported);
     else
         return Open(dbConnectInf->GetDsn(), dbConnectInf->GetUserID(),
                     dbConnectInf->GetPassword(), failOnDataTypeUnsupported);
