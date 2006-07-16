40a33c46e31226cab8dff02b0a078214f60666ac
Stefan Neis <Stefan.Neis@t-online.de>
2006-07-16 20:41:48 +0000

    Avoid trouble during initialisation & typo fix (patch #1523304).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40142 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/datectlg.cpp
+++ src/generic/datectlg.cpp
@@ -574,9 +574,13 @@ void wxDatePickerCtrlGeneric::DoMoveWindow(int x, int y, int w, int h)
 
 wxSize wxDatePickerCtrlGeneric::DoGetBestSize() const
 {
-    int bh=m_btn->GetBestSize().y;
-    int eh=m_txt->GetBestSize().y;
-    return wxSize(DEFAULT_ITEM_WIDTH, bh > eh ? bh : eh);
+    if (m_btn && m_txt)
+    {
+        int bh=m_btn->GetBestSize().y;
+        int eh=m_txt->GetBestSize().y;
+        return wxSize(DEFAULT_ITEM_WIDTH, bh > eh ? bh : eh);
+    }
+    return wxSize(DEFAULT_ITEM_WIDTH,DEFAULT_ITEM_HEIGHT);
 }
 
 
@@ -736,7 +740,7 @@ bool wxDatePickerCtrlGeneric::GetRange(wxDateTime *dt1, wxDateTime *dt2) const
 {
     if (dt1)
         *dt1 = m_cal->GetLowerDateLimit();
-    if (dt1)
+    if (dt2)
         *dt2 = m_cal->GetUpperDateLimit();
     return true;
 }
