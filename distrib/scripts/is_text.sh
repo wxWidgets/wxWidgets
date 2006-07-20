da44b91efc3cbe347690050d2ca5a582d3c7577c
Vadim Zeitlin <vadim@wxwidgets.org>
2006-07-20 15:25:26 +0000

    made RE match the otional tag name after the sticky flags
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40208 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/scripts/is_text.sh
+++ distrib/scripts/is_text.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+###############################################################################
+# Name:      is_text.sh
+# Purpose:   returns 0 if the file in cvs tree is text, 1 if binary
+# Version:   $Id$
+# Author:    VZ
+# Created:   2006-07-19
+# Copyright: (c) Vadim Zeitlin 2006 <vadim@wxwindows.org>
+###############################################################################
+
 if [ $# != 1 ]; then
     echo "Usage: $0 <file>" >&2
     exit 2
@@ -10,7 +20,8 @@ if [ ! -f $entries ]; then
     exit 3
 fi
 
-re="^/`basename $1`/.*/-kb/"
+# we look for -kb in the last field, optionally followed by tag name
+re="^/`basename $1`/.*/-kb/\(T[^/]*\)\{0,1\}$"
 if grep -q "$re" $entries; then
     exit 1
 fi
