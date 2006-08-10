a02f7807ce4899bcd5b112b050ba2f8917f21805
Julian Smart <julian@anthemion.co.uk>
2006-08-10 12:43:38 +0000

    Need include so user code can access book control
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40547 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/generic/propdlg.h
+++ include/wx/generic/propdlg.h
@@ -16,7 +16,7 @@
 
 #if wxUSE_BOOKCTRL
 
-class WXDLLEXPORT wxBookCtrlBase;
+#include "wx/bookctrl.h"
 
 //-----------------------------------------------------------------------------
 // wxPropertySheetDialog
