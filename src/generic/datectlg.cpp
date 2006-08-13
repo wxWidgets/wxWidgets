005eba5abd5ea7ec7630e1c5995464fad6342065
Vadim Zeitlin <vadim@wxwidgets.org>
2006-08-13 01:24:30 +0000

    fixed crash if the control is moved while the popup is open (patch 1539194)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40590 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/datectlg.cpp
+++ src/generic/datectlg.cpp
@@ -569,7 +569,7 @@ void wxDatePickerCtrlGeneric::DoMoveWindow(int x, int y, int w, int h)
     wxControl::DoMoveWindow(x, y, w, h);
 
     if (m_dropped)
-        DropDown(true);
+        DropDown(false);
 }
 
 wxSize wxDatePickerCtrlGeneric::DoGetBestSize() const
