64e941edd4c6ff84e75e98ec551c42096f31ec77
Michael Wetherell <mike.wetherell@ntlworld.com>
2006-08-28 12:27:39 +0000

    We can't define things in someone else's namespace.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40898 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/testprec.h
+++ tests/testprec.h
@@ -1,32 +1,4 @@
 #include "wx/wxprec.h"
-
-#include <string>
-
-// need this to be able to use CPPUNIT_ASSERT_EQUAL with wxString objects
-//
-// NB: at least for VC7.1 the specialization must be done before cppunit
-//     headers inclusion as otherwise it's just ignored!
-namespace CppUnit
-{
-
-template <typename T> struct assertion_traits;
-
-template <>
-struct assertion_traits<wxString>
-{
-    static bool equal(const wxString& s1, const wxString& s2)
-    {
-      return s1 == s2;
-    }
-
-    static std::string toString(const wxString& s)
-    {
-      return std::string(s.mb_str());
-    }
-};
-
-} // namespace CppUnit
-
 #include "wx/cppunit.h"
 
 // define wxHAVE_U_ESCAPE if the compiler supports \uxxxx character constants
@@ -34,7 +6,3 @@ struct assertion_traits<wxString>
     (defined(__GNUC__) && (__GNUC__ >= 3))
     #define wxHAVE_U_ESCAPE
 #endif
-
-#define CPPUNIT_ASSERT_STR_EQUAL(s1, s2)                                      \
-    CPPUNIT_ASSERT_EQUAL( wxString(s1), wxString(s2) )
-
