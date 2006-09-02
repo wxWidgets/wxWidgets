5b5d4ef2ddac3a27bf61d952458059881b7e2fc4
Robin Dunn <robin@alldunn.com>
2006-09-02 19:45:44 +0000

    bump subrel number
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@40963 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/version.h
+++ include/wx/version.h
@@ -27,7 +27,7 @@
 #define wxMAJOR_VERSION      2
 #define wxMINOR_VERSION      7
 #define wxRELEASE_NUMBER     0
-#define wxSUBRELEASE_NUMBER  0
+#define wxSUBRELEASE_NUMBER  1
 #define wxVERSION_STRING   _T("wxWidgets 2.7.0")
 
 /*  nothing to update below this line when updating the version */
