19b47f5b2ca793c32264c67ea672bf4c4c0a01f3
Robin Dunn <robin@alldunn.com>
2006-09-09 19:18:41 +0000

    Show Pulse mode
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@41108 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/Gauge.py
+++ wxPython/demo/Gauge.py
@@ -12,29 +12,21 @@ class TestPanel(wx.Panel):
         wx.StaticText(self, -1, "This example shows the wx.Gauge control.", (45, 15))
 
         self.g1 = wx.Gauge(self, -1, 50, (110, 50), (250, 25))
-        self.g1.SetBezelFace(3)
-        self.g1.SetShadowWidth(3)
+        self.g2 = wx.Gauge(self, -1, 50, (110, 95), (250, 25))
 
-        self.g2 = wx.Gauge(
-                    self, -1, 50, (110, 95), (250, 25), 
-                    wx.GA_HORIZONTAL|wx.GA_SMOOTH
-                    )
+        self.Bind(wx.EVT_TIMER, self.TimerHandler)
+        self.timer = wx.Timer(self)
+        self.timer.Start(100)
 
-        self.g2.SetBezelFace(5)
-        self.g2.SetShadowWidth(5)
-
-        self.Bind(wx.EVT_IDLE, self.IdleHandler)
-
-
-    def IdleHandler(self, event):
+    def TimerHandler(self, event):
         self.count = self.count + 1
 
         if self.count >= 50:
             self.count = 0
 
         self.g1.SetValue(self.count)
-        self.g2.SetValue(self.count)
-
+        self.g2.Pulse()
+    
 
 
 #----------------------------------------------------------------------
