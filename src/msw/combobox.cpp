69102c1ee4f8cb6f100e4076bbfb06d87a687176
Vadim Zeitlin <vadim@wxwidgets.org>
2006-09-10 12:20:39 +0000

    compilation fix after last commit (forgot to remove WXUNUSED)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@41125 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/combobox.cpp
+++ src/msw/combobox.cpp
@@ -601,7 +601,7 @@ bool wxComboBox::IsEditable() const
     return !HasFlag(wxCB_READONLY);
 }
 
-void wxComboBox::SetEditable(bool WXUNUSED(editable))
+void wxComboBox::SetEditable(bool editable)
 {
     HWND hWnd = (HWND)GetEditHWND();
     if ( !::SendMessage(hWnd, EM_SETREADONLY, !editable, 0) )
