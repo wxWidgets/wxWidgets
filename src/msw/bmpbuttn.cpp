f82e78f9935f673c465dd9b691116da291e3d293
Vadim Zeitlin <vadim@wxwidgets.org>
2006-09-22 00:53:07 +0000

    don't release the DC which we don't own in wxUSE_UXTHEME case (backport from HEAD)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_6_BRANCH@41364 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/bmpbuttn.cpp
+++ src/msw/bmpbuttn.cpp
@@ -379,8 +379,7 @@ bool wxBitmapButton::MSWOnDraw(WXDRAWITEMSTRUCT *item)
         }
 
         // draw the bitmap
-        wxDC dst;
-        dst.SetHDC((WXHDC) hDC, false);
+        wxDCTemp dst((WXHDC)hDC);
         dst.DrawBitmap(*bitmap, x1, y1, true);
 
         return true;
