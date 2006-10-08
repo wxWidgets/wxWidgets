33ad028c0177f0dcc69eab6fcce96f02f0528406
Vadim Zeitlin <vadim@wxwidgets.org>
2006-10-08 00:03:49 +0000

    ignore the other WXDLLIMPEXP_XXX and not only BASE one too
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@41680 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- misc/scripts/makeunivtags.bat
+++ misc/scripts/makeunivtags.bat
@@ -1,1 +1,1 @@
-@ctags.exe -o tags.univ --totals --language-force=c++ --c++-types=+px -I WXDLLEXPORT -I WXDLLIMPEXP_DATA_BASE+ -I WXDLLIMPEXP_BASE include/wx/univ/*.h src/univ/*.cpp src/univ/themes/*.cpp
+@ctags.exe -o wxuniv.tags --totals --language-force=c++ --c++-types=+px -I WXDLLEXPORT -I WXDLLIMPEXP_DATA_BASE+ -I WXDLLIMPEXP_BASE -I WXDLLIMPEXP_CORE include/wx/univ/*.h src/univ/*.cpp src/univ/themes/*.cpp
