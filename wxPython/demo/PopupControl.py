b8dc98c8f5c62b90219564de161a9fe1a99c5ae2
Robin Dunn <robin@alldunn.com>
2006-10-13 19:55:29 +0000

    This works ok on Mac now
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@41996 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/demo/PopupControl.py
+++ wxPython/demo/PopupControl.py
@@ -68,14 +68,8 @@ class TestPanel(wx.Panel):
 #----------------------------------------------------------------------
 
 def runTest(frame, nb, log):
-    if wx.Platform == "__WXMAC__":
-        from Main import MessagePanel
-        win = MessagePanel(nb, 'This demo currently fails on the Mac.',
-                           'Sorry', wx.ICON_WARNING)
-        return win
-    else:
-        win = TestPanel(nb, log)
-        return win
+    win = TestPanel(nb, log)
+    return win
 
 #----------------------------------------------------------------------
 
