7b61ea68e2bbd5d54fd73d6eb19c72dc0494be38
Robin Dunn <robin@alldunn.com>
2006-10-18 23:45:42 +0000

    remove "pre"
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@42101 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/config.py
+++ wxPython/config.py
@@ -40,7 +40,7 @@ VER_MAJOR        = 2      # The first three must match wxWidgets
 VER_MINOR        = 7
 VER_RELEASE      = 1
 VER_SUBREL       = 1      # wxPython release num for x.y.z release of wxWidgets
-VER_FLAGS        = "pre"  # release flags, such as prerelease or RC num, etc.
+VER_FLAGS        = ""  # release flags, such as prerelease or RC num, etc.
 
 DESCRIPTION      = "Cross platform GUI toolkit for Python"
 AUTHOR           = "Robin Dunn"
