d4638e6b36cb3d87b85481651284b592c7a15f52
Robert Roebling <robert@roebling.de>
2006-10-29 14:25:54 +0000

    Test SetSizeHints() for wxFrame (does nothing     under wxMSW).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@42617 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/minifram/minifram.cpp
+++ samples/minifram/minifram.cpp
@@ -55,6 +55,11 @@ bool MyApp::OnInit()
   // Create the main frame window
   main_frame = new MyMainFrame((wxFrame *) NULL, wxID_ANY, _T("wxFrame sample"),
      wxPoint(100, 100), wxSize(300, 200));
+     
+  // main_frame->SetMinSize( wxSize(100,100) );
+  // main_frame->SetMaxSize( wxSize(400,400) );
+  // same as
+  main_frame->SetSizeHints( 100,100, 400,400 );
 
   main_frame->CreateToolBar(wxNO_BORDER|wxTB_VERTICAL, ID_TOOLBAR);
   InitToolbar(main_frame->GetToolBar());
