74a94478c95473607e8f25fce54673c2916fbc94
Benjamin Williams <bwilliams@kirix.com>
2006-11-02 07:27:27 +0000

    fixed aui flicker bug caused by typo
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@42931 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/aui/floatpane.cpp
+++ src/aui/floatpane.cpp
@@ -50,7 +50,7 @@ wxAuiFloatingFrame::wxAuiFloatingFrame(wxWindow* parent,
                         pane.floating_pos, pane.floating_size,
                         style | 
                         (pane.HasCloseButton()?wxCLOSE_BOX:0) |
-                        (pane.HasMaximizeButton()?wxMAXIMIZE:0) |
+                        (pane.HasMaximizeButton()?wxMAXIMIZE_BOX:0) |
                         (pane.IsFixed()?0:wxRESIZE_BORDER)
                         )
 {
