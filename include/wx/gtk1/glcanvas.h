0643d43cad854f8d1b5588bb293c992d573b2a25
Vadim Zeitlin <vadim@wxwidgets.org>
2006-11-06 00:57:46 +0000

    move WX_GL_XXX constants from wx/*/glcanvas.h to wx/glcanvas.h (this makes them available for wxCocoa too); also only include <windows.h> via wx/msw/wrapwin.h and not directly for wxMSW
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43097 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/gtk1/glcanvas.h
+++ include/wx/gtk1/glcanvas.h
@@ -22,30 +22,6 @@ extern "C" {
 }
 
 //---------------------------------------------------------------------------
-// Constants for attriblist
-//---------------------------------------------------------------------------
-
-enum
-{
-  WX_GL_RGBA=1,          /* use true color palette */
-  WX_GL_BUFFER_SIZE,     /* bits for buffer if not WX_GL_RGBA */
-  WX_GL_LEVEL,           /* 0 for main buffer, >0 for overlay, <0 for underlay */
-  WX_GL_DOUBLEBUFFER,    /* use doublebuffer */
-  WX_GL_STEREO,          /* use stereoscopic display */
-  WX_GL_AUX_BUFFERS,     /* number of auxiliary buffers */
-  WX_GL_MIN_RED,         /* use red buffer with most bits (> MIN_RED bits) */
-  WX_GL_MIN_GREEN,       /* use green buffer with most bits (> MIN_GREEN bits) */
-  WX_GL_MIN_BLUE,        /* use blue buffer with most bits (> MIN_BLUE bits) */
-  WX_GL_MIN_ALPHA,       /* use blue buffer with most bits (> MIN_ALPHA bits) */
-  WX_GL_DEPTH_SIZE,      /* bits for Z-buffer (0,16,32) */
-  WX_GL_STENCIL_SIZE,    /* bits for stencil buffer */
-  WX_GL_MIN_ACCUM_RED,   /* use red accum buffer with most bits (> MIN_ACCUM_RED bits) */
-  WX_GL_MIN_ACCUM_GREEN, /* use green buffer with most bits (> MIN_ACCUM_GREEN bits) */
-  WX_GL_MIN_ACCUM_BLUE,  /* use blue buffer with most bits (> MIN_ACCUM_BLUE bits) */
-  WX_GL_MIN_ACCUM_ALPHA  /* use blue buffer with most bits (> MIN_ACCUM_ALPHA bits) */
-};
-
-//---------------------------------------------------------------------------
 // classes
 //---------------------------------------------------------------------------
 
