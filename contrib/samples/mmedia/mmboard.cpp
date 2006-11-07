7487078819438f29d0a2653ab8d8141b82e99687
Vadim Zeitlin <vadim@wxwidgets.org>
2006-11-07 15:32:27 +0000

    include the icon also for wxMGL (fixes tinderbox error)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43163 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/samples/mmedia/mmboard.cpp
+++ contrib/samples/mmedia/mmboard.cpp
@@ -33,8 +33,9 @@
 // ----------------------------------------------------------------------------
 // ressources
 // ----------------------------------------------------------------------------
+
 // the application icon
-#if defined(__WXGTK__) || defined(__WXX11__) || defined(__WXMOTIF__) || defined(__WXMAC__)
+#if !defined(__WXMSW__) && !defined(__WXPM__)
     #include "mondrian.xpm"
 #endif
 
