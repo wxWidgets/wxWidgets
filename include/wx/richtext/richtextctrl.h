c7134e1c9c054327d689887ca127ee82a8f93672
Julian Smart <julian@anthemion.co.uk>
2006-11-15 23:03:18 +0000

    Made new functions non-virtual
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43435 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/richtext/richtextctrl.h
+++ include/wx/richtext/richtextctrl.h
@@ -185,8 +185,8 @@ public:
     virtual bool GetStyle(long position, wxRichTextAttr& style);
 
     // get the common set of styles for the range
-    virtual bool GetStyleForRange(const wxRichTextRange& range, wxRichTextAttr& style);
-    virtual bool GetStyleForRange(const wxRichTextRange& range, wxTextAttrEx& style);
+    bool GetStyleForRange(const wxRichTextRange& range, wxRichTextAttr& style);
+    bool GetStyleForRange(const wxRichTextRange& range, wxTextAttrEx& style);
 
     // extended style setting operation with flags including:
     // wxRICHTEXT_SETSTYLE_WITH_UNDO, wxRICHTEXT_SETSTYLE_OPTIMIZE, wxRICHTEXT_SETSTYLE_PARAGRAPHS_ONLY, wxRICHTEXT_SETSTYLE_CHARACTERS_ONLY
