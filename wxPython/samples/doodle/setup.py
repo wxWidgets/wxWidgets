34bec08974b9dce7b46b0a58802e5ea3abdd4aea
Robin Dunn <robin@alldunn.com>
2006-11-18 19:45:11 +0000

    py2exe finds the DLL now
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43497 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/samples/doodle/setup.py
+++ wxPython/samples/doodle/setup.py
@@ -11,15 +11,15 @@ import sys, os
 from distutils.core import setup
 import py2exe
 
-if sys.platform == "win32" and sys.version_info > (2, 4):
-    DATA =  [("", os.path.join(sys.exec_prefix, 'msvcr71.dll'))]
-else:
-    DATA = []
+## if sys.platform == "win32" and sys.version_info > (2, 4):
+##     DATA =  [("", os.path.join(sys.exec_prefix, 'msvcr71.dll'))]
+## else:
+##     DATA = []
     
 
 setup( name = "superdoodle",
        #console = ["superdoodle.py"]
        windows = ["superdoodle.py"],
-       data_files = DATA,
+       #data_files = DATA,
        )
 
