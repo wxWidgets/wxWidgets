13542f4946e681db5bd765e8bad6402a12adfd1f
Robin Dunn <robin@alldunn.com>
2006-11-21 03:50:44 +0000

    Ensure the old tree item still exists before using it
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43557 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wx/tools/XRCed/tree.py
+++ wxPython/wx/tools/XRCed/tree.py
@@ -692,7 +692,8 @@ class XML_Tree(wx.TreeCtrl):
         #oldItem = evt.GetOldItem()
         status = ''
         oldItem = self.selection
-        if oldItem:
+        # use GetItemParent as a way to determine if the itemId is still valid
+        if oldItem and self.GetItemParent(oldItem):
             xxx = self.GetPyData(oldItem)
             # If some data was modified, apply changes
             if g.panel.IsModified():
