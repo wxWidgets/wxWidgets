4db88d421b2725dfc71d063c80d9e61b0e1e9458
Mart Raudsepp <leio@gentoo.org>
2006-11-29 04:10:36 +0000

    [GTK] Do not define *_DEPRECATED macros for the gtk+ stack in preparation of a stable wx release, as we can not know what functions become deprecated in future releases of gtk+ and co, while the stable wx release should last the test of time. We could do some preprocessor magic to automatically define the macros only for development releases (whose minor version number is odd, not even), but as the releases is close, don't do this at this point, as some compilers might not like the initial try. I will bring these macros back to HEAD, together with the automation, once 2.8 branches.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@43716 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -2783,16 +2783,6 @@ typedef GtkWidget *WXWidget;
 #define GTK_CLASS_TYPE(klass) ((klass)->type)
 #endif
 
-#ifdef __WXGTK20__
-#define G_DISABLE_DEPRECATED
-#define PANGO_DISABLE_DEPRECATED
-#define GDK_PIXBUF_DISABLE_DEPRECATED
-#ifndef __VMS
-/* GtkCombo is not defined on VMS if this is defined */
-# define GTK_DISABLE_DEPRECATED
-#endif
-#endif
-
 #endif /*  __WXGTK__ */
 
 #if defined(__WXGTK20__) || (defined(__WXX11__) && wxUSE_UNICODE)
