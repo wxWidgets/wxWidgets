0b8055d224b5d21f8b876ba04a2fed00c988d1b8
Vadim Zeitlin <vadim@wxwidgets.org>
2007-01-08 15:04:19 +0000

    added more wxUSE_TREE/LISTCTRL tests to fix compilation when they're set to 0
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@44171 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/window.cpp
+++ src/mac/carbon/window.cpp
@@ -310,14 +310,17 @@ static pascal OSStatus wxMacWindowControlEventHandler( EventHandlerCallRef handl
             
         case kEventControlActivate :
         case kEventControlDeactivate :
-            if ( thisWindow->IsKindOf( CLASSINFO( wxTreeCtrl ) ) 
-                    || thisWindow->IsKindOf( CLASSINFO( wxListCtrl ) ) 
-                )
+            // FIXME: we should have a virtual function for this!
+#if wxUSE_TREECTRL
+            if ( thisWindow->IsKindOf( CLASSINFO( wxTreeCtrl ) ) )
                 thisWindow->Refresh();
-            
-            //thisWindow->MacActivateStateChanged() ;
-            break ;
 #endif
+#if wxUSE_LISTCTRL
+            if ( thisWindow->IsKindOf( CLASSINFO( wxListCtrl ) ) )
+                thisWindow->Refresh();
+#endif
+            break ;
+#endif // TARGET_API_MAC_OSX
 
         // we emulate this event under Carbon CFM
         case kEventControlSetFocusPart :
