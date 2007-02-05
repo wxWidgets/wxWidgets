e602dae828e1f522c3142d15d6449f89dda33340
Kevin Ollivier <kevino@theolliviers.com>
2007-02-05 01:15:02 +0000

    Committing patch #1604462, which improves autoconf support, adds a wxbase template, allows components to have names which match wx lib naming, etc.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@44359 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ build/bakefiles/wxpresets/libsample/libsample.cpp
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        libsample.cpp
+// Purpose:     The source of a dummy sample wx-based library
+// Author:      Francesco Montorsi
+// Modified by:
+// Created:     26/11/06
+// RCS-ID:      $Id$
+// Copyright:   (c) Francesco Montorsi
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWindows headers)
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+// ============================================================================
+// implementation
+// ============================================================================
+
+void MyUtilityFunction()
+{
+    wxPrintf(wxT("Hello world!\n"));
+    fflush(stdout);
+}
