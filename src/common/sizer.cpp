24f640b8d9e3f082a0df6042a3b26346d1bafc3d
Julian Smart <julian@anthemion.co.uk>
2007-02-16 22:12:58 +0000

    IsShown shouldn't cause empty sizers to not have their positions calculated
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@44514 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/sizer.cpp
+++ src/common/sizer.cpp
@@ -460,6 +460,11 @@ bool wxSizerItem::IsShown() const
             // shown, so are we (this arbitrariness is the reason for
             // deprecating this function)
             {
+                // Some apps (such as dialog editors) depend on an empty sizer still
+                // being laid out correctly and reporting the correct size and position.
+                if (m_sizer->GetChildren().GetCount() == 0)
+                    return true;
+
                 for ( wxSizerItemList::compatibility_iterator
                         node = m_sizer->GetChildren().GetFirst();
                       node;
