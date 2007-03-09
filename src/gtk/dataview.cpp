0a5af992db5324fb1163ee0ccc23b96d66d8d279
Vadim Zeitlin <vadim@wxwidgets.org>
2007-03-09 16:19:45 +0000

    compilation fix for wxUSE_STL==1 build
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@44700 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/dataview.cpp
+++ src/gtk/dataview.cpp
@@ -817,7 +817,8 @@ bool wxGtkDataViewListModelNotifier::RowChanged( unsigned int row )
 bool wxGtkDataViewListModelNotifier::ValueChanged( unsigned int model_col, unsigned int model_row )
 {
     // This adds GTK+'s missing MVC logic for ValueChanged
-    wxNode *node = GetOwner()->m_viewingColumns.GetFirst();
+    wxObjectList::compatibility_iterator
+        node = GetOwner()->m_viewingColumns.GetFirst();
     while (node)
     {
         wxDataViewViewingColumn* viewing_column = (wxDataViewViewingColumn*) node->GetData();
@@ -854,7 +855,8 @@ bool wxGtkDataViewListModelNotifier::RowsReordered( unsigned int *new_order )
     gtk_tree_path_free (path);
 
     // This adds GTK+'s missing MVC logic for RowsReordered
-    wxNode *node = GetOwner()->m_viewingColumns.GetFirst();
+    wxObjectList::compatibility_iterator
+        node = GetOwner()->m_viewingColumns.GetFirst();
     while (node)
     {
         wxDataViewViewingColumn* viewing_column = (wxDataViewViewingColumn*) node->GetData();
