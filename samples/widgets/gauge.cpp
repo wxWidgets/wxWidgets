c6ba246b9870410d706295fb87d1da57878e2a06
Vadim Zeitlin <vadim@wxwidgets.org>
2007-03-20 16:23:12 +0000

    no changes, just fixed spelling of 'indeterminate'
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@44977 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/widgets/gauge.cpp
+++ samples/widgets/gauge.cpp
@@ -211,8 +211,7 @@ void GaugeWidgetsPage::CreateContent()
     sizerLeft->Add(btn, 0, wxALIGN_CENTRE_HORIZONTAL | wxALL, 15);
 
     // middle pane
-    wxStaticBox *box2 = new wxStaticBox(this, wxID_ANY,
-        _T("&Change gauge value"));
+    wxStaticBox *box2 = new wxStaticBox(this, wxID_ANY, _T("&Change gauge value"));
     wxSizer *sizerMiddle = new wxStaticBoxSizer(box2, wxVERTICAL);
 
     wxTextCtrl *text;
@@ -239,7 +238,8 @@ void GaugeWidgetsPage::CreateContent()
     btn = new wxButton(this, GaugePage_Progress, _T("Simulate &progress"));
     sizerMiddle->Add(btn, 0, wxALL | wxGROW, 5);
 
-    btn = new wxButton(this, GaugePage_IndeterminateProgress, _T("Simulate undeterminate job"));
+    btn = new wxButton(this, GaugePage_IndeterminateProgress,
+                       _T("Simulate &indeterminate job"));
     sizerMiddle->Add(btn, 0, wxALL | wxGROW, 5);
 
     btn = new wxButton(this, GaugePage_Clear, _T("&Clear"));
@@ -345,7 +345,7 @@ void GaugeWidgetsPage::StopTimer(wxButton *clicked)
     }
     else
     {
-        clicked->SetLabel(_T("Simulate undeterminate job"));
+        clicked->SetLabel(_T("Simulate indeterminate job"));
         FindWindow(GaugePage_Progress)->Enable();
     }
 
