996173c401b1b3d162e48edb400774402512ae23
Vadim Zeitlin <vadim@wxwidgets.org>
2007-04-01 14:14:58 +0000

    fix unused parameter warning when wxHAS_NATIVE_OVERLAY (patch 1692332)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@45200 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/generic/dragimgg.h
+++ include/wx/generic/dragimgg.h
@@ -157,13 +157,14 @@ public:
     // Attributes
     ////////////////////////////////////////////////////////////////////////////
 
+#ifdef wxHAS_NATIVE_OVERLAY
+    // backing store is not used when native overlays are
+    void SetBackingBitmap(wxBitmap* WXUNUSED(bitmap)) { }
+#else
     // For efficiency, tell wxGenericDragImage to use a bitmap that's already
     // created (e.g. from last drag)
-    void SetBackingBitmap(wxBitmap* bitmap) { 
-#ifndef wxHAS_NATIVE_OVERLAY
-        m_pBackingBitmap = bitmap; 
-#endif
-    }
+    void SetBackingBitmap(wxBitmap* bitmap) { m_pBackingBitmap = bitmap; }
+#endif // wxHAS_NATIVE_OVERLAY/!wxHAS_NATIVE_OVERLAY
 
     // Operations
     ////////////////////////////////////////////////////////////////////////////
