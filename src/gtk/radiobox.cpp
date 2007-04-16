178d7ec235d6309f3eda37f02238a67dcf81ff03
Vadim Zeitlin <vadim@wxwidgets.org>
2007-04-16 14:20:42 +0000

    removed superfluos semicolon
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@45499 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/radiobox.cpp
+++ src/gtk/radiobox.cpp
@@ -49,7 +49,7 @@ public:
 //-----------------------------------------------------------------------------
 
 #include "wx/listimpl.cpp"
-WX_DEFINE_LIST( wxRadioBoxButtonsInfoList );
+WX_DEFINE_LIST( wxRadioBoxButtonsInfoList )
 
 extern bool          g_blockEventsOnDrag;
 
