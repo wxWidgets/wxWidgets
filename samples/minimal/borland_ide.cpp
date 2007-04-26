1b54a5d2cc3fc3f9a291573eacec3c95ec8448c8
Chris Elliott <biol75@york.ac.uk>
2007-04-26 12:42:31 +0000

    add Borland Developer Studio project file (for Turbo Explorer) uses dll build
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@45665 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ samples/minimal/borland_ide.cpp
@@ -0,0 +1,34 @@
+/*************************************************
+// Name:        borland_ide.cpp
+// Author:      Chris Elliott
+// Modified by:
+// Created:     26/Apr/07
+// RCS-ID:      $Id$
+// Copyright:   (c) wxWidgets team
+// Licence:     wxWindows licence
+//
+// Purpose:     Minimal wxWidgets sample file for Borland
+// To use this with other samples, copy the borland_ide.cpp
+// and borland.bdsproj files, then replace all occurences of
+// "minimal" with the name of the new project files
+**************************************************/
+
+#define WinMain WinMain
+
+//wx libs (you may not need all of these
+#pragma link  "wxbase29d.lib"
+#pragma link  "wxmsw29d_adv.lib"
+#pragma link  "wxmsw29d_core.lib"
+#pragma link  "wxmsw29d_html.lib"
+#pragma link  "wxmsw29d_media.lib"
+#pragma link  "wxmsw29d_xrc.lib"
+#pragma link  "wxmsw29d_aui.lib"
+#pragma link  "wxmsw29d_stc.lib"
+#pragma link  "wxmsw29d_richtext.lib"
+
+//graphics libs
+#pragma link  "wxpngd.lib"
+#pragma link  "wxregexd.lib"
+#pragma link  "wxtiffd.lib"
+#pragma link  "wxzlibd.lib"
+
