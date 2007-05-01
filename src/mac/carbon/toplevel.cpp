09ac36f80b4bedc3e5ae3d37c20c2a8e5e970ae2
Stefan Csomor <csomor@advancedconcepts.ch>
2007-05-01 11:55:02 +0000

    Patch 1678304
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@45730 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/toplevel.cpp
+++ src/mac/carbon/toplevel.cpp
@@ -970,6 +970,10 @@ wxTopLevelWindowMac::~wxTopLevelWindowMac()
     FullScreenData *data = (FullScreenData *) m_macFullScreenData ;
     delete data ;
     m_macFullScreenData = NULL ;
+
+    // avoid dangling refs
+    if ( s_macDeactivateWindow == this )
+        s_macDeactivateWindow = NULL;
 }
 
 
