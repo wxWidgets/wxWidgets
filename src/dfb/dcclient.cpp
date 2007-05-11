643de214142254fda8cf0ca95572cff665b6f30d
Vadim Zeitlin <vadim@wxwidgets.org>
2007-05-11 17:16:05 +0000

    don't try to create surfaces of 0 size, even if the window size is 0 (as it may happen)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@45964 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/dfb/dcclient.cpp
+++ src/dfb/dcclient.cpp
@@ -80,6 +80,11 @@ wxIDirectFBSurfacePtr CreateDummySurface(wxWindow *win, const wxRect *rect)
     wxLogTrace(TRACE_PAINT, _T("%p ('%s'): creating dummy DC surface"),
                win, win->GetName().c_str());
     wxSize size(rect ? rect->GetSize() : win->GetSize());
+
+    // we can't create a surface of 0 size but the size of the window may be 0,
+    // so ensure that we have at least a single pixel to draw on
+    size.IncTo(wxSize(1, 1));
+
     return win->GetDfbSurface()->CreateCompatible
            (
              size,
