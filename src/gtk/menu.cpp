b8a922d0d3fcb40934430f404c2cf3ef1e464fb8
Robert Roebling <robert@roebling.de>
2007-05-22 07:48:37 +0000

    [ 1721029 ] [GTK] memory leak in wxMenu
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@46166 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/menu.cpp
+++ src/gtk/menu.cpp
@@ -1018,6 +1018,8 @@ wxMenu::~wxMenu()
    {
        // see wxMenu::Init
        gtk_widget_unref( m_menu );
+       g_object_unref( m_accel );
+       
        // if the menu is inserted in another menu at this time, there was
        // one more reference to it:
        if ( m_owner )
