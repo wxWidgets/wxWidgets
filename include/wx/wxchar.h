13e24d79876b78545ba300e6b6871bc47af60848
Václav Slavík <vslavik@fastmail.fm>
2007-05-30 13:43:27 +0000

    wxchar.h doesn't have to be C file anymore, it's C++
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@46247 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/wxchar.h
+++ include/wx/wxchar.h
@@ -1,23 +1,21 @@
-/*
- * Name:        wx/wxchar.h
- * Purpose:     Declarations common to wx char/wchar_t usage (wide chars)
- * Author:      Joel Farley, Ove Kåven
- * Modified by: Vadim Zeitlin, Robert Roebling, Ron Lee
- * Created:     1998/06/12
- * RCS-ID:      $Id$
- * Copyright:   (c) 1998-2006 wxWidgets dev team
- * Licence:     wxWindows licence
- */
-
-/* THIS IS A C FILE, DON'T USE C++ FEATURES (IN PARTICULAR COMMENTS) IN IT */
+//////////////////////////////////////////////////////////////////////////////
+// Name:        wx/wxchar.h
+// Purpose:     Declarations common to wx char/wchar_t usage (wide chars)
+// Author:      Joel Farley, Ove Kåven
+// Modified by: Vadim Zeitlin, Robert Roebling, Ron Lee
+// Created:     1998/06/12
+// RCS-ID:      $Id$
+// Copyright:   (c) 1998-2006 wxWidgets dev team
+// Licence:     wxWindows licence
+///////////////////////////////////////////////////////////////////////////////
 
 #ifndef _WX_WXCHAR_H_
 #define _WX_WXCHAR_H_
 
-/* include wxChar type definition: */
+// include wxChar type definition:
 #include "wx/chartype.h"
 
-/* and wrappers for CRT functions: */
+// and wrappers for CRT functions:
 #include "wx/wxcrt.h"
 #include "wx/wxcrtvararg.h"
 
