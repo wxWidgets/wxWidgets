68fac1dc3f5ccadefd0fb1fd8ee3aac41e7a0c30
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2007-06-03 19:26:04 +0000

    no real changes; changed formatting
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_XTI@46302 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/xtixml.h
+++ include/wx/xtixml.h
@@ -18,14 +18,14 @@
 
 #include "wx/xtistrm.h"
 
-class WXDLLIMPEXP_XML wxXmlNode ;
+class WXDLLIMPEXP_XML wxXmlNode;
 
 class WXDLLIMPEXP_XML wxXmlWriter : public wxWriter
 {
 public :
 
-    wxXmlWriter( wxXmlNode * parent ) ;
-    virtual ~wxXmlWriter() ;
+    wxXmlWriter( wxXmlNode * parent );
+    virtual ~wxXmlWriter();
 
     //
     // streaming callbacks
@@ -39,42 +39,48 @@ public :
     // these callbacks really write out the values in the stream format
 
     // begins writing out a new toplevel entry which has the indicated unique name
-    virtual void DoBeginWriteTopLevelEntry( const wxString &name )  ;
+    virtual void DoBeginWriteTopLevelEntry( const wxString &name );
 
     // ends writing out a new toplevel entry which has the indicated unique name
-    virtual void DoEndWriteTopLevelEntry( const wxString &name )  ;
+    virtual void DoEndWriteTopLevelEntry( const wxString &name );
 
     // start of writing an object having the passed in ID
-    virtual void DoBeginWriteObject(const wxObject *object, const wxClassInfo *classInfo, int objectID , wxxVariantArray &metadata ) ;
+    virtual void DoBeginWriteObject(const wxObject *object, 
+        const wxClassInfo *classInfo, int objectID , wxxVariantArray &metadata );
 
-    // end of writing an toplevel object name param is used for unique identification within the container
-    virtual void DoEndWriteObject(const wxObject *object, const wxClassInfo *classInfo, int objectID ) ;
+    // end of writing an toplevel object name param is used for unique 
+    // identification within the container
+    virtual void DoEndWriteObject(const wxObject *object, 
+        const wxClassInfo *classInfo, int objectID );
 
     // writes a simple property in the stream format
-    virtual void DoWriteSimpleType( wxxVariant &value )  ;
+    virtual void DoWriteSimpleType( wxxVariant &value );
 
     // start of writing a complex property into the stream (
-    virtual void DoBeginWriteProperty( const wxPropertyInfo *propInfo )  ;
+    virtual void DoBeginWriteProperty( const wxPropertyInfo *propInfo );
 
     // end of writing a complex property into the stream
-    virtual void DoEndWriteProperty( const wxPropertyInfo *propInfo ) ;
+    virtual void DoEndWriteProperty( const wxPropertyInfo *propInfo );
 
-    virtual void DoBeginWriteElement() ;
-    virtual void DoEndWriteElement() ;
+    virtual void DoBeginWriteElement();
+    virtual void DoEndWriteElement();
 
     // insert an object reference to an already written object
-    virtual void DoWriteRepeatedObject( int objectID )  ;
+    virtual void DoWriteRepeatedObject( int objectID );
 
     // insert a null reference
-    virtual void DoWriteNullObject()  ;
+    virtual void DoWriteNullObject();
 
     // writes a delegate in the stream format
-    virtual void DoWriteDelegate( const wxObject *object,  const wxClassInfo* classInfo , const wxPropertyInfo *propInfo ,
-        const wxObject *eventSink , int sinkObjectID , const wxClassInfo* eventSinkClassInfo , const wxHandlerInfo* handlerIndo ) ;
-private :
-    struct wxXmlWriterInternal ;
-    wxXmlWriterInternal* m_data ;
-} ;
+    virtual void DoWriteDelegate( const wxObject *object,  
+        const wxClassInfo* classInfo , const wxPropertyInfo *propInfo ,
+        const wxObject *eventSink , int sinkObjectID , 
+        const wxClassInfo* eventSinkClassInfo , const wxHandlerInfo* handlerIndo );
+
+private:
+    struct wxXmlWriterInternal;
+    wxXmlWriterInternal* m_data;
+};
 
 /*
 wxXmlReader handles streaming in a class from XML
@@ -83,13 +89,13 @@ wxXmlReader handles streaming in a class from XML
 class WXDLLIMPEXP_XML wxXmlReader : public wxReader
 {
 public:
-    wxXmlReader(wxXmlNode *parent) { m_parent = parent ; }
+    wxXmlReader(wxXmlNode *parent) { m_parent = parent; }
     virtual ~wxXmlReader() {}
 
     // Reads a component from XML.  The return value is the root object ID, which can
     // then be used to ask the depersister about that object
 
-    virtual int ReadObject( const wxString &name , wxDepersister *depersist ) ;
+    virtual int ReadObject( const wxString &name , wxDepersister *depersist );
 
 private :
     int ReadComponent(wxXmlNode *parent, wxDepersister *callbacks);
@@ -98,7 +104,7 @@ private :
     wxxVariant ReadValue(wxXmlNode *Node,
         const wxTypeInfo *type );
 
-    wxXmlNode * m_parent ;
+    wxXmlNode * m_parent;
 };
 
 #endif // wxUSE_EXTENDED_RTTI
