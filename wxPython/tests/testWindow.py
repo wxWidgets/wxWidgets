3d4a02ea4ce161d7f9aa647745c22a275b98ea3b
Frank Tobia <frank.tobia@gmail.com>
2007-06-06 01:36:14 +0000

    Additional tests added.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46339 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/tests/testWindow.py
+++ wxPython/tests/testWindow.py
@@ -39,7 +39,6 @@ FindWindowById
 FindWindowByName
 Fit
 FitInside
-GetAcceleratorTable
 GetAdjustedBestSize -> Use GetEffectiveMinSize instead.
 GetAutoLayout
 GetBestFittingSize(*args, **kwargs) -> Use GetEffectiveMinSize instead.
@@ -75,24 +74,14 @@ GetHelpTextAtPoint
 GetId
 GetLabel
 GetLayoutDirection
-GetMaxHeight
-GetMaxSize
-GetMaxWidth
-GetMinHeight
-GetMinSize
-GetMinWidth
-GetName
-GetParent
 GetPosition
 GetPositionTuple
-GetRect
 GetScreenPosition
 GetScreenPositionTuple
 GetScreenRect
 GetScrollPos
 GetScrollRange
 GetScrollThumb
-GetSize
 GetSizer
 GetSizeTuple
 GetTextExtent
@@ -164,7 +153,6 @@ ScrollLines
 ScrollPages
 ScrollWindow
 SendSizeEvent
-SetAcceleratorTable
 SetAutoLayout
 SetBestFittingSize -> Use SetInitialSize
 SetCaret
@@ -189,18 +177,12 @@ SetId
 SetInitialSize
 SetLabel
 SetLayoutDirection
-SetMaxSize
-SetMinSize
-SetName
 SetOwnBackgroundColour
 SetOwnFont
 SetOwnForegroundColour
 SetPosition
-SetRect
 SetScrollbar
 SetScrollPos
-SetSize
-SetSizeHints
 SetSizeHintsSz
 SetSizer
 SetSizerAndFit
@@ -251,6 +233,7 @@ class WindowTest(unittest.TestCase):
         self.RED = wx.Color(255,0,0)
         self.GREEN = wx.Color(0,255,0)
         self.BLUE = wx.Color(0,0,255)
+        # TODO: delegate to testColour module
         self.COLOUR_TESTS = (
                         (wx.Color(255,0,0), self.RED), 
                         (wx.Color(0,255,0), self.GREEN),
@@ -264,7 +247,31 @@ class WindowTest(unittest.TestCase):
                         ((255,0,0), self.RED),
                         ((0,255,0), self.GREEN),
                         ((0,0,255), self.BLUE)
-                   )
+                    )
+        # TODO: delegate to testSize module
+        self.SIZES = ( 
+                        wx.Size(1,1), wx.Size(5,5),
+                        wx.Size(10,10), wx.Size(10,1000),
+                        wx.Size(100,100), wx.Size(100,500),
+                        wx.Size(200,100), wx.Size(500,500),
+                        wx.Size(1000,1000), wx.Size(1,1000),
+                        wx.Size(1000,1), wx.Size(314, 217),
+                        wx.Size(31415, 27182), wx.Size(0,0)
+                    )
+        # TODO: delegate to testRect module
+        # TODO: find out if 32767 is some Windows limit, or a hard one.
+        #       Can we get rid of this magic number?
+        self.RECTS = (
+                        wx.Rect(1,1,1,1), wx.Rect(1,1,1000,1000),
+                        wx.Rect(1000,1000,1,1), wx.Rect(1000,1000,1000,1000),
+                        wx.Rect(10,200,3000,1), wx.Rect(1,999,111,9999),
+                        wx.Rect(32767,32767,32767,32767), wx.Rect()
+                    )
+        self.SIZE_HINTS = ( # minW, minH, maxW, maxH
+                            (0,0,1000,1000), (0,0,1,1),
+                            (0,0,0,0), (10,10,100,100),
+                            (1000,1000,9999,9999), (1, 10, 100, 1000)
+                        )
 
     def tearDown(self):
         self.frame.Destroy()
@@ -333,10 +340,69 @@ class WindowTest(unittest.TestCase):
         self.assert_(self.testControl.IsFrozen())
         self.testControl.Thaw()
         self.assert_(not self.testControl.IsFrozen())
-        
+    
+    # This fails because there does not exist a method for equality 
+    # checking of wxAcceleratorTables
+    # TODO: rewrite to manually check each AcceleratorEntry
+    # TODO: test the AcceleratorTable by firing simulated events
+    #               and checking responses
+    # TODO: implement C++ equality method in wxAcceleratorTable
+    def testAcceleratorTable(self):
+        '''SetAcceleratorTable, GetAcceleratorTable'''
+        # from wx.AcceleratorTable documentation, ids replaced
+        #aTable = wx.AcceleratorTable([(wx.ACCEL_ALT,  ord('X'), wx.ID_ANY),
+        #                      (wx.ACCEL_CTRL, ord('H'), wx.ID_ANY),
+        #                      (wx.ACCEL_CTRL, ord('F'), wx.ID_ANY),
+        #                      (wx.ACCEL_NORMAL, wx.WXK_F3, wx.ID_ANY)
+        #                      ])
+        #self.testControl.SetAcceleratorTable(aTable)
+        #self.assertEquals(aTable, self.testControl.GetAcceleratorTable())
+        pass
+    
     def testParent(self):
+        '''GetParent'''
         parent = self.testControl.GetParent()
         self.assertEquals(parent, self.frame)
+    
+    def testSize(self):
+        '''SetSize, GetSize'''
+        for size in self.SIZES:
+            self.testControl.SetSize(size)
+            self.assertEquals(size, self.testControl.GetSize())
+    
+    def testMinSize(self):
+        '''SetMinSize, GetMinSize'''
+        for min_size in self.SIZES:
+            self.testControl.SetMinSize(min_size)
+            self.assertEquals(min_size, self.testControl.GetMinSize())
+            
+    def testMaxSize(self):
+        '''SetMaxSize, GetMaxSize'''
+        for max_size in self.SIZES:
+            self.testControl.SetMaxSize(max_size)
+            self.assertEquals(max_size, self.testControl.GetMaxSize())
+    
+    def testSizeHints(self):
+        '''SetSizeHints, GetMinWidth, GetMinHeight, GetMaxWidth, GetMaxHeight'''
+        for minW, minH, maxW, maxH in self.SIZE_HINTS:
+            self.testControl.SetSizeHints(minW, minH, maxW, maxH)
+            self.assertEquals(minW, self.testControl.GetMinWidth())
+            self.assertEquals(minH, self.testControl.GetMinHeight())
+            self.assertEquals(maxW, self.testControl.GetMaxWidth())
+            self.assertEquals(maxH, self.testControl.GetMaxHeight())
+        self.assertRaises(wx.PyAssertionError, self.testControl.SetSizeHints, 100,100,10,10)
+    
+    def testRect(self):
+        '''SetRect, GetRect'''
+        for rect in self.RECTS:
+            self.testControl.SetRect(rect)
+            self.assertEquals(rect, self.testControl.GetRect())
+    
+    def testName(self):
+        '''SetName, GetName'''
+        self.testControl.SetName("The Name of the Panel")
+        self.assertEquals("The Name of the Panel", self.testControl.GetName())
+    
 
 def suite():
     suite = unittest.makeSuite(WindowTest)
