aea0716deab42aeaafa46e2c21f37d64fd620d9e
Frank Tobia <frank.tobia@gmail.com>
2007-06-12 15:17:25 +0000

    Impelement test for "Get" and associated helper method.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46422 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/tests/testRect.py
+++ wxPython/tests/testRect.py
@@ -5,9 +5,9 @@ import wx
 This file contains classes and methods for unit testing the API of wx.Rect
         
 Methods yet to test:
-__init__, __add__, __del__, __eq__, __getitem__, __iadd__, __len__, __ne__, __nonzero__,
+__add__, __del__, __eq__, __getitem__, __iadd__, __len__, __ne__, __nonzero__,
 __reduce__, __repr__, __setitem__, __str__, CenterIn, CentreIn, Contains, ContainsRect,
-ContainsXY, Deflate, Get, GetBottom, GetBottomLeft, GetBottomRight, GetHeight, GetLeft,
+ContainsXY, Deflate, GetBottom, GetBottomLeft, GetBottomRight, GetHeight, GetLeft,
 GetPosition, GetRight, GetSize, GetTop, GetTopLeft, GetTopRight, GetWidth, GetX, GetY,
 Inflate, Inside, InsideRect, InsideXY, Intersect, Intersects, IsEmpty, Offset, OffsetXY,
 Set, SetBottom, SetBottomLeft, SetBottomRight, SetHeight, SetLeft, SetPosition,
@@ -19,11 +19,21 @@ asTuple -> Use Get instead.
 def getRectData():
     # TODO: find out if 32767 is some Windows limit, or a hard one.
     #       Can we get rid of this magic number?
+    # NOTE: a wx.Rect can be created with values of greater than 32767,
+    #       but when returned from wx.Window, that's the max.
     return (
                 wx.Rect(1,1,1,1), wx.Rect(1,1,1000,1000),
                 wx.Rect(1000,1000,1,1), wx.Rect(1000,1000,1000,1000),
                 wx.Rect(10,200,3000,1), wx.Rect(1,999,111,9999),
                 wx.Rect(32767,32767,32767,32767), wx.Rect()
+        )
+        
+def getRectTuples():
+    return (
+                (1,1,1,1), (1,1,1000,1000), (1000,1000,1,1), (1000,1000,1000,1000),
+                (10,200,3000,1), (1,999,111,9999), (32767,32767,32767,32767),
+                (9,99,999,9999), (314,159,265,358), (-1,-1,-1,-1), (-9,99,-999,9999),
+                (-32767,-32767,-32767,-32767), (-32767,32767,-32767,32767)
         )
 
 # -----------------------------------------------------------
@@ -41,6 +51,14 @@ class RectTest(unittest.TestCase):
     ##################
     ## Test Methods ##
     ##################
+    
+    def testGet(self):
+        """__init__, Get"""
+        for x,y,w,h in getRectTuples():
+            rect = wx.Rect(x,y,w,h)
+            self.assertEquals((x,y,w,h), rect.Get())
+        rect = wx.Rect()
+        self.assertEquals((0,0,0,0), rect.Get())
     
 
 
