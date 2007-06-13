cc10fcc4a85ecd6c8c0819b07727aa753232b505
Stefan Csomor <csomor@advancedconcepts.ch>
2007-06-13 03:46:20 +0000

    universal os x fixes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@46432 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/popupwin.h
+++ include/wx/popupwin.h
@@ -66,6 +66,8 @@ public:
     #include "wx/motif/popupwin.h"
 #elif defined(__WXMGL__)
     #include "wx/mgl/popupwin.h"
+#elif defined(__WXMAC__)
+    #include "wx/mac/popupwin.h"
 #else
     #error "wxPopupWindow is not supported under this platform."
 #endif
