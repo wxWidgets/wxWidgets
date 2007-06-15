104251bdee0b93b6d4da2a368bf115fd0f54f607
Frank Tobia <frank.tobia@gmail.com>
2007-06-15 20:34:22 +0000

    Skeleton tests for wx.ListBox and wx.Sizer. testListBox is the first concrete test based upon testControl.ControlWithItemsBase.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46484 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ wxPython/tests/testListBox.py
@@ -0,0 +1,41 @@
+import unittest
+import wx
+
+import testControl
+
+"""
+This file contains classes and methods for unit testing the API of wx.ListBox.
+        
+Methods yet to test:
+__init__, AppendAndEnsureVisible, Create, Deselect, DeselectAll, EnsureVisible,
+GetClassDefaultAttributes, GetSelections, HitTest, Insert, InsertItems, IsSelected,
+IsSorted, Select, Set, SetFirstItem, SetFirstItemStr, SetItemBackgroundColour,
+SetItemFont, SetItemForegroundColour, SetSelection, SetStringSelection
+
+"""
+
+class ListBoxTest(testControl.ControlWithItemsBase):
+    #####################
+    ## Fixture Methods ##
+    #####################
+    def setUp(self):
+        self.app = wx.PySimpleApp()
+        self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
+        self.testControl = wx.ListBox(parent=self.frame, id=wx.ID_ANY)
+    
+    def tearDown(self):
+        self.app.Destroy()
+        self.frame.Destroy()
+        
+    ##################
+    ## Test Methods ##
+    ##################
+    
+        
+
+def suite():
+    suite = unittest.makeSuite(ListBoxTest)
+    return suite
+    
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
---
+++ wxPython/tests/testSizer.py
@@ -0,0 +1,66 @@
+import unittest
+import wx
+
+"""
+This file contains classes and methods for unit testing the API of wx.Sizer.
+
+Since wx.Sizer is an abstract base class, tests implemented for wx.Sizer will
+instead be run on instances of derived classes.
+        
+Methods yet to test:
+__init__, __del__, Add, AddF, AddItem, AddMany, AddSizer, AddSpacer, AddStretchSpacer,
+AddWindow, CalcMin, Clear, DeleteWindows, Detach, Fit, FitInside, GetChildren,
+GetContainingWindow, GetItem, GetMinSize, GetMinSizeTuple, GetPosition, GetPositionTuple,
+GetSize, GetSizeTuple, Hide, Insert,InsertF, InsertItem, InsertSizer, InsertSpacer,
+InsertStretchSpacer, InsertWindow, IsShown, Layout, Prepend, PrependF, PrependItem,
+PrependSizer, PrependSpacer, PrependStretchSpacer, PrependWindow, RecalcSizes, Remove,
+RemovePos, RemoveSizer, RemoveWindow, Replace, SetContainingWindow, SetDimension,
+SetItemMinSize, SetMinSize, SetSizeHints, SetVirtualSizeHints, Show, ShowItems
+"""
+
+class SizerTest(unittest.TestCase):
+
+    #####################
+    ## Fixture Methods ##
+    #####################
+    def setUp(self):
+        self.app = wx.PySimpleApp()
+    
+    def tearDown(self):
+        self.app.Destroy()
+        
+    ##################
+    ## Test Methods ##
+    ##################
+    
+    def testConstructorFails(self):
+        self.assertRaises(AttributeError, wx.Sizer)
+    
+# -----------------------------------------------------------
+
+class SizerBase(unittest.TestCase):
+    
+    # to be overridden in derived classes
+    #####################
+    ## Fixture Methods ##
+    #####################
+    def setUp(self):
+        self.app = wx.PySimpleApp()
+        self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
+        self.testControl = None
+    
+    def tearDown(self):
+        self.app.Destroy()
+        self.frame.Destroy()
+        
+    ##################
+    ## Test Methods ##
+    ##################
+
+
+def suite():
+    suite = unittest.makeSuite(SizerTest)
+    return suite
+    
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
