faf75a0f5883ef1e6e9a7a3adf164fe7353b5b09
Václav Slavík <vslavik@fastmail.fm>
2007-06-17 09:29:41 +0000

    don't make read-only text controls editable when enabled
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@46502 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/textctrl.cpp
+++ src/gtk/textctrl.cpp
@@ -1285,14 +1285,7 @@ bool wxTextCtrl::Enable( bool enable )
         return false;
     }
 
-    if ( IsMultiLine() )
-    {
-        SetEditable( enable );
-    }
-    else
-    {
-        gtk_widget_set_sensitive( m_text, enable );
-    }
+    gtk_widget_set_sensitive( m_text, enable );
 
     return true;
 }
