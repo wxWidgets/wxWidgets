31da3adf075ec08f356a8df04353c99c4c23ef12
Frank Tobia <frank.tobia@gmail.com>
2007-06-18 14:18:47 +0000

    runAllTests displays data on each module run.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46516 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/tests/runAllTests.py
+++ wxPython/tests/runAllTests.py
@@ -33,22 +33,34 @@ modules = (testButton, testColor, testControl, testFont, testItemContainer,
             testWindow)
 
 alltests = unittest.TestSuite([mod.suite() for mod in modules])
+maxstr = max(( mod.__name__ for mod in modules ))
+maxlen = len(maxstr)
 
 results = unittest.TestResult()
 alltests.run(results)
 
-if results.wasSuccessful():
-    print "%d tests passed!" % (results.testsRun)
-else:
-    print "\n%d tests failed!\n" % (len(results.failures))
-    for error in results.failures:
-        print "------ " + str(error[0]) + " ------"
-        print error[1]
+total = 0
+for module in modules:
+    suite = module.suite()
+    results = unittest.TestResult()
+    suite.run(results)
     
-    for error in results.errors:
-        print "------ " + str(error[0]) + " ------"
-        print error[1]
-    sys.exit(1)
+    if results.wasSuccessful():
+        print "%s:\t%d tests passed" % (module.__name__, results.testsRun)
+        total += results.testsRun
+    else:
+        print "\n%d tests failed!\n" % (len(results.failures))
+        for error in results.failures:
+            print "------ " + str(error[0]) + " ------"
+            print error[1]
+
+        for error in results.errors:
+            print "------ " + str(error[0]) + " ------"
+            print error[1]
+
+# TODO: add reporting for failures as well
+print "----------------------"
+print "%d tests passed in total!" % (total)
 
 figleaf.stop()
 figleaf.write_coverage(figfile)
