ff2a41ca6770a4a4892fb8f3f1089eac5030e714
Frank Tobia <frank.tobia@gmail.com>
2007-06-22 17:07:24 +0000

    Two more tests for wx.TopLevelWindow. Fixed one minor oversight.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46638 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/tests/testTopLevelWindow.py
+++ wxPython/tests/testTopLevelWindow.py
@@ -9,21 +9,41 @@ wx.TopLevelWindow.
 
 Methods yet to test for wx.TopLevelWindow:
 __init__, CenterOnScreen, CentreOnScreen, EnableCloseButton, GetDefaultItem, GetIcon,
-GetTitle, GetTmpDefaultItem, Iconize, IsActive, IsAlwaysMaximized, IsFullScreen,
-IsIconized, IsMaximized, MacGetMetalAppearance, MacSetMetalAppearance, Maximize,
-RequestUserAttention, Restore, SetDefaultItem, SetIcon, SetIcons, SetShape, SetTitle,
-SetTmpDefaultItem, ShowFullScreen
+GetTmpDefaultItem, Iconize, IsActive, IsAlwaysMaximized, IsFullScreen, IsIconized,
+MacGetMetalAppearance, MacSetMetalAppearance, RequestUserAttention, Restore, SetDefaultItem,
+SetIcon, SetIcons, SetShape, SetTmpDefaultItem, ShowFullScreen
 """
 
 class TopLevelWindowBase(testWindow.WindowTest):
     def setUp(self):
         self.app = wx.PySimpleApp()
         self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
-        self.testControl = wx.Control(parent=self.frame, id=wx.ID_ANY)
+        self.testControl = None
     
     def tearDown(self):
         self.frame.Destroy()
         self.app.Destroy()
+    
+    def testMaximize(self):
+        """Maximize, IsMaximized"""
+        self.testControl.Maximize()
+        self.assert_(self.testControl.IsMaximized())
+        self.testControl.Maximize(False)
+        self.assert_(not self.testControl.IsMaximized())
+        self.testControl.Maximize(True)
+        self.assert_(self.testControl.IsMaximized())
+    
+    def testTitle(self):
+        """SetTitle, GetTitle"""
+        class_under_test = type(self.testControl)
+        title = "Hello, World!"
+        t = class_under_test(parent=self.frame, id=wx.ID_ANY, title=title)
+        self.assertEquals(title, t.GetTitle())
+        self.testControl.SetTitle(title)
+        self.assertEquals(title, self.testControl.GetTitle())
+        title2 = "The quick brown fox jumps over the lazy dog"
+        self.testControl.SetTitle(title2)
+        self.assertEquals(title2, self.testControl.GetTitle())
         
     def testTopLevel(self):
         """IsTopLevel"""
