bdcb213757b966746ffc73bf01ec2e838d867cd3
Václav Slavík <vslavik@fastmail.fm>
2007-06-25 17:53:30 +0000

    compilation fix if wxLongLong_t is not available
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@46941 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/wxcrt.cpp
+++ src/common/wxcrt.cpp
@@ -866,6 +866,7 @@ wxCRT_StrftimeW(wchar_t *s, size_t maxsize, const wchar_t *fmt, const struct tm 
 
 #endif // wxUSE_WCHAR_T
 
+#ifdef wxLongLong_t
 template<typename T>
 static wxULongLong_t
 wxCRT_StrtoullBase(const T* nptr, T** endptr, int base, T* sign)
@@ -1018,6 +1019,8 @@ wxULongLong_t wxCRT_StrtoullA(const char* nptr, char** endptr, int base)
 wxULongLong_t wxCRT_StrtoullW(const wchar_t* nptr, wchar_t** endptr, int base)
     { return wxCRT_DoStrtoull(nptr, endptr, base); }
 #endif
+
+#endif // wxLongLong_t
 
 // ----------------------------------------------------------------------------
 // functions which we may need even if !wxUSE_WCHAR_T
