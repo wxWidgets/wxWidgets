71506e2d25a10259fd1170b8c17f4417a443b141
Roman Rolinsky <rolinsky@femagsoft.com>
2007-06-25 23:55:02 +0000

    test SVN commit
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_XRCED@46948 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wx/tools/XRCed/test.py
+++ wxPython/wx/tools/XRCed/test.py
@@ -1,12 +1,23 @@
 # Test module for future super xrced version :)
+import os
 from globals import *
 import view
 from presenter import Presenter
 from listener import Listener
+from component import Manager
 import undo
 import plugin
 
+# Set application path for loading resources
+if __name__ == '__main__':
+    g.basePath = os.path.dirname(sys.argv[0])
+else:
+    g.basePath = os.path.dirname(__file__)
+
 app = wx.PySimpleApp()
+
+toolArtProvider = view.ToolArtProvider()
+wx.ArtProvider.Push(toolArtProvider)
 
 conf = g.conf = wx.Config(style = wx.CONFIG_USE_LOCAL_FILE)
 conf.localconf = None
@@ -23,13 +34,15 @@ else:
     conf.panelX = conf.panelY = -1
 conf.panelWidth = conf.ReadInt('panelWidth', 200)
 conf.panelHeight = conf.ReadInt('panelHeight', 200)
-
+# Load file history from config
 g.fileHistory = wx.FileHistory()
 g.fileHistory.Load(conf)
 
-plugin.load_plugins('plugins')
-
 g.undoMan = undo.UndoManager()
+
+Manager.init()
+# Load standard plugins
+plugin.load_plugins('plugins')
 
 # Setup MVP
 view.CreateView()
@@ -37,6 +50,6 @@ Presenter.init()
 Listener.Install(view.frame, view.tree, view.panel)
 
 if len(sys.argv) > 1:
-    Presenter.loadXML(sys.argv[1])
+    Presenter.open(sys.argv[1])
 view.frame.Show()
 app.MainLoop()
