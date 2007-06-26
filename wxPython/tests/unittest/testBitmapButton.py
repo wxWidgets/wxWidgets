716bf742cdd4f07309573d667c78b93e661ea3c7
Frank Tobia <frank.tobia@gmail.com>
2007-06-26 01:26:47 +0000

    Add skeleton tests for wx.BitmapButton. Why does the creating the test control cause the interpreter to crash?
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46952 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ wxPython/tests/unittest/testBitmapButton.py
@@ -0,0 +1,38 @@
+import unittest
+import wx
+
+import testButton
+
+"""
+This file contains classes and methods for unit testing the API of 
+wx.BitmapButton
+
+Methods yet to test:
+__init__, Create, GetBitmapDisabled, GetBitmapFocus, GetBitmapHover,
+GetBitmapLabel, GetBitmapSelected, GetMarginX, GetMarginY,
+SetBitmapDisabled, SetBitmapFocus, SetBitmapHover, SetBitmapLabel,
+SetBitmapSelected, SetMargins
+"""
+
+
+class BitmapButtonTest(testButton.ButtonTest):
+    def setUp(self):
+        self.app = wx.PySimpleApp()
+        self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
+        self.testControl = wx.BitmapButton(parent=self.frame, bitmap=wx.NullBitmap)
+    
+    def tearDown(self):
+        self.app.Destroy()
+        self.frame.Destroy()
+        
+    def testTest(self):
+        self.assert_(True)
+        
+        
+
+def suite():
+    suite = unittest.makeSuite(BitmapButtonTest)
+    return unittest.TestSuite(suite)
+    
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
