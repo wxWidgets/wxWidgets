dea7d926225c6acea72a916ed5379ec9f4f08075
Frank Tobia <frank.tobia@gmail.com>
2007-06-27 18:05:12 +0000

    More tests for wx.SpinCtrl.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@46984 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/tests/unittest/testSpinCtrl.py
+++ wxPython/tests/unittest/testSpinCtrl.py
@@ -11,18 +11,7 @@ TODO:
     should it be legal for min value to be greater than max value?
 
 Methods yet to test:
-
-__init__
-Create
-GetClassDefaultAttributes
-GetMax
-GetMin
-GetValue
-SetRange
-SetSelection
-SetValue
-SetValueString
-
+__init__, Create, GetClassDefaultAttributes, SetSelection
 """
 
 class SpinCtrlTest(testControl.ControlTest):
@@ -35,8 +24,56 @@ class SpinCtrlTest(testControl.ControlTest):
     def testAllControlsNeedParents(self):
         if wxtest.PlatformIsNotWindows():
             super(SpinCtrlTest,self).testAllControlsNeedParents()
-        
-
+    
+    def testConstructor(self):
+        """__init__
+        'Nearly all of the complexity of the spin ontrol is in the constructor'
+            - pg.209, 1st paragraph, 'wxPython in Action' """
+        a = wx.SpinCtrl(parent=self.frame, min=-1000, max=1000,
+                            initial=42)
+        self.assertEquals(-1000, a.GetMin())
+        self.assertEquals(1000, a.GetMax())
+        self.assertEquals(42, a.GetValue())
+    
+    def testMinMaxRange(self):
+        """SetRange, GetMin, GetMax"""
+        for min,max in ((1,10),(-100,-10),(100,1000)):
+            self.testControl.SetRange(min,max)
+            self.assertEquals(min, self.testControl.GetMin())
+            self.assertEquals(max, self.testControl.GetMax())
+    
+    def testValue(self):
+        """SetValue, GetValue"""
+        min = self.testControl.GetMin()
+        max = self.testControl.GetMax()
+        for i in range(min,max+1):
+            self.testControl.SetValue(i)
+            self.assertEquals(i, self.testControl.GetValue())
+        for j in range(min-100,min):
+            self.testControl.SetValue(j)
+            self.assertEquals(min, self.testControl.GetValue())
+        for k in range(max,max+100):
+            self.testControl.SetValue(k)
+            self.assertEquals(max, self.testControl.GetValue())
+    
+    def testValueString(self):
+        """SetValueString"""
+        min = self.testControl.GetMin()
+        max = self.testControl.GetMax()
+        for i in range(min,max+1):
+            si = str(i)
+            self.testControl.SetValueString(si)
+            self.assertEquals(i, self.testControl.GetValue())
+        for j in range(min-100,min):
+            sj = str(j)
+            self.testControl.SetValueString(sj)
+            self.assertEquals(min, self.testControl.GetValue())
+        for k in range(max,max+100):
+            sk = str(k)
+            self.testControl.SetValueString(sk)
+            self.assertEquals(max, self.testControl.GetValue())
+            
+            
 def suite():
     suite = unittest.makeSuite(SpinCtrlTest)
     return unittest.TestSuite(suite)
