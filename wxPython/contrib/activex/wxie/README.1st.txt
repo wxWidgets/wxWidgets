bc3d006a2c9125d8cd718e9f1e626ba961b0dcd0
Robin Dunn <robin@alldunn.com>
2007-06-30 00:06:00 +0000

    moving wxPython SOC branches
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_UNITTEST@47035 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/contrib/activex/wxie/README.1st.txt
+++
@@ -1,6 +0,0 @@
-The contents of this dir are from the 9-Jan-2004 version of wxie.zip
-downloaded from:
- 
-    http://members.optusnet.com.au/~blackpaw1/wxactivex.html
-
-
--- wxPython/samples/wxPIA_book/Chapter-09/startup_tip.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-if __name__ == "__main__":
-    app = wx.PySimpleApp()
-    provider = wx.CreateFileTipProvider("tips.txt", 0)
-    wx.ShowTip(None, provider, True)
-    
-
