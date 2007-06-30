69635c0066b17d11a52dd1e4482609566f70efd0
Robin Dunn <robin@alldunn.com>
2007-06-30 00:06:00 +0000

    Move wxPython portion of wx 2.8 branch to new location
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@47034 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/wxPython/lib/rcsizer.py
+++
@@ -1,9 +0,0 @@
-## This file imports items from the wx package into the wxPython package for
-## backwards compatibility.  Some names will also have a 'wx' added on if
-## that is how they used to be named in the old wxPython package.
-
-import wx.lib.rcsizer
-
-__doc__ =  wx.lib.rcsizer.__doc__
-
-RowColSizer = wx.lib.rcsizer.RowColSizer
