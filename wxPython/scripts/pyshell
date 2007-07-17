85e4e97f2105ccc5421d6695a8e49aff819862a0
Bo Yang <techrazy.yang@gmail.com>
2007-07-17 02:46:36 +0000

    syc with the trunk
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2007_DVC@47520 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wxPython/scripts/pyshell
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-from wx.py.PyShell import main
-main()
--- wxPython/wxPython/lib/scrolledpanel.py
+++
@@ -1,9 +0,0 @@
-## This file imports items from the wx package into the wxPython package for
-## backwards compatibility.  Some names will also have a 'wx' added on if
-## that is how they used to be named in the old wxPython package.
-
-import wx.lib.scrolledpanel
-
-__doc__ =  wx.lib.scrolledpanel.__doc__
-
-wxScrolledPanel = wx.lib.scrolledpanel.ScrolledPanel
