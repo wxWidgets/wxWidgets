71e33cfbbddc293991796772c6e98a0e74ff826b
Vadim Zeitlin <vadim@wxwidgets.org>
2007-08-14 22:44:44 +0000

    no real changes, just some cleanup
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48077 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/taskbar.h
+++ include/wx/msw/taskbar.h
@@ -10,44 +10,42 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////
 
-#ifndef _TASKBAR_H_
-#define _TASKBAR_H_
+#ifndef _WX_TASKBAR_H_
+#define _WX_TASKBAR_H_
 
 #include "wx/icon.h"
 
 // private helper class:
 class WXDLLIMPEXP_FWD_ADV wxTaskBarIconWindow;
 
-class WXDLLIMPEXP_ADV wxTaskBarIcon: public wxTaskBarIconBase
+class WXDLLIMPEXP_ADV wxTaskBarIcon : public wxTaskBarIconBase
 {
-    DECLARE_DYNAMIC_CLASS_NO_COPY(wxTaskBarIcon)
 public:
     wxTaskBarIcon();
     virtual ~wxTaskBarIcon();
 
-// Accessors
-    inline bool IsOk() const { return true; }
-    inline bool IsIconInstalled() const { return m_iconAdded; }
+    // Accessors
+    bool IsOk() const { return true; }
+    bool IsIconInstalled() const { return m_iconAdded; }
 
-// Operations
+    // Operations
     bool SetIcon(const wxIcon& icon, const wxString& tooltip = wxEmptyString);
     bool RemoveIcon(void);
-    bool PopupMenu(wxMenu *menu); //, int x, int y);
+    bool PopupMenu(wxMenu *menu);
 
-// Implementation
 protected:
     friend class wxTaskBarIconWindow;
+
     long WindowProc(unsigned int msg, unsigned int wParam, long lParam);
     void RegisterWindowMessages();
 
-// Data members
-protected:
+
     wxTaskBarIconWindow *m_win;
     bool                 m_iconAdded;
     wxIcon               m_icon;
     wxString             m_strTooltip;
 
+    DECLARE_DYNAMIC_CLASS_NO_COPY(wxTaskBarIcon)
 };
 
-#endif
-    // _TASKBAR_H_
+#endif // _WX_TASKBAR_H_
