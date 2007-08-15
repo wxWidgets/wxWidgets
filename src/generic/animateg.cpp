48271822ef3d56c8f91af882b68fd1b674a8e8e6
Vadim Zeitlin <vadim@wxwidgets.org>
2007-08-15 11:36:50 +0000

    use one shot timers in wxAnimationCtrl (patch 1774535)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48085 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/animateg.cpp
+++ src/generic/animateg.cpp
@@ -418,7 +418,7 @@ bool wxAnimationCtrl::Play(bool looped)
     int delay = m_animation.GetDelay(0);
     if (delay == 0)
         delay = 1;      // 0 is invalid timeout for wxTimer.
-    m_timer.Start(delay);
+    m_timer.Start(delay, true);
 
     return true;
 }
@@ -663,7 +663,7 @@ void wxAnimationCtrl::OnTimer(wxTimerEvent &WXUNUSED(event))
     int delay = m_animation.GetDelay(m_currentFrame);
     if (delay == 0)
         delay = 1;      // 0 is invalid timeout for wxTimer.
-    m_timer.Start(delay);
+    m_timer.Start(delay, true);
 }
 
 void wxAnimationCtrl::OnSize(wxSizeEvent &WXUNUSED(event))
