fbbdc7bf854259c178b03c59ec4c8cca753026bd
Václav Slavík <vslavik@fastmail.fm>
2007-08-21 09:40:36 +0000

    use wxString's UTF8 methods in wxCocoa, it's slightly more efficient in UTF8 build
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48266 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/cocoa/string.h
+++ include/wx/cocoa/string.h
@@ -25,7 +25,7 @@
 inline NSString* wxNSStringWithWxString(const wxString &wxstring)
 {
 #if wxUSE_UNICODE
-    return [NSString stringWithUTF8String: wxstring.mb_str(wxConvUTF8)];
+    return [NSString stringWithUTF8String: wxstring.utf8_str()];
 #else
     return [NSString stringWithCString: wxstring.c_str() length:wxstring.Len()];
 #endif // wxUSE_UNICODE
@@ -35,7 +35,7 @@ inline NSString* wxNSStringWithWxString(const wxString &wxstring)
 inline NSString* wxInitNSStringWithWxString(NSString *nsstring, const wxString &wxstring)
 {
 #if wxUSE_UNICODE
-    return [nsstring initWithUTF8String: wxstring.mb_str(wxConvUTF8)];
+    return [nsstring initWithUTF8String: wxstring.utf8_str()];
 #else
     return [nsstring initWithCString: wxstring.c_str() length:wxstring.Len()];
 #endif // wxUSE_UNICODE
@@ -44,7 +44,7 @@ inline NSString* wxInitNSStringWithWxString(NSString *nsstring, const wxString &
 inline wxString wxStringWithNSString(NSString *nsstring)
 {
 #if wxUSE_UNICODE
-    return wxString([nsstring UTF8String], wxConvUTF8);
+    return wxString::FromUTF8([nsstring UTF8String]);
 #else
     return wxString([nsstring lossyCString]);
 #endif // wxUSE_UNICODE
