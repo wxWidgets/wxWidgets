4f33a4dd4c6e92ec613aa73737030e992f5d8c65
Robert Roebling <robert@roebling.de>
2007-08-26 09:32:00 +0000

    compile fix
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48389 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/settings.cpp
+++ src/gtk/settings.cpp
@@ -134,7 +134,10 @@ static bool GetColourFromGTKWidget(GdkColor& gdkColor,
 static void GetTooltipColors()
 {
     GtkWidget* widget = gtk_window_new(GTK_WINDOW_POPUP);
-    gtk_window_set_type_hint((GtkWindow*)widget, GDK_WINDOW_TYPE_HINT_TOOLTIP);
+#if GTK_CHECK_VERSION(2, 10, 0)
+    if (!gtk_check_version(2, 10, 0))
+        gtk_window_set_type_hint((GtkWindow*)widget, GDK_WINDOW_TYPE_HINT_TOOLTIP);
+#endif
     const char* name = "gtk-tooltip";
     if (gtk_check_version(2, 11, 0))
         name = "gtk-tooltips";
