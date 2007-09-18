8a2a5a449eeedc95cbd88d5638516c98a5e92a6c
Michael Wetherell <mike.wetherell@ntlworld.com>
2007-09-18 21:49:27 +0000

    template class WXDLLIMPEXP_FWD_XRC wxVector<wxXmlResourceHandler *>; instantiates as well as forward declares, which gives us multiple definition link errors on some systems. So trying WXDLLIMPEXP_FWD_XRC on the declaration of m_handlers instead.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48775 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/xrc/xmlres.h
+++ include/wx/xrc/xmlres.h
@@ -44,10 +44,6 @@ class wxXmlSubclassFactories;
 class wxXmlResourceModule;
 class wxXmlResourceDataRecords;
 
-// make VC++ happy: otherwise it gives warnings when using this type for a
-// member of DLL-exported wxXmlResource class in shared builds
-template class WXDLLIMPEXP_FWD_XRC wxVector<wxXmlResourceHandler *>;
-
 // These macros indicate current version of XML resources (this information is
 // encoded in root node of XRC file as "version" property).
 //
@@ -277,7 +273,7 @@ private:
     long m_version;
 
     int m_flags;
-    wxVector<wxXmlResourceHandler*> m_handlers;
+    class WXDLLIMPEXP_FWD_XRC wxVector<wxXmlResourceHandler*> m_handlers;
     wxXmlResourceDataRecords *m_data;
 #if wxUSE_FILESYSTEM
     wxFileSystem m_curFileSystem;
