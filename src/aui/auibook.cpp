afd8770decb39abb628b3a80800b5bba6cf19dd8
Julian Smart <julian@anthemion.co.uk>
2007-09-21 12:40:08 +0000

    Make SetFont actually do something
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48868 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/aui/auibook.cpp
+++ src/aui/auibook.cpp
@@ -4370,6 +4370,13 @@ bool wxAuiNotebook::SetFont(const wxFont& font)
     SetSelectedFont(selectedFont);
     SetMeasuringFont(selectedFont);
 
+    if (GetArtProvider())
+    {
+        GetArtProvider()->SetNormalFont(normalFont);
+        GetArtProvider()->SetSelectedFont(selectedFont);
+        GetArtProvider()->SetMeasuringFont(selectedFont);
+    }
+    
     return true;
 }
 
