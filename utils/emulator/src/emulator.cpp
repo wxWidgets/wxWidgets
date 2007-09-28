32b171bc0e6ba4b91c0c4b820b120a906c6dc646
Vadim Zeitlin <vadim@wxwidgets.org>
2007-09-28 22:17:37 +0000

    another compilation fix after wxCmdLineEntryDesc changes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@48985 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- utils/emulator/src/emulator.cpp
+++ utils/emulator/src/emulator.cpp
@@ -73,14 +73,14 @@ IMPLEMENT_APP(wxEmulatorApp)
 
 static const wxCmdLineEntryDesc sg_cmdLineDesc[] =
 {
-    { wxCMD_LINE_OPTION, _T("u"), _T("use-display"),   _T("display number to use (default 100)"), (wxCmdLineParamType)0, 0 },
+    { wxCMD_LINE_OPTION, "u", "use-display", "display number to use (default 100)" },
 
-    { wxCMD_LINE_SWITCH, _T("h"), _T("help"),   _T("displays help on the command line parameters"), (wxCmdLineParamType)0, 0 },
-    { wxCMD_LINE_SWITCH, _T("v"), _T("version"),    _T("print version"), (wxCmdLineParamType)0, 0 },
+    { wxCMD_LINE_SWITCH, "h", "help", "displays help on the command line parameters" },
+    { wxCMD_LINE_SWITCH, "v", "version", "print version" },
 
-    { wxCMD_LINE_PARAM,  NULL, NULL, _T("config file 1"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
+    { wxCMD_LINE_PARAM,  NULL, NULL, "config file 1", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
 
-    { wxCMD_LINE_NONE, NULL, NULL, NULL, (wxCmdLineParamType)0, 0 }
+    wxCMD_LINE_DESC_END
 };
 
 
