e48dd4e3ae31fdc3e078821635597303c8bc9708
Michael Wetherell <mike.wetherell@ntlworld.com>
2007-10-02 14:56:53 +0000

    Define CPPUNIT_STD_NEED_ALLOCATOR to 0 if it is undefined as we compile with -Wundef.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@49019 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/cppunit.h
+++ include/wx/cppunit.h
@@ -28,13 +28,17 @@
 #ifdef __BORLANDC__
     #pragma warn -8022
 #endif
- 
+
+#ifndef CPPUNIT_STD_NEED_ALLOCATOR
+    #define CPPUNIT_STD_NEED_ALLOCATOR 0
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 // Set the default format for the errors, which can be used by an IDE to jump
 // to the error location. This default gets overridden by the cppunit headers
 // for some compilers (e.g. VC++).
 
-#ifndef CPPUNIT_COMPILER_LOCATION_FORMAT 
+#ifndef CPPUNIT_COMPILER_LOCATION_FORMAT
     #define CPPUNIT_COMPILER_LOCATION_FORMAT "%p:%l:"
 #endif
 
