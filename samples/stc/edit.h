bfa30135ba766d12005f6294803d9b9ac377056b
Stefan Csomor <csomor@advancedconcepts.ch>
2007-10-22 08:13:51 +0000

    better appearance on mac w/o sunken border
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@49313 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/stc/edit.h
+++ samples/stc/edit.h
@@ -48,7 +48,11 @@ public:
     Edit (wxWindow *parent, wxWindowID id = wxID_ANY,
           const wxPoint &pos = wxDefaultPosition,
           const wxSize &size = wxDefaultSize,
-          long style = wxSUNKEN_BORDER|wxVSCROLL
+          long style = 
+#ifndef __WXMAC__
+          wxSUNKEN_BORDER|
+#endif
+          wxVSCROLL
          );
 
     //! destructor
