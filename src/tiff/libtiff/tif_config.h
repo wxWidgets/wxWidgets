143973fc139428db2a44267f6851ab127f96a807
Stefan Csomor <csomor@advancedconcepts.ch>
2007-10-22 13:07:48 +0000

    moving os x config defaults
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@49330 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/tiff/libtiff/tif_config.h
+++ src/tiff/libtiff/tif_config.h
@@ -1,3 +1,63 @@
+#ifdef __APPLE__
+
+/* Define to 1 if you have the <assert.h> header file. */
+#define HAVE_ASSERT_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define as 0 or 1 according to the floating point format suported by the
+   machine */
+#define HAVE_IEEEFP 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <io.h> header file. */
+/* #undef HAVE_IO_H */
+
+/* Define to 1 if you have the <search.h> header file. */
+#define HAVE_SEARCH_H 1
+
+/* The size of a `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of a `long', as computed by sizeof. */
+#ifdef __LP64__
+#define SIZEOF_LONG 8
+#else
+#define SIZEOF_LONG 4
+#endif
+
+#if __BIG_ENDIAN__
+    /* Set the native cpu bit order */
+    #define HOST_FILLORDER FILLORDER_MSB2LSB
+    
+    /* Define to 1 if your processor stores words with the most significant byte
+    first (like Motorola and SPARC, unlike Intel and VAX). */
+    #define WORDS_BIGENDIAN 1
+#else
+    /* Set the native cpu bit order */
+    #define HOST_FILLORDER FILLORDER_LSB2MSB
+    
+    /* Define to 1 if your processor stores words with the most significant byte
+        first (like Motorola and SPARC, unlike Intel and VAX). */
+    /* #undef WORDS_BIGENDIAN */
+#endif
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+# ifndef inline
+#  define inline __inline
+# endif
+#endif
+
+#else /* __APPLE__ */
+
 /* Define to 1 if you have the <assert.h> header file. */
 #define HAVE_ASSERT_H 1
 
@@ -45,13 +105,4 @@
    #define lfind _lfind
 #endif
 
-#ifdef __APPLE__
-    #undef lfind
-    #undef HAVE_IO_H
-    #if __BIG_ENDIAN__
-        #undef HOST_FILLORDER
-        #define HOST_FILLORDER FILLORDER_MSB2LSB
-        #define WORDS_BIGENDIAN 1
-    #endif
 #endif
-
