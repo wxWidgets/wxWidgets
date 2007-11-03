d3a5f4096c87655d62ba7e5455f7922b57f31820
Vadim Zeitlin <vadim@wxwidgets.org>
2007-11-03 23:50:04 +0000

    don't undef Windows symbols here, just include wx/msw/winundef.h [backport of 48116 from trunk]
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@49612 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -2839,35 +2839,8 @@ typedef const void* WXWidget;
 /*  included before or after wxWidgets classes, and therefore must be */
 /*  disabled here before any significant wxWidgets headers are included. */
 #ifdef __WXMSW__
-#ifdef GetClassInfo
-#undef GetClassInfo
-#endif
-
-#ifdef GetClassName
-#undef GetClassName
-#endif
-
-#ifdef DrawText
-#undef DrawText
-#endif
-
-#ifdef GetCharWidth
-#undef GetCharWidth
-#endif
-
-#ifdef StartDoc
-#undef StartDoc
-#endif
-
-#ifdef FindWindow
-#undef FindWindow
-#endif
-
-#ifdef FindResource
-#undef FindResource
-#endif
-#endif
-  /*  __WXMSW__ */
+#include "wx/msw/winundef.h"
+#endif /*  __WXMSW__ */
 
 /*  --------------------------------------------------------------------------- */
 /*  macro to define a class without copy ctor nor assignment operator */
