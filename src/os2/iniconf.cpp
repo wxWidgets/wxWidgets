c22bb3442ec1ddd3a511fb956f424dbf78e08054
Stefan Neis <Stefan.Neis@t-online.de>
2007-11-11 22:29:50 +0000

    Avoid deprecation warnings for ::IsEmpty (patch #1829909)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@49836 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/os2/iniconf.cpp
+++ src/os2/iniconf.cpp
@@ -255,11 +255,11 @@ bool wxIniConfig::IsEmpty() const
 
 //  GetPrivateProfileString(m_strGroup, NULL, "",
 //                          szBuf, WXSIZEOF(szBuf), m_strLocalFilename);
-    if ( !::IsEmpty(szBuf) )
+    if ( !::wxIsEmpty(szBuf) )
         return false;
 
 //  GetProfileString(m_strGroup, NULL, "", szBuf, WXSIZEOF(szBuf));
-//  if ( !::IsEmpty(szBuf) )
+//  if ( !::wxIsEmpty(szBuf) )
 //      return false;
 
     return true;
@@ -281,14 +281,14 @@ bool wxIniConfig::Read(const wxString& szKey, wxString *pstr) const
     // NB: the lpDefault param to GetPrivateProfileString can't be NULL
 //  GetPrivateProfileString(m_strGroup, strKey, "",
 //                          szBuf, WXSIZEOF(szBuf), m_strLocalFilename);
-    if ( ::IsEmpty((PSZ)szBuf) )
+    if ( ::wxIsEmpty((PSZ)szBuf) )
     {
         // now look in win.ini
         wxString strKey = GetKeyName(path.Name());
         // GetProfileString(m_strGroup, strKey, "", szBuf, WXSIZEOF(szBuf));
     }
 
-    if ( ::IsEmpty((PSZ)szBuf) )
+    if ( ::wxIsEmpty((PSZ)szBuf) )
     {
         return false;
     }
@@ -310,14 +310,14 @@ bool wxIniConfig::Read(const wxString& szKey, wxString *pstr,
     // NB: the lpDefault param to GetPrivateProfileString can't be NULL
     // GetPrivateProfileString(m_strGroup, strKey, "",
     //                         szBuf, WXSIZEOF(szBuf), m_strLocalFilename);
-    if ( ::IsEmpty((PSZ)szBuf) )
+    if ( ::wxIsEmpty((PSZ)szBuf) )
     {
       // now look in win.ini
       wxString strKey = GetKeyName(path.Name());
       // GetProfileString(m_strGroup, strKey, "", szBuf, WXSIZEOF(szBuf));
     }
 
-    if ( ::IsEmpty((PSZ)szBuf) )
+    if ( ::wxIsEmpty((PSZ)szBuf) )
     {
         *pstr = szDefault;
         return false;
