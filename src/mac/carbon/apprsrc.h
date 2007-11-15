f5d7fd083771b7fee5524f0503f17bda8cf07e85
Stefan Csomor <csomor@advancedconcepts.ch>
2007-11-15 20:19:47 +0000

    cleanup
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@49981 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/apprsrc.h
+++ src/mac/carbon/apprsrc.h
@@ -1,6 +1,1 @@
-#define	kMacSTRWrongMachine 1 
-#define	kMacSTRSmallSize 2 
-#define	kMacSTRNoMemory 3 
-#define	kMacSTROldSystem 4 
-#define	kMacSTRGenericAbout 5 
-#define	kMacSTRNoPre8Yet 6 
+// TODO REMOVE
