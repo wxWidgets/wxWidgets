15041ff9c88a55041f91e04d8019c38e8e188b05
Paul Cornett <paulcor@bullseye.com>
2007-12-06 16:10:38 +0000

    match CFTypeRef declaration to the one in defs.h
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@50546 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/corefoundation/cfref.h
+++ include/wx/mac/corefoundation/cfref.h
@@ -23,8 +23,8 @@
  * Note that Darwin CF uses extern for CF_EXPORT.  If we need this on Win32
  * or non-Darwin Mac OS we'll need to define the appropriate __declspec.
  */
-extern "C" {
 typedef const void *CFTypeRef;
+extern "C" {
 extern /* CF_EXPORT */
 CFTypeRef CFRetain(CFTypeRef cf);
 extern /* CF_EXPORT */
