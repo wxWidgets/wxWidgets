c7135017cc8f89d11b3cfb94f19d2d5fb4e2dc26
Stefan Csomor <csomor@advancedconcepts.ch>
2007-12-08 20:04:06 +0000

    easier expression
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@50582 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/unix/execute.h
+++ include/wx/unix/execute.h
@@ -71,7 +71,7 @@ extern WXDLLIMPEXP_BASE void wxHandleProcessTermination(wxEndProcessData *proc_d
 // child process. The return valus is port-specific.
 extern WXDLLIMPEXP_CORE int wxAddProcessCallback(wxEndProcessData *proc_data, int fd);
 
-#if defined(__DARWIN__) && (defined(__WXMAC__) || defined(__WXCOCOA__))
+#if defined(__WXMAC__) || defined(__WXCOCOA__)
 // For ports (e.g. DARWIN) which can add callbacks based on the pid
 extern int wxAddProcessCallbackForPid(wxEndProcessData *proc_data, int pid);
 #endif
