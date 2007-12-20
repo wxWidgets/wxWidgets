ef329166292a66f2f7b3c43d8a02da818321bd4d
Vadim Zeitlin <vadim@wxwidgets.org>
2007-12-20 01:19:15 +0000

    set up the correct layout direction for memory dc in Render() (patch 1789359)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@50846 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/aui/auibook.cpp
+++ src/aui/auibook.cpp
@@ -1611,6 +1611,11 @@ void wxAuiTabContainer::Render(wxDC* raw_dc, wxWindow* wnd)
         return;
 
     wxMemoryDC dc;
+
+    // use the same layout direction as the window DC uses to ensure that the
+    // text is rendered correctly
+    dc.SetLayoutDirection(raw_dc->GetLayoutDirection());
+
     wxBitmap bmp;
     size_t i;
     size_t page_count = m_pages.GetCount();
