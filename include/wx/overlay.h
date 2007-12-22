30d54e2732fe66d65c76b65a092b52e712d7abb8
Stefan Csomor <csomor@advancedconcepts.ch>
2007-12-22 09:06:39 +0000

    changing to wxDC, so that wxGCDC can be used as well
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@50885 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/overlay.h
+++ include/wx/overlay.h
@@ -29,7 +29,7 @@
 // ----------------------------------------------------------------------------
 
 class WXDLLIMPEXP_FWD_CORE wxOverlayImpl;
-class WXDLLIMPEXP_FWD_CORE wxWindowDC;
+class WXDLLIMPEXP_FWD_CORE wxDC;
 
 class WXDLLEXPORT wxOverlay
 {
@@ -50,13 +50,13 @@ private:
     // returns true if it has been setup
     bool IsOk();
 
-    void Init(wxWindowDC* dc, int x , int y , int width , int height);
+    void Init(wxDC* dc, int x , int y , int width , int height);
 
-    void BeginDrawing(wxWindowDC* dc);
+    void BeginDrawing(wxDC* dc);
 
-    void EndDrawing(wxWindowDC* dc);
+    void EndDrawing(wxDC* dc);
 
-    void Clear(wxWindowDC* dc);
+    void Clear(wxDC* dc);
 
     wxOverlayImpl* m_impl;
 
@@ -72,10 +72,10 @@ class WXDLLEXPORT wxDCOverlay
 public:
     // connects this overlay to the corresponding drawing dc, if the overlay is
     // not initialized yet this call will do so
-    wxDCOverlay(wxOverlay &overlay, wxWindowDC *dc, int x , int y , int width , int height);
+    wxDCOverlay(wxOverlay &overlay, wxDC *dc, int x , int y , int width , int height);
 
     // convenience wrapper that behaves the same using the entire area of the dc
-    wxDCOverlay(wxOverlay &overlay, wxWindowDC *dc);
+    wxDCOverlay(wxOverlay &overlay, wxDC *dc);
 
     // removes the connection between the overlay and the dc
     virtual ~wxDCOverlay();
@@ -84,11 +84,11 @@ public:
     void Clear();
 
 private:
-    void Init(wxWindowDC *dc, int x , int y , int width , int height);
+    void Init(wxDC *dc, int x , int y , int width , int height);
 
     wxOverlay& m_overlay;
 
-    wxWindowDC* m_dc;
+    wxDC* m_dc;
 
 
     DECLARE_NO_COPY_CLASS(wxDCOverlay)
