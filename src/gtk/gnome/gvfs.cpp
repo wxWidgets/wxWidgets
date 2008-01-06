9214b97a17a3652bb01152e54e86e4b6a025ab98
Vadim Zeitlin <vadim@wxwidgets.org>
2008-01-06 13:14:41 +0000

    don't unload libgnomevfs.so, this results in a crash on exit later so keep it loaded as a lesser evil (see Gentoo bug 203389)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@51034 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/gnome/gvfs.cpp
+++ src/gtk/gnome/gvfs.cpp
@@ -85,7 +85,15 @@ wxGnomeVFSLibrary::wxGnomeVFSLibrary()
 wxGnomeVFSLibrary::~wxGnomeVFSLibrary()
 {
     if (m_gnome_vfs_lib)
+    {
+        // we crash on exit later (i.e. after main() finishes) if we unload
+        // this library, apparently it inserts some hooks in other libraries to
+        // which we link implicitly (GTK+ itself?) which are not uninstalled
+        // when it's unloaded resulting in this crash, so just leave it in
+        // memory -- it's a lesser evil
+        m_gnome_vfs_lib->.Detach();
         delete m_gnome_vfs_lib;
+    }
 }
 
 bool wxGnomeVFSLibrary::IsOk()
