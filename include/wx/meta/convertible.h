0fb0ecc4528966916edd520aeb2acc1a69aadd3b
Vadim Zeitlin <vadim@wxwidgets.org>
2008-01-13 01:09:40 +0000

    add useful headers for meta-programming checks (part of patch 1860953)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51186 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ include/wx/meta/convertible.h
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/meta/convertible.h
+// Purpose:     Test if types are convertible
+// Author:      Arne Steinarson
+// Created:     2008-01-10
+// RCS-ID:      $Id$
+// Copyright:   (c) 2008 Arne Steinarson
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_CONVERTIBLE_H_
+#define _WX_CONVERTIBLE_H_
+
+// Helper to decide if an object of type D is convertible to type B (the test
+// succeeds in particular when D derives from B)
+template <class D, class B>
+struct wxConvertibleTo
+{
+    static char Match(B* pb);
+    static int Match(...);
+
+    enum { value = sizeof(Match(static_cast<D*>(NULL))) == sizeof(char) };
+};
+
+#endif // _WX_CONVERTIBLE_H_
+
