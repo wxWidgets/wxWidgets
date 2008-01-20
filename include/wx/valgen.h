c699b4583b6047191ac326728654c6fe8920aa7b
Vadim Zeitlin <vadim@wxwidgets.org>
2008-01-20 22:55:16 +0000

    no changes, just come cleanup and more comments
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51301 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/valgen.h
+++ include/wx/valgen.h
@@ -2,9 +2,8 @@
 // Name:        valgen.h
 // Purpose:     wxGenericValidator class
 // Author:      Kevin Smith
-// Modified by:
 // Created:     Jan 22 1999
-// RCS-ID:
+// RCS-ID:      $Id:$
 // Copyright:   (c) 1999 Julian Smart (assigned from Kevin)
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
@@ -18,60 +17,68 @@
 
 class WXDLLIMPEXP_FWD_BASE wxDateTime;
 
+// ----------------------------------------------------------------------------
+// wxGenericValidator performs data transfer between many standard controls and
+// variables of the type corresponding to their values.
+//
+// It doesn't do any validation so its name is a slight misnomer.
+// ----------------------------------------------------------------------------
+
 class WXDLLEXPORT wxGenericValidator: public wxValidator
 {
-DECLARE_CLASS(wxGenericValidator)
 public:
-  wxGenericValidator(bool* val);
-  wxGenericValidator(int* val);
-  wxGenericValidator(wxString* val);
-  wxGenericValidator(wxArrayInt* val);
+    // Different constructors: each of them creates a validator which can only
+    // be used with some controls, the comments before each constructor
+    // indicate which ones:
+        // wxCheckBox, wxRadioButton, wx(Bitmap)ToggleButton
+    wxGenericValidator(bool* val);
+        // wxChoice, wxGauge, wxRadioBox, wxScrollBar, wxSlider, wxSpinButton
+    wxGenericValidator(int* val);
+        // wxComboBox, wxTextCtrl, wxButton, wxStaticText (read-only)
+    wxGenericValidator(wxString* val);
+        // wxListBox, wxCheckListBox
+    wxGenericValidator(wxArrayInt* val);
 #if wxUSE_DATETIME
-  wxGenericValidator(wxDateTime* val);
+        // wxDatePickerCtrl
+    wxGenericValidator(wxDateTime* val);
 #endif // wxUSE_DATETIME
-  wxGenericValidator(const wxGenericValidator& copyFrom);
+    wxGenericValidator(const wxGenericValidator& copyFrom);
 
-  virtual ~wxGenericValidator(){}
+    virtual ~wxGenericValidator(){}
 
-  // Make a clone of this validator (or return NULL) - currently necessary
-  // if you're passing a reference to a validator.
-  // Another possibility is to always pass a pointer to a new validator
-  // (so the calling code can use a copy constructor of the relevant class).
-  virtual wxObject *Clone() const { return new wxGenericValidator(*this); }
-  bool Copy(const wxGenericValidator& val);
+    // Make a clone of this validator (or return NULL) - currently necessary
+    // if you're passing a reference to a validator.
+    // Another possibility is to always pass a pointer to a new validator
+    // (so the calling code can use a copy constructor of the relevant class).
+    virtual wxObject *Clone() const { return new wxGenericValidator(*this); }
+    bool Copy(const wxGenericValidator& val);
 
-  // Called when the value in the window must be validated.
-  // This function can pop up an error message.
-  virtual bool Validate(wxWindow * WXUNUSED(parent)) { return true; }
+    // Called when the value in the window must be validated: this is not used
+    // by this class
+    virtual bool Validate(wxWindow * WXUNUSED(parent)) { return true; }
 
-  // Called to transfer data to the window
-  virtual bool TransferToWindow();
+    // Called to transfer data to the window
+    virtual bool TransferToWindow();
 
-  // Called to transfer data to the window
-  virtual bool TransferFromWindow();
+    // Called to transfer data to the window
+    virtual bool TransferFromWindow();
 
 protected:
-  void Initialize();
+    void Initialize();
 
-  bool*       m_pBool;
-  int*        m_pInt;
-  wxString*   m_pString;
-  wxArrayInt* m_pArrayInt;
+    bool*       m_pBool;
+    int*        m_pInt;
+    wxString*   m_pString;
+    wxArrayInt* m_pArrayInt;
 #if wxUSE_DATETIME
-  wxDateTime* m_pDateTime;
+    wxDateTime* m_pDateTime;
 #endif // wxUSE_DATETIME
 
 private:
-// Cannot use
-//  DECLARE_NO_COPY_CLASS(wxGenericValidator)
-// because copy constructor is explicitly declared above;
-// but no copy assignment operator is defined, so declare
-// it private to prevent the compiler from defining it:
-    wxGenericValidator& operator=(const wxGenericValidator&);
+    DECLARE_CLASS(wxGenericValidator)
+    DECLARE_NO_ASSIGN_CLASS(wxGenericValidator)
 };
 
-#endif
-  // wxUSE_VALIDATORS
+#endif // wxUSE_VALIDATORS
 
-#endif
-  // _WX_VALGENH__
+#endif // _WX_VALGENH__
