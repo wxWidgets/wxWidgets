2eafd712f13ebf2567d65c8ba5c78ef7006ff023
Vadim Zeitlin <vadim@wxwidgets.org>
2008-02-03 23:43:58 +0000

    another correction to indices used in StringToLines()
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51534 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -8133,7 +8133,7 @@ void wxGrid::StringToLines( const wxString& value, wxArrayString& lines ) const
         startPos += pos + 1;
     }
 
-    if ( startPos < (int)value.length() )
+    if ( startPos < (int)tVal.length() )
     {
         lines.Add( tVal.Mid( startPos ) );
     }
