f986fe7613f8e59727a11285be53017fecaafd0f
Vadim Zeitlin <vadim@wxwidgets.org>
2008-02-17 01:37:23 +0000

    fix wxNotebook::GetPage{Text,Image}() when they were called from the page change event handler for the first added page (patch 1895161)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51853 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/notebook.cpp
+++ src/gtk/notebook.cpp
@@ -527,18 +527,24 @@ bool wxNotebook::InsertPage( size_t position,
 
     m_pages.Insert(win, position);
 
+    // set the label image and text
+    // this must be done before adding the page, as GetPageText
+    // and GetPageImage will otherwise return wrong values in
+    // the page-changed event that results from inserting the
+    // first page.
+    nb_page->m_image = imageId;
+    nb_page->m_text = wxStripMenuCodes(text);
+
     nb_page->m_box = gtk_hbox_new( FALSE, 1 );
     gtk_container_set_border_width((GtkContainer*)nb_page->m_box, 2);
 
     g_signal_connect (win->m_widget, "size_allocate",
                       G_CALLBACK (gtk_page_size_callback), win);
 
-    gtk_notebook_insert_page( notebook, win->m_widget, nb_page->m_box, position );
+    gint idx = gtk_notebook_insert_page(notebook, win->m_widget,
+                                        nb_page->m_box, position);
 
-    nb_page->m_page = (GtkNotebookPage*) g_list_last(notebook->children)->data;
-
-    /* set the label image */
-    nb_page->m_image = imageId;
+    nb_page->m_page = (GtkNotebookPage *)gtk_notebook_get_nth_page(notebook, idx);
 
     if (imageId != -1)
     {
@@ -551,10 +557,6 @@ bool wxNotebook::InsertPage( size_t position,
     }
 
     /* set the label text */
-
-    nb_page->m_text = wxStripMenuCodes(text);
-    if (nb_page->m_text.empty()) nb_page->m_text = wxEmptyString;
-
     nb_page->m_label = GTK_LABEL( gtk_label_new(wxGTK_CONV(nb_page->m_text)) );
     gtk_box_pack_end( GTK_BOX(nb_page->m_box), GTK_WIDGET(nb_page->m_label), FALSE, FALSE, m_padding );
 
