d513b59d1ab02160f21785dc85986f2a298ab9ba
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-02-19 16:35:45 +0000

    fix doxygen.log and put the .css file in the HTML output folder
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51918 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/regen.sh
+++ docs/doxygen/regen.sh
@@ -5,3 +5,14 @@ doxygen
 # this image is not automatically copied by Doxygen because it's not
 # used in doxygen documentation but only in our html footer...
 cp images/powered-by-wxwidgets.png out/html
+
+# this CSS is automatically copied by Doxygen because it's 
+# included by our custom html header...
+cp wxwidgets.css out/html
+
+# Doxygen has the annoying habit to put the full path of the
+# affected files in the log file; remove it to make the log
+# more readable
+currpath=`pwd`/
+cat doxygen.log | sed -e "s|$currpath||g" >temp
+mv temp doxygen.log
