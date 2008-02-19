d4d2d4c35460ea6a23494c135e0af628e2d74629
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-02-19 16:21:38 +0000

    forgot to commit these -- were part of devtips.h
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@51916 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ docs/doxygen/utilities.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        utilities.h
+// Purpose:     Utilities page of the Doxygen manual
+// Author:      wxWidgets team
+// RCS-ID:      $Id$
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+/*!
+
+ @page utilities_page Utilities and libraries supplied with wxWidgets
+
+ In addition to the @ref librarieslist, some
+ additional utilities are supplied in the @c utils hierarchy.
+
+ For other user-contributed packages, please see the Contributions page
+ on the wxWidgets Web site http://www.wxwidgets.org.
+
+ @li @b Helpview: Helpview is a program for displaying wxWidgets HTML
+ Help files. In many cases, you may wish to use the wxWidgets HTML
+ Help classes from within your application, but this provides a
+ handy stand-alone viewer. See @ref html_overview for more details.
+ You can find it in @c samples/html/helpview.
+
+ @li @b Tex2RTF; Supplied with wxWidgets is a utility called Tex2RTF for 
+ converting @e LaTeX manuals HTML, MS HTML Help, wxHTML Help, RTF, and Windows
+ Help RTF formats. Tex2RTF was used for the wxWidgets manuals and can be used 
+ independently by authors wishing to create on-line and printed manuals from the 
+ same @e LaTeX source. Please see the separate documentation for Tex2RTF.
+ You can find it under @c utils/tex2rtf.
+
+ @li @b Helpgen: Helpgen takes C++ header files and generates a Tex2RTF-compatible
+ documentation file for each class it finds, using comments as appropriate.
+ This is a good way to start a reference for a set of classes.
+ Helpgen can be found in @c utils/HelpGen.
+
+ @li @b Emulator: Xnest-based display emulator for X11-based PDA applications.
+ On some systems, the Xnest window does not synchronise with the
+ 'skin' window. This program can be found in @c utils/emulator.
+
+*/
