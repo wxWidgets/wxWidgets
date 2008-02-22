e1f6fc8616600cccb6991398f0a0e242ba4260a2
Václav Slavík <vslavik@fastmail.fm>
2008-02-22 18:37:18 +0000

    fixed strange focus order in wxTipDialog and set initial focus to the checkbox instead of read-only textctrl
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@52000 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/tipdlg.cpp
+++ src/generic/tipdlg.cpp
@@ -225,20 +225,6 @@ wxTipDialog::wxTipDialog(wxWindow *parent,
 
     // 1) create all controls in tab order
 
-    // smart phones does not support or do not waste space for wxButtons
-#ifndef __SMARTPHONE__
-    wxButton *btnClose = new wxButton(this, wxID_CLOSE);
-    SetAffirmativeId(wxID_CLOSE);
-#endif
-
-    m_checkbox = new wxCheckBox(this, wxID_ANY, _("&Show tips at startup"));
-    m_checkbox->SetValue(showAtStartup);
-
-    // smart phones does not support or do not waste space for wxButtons
-#ifndef __SMARTPHONE__
-    wxButton *btnNext = new wxButton(this, wxID_NEXT_TIP, _("&Next Tip"));
-#endif
-
     wxStaticText *text = new wxStaticText(this, wxID_ANY, _("Did you know..."));
 
     if (!isPda)
@@ -278,6 +264,22 @@ wxTipDialog::wxTipDialog(wxWindow *parent,
     wxStaticBitmap *bmp = new wxStaticBitmap(this, wxID_ANY, icon);
 
 //#endif
+
+    m_checkbox = new wxCheckBox(this, wxID_ANY, _("&Show tips at startup"));
+    m_checkbox->SetValue(showAtStartup);
+    m_checkbox->SetFocus();
+
+    // smart phones does not support or do not waste space for wxButtons
+#ifndef __SMARTPHONE__
+    wxButton *btnNext = new wxButton(this, wxID_NEXT_TIP, _("&Next Tip"));
+#endif
+
+    // smart phones does not support or do not waste space for wxButtons
+#ifndef __SMARTPHONE__
+    wxButton *btnClose = new wxButton(this, wxID_CLOSE);
+    SetAffirmativeId(wxID_CLOSE);
+#endif
+
 
     // 2) put them in boxes
 
