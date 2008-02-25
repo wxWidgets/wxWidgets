d230488b29e48d130a8883215db18fbd92daf48c
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-02-25 00:09:20 +0000

    reworked link anchors and link text
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@52071 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/overviews/thread.h
+++ docs/doxygen/overviews/thread.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        thread
+// Name:        thread.h
 // Purpose:     topic overview
 // Author:      wxWidgets team
 // RCS-ID:      $Id$
@@ -10,18 +10,14 @@
 
  @page overview_thread Multithreading overview
 
- Classes: #wxThread, #wxMutex,
- #wxCriticalSection,
- #wxCondition
+ Classes: #wxThread, #wxMutex, #wxCriticalSection, #wxCondition
 
  wxWidgets provides a complete set of classes encapsulating objects necessary in
- multithreaded (MT) programs: the #thread class itself and different
- synchronization objects: #mutexes and
- @ref criticalsection_overview with
- #conditions. The thread API in wxWidgets resembles to
- POSIX1.c threads API (a.k.a. pthreads), although several functions have
- different names and some features inspired by Win32 thread API are there as
- well.
+ multithreaded (MT) programs: the wxThread class itself and different
+ synchronization objects: mutexes (see wxMutex) and critical sections (see 
+ wxCriticalSection)  with conditions (see wxCondition). The thread API in wxWidgets 
+ resembles to POSIX1.c threads API (a.k.a. pthreads), although several functions have
+ different names and some features inspired by Win32 thread API are there as well.
 
  These classes will hopefully make writing MT programs easier and they also
  provide some extra error checking (compared to the native (be it Win32 or Posix)
@@ -33,10 +29,9 @@
  new thread for each new client), but in others it might be a very poor choice
  (example: launching a separate thread when doing a long computation to show a
  progress dialog). Other implementation choices are available: for the progress
- dialog example it is far better to do the calculations in the
- @ref idleevent_overview or even simply do everything at once
- but call wxWindow::Update() periodically to update
- the screen.
+ dialog example it is far better to do the calculations in the idle handler
+ (see @ref wxIdleEvent) or even simply do everything at once but call wxWindow::Update() 
+ periodically to update the screen.
 
  If you do decide to use threads in your application, it is strongly recommended
  that no more than one thread calls GUI functions. The thread sample shows that
@@ -49,17 +44,14 @@
  itself and not by the other threads).
 
  For communication between secondary threads and the main thread, you may use
- wxEvtHandler::AddPendingEvent
- or its short version #wxPostEvent. These functions
+ wxEvtHandler::AddPendingEvent or its short version #wxPostEvent. These functions
  have a thread-safe implementation so that they can be used as they are for
  sending events from one thread to another. However there is no built in method
  to send messages to the worker threads and you will need to use the available
  synchronization classes to implement the solution which suits your needs
  yourself. In particular, please note that it is not enough to derive
- your class from #wxThread and
- #wxEvtHandler to send messages to it: in fact, this does
- not work at all.
+ your class from #wxThread and #wxEvtHandler to send messages to it: 
+ in fact, this does not work at all.
 
- */
-
+*/
 
