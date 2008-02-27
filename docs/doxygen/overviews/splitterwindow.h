23114fe1ed6fff8fbdaa6e2a468f81c7b5c65029
Bryan Petty <bryan@ibaku.net>
2008-02-27 18:37:48 +0000

    More doxygen topic overview cleanup.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@52157 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/overviews/splitterwindow.h
+++ docs/doxygen/overviews/splitterwindow.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        splitterwindow
+// Name:        splitterwindow.h
 // Purpose:     topic overview
 // Author:      wxWidgets team
 // RCS-ID:      $Id$
@@ -8,67 +8,68 @@
 
 /*!
 
- @page splitterwindow_overview wxSplitterWindow overview
+@page overview_splitterwindow wxSplitterWindow Overview
 
- Classes: #wxSplitterWindow
- The following screenshot shows the appearance of a splitter window with a horizontal split.
+Classes: wxSplitterWindow
 
- The style wxSP_3D has been used to show a 3D border and 3D sash.
- #Example
+The following screenshot shows the appearance of a splitter window with a
+horizontal split.
 
+The style wxSP_3D has been used to show a 3D border and 3D sash.
 
- @section wxsplitterwindowexample Example
-
- The following fragment shows how to create a splitter window, creating two
- subwindows and hiding one of them.
+@image html splitter.bmp
 
 
- @code
- splitter = new wxSplitterWindow(this, -1, wxPoint(0, 0), wxSize(400, 400), wxSP_3D);
+@section overview_splitterwindow_example Example
 
-   leftWindow = new MyWindow(splitter);
-   leftWindow-SetScrollbars(20, 20, 50, 50);
+The following fragment shows how to create a splitter window, creating two
+subwindows and hiding one of them.
 
-   rightWindow = new MyWindow(splitter);
-   rightWindow-SetScrollbars(20, 20, 50, 50);
-   rightWindow-Show(@false);
+@code
+splitter = new wxSplitterWindow(this, -1, wxPoint(0, 0),
+                                wxSize(400, 400), wxSP_3D);
 
-   splitter-Initialize(leftWindow);
+leftWindow = new MyWindow(splitter);
+leftWindow->SetScrollbars(20, 20, 50, 50);
 
-   // Set this to prevent unsplitting
- //    splitter-SetMinimumPaneSize(20);
- @endcode
+rightWindow = new MyWindow(splitter);
+rightWindow->SetScrollbars(20, 20, 50, 50);
+rightWindow->Show(false);
 
+splitter->Initialize(leftWindow);
 
- The next fragment shows how the splitter window can be manipulated after creation.
+// Set this to prevent unsplitting
+// splitter->SetMinimumPaneSize(20);
+@endcode
 
+The next fragment shows how the splitter window can be manipulated after
+creation.
 
- @code
- void MyFrame::OnSplitVertical(wxCommandEvent& event)
-   {
-         if ( splitter-IsSplit() )
-             splitter-Unsplit();
-         leftWindow-Show(@true);
-         rightWindow-Show(@true);
-         splitter-SplitVertically( leftWindow, rightWindow );
-   }
+@code
+void MyFrame::OnSplitVertical(wxCommandEvent& event)
+{
+    if ( splitter->IsSplit() )
+        splitter->Unsplit();
+    leftWindow->Show(true);
+    rightWindow->Show(true);
+    splitter->SplitVertically( leftWindow, rightWindow );
+}
 
-   void MyFrame::OnSplitHorizontal(wxCommandEvent& event)
-   {
-         if ( splitter-IsSplit() )
-             splitter-Unsplit();
-         leftWindow-Show(@true);
-         rightWindow-Show(@true);
-         splitter-SplitHorizontally( leftWindow, rightWindow );
-   }
+void MyFrame::OnSplitHorizontal(wxCommandEvent& event)
+{
+    if ( splitter->IsSplit() )
+        splitter->Unsplit();
+    leftWindow->Show(true);
+    rightWindow->Show(true);
+    splitter->SplitHorizontally( leftWindow, rightWindow );
+}
 
-   void MyFrame::OnUnsplit(wxCommandEvent& event)
-   {
-         if ( splitter-IsSplit() )
-             splitter-Unsplit();
-   }
- @endcode
+void MyFrame::OnUnsplit(wxCommandEvent& event)
+{
+    if ( splitter->IsSplit() )
+        splitter->Unsplit();
+}
+@endcode
 
- */
-
+*/
 
