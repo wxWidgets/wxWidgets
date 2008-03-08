a7c93f3fbfc6a9971888df84bdcbe3f14bbe7ce7
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-03-08 15:30:12 +0000

    more comments
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@52384 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/regen.sh
+++ docs/doxygen/regen.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-
+#
 # $Id$
-
+#
 # This bash script regenerates the HTML doxygen version of the
 # wxWidgets manual and adjusts the doxygen log to make it more
 # readable.
+#
+# Usage:
+#    ./regen.sh [html|chm|xml|all]
+#
+# Pass "html" to regen only the HTML output format, "chm" only for
+# the CHM, "xml" only for the XML and "all" to regen them all.
+#
+# If no arguments are passed all formats are regenerated
+# (just like passing "all").
+#
 
+
+# 
 me=$(basename $0)
 path=${0%%/$me}        # path from which the script has been launched
 current=$(pwd)
 cd $path
 
+# prepare folders for the cp commands below
 mkdir -p out/html       # we need to copy files in this folder below
 mkdir -p out/html/wxmsw out/html/wxgtk out/html/wxmac
 
-# this image is not automatically copied by Doxygen because it's not
-# used in doxygen documentation but only in our html footer...
+# these images are not automatically copied by Doxygen because they're not
+# used in doxygen documentation but only in our html footer and by our
+# custom aliases
 cp images/powered-by-wxwidgets.png out/html
 cp images/*logo.png out/html
 cp images/wxmsw/*png out/html/wxmsw
 cp images/wxmac/*png out/html/wxmac
 cp images/wxgtk/*png out/html/wxgtk
 
-# this CSS is automatically copied by Doxygen because it's 
+# this CSS is not automatically copied by Doxygen because it's
 # included by our custom html header...
 cp wxwidgets.css out/html
+
+# which configuration should we use?
+if [[ -z "$1" ]]; then
+    cfgfile="Doxyfile.all"
+else
+    cfgfile="Doxyfile.$1"
+fi
 
 #
 # NOW RUN DOXYGEN
@@ -33,12 +54,6 @@ cp wxwidgets.css out/html
 #     otherwise when generating the CHM file with Doxygen, those files are
 #     not included!
 #
-if [[ -z "$1" ]]; then
-    cfgfile="Doxyfile.all"
-else
-    cfgfile="Doxyfile.$1"
-fi
-
 doxygen $cfgfile
 
 # Doxygen has the annoying habit to put the full path of the
