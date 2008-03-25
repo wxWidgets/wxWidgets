e333075415c35d2e869ea585fa41f01a2c938897
Stefan Csomor <csomor@advancedconcepts.ch>
2008-03-25 07:29:03 +0000

    addind nonowned window implementation
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@52785 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/mac/carbon/toplevel.h
+++ include/wx/mac/carbon/toplevel.h
@@ -12,10 +12,6 @@
 #ifndef _WX_MSW_TOPLEVEL_H_
 #define _WX_MSW_TOPLEVEL_H_
 
-#if wxUSE_SYSTEM_OPTIONS
-    #define wxMAC_WINDOW_PLAIN_TRANSITION _T("mac.window-plain-transition")
-#endif
-
 // ----------------------------------------------------------------------------
 // wxTopLevelWindowMac
 // ----------------------------------------------------------------------------
@@ -62,71 +58,18 @@ public:
     virtual bool IsIconized() const;
     virtual void Restore();
 
-    virtual bool SetShape(const wxRegion& region);
-
     virtual bool ShowFullScreen(bool show, long style = wxFULLSCREEN_ALL) ;
     virtual bool IsFullScreen() const ;
-
-    virtual bool SetTransparent(wxByte alpha);
-    virtual bool CanSetTransparent();
-
-    virtual bool SetBackgroundStyle(wxBackgroundStyle style);
 
     // implementation from now on
     // --------------------------
 
-    static void MacDelayedDeactivation(long timestamp);
-    virtual void MacCreateRealWindow( const wxString& title,
-                                      const wxPoint& pos,
-                                      const wxSize& size,
-                                      long style,
-                                      const wxString& name ) ;
-    virtual void ClearBackground() ;
-
-    WXWindow MacGetWindowRef() { return m_macWindow ; }
-    virtual void MacActivate( long timestamp , bool inIsActivating ) ;
-    virtual void MacPerformUpdates() ;
-
     virtual void SetTitle( const wxString& title);
     virtual wxString GetTitle() const;
-
-    virtual void Raise();
-    virtual void Lower();
-    virtual bool Show( bool show = true );
-
-    virtual bool ShowWithEffect(wxShowEffect effect,
-                                unsigned timeout = 0,
-                                wxDirection dir = wxBOTTOM);
-   
-    virtual bool HideWithEffect(wxShowEffect effect,
-                                unsigned timeout = 0,
-                                wxDirection dir = wxBOTTOM);
      
-    virtual void SetExtraStyle(long exStyle) ;
-
-    virtual bool SetBackgroundColour( const wxColour &colour );
-    
-    virtual void MacInstallTopLevelWindowEventHandler() ;
-
-    bool MacGetMetalAppearance() const ;
-    bool MacGetUnifiedAppearance() const ;
-
-    void MacChangeWindowAttributes( wxUint32 attributesToSet , wxUint32 attributesToClear ) ;
-    wxUint32 MacGetWindowAttributes() const ;
-
-    WXEVENTHANDLERREF    MacGetEventHandler() { return m_macEventHandler ; }
-
-    virtual void        MacGetContentAreaInset( int &left , int &top , int &right , int &bottom ) ;
-
 protected:
     // common part of all ctors
     void Init();
-
-    virtual void DoGetPosition( int *x, int *y ) const;
-    virtual void DoGetSize( int *width, int *height ) const;
-    virtual void DoMoveWindow(int x, int y, int width, int height);
-    virtual void DoGetClientSize(int *width, int *height) const;
-    virtual void DoCentre(int dir);
 
     // is the frame currently iconized?
     bool m_iconized;
@@ -135,28 +78,8 @@ protected:
     // when it is called while the frame is hidden
     bool m_maximizeOnShow;
 
-    WXWindow m_macWindow ;
-
-    wxWindowMac* m_macFocus ;
     void *m_macFullScreenData ;
-
-    static wxTopLevelWindowMac *s_macDeactivateWindow;
 private :
-    // KH: We cannot let this be called directly since the metal appearance is now managed by an
-    // extra style. Calling this function directly can result in blank white window backgrounds.
-    // This is because the ExtraStyle flags get out of sync with the metal appearance and the metal
-    // logic & checks cease to work as expected. To set the metal appearance, use SetExtraStyle.
-    void MacSetMetalAppearance( bool on ) ;
-    void MacSetUnifiedAppearance( bool on ) ;
-    // binary compatible workaround TODO REPLACE
-    void DoMacCreateRealWindow( wxWindow *parent, const wxString& title,
-                                      const wxPoint& pos,
-                                      const wxSize& size,
-                                      long style,
-                                      const wxString& name );
-
-    WXEVENTHANDLERREF    m_macEventHandler ;
-
     DECLARE_EVENT_TABLE()
 };
 
