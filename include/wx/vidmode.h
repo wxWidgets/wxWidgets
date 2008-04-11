wxWidgets/wxWidgets

commit e46fcdb43b604a5003d11a488fe6ca3e11be5095

documented wxVideoMode (otherwise wxDisplay docs are not very useful)
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@53049 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/vidmode.h
@@ -13,11 +13,12 @@
 #define _WX_VMODE_H_
 
 // ----------------------------------------------------------------------------
-// wxVideoMode: a simple struct containing video mode parameters for a display
+// wxVideoMode: a simple class containing video mode parameters for a display
 // ----------------------------------------------------------------------------
 
-struct WXDLLIMPEXP_CORE wxVideoMode
+class WXDLLIMPEXP_CORE wxVideoMode
 {
+public:
     wxVideoMode(int width = 0, int height = 0, int depth = 0, int freq = 0)
     {
         w = width;
@@ -58,6 +59,8 @@ struct WXDLLIMPEXP_CORE wxVideoMode
     // returns true if the object has been initialized
     bool IsOk() const { return w && h; }
 
+
+protected:
 
     // the screen size in pixels (e.g. 640*480), 0 means unspecified
     int w, h;

interface/vidmode.h
@@ -1,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        vidmode.h
+// Purpose:     interface of wxVideoMode
+// Author:      wxWidgets team
+// RCS-ID:      $Id: display.h 52634 2008-03-20 13:45:17Z VS $
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+    @class wxVideoMode
+    @wxheader{display.h}
+
+    Determines the sizes and locations of displays connected to the system.
+
+    @library{wxcore}
+    @category{FIXME}
+
+    @stdobjects
+    ::wxDefaultVideoMode
+
+    @see wxClientDisplayRect(), wxDisplaySize(), wxDisplaySizeMM()
+*/
+class wxVideoMode
+{
+public:
+    /**
+        Constructs this class using the given parameters.
+    */
+    wxVideoMode(int width = 0, int height = 0, int depth = 0, int freq = 0);
+
+    bool operator==(const wxVideoMode& m) const
+    bool operator!=(const wxVideoMode& mode) const
+
+    /**
+        Returns true if this mode matches the other one in the sense that all
+        non zero fields of the other mode have the same value in this one
+        (except for refresh which is allowed to have a greater value).
+    */
+    bool Matches(const wxVideoMode& other) const;
+
+    int GetWidth() const;
+    int GetHeight() const;
+    int GetDepth() const;
+
+    /**
+        Returns true if the object has been initialized
+    */
+    bool IsOk() const;
+};
+
+/**
+    A global wxVideoMode instance used by wxDisplay.
+*/
+wxVideoMode wxDefaultVideoMode;

commit 077f75a51d3d0fee459fbd1800a1e2f53af19fc5

reverted wxVideoMode API breakage by r53049, finished documentation for it
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@53124 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/vidmode.h
@@ -13,12 +13,11 @@
 #define _WX_VMODE_H_
 
 // ----------------------------------------------------------------------------
-// wxVideoMode: a simple class containing video mode parameters for a display
+// wxVideoMode: a simple struct containing video mode parameters for a display
 // ----------------------------------------------------------------------------
 
-class WXDLLIMPEXP_CORE wxVideoMode
+struct WXDLLIMPEXP_CORE wxVideoMode
 {
-public:
     wxVideoMode(int width = 0, int height = 0, int depth = 0, int freq = 0)
     {
         w = width;
@@ -60,8 +59,6 @@ public:
     // returns true if the object has been initialized
     bool IsOk() const { return w && h; }
 
-
-protected:
 
     // the screen size in pixels (e.g. 640*480), 0 means unspecified
     int w, h;

interface/vidmode.h
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 /**
-    @class wxVideoMode
+    @struct wxVideoMode
     @wxheader{display.h}
 
     Determines the sizes and locations of displays connected to the system.
@@ -20,7 +20,7 @@
 
     @see wxClientDisplayRect(), wxDisplaySize(), wxDisplaySizeMM()
 */
-class wxVideoMode
+struct wxVideoMode
 {
 public:
     /**
@@ -38,14 +38,38 @@ public:
     */
     bool Matches(const wxVideoMode& other) const;
 
+    /**
+        Returns the screen width in pixels (e.g. 640), 0 means unspecified.
+    */
     int GetWidth() const;
+
+    /**
+        Returns the screen height in pixels (e.g. 480), 0 means unspecified.
+    */
     int GetHeight() const;
+
+    /**
+        Returns bits per pixel (e.g. 32), 1 is monochrome and 0 means
+        unspecified/known.
+    */
     int GetDepth() const;
 
     /**
         Returns true if the object has been initialized
     */
     bool IsOk() const;
+
+    /// The screen width in pixels (e.g. 640), 0 means unspecified.
+    int w;
+
+    /// The screen height in pixels (e.g. 480), 0 means unspecified.
+    int h;
+
+    /// Bits per pixel (e.g. 32), 1 is monochrome and 0 means unspecified/known.
+    int bpp;
+
+    /// Refresh frequency in Hz, 0 means unspecified/unknown.
+    int refresh;
 };
 
 /**