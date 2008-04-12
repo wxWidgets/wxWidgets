7c0d297a1f761de31d3a4ac924fa2080b5bbaf41
Robert Roebling <robert@roebling.de>
2008-04-12 15:37:09 +0000

    Add wxPopupWindow
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@53140 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ interface/popupwin.h
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        popupwind.h
+// Purpose:     interface of wxPoppWindow
+// Author:      wxWidgets team
+// RCS-ID:      $Id:$
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+    @class wxPopupWindow
+    @wxheader{popupwin.h}
+
+    A special kind of top level window used for popup menus,
+    combobox popups and such.
+
+    @library{wxcore}
+    @category{managedwnd}
+
+    @see wxDialog, wxFrame
+*/
+
+class wxPopupWindow: public wxNonOwnedWindow
+{
+public:
+
+    /**
+      Constructor
+    */
+    wxPopupWindow(wxWindow *parent, int flags = wxBORDER_NONE);
+    
+    /**
+      Create method for two-step creation
+    */
+    bool Create(wxWindow *parent, int flags = wxBORDER_NONE);
+    
+    /**
+        Move the popup window to the right position, i.e. such that it is
+        entirely visible.
+        
+        The popup is positioned at ptOrigin + size if it opens below and to the
+        right (default), at ptOrigin - sizePopup if it opens above and to the
+        left etc.
+        
+        @param ptOrigin
+            Must be given in screen coordinates!
+    */
+    virtual void Position(const wxPoint& ptOrigin,
+                          const wxSize& size);
+};
+
