6d55ca7cb13c6badbd9a3b4b3b42dd76f8e0f709
Václav Slavík <vslavik@fastmail.fm>
2008-04-24 23:20:08 +0000

    Fixed handling of transparent background in borderless wxBitmapButton (patch #1477883)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@53340 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/bmpbuttn.cpp
+++ src/mac/carbon/bmpbuttn.cpp
@@ -14,6 +14,7 @@
 #if wxUSE_BMPBUTTON
 
 #include "wx/bmpbuttn.h"
+#include "wx/image.h"
 
 #ifndef WX_PRECOMP
     #include "wx/dcmemory.h"
@@ -63,14 +64,12 @@ static wxBitmap wxMakeStdSizeBitmap(const wxBitmap& bitmap)
             wxASSERT_MSG( width <= sizeStd && height <= sizeStd,
                           _T("bitmap shouldn't be cropped") );
 
-            newBmp.Create(sizeStd, sizeStd);
-            wxMemoryDC dcMem;
-            dcMem.SelectObject(newBmp);
-            dcMem.Clear();
-
-            dcMem.DrawBitmap(bitmap,
-                             (sizeStd - width)/2, (sizeStd-height)/2,
-                             true);
+            wxImage square_image = bitmap.ConvertToImage();
+            newBmp = square_image.Size
+                     (
+                         wxSize(sizeStd, sizeStd),
+                         wxPoint((sizeStd - width)/2, (sizeStd-height)/2)
+                     );
         }
     }
     //else: let the system rescale the bitmap
