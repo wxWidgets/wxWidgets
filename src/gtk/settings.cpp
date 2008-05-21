241c8d223ee3c2c2ffbc3a8ae5f6309167fbe1de
Paul Cornett <paulcor@bullseye.com>
2008-05-21 16:18:49 +0000

    button text uses fg color, not text color
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@53690 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/settings.cpp
+++ src/gtk/settings.cpp
@@ -90,7 +90,6 @@ enum wxGtkColourType
 {
     wxGTK_FG,
     wxGTK_BG,
-    wxGTK_TEXT,
     wxGTK_BASE
 };
 
@@ -149,10 +148,6 @@ static bool GetColourFromGTKWidget(GdkColor& gdkColor,
 
             case wxGTK_BG:
                 gdkColor = def->bg[state];
-                break;
-
-            case wxGTK_TEXT:
-                gdkColor = def->text[state];
                 break;
 
             case wxGTK_BASE:
@@ -326,7 +321,7 @@ wxColour wxSystemSettingsNative::GetColour( wxSystemColour index )
                 gdkColor.green =
                 gdkColor.blue = 0;
                 GetColourFromGTKWidget(
-                    gdkColor, wxGTK_BUTTON, GTK_STATE_SELECTED, wxGTK_TEXT);
+                    gdkColor, wxGTK_BUTTON, GTK_STATE_SELECTED, wxGTK_FG);
                 gs_objects.m_colHighlightText = wxColour(gdkColor);
             }
             color = gs_objects.m_colHighlightText;
