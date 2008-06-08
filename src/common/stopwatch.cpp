35c9a66079b6468e7fdb76771efe211625ae5a3a
Stefan Csomor <csomor@advancedconcepts.ch>
2008-06-08 16:56:17 +0000

    removing unneeded code for osx
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54031 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/stopwatch.cpp
+++ src/common/stopwatch.cpp
@@ -79,15 +79,6 @@
     #include <sys/timeb.h>
 #endif
 
-#ifdef __WXMAC__
-#ifndef __DARWIN__
-    #include <Timer.h>
-    #include <DriverServices.h>
-#else
-    #include <Carbon/Carbon.h>
-#endif
-#endif
-
 #ifdef __WXPALMOS__
     #include <DateTime.h>
     #include <TimeMgr.h>
@@ -285,26 +276,6 @@ wxLongLong wxGetLocalTimeMillis()
     (void)::ftime(&tp);
     val *= tp.time;
     return (val + tp.millitm);
-#elif defined(__WXMAC__)
-
-    static UInt64 gMilliAtStart = 0;
-
-    Nanoseconds upTime = AbsoluteToNanoseconds( UpTime() );
-
-    if ( gMilliAtStart == 0 )
-    {
-        time_t start = time(NULL);
-        gMilliAtStart = ((UInt64) start) * 1000000L;
-        gMilliAtStart -= upTime.lo / 1000 ;
-        gMilliAtStart -= ( ( (UInt64) upTime.hi ) << 32 ) / (1000 * 1000);
-    }
-
-    UInt64 millival = gMilliAtStart;
-    millival += upTime.lo / (1000 * 1000);
-    millival += ( ( (UInt64) upTime.hi ) << 32 ) / (1000 * 1000);
-    val = millival;
-
-    return val;
 #else // no gettimeofday() nor ftime()
     // We use wxGetLocalTime() to get the seconds since
     // 00:00:00 Jan 1st 1970 and then whatever is available
