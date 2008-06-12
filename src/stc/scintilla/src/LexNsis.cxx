9967de02678d23a149183bc5908c7fadcd4f76eb
Vadim Zeitlin <vadim@wxwidgets.org>
2008-06-12 18:51:27 +0000

    get rid of several dozen of warnings when using g++ >= 4.2 about string literals to char* conversion
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54170 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/stc/scintilla/src/LexNsis.cxx
+++ src/stc/scintilla/src/LexNsis.cxx
@@ -98,7 +98,7 @@ static bool NsisNextLineHasElse(unsigned int start, unsigned int end, Accessor &
   return false;
 }
 
-static int NsisCmp( char *s1, char *s2, bool bIgnoreCase )
+static int NsisCmp( const char *s1, const char *s2, bool bIgnoreCase )
 {
   if( bIgnoreCase )
      return CompareCaseInsensitive( s1, s2);
