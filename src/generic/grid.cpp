44de0d66d2ebd7779f4f8e5ee9952fc391b1556a
Stefan Neis <Stefan.Neis@t-online.de>
2008-06-28 00:17:09 +0000

    Restricted column/row resizing in cell window to left button for consistency         with label windows and because ending the resize is only handled in         case of LeftUp() (Fixing #4212).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@54397 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -6075,7 +6075,8 @@ void wxGrid::ProcessGridCellMouseEvent( wxMouseEvent& event )
 
 
         }
-        else if ( m_cursorMode == WXGRID_CURSOR_RESIZE_ROW )
+        else if ( event.LeftIsDown() &&
+                  m_cursorMode == WXGRID_CURSOR_RESIZE_ROW )
         {
             int cw, ch, left, dummy;
             m_gridWin->GetClientSize( &cw, &ch );
@@ -6093,7 +6094,8 @@ void wxGrid::ProcessGridCellMouseEvent( wxMouseEvent& event )
             dc.DrawLine( left, y, left+cw, y );
             m_dragLastPos = y;
         }
-        else if ( m_cursorMode == WXGRID_CURSOR_RESIZE_COL )
+        else if ( event.LeftIsDown() &&
+                  m_cursorMode == WXGRID_CURSOR_RESIZE_COL )
         {
             int cw, ch, dummy, top;
             m_gridWin->GetClientSize( &cw, &ch );
