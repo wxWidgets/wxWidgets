1eebb103c737c550dc865b418113454ecc5bc40e
Robin Dunn <robin@alldunn.com>
2008-07-10 01:32:06 +0000

    If an item is hidden, don't check it for overflows.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54567 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/gbsizer.cpp
+++ src/common/gbsizer.cpp
@@ -580,7 +580,7 @@ void wxGridBagSizer::AdjustForOverflow()
         {
             wxGBPosition pos(row,col);
             wxGBSizerItem* item = FindItemAtPosition(pos);
-            if ( !item )
+            if ( !item || !item->ShouldAccountFor() )
                 continue;
 
             int endrow, endcol;
@@ -623,7 +623,7 @@ void wxGridBagSizer::AdjustForOverflow()
         {
             wxGBPosition pos(row,col);
             wxGBSizerItem* item = FindItemAtPosition(pos);
-            if ( !item )
+            if ( !item || !item->ShouldAccountFor() )
                 continue;
 
             int endrow, endcol;
