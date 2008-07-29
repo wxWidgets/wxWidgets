6a3ea4ead89a97b4dcc6a1fdc8c48119bdaabc23
Stefan Csomor <csomor@advancedconcepts.ch>
2008-07-29 20:23:22 +0000

    osx regrouping
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54825 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/private.h
+++ include/wx/osx/private.h
@@ -1,7 +1,13 @@
-#ifdef __WXMAC_CLASSIC__
-#include "wx/osx/classic/private.h"
-#elif defined(__WXOSX_IPHONE__)
-#include "wx/osx/core/private.h"
-#else
-#include "wx/osx/carbon/private.h"
+/* common warning snippet for all osx direct includes */
+
+#ifndef wxOSX_USE_CARBON
+#error "this files should only be included after platform.h was included
+#endif
+
+#if wxOSX_USE_IPHONE
+    #include "wx/osx/iphone/private.h"
+#elif wxOSX_USE_CARBON
+    #include "wx/osx/carbon/private.h"
+#elif wxOSX_USE_COCOA
+    #include "wx/osx/cocoa/private.h"
 #endif
--- include/wx/osx/region.h
+++ include/wx/osx/region.h
@@ -1,5 +1,8 @@
-#ifdef __WXMAC_CLASSIC__
-#include "wx/osx/classic/region.h"
-#else
+#if wxOSX_USE_CARBON
 #include "wx/osx/carbon/region.h"
+#else
+#define wxRegionGeneric wxRegion
+#define wxRegionIteratorGeneric wxRegionIterator
+
+#include "wx/generic/region.h"
 #endif
