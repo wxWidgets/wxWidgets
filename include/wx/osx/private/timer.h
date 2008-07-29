f1bb54f84e1a21811eb93bf5e2629830339c1a2c
Stefan Csomor <csomor@advancedconcepts.ch>
2008-07-29 20:27:54 +0000

    osx regrouping
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54828 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/private/timer.h
+++ include/wx/osx/private/timer.h
@@ -1,5 +1,12 @@
-#ifdef __WXMAC_CLASSIC__
-#include "wx/osx/classic/private/timer.h"
-#else
-#include "wx/osx/carbon/private/timer.h"
+/* common warning snippet for all osx direct includes */
+
+#ifndef wxOSX_USE_CARBON
+#error "this files should only be included after platform.h was included
+#endif
+
+#if wxOSX_USE_COCOA_OR_IPHONE
+    // this should become the future version also for carbon
+    #include "wx/osx/core/private/timer.h"
+#elif wxOSX_USE_CARBON
+    #include "wx/osx/carbon/private/timer.h"
 #endif
