c3052fa0fb94bc2924fa5eca7b5a2886fdc00c59
Stefan Csomor <csomor@advancedconcepts.ch>
2008-07-31 09:52:01 +0000

    brush and pen are implementation neutral on osx
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@54871 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/brush.h
+++
@@ -1,5 +0,0 @@
-#ifdef __WXMAC_CLASSIC__
-#include "wx/osx/classic/brush.h"
-#else
-#include "wx/osx/carbon/brush.h"
-#endif
--- include/wx/osx/pen.h
+++
@@ -1,5 +0,0 @@
-#ifdef __WXMAC_CLASSIC__
-#include "wx/osx/classic/pen.h"
-#else
-#include "wx/osx/carbon/pen.h"
-#endif
