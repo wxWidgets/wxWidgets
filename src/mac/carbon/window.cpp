54fa957a4877b58b62e4cb26885a552f8ba593a2
Stefan Csomor <csomor@advancedconcepts.ch>
2008-08-05 17:55:14 +0000

    fixing remainder of bug #3776, clean activateAndIgnoreClick behaviour for DataBrowser
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@54982 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/mac/carbon/window.cpp
+++ src/mac/carbon/window.cpp
@@ -484,7 +484,7 @@ static pascal OSStatus wxMacWindowControlEventHandler( EventHandlerCallRef handl
                 WindowRef owner = cEvent.GetParameter<WindowRef>(kEventParamWindowRef);
                 if ( !IsWindowActive(owner) )
                 {
-                    cEvent.SetParameter(kEventParamClickActivation,(UInt32) kActivateAndIgnoreClick) ;
+                    cEvent.SetParameter(kEventParamClickActivation,typeClickActivationResult, (UInt32) kActivateAndIgnoreClick) ;
                     result = noErr ;
                 }
             }
