e3cdca140e6ba79bda408e9c98f57082fc058875
Julian Smart <julian@anthemion.co.uk>
2008-08-07 09:29:54 +0000

    Fix from John Dallaway for jumping to the top when expanding/collapsing items.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@55007 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- contrib/src/gizmos/splittree.cpp
+++ contrib/src/gizmos/splittree.cpp
@@ -377,7 +377,7 @@ void wxRemotelyScrolledTreeCtrl::AdjustRemoteScrollbars()
         if (scrolledWindow)
         {
             wxRect itemRect;
-            if (GetBoundingRect(GetFirstVisibleItem(), itemRect))
+            if (GetBoundingRect(GetRootItem(), itemRect))
             {
                 // Actually, the real height seems to be 1 less than reported
                 // (e.g. 16 instead of 16)
