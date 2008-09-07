a8e65f2336604dc7f7f5286f9e3f8b13af252d59
Václav Slavík <vslavik@fastmail.fm>
2008-09-07 09:15:46 +0000

    whatever it is, wxXmlResource::GetFirstRoot() isn't implemented - removed
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@55504 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/xrc/xmlres.h
+++ include/wx/xrc/xmlres.h
@@ -128,8 +128,6 @@ public:
     // Destructor.
     virtual ~wxXmlResource();
 
-    wxXmlNode *GetFirstRoot();
-    
     // Loads resources from XML files that match given filemask.
     // This method understands VFS (see filesys.h).
     bool Load(const wxString& filemask);
