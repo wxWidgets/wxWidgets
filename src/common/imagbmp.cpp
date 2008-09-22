622f7a49ef5ddf9a47096e14bf1f8d93be3c8668
Vadim Zeitlin <vadim@wxwidgets.org>
2008-09-22 16:27:43 +0000

    use factor of 10, not 100, to convert mm to cm after mm2inches was fixed in r55800
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@55802 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/imagbmp.cpp
+++ src/common/imagbmp.cpp
@@ -195,8 +195,8 @@ bool wxBMPHandler::SaveDib(wxImage *image,
 
         case wxIMAGE_RESOLUTION_INCHES:
             // convert resolution in inches to resolution in centimeters
-            hres = (wxUint32)(100*mm2inches*hres);
-            vres = (wxUint32)(100*mm2inches*vres);
+            hres = (wxUint32)(10*mm2inches*hres);
+            vres = (wxUint32)(10*mm2inches*vres);
             // fall through to convert it to resolution in meters
 
         case wxIMAGE_RESOLUTION_CM:
