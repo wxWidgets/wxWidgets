5a9f031e294fcdc888cd1ff648ea0d161ec1bcba
Vadim Zeitlin <vadim@wxwidgets.org>
2008-10-08 21:32:04 +0000

    apparently operator<<(wxFileOffset) is needed for all VC versions, not just VC6
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@56173 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/streams/fileback.cpp
+++ tests/streams/fileback.cpp
@@ -19,14 +19,14 @@
 #endif
 
 
-#ifdef __VISUALC6__
+#ifdef __VISUALC__
 // need this to be able to use CPPUNIT_ASSERT_EQUAL with wxFileOffset objects
 static std::ostream& operator<<(std::ostream& ostr, const wxFileOffset& fo)
 {
     ostr << fo;
     return ostr;
 }
-#endif // VC++ 6
+#endif // __VISUALC__
 
 #include "wx/mstream.h"
 #include "wx/private/fileback.h"
