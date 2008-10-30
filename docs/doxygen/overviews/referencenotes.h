b4da9d62df20cc16b3f39bb5ba06f00527819cc6
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-10-30 11:14:46 +0000

    modernized
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56596 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/overviews/referencenotes.h
+++ docs/doxygen/overviews/referencenotes.h
@@ -8,24 +8,16 @@
 
 /**
 
-@page overview_referencenotes Notes on Using the Reference
+@page overview_referencenotes Notes on Using this Reference Manual
 
 In the descriptions of the wxWidgets classes and their member functions, note
 that descriptions of inherited member functions are not duplicated in derived
 classes unless their behaviour is different. So in using a class such as
 wxScrolledWindow, be aware that wxWindow functions may be relevant.
 
-Note also that arguments with default values may be omitted from a function
-call, for brevity. Size and position arguments may usually be given a value of
--1 (the default), in which case wxWidgets will choose a suitable value.
-
-Most strings are returned as wxString objects. However, for remaining char *
-return values, the strings are allocated and deallocated by wxWidgets.
-Therefore, return values should always be copied for long-term use, especially
-since the same buffer is often used by wxWidgets.
-
-The member functions are given in alphabetical order except for constructors
-and destructors which appear first.
+Where not explicitely stated, size and position arguments may usually be given a
+value of ::wxDefaultSize and ::wxDefaultPosition, in which case wxWidgets will
+choose suitable values.
 
 */
 
