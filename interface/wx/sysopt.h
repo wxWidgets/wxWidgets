6a0f372ca0f808501007d5a987093414a5a786ba
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-11-22 14:29:00 +0000

    document mac.textcontrol-use-spell-checker (fixes #10165)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56903 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/sysopt.h
+++ interface/wx/sysopt.h
@@ -89,6 +89,9 @@
     flag{mac.listctrl.always_use_generic}
         Tells wxListCtrl to use the generic control even when it is capable of
         using the native control instead. Also knwon as wxMAC_ALWAYS_USE_GENERIC_LISTCTRL.
+    flag{mac.textcontrol-use-spell-checker}
+        This option only has effect for Mac OS X 10.4 and higher.
+        If 1 activates the spell checking in wxTextCtrl.
     @endFlagTable
 
 
