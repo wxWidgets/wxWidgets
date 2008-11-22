160f78e09db9fff2f7c3a1484514835f1d83eb8e
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2008-11-22 22:50:39 +0000

    added group 'grid' (fixes #10217)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56917 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ docs/doxygen/groups/class_grid.h
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        class_grid.h
+// Purpose:     wxGrid classes group docs
+// Author:      wxWidgets team
+// RCS-ID:      $Id$
+// Licence:     wxWindows license
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+
+@defgroup group_class_grid Grid Related Classes
+@ingroup group_class
+
+*/
+
