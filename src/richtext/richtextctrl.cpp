dc5de0dd3678b3d5307769643fbc5b9b726cfeef
Julian Smart <julian@anthemion.co.uk>
2008-11-28 12:35:15 +0000

    wxRichTextCtrl no longer deletes a character when content is selected before pressing Delete (fixes bug #10153).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56991 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextctrl.cpp
+++ src/richtext/richtextctrl.cpp
@@ -872,13 +872,12 @@ void wxRichTextCtrl::OnChar(wxKeyEvent& event)
 
         long newPos = m_caretPosition;
 
-        DeleteSelectedContent(& newPos);
+        bool processed = DeleteSelectedContent(& newPos);
 
         // Submit range in character positions, which are greater than caret positions,
         // so subtract 1 for deleted character and add 1 for conversion to character position.
         if (newPos > -1)
         {
-            bool processed = false;
             if (event.CmdDown())
             {
                 long pos = wxRichTextCtrl::FindNextWordPosition(-1);
@@ -922,12 +921,11 @@ void wxRichTextCtrl::OnChar(wxKeyEvent& event)
 
         long newPos = m_caretPosition;
 
-        DeleteSelectedContent(& newPos);
+        bool processed = DeleteSelectedContent(& newPos);
 
         // Submit range in character positions, which are greater than caret positions,
         if (newPos < GetBuffer().GetRange().GetEnd()+1)
         {
-            bool processed = false;
             if (event.CmdDown())
             {
                 long pos = wxRichTextCtrl::FindNextWordPosition(1);
