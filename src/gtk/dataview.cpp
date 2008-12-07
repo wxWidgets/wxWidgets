wxWidgets/wxWidgets

commit e2bfe6731edfca732418012b03438db85b4aa0dd

- Rewrite wxHeaderCtrl to be virtual-like: even if we don't need an infinite   number of columns in it, it turns out that getting column information from   the associated control is much easier than copying it into the control. - Provide wxHeaderCtrlSimple derived class which can be used easily if   callback approach of wxHeaderCtrl is not needed. - Because of wxHeaderCtrl virtualization, port-specific implementations of   wxHeaderColumn are not needed any more and were removed. - Use wxHeaderCtrl in the generic wxDataViewCtrl: this means that column   events are broken right now in it as they haven't been implemented by   wxHeaderCtrl yet, this will be fixed a.s.a.p.
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@57161 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/gtk/dataview.cpp
@@ -2527,7 +2527,6 @@ wxDataViewColumn::wxDataViewColumn( const wxString &title, wxDataViewRenderer *c
 {
     Init( align, flags, width );
 
-    gtk_tree_view_column_set_clickable( GTK_TREE_VIEW_COLUMN(m_column), TRUE );
     SetTitle( title );
 }
 
@@ -2568,10 +2567,6 @@ void wxDataViewColumn::Init(wxAlignment align, int flags, int width)
 
     gtk_tree_view_column_set_cell_data_func( column, renderer,
         wxGtkTreeCellDataFunc, (gpointer) GetRenderer(), NULL );
-}
-
-wxDataViewColumn::~wxDataViewColumn()
-{
 }
 
 void wxDataViewColumn::OnInternalIdle()
@@ -2693,7 +2688,21 @@ void wxDataViewColumn::SetSortable( bool sortable )
 {
     GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
 
-    if (sortable)
+    gtk_tree_view_column_set_clickable( GTK_TREE_VIEW_COLUMN(m_column),
+                                        sortable );
+}
+
+bool wxDataViewColumn::IsSortable() const
+{
+    GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
+    return gtk_tree_view_column_get_clickable( column );
+}
+
+void wxDataViewColumn::SetAsSortKey( bool sort )
+{
+    GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
+
+    if (sort)
     {
         gtk_tree_view_column_set_sort_column_id( column, GetModelColumn() );
     }
@@ -2704,7 +2713,7 @@ void wxDataViewColumn::SetSortable( bool sortable )
     }
 }
 
-bool wxDataViewColumn::IsSortable() const
+bool wxDataViewColumn::IsSortKey() const
 {
     GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
     return (gtk_tree_view_column_get_sort_column_id( column ) != -1);

Makefile.in
@@ -2744,7 +2744,6 @@ COND_TOOLKIT_MSW_GUI_HDR =  \
 	wx/msw/frame.h \
 	wx/msw/gauge.h \
 	wx/msw/gdiimage.h \
-	wx/msw/headercol.h \
 	wx/msw/headerctrl.h \
 	wx/msw/icon.h \
 	wx/msw/imaglist.h \
@@ -3285,7 +3284,6 @@ COND_TOOLKIT_WINCE_GUI_HDR =  \
 	wx/msw/frame.h \
 	wx/msw/gauge.h \
 	wx/msw/gdiimage.h \
-	wx/msw/headercol.h \
 	wx/msw/headerctrl.h \
 	wx/msw/icon.h \
 	wx/msw/imaglist.h \
@@ -3639,7 +3637,6 @@ COND_USE_GUI_1_ALL_GUI_HEADERS =  \
 	wx/generic/dirctrlg.h \
 	wx/generic/dragimgg.h \
 	wx/generic/filectrlg.h \
-	wx/generic/headercol.h \
 	wx/generic/headerctrl.h \
 	wx/generic/logg.h \
 	wx/generic/msgdlgg.h \
@@ -4282,7 +4279,6 @@ COND_USE_GUI_1_WXUNIV_0___CORE_SRC_OBJECTS =  \
 	monodll_dirctrlg.o \
 	monodll_dragimgg.o \
 	monodll_filectrlg.o \
-	monodll_headercolg.o \
 	monodll_headerctrlg.o \
 	monodll_listbkg.o \
 	monodll_logg.o \
@@ -4473,7 +4469,6 @@ COND_USE_GUI_1_WXUNIV_1___CORE_SRC_OBJECTS =  \
 	monodll_dirctrlg.o \
 	monodll_dragimgg.o \
 	monodll_filectrlg.o \
-	monodll_headercolg.o \
 	monodll_headerctrlg.o \
 	monodll_listbkg.o \
 	monodll_logg.o \
@@ -5084,7 +5079,6 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS =  \
 	monodll_msw_filedlg.o \
 	monodll_msw_frame.o \
 	monodll_msw_gauge.o \
-	monodll_headercol.o \
 	monodll_headerctrl.o \
 	monodll_msw_iniconf.o \
 	monodll_msw_listbox.o \
@@ -5321,7 +5315,6 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS =  \
 	monodll_msw_filedlg.o \
 	monodll_msw_frame.o \
 	monodll_msw_gauge.o \
-	monodll_headercol.o \
 	monodll_headerctrl.o \
 	monodll_msw_iniconf.o \
 	monodll_msw_listbox.o \
@@ -6106,7 +6099,6 @@ COND_USE_GUI_1_WXUNIV_0___CORE_SRC_OBJECTS_1 =  \
 	monolib_dirctrlg.o \
 	monolib_dragimgg.o \
 	monolib_filectrlg.o \
-	monolib_headercolg.o \
 	monolib_headerctrlg.o \
 	monolib_listbkg.o \
 	monolib_logg.o \
@@ -6297,7 +6289,6 @@ COND_USE_GUI_1_WXUNIV_1___CORE_SRC_OBJECTS_1 =  \
 	monolib_dirctrlg.o \
 	monolib_dragimgg.o \
 	monolib_filectrlg.o \
-	monolib_headercolg.o \
 	monolib_headerctrlg.o \
 	monolib_listbkg.o \
 	monolib_logg.o \
@@ -6908,7 +6899,6 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_1 =  \
 	monolib_msw_filedlg.o \
 	monolib_msw_frame.o \
 	monolib_msw_gauge.o \
-	monolib_headercol.o \
 	monolib_headerctrl.o \
 	monolib_msw_iniconf.o \
 	monolib_msw_listbox.o \
@@ -7145,7 +7135,6 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_1 =  \
 	monolib_msw_filedlg.o \
 	monolib_msw_frame.o \
 	monolib_msw_gauge.o \
-	monolib_headercol.o \
 	monolib_headerctrl.o \
 	monolib_msw_iniconf.o \
 	monolib_msw_listbox.o \
@@ -8080,7 +8069,6 @@ COND_USE_GUI_1_WXUNIV_0___CORE_SRC_OBJECTS_2 =  \
 	coredll_dirctrlg.o \
 	coredll_dragimgg.o \
 	coredll_filectrlg.o \
-	coredll_headercolg.o \
 	coredll_headerctrlg.o \
 	coredll_listbkg.o \
 	coredll_logg.o \
@@ -8271,7 +8259,6 @@ COND_USE_GUI_1_WXUNIV_1___CORE_SRC_OBJECTS_2 =  \
 	coredll_dirctrlg.o \
 	coredll_dragimgg.o \
 	coredll_filectrlg.o \
-	coredll_headercolg.o \
 	coredll_headerctrlg.o \
 	coredll_listbkg.o \
 	coredll_logg.o \
@@ -8882,7 +8869,6 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_2 =  \
 	coredll_msw_filedlg.o \
 	coredll_msw_frame.o \
 	coredll_msw_gauge.o \
-	coredll_headercol.o \
 	coredll_headerctrl.o \
 	coredll_msw_iniconf.o \
 	coredll_msw_listbox.o \
@@ -9119,7 +9105,6 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_2 =  \
 	coredll_msw_filedlg.o \
 	coredll_msw_frame.o \
 	coredll_msw_gauge.o \
-	coredll_headercol.o \
 	coredll_headerctrl.o \
 	coredll_msw_iniconf.o \
 	coredll_msw_listbox.o \
@@ -9596,7 +9581,6 @@ COND_USE_GUI_1_WXUNIV_0___CORE_SRC_OBJECTS_3 =  \
 	corelib_dirctrlg.o \
 	corelib_dragimgg.o \
 	corelib_filectrlg.o \
-	corelib_headercolg.o \
 	corelib_headerctrlg.o \
 	corelib_listbkg.o \
 	corelib_logg.o \
@@ -9787,7 +9771,6 @@ COND_USE_GUI_1_WXUNIV_1___CORE_SRC_OBJECTS_3 =  \
 	corelib_dirctrlg.o \
 	corelib_dragimgg.o \
 	corelib_filectrlg.o \
-	corelib_headercolg.o \
 	corelib_headerctrlg.o \
 	corelib_listbkg.o \
 	corelib_logg.o \
@@ -10398,7 +10381,6 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_3 =  \
 	corelib_msw_filedlg.o \
 	corelib_msw_frame.o \
 	corelib_msw_gauge.o \
-	corelib_headercol.o \
 	corelib_headerctrl.o \
 	corelib_msw_iniconf.o \
 	corelib_msw_listbox.o \
@@ -10635,7 +10617,6 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_3 =  \
 	corelib_msw_filedlg.o \
 	corelib_msw_frame.o \
 	corelib_msw_gauge.o \
-	corelib_headercol.o \
 	corelib_headerctrl.o \
 	corelib_msw_iniconf.o \
 	corelib_msw_listbox.o \
@@ -17778,12 +17759,6 @@ monodll_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monodll_msw_gauge.o: $(srcdir)/src/msw/gauge.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/gauge.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monodll_headercol.o: $(srcdir)/src/msw/headercol.cpp $(MONODLL_ODEP)
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monodll_headercol.o: $(srcdir)/src/msw/headercol.cpp $(MONODLL_ODEP)
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monodll_headerctrl.o: $(srcdir)/src/msw/headerctrl.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/headerctrl.cpp
 
@@ -18476,9 +18451,6 @@ monodll_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONODLL_ODEP)
 
 @COND_USE_GUI_1@monodll_filectrlg.o: $(srcdir)/src/generic/filectrlg.cpp $(MONODLL_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/generic/filectrlg.cpp
-
-@COND_USE_GUI_1@monodll_headercolg.o: $(srcdir)/src/generic/headercolg.cpp $(MONODLL_ODEP)
-@COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/generic/headercolg.cpp
 
 @COND_USE_GUI_1@monodll_headerctrlg.o: $(srcdir)/src/generic/headerctrlg.cpp $(MONODLL_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/generic/headerctrlg.cpp
@@ -22482,12 +22454,6 @@ monolib_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monolib_msw_gauge.o: $(srcdir)/src/msw/gauge.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/gauge.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monolib_headercol.o: $(srcdir)/src/msw/headercol.cpp $(MONOLIB_ODEP)
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monolib_headercol.o: $(srcdir)/src/msw/headercol.cpp $(MONOLIB_ODEP)
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monolib_headerctrl.o: $(srcdir)/src/msw/headerctrl.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/headerctrl.cpp
 
@@ -23180,9 +23146,6 @@ monolib_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONOLIB_ODEP)
 
 @COND_USE_GUI_1@monolib_filectrlg.o: $(srcdir)/src/generic/filectrlg.cpp $(MONOLIB_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/generic/filectrlg.cpp
-
-@COND_USE_GUI_1@monolib_headercolg.o: $(srcdir)/src/generic/headercolg.cpp $(MONOLIB_ODEP)
-@COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/generic/headercolg.cpp
 
 @COND_USE_GUI_1@monolib_headerctrlg.o: $(srcdir)/src/generic/headerctrlg.cpp $(MONOLIB_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/generic/headerctrlg.cpp
@@ -27375,12 +27338,6 @@ coredll_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@coredll_msw_gauge.o: $(srcdir)/src/msw/gauge.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/gauge.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@coredll_headercol.o: $(srcdir)/src/msw/headercol.cpp $(COREDLL_ODEP)
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@coredll_headercol.o: $(srcdir)/src/msw/headercol.cpp $(COREDLL_ODEP)
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@coredll_headerctrl.o: $(srcdir)/src/msw/headerctrl.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/headerctrl.cpp
 
@@ -28073,9 +28030,6 @@ coredll_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(COREDLL_ODEP)
 
 @COND_USE_GUI_1@coredll_filectrlg.o: $(srcdir)/src/generic/filectrlg.cpp $(COREDLL_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/generic/filectrlg.cpp
-
-@COND_USE_GUI_1@coredll_headercolg.o: $(srcdir)/src/generic/headercolg.cpp $(COREDLL_ODEP)
-@COND_USE_GUI_1@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/generic/headercolg.cpp
 
 @COND_USE_GUI_1@coredll_headerctrlg.o: $(srcdir)/src/generic/headerctrlg.cpp $(COREDLL_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/generic/headerctrlg.cpp
@@ -30906,12 +30860,6 @@ corelib_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@corelib_msw_gauge.o: $(srcdir)/src/msw/gauge.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/gauge.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@corelib_headercol.o: $(srcdir)/src/msw/headercol.cpp $(CORELIB_ODEP)
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@corelib_headercol.o: $(srcdir)/src/msw/headercol.cpp $(CORELIB_ODEP)
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/headercol.cpp
-
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@corelib_headerctrl.o: $(srcdir)/src/msw/headerctrl.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/headerctrl.cpp
 
@@ -31604,9 +31552,6 @@ corelib_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(CORELIB_ODEP)
 
 @COND_USE_GUI_1@corelib_filectrlg.o: $(srcdir)/src/generic/filectrlg.cpp $(CORELIB_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/generic/filectrlg.cpp
-
-@COND_USE_GUI_1@corelib_headercolg.o: $(srcdir)/src/generic/headercolg.cpp $(CORELIB_ODEP)
-@COND_USE_GUI_1@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/generic/headercolg.cpp
 
 @COND_USE_GUI_1@corelib_headerctrlg.o: $(srcdir)/src/generic/headerctrlg.cpp $(CORELIB_ODEP)
 @COND_USE_GUI_1@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/generic/headerctrlg.cpp

build/bakefiles/files.bkl
@@ -713,7 +713,6 @@ IMPORTANT: please read docs/tech/tn0016.txt before modifying this file!
     src/generic/dirctrlg.cpp
     src/generic/dragimgg.cpp
     src/generic/filectrlg.cpp
-    src/generic/headercolg.cpp
     src/generic/headerctrlg.cpp
     src/generic/listbkg.cpp
     src/generic/logg.cpp
@@ -778,7 +777,6 @@ IMPORTANT: please read docs/tech/tn0016.txt before modifying this file!
     wx/generic/dirctrlg.h
     wx/generic/dragimgg.h
     wx/generic/filectrlg.h
-    wx/generic/headercol.h
     wx/generic/headerctrl.h
     wx/generic/logg.h
     wx/generic/msgdlgg.h
@@ -1647,7 +1645,6 @@ IMPORTANT: please read docs/tech/tn0016.txt before modifying this file!
     src/msw/filedlg.cpp
     src/msw/frame.cpp
     src/msw/gauge.cpp
-    src/msw/headercol.cpp
     src/msw/headerctrl.cpp
     src/msw/iniconf.cpp
     src/msw/listbox.cpp
@@ -1718,7 +1715,6 @@ IMPORTANT: please read docs/tech/tn0016.txt before modifying this file!
     wx/msw/frame.h
     wx/msw/gauge.h
     wx/msw/gdiimage.h
-    wx/msw/headercol.h
     wx/msw/headerctrl.h
     wx/msw/icon.h
     wx/msw/imaglist.h

build/msw/makefile.bcc
@@ -1599,7 +1599,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_filedlg.obj \
 	$(OBJS)\monodll_frame.obj \
 	$(OBJS)\monodll_gauge.obj \
-	$(OBJS)\monodll_headercol.obj \
 	$(OBJS)\monodll_headerctrl.obj \
 	$(OBJS)\monodll_iniconf.obj \
 	$(OBJS)\monodll_listbox.obj \
@@ -1743,7 +1742,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_dirctrlg.obj \
 	$(OBJS)\monodll_dragimgg.obj \
 	$(OBJS)\monodll_filectrlg.obj \
-	$(OBJS)\monodll_headercolg.obj \
 	$(OBJS)\monodll_headerctrlg.obj \
 	$(OBJS)\monodll_listbkg.obj \
 	$(OBJS)\monodll_logg.obj \
@@ -1981,7 +1979,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_dirctrlg.obj \
 	$(OBJS)\monodll_dragimgg.obj \
 	$(OBJS)\monodll_filectrlg.obj \
-	$(OBJS)\monodll_headercolg.obj \
 	$(OBJS)\monodll_headerctrlg.obj \
 	$(OBJS)\monodll_listbkg.obj \
 	$(OBJS)\monodll_logg.obj \
@@ -2277,7 +2274,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_filedlg.obj \
 	$(OBJS)\monolib_frame.obj \
 	$(OBJS)\monolib_gauge.obj \
-	$(OBJS)\monolib_headercol.obj \
 	$(OBJS)\monolib_headerctrl.obj \
 	$(OBJS)\monolib_iniconf.obj \
 	$(OBJS)\monolib_listbox.obj \
@@ -2421,7 +2417,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_dirctrlg.obj \
 	$(OBJS)\monolib_dragimgg.obj \
 	$(OBJS)\monolib_filectrlg.obj \
-	$(OBJS)\monolib_headercolg.obj \
 	$(OBJS)\monolib_headerctrlg.obj \
 	$(OBJS)\monolib_listbkg.obj \
 	$(OBJS)\monolib_logg.obj \
@@ -2659,7 +2654,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_dirctrlg.obj \
 	$(OBJS)\monolib_dragimgg.obj \
 	$(OBJS)\monolib_filectrlg.obj \
-	$(OBJS)\monolib_headercolg.obj \
 	$(OBJS)\monolib_headerctrlg.obj \
 	$(OBJS)\monolib_listbkg.obj \
 	$(OBJS)\monolib_logg.obj \
@@ -2860,7 +2854,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_filedlg.obj \
 	$(OBJS)\coredll_frame.obj \
 	$(OBJS)\coredll_gauge.obj \
-	$(OBJS)\coredll_headercol.obj \
 	$(OBJS)\coredll_headerctrl.obj \
 	$(OBJS)\coredll_iniconf.obj \
 	$(OBJS)\coredll_listbox.obj \
@@ -3004,7 +2997,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_dirctrlg.obj \
 	$(OBJS)\coredll_dragimgg.obj \
 	$(OBJS)\coredll_filectrlg.obj \
-	$(OBJS)\coredll_headercolg.obj \
 	$(OBJS)\coredll_headerctrlg.obj \
 	$(OBJS)\coredll_listbkg.obj \
 	$(OBJS)\coredll_logg.obj \
@@ -3242,7 +3234,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_dirctrlg.obj \
 	$(OBJS)\coredll_dragimgg.obj \
 	$(OBJS)\coredll_filectrlg.obj \
-	$(OBJS)\coredll_headercolg.obj \
 	$(OBJS)\coredll_headerctrlg.obj \
 	$(OBJS)\coredll_listbkg.obj \
 	$(OBJS)\coredll_logg.obj \
@@ -3342,7 +3333,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_filedlg.obj \
 	$(OBJS)\corelib_frame.obj \
 	$(OBJS)\corelib_gauge.obj \
-	$(OBJS)\corelib_headercol.obj \
 	$(OBJS)\corelib_headerctrl.obj \
 	$(OBJS)\corelib_iniconf.obj \
 	$(OBJS)\corelib_listbox.obj \
@@ -3486,7 +3476,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_dirctrlg.obj \
 	$(OBJS)\corelib_dragimgg.obj \
 	$(OBJS)\corelib_filectrlg.obj \
-	$(OBJS)\corelib_headercolg.obj \
 	$(OBJS)\corelib_headerctrlg.obj \
 	$(OBJS)\corelib_listbkg.obj \
 	$(OBJS)\corelib_logg.obj \
@@ -3724,7 +3713,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_dirctrlg.obj \
 	$(OBJS)\corelib_dragimgg.obj \
 	$(OBJS)\corelib_filectrlg.obj \
-	$(OBJS)\corelib_headercolg.obj \
 	$(OBJS)\corelib_headerctrlg.obj \
 	$(OBJS)\corelib_listbkg.obj \
 	$(OBJS)\corelib_logg.obj \
@@ -5751,9 +5739,6 @@ $(OBJS)\monodll_filedlg.obj: ..\..\src\msw\filedlg.cpp
 $(OBJS)\monodll_frame.obj: ..\..\src\msw\frame.cpp
 	$(CXX) -q -c -P -o$@ $(MONODLL_CXXFLAGS) ..\..\src\msw\frame.cpp
 
-$(OBJS)\monodll_headercol.obj: ..\..\src\msw\headercol.cpp
-	$(CXX) -q -c -P -o$@ $(MONODLL_CXXFLAGS) ..\..\src\msw\headercol.cpp
-
 $(OBJS)\monodll_headerctrl.obj: ..\..\src\msw\headerctrl.cpp
 	$(CXX) -q -c -P -o$@ $(MONODLL_CXXFLAGS) ..\..\src\msw\headerctrl.cpp
 
@@ -7322,11 +7307,6 @@ $(OBJS)\monodll_filectrlg.obj: ..\..\src\generic\filectrlg.cpp
 !endif
 
 !if "$(USE_GUI)" == "1"
-$(OBJS)\monodll_headercolg.obj: ..\..\src\generic\headercolg.cpp
-	$(CXX) -q -c -P -o$@ $(MONODLL_CXXFLAGS) ..\..\src\generic\headercolg.cpp
-!endif
-
-!if "$(USE_GUI)" == "1"
 $(OBJS)\monodll_headerctrlg.obj: ..\..\src\generic\headerctrlg.cpp
 	$(CXX) -q -c -P -o$@ $(MONODLL_CXXFLAGS) ..\..\src\generic\headerctrlg.cpp
 !endif
@@ -7967,9 +7947,6 @@ $(OBJS)\monolib_filedlg.obj: ..\..\src\msw\filedlg.cpp
 
 $(OBJS)\monolib_frame.obj: ..\..\src\msw\frame.cpp
 	$(CXX) -q -c -P -o$@ $(MONOLIB_CXXFLAGS) ..\..\src\msw\frame.cpp
-
-$(OBJS)\monolib_headercol.obj: ..\..\src\msw\headercol.cpp
-	$(CXX) -q -c -P -o$@ $(MONOLIB_CXXFLAGS) ..\..\src\msw\headercol.cpp
 
 $(OBJS)\monolib_headerctrl.obj: ..\..\src\msw\headerctrl.cpp
 	$(CXX) -q -c -P -o$@ $(MONOLIB_CXXFLAGS) ..\..\src\msw\headerctrl.cpp
@@ -9539,11 +9516,6 @@ $(OBJS)\monolib_filectrlg.obj: ..\..\src\generic\filectrlg.cpp
 !endif
 
 !if "$(USE_GUI)" == "1"
-$(OBJS)\monolib_headercolg.obj: ..\..\src\generic\headercolg.cpp
-	$(CXX) -q -c -P -o$@ $(MONOLIB_CXXFLAGS) ..\..\src\generic\headercolg.cpp
-!endif
-
-!if "$(USE_GUI)" == "1"
 $(OBJS)\monolib_headerctrlg.obj: ..\..\src\generic\headerctrlg.cpp
 	$(CXX) -q -c -P -o$@ $(MONOLIB_CXXFLAGS) ..\..\src\generic\headerctrlg.cpp
 !endif
@@ -10571,9 +10543,6 @@ $(OBJS)\coredll_filedlg.obj: ..\..\src\msw\filedlg.cpp
 
 $(OBJS)\coredll_frame.obj: ..\..\src\msw\frame.cpp
 	$(CXX) -q -c -P -o$@ $(COREDLL_CXXFLAGS) ..\..\src\msw\frame.cpp
-
-$(OBJS)\coredll_headercol.obj: ..\..\src\msw\headercol.cpp
-	$(CXX) -q -c -P -o$@ $(COREDLL_CXXFLAGS) ..\..\src\msw\headercol.cpp
 
 $(OBJS)\coredll_headerctrl.obj: ..\..\src\msw\headerctrl.cpp
 	$(CXX) -q -c -P -o$@ $(COREDLL_CXXFLAGS) ..\..\src\msw\headerctrl.cpp
@@ -11780,11 +11749,6 @@ $(OBJS)\coredll_filectrlg.obj: ..\..\src\generic\filectrlg.cpp
 !endif
 
 !if "$(USE_GUI)" == "1"
-$(OBJS)\coredll_headercolg.obj: ..\..\src\generic\headercolg.cpp
-	$(CXX) -q -c -P -o$@ $(COREDLL_CXXFLAGS) ..\..\src\generic\headercolg.cpp
-!endif
-
-!if "$(USE_GUI)" == "1"
 $(OBJS)\coredll_headerctrlg.obj: ..\..\src\generic\headerctrlg.cpp
 	$(CXX) -q -c -P -o$@ $(COREDLL_CXXFLAGS) ..\..\src\generic\headerctrlg.cpp
 !endif
@@ -11941,9 +11905,6 @@ $(OBJS)\corelib_filedlg.obj: ..\..\src\msw\filedlg.cpp
 
 $(OBJS)\corelib_frame.obj: ..\..\src\msw\frame.cpp
 	$(CXX) -q -c -P -o$@ $(CORELIB_CXXFLAGS) ..\..\src\msw\frame.cpp
-
-$(OBJS)\corelib_headercol.obj: ..\..\src\msw\headercol.cpp
-	$(CXX) -q -c -P -o$@ $(CORELIB_CXXFLAGS) ..\..\src\msw\headercol.cpp
 
 $(OBJS)\corelib_headerctrl.obj: ..\..\src\msw\headerctrl.cpp
 	$(CXX) -q -c -P -o$@ $(CORELIB_CXXFLAGS) ..\..\src\msw\headerctrl.cpp
@@ -13147,11 +13108,6 @@ $(OBJS)\corelib_dragimgg.obj: ..\..\src\generic\dragimgg.cpp
 !if "$(USE_GUI)" == "1"
 $(OBJS)\corelib_filectrlg.obj: ..\..\src\generic\filectrlg.cpp
 	$(CXX) -q -c -P -o$@ $(CORELIB_CXXFLAGS) ..\..\src\generic\filectrlg.cpp
-!endif
-
-!if "$(USE_GUI)" == "1"
-$(OBJS)\corelib_headercolg.obj: ..\..\src\generic\headercolg.cpp
-	$(CXX) -q -c -P -o$@ $(CORELIB_CXXFLAGS) ..\..\src\generic\headercolg.cpp
 !endif
 
 !if "$(USE_GUI)" == "1"

build/msw/makefile.gcc
@@ -1617,7 +1617,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_filedlg.o \
 	$(OBJS)\monodll_frame.o \
 	$(OBJS)\monodll_gauge.o \
-	$(OBJS)\monodll_headercol.o \
 	$(OBJS)\monodll_headerctrl.o \
 	$(OBJS)\monodll_iniconf.o \
 	$(OBJS)\monodll_listbox.o \
@@ -1761,7 +1760,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_dirctrlg.o \
 	$(OBJS)\monodll_dragimgg.o \
 	$(OBJS)\monodll_filectrlg.o \
-	$(OBJS)\monodll_headercolg.o \
 	$(OBJS)\monodll_headerctrlg.o \
 	$(OBJS)\monodll_listbkg.o \
 	$(OBJS)\monodll_logg.o \
@@ -2001,7 +1999,6 @@ ____CORE_SRC_FILENAMES_OBJECTS =  \
 	$(OBJS)\monodll_dirctrlg.o \
 	$(OBJS)\monodll_dragimgg.o \
 	$(OBJS)\monodll_filectrlg.o \
-	$(OBJS)\monodll_headercolg.o \
 	$(OBJS)\monodll_headerctrlg.o \
 	$(OBJS)\monodll_listbkg.o \
 	$(OBJS)\monodll_logg.o \
@@ -2301,7 +2298,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_filedlg.o \
 	$(OBJS)\monolib_frame.o \
 	$(OBJS)\monolib_gauge.o \
-	$(OBJS)\monolib_headercol.o \
 	$(OBJS)\monolib_headerctrl.o \
 	$(OBJS)\monolib_iniconf.o \
 	$(OBJS)\monolib_listbox.o \
@@ -2445,7 +2441,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_dirctrlg.o \
 	$(OBJS)\monolib_dragimgg.o \
 	$(OBJS)\monolib_filectrlg.o \
-	$(OBJS)\monolib_headercolg.o \
 	$(OBJS)\monolib_headerctrlg.o \
 	$(OBJS)\monolib_listbkg.o \
 	$(OBJS)\monolib_logg.o \
@@ -2685,7 +2680,6 @@ ____CORE_SRC_FILENAMES_1_OBJECTS =  \
 	$(OBJS)\monolib_dirctrlg.o \
 	$(OBJS)\monolib_dragimgg.o \
 	$(OBJS)\monolib_filectrlg.o \
-	$(OBJS)\monolib_headercolg.o \
 	$(OBJS)\monolib_headerctrlg.o \
 	$(OBJS)\monolib_listbkg.o \
 	$(OBJS)\monolib_logg.o \
@@ -2900,7 +2894,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_filedlg.o \
 	$(OBJS)\coredll_frame.o \
 	$(OBJS)\coredll_gauge.o \
-	$(OBJS)\coredll_headercol.o \
 	$(OBJS)\coredll_headerctrl.o \
 	$(OBJS)\coredll_iniconf.o \
 	$(OBJS)\coredll_listbox.o \
@@ -3044,7 +3037,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_dirctrlg.o \
 	$(OBJS)\coredll_dragimgg.o \
 	$(OBJS)\coredll_filectrlg.o \
-	$(OBJS)\coredll_headercolg.o \
 	$(OBJS)\coredll_headerctrlg.o \
 	$(OBJS)\coredll_listbkg.o \
 	$(OBJS)\coredll_logg.o \
@@ -3284,7 +3276,6 @@ ____CORE_SRC_FILENAMES_2_OBJECTS =  \
 	$(OBJS)\coredll_dirctrlg.o \
 	$(OBJS)\coredll_dragimgg.o \
 	$(OBJS)\coredll_filectrlg.o \
-	$(OBJS)\coredll_headercolg.o \
 	$(OBJS)\coredll_headerctrlg.o \
 	$(OBJS)\coredll_listbkg.o \
 	$(OBJS)\coredll_logg.o \
@@ -3390,7 +3381,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_filedlg.o \
 	$(OBJS)\corelib_frame.o \
 	$(OBJS)\corelib_gauge.o \
-	$(OBJS)\corelib_headercol.o \
 	$(OBJS)\corelib_headerctrl.o \
 	$(OBJS)\corelib_iniconf.o \
 	$(OBJS)\corelib_listbox.o \
@@ -3534,7 +3524,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_dirctrlg.o \
 	$(OBJS)\corelib_dragimgg.o \
 	$(OBJS)\corelib_filectrlg.o \
-	$(OBJS)\corelib_headercolg.o \
 	$(OBJS)\corelib_headerctrlg.o \
 	$(OBJS)\corelib_listbkg.o \
 	$(OBJS)\corelib_logg.o \
@@ -3774,7 +3763,6 @@ ____CORE_SRC_FILENAMES_3_OBJECTS =  \
 	$(OBJS)\corelib_dirctrlg.o \
 	$(OBJS)\corelib_dragimgg.o \
 	$(OBJS)\corelib_filectrlg.o \
-	$(OBJS)\corelib_headercolg.o \
 	$(OBJS)\corelib_headerctrlg.o \
 	$(OBJS)\corelib_listbkg.o \
 	$(OBJS)\corelib_logg.o \
@@ -5903,9 +5891,6 @@ $(OBJS)\monodll_filedlg.o: ../../src/msw/filedlg.cpp
 $(OBJS)\monodll_frame.o: ../../src/msw/frame.cpp
 	$(CXX) -c -o $@ $(MONODLL_CXXFLAGS) $(CPPDEPS) $<
 
-$(OBJS)\monodll_headercol.o: ../../src/msw/headercol.cpp
-	$(CXX) -c -o $@ $(MONODLL_CXXFLAGS) $(CPPDEPS) $<
-
 $(OBJS)\monodll_headerctrl.o: ../../src/msw/headerctrl.cpp
 	$(CXX) -c -o $@ $(MONODLL_CXXFLAGS) $(CPPDEPS) $<
 
@@ -7586,11 +7571,6 @@ $(OBJS)\monodll_filectrlg.o: ../../src/generic/filectrlg.cpp
 endif
 
 ifeq ($(USE_GUI),1)
-$(OBJS)\monodll_headercolg.o: ../../src/generic/headercolg.cpp
-	$(CXX) -c -o $@ $(MONODLL_CXXFLAGS) $(CPPDEPS) $<
-endif
-
-ifeq ($(USE_GUI),1)
 $(OBJS)\monodll_headerctrlg.o: ../../src/generic/headerctrlg.cpp
 	$(CXX) -c -o $@ $(MONODLL_CXXFLAGS) $(CPPDEPS) $<
 endif
@@ -8232,9 +8212,6 @@ $(OBJS)\monolib_filedlg.o: ../../src/msw/filedlg.cpp
 	$(CXX) -c -o $@ $(MONOLIB_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\monolib_frame.o: ../../src/msw/frame.cpp
-	$(CXX) -c -o $@ $(MONOLIB_CXXFLAGS) $(CPPDEPS) $<
-
-$(OBJS)\monolib_headercol.o: ../../src/msw/headercol.cpp
 	$(CXX) -c -o $@ $(MONOLIB_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\monolib_headerctrl.o: ../../src/msw/headerctrl.cpp
@@ -9917,11 +9894,6 @@ $(OBJS)\monolib_filectrlg.o: ../../src/generic/filectrlg.cpp
 endif
 
 ifeq ($(USE_GUI),1)
-$(OBJS)\monolib_headercolg.o: ../../src/generic/headercolg.cpp
-	$(CXX) -c -o $@ $(MONOLIB_CXXFLAGS) $(CPPDEPS) $<
-endif
-
-ifeq ($(USE_GUI),1)
 $(OBJS)\monolib_headerctrlg.o: ../../src/generic/headerctrlg.cpp
 	$(CXX) -c -o $@ $(MONOLIB_CXXFLAGS) $(CPPDEPS) $<
 endif
@@ -10950,9 +10922,6 @@ $(OBJS)\coredll_filedlg.o: ../../src/msw/filedlg.cpp
 	$(CXX) -c -o $@ $(COREDLL_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\coredll_frame.o: ../../src/msw/frame.cpp
-	$(CXX) -c -o $@ $(COREDLL_CXXFLAGS) $(CPPDEPS) $<
-
-$(OBJS)\coredll_headercol.o: ../../src/msw/headercol.cpp
 	$(CXX) -c -o $@ $(COREDLL_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\coredll_headerctrl.o: ../../src/msw/headerctrl.cpp
@@ -12272,11 +12241,6 @@ $(OBJS)\coredll_filectrlg.o: ../../src/generic/filectrlg.cpp
 endif
 
 ifeq ($(USE_GUI),1)
-$(OBJS)\coredll_headercolg.o: ../../src/generic/headercolg.cpp
-	$(CXX) -c -o $@ $(COREDLL_CXXFLAGS) $(CPPDEPS) $<
-endif
-
-ifeq ($(USE_GUI),1)
 $(OBJS)\coredll_headerctrlg.o: ../../src/generic/headerctrlg.cpp
 	$(CXX) -c -o $@ $(COREDLL_CXXFLAGS) $(CPPDEPS) $<
 endif
@@ -12432,9 +12396,6 @@ $(OBJS)\corelib_filedlg.o: ../../src/msw/filedlg.cpp
 	$(CXX) -c -o $@ $(CORELIB_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\corelib_frame.o: ../../src/msw/frame.cpp
-	$(CXX) -c -o $@ $(CORELIB_CXXFLAGS) $(CPPDEPS) $<
-
-$(OBJS)\corelib_headercol.o: ../../src/msw/headercol.cpp
 	$(CXX) -c -o $@ $(CORELIB_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\corelib_headerctrl.o: ../../src/msw/headerctrl.cpp
@@ -13750,11 +13711,6 @@ endif
 
 ifeq ($(USE_GUI),1)
 $(OBJS)\corelib_filectrlg.o: ../../src/generic/filectrlg.cpp
-	$(CXX) -c -o $@ $(CORELIB_CXXFLAGS) $(CPPDEPS) $<
-endif
-
-ifeq ($(USE_GUI),1)
-$(OBJS)\corelib_headercolg.o: ../../src/generic/headercolg.cpp
 	$(CXX) -c -o $@ $(CORELIB_CXXFLAGS) $(CPPDEPS) $<
 endif
 

commit 140a119ad2c5697bef8910783bb8a16bf293fa54

restore sorting functionality broken by r57161
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@57168 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/gtk/dataview.cpp
@@ -2686,7 +2686,16 @@ void wxDataViewColumn::SetSortable( bool sortable )
 {
     GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
 
-    gtk_tree_view_column_set_clickable( column, sortable );
+    if ( sortable )
+    {
+        gtk_tree_view_column_set_sort_column_id( column, GetModelColumn() );
+    }
+    else
+    {
+        gtk_tree_view_column_set_sort_column_id( column, -1 );
+        gtk_tree_view_column_set_sort_indicator( column, FALSE );
+        gtk_tree_view_column_set_clickable( column, FALSE );
+    }
 }
 
 bool wxDataViewColumn::IsSortable() const
@@ -2695,25 +2704,18 @@ bool wxDataViewColumn::IsSortable() const
     return gtk_tree_view_column_get_clickable( column );
 }
 
-void wxDataViewColumn::SetAsSortKey( bool sort )
+void wxDataViewColumn::SetAsSortKey( bool WXUNUSED(sort) )
 {
-    GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
-
-    if (sort)
-    {
-        gtk_tree_view_column_set_sort_column_id( column, GetModelColumn() );
-    }
-    else
-    {
-        gtk_tree_view_column_set_sort_column_id( column, -1 );
-        gtk_tree_view_column_set_sort_indicator( column, FALSE );
-    }
+    // it might not make sense to have this function in wxHeaderColumnBase at
+    // all in fact, changing of the sort order should only be done using the
+    // associated control API
+    wxFAIL_MSG( "not implemented" );
 }
 
 bool wxDataViewColumn::IsSortKey() const
 {
     GtkTreeViewColumn *column = GTK_TREE_VIEW_COLUMN(m_column);
-    return (gtk_tree_view_column_get_sort_column_id( column ) != -1);
+    return gtk_tree_view_column_get_sort_indicator( column );
 }
 
 bool wxDataViewColumn::IsResizeable() const