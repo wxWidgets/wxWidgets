9653bb3552bb6f14b252f661f54d95f32fce4d00
Bryan Petty <bryan@ibaku.net>
2009-01-06 08:11:05 +0000

    Fixed a couple Doxygen warnings, patch from Tim Stahlhut.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@57847 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/wxcrt.h
+++ interface/wx/wxcrt.h
@@ -91,7 +91,7 @@ wxArrayString wxStringTokenize(const wxString& string,
 
     This function copies the source string @a src to the destination buffer @a
     dst of size @a n without overflowing the buffer and ensuring that it is
-    always @NUL-terminated.
+    always @NULL-terminated.
 
     Example of use:
     @code
@@ -121,7 +121,7 @@ wxArrayString wxStringTokenize(const wxString& string,
     @param dst
         Destination buffer of size (greater or) equal to @a n.
     @param src
-        @NUL-terminated source string.
+        @NULL-terminated source string.
     @param n
         The size of the destination buffer.
     @return
