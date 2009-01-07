bb32406153a194de2bb488c54a11e12b184cf5bc
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2009-01-07 17:56:52 +0000

    document wxConvertFromIeeeExtended and wxConvertToIeeeExtended
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@57892 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/math.h
+++ interface/wx/math.h
@@ -24,5 +24,24 @@ int wxFinite(double x);
 */
 bool wxIsNaN(double x);
 
+/**
+    Converts the given array of 10 bytes (corresponding to 80 bits) to
+    a float number according to the IEEE floating point standard format
+    (aka IEEE standard 754).
+
+    @see wxConvertToIeeeExtended() to perform the opposite operation
+*/
+wxFloat64 wxConvertFromIeeeExtended(const wxInt8 *bytes);
+
+/**
+    Converts the given floating number @a num in a sequence of 10 bytes
+    which are stored in the given array @a bytes (which must be large enough)
+    according to the IEEE floating point standard format
+    (aka IEEE standard 754).
+
+    @see wxConvertFromIeeeExtended() to perform the opposite operation
+*/
+void wxConvertToIeeeExtended(wxFloat64 num, wxInt8 *bytes);
+
 //@}
 
