8caabedb27e0a1a35bbc392b84a92285e56ca6e3
Paul Cornett <paulcor@bullseye.com>
2009-01-12 05:34:07 +0000

    use text cursor only when control is enabled, #10347
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@58030 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/textctrl.cpp
+++ src/gtk/textctrl.cpp
@@ -1252,6 +1252,7 @@ bool wxTextCtrl::Enable( bool enable )
     }
 
     gtk_widget_set_sensitive( m_text, enable );
+    SetCursor(enable ? wxCursor(wxCURSOR_IBEAM) : wxCursor());
 
     return true;
 }
