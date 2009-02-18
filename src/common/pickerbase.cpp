60433f3f826017053d1845ceebc596e544cc0ca2
Robert Roebling <robert@roebling.de>
2009-02-18 21:21:19 +0000

    Uncomment code accidentally left commented
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@59006 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/pickerbase.cpp
+++ src/common/pickerbase.cpp
@@ -94,7 +94,7 @@ bool wxPickerBase::CreateBase(wxWindow *parent,
         // the m_picker; for very long strings, this real-time synchronization could
         // become a CPU-blocker and thus should be avoided.
         // 32 characters will be more than enough for all common uses.
-        // m_text->SetMaxLength(32);
+        m_text->SetMaxLength(32);
 
         // set the initial contents of the textctrl
         m_text->SetValue(text);
