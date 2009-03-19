778c9b9f490e94f9de632d40438c959ee5b5bb8f
Francesco Montorsi <f18m_cpp217828@yahoo.it>
2009-03-19 01:37:11 +0000

    commit temporary fix for the sample for 2.9.0 release
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@59619 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/statbar/statbar.cpp
+++ samples/statbar/statbar.cpp
@@ -740,6 +740,11 @@ void MyStatusBar::OnSize(wxSizeEvent& event)
         return;
 #endif
 
+    // TEMPORARY HACK: TODO find a more general solution
+#ifdef wxStatusBarGeneric
+    wxStatusBar::OnSize(event);
+#endif
+
     wxRect rect;
     if (!GetFieldRect(Field_Checkbox, rect))
     {
