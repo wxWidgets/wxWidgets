427d3c8d897aeff6dbd8045f98a64baaf052eaa1
Václav Slavík <vslavik@fastmail.fm>
2009-03-21 09:41:09 +0000

    wxHTML tables handler didn't properly restore m_enclosingContainer state (fixes #10370)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@59687 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/html/m_tables.cpp
+++ src/html/m_tables.cpp
@@ -684,6 +684,7 @@ TAG_HANDLER_BEGIN(TABLE, "TABLE,TR,TD,TH")
         {
             wxHtmlTableCell *oldt = m_Table;
 
+            wxHtmlContainerCell *oldEnclosing = m_enclosingContainer;
             m_enclosingContainer = c = m_WParser->OpenContainer();
 
             m_Table = new wxHtmlTableCell(c, tag, m_WParser->GetPixelScale());
@@ -722,6 +723,7 @@ TAG_HANDLER_BEGIN(TABLE, "TABLE,TR,TD,TH")
             m_WParser->CloseContainer();
 
             m_Table = oldt;
+            m_enclosingContainer = oldEnclosing;
 
             return true; // ParseInner() called
         }
