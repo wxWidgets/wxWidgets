561d9316e92aec46dec45525c701bca99f749ee0
Vadim Zeitlin <vadim@wxwidgets.org>
2009-03-22 21:07:51 +0000

    set the PATH to contain ../lib/*_dll directories before running the tests
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@59741 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -5,6 +5,9 @@ REM This script is used to return the correct return value to the caller
 REM which is required by Buildbot to recognize failures.
 REM $Id: runtests.bat 59695 2009-03-21 13:49:20Z FM $
 
+rem set the path for running the tests if they use DLL build of wx
+for /d %%x in (..\lib\*_dll) do @PATH=%x;%PATH%
+
 set failure=0
 
 for /d %%x in (*) do @(
