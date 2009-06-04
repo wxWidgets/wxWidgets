e6ad974372346beb7beea39d10e243e041496ff9
Bartosz Bekier <bartosz.bekier@gmail.com>
2009-06-04 22:40:31 +0000

    File system watcher public interface in its basic form.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2009_FSWATCHER@60898 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/fswatcher.h
+++ include/wx/fswatcher.h
@@ -16,6 +16,75 @@
 #if wxUSE_FSWATCHER
 
 #include "wx/event.h"
+#include "wx/filename.h"
+
+// ----------------------------------------------------------------------------
+// wxFileSystemWatcherEventType & wxFileSystemWatcherEvent
+// ----------------------------------------------------------------------------
+
+/**
+    Possible types of file system events.
+    This is a subset that will work fine an all platforms (actually, we will
+    see how it works on Mac).
+ */
+enum wxFileSystemWatcherEventType
+{
+    wxFSW_EVENT_CREATE = 0x01,
+    wxFSW_EVENT_DELETE = 0x02,
+    wxFSW_EVENT_RENAME = 0x04,
+    wxFSW_EVENT_MODIFY = 0x08,
+    wxFSW_EVENT_ACCESS = 0x10,
+    wxFSW_EVENT_ALL = wxFSW_EVENT_CREATE | wxFSW_EVENT_DELETE |
+                      wxFSW_EVENT_RENAME | wxFSW_EVENT_MODIFY |
+                      wxFSW_EVENT_ACCESS,
+};
+
+typedef int wxFileSystemWatcherEventFlags;
+
+/**
+    Event containing information about file system change.
+ */
+class WXDLLIMPEXP_FWD_BASE wxFileSystemWatcherEvent;
+wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_BASE, wxEVT_FSWATCHER,
+                         wxFileSystemWatcherEvent);
+
+class WXDLLIMPEXP_BASE wxFileSystemWatcherEvent: public wxEvent
+{
+public:
+    wxFileSystemWatcherEvent(int watchid = wxID_ANY) :
+        wxEvent(watchid, wxEVT_FSWATCHER)
+    {
+    }
+
+    /**
+        Returns the path at which the event occurred.
+     */
+    const wxFileName& GetPath() const;
+
+    /**
+        In case of rename(move?) events, returns the new path related to the
+        event. The "new" means newer in the sense of time. In case of other
+        events it returns the same path as GetPath().
+     */
+    const wxFileName& GetNewPath() const;
+
+    /**
+        Returns the type of file system event that occurred.
+     */
+    wxFileSystemWatcherEventType GetChangeType() const;
+
+    virtual wxEvent* Clone() const
+    {
+        return new wxFileSystemWatcherEvent(*this);
+    }
+
+    virtual wxEventCategory GetEventCategory() const;
+};
+
+
+// ----------------------------------------------------------------------------
+// wxFileSystemWatcherBase: interface for wxFileSystemWatcher
+// ----------------------------------------------------------------------------
 
 /**
     Main entry point for clients interested in file system events.
@@ -24,9 +93,58 @@
 class WXDLLIMPEXP_BASE wxFileSystemWatcherBase: public wxEvtHandler
 {
 public:
+    wxFileSystemWatcherBase()
+    {
+        Init();
+    }
 
+    wxFileSystemWatcherBase(const wxFileName& path,
+                        wxFileSystemWatcherEventFlags events = wxFSW_EVENT_ALL)
+    {
+        Init();
+        Add(path, events);
+    }
+
+    virtual ~wxFileSystemWatcherBase()
+    {
+    }
+
+    /**
+        Adds path to currently watched files. Any events concerning this
+        particular path will be sent to handler. Optionally a filter can be
+        specified to receive only events of particular type.
+     */
+    virtual int Add(const wxFileName& path,
+                   wxFileSystemWatcherEventFlags events = wxFSW_EVENT_ALL) = 0;
+
+    /**
+        Like above, but recursively adds every file/dir in the tree rooted in
+        path. Additionally a file mask can be specified to include only files
+        of particular type.
+     */
+    virtual int AddTree(const wxFileName& path,
+                        wxFileSystemWatcherEventFlags events = wxFSW_EVENT_ALL,
+                        const wxString& filter = wxEmptyString) = 0;
+
+    /**
+        Removes path from the list of watched paths.
+     */
+    virtual int Remove(const wxFileName& path) = 0;
+
+    /**
+        Same as above, but also removes every file belonging to the tree rooted
+        at path.
+     */
+    virtual int RemoveTree(const wxFileName& path) = 0;
+
+    /**
+        Stops watching all paths.
+     */
+    virtual int RemoveAll() = 0;
+
+protected:
+    int Init();
 };
-
 
 // include the platform specific file defining wxFileSystemWatcher
 // inheriting from wxFileSystemWatcherBase
