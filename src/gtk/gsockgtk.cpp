c579f80892a919fc8408bf7b366ab3d86d4f38f0
Vadim Zeitlin <vadim@wxwidgets.org>
2009-06-12 03:35:38 +0000

    avoid name clashes with GSocket from glib 2.21+ (closes #10883)
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@61009 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/gsockgtk.cpp
+++ src/gtk/gsockgtk.cpp
@@ -15,8 +15,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// newer versions of glib define its own GSocket but we unfortunately use this
+// name in our own (semi-)public header and so can't change it -- rename glib
+// one instead
+#define GSocket GlibGSocket
 #include <gdk/gdk.h>
 #include <glib.h>
+#undef GSocket
 
 #include "wx/gsocket.h"
 #include "wx/unix/gsockunx.h"
