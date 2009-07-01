9630954d5d9541f2bd9fd84beb427dc59c9ded31
Vadim Zeitlin <vadim@wxwidgets.org>
2009-07-01 09:02:30 +0000

    remove _T()s and wxChars from the test making it much more readable
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@61270 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/filename/filenametest.cpp
+++ tests/filename/filenametest.cpp
@@ -44,69 +44,69 @@ inline std::ostream& operator<<(std::ostream& o, const wxFileName& fn)
 
 static struct TestFileNameInfo
 {
-    const wxChar *fullname;
-    const wxChar *volume;
-    const wxChar *path;
-    const wxChar *name;
-    const wxChar *ext;
+    const char *fullname;
+    const char *volume;
+    const char *path;
+    const char *name;
+    const char *ext;
     bool isAbsolute;
     wxPathFormat format;
 } filenames[] =
 {
     // the empty string
-    { _T(""), _T(""), _T(""), _T(""), _T(""), false, wxPATH_UNIX },
-    { _T(""), _T(""), _T(""), _T(""), _T(""), false, wxPATH_DOS },
-    { _T(""), _T(""), _T(""), _T(""), _T(""), false, wxPATH_VMS },
+    { "", "", "", "", "", false, wxPATH_UNIX },
+    { "", "", "", "", "", false, wxPATH_DOS },
+    { "", "", "", "", "", false, wxPATH_VMS },
 
     // Unix file names
-    { _T("/usr/bin/ls"), _T(""), _T("/usr/bin"), _T("ls"), _T(""), true, wxPATH_UNIX },
-    { _T("/usr/bin/"), _T(""), _T("/usr/bin"), _T(""), _T(""), true, wxPATH_UNIX },
-    { _T("~/.zshrc"), _T(""), _T("~"), _T(".zshrc"), _T(""), true, wxPATH_UNIX },
-    { _T("../../foo"), _T(""), _T("../.."), _T("foo"), _T(""), false, wxPATH_UNIX },
-    { _T("foo.bar"), _T(""), _T(""), _T("foo"), _T("bar"), false, wxPATH_UNIX },
-    { _T("~/foo.bar"), _T(""), _T("~"), _T("foo"), _T("bar"), true, wxPATH_UNIX },
-    { _T("/foo"), _T(""), _T("/"), _T("foo"), _T(""), true, wxPATH_UNIX },
-    { _T("Mahogany-0.60/foo.bar"), _T(""), _T("Mahogany-0.60"), _T("foo"), _T("bar"), false, wxPATH_UNIX },
-    { _T("/tmp/wxwin.tar.bz"), _T(""), _T("/tmp"), _T("wxwin.tar"), _T("bz"), true, wxPATH_UNIX },
+    { "/usr/bin/ls", "", "/usr/bin", "ls", "", true, wxPATH_UNIX },
+    { "/usr/bin/", "", "/usr/bin", "", "", true, wxPATH_UNIX },
+    { "~/.zshrc", "", "~", ".zshrc", "", true, wxPATH_UNIX },
+    { "../../foo", "", "../..", "foo", "", false, wxPATH_UNIX },
+    { "foo.bar", "", "", "foo", "bar", false, wxPATH_UNIX },
+    { "~/foo.bar", "", "~", "foo", "bar", true, wxPATH_UNIX },
+    { "/foo", "", "/", "foo", "", true, wxPATH_UNIX },
+    { "Mahogany-0.60/foo.bar", "", "Mahogany-0.60", "foo", "bar", false, wxPATH_UNIX },
+    { "/tmp/wxwin.tar.bz", "", "/tmp", "wxwin.tar", "bz", true, wxPATH_UNIX },
 
     // Windows file names
-    { _T("foo.bar"), _T(""), _T(""), _T("foo"), _T("bar"), false, wxPATH_DOS },
-    { _T("\\foo.bar"), _T(""), _T("\\"), _T("foo"), _T("bar"), false, wxPATH_DOS },
-    { _T("c:foo.bar"), _T("c"), _T(""), _T("foo"), _T("bar"), false, wxPATH_DOS },
-    { _T("c:\\foo.bar"), _T("c"), _T("\\"), _T("foo"), _T("bar"), true, wxPATH_DOS },
-    { _T("c:\\Windows\\command.com"), _T("c"), _T("\\Windows"), _T("command"), _T("com"), true, wxPATH_DOS },
+    { "foo.bar", "", "", "foo", "bar", false, wxPATH_DOS },
+    { "\\foo.bar", "", "\\", "foo", "bar", false, wxPATH_DOS },
+    { "c:foo.bar", "c", "", "foo", "bar", false, wxPATH_DOS },
+    { "c:\\foo.bar", "c", "\\", "foo", "bar", true, wxPATH_DOS },
+    { "c:\\Windows\\command.com", "c", "\\Windows", "command", "com", true, wxPATH_DOS },
 
 #if 0
     // NB: when using the wxFileName::GetLongPath() function on these two
     //     strings, the program will hang for several seconds blocking inside
     //     Win32 GetLongPathName() function
-    { _T("\\\\server\\foo.bar"), _T("server"), _T("\\"), _T("foo"), _T("bar"), true, wxPATH_DOS },
-    { _T("\\\\server\\dir\\foo.bar"), _T("server"), _T("\\dir"), _T("foo"), _T("bar"), true, wxPATH_DOS },
+    { "\\\\server\\foo.bar", "server", "\\", "foo", "bar", true, wxPATH_DOS },
+    { "\\\\server\\dir\\foo.bar", "server", "\\dir", "foo", "bar", true, wxPATH_DOS },
 #endif
 
     // consecutive [back]slashes should be treated as single occurrences of
     // them and not interpreted as share names if there is a volume name
-    { _T("c:\\aaa\\bbb\\ccc"), _T("c"), _T("\\aaa\\bbb"), _T("ccc"), _T(""), true, wxPATH_DOS },
-    { _T("c:\\\\aaa\\bbb\\ccc"), _T("c"), _T("\\\\aaa\\bbb"), _T("ccc"), _T(""), true, wxPATH_DOS },
+    { "c:\\aaa\\bbb\\ccc", "c", "\\aaa\\bbb", "ccc", "", true, wxPATH_DOS },
+    { "c:\\\\aaa\\bbb\\ccc", "c", "\\\\aaa\\bbb", "ccc", "", true, wxPATH_DOS },
 
     // wxFileName support for Mac file names is broken currently
 #if 0
     // Mac file names
-    { _T("Volume:Dir:File"), _T("Volume"), _T("Dir"), _T("File"), _T(""), true, wxPATH_MAC },
-    { _T("Volume:Dir:Subdir:File"), _T("Volume"), _T("Dir:Subdir"), _T("File"), _T(""), true, wxPATH_MAC },
-    { _T("Volume:"), _T("Volume"), _T(""), _T(""), _T(""), true, wxPATH_MAC },
-    { _T(":Dir:File"), _T(""), _T("Dir"), _T("File"), _T(""), false, wxPATH_MAC },
-    { _T(":File.Ext"), _T(""), _T(""), _T("File"), _T(".Ext"), false, wxPATH_MAC },
-    { _T("File.Ext"), _T(""), _T(""), _T("File"), _T(".Ext"), false, wxPATH_MAC },
+    { "Volume:Dir:File", "Volume", "Dir", "File", "", true, wxPATH_MAC },
+    { "Volume:Dir:Subdir:File", "Volume", "Dir:Subdir", "File", "", true, wxPATH_MAC },
+    { "Volume:", "Volume", "", "", "", true, wxPATH_MAC },
+    { ":Dir:File", "", "Dir", "File", "", false, wxPATH_MAC },
+    { ":File.Ext", "", "", "File", ".Ext", false, wxPATH_MAC },
+    { "File.Ext", "", "", "File", ".Ext", false, wxPATH_MAC },
 #endif // 0
 
 #if 0
     // VMS file names
     // NB: on Windows they have the same effect of the \\server\\ strings
     //     (see the note above)
-    { _T("device:[dir1.dir2.dir3]file.txt"), _T("device"), _T("dir1.dir2.dir3"), _T("file"), _T("txt"), true, wxPATH_VMS },
+    { "device:[dir1.dir2.dir3]file.txt", "device", "dir1.dir2.dir3", "file", "txt", true, wxPATH_VMS },
 #endif
-    { _T("file.txt"), _T(""), _T(""), _T("file"), _T("txt"), false, wxPATH_VMS },
+    { "file.txt", "", "", "file", "txt", false, wxPATH_VMS },
 };
 
 // ----------------------------------------------------------------------------
@@ -167,14 +167,14 @@ void FileNameTestCase::TestConstruction()
         if ( fni.format == wxPATH_DOS )
         {
             // copy the backslashes at beginning unchanged
-            const wxChar *p = fni.fullname;
-            while ( *p == _T('\\') )
+            const char *p = fni.fullname;
+            while ( *p == '\\' )
                 fullnameOrig += *p++;
 
             // replace consecutive slashes with single ones in the rest
-            for ( wxChar chPrev = _T('\0'); *p; p++ )
+            for ( char chPrev = '\0'; *p; p++ )
             {
-                if ( *p == _T('\\') && chPrev == _T('\\') )
+                if ( *p == '\\' && chPrev == '\\' )
                     continue;
 
                 chPrev = *p;
@@ -186,7 +186,7 @@ void FileNameTestCase::TestConstruction()
             fullnameOrig = fni.fullname;
         }
 
-        fullnameOrig.Replace(_T("//"), _T("/"));
+        fullnameOrig.Replace("//", "/");
 
 
         wxString fullname = fn.GetFullPath(fni.format);
@@ -197,8 +197,8 @@ void FileNameTestCase::TestConstruction()
         // if the test is run from root directory or its direct subdirectory
         CPPUNIT_ASSERT_MESSAGE
         (
-            (const char *)wxString::Format(_T("Normalize(%s) failed"), fni.fullname).mb_str(),
-            fn.Normalize(wxPATH_NORM_ALL, _T("/foo/bar/baz"), fni.format)
+            (const char *)wxString::Format("Normalize(%s) failed", fni.fullname).mb_str(),
+            fn.Normalize(wxPATH_NORM_ALL, "/foo/bar/baz", fni.format)
         );
 
         if ( *fni.volume && *fni.path )
@@ -260,19 +260,19 @@ void FileNameTestCase::TestSplit()
     }
 
     // special case of empty extension
-    wxFileName fn(_T("foo."));
-    CPPUNIT_ASSERT_EQUAL( wxString(_T("foo.")), fn.GetFullPath() );
+    wxFileName fn("foo.");
+    CPPUNIT_ASSERT_EQUAL( wxString("foo."), fn.GetFullPath() );
 }
 
 void FileNameTestCase::TestSetPath()
 {
-    wxFileName fn(_T("d:\\test\\foo.bar"), wxPATH_DOS);
-    fn.SetPath(_T("c:\\temp"), wxPATH_DOS);
-    CPPUNIT_ASSERT( fn.SameAs(wxFileName(_T("c:\\temp\\foo.bar"), wxPATH_DOS)) );
+    wxFileName fn("d:\\test\\foo.bar", wxPATH_DOS);
+    fn.SetPath("c:\\temp", wxPATH_DOS);
+    CPPUNIT_ASSERT( fn.SameAs(wxFileName("c:\\temp\\foo.bar", wxPATH_DOS)) );
 
-    fn = wxFileName(_T("/usr/bin/ls"), wxPATH_UNIX);
-    fn.SetPath(_T("/usr/local/bin"), wxPATH_UNIX);
-    CPPUNIT_ASSERT( fn.SameAs(wxFileName(_T("/usr/local/bin/ls"), wxPATH_UNIX)) );
+    fn = wxFileName("/usr/bin/ls", wxPATH_UNIX);
+    fn.SetPath("/usr/local/bin", wxPATH_UNIX);
+    CPPUNIT_ASSERT( fn.SameAs(wxFileName("/usr/local/bin/ls", wxPATH_UNIX)) );
 }
 
 void FileNameTestCase::TestNormalize()
@@ -352,7 +352,7 @@ void FileNameTestCase::TestNormalize()
     };
 
     // set the env var ABCDEF
-    wxSetEnv(_T("ABCDEF"), _T("abcdef"));
+    wxSetEnv("ABCDEF", "abcdef");
 
     for ( size_t i = 0; i < WXSIZEOF(tests); i++ )
     {
@@ -368,8 +368,8 @@ void FileNameTestCase::TestNormalize()
 
         // compare result with expected string
         wxString expected(tests[i].expected);
-        expected.Replace(_T("HOME/"), home);
-        expected.Replace(_T("CWD/"), cwd);
+        expected.Replace("HOME/", home);
+        expected.Replace("CWD/", cwd);
         WX_ASSERT_EQUAL_MESSAGE
         (
             ("array element #%d", (int)i),
@@ -438,7 +438,7 @@ void FileNameTestCase::TestReplace()
         wxFileName fn(fnt.original, fnt.fmt);
 
         // set the environment variable
-        wxSetEnv(_T("TEST_VAR"), fnt.env_contents);
+        wxSetEnv("TEST_VAR", fnt.env_contents);
 
         // be sure this ReplaceEnvVariable does not fail
         WX_ASSERT_MESSAGE
@@ -470,25 +470,25 @@ void FileNameTestCase::TestReplace()
         fn.ReplaceHomeDir()
     );
 
-    CPPUNIT_ASSERT_EQUAL( wxString(_T("~/test1/test2/test3/some file")),
+    CPPUNIT_ASSERT_EQUAL( wxString("~/test1/test2/test3/some file"),
                           fn.GetFullPath(wxPATH_UNIX) );
 }
 
 void FileNameTestCase::TestStrip()
 {
-    CPPUNIT_ASSERT_EQUAL( "", wxFileName::StripExtension(_T("")) );
-    CPPUNIT_ASSERT_EQUAL( ".", wxFileName::StripExtension(_T(".")) );
-    CPPUNIT_ASSERT_EQUAL( ".vimrc", wxFileName::StripExtension(_T(".vimrc")) );
-    CPPUNIT_ASSERT_EQUAL( "bad", wxFileName::StripExtension(_T("bad")) );
-    CPPUNIT_ASSERT_EQUAL( "good", wxFileName::StripExtension(_T("good.wav")) );
-    CPPUNIT_ASSERT_EQUAL( "good.wav", wxFileName::StripExtension(_T("good.wav.wav")) );
+    CPPUNIT_ASSERT_EQUAL( "", wxFileName::StripExtension("") );
+    CPPUNIT_ASSERT_EQUAL( ".", wxFileName::StripExtension(".") );
+    CPPUNIT_ASSERT_EQUAL( ".vimrc", wxFileName::StripExtension(".vimrc") );
+    CPPUNIT_ASSERT_EQUAL( "bad", wxFileName::StripExtension("bad") );
+    CPPUNIT_ASSERT_EQUAL( "good", wxFileName::StripExtension("good.wav") );
+    CPPUNIT_ASSERT_EQUAL( "good.wav", wxFileName::StripExtension("good.wav.wav") );
 }
 
 #ifdef __WINDOWS__
 
 void FileNameTestCase::TestShortLongPath()
 {
-    wxFileName fn(_T("C:\\Program Files\\Windows NT\\Accessories\\wordpad.exe"));
+    wxFileName fn("C:\\Program Files\\Windows NT\\Accessories\\wordpad.exe");
 
     // incredibly enough, GetLongPath() used to return different results during
     // the first and subsequent runs, test for this
