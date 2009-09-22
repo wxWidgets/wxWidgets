66b33d22fadb78c860a450ca408b70387f242363
Julian Smart <julian@anthemion.co.uk>
2009-09-22 16:36:42 +0000

    Fix for #4696: added SetDefaultStyleToCursorStyle() to SetInsertionPoint() so style is taken from context, not the style at a previous location.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@62020 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextctrl.cpp
+++ src/richtext/richtextctrl.cpp
@@ -2517,6 +2517,8 @@ void wxRichTextCtrl::SetInsertionPoint(long pos)
     m_caretPosition = pos - 1;
 
     PositionCaret();
+
+    SetDefaultStyleToCursorStyle();
 }
 
 void wxRichTextCtrl::SetInsertionPointEnd()
