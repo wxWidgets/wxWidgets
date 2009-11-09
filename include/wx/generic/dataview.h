d4953dd4690bcaf6476ba5d93a01651ec4e91692
Vadim Zeitlin <vadim@wxwidgets.org>
2009-11-09 01:57:52 +0000

    Remove unused anywhere wxDataViewRenderer::RightClick().
    Code calling this method was removed by r53020 but its declaration was still
    left (almost certainly by mistake) in the header, remove it now too.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@62578 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/generic/dataview.h
+++ include/wx/generic/dataview.h
@@ -79,12 +79,6 @@ public:
                             const wxDataViewItem & WXUNUSED(item),
                             unsigned int WXUNUSED(col) )
                             { return false; }
-    virtual bool RightClick( wxPoint WXUNUSED(cursor),
-                             wxRect WXUNUSED(cell),
-                             wxDataViewModel *WXUNUSED(model),
-                             const wxDataViewItem & WXUNUSED(item),
-                             unsigned int WXUNUSED(col) )
-                             { return false; }
     virtual bool StartDrag( wxPoint WXUNUSED(cursor),
                             wxRect WXUNUSED(cell),
                             wxDataViewModel *WXUNUSED(model),
