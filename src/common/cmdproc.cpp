62b50655600d64ed3f9d26abe3c05f3e125e671c
Vadim Zeitlin <vadim@wxwidgets.org>
2009-12-06 02:29:49 +0000

    Use wxAcceleratorEntry::ToString() for undo/redo accelerator labels.
    Don't hardcode the string representation of the accelerators, it was
    inconsistent with the rest of wxWidgets which used '-' while here a '+' was
    used.
    
    Closes #11512.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@62791 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/cmdproc.cpp
+++ src/common/cmdproc.cpp
@@ -28,6 +28,7 @@
     #include "wx/intl.h"
     #include "wx/string.h"
     #include "wx/menu.h"
+    #include "wx/accel.h"
 #endif //WX_PRECOMP
 
 #include "wx/cmdproc.h"
@@ -59,8 +60,8 @@ wxCommandProcessor::wxCommandProcessor(int maxCommands)
 #if wxUSE_MENUS
     m_commandEditMenu = NULL;
 #endif // wxUSE_MENUS
-    m_undoAccelerator = wxT("\tCtrl+Z");
-    m_redoAccelerator = wxT("\tCtrl+Y");
+    m_undoAccelerator = wxAcceleratorEntry(wxACCEL_CTRL, 'Z').ToString();
+    m_redoAccelerator = wxAcceleratorEntry(wxACCEL_CTRL, 'Y').ToString();
 
     m_lastSavedCommand =
     m_currentCommand = wxList::compatibility_iterator();
