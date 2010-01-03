554946db7d7fdbcc7ef6529b3546693995522aa4
Stefan Csomor <csomor@advancedconcepts.ch>
2010-01-03 19:58:18 +0000

    correcting error message
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63049 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/private/timer.h
+++ include/wx/osx/private/timer.h
@@ -1,7 +1,7 @@
 /* common warning snippet for all osx direct includes */
 
 #ifndef wxOSX_USE_CARBON
-#error "this files should only be included after platform.h was included
+#error "this files should only be included after platform.h was included"
 #endif
 
 #if 1 // revert to wxOSX_USE_COCOA_OR_IPHONE in case of problems
