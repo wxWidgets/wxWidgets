e9d564b9053af7339740fa1586826a41ac77a9f7
Robin Dunn <robin@alldunn.com>
2010-02-05 00:27:19 +0000

    subrelease number should be reset to zero when incrementing the release number
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@63392 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/version.h
+++ include/wx/version.h
@@ -29,7 +29,7 @@
 #define wxMAJOR_VERSION      2
 #define wxMINOR_VERSION      8
 #define wxRELEASE_NUMBER     11
-#define wxSUBRELEASE_NUMBER  1
+#define wxSUBRELEASE_NUMBER  0
 #define wxVERSION_STRING   wxT("wxWidgets 2.8.11")
 
 /*  nothing to update below this line when updating the version */
