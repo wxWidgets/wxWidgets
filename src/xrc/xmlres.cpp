e03951b772a4e202282fbbd6eb4ff618204148db
Václav Slavík <vslavik@fastmail.fm>
2010-02-11 12:36:16 +0000

    Fixed possible buffer overflow in wxXmlResource::GetXRCID() (patch #11715).
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63464 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xrc/xmlres.cpp
+++ src/xrc/xmlres.cpp
@@ -1996,9 +1996,9 @@ static XRCID_record *XRCID_Records[XRCID_TABLE_SIZE] = {NULL};
 
 static int XRCID_Lookup(const char *str_id, int value_if_not_found = wxID_NONE)
 {
-    int index = 0;
+    unsigned int index = 0;
 
-    for (const char *c = str_id; *c != '\0'; c++) index += (int)*c;
+    for (const char *c = str_id; *c != '\0'; c++) index += (unsigned int)*c;
     index %= XRCID_TABLE_SIZE;
 
     XRCID_record *oldrec = NULL;
