db06bfb4026c7cde587c139b67fac1dd65a0a214
Vadim Zeitlin <vadim@wxwidgets.org>
2010-03-05 23:55:00 +0000

    Use wxWindow::ReleaseMouse() instead of MSW ::ReleaseCapture() function.
    As we capture the mouse using wxWindow::CaptureMouse(), we must release it
    with ReleaseMouse() and not just ReleaseCapture().
    
    Closes #11791.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63628 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/tglbtn.cpp
+++ src/msw/tglbtn.cpp
@@ -187,7 +187,7 @@ void wxBitmapToggleButton::OnMouse(wxMouseEvent &event)
 
         if (event.LeftUp())
         {
-            ReleaseCapture();
+            ReleaseMouse();
             m_capturing = false;
             if (mouse_in)
             {
