a45c9ba59b610080585f76b0d02e1f1499c96ffe
Kevin Ollivier <kevino@theolliviers.com>
2010-03-06 04:51:51 +0000

    A call to wxPopupWindow::Show shouldn't automatically cause the popup window to steal focus.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63641 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/popupwin.h
+++ include/wx/osx/popupwin.h
@@ -26,6 +26,8 @@ public:
         { (void)Create(parent, flags); }
 
     bool Create(wxWindow *parent, int flags = wxBORDER_NONE);
+    
+    virtual bool Show(bool show);
 
 protected:
     DECLARE_DYNAMIC_CLASS_NO_COPY(wxPopupWindow)
