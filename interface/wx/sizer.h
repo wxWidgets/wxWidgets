0e92a846e4bb532856339f39cb10a0f65fc6055d
Robert Roebling <robert@roebling.de>
2010-03-16 19:13:36 +0000

    Don't use grouping of different methods as this creates confusing output
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63694 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/sizer.h
+++ interface/wx/sizer.h
@@ -1633,23 +1633,28 @@ public:
     wxGridSizer( int rows, int cols, const wxSize& gap );
     //@}
 
-    //@{
     /**
-        Returns the number of columns or rows that has been specified for the
+        Returns the number of columns that has been specified for the
         sizer.
 
         Returns zero if the sizer is automatically adjusting the number of
-        columns/rows depending on number of its children. To get the effective
-        number of columns or rows being currently used, see
-        GetEffectiveColsCount() and GetEffectiveRowsCount().
+        columns depending on number of its children. To get the effective
+        number of columns or rows being currently used, see GetEffectiveColsCount()
     */
     int GetCols() const;
-    int GetRows() const;
-    //@}
-
-    //@{
+    
     /**
-        Returns the number of columns or rows currently used by the sizer.
+        Returns the number of rows that has been specified for the
+        sizer.
+
+        Returns zero if the sizer is automatically adjusting the number of
+        rows depending on number of its children. To get the effective
+        number of columns or rows being currently used, see GetEffectiveRowsCount().
+    */
+    int GetRows() const;
+
+    /**
+        Returns the number of columns currently used by the sizer.
 
         This will depend on the number of children the sizer has if
         the sizer is automatically adjusting the number of columns/rows.
@@ -1657,8 +1662,16 @@ public:
         @since 2.9.1
     */
     int GetEffectiveColsCount() const;
+    
+    /**
+        Returns the number of rows currently used by the sizer.
+
+        This will depend on the number of children the sizer has if
+        the sizer is automatically adjusting the number of columns/rows.
+
+        @since 2.9.1
+    */
     int GetEffectiveRowsCount() const;
-    //@}
 
     /**
         Returns the horizontal gap (in pixels) between cells in the sizer.
