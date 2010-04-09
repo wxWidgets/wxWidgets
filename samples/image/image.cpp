9b2f7ad51323d51c836cb476b0b303d40bcdee24
Stefan Csomor <csomor@advancedconcepts.ch>
2010-04-09 08:10:31 +0000

    reverting r57083 for OSX, fixes #11922
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63921 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/image/image.cpp
+++ samples/image/image.cpp
@@ -185,8 +185,11 @@ private:
     {
         wxPaintDC dc(this);
 
+#ifndef __WXOSX__
+        // on OSX the immediate Update from within ClearBackground leads to a recursion
         if ( GetMenuBar()->IsChecked(ID_PAINT_BG) )
             ClearBackground();
+#endif
 
         dc.SetUserScale(m_zoom, m_zoom);
 
