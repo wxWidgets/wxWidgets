9def6786d6dfa2be639dc339a5b100e6bedd99aa
Vadim Zeitlin <vadim@wxwidgets.org>
2010-04-12 11:21:37 +0000

    Fix string stream unit test compilation in non-Unicode build.
    wxScopedCharBuffer can't be constructed from char* string, just use
    wxCharBuffer instead, we don't care about efficiency here.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63950 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/streams/sstream.cpp
+++ tests/streams/sstream.cpp
@@ -106,7 +106,7 @@ void strStream::CheckString(const wxString& text)
 {
     wxStringOutputStream sos;
 
-    const wxScopedCharBuffer buf(text.mb_str());
+    const wxCharBuffer buf(text.mb_str());
     sos.Write(buf, buf.length());
 
     CPPUNIT_ASSERT_EQUAL( text, sos.GetString() );
