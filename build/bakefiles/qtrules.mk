17cc893ce781aa9b960ec38f71f447e9a212d78d
Peter Most <Peter_Most@gmx.de>
2010-05-01 12:35:27 +0000

    Improved MOC rules
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/wxQT@64188 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- build/bakefiles/qtrules.mk
+++ build/bakefiles/qtrules.mk
@@ -1,9 +1,9 @@
 #
 # Rule for Qt's moc precompiler:
 #
-$(srcdir)/src/qt/%_qt_h.moc.cpp: ../include/wx/qt/%_qt.h
+$(srcdir)/src/qt/%_qt_h.moc.cpp: $(srcdir)/include/wx/qt/%_qt.h
 	moc $< -o $@
 
-$(srcdir)/src/qt/%_qt_cpp.moc.cpp: ../src/qt/%_qt.cpp
+$(srcdir)/src/qt/%_qt_cpp.moc.cpp: $(srcdir)/src/qt/%_qt.cpp
 	moc $< -o $@
     
