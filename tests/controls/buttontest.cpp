7e02b8d549f1b7f987a59c71e723b433b7043c13
Steve Lamerton <steve.lamerton@gmail.com>
2010-06-24 09:48:30 +0000

    Add an intentionally simple ButtonTestCase, mainly for demonstration purposes.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@64703 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ tests/controls/buttontest.cpp
@@ -0,0 +1,113 @@
+///////////////////////////////////////////////////////////////////////////////
+// Name:        tests/controls/buttontest.cpp
+// Purpose:     wxButton unit test
+// Author:      Steven Lamerton
+// Created:     2010-06-21
+// RCS-ID:      $Id$
+// Copyright:   (c) 2010 Steven Lamerton
+///////////////////////////////////////////////////////////////////////////////
+
+#include "testprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+    #include "wx/app.h"
+    #include "wx/event.h"
+    #include "wx/button.h"
+#endif // WX_PRECOMP
+
+#include "testableframe.h"
+#include "wx/uiaction.h"
+
+class ButtonTestCase : public CppUnit::TestCase
+{
+public:
+    ButtonTestCase() { }
+
+    void setUp();
+    void tearDown();
+
+private:
+    CPPUNIT_TEST_SUITE( ButtonTestCase );
+        CPPUNIT_TEST( Click );
+        CPPUNIT_TEST( Disabled );
+    CPPUNIT_TEST_SUITE_END();
+
+    void Click();
+    void Disabled();
+
+    wxButton* m_button;
+
+    DECLARE_NO_COPY_CLASS(ButtonTestCase)
+};
+
+// register in the unnamed registry so that these tests are run by default
+CPPUNIT_TEST_SUITE_REGISTRATION( ButtonTestCase );
+
+// also include in it's own registry so that these tests can be run alone
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( ButtonTestCase, "ButtonTestCase" );
+
+void ButtonTestCase::setUp()
+{
+    //We use wxTheApp->GetTopWindow() as there is only a single testable frame
+    //so it will always be returned
+    m_button = new wxButton(wxTheApp->GetTopWindow(), wxID_ANY, "wxButton");
+}
+
+void ButtonTestCase::Click()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                          wxTestableFrame);
+
+    //We use connect instead of bind for maximum compatibility
+    m_button->Connect(wxEVT_COMMAND_BUTTON_CLICKED,
+                      wxEventHandler(wxTestableFrame::OnEvent),
+                      NULL,
+                      frame);
+
+    wxUIActionSimulator sim;
+
+    //We move in slightly to account for window decorations
+    sim.MouseMove(m_button->GetScreenPosition() + wxPoint(10, 10));
+    sim.MouseClick();
+
+    //We have to yield to let event processing take place
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL( 1, frame->GetEventCount() );
+}
+
+void ButtonTestCase::Disabled()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                          wxTestableFrame);
+
+    m_button->Connect(wxEVT_COMMAND_BUTTON_CLICKED,
+                      wxEventHandler(wxTestableFrame::OnEvent),
+                      NULL,
+                      frame);
+
+    wxUIActionSimulator sim;
+
+    //In this test we disable the button and check events are not sent
+    m_button->Disable();
+
+    sim.MouseMove(m_button->GetScreenPosition() + wxPoint(10, 10));
+    sim.MouseClick();
+
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL( 0, frame->GetEventCount() );
+
+    //Always remember to return controls to their default state
+    //for following tests
+    m_button->Enable();
+}
+
+void ButtonTestCase::tearDown()
+{
+    delete m_button;
+}
