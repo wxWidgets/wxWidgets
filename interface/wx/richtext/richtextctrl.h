826c4761df510dc2dc5ff1af37096efe51b94b54
Julian Smart <julian@anthemion.co.uk>
2010-06-26 09:15:03 +0000

    Documentation change for new wxRTC function
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@64749 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/richtext/richtextctrl.h
+++ interface/wx/richtext/richtextctrl.h
@@ -1180,6 +1180,13 @@ public:
     virtual void PaintBackground(wxDC& dc);
 
     /**
+        Other user defined painting after everything else (i.e. all text) is painted.
+
+        @since 2.9.1
+    */
+    virtual void PaintAboveContent(wxDC& dc);
+
+    /**
         Pastes content from the clipboard to the buffer.
     */
     virtual void Paste();
