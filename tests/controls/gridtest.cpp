01a4861712673d90734d8e7413f41494e9b0d75d
Steve Lamerton <steve.lamerton@gmail.com>
2010-06-26 10:29:35 +0000

    Add tests for correct events when wxGird cells are clicked.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@64750 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/gridtest.cpp
+++ tests/controls/gridtest.cpp
@@ -31,10 +31,12 @@ public:
 
 private:
     CPPUNIT_TEST_SUITE( GridTestCase );
-        CPPUNIT_TEST( ItemEdit );
+        CPPUNIT_TEST( CellEdit );
+        CPPUNIT_TEST( CellClick );
     CPPUNIT_TEST_SUITE_END();
 
-    void ItemEdit();
+    void CellEdit();
+    void CellClick();
 
     wxGrid *m_grid;
 
@@ -50,8 +52,8 @@ CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( GridTestCase, "GridTestCase" );
 void GridTestCase::setUp()
 {
     m_grid = new wxGrid(wxTheApp->GetTopWindow(), wxID_ANY);
-    m_grid->CreateGrid(10, 10);
-    m_grid->SetSize(200, 200);
+    m_grid->CreateGrid(10, 2);
+    m_grid->SetSize(400, 200);
     m_grid->Refresh();
     m_grid->Update();
 }
@@ -61,7 +63,7 @@ void GridTestCase::tearDown()
     wxDELETE(m_grid);
 }
 
-void GridTestCase::ItemEdit()
+void GridTestCase::CellEdit()
 {
     wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
                                           wxTestableFrame);
@@ -82,4 +84,49 @@ void GridTestCase::ItemEdit()
 
     CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_CHANGING));
     CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_CHANGED));
+}
+
+void GridTestCase::CellClick()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                          wxTestableFrame);
+
+    frame->CountWindowEvents(m_grid, wxEVT_GRID_CELL_LEFT_CLICK);
+    frame->CountWindowEvents(m_grid, wxEVT_GRID_CELL_LEFT_DCLICK);
+    frame->CountWindowEvents(m_grid, wxEVT_GRID_CELL_RIGHT_CLICK);
+    frame->CountWindowEvents(m_grid, wxEVT_GRID_CELL_RIGHT_DCLICK);
+
+
+    wxUIActionSimulator sim;
+
+    wxRect rect = m_grid->CellToRect(0, 0);
+    wxPoint point = m_grid->CalcScrolledPosition(rect.GetPosition());
+    point = frame->ClientToScreen(point + wxPoint(m_grid->GetRowLabelSize(),
+                                                  m_grid->GetColLabelSize())
+                                        + wxPoint(2, 2));
+
+    sim.MouseMove(point);
+    sim.MouseClick();
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_LEFT_CLICK));
+
+    sim.MouseDblClick();
+    wxYield();
+
+    //A double click event sends a single click event first
+    //test to ensure this still happens in the future
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_LEFT_CLICK));
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_LEFT_DCLICK));
+
+    sim.MouseClick(wxMOUSE_BTN_RIGHT);
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_RIGHT_CLICK));
+
+    sim.MouseDblClick(wxMOUSE_BTN_RIGHT);
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_RIGHT_CLICK));
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_CELL_RIGHT_DCLICK)); 
 }
