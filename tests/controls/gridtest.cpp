69a9f57fce51894c7e3452d09203a9edcb49b050
Steve Lamerton <steve.lamerton@gmail.com>
2010-07-11 11:18:01 +0000

    Add more wxGrid tests for cursor movement, cell selection, column and row adding and inserting, reordering columns and line formatting.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@64889 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/gridtest.cpp
+++ tests/controls/gridtest.cpp
@@ -38,6 +38,11 @@ private:
         CPPUNIT_TEST( SortClick );
         CPPUNIT_TEST( Size );
         CPPUNIT_TEST( RangeSelect );
+        CPPUNIT_TEST( Cursor );
+        CPPUNIT_TEST( Selection );
+        CPPUNIT_TEST( AddRowCol );
+        CPPUNIT_TEST( ColumnOrder );
+        CPPUNIT_TEST( LineFormatting );
     CPPUNIT_TEST_SUITE_END();
 
     void CellEdit();
@@ -47,6 +52,11 @@ private:
     void SortClick();
     void Size();
     void RangeSelect();
+    void Cursor();
+    void Selection();
+    void AddRowCol();
+    void ColumnOrder();
+    void LineFormatting();
 
     wxGrid *m_grid;
 
@@ -283,4 +293,155 @@ void GridTestCase::RangeSelect()
     wxYield();
 
     CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_GRID_RANGE_SELECT));
+}
+
+void GridTestCase::Cursor()
+{
+    m_grid->SetGridCursor(1, 1);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetGridCursorRow());
+
+    m_grid->MoveCursorDown(false);
+    m_grid->MoveCursorLeft(false);
+    m_grid->MoveCursorUp(false);
+    m_grid->MoveCursorUp(false);
+    m_grid->MoveCursorRight(false);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetGridCursorRow());
+
+    m_grid->SetCellValue(0, 0, "some text");
+    m_grid->SetCellValue(3, 0, "other text");
+    m_grid->SetCellValue(0, 1, "more text");
+    m_grid->SetCellValue(3, 1, "extra text");
+
+    m_grid->Update();
+    m_grid->Refresh();
+
+    m_grid->MoveCursorLeftBlock(false);
+
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetGridCursorRow());
+
+    m_grid->MoveCursorDownBlock(false);
+
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetGridCursorRow());
+
+    m_grid->MoveCursorRightBlock(false);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetGridCursorRow());
+
+    m_grid->MoveCursorUpBlock(false);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetGridCursorCol());
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetGridCursorRow());
+}
+
+void GridTestCase::Selection()
+{
+    m_grid->SelectAll();
+
+    CPPUNIT_ASSERT(m_grid->IsSelection());
+    CPPUNIT_ASSERT(m_grid->IsInSelection(0, 0));
+    CPPUNIT_ASSERT(m_grid->IsInSelection(9, 1));
+
+    m_grid->SelectBlock(1, 0, 3, 1);
+
+    wxGridCellCoordsArray topleft = m_grid->GetSelectionBlockTopLeft();
+    wxGridCellCoordsArray bottomright = m_grid->GetSelectionBlockBottomRight();
+
+    CPPUNIT_ASSERT_EQUAL(1, topleft.Count());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Count());
+
+    CPPUNIT_ASSERT_EQUAL(0, topleft.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(1, topleft.Item(0).GetRow());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(3, bottomright.Item(0).GetRow());
+
+    m_grid->SelectCol(1);
+
+    CPPUNIT_ASSERT(m_grid->IsInSelection(0, 1));
+    CPPUNIT_ASSERT(m_grid->IsInSelection(9, 1));
+    CPPUNIT_ASSERT(!m_grid->IsInSelection(3, 0));
+
+    m_grid->SelectRow(4);
+
+    CPPUNIT_ASSERT(m_grid->IsInSelection(4, 0));
+    CPPUNIT_ASSERT(m_grid->IsInSelection(4, 1));
+    CPPUNIT_ASSERT(!m_grid->IsInSelection(3, 0));
+}
+
+void GridTestCase::AddRowCol()
+{
+    CPPUNIT_ASSERT_EQUAL(10, m_grid->GetNumberRows());
+    CPPUNIT_ASSERT_EQUAL(2, m_grid->GetNumberCols());
+
+    m_grid->AppendCols();
+    m_grid->AppendRows();
+
+    CPPUNIT_ASSERT_EQUAL(11, m_grid->GetNumberRows());
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetNumberCols());
+
+    m_grid->AppendCols(2);
+    m_grid->AppendRows(2);
+
+    CPPUNIT_ASSERT_EQUAL(13, m_grid->GetNumberRows());
+    CPPUNIT_ASSERT_EQUAL(5, m_grid->GetNumberCols());
+
+    m_grid->InsertCols(1, 2);
+    m_grid->InsertRows(2, 3);
+
+    CPPUNIT_ASSERT_EQUAL(16, m_grid->GetNumberRows());
+    CPPUNIT_ASSERT_EQUAL(7, m_grid->GetNumberCols());
+}
+
+void GridTestCase::ColumnOrder()
+{
+    m_grid->AppendCols(2);
+
+    CPPUNIT_ASSERT_EQUAL(4, m_grid->GetNumberCols());
+
+    wxArrayInt neworder;
+    neworder.push_back(1);
+    neworder.push_back(3);
+    neworder.push_back(2);
+    neworder.push_back(0);
+
+    m_grid->SetColumnsOrder(neworder);
+
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetColPos(1));
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetColPos(3));
+    CPPUNIT_ASSERT_EQUAL(2, m_grid->GetColPos(2));
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetColPos(0));
+
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetColAt(0));
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetColAt(1));
+    CPPUNIT_ASSERT_EQUAL(2, m_grid->GetColAt(2));
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetColAt(3));
+
+    m_grid->ResetColPos();
+
+    CPPUNIT_ASSERT_EQUAL(0, m_grid->GetColPos(0));
+    CPPUNIT_ASSERT_EQUAL(1, m_grid->GetColPos(1));
+    CPPUNIT_ASSERT_EQUAL(2, m_grid->GetColPos(2));
+    CPPUNIT_ASSERT_EQUAL(3, m_grid->GetColPos(3));
+}
+
+void GridTestCase::LineFormatting()
+{
+    CPPUNIT_ASSERT(m_grid->GridLinesEnabled());
+
+    m_grid->EnableGridLines(false);
+
+    CPPUNIT_ASSERT(!m_grid->GridLinesEnabled());
+
+    m_grid->EnableGridLines();
+
+    m_grid->SetGridLineColour(wxColour(*wxRED));
+
+    CPPUNIT_ASSERT_EQUAL(m_grid->GetGridLineColour().GetRGB(), 
+                         wxColour(*wxRED).GetRGB());
 }
