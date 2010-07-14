bdf6a1cede392ab1e101e6766e8836bca3f96865
Steve Lamerton <steve.lamerton@gmail.com>
2010-07-14 14:38:44 +0000

    Add extra tests to wxTreebook for adding and inserting pages, expanding them and deleting them.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@64963 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/treebooktest.cpp
+++ tests/controls/treebooktest.cpp
@@ -41,7 +41,14 @@ private:
     CPPUNIT_TEST_SUITE( TreebookTestCase );
         wxBOOK_CTRL_BASE_TESTS();
         CPPUNIT_TEST( Image );
+        CPPUNIT_TEST( SubPages );
+        CPPUNIT_TEST( Expand );
+        CPPUNIT_TEST( Delete );
     CPPUNIT_TEST_SUITE_END();
+
+    void SubPages();
+    void Expand();
+    void Delete();
 
     wxTreebook *m_treebook;
 
@@ -64,3 +71,74 @@ void TreebookTestCase::tearDown()
 {
     wxDELETE(m_treebook);
 }
+
+void TreebookTestCase::SubPages()
+{
+    wxPanel* subpanel1 = new wxPanel(m_treebook);
+    wxPanel* subpanel2 = new wxPanel(m_treebook);
+    wxPanel* subpanel3 = new wxPanel(m_treebook);
+
+    m_treebook->AddSubPage(subpanel1, "Subpanel 1", false, 0);
+
+    CPPUNIT_ASSERT_EQUAL(2, m_treebook->GetPageParent(3));
+
+    m_treebook->InsertSubPage(1, subpanel2, "Subpanel 2", false, 1);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_treebook->GetPageParent(2));
+
+    m_treebook->AddSubPage(subpanel3, "Subpanel 3", false, 2);
+
+    CPPUNIT_ASSERT_EQUAL(3, m_treebook->GetPageParent(5));
+}
+
+void TreebookTestCase::Expand()
+{
+    wxPanel* subpanel1 = new wxPanel(m_treebook);
+    wxPanel* subpanel2 = new wxPanel(m_treebook);
+    wxPanel* subpanel3 = new wxPanel(m_treebook);
+
+    m_treebook->AddSubPage(subpanel1, "Subpanel 1", false, 0);
+    m_treebook->InsertSubPage(1, subpanel2, "Subpanel 2", false, 1);
+    m_treebook->AddSubPage(subpanel3, "Subpanel 3", false, 2);
+
+    CPPUNIT_ASSERT(!m_treebook->IsNodeExpanded(1));
+    CPPUNIT_ASSERT(!m_treebook->IsNodeExpanded(3));
+
+    m_treebook->CollapseNode(1);
+
+    CPPUNIT_ASSERT(!m_treebook->IsNodeExpanded(1));
+
+    m_treebook->ExpandNode(3, false);
+
+    CPPUNIT_ASSERT(!m_treebook->IsNodeExpanded(3));
+
+    m_treebook->ExpandNode(1);
+
+    CPPUNIT_ASSERT(m_treebook->IsNodeExpanded(1));
+}
+
+void TreebookTestCase::Delete()
+{
+    wxPanel* subpanel1 = new wxPanel(m_treebook);
+    wxPanel* subpanel2 = new wxPanel(m_treebook);
+    wxPanel* subpanel3 = new wxPanel(m_treebook);
+
+    m_treebook->AddSubPage(subpanel1, "Subpanel 1", false, 0);
+    m_treebook->InsertSubPage(1, subpanel2, "Subpanel 2", false, 1);
+    m_treebook->AddSubPage(subpanel3, "Subpanel 3", false, 2);
+
+    CPPUNIT_ASSERT_EQUAL(6, m_treebook->GetPageCount());
+
+    m_treebook->DeletePage(3);
+
+    CPPUNIT_ASSERT_EQUAL(3, m_treebook->GetPageCount());
+
+    m_treebook->DeletePage(1);
+
+    CPPUNIT_ASSERT_EQUAL(1, m_treebook->GetPageCount());
+
+    m_treebook->DeletePage(0);
+
+    CPPUNIT_ASSERT_EQUAL(0, m_treebook->GetPageCount());
+}
+
