f203de0cc435ab836505ad32b88cda5dc318db68
Vadim Zeitlin <vadim@wxwidgets.org>
2010-07-22 12:09:04 +0000

    Replace 2.9.1 version with 2.9.2.
    Also update the inc_release script to take src/wxWindows.xcodeproj into
    account.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65038 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/version.h
+++ include/wx/version.h
@@ -28,9 +28,9 @@
 /*  NB: this file is parsed by automatic tools so don't change its format! */
 #define wxMAJOR_VERSION      2
 #define wxMINOR_VERSION      9
-#define wxRELEASE_NUMBER     1
+#define wxRELEASE_NUMBER     2
 #define wxSUBRELEASE_NUMBER  0
-#define wxVERSION_STRING   wxT("wxWidgets 2.9.1")
+#define wxVERSION_STRING   wxT("wxWidgets 2.9.2")
 
 /*  nothing to update below this line when updating the version */
 /*  ---------------------------------------------------------------------------- */
