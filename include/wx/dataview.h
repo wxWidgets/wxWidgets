780e4fd0031efe694241303d8eb161dae204dfc3
Václav Slavík <vslavik@fastmail.fm>
2010-07-29 15:30:11 +0000

    Fix gcc warning in wxDataViewListCtrl::ItemToRow().
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65138 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/dataview.h
+++ include/wx/dataview.h
@@ -951,7 +951,7 @@ public:
         { return (const wxDataViewListStore*) GetModel(); }
 
     int ItemToRow(const wxDataViewItem &item) const
-        { return item.IsOk() ? GetStore()->GetRow(item) : wxNOT_FOUND; }
+        { return item.IsOk() ? (int)GetStore()->GetRow(item) : wxNOT_FOUND; }
     wxDataViewItem RowToItem(int row) const
         { return row == wxNOT_FOUND ? wxDataViewItem() : GetStore()->GetItem(row); }
 
