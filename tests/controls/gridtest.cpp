a373c1a0cfb9afebd24d3a94659f7a8b42f6aad3
Steve Lamerton <steve.lamerton@gmail.com>
2010-08-04 14:33:58 +0000

    Add new tests to GridTestCase for various selection modes, cell formatting options, editability and read only status. Also use the various existing column tests to check the correct functionality under native column labels as well as native column headers.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@65180 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/gridtest.cpp
+++ tests/controls/gridtest.cpp
@@ -46,7 +46,15 @@ private:
         CPPUNIT_TEST( LineFormatting );
         CPPUNIT_TEST( SortSupport );
         CPPUNIT_TEST( Labels );
-        CPPUNIT_TEST( PseudoTest_Native );
+        CPPUNIT_TEST( SelectionMode );
+        CPPUNIT_TEST( CellFormatting );
+        CPPUNIT_TEST( Editable );
+        CPPUNIT_TEST( ReadOnly );
+        CPPUNIT_TEST( PseudoTest_NativeHeader );
+        CPPUNIT_TEST( LabelClick );
+        CPPUNIT_TEST( SortClick );
+        CPPUNIT_TEST( ColumnOrder );
+        CPPUNIT_TEST( PseudoTest_NativeLabels );
         CPPUNIT_TEST( LabelClick );
         CPPUNIT_TEST( SortClick );
         CPPUNIT_TEST( ColumnOrder );
@@ -66,9 +74,16 @@ private:
     void LineFormatting();
     void SortSupport();
     void Labels();
-    void PseudoTest_Native() { ms_native = true; }
+    void SelectionMode();
+    void CellFormatting();
+    void Editable();
+    void ReadOnly();
+    void PseudoTest_NativeHeader() { ms_nativeheader = true; }
+    void PseudoTest_NativeLabels() { ms_nativeheader = false;
+                                     ms_nativelabels = true; }
 
-    static bool ms_native;
+    static bool ms_nativeheader;
+    static bool ms_nativelabels;
 
     wxGrid *m_grid;
 
@@ -82,13 +97,21 @@ CPPUNIT_TEST_SUITE_REGISTRATION( GridTestCase );
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( GridTestCase, "GridTestCase" );
 
 //initialise the static variable
-bool GridTestCase::ms_native = false;
+bool GridTestCase::ms_nativeheader = false;
+bool GridTestCase::ms_nativelabels = false;
 
 void GridTestCase::setUp()
 {
     m_grid = new wxGrid(wxTheApp->GetTopWindow(), wxID_ANY);
     m_grid->CreateGrid(10, 2);
     m_grid->SetSize(400, 200);
+
+    if( ms_nativeheader )
+        m_grid->UseNativeColHeader();
+
+    if( ms_nativelabels )
+        m_grid->SetUseNativeColLabels();
+
     m_grid->Refresh();
     m_grid->Update();
 }
@@ -201,9 +224,6 @@ void GridTestCase::CellSelect()
 
 void GridTestCase::LabelClick()
 {
-    if( ms_native )
-        m_grid->UseNativeColHeader();
-
     wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
                                           wxTestableFrame);
 
@@ -239,7 +259,7 @@ void GridTestCase::LabelClick()
     sim.MouseDblClick(wxMOUSE_BTN_RIGHT);
     wxYield();
 
-    if( ms_native )
+    if( ms_nativeheader )
     {
         //Right double click not supported with native headers so we get two 
         //right click events
@@ -254,9 +274,6 @@ void GridTestCase::LabelClick()
 
 void GridTestCase::SortClick()
 {
-    if( ms_native )
-        m_grid->UseNativeColHeader();
-
     m_grid->SetSortingColumn(0);
 
     wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
@@ -430,9 +447,6 @@ void GridTestCase::AddRowCol()
 
 void GridTestCase::ColumnOrder()
 {
-    if( ms_native )
-        m_grid->UseNativeColHeader();
-
     m_grid->AppendCols(2);
 
     CPPUNIT_ASSERT_EQUAL(4, m_grid->GetNumberCols());
@@ -521,4 +535,152 @@ void GridTestCase::Labels()
 
     CPPUNIT_ASSERT_EQUAL(static_cast<int>(wxVERTICAL), 
                          static_cast<int>(m_grid->GetColLabelTextOrientation()));
+}
+
+void GridTestCase::SelectionMode()
+{
+    //We already test this mode in Select
+    CPPUNIT_ASSERT_EQUAL(wxGrid::wxGridSelectCells, 
+                         m_grid->GetSelectionMode());
+
+    //Test row selection be selecting a single cell and checking the whole
+    //row is selected
+    m_grid->SetSelectionMode(wxGrid::wxGridSelectRows);
+    m_grid->SelectBlock(3, 1, 3, 1);
+
+    wxGridCellCoordsArray topleft = m_grid->GetSelectionBlockTopLeft();
+    wxGridCellCoordsArray bottomright = m_grid->GetSelectionBlockBottomRight();
+
+    CPPUNIT_ASSERT_EQUAL(1, topleft.Count());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Count());
+
+    CPPUNIT_ASSERT_EQUAL(0, topleft.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(3, topleft.Item(0).GetRow());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(3, bottomright.Item(0).GetRow());
+
+    CPPUNIT_ASSERT_EQUAL(wxGrid::wxGridSelectRows, 
+                         m_grid->GetSelectionMode());
+
+
+    //Test column selection be selecting a single cell and checking the whole
+    //column is selected
+    m_grid->SetSelectionMode(wxGrid::wxGridSelectColumns);
+    m_grid->SelectBlock(3, 1, 3, 1);
+
+    topleft = m_grid->GetSelectionBlockTopLeft();
+    bottomright = m_grid->GetSelectionBlockBottomRight();
+
+    CPPUNIT_ASSERT_EQUAL(1, topleft.Count());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Count());
+
+    CPPUNIT_ASSERT_EQUAL(1, topleft.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(0, topleft.Item(0).GetRow());
+    CPPUNIT_ASSERT_EQUAL(1, bottomright.Item(0).GetCol());
+    CPPUNIT_ASSERT_EQUAL(9, bottomright.Item(0).GetRow());
+
+    CPPUNIT_ASSERT_EQUAL(wxGrid::wxGridSelectColumns, 
+                         m_grid->GetSelectionMode());
+}
+
+void GridTestCase::CellFormatting()
+{
+    //Check that initial alignment is default
+    int horiz, cellhoriz, vert, cellvert;
+
+    m_grid->GetDefaultCellAlignment(&horiz, &vert);
+    m_grid->GetCellAlignment(0, 0, &cellhoriz, &cellvert);
+
+    CPPUNIT_ASSERT_EQUAL(cellhoriz, horiz);
+    CPPUNIT_ASSERT_EQUAL(cellvert, vert);
+
+    //Check initial text colour and background colour are default
+    wxColour text, back;
+
+    back = m_grid->GetDefaultCellBackgroundColour();
+    
+    CPPUNIT_ASSERT_EQUAL(back, m_grid->GetCellBackgroundColour(0, 0));
+
+    back = m_grid->GetDefaultCellTextColour();
+    
+    CPPUNIT_ASSERT_EQUAL(back, m_grid->GetCellTextColour(0, 0));
+
+    m_grid->SetCellAlignment(wxALIGN_CENTRE, 0, 0);
+    m_grid->GetCellAlignment(0, 0, &cellhoriz, &cellvert);
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<int>(wxALIGN_CENTRE), cellhoriz);
+    CPPUNIT_ASSERT_EQUAL(static_cast<int>(wxALIGN_CENTRE), cellvert);
+
+    m_grid->SetCellAlignment(0, 0, wxALIGN_LEFT, wxALIGN_BOTTOM);
+    m_grid->GetCellAlignment(0, 0, &cellhoriz, &cellvert);
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<int>(wxALIGN_LEFT), cellhoriz);
+    CPPUNIT_ASSERT_EQUAL(static_cast<int>(wxALIGN_BOTTOM), cellvert);
+
+    m_grid->SetCellTextColour(*wxRED, 0, 0);
+
+    CPPUNIT_ASSERT_EQUAL(*wxRED, m_grid->GetCellTextColour(0, 0));
+
+    m_grid->SetCellTextColour(0, 0, *wxGREEN);
+
+    CPPUNIT_ASSERT_EQUAL(*wxGREEN, m_grid->GetCellTextColour(0, 0));
+}
+
+void GridTestCase::Editable()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                          wxTestableFrame);
+
+    //As the grid is not editable we shouldn't create an editor
+    EventCounter count(m_grid, wxEVT_GRID_EDITOR_CREATED);
+
+    wxUIActionSimulator sim;
+
+    CPPUNIT_ASSERT(m_grid->IsEditable());
+
+    m_grid->EnableEditing(false);
+
+    CPPUNIT_ASSERT(!m_grid->IsEditable());
+
+    m_grid->SetFocus();
+    m_grid->SetGridCursor(1, 1);
+    m_grid->ShowCellEditControl();
+
+    sim.Text("abab");
+    sim.Char(WXK_RETURN);
+
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(0, frame->GetEventCount());
+}
+
+void GridTestCase::ReadOnly()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                          wxTestableFrame);
+
+    //As the cell is readonly we shouldn't create an editor
+    EventCounter count(m_grid, wxEVT_GRID_EDITOR_CREATED);
+
+    wxUIActionSimulator sim;
+
+    CPPUNIT_ASSERT(!m_grid->IsReadOnly(1, 1));
+
+    m_grid->SetReadOnly(1, 1);
+
+    CPPUNIT_ASSERT(m_grid->IsReadOnly(1, 1));
+
+    m_grid->SetFocus();
+    m_grid->SetGridCursor(1, 1);
+
+    CPPUNIT_ASSERT(m_grid->IsCurrentCellReadOnly());
+
+    m_grid->ShowCellEditControl();
+
+    sim.Text("abab");
+    sim.Char(WXK_RETURN);
+
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(0, frame->GetEventCount());
 }
