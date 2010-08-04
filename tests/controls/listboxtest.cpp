fc4642d0841a38afa040dfe9b6d67cab2e6ed6d3
Steve Lamerton <steve.lamerton@gmail.com>
2010-08-04 14:46:38 +0000

    Add a set of new wxListBox tests for item click events, hit testing and a specific test for bug #12143.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@65189 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/listboxtest.cpp
+++ tests/controls/listboxtest.cpp
@@ -19,6 +19,8 @@
 #endif // WX_PRECOMP
 
 #include "itemcontainertest.h"
+#include "testableframe.h"
+#include "wx/uiaction.h"
 
 class ListBoxTestCase : public ItemContainerTestCase, public CppUnit::TestCase
 {
@@ -36,10 +38,29 @@ private:
         wxITEM_CONTAINER_TESTS();
         CPPUNIT_TEST( Sort );
         CPPUNIT_TEST( MultipleSelect );
+        CPPUNIT_TEST( ClickEvents );
+        CPPUNIT_TEST( ClickNotOnItem );
+        CPPUNIT_TEST( HitTest );
+        //We also run all tests as an ownerdrawn list box we do not need to 
+        //run the wxITEM_CONTAINER_TESTS as they are tested with wxCheckListBox
+#ifdef __WXMSW__
+        CPPUNIT_TEST( PseudoTest_OwnerDrawn );
+        CPPUNIT_TEST( Sort );
+        CPPUNIT_TEST( MultipleSelect );
+        CPPUNIT_TEST( ClickEvents );
+        CPPUNIT_TEST( ClickNotOnItem );
+        CPPUNIT_TEST( HitTest );
+#endif
     CPPUNIT_TEST_SUITE_END();
 
     void Sort();
     void MultipleSelect();
+    void ClickEvents();
+    void ClickNotOnItem();
+    void HitTest();
+    void PseudoTest_OwnerDrawn() { ms_ownerdrawn = true; }
+
+    static bool ms_ownerdrawn;
 
     wxListBox* m_list;
 
@@ -52,9 +73,22 @@ CPPUNIT_TEST_SUITE_REGISTRATION( ListBoxTestCase );
 // also include in it's own registry so that these tests can be run alone
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( ListBoxTestCase, "ListBoxTestCase" );
 
+//initialise the static variable
+bool ListBoxTestCase::ms_ownerdrawn = false;
+
 void ListBoxTestCase::setUp()
 {
-    m_list = new wxListBox(wxTheApp->GetTopWindow(), wxID_ANY);
+    if( ms_ownerdrawn )
+    {
+        m_list = new wxListBox(wxTheApp->GetTopWindow(), wxID_ANY, 
+                               wxDefaultPosition, wxSize(300, 200), 0, NULL,
+                               wxLB_OWNERDRAW);
+    }
+    else
+    {
+        m_list = new wxListBox(wxTheApp->GetTopWindow(), wxID_ANY, 
+                               wxDefaultPosition, wxSize(300, 200));
+    }
 }
 
 void ListBoxTestCase::tearDown()
@@ -132,4 +166,86 @@ void ListBoxTestCase::MultipleSelect()
     CPPUNIT_ASSERT(!m_list->IsSelected(1));
     CPPUNIT_ASSERT(m_list->IsSelected(2));
     CPPUNIT_ASSERT(!m_list->IsSelected(3));
+}
+
+void ListBoxTestCase::ClickEvents()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                              wxTestableFrame);
+
+    EventCounter count(frame, wxEVT_COMMAND_LISTBOX_SELECTED);
+    EventCounter count1(frame, wxEVT_COMMAND_LISTBOX_DOUBLECLICKED);
+
+    wxUIActionSimulator sim;
+
+    wxArrayString testitems;
+    testitems.Add("item 0");
+    testitems.Add("item 1");
+    testitems.Add("item 2");
+
+    m_list->Append(testitems);
+
+    m_list->Update();
+    m_list->Refresh();
+
+    sim.MouseMove(m_list->ClientToScreen(wxPoint(10, 10)));
+    sim.MouseClick();
+
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount());
+
+    sim.MouseDblClick();
+
+    wxYield();
+
+    CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount());
+}
+
+void ListBoxTestCase::ClickNotOnItem()
+{
+    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
+                                              wxTestableFrame);
+
+    EventCounter count(frame, wxEVT_COMMAND_LISTBOX_SELECTED);
+    EventCounter count1(frame, wxEVT_COMMAND_LISTBOX_DOUBLECLICKED);
+
+    wxUIActionSimulator sim;
+
+    wxArrayString testitems;
+    testitems.Add("item 0");
+    testitems.Add("item 1");
+    testitems.Add("item 2");
+
+    m_list->Append(testitems);
+
+    m_list->Update();
+    m_list->Refresh();
+
+    sim.MouseMove(m_list->ClientToScreen(wxPoint(m_list->GetSize().x - 10, m_list->GetSize().y - 10)));
+    sim.MouseClick();
+    sim.MouseDblClick();
+
+    wxYield();
+
+    //If we are not clicking on an item we shouldn't have any events
+    CPPUNIT_ASSERT_EQUAL(0, frame->GetEventCount());
+}
+
+void ListBoxTestCase::HitTest()
+{
+#if defined(__WXMSW__) || defined(__WXOSX__) || defined(__WXGTK20__)
+    wxArrayString testitems;
+    testitems.Add("item 0");
+    testitems.Add("item 1");
+    testitems.Add("item 2");
+
+    m_list->Append(testitems);
+
+    CPPUNIT_ASSERT(m_list->HitTest(wxPoint(10, 10)) != wxNOT_FOUND);
+    CPPUNIT_ASSERT(m_list->HitTest(10, 10) != wxNOT_FOUND);
+
+    CPPUNIT_ASSERT(m_list->HitTest(wxPoint(290, 190)) == wxNOT_FOUND);
+    CPPUNIT_ASSERT(m_list->HitTest(290, 190) == wxNOT_FOUND);
+#endif
 }
