12920097dc47cfca386efb109e7a7edccf792dc9
Steve Lamerton <steve.lamerton@gmail.com>
2010-08-04 16:34:18 +0000

    A series of new wxWindow tests for cursor and caret, property, tooltip, help, parent and sibling functions.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@65194 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/windowtest.cpp
+++ tests/controls/windowtest.cpp
@@ -21,6 +21,8 @@
 
 #include "testableframe.h"
 #include "wx/uiaction.h"
+#include "wx/caret.h"
+#include "wx/cshelp.h"
 
 class WindowTestCase : public CppUnit::TestCase
 {
@@ -32,14 +34,26 @@ public:
 
 private:
     CPPUNIT_TEST_SUITE( WindowTestCase );
-        CPPUNIT_TEST( ShowHide );
-        CPPUNIT_TEST( Key );
-        CPPUNIT_TEST( Focus );
+        CPPUNIT_TEST( ShowHideEvent );
+        CPPUNIT_TEST( KeyEvent );
+        CPPUNIT_TEST( FocusEvent );
+        CPPUNIT_TEST( Mouse );
+        CPPUNIT_TEST( Properties );
+        CPPUNIT_TEST( ToolTip );
+        CPPUNIT_TEST( Help );
+        CPPUNIT_TEST( Parent );
+        CPPUNIT_TEST( Siblings );
     CPPUNIT_TEST_SUITE_END();
 
-    void ShowHide();
-    void Key();
-    void Focus();
+    void ShowHideEvent();
+    void KeyEvent();
+    void FocusEvent();
+    void Mouse();
+    void Properties();
+    void ToolTip();
+    void Help();
+    void Parent();
+    void Siblings();
 
     wxWindow *m_window;
 
@@ -59,10 +73,10 @@ void WindowTestCase::setUp()
 
 void WindowTestCase::tearDown()
 {
-    wxDELETE(m_window);
+    wxTheApp->GetTopWindow()->DestroyChildren();
 }
 
-void WindowTestCase::ShowHide()
+void WindowTestCase::ShowHideEvent()
 {
 #if defined(__WXMSW__) || defined(__WXGTK__) || defined (__WXPM__)
    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
@@ -82,7 +96,7 @@ void WindowTestCase::ShowHide()
 #endif
 }
 
-void WindowTestCase::Key()
+void WindowTestCase::KeyEvent()
 {
     wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
                                           wxTestableFrame);
@@ -104,7 +118,7 @@ void WindowTestCase::Key()
     CPPUNIT_ASSERT_EQUAL(4, frame->GetEventCount(wxEVT_CHAR));
 }
 
-void WindowTestCase::Focus()
+void WindowTestCase::FocusEvent()
 {
    wxTestableFrame* frame = wxStaticCast(wxTheApp->GetTopWindow(),
                                           wxTestableFrame);
@@ -123,4 +137,104 @@ void WindowTestCase::Focus()
 
     CPPUNIT_ASSERT_EQUAL(1, frame->GetEventCount(wxEVT_KILL_FOCUS));
     CPPUNIT_ASSERT(!m_window->HasFocus());
+}
+
+void WindowTestCase::Mouse()
+{
+    //No cursor by default
+    CPPUNIT_ASSERT(!m_window->GetCursor().IsOk());
+    
+    wxCursor cursor(wxCURSOR_CHAR);
+    m_window->SetCursor(cursor);
+
+    CPPUNIT_ASSERT(m_window->GetCursor().IsOk());
+
+    //A plain window doesn't have a caret
+    CPPUNIT_ASSERT(!m_window->GetCaret());
+
+    wxCaret* caret = new wxCaret(m_window, 16, 16);
+    m_window->SetCaret(caret);
+
+    CPPUNIT_ASSERT(m_window->GetCaret()->IsOk());
+
+    m_window->CaptureMouse();
+
+    CPPUNIT_ASSERT(m_window->HasCapture());
+
+    m_window->ReleaseMouse();
+
+    CPPUNIT_ASSERT(!m_window->HasCapture());
+}
+
+void WindowTestCase::Properties()
+{
+    m_window->SetLabel("label");
+
+    CPPUNIT_ASSERT_EQUAL("label", m_window->GetLabel());
+
+    m_window->SetName("name");
+
+    CPPUNIT_ASSERT_EQUAL("name", m_window->GetName());
+
+    //As we used wxID_ANY we should have a negative id
+    CPPUNIT_ASSERT(m_window->GetId() < 0);
+
+    m_window->SetId(wxID_HIGHEST + 10);
+
+    CPPUNIT_ASSERT_EQUAL(wxID_HIGHEST + 10, m_window->GetId());
+}
+
+void WindowTestCase::ToolTip()
+{
+    CPPUNIT_ASSERT(!m_window->GetToolTip());
+    CPPUNIT_ASSERT_EQUAL("", m_window->GetToolTipText());
+
+    m_window->SetToolTip("text tip");
+
+    CPPUNIT_ASSERT_EQUAL("text tip", m_window->GetToolTipText());
+
+    m_window->UnsetToolTip();
+
+    CPPUNIT_ASSERT(!m_window->GetToolTip());
+    CPPUNIT_ASSERT_EQUAL("", m_window->GetToolTipText());
+
+    wxToolTip* tip = new wxToolTip("other tip");
+
+    m_window->SetToolTip(tip);
+
+    CPPUNIT_ASSERT_EQUAL(tip, m_window->GetToolTip());
+    CPPUNIT_ASSERT_EQUAL("other tip", m_window->GetToolTipText());
+}
+
+void WindowTestCase::Help()
+{
+    wxHelpProvider::Set(new wxSimpleHelpProvider());
+
+    CPPUNIT_ASSERT_EQUAL("", m_window->GetHelpText());
+
+    m_window->SetHelpText("helptext");
+
+    CPPUNIT_ASSERT_EQUAL("helptext", m_window->GetHelpText());
+}
+
+void WindowTestCase::Parent()
+{
+    CPPUNIT_ASSERT_EQUAL(static_cast<wxWindow*>(NULL), m_window->GetGrandParent());
+    CPPUNIT_ASSERT_EQUAL(wxTheApp->GetTopWindow(), m_window->GetParent());
+}
+
+void WindowTestCase::Siblings()
+{
+    CPPUNIT_ASSERT_EQUAL(static_cast<wxWindow*>(NULL), m_window->GetNextSibling());
+    CPPUNIT_ASSERT_EQUAL(static_cast<wxWindow*>(NULL), m_window->GetPrevSibling());
+
+    wxWindow* newwin = new wxWindow(wxTheApp->GetTopWindow(), wxID_ANY);
+
+    CPPUNIT_ASSERT_EQUAL(newwin, m_window->GetNextSibling());
+    CPPUNIT_ASSERT_EQUAL(static_cast<wxWindow*>(NULL), m_window->GetPrevSibling());
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<wxWindow*>(NULL), newwin->GetNextSibling());
+    CPPUNIT_ASSERT_EQUAL(m_window, newwin->GetPrevSibling());
+
+    wxDELETE(newwin);
 }
