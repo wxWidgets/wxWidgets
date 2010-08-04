da729346cab24204476371a61358ab3fdd987088
Steve Lamerton <steve.lamerton@gmail.com>
2010-08-04 14:26:10 +0000

    Expand the ButtonTestCase example to include tests for Set/GetAuthNeeded, bitmap margins and bitmap setting.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2010_GUI_TEST@65177 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/buttontest.cpp
+++ tests/controls/buttontest.cpp
@@ -20,6 +20,11 @@
 
 #include "testableframe.h"
 #include "wx/uiaction.h"
+#include "wx/artprov.h"
+//For CPPUNIT_ASSERT_EQUAL to work a class must have a stream output function
+//for those classes which do not have them by default we define them in 
+//asserthelper.h so they can be reused
+#include "asserthelper.h"
 
 class ButtonTestCase : public CppUnit::TestCase
 {
@@ -33,10 +38,16 @@ private:
     CPPUNIT_TEST_SUITE( ButtonTestCase );
         CPPUNIT_TEST( Click );
         CPPUNIT_TEST( Disabled );
+        CPPUNIT_TEST( Auth );
+        CPPUNIT_TEST( BitmapMargins );
+        CPPUNIT_TEST( Bitmap );
     CPPUNIT_TEST_SUITE_END();
 
     void Click();
     void Disabled();
+    void Auth();
+    void BitmapMargins();
+    void Bitmap();
 
     wxButton* m_button;
 
@@ -100,4 +111,61 @@ void ButtonTestCase::Disabled()
     wxYield();
 
     CPPUNIT_ASSERT_EQUAL( 0, frame->GetEventCount() );
+}
+
+void ButtonTestCase::Auth()
+{
+    //Some functions only work on specific operating system versions, for
+    //this we need a runtime check
+    int major = 0;
+
+    if(wxGetOsVersion(&major) != wxOS_WINDOWS_NT || major < 6)
+        return;
+
+    //We are running Windows Vista or newer
+    CPPUNIT_ASSERT(!m_button->GetAuthNeeded());
+
+    m_button->SetAuthNeeded();
+
+    CPPUNIT_ASSERT(m_button->GetAuthNeeded());
+
+    //We test both states
+    m_button->SetAuthNeeded(false);
+
+    CPPUNIT_ASSERT(!m_button->GetAuthNeeded());
+}
+
+void ButtonTestCase::BitmapMargins()
+{
+    //Some functions only work on specific platfroms in which case we can use
+    //a preprocessor check
+#ifdef __WXMSW__
+    //We must set a bitmap before we can set its margins, when writing unit
+    //tests it is easiest to use an image from wxArtProvider
+    m_button->SetBitmap(wxArtProvider::GetIcon(wxART_INFORMATION, wxART_OTHER, 
+                                               wxSize(32, 32)));
+
+    m_button->SetBitmapMargins(15, 15);
+
+    CPPUNIT_ASSERT_EQUAL(wxSize(15, 15), m_button->GetBitmapMargins());
+
+    m_button->SetBitmapMargins(wxSize(20, 20));
+
+    CPPUNIT_ASSERT_EQUAL(wxSize(20, 20), m_button->GetBitmapMargins());
+#endif
+}
+
+void ButtonTestCase::Bitmap()
+{
+    //We start with no bitmaps
+    CPPUNIT_ASSERT(!m_button->GetBitmap().IsOk());
+
+    //Set bitmap should set a bitmap for all states
+    m_button->SetBitmap(wxArtProvider::GetIcon(wxART_INFORMATION, wxART_OTHER, 
+                                               wxSize(32, 32)));
+
+    CPPUNIT_ASSERT(m_button->GetBitmapDisabled().IsOk());
+    CPPUNIT_ASSERT(m_button->GetBitmapPressed().IsOk());
+    CPPUNIT_ASSERT(m_button->GetBitmapCurrent().IsOk());
+    CPPUNIT_ASSERT(m_button->GetBitmapFocus().IsOk());
 }
