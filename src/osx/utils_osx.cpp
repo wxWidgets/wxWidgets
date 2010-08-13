128ad183497f48ce80eb077b168fe2423bdb2b23
Stefan Csomor <csomor@advancedconcepts.ch>
2010-08-13 04:23:37 +0000

    no-op change to get build bot running
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65283 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/osx/utils_osx.cpp
+++ src/osx/utils_osx.cpp
@@ -61,7 +61,7 @@ bool wxCheckForInterrupt(wxWindow *WXUNUSED(wnd))
 // Return true if we have a colour display
 bool wxColourDisplay()
 {
-    // always the case on OSX
+    // always the case on OS X
     return true;
 }
 
