343e567e3e4581cbe21e9801523fdf3cf3d728b0
Robert Roebling <robert@roebling.de>
2010-08-18 21:36:40 +0000

    Mention that you cannot call wxRadioButton::SetValue(false) if that radiobutton belongs to a group
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65344 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/radiobut.h
+++ interface/wx/radiobut.h
@@ -107,6 +107,10 @@ public:
     /**
         Sets the radio button to selected or deselected status. This does not cause a
         @c wxEVT_COMMAND_RADIOBUTTON_SELECTED event to get emitted.
+        
+        If the radio button belongs to a group you can only select a radio button
+        (which will deselect the previously selected button) and you therefore
+        must not call this method with value set to @false in that case.
 
         @param value
             @true to select, @false to deselect.
