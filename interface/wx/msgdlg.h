b8737371e220ef1920e6a3d2aed88c4bc2b04daf
Vadim Zeitlin <vadim@wxwidgets.org>
2010-09-04 09:36:25 +0000

    Add a link to Microsoft guidelines from wxICON_QUESTION documentation.
    Microsoft documentation provides explanations as to how the icons should be
    used in the message dialogs which can be useful for people wondering which
    icon style to use an when.
    
    See #12417.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65465 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/msgdlg.h
+++ interface/wx/msgdlg.h
@@ -47,8 +47,11 @@
     @style{wxICON_QUESTION}
         Displays a question mark symbol. This icon is automatically used
         with @c wxYES_NO so it's usually unnecessary to specify it explicitly.
-        This style is not supported for MSW task dialogs, as question icons do
-        not follow the guidelines. No icon will be displayed in this case.
+        This style is not supported for message dialogs under wxMSW when a task
+        dialog is used to implement them (i.e. when running under Windows Vista
+        or later) because <a href="http://msdn.microsoft.com/en-us/library/aa511273.aspx">Microsoft
+        guidelines</a> indicate that no icon should be used for routine
+        confirmations. If it is specified, no icon will be displayed.
     @style{wxICON_INFORMATION}
         Displays an information symbol. This icon is used by default if
         @c wxYES_NO is not given so it is usually unnecessary to specify it
