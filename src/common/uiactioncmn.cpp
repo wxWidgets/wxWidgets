c3f62b348fe082aa471f9c56377f018ebec77dce
Vadim Zeitlin <vadim@wxwidgets.org>
2010-09-11 10:18:10 +0000

    Remove assert checking for wxMOD_CONTROL from wxUIActionSimulator.
    It doesn't make sense to fail if modifiers parameter includes wxMOD_CONTROL
    telling the caller to use wxMOD_CMD instead as the latter is exactly the same
    as the former under non-Mac platforms.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65515 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/uiactioncmn.cpp
+++ src/common/uiactioncmn.cpp
@@ -50,8 +50,6 @@ wxUIActionSimulator::MouseDragDrop(long x1, long y1, long x2, long y2,
 bool
 wxUIActionSimulator::Key(int keycode, int modifiers, bool isDown)
 {
-    wxASSERT_MSG( !(modifiers & wxMOD_CONTROL),
-        "wxMOD_CONTROL is not implemented, use wxMOD_CMD instead" );
     wxASSERT_MSG( (modifiers & wxMOD_ALTGR) != wxMOD_ALTGR,
         "wxMOD_ALTGR is not implemented" );
     wxASSERT_MSG( !(modifiers & wxMOD_META ),
