fd7c5da65e46b33061e551faa133b21548e68bb9
Václav Slavík <vslavik@fastmail.fm>
2010-10-25 09:43:04 +0000

    Add wxImplicitConversionType tests.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65921 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/misc/metatest.cpp
+++ tests/misc/metatest.cpp
@@ -14,8 +14,13 @@
 #endif
 
 #include "wx/object.h"
+#include "wx/utils.h"
 #include "wx/meta/pod.h"
 #include "wx/meta/movable.h"
+
+#ifndef wxNO_RTTI
+#include <typeinfo>
+#endif
 
 // ----------------------------------------------------------------------------
 // test class
@@ -30,10 +35,12 @@ private:
     CPPUNIT_TEST_SUITE( MetaProgrammingTestCase );
         CPPUNIT_TEST( IsPod );
         CPPUNIT_TEST( IsMovable );
+        CPPUNIT_TEST( ImplicitConversion );
     CPPUNIT_TEST_SUITE_END();
 
     void IsPod();
     void IsMovable();
+    void ImplicitConversion();
 
     DECLARE_NO_COPY_CLASS(MetaProgrammingTestCase)
 };
@@ -66,4 +73,22 @@ void MetaProgrammingTestCase::IsMovable()
     CPPUNIT_ASSERT(wxIsMovable<wxObject*>::value);
 #endif
     CPPUNIT_ASSERT(!wxIsMovable<wxObject>::value);
+}
+
+void MetaProgrammingTestCase::ImplicitConversion()
+{
+    // wxImplicitConversionType<> is used to implement wxMax(). We test it
+    // indirectly through that here.
+
+    // test that wxMax(1.1,1) returns float, not long int
+    float f = wxMax(1.1f, 1l);
+    CPPUNIT_ASSERT_EQUAL( 1.1f, f);
+
+#ifndef wxNO_RTTI
+    CPPUNIT_ASSERT(typeid(wxImplicitConversionType<char,int>::value) == typeid(int));
+    CPPUNIT_ASSERT(typeid(wxImplicitConversionType<int,unsigned>::value) == typeid(unsigned));
+#ifdef wxLongLong_t
+    CPPUNIT_ASSERT(typeid(wxImplicitConversionType<wxLongLong_t,float>::value) == typeid(float));
+#endif
+#endif // !wxNO_RTTI
 }
