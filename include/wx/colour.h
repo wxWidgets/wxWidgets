63a6a75000ded502087a9e063569d1ce864951b9
Robin Dunn <robin@alldunn.com>
2010-11-23 22:14:57 +0000

    Use an enum for the colour/string conversion flags
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@66249 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/colour.h
+++ include/wx/colour.h
@@ -35,10 +35,11 @@ class WXDLLIMPEXP_FWD_CORE wxColour;
 
 
 // flags for wxColour -> wxString conversion (see wxColour::GetAsString)
-#define wxC2S_NAME              1   // return colour name, when possible
-#define wxC2S_CSS_SYNTAX        2   // return colour in rgb(r,g,b) syntax
-#define wxC2S_HTML_SYNTAX       4   // return colour in #rrggbb syntax
-
+enum {
+    wxC2S_NAME             = 1,   // return colour name, when possible
+    wxC2S_CSS_SYNTAX       = 2,   // return colour in rgb(r,g,b) syntax
+    wxC2S_HTML_SYNTAX      = 4    // return colour in #rrggbb syntax
+};
 
 const unsigned char wxALPHA_TRANSPARENT = 0;
 const unsigned char wxALPHA_OPAQUE = 0xff;
