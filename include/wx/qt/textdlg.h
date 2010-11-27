63d08d94203405e7a180d077c9c039c8baa7d51b
Peter Most <Peter_Most@gmx.de>
2010-11-27 17:30:57 +0000

    Use the generic text entry dialog for now
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/wxQT@66282 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/qt/textdlg.h
+++ include/wx/qt/textdlg.h
@@ -9,54 +9,58 @@
 #ifndef _WX_QT_TEXTDLG_H_
 #define _WX_QT_TEXTDLG_H_
 
-#if wxUSE_TEXTDLG
+// Use the generic version for now:
+#include "wx/generic/textdlgg.h"
 
-#if wxUSE_VALIDATORS
-#include "wx/valtext.h"
-#include "wx/textctrl.h"
-#endif
-
-#include "wx/dialog.h"
-#include <QtGui/QInputDialog>
-
-class WXDLLIMPEXP_CORE wxTextEntryDialog : public wxDialog
-{
-public:
-    wxTextEntryDialog(wxWindow *parent,
-                      const wxString& message,
-                      const wxString& caption = wxGetTextFromUserPromptStr,
-                      const wxString& value = wxEmptyString,
-                      long style = wxTextEntryDialogStyle,
-                      const wxPoint& pos = wxDefaultPosition);
-
-    void SetValue(const wxString& val);
-    const wxString &GetValue() const;
-
-#if wxUSE_VALIDATORS
-    void SetTextValidator( const wxTextValidator& validator );
-    void SetTextValidator( wxTextValidatorStyle style = wxFILTER_NONE );
-    wxTextValidator* GetTextValidator();
-#endif
-
-protected:
-    virtual QInputDialog *GetHandle() const;
-    
-private:
-};
-
-
-
-class WXDLLIMPEXP_CORE wxPasswordEntryDialog : public wxTextEntryDialog
-{
-public:
-    wxPasswordEntryDialog(wxWindow *parent,
-                      const wxString& message,
-                      const wxString& caption = wxGetPasswordFromUserPromptStr,
-                      const wxString& value = wxEmptyString,
-                      long style = wxTextEntryDialogStyle,
-                      const wxPoint& pos = wxDefaultPosition);
-};
-
-#endif // wxUSE_TEXTDLG
+//#if wxUSE_TEXTDLG
+//
+//#if wxUSE_VALIDATORS
+//#include "wx/valtext.h"
+//#include "wx/textctrl.h"
+//#endif
+//
+//#include "wx/dialog.h"
+//#include "wx/qt/pointer_qt.h"
+//#include <QtGui/QInputDialog>
+//
+//class WXDLLIMPEXP_CORE wxTextEntryDialog : public wxDialog
+//{
+//public:
+//    wxTextEntryDialog(wxWindow *parent,
+//                      const wxString& message,
+//                      const wxString& caption = wxGetTextFromUserPromptStr,
+//                      const wxString& value = wxEmptyString,
+//                      long style = wxTextEntryDialogStyle,
+//                      const wxPoint& pos = wxDefaultPosition);
+//
+//    void SetValue(const wxString& val);
+//    const wxString &GetValue() const;
+//
+//#if wxUSE_VALIDATORS
+//    void SetTextValidator( const wxTextValidator& validator );
+//    void SetTextValidator( wxTextValidatorStyle style = wxFILTER_NONE );
+//    wxTextValidator* GetTextValidator();
+//#endif
+//
+//    virtual QInputDialog *GetHandle() const;
+//
+//private:
+//    wxQtPointer< QInputDialog > m_qtInputDialog;
+//};
+//
+//
+//
+//class WXDLLIMPEXP_CORE wxPasswordEntryDialog : public wxTextEntryDialog
+//{
+//public:
+//    wxPasswordEntryDialog(wxWindow *parent,
+//                      const wxString& message,
+//                      const wxString& caption = wxGetPasswordFromUserPromptStr,
+//                      const wxString& value = wxEmptyString,
+//                      long style = wxTextEntryDialogStyle,
+//                      const wxPoint& pos = wxDefaultPosition);
+//};
+//
+//#endif // wxUSE_TEXTDLG
 
 #endif // _WX_QT_TEXTDLG_H_
--- src/qt/textdlg.cpp
+++ src/qt/textdlg.cpp
@@ -6,62 +6,65 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
+#include "../generic/textdlgg.cpp"
+
 // For compilers that support precompilation, includes "wx.h".
-#include "wx/wxprec.h"
+//#include "wx/wxprec.h"
+//
+//#include "wx/textdlg.h"
 
-#include "wx/textdlg.h"
-
-wxTextEntryDialog::wxTextEntryDialog(wxWindow *parent,
-                  const wxString& message,
-                  const wxString& caption,
-                  const wxString& value,
-                  long style,
-                  const wxPoint& pos)
-{
-}
-
-void wxTextEntryDialog::SetValue(const wxString& val)
-{
-}
-
-const wxString &wxTextEntryDialog::GetValue() const
-{
-    static const wxString s_string;
-
-    return s_string;
-}
-
-#if wxUSE_VALIDATORS
-void wxTextEntryDialog::SetTextValidator( const wxTextValidator& validator )
-{
-}
-
-void wxTextEntryDialog::SetTextValidator( wxTextValidatorStyle style)
-{
-}
-
-wxTextValidator* wxTextEntryDialog::GetTextValidator()
-{
-    return NULL;
-}
-
-#endif
-
-QInputDialog *wxTextEntryDialog::GetHandle() const
-{
-    return NULL;
-}
-
-//##############################################################################
-
-wxPasswordEntryDialog::wxPasswordEntryDialog(wxWindow *parent,
-                  const wxString& message,
-                  const wxString& caption,
-                  const wxString& value,
-                  long style,
-                  const wxPoint& pos)
-    : wxTextEntryDialog( parent, message, caption, value, style, pos )
-{
-}
+//wxTextEntryDialog::wxTextEntryDialog(wxWindow *parent,
+//                  const wxString& message,
+//                  const wxString& caption,
+//                  const wxString& value,
+//                  long style,
+//                  const wxPoint& pos)
+//{
+//    m_qtInputDialog = new QInputDialog( parent->GetHandle() );
+//    m_qtInputDialog->setInputMode( TextInput );
+//}
+//
+//void wxTextEntryDialog::SetValue(const wxString& val)
+//{
+//    m_qtInputDialog->setTextValue( wxQtConvertString( val ));
+//}
+//
+//const wxString &wxTextEntryDialog::GetValue() const
+//{
+//    return wxQtConvertString( m_qtInputDialog->textValue() );
+//}
+//
+//#if wxUSE_VALIDATORS
+//void wxTextEntryDialog::SetTextValidator( const wxTextValidator& validator )
+//{
+//}
+//
+//void wxTextEntryDialog::SetTextValidator( wxTextValidatorStyle style)
+//{
+//}
+//
+//wxTextValidator* wxTextEntryDialog::GetTextValidator()
+//{
+//    return NULL;
+//}
+//
+//#endif
+//
+//QInputDialog *wxTextEntryDialog::GetHandle() const
+//{
+//    return m_qtInputDialog;
+//}
+//
+////##############################################################################
+//
+//wxPasswordEntryDialog::wxPasswordEntryDialog(wxWindow *parent,
+//                  const wxString& message,
+//                  const wxString& caption,
+//                  const wxString& value,
+//                  long style,
+//                  const wxPoint& pos)
+//    : wxTextEntryDialog( parent, message, caption, value, style, pos )
+//{
+//}
 
 
