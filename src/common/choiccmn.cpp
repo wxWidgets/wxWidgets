37788684ba0dfe52d3b4f67e5c2dae521893bfff
Vadim Zeitlin <vadim@wxwidgets.org>
2011-01-04 14:13:29 +0000

    Use wxControl instead of wxControlWithItems with wxRTTI macros.
    wxControlWithItems is just a convenient combination of wxControl and
    wxItemContainer mix-in and it is not useful to include it in wxRTTI classes
    hierarchy. Also, using wxControlWithItems as the base class for wxChoice and
    wxListBox but not for wxComboBox is inconsistent but wxControlWithItems can't
    be used for the latter so resolve this by not using it at all.
    
    Ideally we'd have a way of retrieving the list of supported interfaces (such
    as wxItemContainer or wxTextEntry) via wxRTTI too.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@66574 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/choiccmn.cpp
+++ src/common/choiccmn.cpp
@@ -65,7 +65,7 @@ wxFLAGS_MEMBER(wxHSCROLL)
 
 wxEND_FLAGS( wxChoiceStyle )
 
-wxIMPLEMENT_DYNAMIC_CLASS_XTI(wxChoice, wxControlWithItems, "wx/choice.h")
+wxIMPLEMENT_DYNAMIC_CLASS_XTI(wxChoice, wxControl, "wx/choice.h")
 
 wxBEGIN_PROPERTIES_TABLE(wxChoice)
 wxEVENT_PROPERTY( Select, wxEVT_COMMAND_CHOICE_SELECTED, wxCommandEvent )
