595663964aac0d33489a41e27a24c1d029373103
Stefan Csomor <csomor@advancedconcepts.ch>
2011-01-07 22:37:43 +0000

    adding xti info
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@66644 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/hyperlnkcmn.cpp
+++ src/common/hyperlnkcmn.cpp
@@ -42,14 +42,62 @@
 // implementation
 // ============================================================================
 
-#if wxUSE_EXTENDED_RTTI
-    wxIMPLEMENT_DYNAMIC_CLASS_XTI( wxHyperlinkCtrl, wxControl, "wx/hyperlink.h")
-#else
-    wxIMPLEMENT_DYNAMIC_CLASS( wxHyperlinkCtrl, wxControl )
-#endif // wxUSE_EXTENDED_RTTI
+wxDEFINE_FLAGS( wxHyperlinkStyle )
+wxBEGIN_FLAGS( wxHyperlinkStyle )
+// new style border flags, we put them first to
+// use them for streaming out
+wxFLAGS_MEMBER(wxBORDER_SIMPLE)
+wxFLAGS_MEMBER(wxBORDER_SUNKEN)
+wxFLAGS_MEMBER(wxBORDER_DOUBLE)
+wxFLAGS_MEMBER(wxBORDER_RAISED)
+wxFLAGS_MEMBER(wxBORDER_STATIC)
+wxFLAGS_MEMBER(wxBORDER_NONE)
+
+// old style border flags
+wxFLAGS_MEMBER(wxSIMPLE_BORDER)
+wxFLAGS_MEMBER(wxSUNKEN_BORDER)
+wxFLAGS_MEMBER(wxDOUBLE_BORDER)
+wxFLAGS_MEMBER(wxRAISED_BORDER)
+wxFLAGS_MEMBER(wxSTATIC_BORDER)
+wxFLAGS_MEMBER(wxBORDER)
+
+// standard window styles
+wxFLAGS_MEMBER(wxTAB_TRAVERSAL)
+wxFLAGS_MEMBER(wxCLIP_CHILDREN)
+wxFLAGS_MEMBER(wxTRANSPARENT_WINDOW)
+wxFLAGS_MEMBER(wxWANTS_CHARS)
+wxFLAGS_MEMBER(wxFULL_REPAINT_ON_RESIZE)
+wxFLAGS_MEMBER(wxALWAYS_SHOW_SB )
+wxFLAGS_MEMBER(wxVSCROLL)
+wxFLAGS_MEMBER(wxHSCROLL)
+
+wxFLAGS_MEMBER(wxHL_CONTEXTMENU)
+wxFLAGS_MEMBER(wxHL_ALIGN_LEFT)
+wxFLAGS_MEMBER(wxHL_ALIGN_RIGHT)
+wxFLAGS_MEMBER(wxHL_ALIGN_CENTRE)
+wxEND_FLAGS( wxHyperlinkStyle )
+
+wxIMPLEMENT_DYNAMIC_CLASS_XTI( wxHyperlinkCtrl, wxControl, "wx/hyperlink.h")
 
 IMPLEMENT_DYNAMIC_CLASS(wxHyperlinkEvent, wxCommandEvent)
 wxDEFINE_EVENT( wxEVT_COMMAND_HYPERLINK, wxHyperlinkEvent );
+
+wxBEGIN_PROPERTIES_TABLE(wxHyperlinkCtrl)
+wxPROPERTY( Label, wxString, SetLabel, GetLabel, wxString(), \
+           0 /*flags*/, wxT("The link label"), wxT("group") )
+
+wxPROPERTY( URL, wxString, SetURL, GetURL, wxString(), \
+           0 /*flags*/, wxT("The link URL"), wxT("group") )
+wxPROPERTY_FLAGS( WindowStyle, wxHyperlinkStyle, long, SetWindowStyleFlag, \
+                 GetWindowStyleFlag, wxEMPTY_PARAMETER_VALUE, 0 /*flags*/,     \
+                 wxT("The link style"), wxT("group")) // style
+wxEND_PROPERTIES_TABLE()
+
+wxEMPTY_HANDLERS_TABLE(wxHyperlinkCtrl)
+
+wxCONSTRUCTOR_7( wxHyperlinkCtrl, wxWindow*, Parent, wxWindowID, Id, wxString, \
+                Label, wxString, URL, wxPoint, Position, wxSize, Size, long, WindowStyle )
+
 
 const char wxHyperlinkCtrlNameStr[] = "hyperlink";
 
