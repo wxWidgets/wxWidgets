c742231d299bf6121cc3c3d6f1457dd4435e671a
Paul Cornett <paulcor@bullseye.com>
2011-01-24 17:36:44 +0000

    remove deprecated voidp, closes #12896
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@66743 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/imagpng.cpp
+++ src/common/imagpng.cpp
@@ -522,7 +522,7 @@ wxPNGHandler::LoadFile(wxImage *image,
     png_structp png_ptr = png_create_read_struct
                           (
                             PNG_LIBPNG_VER_STRING,
-                            (voidp) NULL,
+                            NULL,
                             wx_png_error,
                             wx_png_warning
                           );
