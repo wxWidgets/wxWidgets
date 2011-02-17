8f5bce64e022d574d61049a40a3bc3bb8a379e69
Stefan Csomor <csomor@advancedconcepts.ch>
2011-02-17 06:46:09 +0000

    patch applied with thanks, fixes #10524
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@66934 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/osx/cocoa/stattext.mm
+++ src/osx/cocoa/stattext.mm
@@ -44,6 +44,25 @@
     }
 }
 
+- (void) setEnabled:(BOOL) flag 
+{ 
+    [super setEnabled: flag]; 
+    
+    if (![self drawsBackground]) { 
+        // Static text is drawn incorrectly when disabled. 
+        // For an explanation, see 
+        // http://www.cocoabuilder.com/archive/message/cocoa/2006/7/21/168028 
+        if (flag)
+        { 
+            [self setTextColor: [NSColor controlTextColor]]; 
+        }
+        else 
+        { 
+            [self setTextColor: [NSColor secondarySelectedControlColor]]; 
+        } 
+    } 
+} 
+
 @end
 
 class wxStaticTextCocoaImpl : public wxWidgetCocoaImpl
