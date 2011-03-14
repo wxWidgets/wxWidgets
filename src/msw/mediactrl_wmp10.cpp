d904d43ce20c78a138f429e3b68c10c6c09d1f36
Julian Smart <julian@anthemion.co.uk>
2011-03-14 10:27:12 +0000

    Added initialisation and checks
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@67183 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/mediactrl_wmp10.cpp
+++ src/msw/mediactrl_wmp10.cpp
@@ -723,7 +723,9 @@ wxWMP10MediaBackend::wxWMP10MediaBackend()
 #ifndef WXTEST_ATL
                 m_pAX(NULL),
 #endif
-                m_pWMPPlayer(NULL)
+                m_pWMPPlayer(NULL),
+                m_pWMPSettings(NULL),
+                m_pWMPControls(NULL)
 
 {
 }
@@ -746,8 +748,10 @@ wxWMP10MediaBackend::~wxWMP10MediaBackend()
 #endif
 
         m_pWMPPlayer->Release();
-        m_pWMPSettings->Release();
-        m_pWMPControls->Release();
+        if (m_pWMPSettings)
+            m_pWMPSettings->Release();
+        if (m_pWMPControls)
+            m_pWMPControls->Release();
     }
 }
 
