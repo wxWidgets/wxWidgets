80b8c88a63883fb4e676d6fc45a34248839e6834
Václav Slavík <vslavik@fastmail.fm>
2011-03-25 15:08:25 +0000

    Don't use deprecated gtk_widget_ref().
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@67308 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/frame.cpp
+++ src/gtk/frame.cpp
@@ -271,7 +271,7 @@ void wxFrame::DetachMenuBar()
 #if wxUSE_LIBHILDON || wxUSE_LIBHILDON2
         hildon_window_set_menu(HILDON_WINDOW(m_widget), NULL);
 #else // !wxUSE_LIBHILDON && !wxUSE_LIBHILDON2
-        gtk_widget_ref( m_frameMenuBar->m_widget );
+        g_object_ref( m_frameMenuBar->m_widget );
 
         gtk_container_remove( GTK_CONTAINER(m_mainWidget), m_frameMenuBar->m_widget );
 #endif // wxUSE_LIBHILDON || wxUSE_LIBHILDON2 /!wxUSE_LIBHILDON && !wxUSE_LIBHILDON2
