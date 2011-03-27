8bffcd08c26ab977d908b119a9a41a6ce9887b64
Václav Slavík <vslavik@fastmail.fm>
2011-03-27 17:23:39 +0000

    Renamed wx/gtk/private/compat.h to gtk2-compat.h.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@67318 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/gtk/private.h
+++ include/wx/gtk/private.h
@@ -15,7 +15,7 @@
 #include <gtk/gtk.h>
 
 #include "wx/gtk/private/string.h"
-#include "wx/gtk/private/compat.h"
+#include "wx/gtk/private/gtk2-compat.h"
 
 // pango_version_check symbol is quite recent ATM (4/2007)... so we
 // use our own wrapper which implements a smart trick.
