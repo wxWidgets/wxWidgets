1bdeb24eeaa07673355edd83b4b801cc8f2afc38
Vadim Zeitlin <vadim@wxwidgets.org>
2011-05-09 09:21:25 +0000

    Document wxBitmapToggleButton XRC parameters.
    Describe the properties specific to this class in the XRC format
    documentation.
    
    Closes #13204.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@67717 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/doxygen/overviews/xrc_format.h
+++ docs/doxygen/overviews/xrc_format.h
@@ -586,6 +586,17 @@ Example:
 @endcode
 
 
+@subsubsection xrc_wxtogglebuttton wxBitmapToggleButton
+
+@beginTable
+@hdr3col{property, type, description}
+@row3col{bitmap, @ref overview_xrcformat_type_bitmap,
+     Label to display on the button (required).}
+@row3col{checked, @ref overview_xrcformat_type_bool,
+     Should the button be checked/pressed initially (default: 0)?}
+@endTable
+
+
 @subsubsection xrc_wxbutton wxButton
 
 @beginTable
