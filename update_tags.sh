1ed5e15248eda18a2e8afd5fc554c36e7fd55034
John Chain <jchain3@gmail.com>
2011-06-08 08:19:58 +0000

    Added personal settings about tag generation and vim
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2011_GTK3@67886 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ update_tags.sh
@@ -0,0 +1,6 @@
+#!/bin/bash  
+
+SRCLIST='source.list'
+find src/gtk src/common src/generic src/unix include/wx/gtk include/wx/generic include/wx/unix -name "*.cpp" -o -name "*.h" > $SRCLIST
+gtags -f $SRCLIST
+ctags -R --c++-kinds=+p --fields=+ialS --extra=+q --language-force=c++ -L $SRCLIST
