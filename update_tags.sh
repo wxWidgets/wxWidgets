447593de93105076a722f9a4c4681005b3dcdf72
John Chain <jchain3@gmail.com>
2011-06-09 03:30:20 +0000

    Update tags files for easy browsing
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2011_GTK3@67906 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- update_tags.sh
+++ update_tags.sh
@@ -2,5 +2,6 @@
 
 SRCLIST='source.list'
 find src/gtk src/common src/generic src/unix include/wx/gtk include/wx/generic include/wx/unix -name "*.cpp" -o -name "*.h" > $SRCLIST
+find -L cinclude -name "*.h" >> $SRCLIST
 gtags -f $SRCLIST
 ctags -R --c++-kinds=+p --fields=+ialS --extra=+q --language-force=c++ -L $SRCLIST
