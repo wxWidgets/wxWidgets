71c667e138ed5a50b3926d0837e4fb9a0a98b1af
Linas Valiukas <shirshegsm@gmail.com>
2011-07-14 01:45:00 +0000

    Removed '#define ClassA SomewhatRelatedClassB'
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2011_WXIOS@68256 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/numdlgg.cpp
+++ src/generic/numdlgg.cpp
@@ -49,11 +49,6 @@
 // this is where wxGetNumberFromUser() is declared
 #include "wx/numdlg.h"
 
-#if !wxUSE_SPINCTRL
-    // wxTextCtrl will do instead of wxSpinCtrl if we don't have it
-    #define wxSpinCtrl wxTextCtrl
-#endif
-
 // ============================================================================
 // implementation
 // ============================================================================
@@ -105,7 +100,11 @@ wxNumberEntryDialog::wxNumberEntryDialog(wxWindow *parent,
     // spin ctrl
     wxString valStr;
     valStr.Printf(wxT("%ld"), m_value);
+#if wxUSE_SPINCTRL
     m_spinctrl = new wxSpinCtrl(this, wxID_ANY, valStr, wxDefaultPosition, wxSize( 140, wxDefaultCoord ), wxSP_ARROW_KEYS, (int)m_min, (int)m_max, (int)m_value);
+#else
+    m_spinctrl = new wxTextCtrl(this, wxID_ANY, valStr, wxDefaultPosition, wxSize( 140, wxDefaultCoord ));
+#endif
     inputsizer->Add( m_spinctrl, 1, wxCENTER | wxLEFT | wxRIGHT, 10 );
     // add both
     topsizer->Add( inputsizer, 0, wxEXPAND | wxLEFT|wxRIGHT, 5 );
