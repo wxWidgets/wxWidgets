2dd5e90e81c9e675b38171e06462668ec924b582
Linas Valiukas <shirshegsm@gmail.com>
2011-08-12 22:29:39 +0000

    wxStaticText background colors
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2011_WXIOS@68665 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/mobile/tests/tests_stattext.cpp
+++ tests/mobile/tests/tests_stattext.cpp
@@ -43,13 +43,16 @@ bool MobileTestsWxStaticTextPanel::CreateControls()
     // 
     
     m_textLeft = new wxStaticText(this, wxID_ANY, "Left", wxPoint(0, 0), wxSize(300, 40), wxALIGN_LEFT);
-    textSizer->Add(m_textLeft, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    m_textLeft->SetBackgroundColour(*wxRED);
+    //textSizer->Add(m_textLeft, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
     m_textCentre = new wxStaticText(this, wxID_ANY, "Centre", wxPoint(0, 45), wxSize(300, 40), wxALIGN_CENTRE);
-    textSizer->Add(m_textCentre, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    m_textCentre->SetBackgroundColour(*wxGREEN);
+    //textSizer->Add(m_textCentre, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
     m_textRight = new wxStaticText(this, wxID_ANY, "Right", wxPoint(0, 90), wxSize(300, 40), wxALIGN_RIGHT);
-    textSizer->Add(m_textCentre, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    m_textRight->SetBackgroundColour(*wxBLUE);
+    //textSizer->Add(m_textRight, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
     return true;
 }
