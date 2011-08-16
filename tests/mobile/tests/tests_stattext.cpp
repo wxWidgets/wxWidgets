d831cdf8efb20f3df181fd70e350f51e44ce594b
Stefan Csomor <csomor@advancedconcepts.ch>
2011-08-16 15:17:50 +0000

    integrating sizers
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/SOC2011_WXIOS@68732 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/mobile/tests/tests_stattext.cpp
+++ tests/mobile/tests/tests_stattext.cpp
@@ -36,23 +36,24 @@ bool MobileTestsWxStaticTextPanel::CreateWithControls(wxWindow* parent,
 
 bool MobileTestsWxStaticTextPanel::CreateControls()
 {
-    wxBoxSizer* textSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer* textSizer = new wxBoxSizer(wxVERTICAL);
+    SetSizer(textSizer);
     
     //
     // Various labels
     // 
     
-    m_textLeft = new wxStaticText(this, wxID_ANY, "Left", wxPoint(0, 0), wxSize(300, 40), wxALIGN_LEFT);
+    m_textLeft = new wxStaticText(this, wxID_ANY, "Left", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
     m_textLeft->SetBackgroundColour(*wxRED);
-    //textSizer->Add(m_textLeft, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    textSizer->Add(m_textLeft, 0, wxEXPAND|wxALL, 5);
 
-    m_textCentre = new wxStaticText(this, wxID_ANY, "Centre", wxPoint(0, 45), wxSize(300, 40), wxALIGN_CENTRE);
+    m_textCentre = new wxStaticText(this, wxID_ANY, "Centre", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
     m_textCentre->SetBackgroundColour(*wxGREEN);
-    //textSizer->Add(m_textCentre, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    textSizer->Add(m_textCentre, 0, wxEXPAND|wxALL, 5);
 
-    m_textRight = new wxStaticText(this, wxID_ANY, "Right", wxPoint(0, 90), wxSize(300, 40), wxALIGN_RIGHT);
+    m_textRight = new wxStaticText(this, wxID_ANY, "Right", wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
     m_textRight->SetBackgroundColour(*wxBLUE);
-    //textSizer->Add(m_textRight, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    textSizer->Add(m_textRight, 0, wxEXPAND|wxALL, 5);
 
     return true;
 }
