c1540a0185845ee66db113448da35c3d1de0b970
Julian Smart <julian@anthemion.co.uk>
2011-10-07 03:51:10 +0000

    Allow selected symbol to be scrolled into view initially
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@69320 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextsymboldlg.cpp
+++ src/richtext/richtextsymboldlg.cpp
@@ -562,7 +562,7 @@ bool wxSymbolPickerDialog::TransferDataToWindow()
         m_symbolsCtrl->SetSelection(sel);
     }
 
-    UpdateSymbolDisplay();
+    UpdateSymbolDisplay(true, m_symbol.empty());
 
     m_dontUpdate = false;
 
