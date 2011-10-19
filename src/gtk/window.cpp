4a99d5975088e9c3cf05b3d4d9889bd9b70ea75d
Václav Slavík <vslavik@fastmail.fm>
2011-10-19 16:20:10 +0000

    Move InitMouseEvent<T>() to wx/gtk/private/event.h.
    It is needed in wxDataViewCtrl now too, in addition to wxWindow.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@69472 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/window.cpp
+++ src/gtk/window.cpp
@@ -37,6 +37,8 @@
 
 #include "wx/gtk/private.h"
 #include "wx/gtk/private/win_gtk.h"
+#include "wx/gtk/private/event.h"
+using namespace wxGTKImpl;
 
 #include <gdk/gdkx.h>
 
@@ -1100,43 +1102,6 @@ gtk_window_key_release_callback( GtkWidget * WXUNUSED(widget),
 // ----------------------------------------------------------------------------
 // mouse event processing helpers
 // ----------------------------------------------------------------------------
-
-// init wxMouseEvent with the info from GdkEventXXX struct
-template<typename T> void InitMouseEvent(wxWindowGTK *win,
-                                         wxMouseEvent& event,
-                                         T *gdk_event)
-{
-    event.m_shiftDown = (gdk_event->state & GDK_SHIFT_MASK) != 0;
-    event.m_controlDown = (gdk_event->state & GDK_CONTROL_MASK) != 0;
-    event.m_altDown = (gdk_event->state & GDK_MOD1_MASK) != 0;
-    event.m_metaDown = (gdk_event->state & GDK_META_MASK) != 0;
-    event.m_leftDown = (gdk_event->state & GDK_BUTTON1_MASK) != 0;
-    event.m_middleDown = (gdk_event->state & GDK_BUTTON2_MASK) != 0;
-    event.m_rightDown = (gdk_event->state & GDK_BUTTON3_MASK) != 0;
-
-    // In gdk/win32 VK_XBUTTON1 is translated to GDK_BUTTON4_MASK
-    // and VK_XBUTTON2 to GDK_BUTTON5_MASK. In x11/gdk buttons 4/5
-    // are wheel rotation and buttons 8/9 don't change the state.
-    event.m_aux1Down = (gdk_event->state & GDK_BUTTON4_MASK) != 0;
-    event.m_aux2Down = (gdk_event->state & GDK_BUTTON5_MASK) != 0;
-
-    wxPoint pt = win->GetClientAreaOrigin();
-    event.m_x = (wxCoord)gdk_event->x - pt.x;
-    event.m_y = (wxCoord)gdk_event->y - pt.y;
-
-    if ((win->m_wxwindow) && (win->GetLayoutDirection() == wxLayout_RightToLeft))
-    {
-        // origin in the upper right corner
-        GtkAllocation a;
-        gtk_widget_get_allocation(win->m_wxwindow, &a);
-        int window_width = a.width;
-        event.m_x = window_width - event.m_x;
-    }
-
-    event.SetEventObject( win );
-    event.SetId( win->GetId() );
-    event.SetTimestamp( gdk_event->time );
-}
 
 static void AdjustEventButtonState(wxMouseEvent& event)
 {
