309f2b20311a0ebf665f461ba32f2993abf91d2f
Stefan Csomor <csomor@advancedconcepts.ch>
2011-12-12 15:54:40 +0000

    adding defaults for Xcode builds
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@69993 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/platform.h
+++ include/wx/platform.h
@@ -33,6 +33,12 @@
 #    include <TargetConditionals.h>
 #    include <AvailabilityMacros.h>
 #    include "wx/osx/config_xcode.h"
+#    ifndef __WXOSX__
+#        define __WXOSX__ 1
+#    endif
+#    ifndef __WXMAC__
+#        define __WXMAC__ 1
+#    endif
 #endif
 
 /*
