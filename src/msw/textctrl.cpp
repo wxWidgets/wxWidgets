f79f06665c112b6eb081216d66589cad189fa166
Vadim Zeitlin <vadim@wxwidgets.org>
2011-12-27 17:32:29 +0000

    Compilation fix for PCH-less build after r70122.
    Include the header declaring wxClientDC.
    
    Closes #13809.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@70130 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/textctrl.cpp
+++ src/msw/textctrl.cpp
@@ -30,6 +30,7 @@
     #include "wx/textctrl.h"
     #include "wx/settings.h"
     #include "wx/brush.h"
+    #include "wx/dcclient.h"
     #include "wx/utils.h"
     #include "wx/intl.h"
     #include "wx/log.h"
