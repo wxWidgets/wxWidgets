f2012fa63905c4b1e2a144a56e48097a46e3f3e1
Vadim Zeitlin <vadim@wxwidgets.org>
2012-01-15 18:50:35 +0000

    Remove __WXMGL__ tests from the samples too.
    __WXMGL__ is not used any longer, remove it from the samples too.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@70359 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/taskbar/tbtest.cpp
+++ samples/taskbar/tbtest.cpp
@@ -29,7 +29,7 @@
 #endif
 
 // the application icon (under Windows and OS/2 it is in resources)
-#if defined(__WXGTK__) || defined(__WXMOTIF__) || defined(__WXMAC__) || defined(__WXMGL__) || defined(__WXX11__)
+#if defined(__WXGTK__) || defined(__WXMOTIF__) || defined(__WXMAC__) || defined(__WXX11__)
     #include "../sample.xpm"
 #endif
 
