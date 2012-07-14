b391c4e97296c091d54d4bd7d4eab23d4b55b73e
Robin Dunn <robin@alldunn.com>
2012-07-14 21:36:25 +0000

    Add missing styles
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72084 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/dirdlg.h
+++ interface/wx/dirdlg.h
@@ -6,6 +6,10 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
+#define wxDD_CHANGE_DIR         0x0100
+#define wxDD_DIR_MUST_EXIST     0x0200
+#define wxDD_NEW_DIR_BUTTON     0 // deprecated, on by default now,
+
 #define wxDD_DEFAULT_STYLE      (wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER)
 
 /**
