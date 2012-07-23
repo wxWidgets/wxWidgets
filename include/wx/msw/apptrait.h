7d17efb14ddd687a65cf556234354e64086c9d62
Vadim Zeitlin <vadim@wxwidgets.org>
2012-07-23 15:03:30 +0000

    Define wxGUIAppTraits for wxGTK/Windows port.
    Closes #14505.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72178 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/apptrait.h
+++ include/wx/msw/apptrait.h
@@ -37,6 +37,8 @@ public:
 
 #if wxUSE_GUI
 
+#if defined(__WXMSW__)
+
 class WXDLLIMPEXP_CORE wxGUIAppTraits : public wxGUIAppTraitsBase
 {
 public:
@@ -57,6 +59,38 @@ public:
     virtual bool WriteToStderr(const wxString& text);
 #endif // !__WXWINCE__
 };
+
+#elif defined(__WXGTK__)
+
+class WXDLLIMPEXP_CORE wxGUIAppTraits : public wxGUIAppTraitsBase
+{
+public:
+    virtual wxEventLoopBase *CreateEventLoop();
+    virtual void *BeforeChildWaitLoop() { return NULL; }
+    virtual void AfterChildWaitLoop(void *WXUNUSED(data)) { }
+#if wxUSE_TIMER
+    virtual wxTimerImpl *CreateTimerImpl(wxTimer *timer);
+#endif
+
+#if wxUSE_THREADS && defined(__WXGTK20__)
+    virtual void MutexGuiEnter();
+    virtual void MutexGuiLeave();
+#endif
+
+#if wxUSE_THREADS
+    virtual bool DoMessageFromThreadWait() { return true; }
+    virtual WXDWORD WaitForThread(WXHANDLE hThread, int WXUNUSED(flags))
+        { return DoSimpleWaitForThread(hThread); }
+#endif // wxUSE_THREADS
+    virtual wxPortId GetToolkitVersion(int *majVer = NULL, int *minVer = NULL) const;
+
+#ifndef __WXWINCE__
+    virtual bool CanUseStderr() { return false; }
+    virtual bool WriteToStderr(const wxString& WXUNUSED(text)) { return false; }
+#endif // !__WXWINCE__
+};
+
+#endif
 
 #endif // wxUSE_GUI
 
