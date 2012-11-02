2ab6738554012b4789cd12d035f112caf68a6c05
Stefan Csomor <csomor@advancedconcepts.ch>
2012-11-02 17:02:15 +0000

    missing commit, fixes #14797
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72865 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/cocoa/private.h
+++ include/wx/osx/cocoa/private.h
@@ -268,7 +268,8 @@ public:
     
     void SetPressedBitmap( const wxBitmap& bitmap );
     void GetLayoutInset(int &left , int &top , int &right, int &bottom) const;
-    
+    void SetAcceleratorFromLabel(const wxString& label);
+
 private:
     NSButton *GetNSButton() const;
 };
