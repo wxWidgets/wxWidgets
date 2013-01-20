d1c629ce8fd065109e20063d5e9aeeca075398a3
Vadim Zeitlin <vadim@wxwidgets.org>
2013-01-20 02:08:46 +0000

    Fix some harmless but annoying warnings about casts in libtiff code.
    Avoid MSVC C4244 warnings due to implicit casts between 64 and 32 bit
    integers.
    
    This patch was also submitted to libtiff via
    
    	http://bugzilla.maptools.org/show_bug.cgi?id=2427
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@73388 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/tiff/libtiff/tif_read.c
+++ src/tiff/libtiff/tif_read.c
@@ -47,7 +47,7 @@ TIFFFillStripPartial( TIFF *tif, int strip, tmsize_t read_ahead, int restart )
 {
 	static const char module[] = "TIFFFillStripPartial";
 	register TIFFDirectory *td = &tif->tif_dir;
-        uint64 unused_data;
+        tmsize_t unused_data;
         uint64 read_offset;
         tmsize_t cc, to_read;
         /* tmsize_t bytecountm; */
@@ -117,7 +117,7 @@ TIFFFillStripPartial( TIFF *tif, int strip, tmsize_t read_ahead, int restart )
         if( (uint64) to_read > td->td_stripbytecount[strip] 
             - tif->tif_rawdataoff - tif->tif_rawdataloaded )
         {
-                to_read = td->td_stripbytecount[strip]
+                to_read = (tmsize_t)td->td_stripbytecount[strip]
                         - tif->tif_rawdataoff - tif->tif_rawdataloaded;
         }
 
