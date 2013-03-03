8ae80de41f98d12fbdb36eb4732195d21d1f4fa8
Vadim Zeitlin <vadim@wxwidgets.org>
2013-03-03 12:19:20 +0000

    Revert "Fix off by 1 error in wxGenericListCtrl::HitTest()."
    Revert r73563, this breaks the selection in the control apparently.
    
    See #15063.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@73596 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/listctrl.cpp
+++ src/generic/listctrl.cpp
@@ -4144,10 +4144,6 @@ long wxListMainWindow::HitTest( int x, int y, int &flags ) const
 
     if ( InReportView() )
     {
-        // Account for the header height if it's present.
-        if ( HasHeader() )
-            y -= GetListCtrl()->m_headerWin->GetSize().y + 1;
-
         size_t current = y / GetLineHeight();
         if ( current < count )
         {
