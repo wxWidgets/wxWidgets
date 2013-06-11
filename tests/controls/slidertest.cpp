e72e0814b63e90ede2e8768047858c70dc3eb130
Stefan Csomor <csomor@advancedconcepts.ch>
2013-06-11 22:27:48 +0000

    draghino explicitly
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74171 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/slidertest.cpp
+++ tests/controls/slidertest.cpp
@@ -198,13 +198,7 @@ void SliderTestCase::Thumb()
     sim.MouseMove(m_slider->ClientToScreen(wxPoint(10, 10)));
     wxYield();
 
-    sim.MouseDown();
-    wxYield();
-
-    sim.MouseMove(m_slider->ClientToScreen(wxPoint(50, 10)));
-    wxYield();
-
-    sim.MouseUp();
+    sim.MouseClickAndDragTo(m_slider->ClientToScreen(wxPoint(50, 10)));
     wxYield();
 
     CPPUNIT_ASSERT(track.GetCount() != 0);
