e14c237bc692e437b3b638fd5b213678298a7805
Julian Smart <julian@anthemion.co.uk>
2013-06-12 09:52:44 +0000

    Backported fix to not give focus to the search control buttons
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@74180 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/srchctlg.cpp
+++ src/generic/srchctlg.cpp
@@ -212,6 +212,14 @@ public:
 
     void SetBitmapLabel(const wxBitmap& label) { m_bmp = label; }
 
+    // Backported from 2.9; see http://trac.wxwidgets.org/ticket/12808
+    // The buttons in wxSearchCtrl shouldn't accept focus from keyboard because 
+    // this would interfere with the usual TAB processing: the user expects 
+    // that pressing TAB in the search control should switch focus to the next 
+ 	// control and not give it to the button inside the same control. Besides, 
+ 	// the search button can be already activated by pressing "Enter" so there 
+ 	// is really no reason for it to be able to get focus from keyboard. 
+ 	virtual bool AcceptsFocusFromKeyboard() const { return false; }
 
 protected:
     wxSize DoGetBestSize() const
