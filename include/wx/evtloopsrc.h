64b4a35954c4cd98a63a9f3d9f7e17ef0c15f30c
Vadim Zeitlin <vadim@wxwidgets.org>
2013-07-03 00:30:16 +0000

    Include wx/evtloop.h from wx/evtloopsrc.h.
    This is necessary to ensure that wxUSE_EVENTLOOP_SOURCE which wraps the entire
    contents of this header is defined if it's included directly and not after
    already including wx/evtloop.h.
    
    See #10258.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74345 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/evtloopsrc.h
+++ include/wx/evtloopsrc.h
@@ -11,6 +11,9 @@
 #ifndef _WX_EVTLOOPSRC_H_
 #define _WX_EVTLOOPSRC_H_
 
+// Include the header to get wxUSE_EVENTLOOP_SOURCE definition from it.
+#include "wx/evtloop.h"
+
 // ----------------------------------------------------------------------------
 // wxEventLoopSource: a source of events which may be added to wxEventLoop
 // ----------------------------------------------------------------------------
