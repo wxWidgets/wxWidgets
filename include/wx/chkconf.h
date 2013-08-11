c8841d1493e0d19085990c9e331dd85c4da45d8e
Stefan Csomor <csomor@advancedconcepts.ch>
2013-08-11 06:39:20 +0000

    using #ifdef wxABORT_ON_CONFIG_ERROR not just #if as elsewhere
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74653 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/chkconf.h
+++ include/wx/chkconf.h
@@ -2208,7 +2208,7 @@
 #   if !wxUSE_NOTEBOOK
 #       ifdef __WXOSX_COCOA__
 #           if !wxUSE_TOOLBAR || !wxOSX_USE_NATIVE_TOOLBAR
-#               if wxABORT_ON_CONFIG_ERROR
+#               ifdef wxABORT_ON_CONFIG_ERROR
 #                   error "wxUSE_PREFERENCES_EDITOR requires native toolbar in wxOSX"
 #               else
 #                   undef wxUSE_PREFERENCES_EDITOR
@@ -2216,7 +2216,7 @@
 #               endif
 #           endif
 #       else
-#           if wxABORT_ON_CONFIG_ERROR
+#           ifdef wxABORT_ON_CONFIG_ERROR
 #               error "wxUSE_PREFERENCES_EDITOR requires wxNotebook"
 #           else
 #               undef wxUSE_PREFERENCES_EDITOR
