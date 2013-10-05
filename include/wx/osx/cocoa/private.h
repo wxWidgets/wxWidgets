8155d71a7dbd17918da5e5dafae057f29cbb0e22
Vadim Zeitlin <vadim@wxwidgets.org>
2013-10-05 23:30:45 +0000

    Improve handling of keyboard entry using IME.
    Pass the keyboard events to the IME before generating our events for them, the
    IME may need them for its own use.
    
    Closes #15384.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74945 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/cocoa/private.h
+++ include/wx/osx/cocoa/private.h
@@ -160,6 +160,7 @@ public :
     virtual void                cursorUpdate(WX_NSEvent event, WXWidget slf, void* _cmd);
     virtual void                keyEvent(WX_NSEvent event, WXWidget slf, void* _cmd);
     virtual void                insertText(NSString* text, WXWidget slf, void* _cmd);
+    virtual void                doCommandBySelector(void* sel, WXWidget slf, void* _cmd);
     virtual bool                performKeyEquivalent(WX_NSEvent event, WXWidget slf, void* _cmd);
     virtual bool                acceptsFirstResponder(WXWidget slf, void* _cmd);
     virtual bool                becomeFirstResponder(WXWidget slf, void* _cmd);
