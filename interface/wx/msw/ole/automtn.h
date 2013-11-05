768670dffe2d086a0aeb50048d75d33f7543c6f9
Vadim Zeitlin <vadim@wxwidgets.org>
2013-11-05 13:39:48 +0000

    Fix wxOleConvertVariantFlags mentions in the documentation.
    In some case wxConvertVariantToOleFlags was used instead.
    
    Closes #15641.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_3_0_BRANCH@75127 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/msw/ole/automtn.h
+++ interface/wx/msw/ole/automtn.h
@@ -610,7 +610,7 @@ public:
 
     /**
         Returns the flags used for conversions between wxVariant and OLE
-        VARIANT, see wxConvertVariantToOleFlags.
+        VARIANT, see wxOleConvertVariantFlags.
 
         The default value is wxOleConvertVariant_Default for compatibility but
         it can be changed using SetConvertVariantFlags().
@@ -624,7 +624,7 @@ public:
 
     /**
         Sets the flags used for conversions between wxVariant and OLE VARIANT,
-        see wxConvertVariantToOleFlags.
+        see wxOleConvertVariantFlags.
 
         The default value is wxOleConvertVariant_Default.
 
