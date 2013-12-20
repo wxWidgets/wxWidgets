b010793ea84807697f270a3feddc4da78aa5cb0f
Vadim Zeitlin <vadim@wxwidgets.org>
2013-12-20 17:50:27 +0000

    Document wxEVT_MEDIA_XXX event types.
    Closes #15768.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75402 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/mediactrl.h
+++ interface/wx/mediactrl.h
@@ -48,6 +48,7 @@ enum wxMediaCtrlPlayerControls
     @beginEventTable{wxMediaEvent}
     @event{EVT_MEDIA_LOADED(id\, func)}
            Sent when a media has loaded enough data that it can start playing.
+           Processes a @c wxEVT_MEDIA_LOADED event type.
     @event{EVT_MEDIA_STOP(id\, func)}
            Sent when a media has switched to the @c wxMEDIASTATE_STOPPED state.
            You may be able to Veto this event to prevent it from stopping,
@@ -55,14 +56,19 @@ enum wxMediaCtrlPlayerControls
            the media (note that this may not have the desired effect - if you
            want to loop the media, for example, catch the @c EVT_MEDIA_FINISHED
            and play there instead).
+           Processes a @c wxEVT_MEDIA_STOP event type.
     @event{EVT_MEDIA_FINISHED(id\, func)}
            Sent when a media has finished playing in a wxMediaCtrl.
+           Processes a @c wxEVT_MEDIA_FINISHED event type.
     @event{EVT_MEDIA_STATECHANGED(id\, func)}
            Sent when a media has switched its state (from any media state).
+           Processes a @c wxEVT_MEDIA_STATECHANGED event type.
     @event{EVT_MEDIA_PLAY(id\, func)}
            Sent when a media has switched to the @c wxMEDIASTATE_PLAYING state.
+           Processes a @c wxEVT_MEDIA_PLAY event type.
     @event{EVT_MEDIA_PAUSE(id\, func)}
            Sent when a media has switched to the @c wxMEDIASTATE_PAUSED state.
+           Processes a @c wxEVT_MEDIA_PAUSE event type.
     @endEventTable
 
     @library{wxmedia}
@@ -442,3 +448,9 @@ public:
     wxFileOffset Tell();
 };
 
+wxEventType wxEVT_MEDIA_LOADED;
+wxEventType wxEVT_MEDIA_STOP;
+wxEventType wxEVT_MEDIA_FINISHED;
+wxEventType wxEVT_MEDIA_STATECHANGED;
+wxEventType wxEVT_MEDIA_PLAY;
+wxEventType wxEVT_MEDIA_PAUSE;
