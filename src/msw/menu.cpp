2d3a1aab74168b99bd12689ad0da677ab1844320
Vadim Zeitlin <vadim@wxwidgets.org>
2013-12-29 00:01:38 +0000

    Fix handling of menu items checked before being attached in wxMSW.
    Calling wxMenuItem::Check() before appending the item to a menu didn't have
    any effect, fix this by checking the item state when actually attaching it.
    
    Closes #15748.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75448 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/menu.cpp
+++ src/msw/menu.cpp
@@ -526,6 +526,13 @@ bool wxMenu::DoInsertOrAppend(wxMenuItem *pItem, size_t pos)
             checkInitially = true;
     }
 
+    // Also handle the case of check menu items that had been checked before
+    // being attached to the menu: we don't need to actually call Check() on
+    // them, so we don't use checkInitially in this case, but we do need to
+    // make them checked at Windows level too.
+    if ( pItem->IsCheck() && pItem->IsChecked() )
+        flags |= MF_CHECKED;
+
     // adjust position to account for the title of a popup menu, if any
     if ( !GetMenuBar() && !m_title.empty() )
         pos += 2; // for the title itself and its separator
@@ -609,6 +616,12 @@ bool wxMenu::DoInsertOrAppend(wxMenuItem *pItem, size_t pos)
                 {
                     mii.fMask |= MIIM_ID;
                     mii.wID = id;
+                }
+
+                if ( flags & MF_CHECKED )
+                {
+                    mii.fMask |= MIIM_STATE;
+                    mii.fState = MFS_CHECKED;
                 }
 
                 mii.dwItemData = reinterpret_cast<ULONG_PTR>(pItem);
