7f3cd4d682f2976186bdb1ad7d7aabcf483294ee
Robin Dunn <robin@alldunn.com>
2014-01-14 05:35:33 +0000

    add missing GetVeto method
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_3_0_BRANCH@75622 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/event.h
+++ interface/wx/event.h
@@ -4083,6 +4083,11 @@ public:
         You can only veto a shutdown if CanVeto() returns @true.
     */
     void Veto(bool veto = true);
+
+    /**
+       Returns whether the Veto flag was set.
+    */
+    bool GetVeto() const;
 };
 
 
