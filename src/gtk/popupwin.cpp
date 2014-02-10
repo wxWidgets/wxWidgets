ec3872a8e186fde86877250ea6e33ad3db08cce4
Paul Cornett <paulcor@bullseye.com>
2014-02-10 17:25:32 +0000

    remove stuff that is already done by gtk_window_set_transient_for()
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75866 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/popupwin.cpp
+++ src/gtk/popupwin.cpp
@@ -113,18 +113,7 @@ bool wxPopupWindow::Create( wxWindow *parent, int style )
     {
         GtkWidget *toplevel = gtk_widget_get_toplevel( parent->m_widget );
         if (GTK_IS_WINDOW (toplevel))
-        {
-#if GTK_CHECK_VERSION(2,10,0)
-#ifndef __WXGTK3__
-            if (!gtk_check_version(2,10,0))
-#endif
-            {
-                gtk_window_group_add_window (gtk_window_get_group (GTK_WINDOW (toplevel)), GTK_WINDOW (m_widget));
-            }
-#endif
             gtk_window_set_transient_for (GTK_WINDOW (m_widget), GTK_WINDOW (toplevel));
-        }
-        gtk_window_set_screen (GTK_WINDOW (m_widget), gtk_widget_get_screen (GTK_WIDGET (parent->m_widget)));
     }
 
     gtk_window_set_resizable (GTK_WINDOW (m_widget), FALSE);
