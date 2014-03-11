8b5d2687dba302510a61d4cab66046ba6d5a98f2
Vadim Zeitlin <vadim@wxwidgets.org>
2014-03-11 20:48:16 +0000

    Remove unnecessarily overridden methods from wxToggleButtonBase.
    These methods already do exactly the same thing in the base wxAnyButton class,
    there is no need to override them again.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76119 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/tglbtn.h
+++ include/wx/tglbtn.h
@@ -57,15 +57,7 @@ public:
         }
     }
 
-    // Buttons on MSW can look bad if they are not native colours, because
-    // then they become owner-drawn and not theme-drawn.  Disable it here
-    // in wxToggleButtonBase to make it consistent.
-    virtual bool ShouldInheritColours() const { return false; }
-
 protected:
-    // choose the default border for this window
-    virtual wxBorder GetDefaultBorder() const { return wxBORDER_NONE; }
-
     wxDECLARE_NO_COPY_CLASS(wxToggleButtonBase);
 };
 
