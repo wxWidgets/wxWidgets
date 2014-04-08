fc014a6f194373776b5227e55b45a3c892df3ab3
Vadim Zeitlin <vadim@wxwidgets.org>
2014-04-08 21:38:09 +0000

    Don't set wxINTERPOLATION_GOOD quality by default in wxGDIPlusContext.
    This is inconsistent with wxOSX and also doesn't play well with the existence
    of wxINTERPOLATION_DEFAULT which is different from wxINTERPOLATION_GOOD.
    
    See #14134.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76305 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/graphics.cpp
+++ src/msw/graphics.cpp
@@ -1422,8 +1422,6 @@ void wxGDIPlusContext::Init(Graphics* graphics, int width, int height)
     m_context->SetPixelOffsetMode(PixelOffsetModeHalf);
     m_context->SetSmoothingMode(SmoothingModeHighQuality);
 
-    SetInterpolationQuality(wxINTERPOLATION_GOOD);
-
     m_state1 = m_context->Save();
     m_state2 = m_context->Save();
 }
