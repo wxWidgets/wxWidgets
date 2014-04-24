wxWidgets/wxWidgets

commit aaa52a40eefeffda8a038cc1b2ff3feda0ebdbe9

Don't use invalid pointer in file dialog hook procedure in wxMSW.
We can receive WM_NOTIFY for other than CDN_XXX messages if we have a native
control as our immediate child (which can happen with "extra" controls) and
the LPARAM is not a pointer to OFNOTIFY at all in this case, so don't try to
use it as such.

This fixes a crash when adding a "bare" extra control, see #16003.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_3_0_BRANCH@75937 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/filedlg.cpp
@@ -172,22 +172,26 @@ wxFileDialogHookFunction(HWND      hDlg,
 
         case WM_NOTIFY:
             {
-                OFNOTIFY* const
-                    pNotifyCode = reinterpret_cast<OFNOTIFY *>(lParam);
-                wxFileDialog* const
-                    dialog = reinterpret_cast<wxFileDialog *>(
-                                    pNotifyCode->lpOFN->lCustData
-                                );
-
-                switch ( pNotifyCode->hdr.code )
+                NMHDR* const pNM = reinterpret_cast<NMHDR*>(lParam);
+                if ( pNM->code >= CDN_FIRST && pNM->code < CDN_LAST )
                 {
-                    case CDN_INITDONE:
-                        dialog->MSWOnInitDone((WXHWND)hDlg);
-                        break;
+                    OFNOTIFY* const
+                        pNotifyCode = reinterpret_cast<OFNOTIFY *>(lParam);
+                    wxFileDialog* const
+                        dialog = reinterpret_cast<wxFileDialog *>(
+                                        pNotifyCode->lpOFN->lCustData
+                                    );
 
-                    case CDN_SELCHANGE:
-                        dialog->MSWOnSelChange((WXHWND)hDlg);
-                        break;
+                    switch ( pNotifyCode->hdr.code )
+                    {
+                        case CDN_INITDONE:
+                            dialog->MSWOnInitDone((WXHWND)hDlg);
+                            break;
+
+                        case CDN_SELCHANGE:
+                            dialog->MSWOnSelChange((WXHWND)hDlg);
+                            break;
+                    }
                 }
             }
             break;

commit 37ba576b4a302e835f2144281a3f6cbb2578a12f

Fixed wxFileDialog::MSWOnInitDone and wxFileDialog::MSWOnSelChange never being called.
A condition was checking for the wrong CDN_XXX message range resulting in CDN_INITDONE and CDN_SELCHANGE no longer being processed. Broken since r75937 (branches/WX_3_0_BRANCH/) and r75941 (trunk/).

See #16003.


git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76394 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/filedlg.cpp
@@ -173,7 +173,7 @@ wxFileDialogHookFunction(HWND      hDlg,
         case WM_NOTIFY:
             {
                 NMHDR* const pNM = reinterpret_cast<NMHDR*>(lParam);
-                if ( pNM->code >= CDN_FIRST && pNM->code < CDN_LAST )
+                if ( pNM->code > CDN_LAST && pNM->code <= CDN_FIRST )
                 {
                     OFNOTIFY* const
                         pNotifyCode = reinterpret_cast<OFNOTIFY *>(lParam);