705f6c1c8bd01d81af4bfcaaf82e332dada699db
Stefan Csomor <csomor@advancedconcepts.ch>
2014-05-13 19:55:12 +0000

    allowing compile under ios, where menus are not available
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76521 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/osx/srchctrl.h
+++ include/wx/osx/srchctrl.h
@@ -39,10 +39,12 @@ public:
                 const wxValidator& validator = wxDefaultValidator,
                 const wxString& name = wxSearchCtrlNameStr);
 
+#if wxUSE_MENUS
     // get/set search button menu
     // --------------------------
     virtual void SetMenu( wxMenu* menu );
     virtual wxMenu* GetMenu();
+#endif  // wxUSE_MENUS
 
     // get/set search options
     // ----------------------
@@ -67,7 +69,9 @@ protected:
 
     void Init();
 
+#if wxUSE_MENUS
     wxMenu *m_menu;
+#endif  // wxUSE_MENUS
 
     wxString m_descriptiveText;
 
