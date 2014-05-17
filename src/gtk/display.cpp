0d54910b43f734763c32b28d4d6d93161c1fee7d
Vadim Zeitlin <vadim@wxwidgets.org>
2014-05-17 12:29:54 +0000

    Don't use X11-only GDK functions in wxGTK/Win32 build.
    Add GDK_WINDOWING_X11 checks to allow compiling wxGTK display code under
    Win32.
    
    Closes #16270.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76561 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/display.cpp
+++ src/gtk/display.cpp
@@ -26,7 +26,9 @@ GdkWindow* wxGetTopLevelGDK();
 
 #if !(wxUSE_LIBHILDON || wxUSE_LIBHILDON2)
 
+#ifdef GDK_WINDOWING_X11
 void wxGetWorkAreaX11(Screen* screen, int& x, int& y, int& width, int& height);
+#endif
 
 #ifndef __WXGTK3__
 static inline int wx_gdk_screen_get_primary_monitor(GdkScreen* screen)
@@ -175,10 +177,12 @@ bool wxDisplayImplGTK::IsPrimary() const
     return gdk_screen_get_primary_monitor(m_screen) == int(m_index);
 }
 
+#ifdef GDK_WINDOWING_X11
 wxArrayVideoModes wxXF86VidMode_GetModes(const wxVideoMode& mode, Display* pDisplay, int nScreen);
 wxVideoMode wxXF86VidMode_GetCurrentMode(Display* display, int nScreen);
 bool wxXF86VidMode_ChangeMode(const wxVideoMode& mode, Display* display, int nScreen);
 wxArrayVideoModes wxX11_GetModes(const wxDisplayImpl* impl, const wxVideoMode& modeMatch, Display* display);
+#endif
 
 wxArrayVideoModes wxDisplayImplGTK::GetModes(const wxVideoMode& mode) const
 {
@@ -196,7 +200,9 @@ wxArrayVideoModes wxDisplayImplGTK::GetModes(const wxVideoMode& mode) const
         modes = wxX11_GetModes(this, mode, display);
 #endif
     }
-#endif // GDK_WINDOWING_X11
+#else
+    wxUnusedVar(mode);
+#endif
     return modes;
 }
 
