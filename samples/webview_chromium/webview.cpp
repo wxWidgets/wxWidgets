a96ab5e0d5bcf85fba1a5f06f8a42f448548919e
Haojian Wu <hokein.wu@gmail.com>
2014-06-13 01:38:02 -0700

    Fix argument inconsistency issue.

--- samples/webview_chromium/webview.cpp
+++ samples/webview_chromium/webview.cpp
@@ -768,7 +768,7 @@ void WebFrame::OnFindText(wxCommandEvent& evt)
     {
         count++;
     }
-    wxLogMessage("Searching for:%s  current match:%i/%i", m_findText.c_str(), count, m_findCount);
+    wxLogMessage("Searching for:%s  current match:%ld/%d", m_findText.c_str(), count, m_findCount);
 }
 
 /**
