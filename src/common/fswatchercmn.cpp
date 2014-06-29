6446625f9651764bd15740218cea58deba91db36
Vadim Zeitlin <vadim@wxwidgets.org>
2014-06-29 23:39:09 +0000

    Fix harmless unused parameters warnings in builds without wxLogTrace().
    Fix warnings appearing when building with --disable-debug under Unix by
    either referencing the parameters or variables which become unused then,
    because wxLogTrace() calls are compiled out, or by not compiling the code
    whose only purpose is to call wxLogTrace() at all.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76786 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/fswatchercmn.cpp
+++ src/common/fswatchercmn.cpp
@@ -144,9 +144,12 @@ wxFileSystemWatcherBase::AddAny(const wxFileName& path,
     else
     {
         wxFSWatchInfo& watch2 = it->second;
-        int count = watch2.IncRef();
+        const int count = watch2.IncRef();
+
         wxLogTrace(wxTRACE_FSWATCHER,
                    "'%s' is now watched %d times", canonical, count);
+
+        wxUnusedVar(count); // could be unused if debug tracing is disabled
     }
     return true;
 }
