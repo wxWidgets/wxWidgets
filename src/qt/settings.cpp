0ec7524902d92625a564881a447b74f9aa4bd707
Mariano Reingart <reingart@gmail.com>
2014-07-08 15:36:45 -0300

    Implement native system settings GetColour in wxQT
    This was causing incorrect background in paint events and other drawing issues.

--- src/qt/settings.cpp
+++ src/qt/settings.cpp
@@ -9,10 +9,128 @@
 #include "wx/wxprec.h"
 
 #include "wx/settings.h"
+#include "wx/qt/converter.h"
+#include <QtGui/QPalette>
 
 wxColour wxSystemSettingsNative::GetColour(wxSystemColour index)
 {
-    return wxColour();
+    QPalette pal = qApp->palette();
+    QColor color;
+    switch (index)
+    {
+        case wxSYS_COLOUR_SCROLLBAR:
+        case wxSYS_COLOUR_BACKGROUND:
+        //case wxSYS_COLOUR_DESKTOP:
+        case wxSYS_COLOUR_INACTIVECAPTION:
+        case wxSYS_COLOUR_MENU:
+        case wxSYS_COLOUR_WINDOWFRAME:
+        case wxSYS_COLOUR_ACTIVEBORDER:
+        case wxSYS_COLOUR_INACTIVEBORDER:
+            color = pal.color(QPalette::Window);
+            break;
+
+        //case wxSYS_COLOUR_3DFACE:
+        case wxSYS_COLOUR_3DLIGHT:
+            color = pal.color(QPalette::Light);
+            break;
+
+        case wxSYS_COLOUR_BTNFACE:
+            color = pal.color(QPalette::Button);
+            break;
+
+        case wxSYS_COLOUR_WINDOW:
+            color = pal.color(QPalette::Base);
+            break;
+
+        case wxSYS_COLOUR_MENUBAR:
+            color = pal.window().color();
+            break;
+
+        case wxSYS_COLOUR_3DDKSHADOW:
+            color = pal.color(QPalette::Shadow);
+            break;
+
+        case wxSYS_COLOUR_BTNSHADOW:
+            color = pal.color(QPalette::Dark);
+            break;
+
+        case wxSYS_COLOUR_GRAYTEXT:
+        //case wxSYS_COLOUR_3DSHADOW:
+            color = pal.color(QPalette::Disabled, QPalette::Text);
+            break;
+
+        case wxSYS_COLOUR_BTNHIGHLIGHT:
+        //case wxSYS_COLOUR_BTNHILIGHT:
+        //case wxSYS_COLOUR_3DHIGHLIGHT:
+        //case wxSYS_COLOUR_3DHILIGHT:
+            color = pal.color(QPalette::Light);
+            break;
+
+        case wxSYS_COLOUR_HIGHLIGHT:
+            color = pal.highlight().color();
+            break;
+
+        case wxSYS_COLOUR_LISTBOX:
+            color = pal.base().color();
+            break;
+
+        case wxSYS_COLOUR_LISTBOXTEXT:
+            color = pal.color(QPalette::WindowText);
+            break;
+
+        case wxSYS_COLOUR_LISTBOXHIGHLIGHTTEXT:
+            // This is for the text in a list control (or tree) when the
+            // item is selected, but not focused
+            color = pal.color(QPalette::HighlightedText);
+            break;
+
+        case wxSYS_COLOUR_WINDOWTEXT:
+            color = pal.color(QPalette::Text);
+            break;
+
+        case wxSYS_COLOUR_MENUTEXT:
+        case wxSYS_COLOUR_CAPTIONTEXT:
+        case wxSYS_COLOUR_INACTIVECAPTIONTEXT:
+        case wxSYS_COLOUR_BTNTEXT:
+            color = pal.color(QPalette::ButtonText);
+            break;
+
+        case wxSYS_COLOUR_INFOBK:
+            color = pal.color(QPalette::ToolTipBase);
+            break;
+
+        case wxSYS_COLOUR_INFOTEXT:
+            color = pal.color(QPalette::ToolTipText);
+            break;
+
+        case wxSYS_COLOUR_HIGHLIGHTTEXT:
+            color = pal.color(QPalette::BrightText);
+            break;
+
+        case wxSYS_COLOUR_APPWORKSPACE:
+            color = QColor(Qt::white);    // ?
+            break;
+
+        case wxSYS_COLOUR_ACTIVECAPTION:
+        case wxSYS_COLOUR_MENUHILIGHT:
+            color = pal.color(QPalette::Highlight);
+            break;
+
+        case wxSYS_COLOUR_HOTLIGHT:
+        case wxSYS_COLOUR_GRADIENTACTIVECAPTION:
+        case wxSYS_COLOUR_GRADIENTINACTIVECAPTION:
+            // TODO
+            color = QColor(Qt::black);
+            break;
+
+        case wxSYS_COLOUR_MAX:
+        default:
+            wxFAIL_MSG( wxT("unknown system colour index") );
+            color = QColor(Qt::white);
+            break;
+    }
+
+    return wxColor( color.red(), color.green(), color.blue(), color.alpha() );
 }
 
 wxFont wxSystemSettingsNative::GetFont(wxSystemFont index)
