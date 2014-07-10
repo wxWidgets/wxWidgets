wxWidgets/wxWidgets

commit def3b2c90fb7513f7847f492bf5a9894b43e697c

Items appended via the menu have no item data. Don't crash if right-clicking these items.
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76813 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

samples/treectrl/treetest.cpp
@@ -1601,7 +1601,7 @@ void MyTreeCtrl::OnItemMenu(wxTreeEvent& event)
     wxPoint screenpt = ClientToScreen(clientpt);
 
     wxLogMessage(wxT("OnItemMenu for item \"%s\" at screen coords (%i, %i)"),
-                 item->GetDesc(), screenpt.x, screenpt.y);
+                 item ? item->GetDesc() : wxS("unknown"), screenpt.x, screenpt.y);
 
     ShowMenu(itemId, clientpt);
     event.Skip();
@@ -1646,7 +1646,7 @@ void MyTreeCtrl::OnItemRClick(wxTreeEvent& event)
 
     MyTreeItemData *item = (MyTreeItemData *)GetItemData(itemId);
 
-    wxLogMessage(wxT("Item \"%s\" right clicked"), item->GetDesc());
+    wxLogMessage(wxT("Item \"%s\" right clicked"), item ? item->GetDesc() : wxS("unknown"));
 
     event.Skip();
 }

commit 116112bcf5e7d2d5ba517bcfe5553c9ba5ffd49c

Fix a compile error with some compilers introduced in r76813.
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76883 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

samples/treectrl/treetest.cpp
@@ -1601,7 +1601,7 @@ void MyTreeCtrl::OnItemMenu(wxTreeEvent& event)
     wxPoint screenpt = ClientToScreen(clientpt);
 
     wxLogMessage(wxT("OnItemMenu for item \"%s\" at screen coords (%i, %i)"),
-                 item ? item->GetDesc() : wxS("unknown"), screenpt.x, screenpt.y);
+                 item ? item->GetDesc() : wxString(wxS("unknown")), screenpt.x, screenpt.y);
 
     ShowMenu(itemId, clientpt);
     event.Skip();
@@ -1646,7 +1646,7 @@ void MyTreeCtrl::OnItemRClick(wxTreeEvent& event)
 
     MyTreeItemData *item = (MyTreeItemData *)GetItemData(itemId);
 
-    wxLogMessage(wxT("Item \"%s\" right clicked"), item ? item->GetDesc() : wxS("unknown"));
+    wxLogMessage(wxT("Item \"%s\" right clicked"), item ? item->GetDesc() : wxString(wxS("unknown")));
 
     event.Skip();
 }

samples/treectrl/treetest.h
@@ -45,7 +45,7 @@ public:
     MyTreeItemData(const wxString& desc) : m_desc(desc) { }
 
     void ShowInfo(wxTreeCtrl *tree);
-    const wxChar *GetDesc() const { return m_desc.c_str(); }
+    wxString const& GetDesc() const { return m_desc; }
 
 private:
     wxString m_desc;