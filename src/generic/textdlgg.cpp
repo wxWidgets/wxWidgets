6aeaa3592065ee21b077773b9bd4a59fd434b89c
Mariano Reingart <reingart@gmail.com>
2014-08-11 19:23:00 -0300

    Revert generic text dialog due wxQT merge changes
     * This solves the issues of the previos commit about the extern defaults wxGetTextFromUserPromptStr / wxGetTextFromUserPromptStr
     * Removed the now superflous textdlg.cpp that was used to define that defaults

--- src/generic/textdlgg.cpp
+++ src/generic/textdlgg.cpp
@@ -41,6 +41,9 @@
     #include "wx/statline.h"
 #endif
 
+const char wxGetTextFromUserPromptStr[] = "Input Text";
+const char wxGetPasswordFromUserPromptStr[] = "Enter Password";
+
 // ----------------------------------------------------------------------------
 // constants
 // ----------------------------------------------------------------------------
