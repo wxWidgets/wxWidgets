f360423eed9c5ee75cbb53c8ea43b921376de1eb
Vadim Zeitlin <vadim@wxwidgets.org>
2014-08-23 01:08:32 +0000

    Revert generic text dialog due wxQT merge changes
     * This solves the issues of the previos commit about the extern defaults wxGetTextFromUserPromptStr / wxGetTextFromUserPromptStr
     * Removed the now superflous textdlg.cpp that was used to define that defaults
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/wxQT@77440 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/textdlg.h
+++ include/wx/textdlg.h
@@ -11,35 +11,7 @@
 #ifndef _WX_TEXTDLG_H_BASE_
 #define _WX_TEXTDLG_H_BASE_
 
-#include "wx/defs.h"
-
-extern WXDLLIMPEXP_DATA_CORE(const char) wxGetTextFromUserPromptStr[];
-extern WXDLLIMPEXP_DATA_CORE(const char) wxGetPasswordFromUserPromptStr[];
-
-#define wxTextEntryDialogStyle (wxOK | wxCANCEL | wxCENTRE | wxWS_EX_VALIDATE_RECURSIVELY)
-
 #include "wx/generic/textdlgg.h"
 
-// ----------------------------------------------------------------------------
-// function to get a string from user
-// ----------------------------------------------------------------------------
-
-WXDLLIMPEXP_CORE wxString
-    wxGetTextFromUser(const wxString& message,
-                    const wxString& caption = wxGetTextFromUserPromptStr,
-                    const wxString& default_value = wxEmptyString,
-                    wxWindow *parent = NULL,
-                    wxCoord x = wxDefaultCoord,
-                    wxCoord y = wxDefaultCoord,
-                    bool centre = true);
-
-WXDLLIMPEXP_CORE wxString
-    wxGetPasswordFromUser(const wxString& message,
-                        const wxString& caption = wxGetPasswordFromUserPromptStr,
-                        const wxString& default_value = wxEmptyString,
-                        wxWindow *parent = NULL,
-                        wxCoord x = wxDefaultCoord,
-                        wxCoord y = wxDefaultCoord,
-                        bool centre = true);
-
 #endif // _WX_TEXTDLG_H_BASE_
+
