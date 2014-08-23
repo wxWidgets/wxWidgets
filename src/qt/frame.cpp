2e124f8387abfbf0658b357d789149bacfb148e3
Vadim Zeitlin <vadim@wxwidgets.org>
2014-08-23 01:09:15 +0000

    Add MiniFrame TLW for wxQT
    Also enables support for frameless and no task bar styles.
    Note that tool window style seems to have no effect on Ubuntu (tested with the Qt official examples)
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/wxQT@77449 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/qt/frame.cpp
+++ src/qt/frame.cpp
@@ -126,25 +126,23 @@ void wxFrame::SetWindowStyleFlag( long style )
     wxWindow::SetWindowStyleFlag( style );
 
     Qt::WindowFlags qtFlags = GetHandle()->windowFlags();
-//
-//    wxCHECK_RET( !HasFlag( wxFRAME_FLOAT_ON_PARENT ) && !HasFlag( wxTINY_CAPTION ) ,
-//                 "wxFRAME_FLOAT_ON_PARENT, wxTINY_CAPTION not supported. Use wxFRAME_TOOL_WINDOW instead." );
-//    wxCHECK_RET( !HasFlag( wxFRAME_TOOL_WINDOW ) || HasFlag( wxFRAME_NO_TASKBAR ) ,
-//                 "wxFRAME_TOOL_WINDOW without wxFRAME_NO_TASKBAR not supported." );
-//
-//    if ( HasFlag( wxFRAME_TOOL_WINDOW ) )
-//    {
-//        qtFlags &= ~Qt::WindowType_Mask;
-//        qtFlags = Qt::Tool;
-//    }
-//    else if ( HasFlag( wxFRAME_NO_TASKBAR ) )
-//    {
-//        qtFlags &= ~Qt::WindowType_Mask;
-//        qtFlags = Qt::Dialog;
-//    }
-//
-    
 
+    wxCHECK_RET( !HasFlag( wxFRAME_FLOAT_ON_PARENT ) && !HasFlag( wxTINY_CAPTION ) ,
+                 "wxFRAME_FLOAT_ON_PARENT, wxTINY_CAPTION not supported. Use wxFRAME_TOOL_WINDOW instead." );
+    wxCHECK_RET( !HasFlag( wxFRAME_TOOL_WINDOW ) || HasFlag( wxFRAME_NO_TASKBAR ) ,
+                 "wxFRAME_TOOL_WINDOW without wxFRAME_NO_TASKBAR not supported." );
+
+    if ( HasFlag( wxFRAME_TOOL_WINDOW ) )
+    {
+        qtFlags &= ~Qt::WindowType_Mask;
+        qtFlags |= Qt::Tool;
+    }
+    else if ( HasFlag( wxFRAME_NO_TASKBAR ) )
+    {
+        qtFlags &= ~Qt::WindowType_Mask;
+        qtFlags |= Qt::Dialog;
+    }
+    
     if ( ( (style & wxSIMPLE_BORDER) || (style & wxNO_BORDER) )
          != qtFlags.testFlag( Qt::FramelessWindowHint ) )
     {
---
+++ src/qt/minifram.cpp
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        src/qt/minifram.cpp
+// Purpose:     wxMiniFrame
+// Author:      Mariano Reingart
+// Copyright:   (c) 2014 wxWidgets dev team
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// For compilers that support precompilation, includes "wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#if wxUSE_MINIFRAME
+
+#include "wx/minifram.h"
+
+IMPLEMENT_DYNAMIC_CLASS(wxMiniFrame, wxFrame)
+
+#endif // wxUSE_MINIFRAME
