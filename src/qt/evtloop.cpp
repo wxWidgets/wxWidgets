bd8dc3d315c2b265b21b56ca1e8c34f4f0b9cf0b
Vadim Zeitlin <vadim@wxwidgets.org>
2014-08-23 01:06:31 +0000

    Provisional patch to support nested event loops in wxQT
    Without the patch, the wxExecute (wxAppTraits::RunLoopUntilChildExit) loop.Run() don't process synchonics events (not entering the new loop), so it cannot process exit code and streams (causing a SIGSEGV crash).
    The problem is that QCoreApplication::exec() seems to not start a new loop, maybe it should create a real new loop with QEventLoop but then the inner loop don't seems to process events/signals for the outer one (app).
    Provisionaly, to get wxExecute working, it calls processEvents() on the main app loop until wxGUIEventLoop::ScheduleExit() is called, terminating the inner loop.
    This seems to work for the exec sample, but exec tests keep crashing, will investigate further.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/wxQT@77410 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/qt/evtloop.cpp
+++ src/qt/evtloop.cpp
@@ -71,13 +71,27 @@ wxQtEventLoopBase::~wxQtEventLoopBase()
 void wxQtEventLoopBase::ScheduleExit(int rc)
 {
     wxCHECK_RET( IsInsideRun(), wxT("can't call ScheduleExit() if not started") );
-    
+    m_shouldExit = true;
     QCoreApplication::exit( rc );
 }
 
 int wxQtEventLoopBase::DoRun()
 {
-    return QCoreApplication::exec();
+    int ret;
+
+    // This is placed inside of a loop to take into account nested event loops
+    while ( !m_shouldExit )
+    {
+        // This will print Qt warnins if app already started:
+        // "QCoreApplication::exec: The event loop is already running"
+        // TODO: check the loopLevel (nested) like in wxGTK
+        ret = QCoreApplication::exec();
+        // process pending events (if exec was started previously)
+        // TODO: use a real new QEventLoop() ?
+        QCoreApplication::processEvents();
+    }
+    OnExit();
+    return ret;
 }
 
 bool wxQtEventLoopBase::Pending() const
