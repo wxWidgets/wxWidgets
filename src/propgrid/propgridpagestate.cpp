14432092198e917eac2b5ae7172b71c568f17669
Artur Wieczorek <artwik@wp.pl>
2014-08-26 22:54:01 +0000

    Fix deleting aggregate properties from within wxPG event handler.
    Do not explicitly remove sub-properties of aggregate property because they will be removed automatically in their parent property dtor.
    
    See #16459
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77485 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/propgrid/propgridpagestate.cpp
+++ src/propgrid/propgridpagestate.cpp
@@ -277,7 +277,7 @@ void wxPropertyGridPageState::InitNonCatMode()
 
 void wxPropertyGridPageState::DoClear()
 {
-    if ( m_pPropGrid && m_pPropGrid->GetState() == this  )
+    if ( m_pPropGrid && m_pPropGrid->GetState() == this )
     {
         m_pPropGrid->ClearSelection(false);
     }
@@ -295,7 +295,11 @@ void wxPropertyGridPageState::DoClear()
               !it.AtEnd();
               it++ )
         {
-            DoDelete(*it, true);
+            wxPGProperty *p = *it;
+            // Do not attempt to explicitly remove sub-properties.
+            // They will be removed in their parent property dtor.
+            if ( !p->GetParent()->HasFlag(wxPG_PROP_AGGREGATE) )
+                DoDelete(p, true);
         }
     }
     else
