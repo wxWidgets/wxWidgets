2f98230bd80b158897f59f87c1b74a355ba1660e
Bryan Petty <bryan@ibaku.net>
2014-09-10 14:58:31 +0000

    Update docs of taskbar button and wxAppProgressIndicator.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77643 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/taskbarbutton.h
+++ interface/wx/taskbarbutton.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        taskbarbutton.h
-// Purpose:     interface of wxTaskBarButton
+// Purpose:     Interface of taskbar buttons features.
 // Author:      wxWidgets team
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
@@ -329,53 +329,6 @@ public:
             button.
     */
     virtual wxThumbBarButton* RemoveThumbBarButton(int id);
-};
-
-/**
-    @class wxAppProgressIndicator
-
-    A helper class that can be used to update the progress bar in the taskbar button.
-
-    @library{wxcore}
-    @category{misc}
-
-    @onlyfor{wxmsw}
-
-    @see wxTaskBarButton
-    @since 3.1.0
-*/
-class WXDLLIMPEXP_CORE wxAppProgressIndicator
-{
-public:
-    /**
-        Constructs the wxAppProgressIndicator.
-
-        @param parent
-            The parent window of wxAppProgressIndicator. Note that the
-            window should has taskbar button showing.
-        @param maxValue
-    */
-    wxAppProgressIndicator(wxWindow* parent, int maxValue);
-    virtual ~wxAppProgressIndicator();
-
-    /**
-        Set the progress value in taskbar button of parent window.
-
-        @param value
-            The new value of the progress meter. It should be less than or equal
-            to the range.
-     */
-    void SetValue(int value);
-
-    /**
-        Set the progress range in taskbar button of parent window.
-     */
-    void SetRange(int range);
-
-    /**
-        Makes the progress bar run in indeterminate mode.
-    */
-    bool Pulse();
 };
 
 /**
