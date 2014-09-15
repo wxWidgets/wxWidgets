2a5d51c08f7da41803a1630170afb14e97e37b39
Vadim Zeitlin <vadim@wxwidgets.org>
2014-09-15 16:01:52 +0000

    Get rid of IsOk() accessor in Cairo implementation code.
    This was quite useless as m_ok flag was public anyhow, just use it directly.
    
    Incidentally closes #16560.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77709 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/cairo.cpp
+++ src/common/cairo.cpp
@@ -250,8 +250,6 @@ private:
     wxCairo();
     ~wxCairo();
 
-    bool IsOk();
-
     wxDynamicLibrary m_libCairo;
     wxDynamicLibrary m_libPangoCairo;
 
@@ -341,7 +339,7 @@ wxCairo::~wxCairo()
     if ( !ms_lib )
     {
         ms_lib = new wxCairo();
-        if ( !ms_lib->IsOk() )
+        if ( !ms_lib->m_ok )
         {
             delete ms_lib;
             ms_lib = NULL;
@@ -358,11 +356,6 @@ wxCairo::~wxCairo()
         delete ms_lib;
         ms_lib = NULL;
     }
-}
-
-bool wxCairo::IsOk()
-{
-    return m_ok;
 }
 
 // ============================================================================
