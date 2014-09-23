c24ded647e25ceb9d1cbdcb1c2a1643a292c666f
Vadim Zeitlin <vadim@wxwidgets.org>
2014-09-23 17:39:08 +0000

    in wxGTK, wxOSX/Cocoa, plus wxUniv, GetNumberOfLines() always returns the number of logical, not physical, lines. See #12366, maybe they did not test wxUniv at that time.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77766 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/controls/textctrltest.cpp
+++ tests/controls/textctrltest.cpp
@@ -500,8 +500,9 @@ void TextCtrlTestCase::Lines()
     // This currently doesn't work neither in wxGTK nor wxOSX/Cocoa, see
     // #12366, where GetNumberOfLines() always returns the number of logical,
     // not physical, lines.
+    // Update 5,23, 2014, same situation in wxUniv.
     m_text->AppendText("\n" + wxString(50, '1') + ' ' + wxString(50, '2'));
-#if defined(__WXGTK__) || defined(__WXOSX_COCOA__)
+#if defined(__WXGTK__) || defined(__WXOSX_COCOA__) || defined(__WXUNIVERSAL__)
     CPPUNIT_ASSERT_EQUAL(6, m_text->GetNumberOfLines());
 #else
     CPPUNIT_ASSERT_EQUAL(7, m_text->GetNumberOfLines());
