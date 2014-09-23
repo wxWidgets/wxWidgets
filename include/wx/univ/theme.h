d1d874dd15052cf296882fb6ec8dfefc8e077034
Vadim Zeitlin <vadim@wxwidgets.org>
2014-09-23 17:39:06 +0000

    wxUniv focus on wx11 port, so modified unv/theme.h, make GTK theme as the default theme
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77765 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/univ/theme.h
+++ include/wx/univ/theme.h
@@ -184,10 +184,10 @@ struct WXDLLIMPEXP_CORE wxThemeInfo
 // if no theme was picked, get any theme compiled in (sorted by
 // quality/completeness of the theme):
 #ifndef wxUNIV_DEFAULT_THEME
-    #if wxUSE_THEME_WIN32
-        #define wxUNIV_DEFAULT_THEME win32
-    #elif wxUSE_THEME_GTK
+    #if wxUSE_THEME_GTK
         #define wxUNIV_DEFAULT_THEME gtk
+    #elif wxUSE_THEME_WIN32
+        #define wxUNIV_DEFAULT_THEME win32
     #elif wxUSE_THEME_MONO
         #define wxUNIV_DEFAULT_THEME mono
     #endif
