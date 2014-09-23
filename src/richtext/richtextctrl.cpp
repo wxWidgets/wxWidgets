07a471df832ad1489a557eaa0f629d0870146ff8
Vadim Zeitlin <vadim@wxwidgets.org>
2014-09-23 17:39:56 +0000

    wxUniv not support unicode yet
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77780 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextctrl.cpp
+++ src/richtext/richtextctrl.cpp
@@ -1413,9 +1413,9 @@ void wxRichTextCtrl::OnChar(wxKeyEvent& event)
                     GetId());
                 cmdEvent1.SetEventObject(this);
                 cmdEvent1.SetFlags(flags);
-#if wxUSE_UNICODE
+#if wxUSE_UNICODE || !__WXUNIVERSAL__
                 cmdEvent1.SetCharacter(event.GetUnicodeKey());
-#else
+#elif !wxUSE_UNICODE || __WXUNIVERSAL__
                 cmdEvent1.SetCharacter((wxChar) keycode);
 #endif
                 cmdEvent1.SetPosition(m_caretPosition+1);
@@ -1430,9 +1430,9 @@ void wxRichTextCtrl::OnChar(wxKeyEvent& event)
                     GetId());
                 cmdEvent.SetEventObject(this);
                 cmdEvent.SetFlags(flags);
-#if wxUSE_UNICODE
+#if wxUSE_UNICODE || !__WXUNIVERSAL__
                 cmdEvent.SetCharacter(event.GetUnicodeKey());
-#else
+#elif !wxUSE_UNICODE || __WXUNIVERSAL__
                 cmdEvent.SetCharacter((wxChar) keycode);
 #endif
                 cmdEvent.SetPosition(m_caretPosition+1);
@@ -1473,9 +1473,9 @@ void wxRichTextCtrl::OnChar(wxKeyEvent& event)
                 long newPos = m_caretPosition;
                 DeleteSelectedContent(& newPos);
 
-#if wxUSE_UNICODE
+#if wxUSE_UNICODE || !__WXUNIVERSAL__
                 wxString str = event.GetUnicodeKey();
-#else
+#elif !wxUSE_UNICODE || __WXUNIVERSAL__
                 wxString str = (wxChar) event.GetKeyCode();
 #endif
                 GetFocusObject()->InsertTextWithUndo(& GetBuffer(), newPos+1, str, this, 0);
