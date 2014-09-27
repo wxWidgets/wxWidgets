42fdb98f74b374d3298dd2cf25ac2b35d8f01282
Vadim Zeitlin <vadim@wxwidgets.org>
2014-09-27 16:12:40 +0000

    Don't prevent the other button release handlers from running in wxGTK.
    There doesn't seem to be any advantage in doing this, but it can result in
    problems see #16055. So just always let the other handlers for this event
    execute as well, even if we did process it.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77892 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/window.cpp
+++ src/gtk/window.cpp
@@ -1596,11 +1596,15 @@ gtk_window_button_release_callback( GtkWidget *WXUNUSED(widget),
     event.SetEventObject( win );
     event.SetId( win->GetId() );
 
-    bool ret = win->GTKProcessEvent(event);
+    // We ignore the result of the event processing here as we don't really
+    // want to prevent the other handlers from running even if we did process
+    // this event ourselves, there is no real advantage in doing this and it
+    // could actually be harmful, see #16055.
+    (void)win->GTKProcessEvent(event);
 
     g_lastMouseEvent = NULL;
 
-    return ret;
+    return FALSE;
 }
 
 //-----------------------------------------------------------------------------
