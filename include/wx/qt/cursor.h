b56a3f52351c5f36333c7ecd8f4f6939a612e70d
Mariano Reingart <reingart@gmail.com>
2014-09-29 02:55:33 +0000

    Improve wxQT cursor implementation, thanks @seandepagnier
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77911 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/qt/cursor.h
+++ include/wx/qt/cursor.h
@@ -19,13 +19,15 @@ class WXDLLIMPEXP_CORE wxCursor : public wxGDIObject
 public:
     wxCursor() { }
     wxCursor( const wxCursor & );
-    wxCursor( const wxImage& image ) { InitFromImage(image); }
-    wxCursor( const wxString& name,
-              wxBitmapType type = wxCURSOR_DEFAULT_TYPE,
-              int hotSpotX = 0, int hotSpotY = 0 );
     wxCursor(wxStockCursor id) { InitFromStock(id); }
 #if WXWIN_COMPATIBILITY_2_8
     wxCursor(int id) { InitFromStock((wxStockCursor)id); }
+#endif
+#if wxUSE_IMAGE
+    wxCursor( const wxImage & image );
+    wxCursor(const wxString& name,
+             wxBitmapType type = wxCURSOR_DEFAULT_TYPE,
+             int hotSpotX = 0, int hotSpotY = 0);
 #endif
 
     QCursor m_qtCursor;
