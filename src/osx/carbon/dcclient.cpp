a2d8746ba7d1c26539a27e4c9e6ec514bf7bc9f7
Vadim Zeitlin <vadim@wxwidgets.org>
2015-01-08 14:11:05 +0000

    only flush a client dc if it was not inheriting the native CGContextRef from an outside paint context, fixes #16334
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_3_0_BRANCH@78356 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/osx/carbon/dcclient.cpp
+++ src/osx/carbon/dcclient.cpp
@@ -174,7 +174,7 @@ wxClientDCImpl::wxClientDCImpl( wxDC *owner, wxWindow *window ) :
 
 wxClientDCImpl::~wxClientDCImpl()
 {
-    if( GetGraphicsContext() && GetGraphicsContext()->GetNativeContext() )
+    if( GetGraphicsContext() && GetGraphicsContext()->GetNativeContext() && !m_release )
         Flush();
 }
 
