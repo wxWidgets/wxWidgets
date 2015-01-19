eab495c496c2ed4983ac328cd864fc9097752fd9
Mariano Reingart <reingart@gmail.com>
2015-01-19 02:39:52 +0000

    wxQt: fix performance issue with font comparisons
    Thanks @seandepagnier
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78381 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/fontcmn.cpp
+++ src/common/fontcmn.cpp
@@ -389,7 +389,8 @@ bool wxFontBase::operator==(const wxFont& font) const
             // in wxGTK1 GetPixelSize() calls GetInternalFont() which uses
             // operator==() resulting in infinite recursion so we can't use it
             // in that port
-#if !defined(__WXGTK__) || defined(__WXGTK20__)
+            // in wxQT, GetPixelSize is too slow to be used here
+#if (!defined(__WXGTK__) || defined(__WXGTK20__)) && !defined(__WXQT__)
             GetPixelSize() == font.GetPixelSize() &&
 #endif
             GetFamily() == font.GetFamily() &&
