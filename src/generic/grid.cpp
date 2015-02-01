ecd32f0583dc68317106cebe4d5f6a8d830919e5
Paul Cornett <paulcor@bullseye.com>
2015-02-01 18:06:06 +0000

    avoid passing invalid rect to Refresh(), closes #16819
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78431 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -2102,7 +2102,8 @@ void wxGridWindow::OnFocus(wxFocusEvent& event)
                                             m_owner->GetGridCursorCol());
         const wxRect cursor =
             m_owner->BlockToDeviceRect(cursorCoords, cursorCoords);
-        Refresh(true, &cursor);
+        if (cursor != wxGridNoCellRect)
+            Refresh(true, &cursor);
     }
 
     if ( !m_owner->GetEventHandler()->ProcessEvent( event ) )
