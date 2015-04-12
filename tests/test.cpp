wxWidgets/wxWidgets

commit 3e67b1ef6801f146c0d7463cab316bf8053469f2

Fix the unit test suite exit code in case of failure.
Return the program exit code from the (overridden) OnRun(), not OnExit(),
doing it from the latter didn't work since many years.

The failures in the GUI tests will be detected by buildbot now.

tests/test.cpp
@@ -336,13 +336,15 @@ public:
 
         event.Skip();
     }
-#else // !wxUSE_GUI
+
     virtual int OnRun()
     {
-        m_exitcode = RunTests();
+        if ( TestAppBase::OnRun() != 0 )
+            m_exitcode = EXIT_FAILURE;
+
         return m_exitcode;
     }
-#endif // wxUSE_GUI/!wxUSE_GUI
+#endif // wxUSE_GUI
 
 private:
     void List(Test *test, const string& parent = "") const;
@@ -715,7 +717,7 @@ int TestApp::OnExit()
     delete GetTopWindow();
 #endif // wxUSE_GUI
 
-    return m_exitcode;
+    return TestAppBase::OnExit();
 }
 
 // List the tests

commit 2d7875c9268f61aee195cac60f86db5a04681fc9

Restore OnRun() in the non-GUI unit test suite.
Commit 3e67b1ef6801f146c0d7463cab316bf8053469f2 broke the test suite in
non-GUI case, we still need to override OnRun() then to run the tests.

Also make it more clear that m_exitcode is only used in the GUI case.

tests/test.cpp
@@ -344,7 +344,12 @@ public:
 
         return m_exitcode;
     }
-#endif // wxUSE_GUI
+#else // !wxUSE_GUI
+    virtual int OnRun()
+    {
+        return RunTests();
+    }
+#endif // wxUSE_GUI/!wxUSE_GUI
 
 private:
     void List(Test *test, const string& parent = "") const;
@@ -375,8 +380,10 @@ private:
     FilterEventFunc m_filterEventFunc;
     ProcessEventFunc m_processEventFunc;
 
+#if wxUSE_GUI
     // the program exit code
     int m_exitcode;
+#endif // wxUSE_GUI
 };
 
 IMPLEMENT_APP_NO_MAIN(TestApp)
@@ -512,7 +519,9 @@ TestApp::TestApp()
 
     m_locale = NULL;
 
+#if wxUSE_GUI
     m_exitcode = EXIT_SUCCESS;
+#endif // wxUSE_GUI
 }
 
 // Init