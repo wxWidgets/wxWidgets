a1902c1456d7c221c2497e5fb6844f023b21a293
Dimitri Schoolwerth <dimitri@schoolwerth.com>
2015-04-17 14:30:23 +0400

    Fix compilation error in ownerdrw sample.
    A frame's title parameter is using const wxChar * and is passed a const
    char * as argument. Use a wxString as parameter to fix the error.

--- samples/ownerdrw/ownerdrw.cpp
+++ samples/ownerdrw/ownerdrw.cpp
@@ -39,7 +39,7 @@ class OwnerDrawnFrame : public wxFrame
 {
 public:
     // ctor & dtor
-    OwnerDrawnFrame(wxFrame *frame, const wxChar *title, int x, int y, int w, int h);
+    OwnerDrawnFrame(wxFrame *frame, const wxString& title, int x, int y, int w, int h);
     ~OwnerDrawnFrame(){};
 
     // notifications
@@ -255,7 +255,7 @@ void OwnerDrawnFrame::InitMenu()
 }
 
 // main frame constructor
-OwnerDrawnFrame::OwnerDrawnFrame(wxFrame *frame, const wxChar *title,
+OwnerDrawnFrame::OwnerDrawnFrame(wxFrame *frame, const wxString& title,
                                  int x, int y, int w, int h)
          : wxFrame(frame, wxID_ANY, title, wxPoint(x, y), wxSize(w, h))
 {
