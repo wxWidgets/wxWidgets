7145a601b0bc11e12bbbae0a69e26f8abbe97840
Artur Wieczorek <artwik@wp.pl>
2015-05-14 18:47:38 +0200

    Consistently use wxT macro with string literals in propgrid sample.
    Continued unification of string literals in the sample.

--- samples/propgrid/propgrid_minimal.cpp
+++ samples/propgrid/propgrid_minimal.cpp
@@ -59,12 +59,12 @@ void MyFrame::OnPropertyGridChange(wxPropertyGridEvent &event)
 
     if ( p )
     {
-        wxLogVerbose("OnPropertyGridChange(%s, value=%s)",
+        wxLogVerbose(wxT("OnPropertyGridChange(%s, value=%s)"),
                    p->GetName().c_str(), p->GetValueAsString().c_str());
     }
     else
     {
-        wxLogVerbose("OnPropertyGridChange(NULL)");
+        wxLogVerbose(wxT("OnPropertyGridChange(NULL)"));
     }
 }
 
@@ -72,7 +72,7 @@ void MyFrame::OnPropertyGridChanging(wxPropertyGridEvent &event)
 {
     wxPGProperty* p = event.GetProperty();
 
-    wxLogVerbose("OnPropertyGridChanging(%s)", p->GetName().c_str());
+    wxLogVerbose(wxT("OnPropertyGridChanging(%s)"), p->GetName().c_str());
 }
 
 void MyFrame::OnAction(wxCommandEvent &)
