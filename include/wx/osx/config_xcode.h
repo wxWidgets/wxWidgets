ecf85bf4cbe99c773b17e04bc3798e3c235df8e6
Olly Betts <olly@survex.com>
2015-05-22 02:27:27 +0200

    Remove WXWIN_OS_DESCRIPTION.
    This constant wasn't very useful as it contained the description of the OS on
    the machine where the library was built, not the one on which the application
    using it was running. It also wasn't used anywhere in wxWidgets and apparently
    wasn't meant to be used outside of it.
    
    Finally, putting the output of `uname -r` into it created problems with
    creating reproducible builds as just a change in the kernel version changed
    the build results.
    
    Closes #17002.

--- include/wx/osx/config_xcode.h
+++ include/wx/osx/config_xcode.h
@@ -122,7 +122,6 @@
 #define HAVE_LOCALE_T 1
 #define wxHAS_KQUEUE 1
 
-#define WXWIN_OS_DESCRIPTION "Darwin 7.9.0 Power Macintosh"
 #define PACKAGE_BUGREPORT "wx-dev@lists.wxwidgets.org"
 #define PACKAGE_NAME "wxWidgets"
 #define PACKAGE_STRING "wxWidgets 3.1.0"
