5decea57f48d838437e623624b15fe4ef3d35a31
Artur Wieczorek <artwik@wp.pl>
2015-05-31 18:05:53 +0200

    Don't declare wxPropertyGridManager::SetColumnTitle if wxUSE_HEADERCTRL == 0.
    This method already has no definition in this case so there is no reason to declare it.

--- include/wx/propgrid/manager.h
+++ include/wx/propgrid/manager.h
@@ -538,6 +538,7 @@ public:
         return p->GetParentState()->DoSelectProperty(p, focus);
     }
 
+#if wxUSE_HEADERCTRL
     /**
         Sets a column title. Default title for column 0 is "Property",
         and "Value" for column 1.
@@ -546,6 +547,7 @@ public:
                  member function will make it visible.
     */
     void SetColumnTitle( int idx, const wxString& title );
+#endif // wxUSE_HEADERCTRL
 
     /**
         Sets number of columns on given page (default is current page).
