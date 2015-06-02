5aae7c7387caccdb46bf351991e9d1436671704d
Jan van Dijk <jan@epgmod.phys.tue.nl>
2015-06-02 02:07:39 +0200

    Work around clang warning about side effects inside typeid().
    This warning was harmless but very annoying as it was given for each and every
    inclusion of this header.
    
    Closes #16968.

--- include/wx/any.h
+++ include/wx/any.h
@@ -159,7 +159,8 @@ private:
 public: \
     static bool IsSameClass(const wxAnyValueType* otherType) \
     { \
-        return wxTypeId(*sm_instance.get()) == wxTypeId(*otherType); \
+        const wxAnyValueType* const inst = sm_instance.get(); \
+        return wxTypeId(*inst) == wxTypeId(*otherType); \
     } \
     virtual bool IsSameType(const wxAnyValueType* otherType) const \
     { \
