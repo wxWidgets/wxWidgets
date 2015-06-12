wxWidgets/wxWidgets

commit 3a8ad95230a3eace683c1cc07807d1e0d1e4a296

Call SetThreadUILanguage() on Windows
Starting with Vista, SetThreadLocale() does basically nothing and does
not affect UI language. We need to call SetThreadUILanguage() as well
from wxLocale::Init() to have the locale reflected in e.g. standard
dialogs.

src/common/intl.cpp
@@ -472,6 +472,13 @@ bool wxLocale::Init(int language, int flags)
 
             // change locale used by Windows functions
             ::SetThreadLocale(lcid);
+
+            // SetThreadUILanguage() is available on XP, but with unclear
+            // behavior, so avoid calling it there.
+            if ( wxGetWinVersion() >= wxWinVersion_Vista )
+            {
+                SetThreadUILanguage(LANGIDFROMLCID(lcid));
+            }
 #endif
 
             // and also call setlocale() to change locale used by the CRT

commit 09e1fbef70d3ec17fad77b034e631944bc194c05

Fix MinGW and VC8 compilation error
3a8ad952 introduced compilation failures with (some versions of) MinGW
and VC8 because their winnls.h apparently doesn't declare
SetThreadUILanguage(). Just declare the function ourselves to fix it.

src/common/intl.cpp
@@ -50,6 +50,8 @@
 
 #ifdef __WIN32__
     #include "wx/msw/private.h"
+    // Some compilers' winnls.h header doesn't declare this function:
+    extern "C" WINBASEAPI LANGID WINAPI SetThreadUILanguage(LANGID LangId);
 #endif
 
 #include "wx/file.h"