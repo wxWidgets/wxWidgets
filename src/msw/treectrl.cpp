0c1458484294b2fe871aeeb3e3bc25cb2d1cf316
Vadim Zeitlin <vadim@wxwidgets.org>
2015-06-14 15:12:19 +0200

    Fix harmless signed/unsigned comparison warning in wxMSW wxTreeCtrl.
    Don't cast NMHDR::code field to int, this cast was added in the previous
    millennium, probably to work around the wrong definition of NM_DBLCLK in some
    ancient MinGW headers, but nowadays all MinGW distributions (tested with
    MinGW 4.8.1, MinGW-64 4.9.1 and TDM-GCC 4.9.2) define it correctly and so
    using this cast results in a warning -- just remove it to get rid of it.

--- src/msw/treectrl.cpp
+++ src/msw/treectrl.cpp
@@ -3665,7 +3665,7 @@ bool wxTreeCtrl::MSWOnNotify(int idCtrl, WXLPARAM lParam, WXLPARAM *result)
                     if ( MSWIsOnItem(tvhti.flags) )
                     {
                         event.m_item = tvhti.hItem;
-                        eventType = (int)hdr->code == NM_DBLCLK
+                        eventType = hdr->code == NM_DBLCLK
                                     ? wxEVT_TREE_ITEM_ACTIVATED
                                     : wxEVT_TREE_ITEM_RIGHT_CLICK;
 
