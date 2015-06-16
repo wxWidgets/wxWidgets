8fd76acf98f574d8bea3377c976409f19ca5f6e5
Suzumizaki-Kimitaka <szmml@h12u.com>
2015-06-16 10:54:40 +0900

    Restores comments and checking __VISUALC__.

--- include/wx/msw/debughlp.h
+++ include/wx/msw/debughlp.h
@@ -19,24 +19,15 @@
 #endif // __WXWINCE__
 #include "wx/msw/private.h"
 
-// All known versions of imagehlp.h define API_VERSION_NUMBER but it's not
-// documented, so deal with the possibility that it's not defined just in case.
-#ifndef API_VERSION_NUMBER
-    #define API_VERSION_NUMBER 0
-#endif
 
-// wxUSE_DBGHELP is a bit special as it is not defined in wx/setup.h and we try
-// to auto-detect whether we should be using debug help API or not ourselves
-// below. However if the auto-detection fails, you can always predefine it as 0
-// to avoid even trying.
 #ifndef wxUSE_DBGHELP
-    // The version of imagehlp.h from VC6 (7) is too old and is missing some
-    // required symbols while the version from VC7 (9) is good enough. As we
-    // don't know anything about version 8, don't use it unless we can test it.
-    #if API_VERSION_NUMBER >= 9
-        #define wxUSE_DBGHELP 1
-    #else
-        #define wxUSE_DBGHELP 0
+    // The only compiler which is known to have the necessary headers is MSVC.
+    #ifdef __VISUALC__
+        #if API_VERSION_NUMBER >= 9
+            #define wxUSE_DBGHELP 1
+        #else
+            #define wxUSE_DBGHELP 0
+        #endif
     #endif
 #endif
 
