d267735a2d2f05c39293459dc9516a14fad11459
Vadim Zeitlin <vadim@wxwidgets.org>
2015-07-09 14:28:12 +0200

    Fix conflicting sizer flags in wxWizard under wxOSX.
    Don't use wxGROW and wxALIGN_CENTRE together in a wxBoxSizer, this doesn't
    make sense as the latter is just ignored.
    
    Also use wxEXPAND instead of wxGROW for consistency with the flags elsewhere
    in the same file.
    
    Closes #17063.

--- src/generic/wizard.cpp
+++ src/generic/wizard.cpp
@@ -411,7 +411,7 @@ void wxWizard::AddButtonRow(wxBoxSizer *mainColumn)
         mainColumn->Add(
             buttonRow,
             0, // Vertically unstretchable
-            wxGROW|wxALIGN_CENTRE
+            wxEXPAND
             );
     else
 #endif
