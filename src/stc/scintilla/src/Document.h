4b704d41935d31c6d2fd18ae6a8652a7cdcb774e
Jouk <joukj@hrem.nano.tudelft.nl>
2015-07-16 07:47:43 +0200

    Define also != operator, since find on OpenVMS-AXP needs it

--- src/stc/scintilla/src/Document.h
+++ src/stc/scintilla/src/Document.h
@@ -207,6 +207,9 @@ public:
 		bool operator==(const WatcherWithUserData &other) const {
 			return (watcher == other.watcher) && (userData == other.userData);
 		}
+		bool operator!=(const WatcherWithUserData &other) const {
+			return (watcher != other.watcher) || (userData != other.userData);
+		}
 	};
 
 private:
