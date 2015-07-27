627b460378dfd9fafd79950467ac7c8e9b570a0f
Paul Cornett <paulcor@users.noreply.github.com>
2015-07-27 10:33:47 -0700

    use delete for memory allocated with new, closes #16797

--- src/x11/dnd.cpp
+++ src/x11/dnd.cpp
@@ -109,7 +109,7 @@ bool wxFileDropTarget::OnDrop(long x, long y, const void *data, size_t size )
 
   bool ret = OnDropFiles( x, y, 1, files );
 
-  free( files );
+  delete[] files;
 
   return ret;
 }
