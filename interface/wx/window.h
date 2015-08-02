949d2eeaa5c9c17805fedde8c07809cae981af3e
Vadim Zeitlin <vadim@wxwidgets.org>
2015-08-02 19:51:57 +0200

    Provide correct documentation for wxWindow::SetPosition().
    Just refer to Move(), this is trivial but necessary as otherwise Doxygen
    "helpfully" reuses the first comment in the section for the otherwise
    undocumented SetPosition() which didn't make any sense at all.

--- interface/wx/window.h
+++ interface/wx/window.h
@@ -1655,6 +1655,11 @@ public:
     */
     void Move(const wxPoint& pt, int flags = wxSIZE_USE_EXISTING);
 
+    /**
+        Moves the window to the specified position.
+
+        This is exactly the same as calling Move() with the default arguments.
+     */
     void SetPosition(const wxPoint& pt);
 
     //@}
