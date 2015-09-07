8bceeba1437615c246d0687be29c4477ce782954
Paul Cornett <paulcor@users.noreply.github.com>
2015-09-07 09:43:18 -0700

    Show widget at GTK+ level regardless of "child visibility" status, see #17134, #4343

--- src/gtk/window.cpp
+++ src/gtk/window.cpp
@@ -2751,7 +2751,7 @@ void wxWindowGTK::PostCreation()
 
     // unless the window was created initially hidden (i.e. Hide() had been
     // called before Create()), we should show it at GTK+ level as well
-    if ( IsShown() )
+    if (m_isShown)
         gtk_widget_show( m_widget );
 }
 
@@ -2969,7 +2969,7 @@ void wxWindowGTK::DoSetSize( int x, int y, int width, int height, int sizeFlags 
 
 bool wxWindowGTK::GTKShowFromOnIdle()
 {
-    if (IsShown() && m_showOnIdle && !gtk_widget_get_visible (m_widget))
+    if (m_isShown && m_showOnIdle && !gtk_widget_get_visible(m_widget))
     {
         GtkAllocation alloc;
         alloc.x = m_x;
