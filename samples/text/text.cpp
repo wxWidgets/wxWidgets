83659d5d1dd35c38b323f8aabdaf70fa5f4d653a
Vadim Zeitlin <vadim@wxwidgets.org>
2015-09-23 02:40:47 +0200

    Fix the recently broken test in the text sample
    Correct the text broken by f137753f284ce80cdad100badf7d4ff81572c8d7 (thanks
    buildbot and clang for the warning).

--- samples/text/text.cpp
+++ samples/text/text.cpp
@@ -776,7 +776,7 @@ void MyTextCtrl::OnMouseEvent(wxMouseEvent& ev)
     if ( !ms_logMouse )
         return;
 
-    if ( !ev.GetEventType() != wxEVT_MOTION )
+    if ( ev.GetEventType() != wxEVT_MOTION )
     {
         wxString msg;
         if ( ev.Entering() )

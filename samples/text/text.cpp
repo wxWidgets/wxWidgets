wxWidgets/wxWidgets

commit f137753f284ce80cdad100badf7d4ff81572c8d7

Don't log drag events as well as move ones in the text sample
Dragging the mouse (even accidentally by a pixel or two) resulted in a
confusing "Unknown event" line in the log, so don't log these messages neither
just as we already don't log the mouse motion events.

samples/text/text.cpp
@@ -776,7 +776,7 @@ void MyTextCtrl::OnMouseEvent(wxMouseEvent& ev)
     if ( !ms_logMouse )
         return;
 
-    if ( !ev.Moving() )
+    if ( !ev.GetEventType() != wxEVT_MOTION )
     {
         wxString msg;
         if ( ev.Entering() )

commit 83659d5d1dd35c38b323f8aabdaf70fa5f4d653a

Fix the recently broken test in the text sample
Correct the text broken by f137753f284ce80cdad100badf7d4ff81572c8d7 (thanks
buildbot and clang for the warning).

samples/text/text.cpp
@@ -776,7 +776,7 @@ void MyTextCtrl::OnMouseEvent(wxMouseEvent& ev)
     if ( !ms_logMouse )
         return;
 
-    if ( !ev.GetEventType() != wxEVT_MOTION )
+    if ( ev.GetEventType() != wxEVT_MOTION )
     {
         wxString msg;
         if ( ev.Entering() )