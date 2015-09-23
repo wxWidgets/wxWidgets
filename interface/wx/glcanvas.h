645eebe647d8b5eb33dd5edb1da1d59306c7c9ff
Vadim Zeitlin <vadim@wxwidgets.org>
2015-09-23 03:21:08 +0200

    Adjust WX_GL_CORE_PROFILE documentation after backport to 3.0
    As it and the other related flags are now present in 3.0.3 as well, they must
    be documented as being new since that version and not since 3.1.0.
    
    See #16402.

--- interface/wx/glcanvas.h
+++ interface/wx/glcanvas.h
@@ -148,7 +148,7 @@ enum
         See ::WX_GL_MAJOR_VERSION and ::WX_GL_MINOR_VERSION for more precise
         version selection.
 
-        @since 3.1.0
+        @since 3.0.3
      */
     WX_GL_CORE_PROFILE,
 
@@ -162,7 +162,7 @@ enum
         result in using OpenGL version at least 3.2 but can still be used
         there for portability.
 
-        @since 3.1.0
+        @since 3.0.3
      */
     WX_GL_MAJOR_VERSION,
 
@@ -172,7 +172,7 @@ enum
         This attribute has the same semantics as ::WX_GL_MAJOR_VERSION but is
         for the minor OpenGL version, e.g. 2 if OpenGL 3.2 is requested.
 
-        @since 3.1.0
+        @since 3.0.3
      */
     WX_GL_MINOR_VERSION
 
