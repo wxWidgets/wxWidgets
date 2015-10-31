f8f531687c62de7d35334bbefedc7293a1cffc5d
Bryan Petty <bryan@ibaku.net>
2015-10-31 14:45:00 -0600

    Release script improvements.

--- build/tools/release.sh
+++ build/tools/release.sh
@@ -6,17 +6,16 @@
 # distribution and we don't need to maintain the list of them ourselves but we
 # also don't run the risk of including anything unwanted.
 #
-# To summarize, here are the steps to create the release:
-#
-#   % git tag vX.Y.Z -m 'Tag X.Y.Z release.'
-#   % ./build/tools/git-make-release.sh X.Y.Z
-#   % ... upload ../wxWidgets-X.Y.Z.{7z,tar.bz2,zip} ...
+# See docs/contributing/how-to-release.md for usage instructions.
 
 version=$1
 if [ -z "$version" ]; then
     echo "Must specify the distribution version." >&2
     exit 1
 fi
+
+root="$(readlink -f $(dirname $(readlink -f $0))/../..)"
+cd "$root"
 
 if ! git diff --quiet; then
     echo "Working copy has modifications, commit or stash them." >&2
@@ -27,18 +26,19 @@ set -e
 set -x
 
 prefix=wxWidgets-$version
-destdir=$(dirname $(readlink -f $0))/../../../$prefix
+destdir="$root/distrib/release/$version"
 
 cleanup() {
-    rm -rf $destdir
+    rm -rf $destdir/$prefix
 }
 
 trap cleanup INT TERM EXIT
 
 cleanup
 
-git archive --prefix=$prefix/ HEAD | (cd ..; tar x)
-cd ..
+mkdir -p $destdir
+git archive --prefix=$prefix/ HEAD | (cd $destdir; tar x)
+cd $destdir
 # All setup0.h files are supposed to be renamed to just setup.h when checked
 # out and in the distribution.
 find $prefix/include/wx -type f -name setup0.h | while read f; do
@@ -56,3 +56,16 @@ zip -q -r ../$prefix.zip .
 
 7z a ../$prefix.7z . >/dev/null
 7z a ../${prefix}-headers.7z include >/dev/null
+
+# Build HTML documentation packages.
+prefix_docs=$prefix-docs-html
+cd "$root/docs/doxygen"
+rm -rf out
+./regen.sh html
+cd out
+mv html "$prefix_docs"
+tar cjf "$destdir/$prefix_docs.tar.bz2" "$prefix_docs"
+cd "$prefix_docs"
+zip -q -r "$destdir/$prefix_docs.zip" .
+cd "$root"
+rm -rf "$root/docs/doxygen/out"
