69584dd3b40b60b200d6144f4651138b33e71a94
Vadim Zeitlin <vadim@wxwidgets.org>
2015-11-07 23:36:10 +0100

    Show make version as well in AppVeyor MinGW builds
    Also remove quotes which are not needed in Windows shell and show just the
    first line of the versions.

--- build/tools/appveyor.bat
+++ build/tools/appveyor.bat
@@ -14,11 +14,13 @@ goto :eof
 :mingw
 cd build\msw
 path C:\MinGW\bin;%path%
-echo "--- Compiler version:"
-g++ --version
-echo "--- Binutils version:"
-ar --version
-echo "--- Starting the build"
+echo --- Tools versions:
+g++ --version | head -1
+mingw32-make --version | head -1
+ar --version | head -1
+echo.
+echo --- Starting the build
+echo.
 mingw32-make SHELL=cmd -f makefile.gcc setup_h BUILD=debug SHARED=0 USE_STC=0 USE_RIBBON=0 USE_PROPGRID=0 USE_AUI=0
 mingw32-make SHELL=cmd -j3 -f makefile.gcc BUILD=debug SHARED=0 USE_STC=0 USE_RIBBON=0 USE_PROPGRID=0 USE_AUI=0
 goto :eof
