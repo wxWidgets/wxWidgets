c61488cd50e8ba11d97f1c635ea9e52b9f2fe831
ARATA Mizuki <minorinoki@gmail.com>
2015-11-09 19:45:50 +0900

    Don't use `wxOVERRIDE` in `wxDECLARE_EVENT_TABLE`.
    It causes tons of warnings if the client code does not make use of `override` keyword.

--- include/wx/event.h
+++ include/wx/event.h
@@ -3973,10 +3973,12 @@ typedef void (wxEvtHandler::*wxClipboardTextEventFunction)(wxClipboardTextEvent&
     private:                                                            \
         static const wxEventTableEntry sm_eventTableEntries[];          \
     protected:                                                          \
+        wxCLANG_WARNING_SUPPRESS(inconsistent-missing-override)         \
         static const wxEventTable        sm_eventTable;                 \
-        const wxEventTable* GetEventTable() const wxOVERRIDE;           \
+        const wxEventTable* GetEventTable() const;                      \
         static wxEventHashTable          sm_eventHashTable;             \
-        wxEventHashTable& GetEventHashTable() const wxOVERRIDE
+        wxEventHashTable& GetEventHashTable() const                     \
+        wxCLANG_WARNING_RESTORE(inconsistent-missing-override)
 
 // N.B.: when building DLL with Borland C++ 5.5 compiler, you must initialize
 //       sm_eventTable before using it in GetEventTable() or the compiler gives
