6a3b6600dd6d503c0480e9ec7b65b379d8590c13
Vadim Zeitlin <vadim@wxwidgets.org>
2015-12-30 15:56:44 +0100

    Test builds with newer g++, clang and OS X on Travis CI
    Configure the build matrix to test building on more platforms and using clang
    as well and also test C++11 builds using both g++ and clang.

--- build/tools/before_install.sh
+++ build/tools/before_install.sh
@@ -2,7 +2,7 @@
 #
 # This script is used by Travis CI to install the dependencies before building
 # wxWidgets but can also be run by hand if necessary but currently it only
-# works for Ubuntu 12.04 and 14.04 used by Travis.
+# works for Ubuntu 12.04 and 14.04 and OS X used by Travis builds.
 
 SUDO=sudo
 
@@ -12,5 +12,10 @@ case $(uname -s) in
             $SUDO apt-get update
             $SUDO apt-get install -y libcppunit-dev libgtk2.0-dev libnotify-dev
         fi
+        ;;
+
+    Darwin)
+        brew update
+        brew install cppunit --universal
         ;;
 esac
