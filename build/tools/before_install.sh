844a7e426aa8ef27544a19348b0df1d270d9461a
Vadim Zeitlin <vadim@wxwidgets.org>
2015-12-30 18:03:53 +0100

    Extract Travis before install script steps into a separate file
    This allows to do make these steps more complicated, e.g. avoid using apt-get
    under non-Linux (and even non-Debian/Ubuntu) platforms.
    
    Also install GTK+ development package explicitly, while it's already available
    on the older Travis Ubuntu 12.04 VMs, this is not the case for the newer 14.04
    ones.

---
+++ build/tools/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# This script is used by Travis CI to install the dependencies before building
+# wxWidgets but can also be run by hand if necessary but currently it only
+# works for Ubuntu 12.04 and 14.04 used by Travis.
+
+SUDO=sudo
+
+case $(uname -s) in
+    Linux)
+        if [ -f /etc/apt/sources.list ]; then
+            $SUDO apt-get update
+            $SUDO apt-get install -y libcppunit-dev libgtk2.0-dev libnotify-dev
+        fi
+        ;;
+esac
