wxWidgets/wxWidgets

commit dbd98a107887b568faf0ef8bb930042f1941e10b

Don't attempt to uncheck menu radio item in wxGTK
Unchecking radio item makes no sense and wxMSW does nothing in this case, so
don't do anything in wxGTK neither, in particular don't generate a spurious
wxEVT_MENU.

Closes #17318.

src/gtk/menu.cpp
@@ -684,18 +684,23 @@ void wxMenuItem::Check( bool check )
     if (check == m_isChecked)
         return;
 
-    wxMenuItemBase::Check( check );
-
     switch ( GetKind() )
     {
-        case wxITEM_CHECK:
         case wxITEM_RADIO:
+            // It doesn't make sense to uncheck a radio item.
+            if ( !check )
+                return;
+
+            wxFALLTHROUGH;
+        case wxITEM_CHECK:
             gtk_check_menu_item_set_active( (GtkCheckMenuItem*)m_menuItem, (gint)check );
             break;
 
         default:
             wxFAIL_MSG( wxT("can't check this item") );
     }
+
+    wxMenuItemBase::Check( check );
 }
 
 void wxMenuItem::Enable( bool enable )

commit 13783d873391d8b5c34593f67fc105715eb8cf74

Fixed regression in wxMenuItem::Check introduced by dbd98a10.
Internal check flag (via wxMenuItemBase::Check) has to be set as a first to avoid generating a spurious wxEVT_MENU when menu item is checked.

src/gtk/menu.cpp
@@ -693,14 +693,13 @@ void wxMenuItem::Check( bool check )
 
             wxFALLTHROUGH;
         case wxITEM_CHECK:
+            wxMenuItemBase::Check( check );
             gtk_check_menu_item_set_active( (GtkCheckMenuItem*)m_menuItem, (gint)check );
             break;
 
         default:
             wxFAIL_MSG( wxT("can't check this item") );
     }
-
-    wxMenuItemBase::Check( check );
 }
 
 void wxMenuItem::Enable( bool enable )