5e694fdcd04375d915e60b1b67201978df05ce8f
Jens Göpfert <jens.goepfert@tracetronic.de>
2016-01-08 13:45:40 +0100

    WM_APPCOMMAND not handled anymore. Used new wx-Key constants in Key-Events

--- src/msw/window.cpp
+++ src/msw/window.cpp
@@ -2896,77 +2896,6 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
             break;
 #endif // MM_JOY1MOVE
 
-        case WM_APPCOMMAND:
-            {
-                short cmd;
-                processed = false;
-                cmd = GET_APPCOMMAND_LPARAM(lParam);
-
-                wxKeyEvent event(wxEVT_KEY_DOWN);
-                InitAnyKeyEvent(event, 0, 0);
-                event.m_keyCode = 0;
-                event.m_rawCode = wParam;
-                event.m_rawFlags = lParam;
-                switch ( cmd )
-                {
-                    case APPCOMMAND_BROWSER_BACKWARD:
-                        event.m_keyCode = WXK_BROWSER_BACK;
-                        break;
-                    case APPCOMMAND_BROWSER_FORWARD:
-                        event.m_keyCode = WXK_BROWSER_FORWARD;
-                        break;
-                    case APPCOMMAND_BROWSER_REFRESH:
-                        event.m_keyCode = WXK_BROWSER_REFRESH;
-                        break;
-                    case APPCOMMAND_BROWSER_STOP:
-                        event.m_keyCode = WXK_BROWSER_STOP;
-                        break;
-                    case APPCOMMAND_BROWSER_SEARCH:
-                        event.m_keyCode = WXK_BROWSER_SEARCH;
-                        break;
-                    case APPCOMMAND_BROWSER_FAVORITES:
-                        event.m_keyCode = WXK_BROWSER_FAVORITES;
-                        break;
-                    case APPCOMMAND_BROWSER_HOME:
-                        event.m_keyCode = WXK_BROWSER_HOME;
-                        break;
-                    case APPCOMMAND_VOLUME_MUTE:
-                        event.m_keyCode = WXK_VOLUME_MUTE;
-                        break;
-                    case APPCOMMAND_VOLUME_DOWN:
-                        event.m_keyCode = WXK_VOLUME_DOWN;
-                        break;
-                    case APPCOMMAND_VOLUME_UP:
-                        event.m_keyCode = WXK_VOLUME_UP;
-                        break;
-                    case APPCOMMAND_MEDIA_NEXTTRACK:
-                        event.m_keyCode = WXK_MEDIA_NEXT_TRACK;
-                        break;
-                    case APPCOMMAND_MEDIA_PREVIOUSTRACK:
-                        event.m_keyCode = WXK_MEDIA_PREV_TRACK;
-                        break;
-                    case APPCOMMAND_MEDIA_STOP:
-                        event.m_keyCode = WXK_MEDIA_STOP;
-                        break;
-                    case APPCOMMAND_MEDIA_PLAY_PAUSE:
-                        event.m_keyCode = WXK_MEDIA_PLAY_PAUSE;
-                        break;
-                    case APPCOMMAND_LAUNCH_MAIL:
-                        event.m_keyCode = WXK_LAUNCH_MAIL;
-                        break;
-                    case APPCOMMAND_LAUNCH_APP1:
-                        event.m_keyCode = WXK_LAUNCH_APP1;
-                        break;
-                    case APPCOMMAND_LAUNCH_APP2:
-                        event.m_keyCode = WXK_LAUNCH_APP2;
-                        break;
-                }
-                if ( event.m_keyCode > 0 )
-                {
-                    processed = HandleWindowEvent(event);
-                }
-            }
-            break;
         case WM_COMMAND:
             {
                 WORD id, cmd;
@@ -6203,6 +6132,58 @@ int VKToWX(WXWORD vk, WXLPARAM lParam, wchar_t *uc)
 
             if ( uc )
                 *uc = WXK_RETURN;
+            break;
+
+        case VK_BROWSER_BACK:
+            wxk = WXK_BROWSER_BACK;
+            break;
+        case VK_BROWSER_FORWARD:
+            wxk = WXK_BROWSER_FORWARD;
+            break;
+        case VK_BROWSER_REFRESH:
+            wxk = WXK_BROWSER_REFRESH;
+            break;
+        case VK_BROWSER_STOP:
+            wxk = WXK_BROWSER_STOP;
+            break;
+        case VK_BROWSER_SEARCH:
+            wxk = WXK_BROWSER_SEARCH;
+            break;
+        case VK_BROWSER_FAVORITES:
+            wxk = WXK_BROWSER_FAVORITES;
+            break;
+        case VK_BROWSER_HOME:
+            wxk = WXK_BROWSER_HOME;
+            break;
+        case VK_VOLUME_MUTE:
+            wxk = WXK_VOLUME_MUTE;
+            break;
+        case VK_VOLUME_DOWN:
+            wxk = WXK_VOLUME_DOWN;
+            break;
+        case VK_VOLUME_UP:
+            wxk = WXK_VOLUME_UP;
+            break;
+        case VK_MEDIA_NEXT_TRACK:
+            wxk = WXK_MEDIA_NEXT_TRACK;
+            break;
+        case VK_MEDIA_PREV_TRACK:
+            wxk = WXK_MEDIA_PREV_TRACK;
+            break;
+        case VK_MEDIA_STOP:
+            wxk = WXK_MEDIA_STOP;
+            break;
+        case VK_MEDIA_PLAY_PAUSE:
+            wxk = WXK_MEDIA_PLAY_PAUSE;
+            break;
+        case VK_LAUNCH_MAIL:
+            wxk = WXK_LAUNCH_MAIL;
+            break;
+        case VK_LAUNCH_APP1:
+            wxk = WXK_LAUNCH_APP1;
+            break;
+        case VK_LAUNCH_APP2:
+            wxk = WXK_LAUNCH_APP2;
             break;
 
         default:
