wxWidgets/wxWidgets

commit 3e75b231b0acb6657c611a7f5424ef75d9e15011

fix GdkCursor memory leaks

src/gtk/minifram.cpp
@@ -288,6 +288,8 @@ gtk_window_motion_notify_callback( GtkWidget *widget, GdkEventMotion *gdk_event,
                 cursor = gdk_cursor_new_for_display(display, GDK_BOTTOM_RIGHT_CORNER);
             }
             gdk_window_set_cursor(window, cursor);
+            if (cursor)
+                g_object_unref(cursor);
         }
         return TRUE;
     }

src/gtk/assertdlg_gtk.cpp
@@ -116,6 +116,7 @@ void gtk_assert_dialog_process_backtrace (GtkAssertDialog *dlg)
     GdkDisplay* display = gdk_window_get_display(parent);
     GdkCursor* cur = gdk_cursor_new_for_display(display, GDK_WATCH);
     gdk_window_set_cursor (parent, cur);
+    g_object_unref(cur);
     gdk_flush ();
 
     (*dlg->callback)(dlg->userdata);

commit e72133371eb2237ef509b2e0fb2549d1fe5d841b

fix cursor unref for GTK2, broken by 3e75b23

src/gtk/minifram.cpp
@@ -289,7 +289,13 @@ gtk_window_motion_notify_callback( GtkWidget *widget, GdkEventMotion *gdk_event,
             }
             gdk_window_set_cursor(window, cursor);
             if (cursor)
+            {
+#ifdef __WXGTK3__
                 g_object_unref(cursor);
+#else
+                gdk_cursor_unref(cursor);
+#endif
+            }
         }
         return TRUE;
     }