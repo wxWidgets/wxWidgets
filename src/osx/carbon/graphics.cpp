8317eb1a644871a1a744fc4ac4894a467632f26e
Daniel Kulp <dkulp@apache.org>
2016-01-19 12:58:22 -0500

    Move the stikeout down a bit

--- src/osx/carbon/graphics.cpp
+++ src/osx/carbon/graphics.cpp
@@ -2335,8 +2335,8 @@ void wxMacCoreGraphicsContext::DoDrawText( const wxString &str, wxDouble x, wxDo
     if ( fref->GetStrikethrough() )
     {
         CGFloat width = CTLineGetTypographicBounds(line, NULL, NULL, NULL);
-        CGFloat height = CTFontGetSize( font );
-        CGPoint points[] = { {0.0, height / 2},  {width, height / 2} };
+        CGFloat height = CTFontGetXHeight( font );
+        CGPoint points[] = { {0.0, height * 0.6},  {width, height * 0.6} };
         CGContextSetStrokeColorWithColor(m_cgContext, col);
         CGContextSetShouldAntialias(m_cgContext, false);
         CGContextSetLineWidth(m_cgContext, 1.0);
