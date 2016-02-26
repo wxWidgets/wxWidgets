2072bfa00efb9941895c71e954322bb7d213cf4a
Artur Wieczorek <artwik@wp.pl>
2016-02-26 21:22:18 +0100

    Fixed rescaling wxBitmap.
    Create rescaled wxBitmap with the same colour depth as the source bitmap.

--- src/common/artprov.cpp
+++ src/common/artprov.cpp
@@ -222,7 +222,7 @@ void wxArtProvider::RescaleBitmap(wxBitmap& bmp, const wxSize& sizeNeeded)
     bmp = wxBitmap(img);
 #else // !wxUSE_IMAGE
     // Fallback method of scaling the bitmap
-    wxBitmap newBmp(sizeNeeded);
+    wxBitmap newBmp(sizeNeeded, bmp.GetDepth());
 #if defined(__WXMSW__) || defined(__WXOSX__)
     // wxBitmap::UseAlpha() is used only on wxMSW and wxOSX.
     newBmp.UseAlpha(bmp.HasAlpha());
