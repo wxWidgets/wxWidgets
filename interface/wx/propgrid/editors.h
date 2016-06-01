9de3f8395cc8242fa8d9f0d8a413aa1075b39f23
Artur Wieczorek <artwik@wp.pl>
2016-06-01 18:52:06 +0200

    Updated wxPGEditor interface documentation
    Editor controls are no longer bound to the fixed ID's wxPG_SUBID1 and wxPG_SUBID2.

--- interface/wx/propgrid/editors.h
+++ interface/wx/propgrid/editors.h
@@ -45,7 +45,7 @@ public:
 
     /**
         Returns pointer to the name of the editor. For example,
-        wxPGEditor_TextCtrl has name "TextCtrl". If you dont' need to access
+        wxPGEditor_TextCtrl has name "TextCtrl". If you don't need to access
         your custom editor by string name, then you do not need to implement
         this function.
     */
@@ -67,8 +67,6 @@ public:
             Initial size for control(s).
 
         @remarks
-        - Primary control shall use id wxPG_SUBID1, and secondary (button) control
-          shall use wxPG_SUBID2.
         - Unlike in previous version of wxPropertyGrid, it is no longer
           necessary to call wxEvtHandler::Connect() for interesting editor
           events. Instead, all events from control are now automatically
