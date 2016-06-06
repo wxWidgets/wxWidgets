1006354d61c1d8002ea27c9da764498d3bb5b0b8
Hanmac <hanmac@gmx.de>
2016-06-06 09:10:40 +0200

    remove unused constructor and destructor from wxGTKMimeTypesManagerFactory

--- include/wx/gtk/mimetype.h
+++ include/wx/gtk/mimetype.h
@@ -2,14 +2,13 @@
 // Name:        wx/gtk/mimetype.h
 // Purpose:     classes and functions to manage MIME types
 // Author:      Hans Mackowiak
-// Modified by:
-// Created:     05.06.2016
+// Created:     2016-06-05
 // Copyright:   (c) 2016 Hans Mackowiak <hanmac@gmx.de>
-// Licence:     wxWindows licence (part of wxExtra library)
+// Licence:     wxWindows licence (part of wxGTK library)
 /////////////////////////////////////////////////////////////////////////////
 
-#ifndef _GTK_MIMETYPE_IMPL_H
-#define _GTK_MIMETYPE_IMPL_H
+#ifndef _WX_GTK_MIMETYPE_IMPL_H
+#define _WX_GTK_MIMETYPE_IMPL_H
 
 #include "wx/unix/mimetype.h"
 
@@ -25,13 +24,11 @@ protected:
 class WXDLLIMPEXP_CORE wxGTKMimeTypesManagerFactory : public wxMimeTypesManagerFactory
 {
 public:
-    wxGTKMimeTypesManagerFactory() {}
-    virtual ~wxGTKMimeTypesManagerFactory() {}
     wxMimeTypesManagerImpl *CreateMimeTypesManagerImpl() wxOVERRIDE;
 };
 
 #endif // wxUSE_MIMETYPE
 
-#endif // _GTK_MIMETYPE_IMPL_H
+#endif // _WX_GTK_MIMETYPE_IMPL_H
 
 
