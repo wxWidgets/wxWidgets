295e292be636f8e1ae7ec288fd9d499c104c64e6
Hanmac <hanmac@gmx.de>
2016-06-06 09:16:02 +0200

    update GTKMimeType using wxGtkObject

--- src/gtk/mimetype.cpp
+++ src/gtk/mimetype.cpp
@@ -2,10 +2,9 @@
 // Name:        src/gtk/mimetype.cpp
 // Purpose:     classes and functions to manage MIME types
 // Author:      Hans Mackowiak
-// Modified by:
-// Created:     05.06.2016
+// Created:     2016-06-05
 // Copyright:   (c) 2016 Hans Mackowiak <hanmac@gmx.de>
-// Licence:     wxWindows licence (part of wxExtra library)
+// Licence:     wxWindows licence (part of wxGTK library)
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -15,7 +14,7 @@
     #pragma hdrstop
 #endif
 
-#if wxUSE_MIMETYPE && wxUSE_FILE
+#if wxUSE_MIMETYPE
 
 #include "wx/gtk/mimetype.h"
 
@@ -23,24 +22,26 @@
 #include <gtk/gtk.h>
 
 #include "wx/gtk/private/string.h"
+#include "wx/gtk/private/object.h"
 
 wxString wxGTKMimeTypesManagerImpl::GetIconFromMimeType(const wxString& mime)
 {
     wxString icon;
-    wxGtkString type(g_content_type_from_mime_type(mime.mb_str()));
+    wxGtkString type(g_content_type_from_mime_type(mime.utf8_str()));
 
-    GIcon *gicon(g_content_type_get_icon(type));
-    if (gicon) {
+    wxGtkObject<GIcon> gicon(g_content_type_get_icon(type));
+    if (gicon)
+    {
         GtkIconTheme *theme(gtk_icon_theme_get_default());
-        if (theme) {
-            GtkIconInfo *ticon(gtk_icon_theme_lookup_by_gicon(theme, gicon, 256, GTK_ICON_LOOKUP_NO_SVG));
+        if (theme)
+        {
+            wxGtkObject<GtkIconInfo> ticon(gtk_icon_theme_lookup_by_gicon(theme, gicon, 256, GTK_ICON_LOOKUP_NO_SVG));
 
-            if (ticon) {
+            if (ticon)
+            {
                 icon = wxString(gtk_icon_info_get_filename(ticon));
-                g_object_unref(ticon);
             }
         }
-        g_object_unref(gicon);
     }
     return icon;
 }
@@ -50,5 +51,4 @@ wxMimeTypesManagerImpl *wxGTKMimeTypesManagerFactory::CreateMimeTypesManagerImpl
     return new wxGTKMimeTypesManagerImpl();
 }
 
-#endif
-  // wxUSE_MIMETYPE && wxUSE_FILE
+#endif // wxUSE_MIMETYPE
