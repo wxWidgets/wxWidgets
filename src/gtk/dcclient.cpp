c845b953a2e0a65b509d9127caaecf1cb64477e2
Artur Wieczorek <artwik@wp.pl>
2016-07-17 00:01:28 +0200

    Don't destroy clipping region if empty clipping region is required (wxDC, GTK).
    For compatibility with wxGCDC (Cairo and all native MSW renderers) current clipping region shouldn't be explicitly destroyed if empty clipping region is requested. It should be handled as any other region.

--- src/gtk/dcclient.cpp
+++ src/gtk/dcclient.cpp
@@ -1904,12 +1904,6 @@ void wxWindowDCImpl::DoSetDeviceClippingRegion( const wxRegion &region  )
 {
     wxCHECK_RET( IsOk(), wxT("invalid window dc") );
 
-    if (region.Empty())
-    {
-        DestroyClippingRegion();
-        return;
-    }
-
     if (!m_gdkwindow) return;
 
     if (!m_currentClippingRegion.IsNull())
