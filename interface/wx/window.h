1c4e20d87097754e715f59e240fb65d2c900c2ef
Vadim Zeitlin <vadim@wxwidgets.org>
2016-08-09 01:10:24 +0200

    Fix documentation of {From,To}DIP() overloads taking int
    They were mistakenly using wxSize (already documented just above).

--- interface/wx/window.h
+++ interface/wx/window.h
@@ -989,7 +989,7 @@ public:
     static wxPoint FromDIP(const wxPoint& pt, const wxWindow* w);
 
     /// @overload
-    static wxSize FromDIP(const wxSize& sz, const wxWindow* w);
+    static wxSize FromDIP(int d, const wxWindow* w);
 
 
     /**
@@ -1056,7 +1056,7 @@ public:
     static wxPoint ToDIP(const wxPoint& pt, const wxWindow* w);
 
     /// @overload
-    static wxSize ToDIP(const wxSize& sz, const wxWindow* w);
+    static wxSize ToDIP(int d, const wxWindow* w);
 
     /**
         This functions returns the best acceptable minimal size for the window.
