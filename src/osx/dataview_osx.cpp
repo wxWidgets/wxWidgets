71aa152af2c737b0849262767d0236b3a01ab0cc
Václav Slavík <vaclav@slavik.io>
2016-10-06 17:31:11 +0200

    OS X: Reset selection in wxDataViewCtrl::SetSelections
    SetSelections() should reset selection to the specified items
    (consistently with other platforms), not add these items to the existing
    selection.

--- src/osx/dataview_osx.cpp
+++ src/osx/dataview_osx.cpp
@@ -589,6 +589,10 @@ void wxDataViewCtrl::SelectAll()
 
 void wxDataViewCtrl::SetSelections(wxDataViewItemArray const& sel)
 {
+    wxDataViewWidgetImpl* dataViewWidgetPtr(GetDataViewPeer());
+
+    dataViewWidgetPtr->UnselectAll();
+
     size_t const noOfSelections = sel.GetCount();
 
     size_t i;
@@ -608,8 +612,6 @@ void wxDataViewCtrl::SetSelections(wxDataViewItemArray const& sel)
     }
 
    // finally select the items:
-    wxDataViewWidgetImpl* dataViewWidgetPtr(GetDataViewPeer()); // variable definition for abbreviational purposes
-
     for (i=0; i<noOfSelections; ++i)
       dataViewWidgetPtr->Select(sel[i]);
 }
