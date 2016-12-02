7c730334a2021775bbfc6bbb16d8c393df4be003
Laurent Poujoulat <laurent.poujoulat@art-technology.fr>
2016-12-02 20:47:43 +0100

    Fix long long format specifier when using MinGW with ANSI STDIO
    When using MinGW own, more standard-compliant, STDIO library, the correct
    format specified to use for long long values is "%lld" and not "%I64d" as when
    using MSVC CRT.
    
    Closes #17736.

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -1083,7 +1083,7 @@ typedef wxUint32 wxDword;
     #define wxLongLong_t __int64
     #define wxLongLongSuffix i64
     #define wxLongLongFmtSpec "L"
-#elif defined(__MINGW32__)
+#elif defined(__MINGW32__) && (__USE_MINGW_ANSI_STDIO != 1)
     #define wxLongLong_t long long
     #define wxLongLongSuffix ll
     #define wxLongLongFmtSpec "I64"
