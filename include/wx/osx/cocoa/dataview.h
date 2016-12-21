9291a6a7fc48f413cb1979c57e9edd41375ef38e
Vadim Zeitlin <vadim@zeitlins.org>
2016-12-21 20:50:44 +0100

    No real changes, just remove leading underscore from variables
    Now that these variables are in a public header, don't use leading underscores
    for them, if only for consistency with all the others.

--- include/wx/osx/cocoa/dataview.h
+++ include/wx/osx/cocoa/dataview.h
@@ -375,8 +375,8 @@ private:
 @interface wxTextFieldCell : NSTextFieldCell
 {
 @private
-    int _wxAlignment;
-    BOOL _adjustRect;
+    int wxAlignment;
+    BOOL adjustRect;
 }
 
     -(void) setWXAlignment:(int)alignment;
