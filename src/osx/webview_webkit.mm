f6108e70133a1a3baa42e6771d17cd3eff6e01c1
Stefano D. Mtangoo <mwinjilisti@gmail.com>
2017-01-23 11:37:22 +0300

    Webview OSX: Emit wxEVT_CONTEXT_MENU default context menu disabled

--- src/osx/webview_webkit.mm
+++ src/osx/webview_webkit.mm
@@ -1064,7 +1064,17 @@ wxString nsErrorToWxHtmlError(NSError* error, wxWebViewNavigationError* out)
     if(webKitWindow->IsContextMenuEnabled())
         return defaultMenuItems;
     else
+    {
+        wxContextMenuEvent event(wxEVT_CONTEXT_MENU,
+                             webKitWindow->GetId(),
+                             webKitWindow->ClientToScreen(wxDefaultPosition));
+        event.SetEventObject(webKitWindow);
+
+        if (webKitWindow && webKitWindow->GetEventHandler())
+            webKitWindow->GetEventHandler()->ProcessEvent(event);
+
         return nil;
+    }
 }
 @end
 
