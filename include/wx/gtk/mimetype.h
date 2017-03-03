f11409ab09c4af082c9b3bf6196c1c5b4bbffc43
Tim S <stahta01@users.sourceforge.net>
2017-03-03 12:13:59 -0500

    When not Unix skip wxOVERRIDE in GetIconFromMimeType.

--- include/wx/gtk/mimetype.h
+++ include/wx/gtk/mimetype.h
@@ -23,7 +23,11 @@
 class WXDLLIMPEXP_CORE wxGTKMimeTypesManagerImpl : public wxMimeTypesManagerImpl
 {
 protected:
+#if defined(__UNIX__)
     wxString GetIconFromMimeType(const wxString& mime) wxOVERRIDE;
+#else
+    wxString GetIconFromMimeType(const wxString& mime);
+#endif
 };
 
 
