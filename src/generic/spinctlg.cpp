c31111167322eb28b74099ad2943fd5a01742e43
tommash <tmarek@poczta.fm>
2017-03-15 12:37:00 +0100

    Update spinctlg.cpp
    This prevents wxSpinCtrlDouble from sending the event when the value has not changed in the text box.
    Currently every time the focus is removed from the text-box wxSpinCtrlDouble sends the event!

--- src/generic/spinctlg.cpp
+++ src/generic/spinctlg.cpp
@@ -409,8 +409,8 @@ void wxSpinCtrlGenericBase::OnSpinButton(wxSpinEvent& event)
 
 void wxSpinCtrlGenericBase::OnTextLostFocus(wxFocusEvent& event)
 {
-    SyncSpinToText(SendEvent_Text);
-    DoSendEvent();
+    if (SyncSpinToText(SendEvent_Text))
+        DoSendEvent();
 
     event.Skip();
 }
