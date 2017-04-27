a6c864ae7892bc8777b3e373ca43d1187135d78f
Robin Dunn <robin@alldunn.com>
2017-04-27 09:28:22 -0700

    Add a missing constant and enum

--- interface/wx/grid.h
+++ interface/wx/grid.h
@@ -5,6 +5,21 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
+
+/// Magic constant which tells (to some functions) to automatically calculate
+/// the appropriate size
+#define wxGRID_AUTOSIZE (-1)
+
+/// Many wxGrid methods work either with columns or rows, this enum is used for
+/// the parameter indicating which one should it be
+enum wxGridDirection
+{
+    wxGRID_COLUMN,
+    wxGRID_ROW
+};
+
+
+
 /**
     @class wxGridCellRenderer
 
