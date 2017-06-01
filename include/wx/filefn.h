8a54486e89124fdef2778909ccd6849213d0184f
Adrian DC <radian.dc@gmail.com>
2017-06-01 12:18:50 +0200

    wx/filefn.h: Avoid _wopen,_w... Windows redundant declarations
     * Fixes warnings triggered by -Wredundant-decls
     * Exclusion flags based on original headers: io.h, stat.h
    
     * Restrict them out of:
        __MSVCRT__ && _IO_H_ && _WIO_DEFINED,
        __MSVCRT__ && _STAT_H_ && _WSTAT_DEFINED
    
    Change-Id: I230ce4453178af573b05563d9ca8919f8d475b6e

--- include/wx/filefn.h
+++ include/wx/filefn.h
@@ -293,12 +293,18 @@ enum wxPosixPermissions
             #define wxCRT_OpenW       _wopen
         #endif
 
-        wxDECL_FOR_STRICT_MINGW32(int, _wopen, (const wchar_t*, int, ...))
-        wxDECL_FOR_STRICT_MINGW32(int, _waccess, (const wchar_t*, int))
-        wxDECL_FOR_STRICT_MINGW32(int, _wchmod, (const wchar_t*, int))
+        #if !defined (_IO_H_) || !defined (__MSVCRT__) || !defined (_WIO_DEFINED)
+            wxDECL_FOR_STRICT_MINGW32(int, _wopen, (const wchar_t*, int, ...))
+            wxDECL_FOR_STRICT_MINGW32(int, _waccess, (const wchar_t*, int))
+            wxDECL_FOR_STRICT_MINGW32(int, _wchmod, (const wchar_t*, int))
+        #endif
+
         wxDECL_FOR_STRICT_MINGW32(int, _wmkdir, (const wchar_t*))
         wxDECL_FOR_STRICT_MINGW32(int, _wrmdir, (const wchar_t*))
-        wxDECL_FOR_STRICT_MINGW32(int, _wstati64, (const wchar_t*, struct _stati64*))
+
+        #if !defined (_STAT_H_) || !defined (__MSVCRT__) || !defined (_WSTAT_DEFINED)
+            wxDECL_FOR_STRICT_MINGW32(int, _wstati64, (const wchar_t*, struct _stati64*))
+        #endif
 
         #define   wxCRT_AccessW     _waccess
         #define   wxCRT_ChmodW      _wchmod
