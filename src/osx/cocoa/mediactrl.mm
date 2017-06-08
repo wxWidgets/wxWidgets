07b2ead8f3d27b6c34fb4e6fc09eca310a02a02b
Stefan Csomor <csomor@advancedconcepts.ch>
2017-06-08 10:37:58 +0200

    properly support weak_linking AVKit when compiling under 10.9 + SDK, even when deployment can be lower, removing QTKit

--- src/osx/cocoa/mediactrl.mm
+++ src/osx/cocoa/mediactrl.mm
@@ -32,7 +32,7 @@
 
 #include "wx/osx/private.h"
 
-#if wxOSX_USE_COCOA && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9
+#if wxOSX_USE_COCOA && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9
     #define wxOSX_USE_AVKIT 1
 #else
     #define wxOSX_USE_AVKIT 0
