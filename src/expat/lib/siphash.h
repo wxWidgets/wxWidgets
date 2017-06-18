b3a05337013752282045a0af27310830d586589c
Cătălin Răceanu <catalinraceanu2@gmail.com>
2017-06-18 22:15:01 +0300

    Define int types for old VS versions (2003/7.1 up to 2008/9.0) where stdint.h was missing.

--- src/expat/lib/siphash.h
+++ src/expat/lib/siphash.h
@@ -76,7 +76,15 @@
 #define SIPHASH_H
 
 #include <stddef.h> /* size_t */
+
+#if defined(_WIN32) && defined(_MSC_VER) && _MSC_VER < 1600  /* 1600 is vs2010/10.0 */
+  /* For vs2003/7.1 up to vs2008/9.0 */
+  typedef unsigned __int8   uint8_t;
+  typedef unsigned __int32 uint32_t;
+  typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h> /* uint64_t uint32_t uint8_t */
+#endif
 
 
 #define SIP_ROTL(x, b) (uint64_t)(((x) << (b)) | ( (x) >> (64 - (b))))
