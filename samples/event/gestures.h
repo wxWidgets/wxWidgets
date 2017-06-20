57c031a08f6f362e1f175990cd6ae1c899373ed9
Prashantkn <ohhereim@gmail.com>
2017-06-20 23:49:11 +0530

    Add gestures.h in event sample

---
+++ samples/event/gestures.h
@@ -0,0 +1,21 @@
+#include "wx/wx.h"
+
+class MyGestureFrame : public wxFrame
+{
+public:
+    MyGestureFrame();
+};
+ 
+class MyGesturePanel : public wxPanel
+{
+public:
+    MyGesturePanel(MyGestureFrame* parent);
+ 
+    void OnPaint(wxPaintEvent& event);
+    void OnPan(wxPanGestureEvent& event);
+    void OnZoom(wxZoomGestureEvent& event);
+    void OnRotate(wxRotateGestureEvent& event);
+ 
+private:
+    wxBitmap m_bitmap;
+};
