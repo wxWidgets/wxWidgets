a6b5feda41d8e2c200874413192bffe56c3d3cca
Prashantkn <ohhereim@gmail.com>
2017-06-21 02:37:37 +0530

    Add wxLog messages

--- samples/event/gestures.cpp
+++ samples/event/gestures.cpp
@@ -17,7 +17,7 @@ MyGestureFrame::MyGestureFrame() : wxFrame(NULL, wxID_ANY, wxT("Multi-touch Gest
 MyGesturePanel::MyGesturePanel(MyGestureFrame *parent) : wxPanel(parent, wxID_ANY)
 {
     wxInitAllImageHandlers();
-    if(!m_bitmap.LoadFile("image.jpg", wxBITMAP_TYPE_ANY))
+    if(!m_bitmap.LoadFile("../image/horse.jpg", wxBITMAP_TYPE_ANY))
     {
         wxLogError("Can't load the image");
     }
@@ -36,14 +36,63 @@ void MyGesturePanel::OnPaint(wxPaintEvent& event)
  
 void MyGesturePanel::OnPan(wxPanGestureEvent& event)
 {
+    static int previousLocationX, previousLocationY;
+    if(event.IsGestureStart())
+    {
+        previousLocationX = event.GetPosition().x;
+        previousLocationY = event.GetPosition().y;
+        wxLogMessage("Pan gesture started\n");
+        return ;
+    }
+ 
+    int panDeltaY = abs(event.GetPosition().y - previousLocationY);
+    int panDeltaX = abs(event.GetPosition().x - previousLocationX);
+    
+    wxDirection direction = event.GetPanDirection();
+
+    if(direction == wxUP)
+    {
+        wxLogMessage("Pan gesture performed in Up direction");
+    }
+    else if(direction == wxDOWN)
+    {
+        wxLogMessage("Pan gesture performed in Down direction");
+    }
+    else if(direction == wxRIGHT)
+    {
+        wxLogMessage("Pan gesture performed in Right direction");   
+    }
+    else
+    {
+        wxLogMessage("Pan gesture performed in Left direction");
+    }
+
+    wxLogMessage(wxT("with deltaX = %d and deltaY = %d\n"),panDeltaX, panDeltaY);
+ 
+    previousLocationX = event.GetPosition().x;
+    previousLocationY = event.GetPosition().y;
  
 }
 void MyGesturePanel::OnZoom(wxZoomGestureEvent& event)
 {
+    if(event.IsGestureStart())
+    {
+        wxLogMessage("Zoom gesture started\n");
+        return ;
+    }
 
+    wxLogMessage(wxT("Zoom gesture performed with zoom center at (%d, %d) and zoom factor = %f\n"),
+        event.GetPosition().x, event.GetPosition().y, event.GetZoomFactor());
 }
  
 void MyGesturePanel::OnRotate(wxRotateGestureEvent& event)
 {
- 
+    if(event.IsGestureStart())
+    {
+        wxLogMessage("Rotate gesture started\n");
+        return ;
+    }
+
+    wxLogMessage(wxT("Rotate gesture performed with rotation center at (%d, %d) and rotation angle = %f\n"),
+        event.GetPosition().x, event.GetPosition().y, event.GetAngle());
 }
