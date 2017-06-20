2d0e4322f43cd22505a4cb7cfb02bf3f79a7607e
Jose Lorenzo <josee.loren@gmail.com>
2017-06-20 01:21:27 +0100

    Trying to allocate memory for result string

--- src/common/webview.cpp
+++ src/common/webview.cpp
@@ -65,6 +65,7 @@ wxDEFINE_EVENT( wxEVT_WEBVIEW_TITLE_CHANGED, wxWebViewEvent );
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 wxDEFINE_EVENT( wxEVT_WEBVIEW_RUNSCRIPT_RESULT, wxWebViewEvent );
 =======
 wxDEFINE_EVENT( wxEVT_RUNSCRIPT_RESULT, wxWebViewEvent );
@@ -109,6 +110,8 @@ wxDEFINE_EVENT( wxEVT_WEBVIEW_RUNSCRIPT_RESULT, wxWebViewEvent );
 =======
 wxDEFINE_EVENT( wxEVT_WEBVIEW_RUNSCRIPT_RESULT, wxWebViewEvent );
 >>>>>>> Set up the new async enviroment
+=======
+>>>>>>> Trying to allocate memory for result string
 
 wxStringWebViewFactoryMap wxWebView::m_factoryMap;
 
