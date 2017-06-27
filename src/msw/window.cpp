a110668f1e22698cc17ae83ab87ea50ab97de8e1
Prashantkn <ohhereim@gmail.com>
2017-06-27 13:33:17 +0530

    Remove duplicate code and call CloseGestureInfoHandle if an event is processed

--- src/msw/window.cpp
+++ src/msw/window.cpp
@@ -3177,81 +3177,53 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
                 {
                     wxLogDebug("This is Not the window targeted by this gesture!");
                 }
-                    
+
+                int x = gestureInfo.ptsLocation.x;
+                int y = gestureInfo.ptsLocation.y;
+                ScreenToClient(&x, &y);
+
                 // dwID field is used to determine the type of gesture
                 switch(gestureInfo.dwID)
                 {
                     // Pan gesture
                     case GID_PAN:
                     {
-                        // ptsLocation(POINTS struct) is the current position of the pan
-                        int x = gestureInfo.ptsLocation.x;
-                        int y = gestureInfo.ptsLocation.y;
-                        // Convert them into client coordinates
-                        ScreenToClient(&x, &y);
+                        // (x,y) is the current position of the pan
                         processed = HandlePanGesture(x, y, gestureInfo.dwFlags);
-
-                        if(!processed)
-                        {
-                            // If not processed, we must call this to avoid memory leaks
-                            if(!::CloseGestureInfoHandle((HGESTUREINFO)lParam))
-                            {
-                                wxLogLastError(wxT("CloseGestureInfoHandle"));
-                            }
-                        }
                     }
                     break;
 
                     // Zoom gesture
                     case GID_ZOOM:
                     {
-                        // ptsLocation is the mid-point of the 2 fingers
-                        int x = gestureInfo.ptsLocation.x;
-                        int y = gestureInfo.ptsLocation.y;
-                        // Convert them into client coordinates
-                        ScreenToClient(&x, &y);
-                            
+                        // (x,y) is the mid-point of 2 fingers
                         // ullArgument field is a 64-bit unsigned integer, but the relevant information
                         // is in it's lower 4 bytes and represents distance between the fingers
                         // this is used to extract those lower 4 bytes
                         DWORD fingerDistance = (DWORD)((gestureInfo.ullArguments) & 0x00000000ffffffff);
                         processed = HandleZoomGesture(x, y, fingerDistance, gestureInfo.dwFlags);
-
-                        if(!processed)
-                        {
-                            // If not processed, we must call this to avoid memory leaks
-                            if(!::CloseGestureInfoHandle((HGESTUREINFO)lParam))
-                            {
-                                wxLogLastError(wxT("CloseGestureInfoHandle"));
-                            }
-                        }
                     }    
                     break;
 
                     // Rotate gesture
                     case GID_ROTATE:
                     {
-                        // ptsLocation is the center point of rotation
-                        int x = gestureInfo.ptsLocation.x;
-                        int y = gestureInfo.ptsLocation.y;
-                        // Convert them into client coordinates
-                        ScreenToClient(&x, &y);
-
+                        // (x,y) is the center point of rotation
                         // Again, we need the lower 4 bytes and this will used as an argument
                         // to obtain the angle to rotate
                         DWORD angleArgument = (DWORD)((gestureInfo.ullArguments) & 0x00000000ffffffff);
                         processed = HandleRotateGesture(x, y, angleArgument, gestureInfo.dwFlags);
-
-                        if(!processed)
-                        {
-                            // If not processed, we must call this to avoid memory leaks
-                            if(!::CloseGestureInfoHandle((HGESTUREINFO)lParam))
-                            {
-                                wxLogLastError(wxT("CloseGestureInfoHandle"));
-                            }
-                        }
                     }
                     break;
+                }
+
+                if(processed)
+                {
+                    // If processed, we must call this to avoid memory leaks
+                    if(!::CloseGestureInfoHandle((HGESTUREINFO)lParam))
+                    {
+                         wxLogLastError(wxT("CloseGestureInfoHandle"));
+                    }
                 }
             }
             break;
