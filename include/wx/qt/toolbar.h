f8c14d1176cbbed9e8265e6cde24e6485531d13d
Scott Furry <scott.wl.furry@gmail.com>
2017-07-14 16:53:11 +0200

    Define more wxToolBar::SetToolXXX() methods in wxQt
    Provide a stub for SetToolShortHelp() and really implement
    SetTool{Normal,Disabled}Bitmap().

--- include/wx/qt/toolbar.h
+++ include/wx/qt/toolbar.h
@@ -43,6 +43,11 @@ public:
     virtual QToolBar *GetQToolBar() const { return m_qtToolBar; }
 
     virtual void SetWindowStyleFlag( long style ) wxOVERRIDE;
+
+    virtual void SetToolShortHelp(int id, const wxString& helpString) wxOVERRIDE;
+    virtual void SetToolNormalBitmap(int id, const wxBitmap& bitmap) wxOVERRIDE;
+    virtual void SetToolDisabledBitmap(int id, const wxBitmap& bitmap) wxOVERRIDE;
+
     virtual bool Realize() wxOVERRIDE;
 
     virtual wxToolBarToolBase *CreateTool(int toolid,
@@ -59,7 +64,6 @@ public:
     QWidget *GetHandle() const wxOVERRIDE;
 
 protected:
-
     QActionGroup* GetActionGroup(size_t pos);
     virtual bool DoInsertTool(size_t pos, wxToolBarToolBase *tool) wxOVERRIDE;
     virtual bool DoDeleteTool(size_t pos, wxToolBarToolBase *tool) wxOVERRIDE;
