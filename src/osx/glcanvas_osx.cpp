3063ea6ca9590f78875860a3bcd3be02a06bfc73
Stefan Csomor <csomor@advancedconcepts.ch>
2017-09-25 17:42:11 +0200

    Fixing Creation of wxGLCanvas on iOS

--- src/osx/glcanvas_osx.cpp
+++ src/osx/glcanvas_osx.cpp
@@ -501,9 +501,14 @@ bool wxGLCanvas::Create(wxWindow *parent,
     // Make a copy of attributes. Will use at wxGLContext ctor
     m_GLAttrs = dispAttrs;
 
+#if wxOSX_USE_IPHONE
+    if ( !wxGLCanvas::DoCreate(parent,id,pos,size,style,name) )
+        return false;
+#else
     if ( !wxWindow::Create(parent, id, pos, size, style, name) )
         return false;
-
+#endif
+    
     return true;
 }
 
