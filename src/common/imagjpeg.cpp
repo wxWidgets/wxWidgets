cc8657e4363a4670994f2907755d1f7b43d586e9
Vadim Zeitlin <vadim@wxwidgets.org>
2017-11-12 16:32:35 +0100

    Upgrade libjpeg to the latest version 9b
    There don't seem to be any really important changes affecting our use of
    the library, but it seems better to use the 2016 version instead of the
    1998 one and it also allows to get rid of many hacks that were needed
    before as the things they worked around are now taken care of by the
    library itself.

--- src/common/imagjpeg.cpp
+++ src/common/imagjpeg.cpp
@@ -37,17 +37,7 @@
     #define boolean wxHACK_BOOLEAN
 #endif
 
-extern "C"
-{
-    #if defined(__WXMSW__)
-        #define XMD_H
-    #endif
-    #include "jpeglib.h"
-}
-
-#ifndef HAVE_WXJPEG_BOOLEAN
-typedef boolean wxjpeg_boolean;
-#endif
+#include "jpeglib.h"
 
 #include "wx/filefn.h"
 #include "wx/wfstream.h"
@@ -103,7 +93,7 @@ CPP_METHODDEF(void) wx_init_source ( j_decompress_ptr WXUNUSED(cinfo) )
 {
 }
 
-CPP_METHODDEF(wxjpeg_boolean) wx_fill_input_buffer ( j_decompress_ptr cinfo )
+CPP_METHODDEF(boolean) wx_fill_input_buffer ( j_decompress_ptr cinfo )
 {
     wx_src_ptr src = (wx_src_ptr) cinfo->src;
 
@@ -386,7 +376,7 @@ CPP_METHODDEF(void) wx_init_destination (j_compress_ptr cinfo)
     dest->pub.free_in_buffer = OUTPUT_BUF_SIZE;
 }
 
-CPP_METHODDEF(wxjpeg_boolean) wx_empty_output_buffer (j_compress_ptr cinfo)
+CPP_METHODDEF(boolean) wx_empty_output_buffer (j_compress_ptr cinfo)
 {
     wx_dest_ptr dest = (wx_dest_ptr) cinfo->dest;
 
