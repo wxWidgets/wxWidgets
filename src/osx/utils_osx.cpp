6b75b2fdc472768dcb97905e5b93771944f549ec
Tim Kosse <tim.kosse@filezilla-project.org>
2017-12-04 13:15:44 +0100

    Fix crash when switching between monitors in wxOSX
    It is possible for CGDisplayModeCopyPixelEncoding to return NULL, e.g.
    during a switch between the built-in screen and an external monitor on a
    Macbook. Check the return value to prevent a crash.
    
    See #18015.
    
    (cherry-pick of 26f48c225e3ac397ec1ba5de2e135d630dbc2e4b from master)

--- src/osx/utils_osx.cpp
+++ src/osx/utils_osx.cpp
@@ -77,16 +77,18 @@ int wxDisplayDepth()
         CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(kCGDirectMainDisplay);
         CFStringRef encoding = CGDisplayModeCopyPixelEncoding(currentMode);
         
-        if(CFStringCompare(encoding, CFSTR(IO32BitDirectPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
-            theDepth = 32;
-        else if(CFStringCompare(encoding, CFSTR(IO16BitDirectPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
-            theDepth = 16;
-        else if(CFStringCompare(encoding, CFSTR(IO8BitIndexedPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
-            theDepth = 8;
-        else
-            theDepth = 32; // some reasonable default
+        theDepth = 32; // some reasonable default
+        if(encoding)
+        {
+            if(CFStringCompare(encoding, CFSTR(IO32BitDirectPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
+                theDepth = 32;
+            else if(CFStringCompare(encoding, CFSTR(IO16BitDirectPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
+                theDepth = 16;
+            else if(CFStringCompare(encoding, CFSTR(IO8BitIndexedPixels), kCFCompareCaseInsensitive) == kCFCompareEqualTo)
+                theDepth = 8;
+            CFRelease(encoding);
+        }
 
-        CFRelease(encoding);
         CGDisplayModeRelease(currentMode);
     }
     else
