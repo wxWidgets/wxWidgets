wxWidgets/wxWidgets

commit 72fe57ec182ba0bc9a51ad27adc883a945d39b81

Use wxTextEntry::Clear() in wxOwnerDrawnComboBox::Clear()
This seems more natural than calling SetValue(wxEmptyString).

No real changes.

src/generic/odcombo.cpp
@@ -991,9 +991,7 @@ void wxOwnerDrawnComboBox::DoClear()
 
     GetVListBoxComboPopup()->Clear();
 
-    // NB: This really needs to be SetValue() instead of ChangeValue(),
-    //     as wxTextEntry API expects an event to be sent.
-    SetValue(wxEmptyString);
+    wxTextEntry::Clear();
 }
 
 void wxOwnerDrawnComboBox::Clear()

commit f0d5e9541254f5e0ae81dc55fb667d101cf55a6a

Fix crash when clearing read-only wxOwnerDrawnComboBox
Check that we have an associated text entry before clearing it.

Fixes a crash introduced by 72fe57ec182ba0bc9a51ad27adc883a945d39b81
without reverting it as it still seems reasonable to use Clear() here.

Closes #18013.

src/generic/odcombo.cpp
@@ -991,7 +991,9 @@ void wxOwnerDrawnComboBox::DoClear()
 
     GetVListBoxComboPopup()->Clear();
 
-    wxTextEntry::Clear();
+    // There is no text entry when using wxCB_READONLY style, so test for it.
+    if ( GetTextCtrl() )
+        wxTextEntry::Clear();
 }
 
 void wxOwnerDrawnComboBox::Clear()