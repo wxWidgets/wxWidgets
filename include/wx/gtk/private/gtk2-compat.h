05454e778c14fa579cfc87b14b8541f52c61a855
Paul Cornett <paulcor@users.noreply.github.com>
2018-03-12 10:06:23 -0700

    Build fix for GTK+ < 2.18
    Broken by 986f61f33ea

--- include/wx/gtk/private/gtk2-compat.h
+++ include/wx/gtk/private/gtk2-compat.h
@@ -256,6 +256,11 @@ inline void wx_gtk_widget_set_can_focus(GtkWidget *widget, gboolean can)
 }
 #define gtk_widget_set_can_focus wx_gtk_widget_set_can_focus
 
+static inline gboolean wx_gtk_widget_has_focus(GtkWidget* widget)
+{
+    return GTK_WIDGET_HAS_FOCUS(widget);
+}
+#define gtk_widget_has_focus wx_gtk_widget_has_focus
 
 inline gboolean wx_gtk_widget_get_can_default(GtkWidget *widget)
 {
