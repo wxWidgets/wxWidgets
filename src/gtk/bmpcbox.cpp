cfb5084ce6f21409d0893cb6cbc14dd23784b831
Paul Cornett <paulcor@users.noreply.github.com>
2018-06-06 09:09:12 -0700

    Avoid GLib warning "couldn't find weak ref" with wxBitmapComboBox
    Should have been part of c391cfd617
    
    (cherry picked from commit b839388c8286b3ff34901d3055f9d70bf3af14e4)

--- src/gtk/bmpcbox.cpp
+++ src/gtk/bmpcbox.cpp
@@ -128,6 +128,7 @@ void wxBitmapComboBox::GTKCreateComboBoxWidget()
         m_widget = gtk_combo_box_entry_new_with_model( GTK_TREE_MODEL(store), m_stringCellIndex );
 #endif
         m_entry = GTK_ENTRY(gtk_bin_get_child(GTK_BIN(m_widget)));
+        g_object_add_weak_pointer(G_OBJECT(m_entry), (void**)&m_entry);
         gtk_editable_set_editable(GTK_EDITABLE(m_entry), true);
     }
     g_object_ref(m_widget);
