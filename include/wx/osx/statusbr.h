caa2fde425b9d5fd87d1e915c2b177d1da4b35f9
Stefan Csomor <csomor@advancedconcepts.ch>
2018-06-15 10:06:17 +0200

    added missing override

--- include/wx/osx/statusbr.h
+++ include/wx/osx/statusbr.h
@@ -35,7 +35,7 @@ protected:
     virtual void DrawField(wxDC& dc, int i, int textHeight) wxOVERRIDE;
     virtual void DoUpdateStatusText(int number = 0) wxOVERRIDE;
 
-    virtual void InitColours();
+    virtual void InitColours() wxOVERRIDE;
 
 private:
     wxColour m_textActive, m_textInactive,
