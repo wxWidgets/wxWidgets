2842108e241f49c7e1e115cfe44d22bfaf68819f
PB <pbfordev@gmail.com>
2018-06-25 09:36:43 +0200

    Fix typos in container classes descriptions
    Closes https://github.com/wxWidgets/wxWidgets/pull/845

--- docs/doxygen/mainpages/cat_classes.h
+++ docs/doxygen/mainpages/cat_classes.h
@@ -632,13 +632,13 @@ these classes provide a subset or almost complete STL API.
 
 Related Overviews: @ref overview_container
 
-@li wxArray<T>: Legacy dynamic array, use srd::vector or wxVector instead.
+@li wxArray<T>: A legacy dynamic array, use std::vector or wxVector instead.
 @li wxArrayString: An efficient container for storing wxString objects
 @li wxHashMap<T>: A type-safe hash map implementation (macro based)
 @li wxHashSet<T>: A type-safe hash set implementation(macro based)
 @li wxHashTable: A simple hash table implementation (deprecated, use wxHashMap)
 @li wxList<T>: A type-safe linked list implementation (macro based)
-@li wxVector<T>: Template base vector implementation identical to std::vector.
+@li wxVector<T>: A template-based vector implementation identical to std::vector
 
 
 
