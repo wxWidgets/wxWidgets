a02efd1fc7eeffcf3ab2fc9805da03c98c5cd051
Vitaly Stakhovsky <vvs31415@gitlab.com>
2018-07-30 15:04:36 +0200

    Implement wxFontDialog::SetTitle() in wxMSW
    Base class SetTitle() implementation didnd't work for this class as it
    used the (invalid) HWND of not yet existing dialog, so add a hook
    procedure for the common font dialog, similar to the existing one for
    wxColourDialog, which allows us to set the dialog title when the dialog
    is really created.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/865
    
    Closes #18177.

--- include/wx/msw/fontdlg.h
+++ include/wx/msw/fontdlg.h
@@ -25,8 +25,12 @@ public:
         : wxFontDialogBase(parent, data) { Create(parent, data); }
 
     virtual int ShowModal() wxOVERRIDE;
+    virtual void SetTitle(const wxString& title) wxOVERRIDE;
+    virtual wxString GetTitle() const wxOVERRIDE;
 
 protected:
+    wxString m_title;
+
     wxDECLARE_DYNAMIC_CLASS_NO_COPY(wxFontDialog);
 };
 
