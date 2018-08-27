f57285605151528afba4133e8aac3739d921bf59
Vadim Zeitlin <vadim@wxwidgets.org>
2018-08-27 02:01:37 +0200

    Fix ifacecheck utility build after array macro changes
    Previously it was somehow possible to implicitly convert "false" to
    wxMethodPtrArray (which is an array of wxMethod pointers), but this,
    correctly, doesn't compile any more, so return an empty array instead.

--- utils/ifacecheck/src/xmlparser.cpp
+++ utils/ifacecheck/src/xmlparser.cpp
@@ -613,7 +613,7 @@ wxMethodPtrArray wxClass::RecursiveUpwardFindMethodsNamed(const wxString& name,
             if (!parent) {
                 wxLogError("Could not find parent '%s' of class '%s'...",
                          m_parents[i], GetName());
-                return false;
+                return wxMethodPtrArray();
             }
 
             wxMethodPtrArray temp = parent->RecursiveUpwardFindMethodsNamed(name, allclasses);
