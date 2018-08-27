372daae9dcf7982fd56c5c1cf833892f1ddbe022
Hartwig <git@skywind.eu>
2018-08-27 21:07:11 +0200

    Remove stray backspace character from wxCocoaDataViewControl
    Somehow, a raw ASCII ^H made its way into this source file. Remove it.
    
    See https://github.com/wxWidgets/wxWidgets/pull/901

--- src/osx/cocoa/dataview.mm
+++ src/osx/cocoa/dataview.mm
@@ -2520,7 +2520,7 @@ void wxCocoaDataViewControl::SetRowHeight(int height)
 
 int wxCocoaDataViewControl::GetDefaultRowHeight() const
 {
-    // Custom setup of NSLayoutManager is necessary to match NSTableView sizing.
+    // Custom setup of NSLayoutManager is necessary to match NSTableView sizing.
     // See https://stackoverflow.com/questions/17095927/dynamically-changing-row-height-after-font-size-of-entire-nstableview-nsoutlin
     NSLayoutManager *lm = [[NSLayoutManager alloc] init];
     [lm setTypesetterBehavior:NSTypesetterBehavior_10_2_WithCompatibility];
