398e8744bdb48b78c3d9f690c861306c69b22313
Václav Slavík <vaclav@slavik.io>
2018-09-16 14:03:11 +0200

    Fix TLW appearance in macOS 10.14's dark mode
    Set toplevel window's background colour to the correct value of
    NSColor.windowBackgroundColor (i.e. wxSYS_COLOUR_APPWORKSPACE). Without this,
    the window's frame would be rendered slightly incorrectly, because of the way
    the dark mode blends colors - even when no part of the window's interior, where
    the background color would be used, is visible.

--- src/osx/nonownedwnd_osx.cpp
+++ src/osx/nonownedwnd_osx.cpp
@@ -152,7 +152,7 @@ bool wxNonOwnedWindow::Create(wxWindow *parent,
     wxWindowCreateEvent event(this);
     HandleWindowEvent(event);
 
-    SetBackgroundColour(wxSystemSettings::GetColour( wxSYS_COLOUR_3DFACE ));
+    SetBackgroundColour(wxSystemSettings::GetColour( wxSYS_COLOUR_APPWORKSPACE ));
 
     if ( parent )
         parent->AddChild(this);
