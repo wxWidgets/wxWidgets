4c95867c1297d13d203168098161a23732dcb747
Stefan Csomor <csomor@advancedconcepts.ch>
2018-09-20 20:53:24 +0200

    fixing graphics state stack error

--- src/osx/carbon/utilscocoa.mm
+++ src/osx/carbon/utilscocoa.mm
@@ -266,13 +266,11 @@ CGContextRef WXDLLIMPEXP_CORE wxOSXCreateBitmapContextFromNSImage( WX_NSImage ns
         
         hbitmap = CGBitmapContextCreate(NULL, imageSize.width*scale, imageSize.height*scale, 8, 0, wxMacGetGenericRGBColorSpace(), kCGImageAlphaPremultipliedFirst);
         CGContextScaleCTM( hbitmap, scale, scale );
+        CGContextClearRect(hbitmap,CGRectMake(0, 0, imageSize.width, imageSize.height));
     
         NSGraphicsContext *previousContext = [NSGraphicsContext currentContext];
         NSGraphicsContext *nsGraphicsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:hbitmap flipped:NO];
-        [NSGraphicsContext saveGraphicsState];
         [NSGraphicsContext setCurrentContext:nsGraphicsContext];
-        [[NSColor whiteColor] setFill];
-        NSRectFill(NSMakeRect(0.0, 0.0, imageSize.width, imageSize.height));
         [nsimage drawAtPoint:NSZeroPoint fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
         [NSGraphicsContext setCurrentContext:previousContext];
 
