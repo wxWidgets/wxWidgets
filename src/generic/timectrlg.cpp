41898c8dc406aef32b959d27eac70524b76b9b9a
Artur Wieczorek <artwik@wp.pl>
2018-11-10 14:49:52 +0100

    Compilation fix for wxUSE_INTL=0 build

--- src/generic/timectrlg.cpp
+++ src/generic/timectrlg.cpp
@@ -86,7 +86,7 @@ public:
         // nice to add support to "%k" and "%l" (hours with leading blanks
         // instead of zeros) too as this is the most common unsupported case in
         // practice.
-#if wxUSE_XLOCALE
+#if wxUSE_INTL
         m_useAMPM = wxLocale::GetInfo(wxLOCALE_TIME_FMT).Contains("%p");
 #else
         m_useAMPM = false;
