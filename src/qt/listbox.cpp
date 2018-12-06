3e79f903b458fbe90ed6e099b53e2502a2f8f7b5
chris2oph <chris.lemin@gmail.com>
2018-12-06 09:52:08 +0000

    Make wxListBox::SetString() actually work in wxQt
    Do set the string on QListWidgetItem.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/1047

--- src/qt/listbox.cpp
+++ src/qt/listbox.cpp
@@ -160,7 +160,7 @@ wxString wxListBox::GetString(unsigned int n) const
     return wxQtConvertString( item->text() );
 }
 
-void wxListBox::SetString(unsigned int n, const wxString& WXUNUSED(s))
+void wxListBox::SetString(unsigned int n, const wxString& s)
 {
     QListWidgetItem* item = m_qtListWidget->item(n);
     wxCHECK_RET(item != NULL, wxT("wrong listbox index") );
@@ -169,6 +169,7 @@ void wxListBox::SetString(unsigned int n, const wxString& WXUNUSED(s))
         item->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsSelectable);
         item->setCheckState(Qt::Unchecked);
     }
+    item->setText(wxQtConvertString(s));
 }
 
 int wxListBox::GetSelection() const
