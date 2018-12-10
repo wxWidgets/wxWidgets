9a873ef7f829d210fa16c73860af8c551ddf7db4
Graham Dawes <graham.dawes@geoteric.com>
2018-12-10 09:26:10 +0000

    Applied review suggestions and attempted to fix CI build error

--- src/qt/fontenum.cpp
+++ src/qt/fontenum.cpp
@@ -8,18 +8,23 @@
 // For compilers that support precompilation, includes "wx.h".
 #include "wx/wxprec.h"
 #include "wx/fontenum.h"
-#include <QFontDatabase>
 
-bool wxFontEnumerator::EnumerateFacenames( wxFontEncoding WXUNUSED(encoding), bool fixedWidthOnly)
+#include <QtCore/QStringList>
+#include <QtGui/QFontDatabase>
+
+bool wxFontEnumerator::EnumerateFacenames(wxFontEncoding WXUNUSED(encoding), bool fixedWidthOnly)
 {
     QFontDatabase fontDatabase;
     const QStringList allFonts = fontDatabase.families(QFontDatabase::Any);
-    for( QStringList::const_iterator i = allFonts.begin(); i != allFonts.end(); ++i)
+    for( QStringList::const_iterator i = allFonts.begin(); i != allFonts.end(); ++i )
     {
-        const QString &fontFamily = *i;
+        const QString& fontFamily = *i;
         if ( !fixedWidthOnly || fontDatabase.isFixedPitch(fontFamily) )
         {
-            this->OnFacename(fontFamily.toStdString());
+            if ( !OnFacename(fontFamily.toStdString()))
+            {
+                return false;
+            }
         }
     }
 
