4f05b3c8f44573e070110df107a840da69675ae3
Liam Treacy <liam.treacy@gmail.com>
2018-12-12 09:59:36 +0000

    Changing AND operation to bitwise AND

--- src/qt/radiobut.cpp
+++ src/qt/radiobut.cpp
@@ -41,7 +41,7 @@ bool wxRadioButton::Create( wxWindow *parent,
 {
     m_qtRadioButton = new QRadioButton( parent->GetHandle() );
 
-    if ( ( style & wxRB_GROUP ) || ( style && wxRB_SINGLE ) )
+    if ( ( style & wxRB_GROUP ) || ( style & wxRB_SINGLE ) )
     {
         createAndJoinNewGroup();
     }
