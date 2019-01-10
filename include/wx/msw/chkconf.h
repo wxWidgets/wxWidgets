295e4140be804479f3edd6b59534deff58c62777
Tomay <tomay3000@gmail.com>
2019-01-10 04:44:29 +0100

    Update chkconf.h

--- include/wx/msw/chkconf.h
+++ include/wx/msw/chkconf.h
@@ -118,6 +118,14 @@
 #    endif
 #endif  /* wxUSE_UXTHEME */
 
+#ifndef wxUSE_WINSOCK2
+#    ifdef wxABORT_ON_CONFIG_ERROR
+#        error "wxUSE_WINSOCK2 must be defined."
+#    else
+#        define wxUSE_WINSOCK2 0
+#    endif
+#endif  /* wxUSE_WINSOCK2 */
+
 /*
  * Unfortunately we can't use compiler TLS support if the library can be used
  * inside a dynamically loaded DLL under Windows XP, as this can result in hard
