f4ea128007bfba6365158a2e6eaca9c81f627c5a
Vadim Zeitlin <vadim@wxwidgets.org>
2019-01-24 22:51:43 +0100

    Include wx/stopwatch.h explicitly from wxTextCtrl unit test
    Don't rely on it being implicitly included from another header when we
    use wxStopWatch in this file (with wxGTK only).

--- tests/controls/textctrltest.cpp
+++ tests/controls/textctrltest.cpp
@@ -26,6 +26,10 @@
 #include "wx/scopeguard.h"
 #include "wx/uiaction.h"
 
+#ifdef __WXGTK__
+    #include "wx/stopwatch.h"
+#endif
+
 #include "textentrytest.h"
 #include "testableframe.h"
 #include "asserthelper.h"
