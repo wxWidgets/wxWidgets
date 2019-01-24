7c831d25ee23591c9693a31c14391a91c6e16d01
Vadim Zeitlin <vadim@wxwidgets.org>
2019-01-24 22:51:06 +0100

    Group headers together in wxTextCtrl unit test
    No real changes.

--- tests/controls/textctrltest.cpp
+++ tests/controls/textctrltest.cpp
@@ -24,11 +24,11 @@
 #endif // WX_PRECOMP
 
 #include "wx/scopeguard.h"
+#include "wx/uiaction.h"
 
 #include "textentrytest.h"
 #include "testableframe.h"
 #include "asserthelper.h"
-#include "wx/uiaction.h"
 
 static const int TEXT_HEIGHT = 200;
 
