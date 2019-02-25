60fb8f93cb5db02077447c24063725170f5e87a3
Paul Cornett <paulcor@users.noreply.github.com>
2019-02-25 00:48:41 -0800

    Check for self-assignment in operator=

--- src/qt/bitmap.cpp
+++ src/qt/bitmap.cpp
@@ -478,9 +478,12 @@ wxMask::wxMask(const wxMask &mask)
 
 wxMask& wxMask::operator=(const wxMask &mask)
 {
-    delete m_qtBitmap;
-    QBitmap *mask_bmp = mask.GetHandle();
-    m_qtBitmap = mask_bmp ? new QBitmap(*mask_bmp) : NULL;
+    if (this != &mask)
+    {
+        delete m_qtBitmap;
+        QBitmap *mask_bmp = mask.GetHandle();
+        m_qtBitmap = mask_bmp ? new QBitmap(*mask_bmp) : NULL;
+    }
     return *this;
 }
 
--- src/qt/region.cpp
+++ src/qt/region.cpp
@@ -351,10 +351,12 @@ wxRegionIterator::~wxRegionIterator()
 
 wxRegionIterator& wxRegionIterator::operator=(const wxRegionIterator& ri)
 {
-    delete m_qtRects;
-
-    m_qtRects = new QVector< QRect >( *ri.m_qtRects );
-    m_pos = ri.m_pos;
+    if (this != &ri)
+    {
+        delete m_qtRects;
+        m_qtRects = new QVector< QRect >( *ri.m_qtRects );
+        m_pos = ri.m_pos;
+    }
     return *this;
 }
 
