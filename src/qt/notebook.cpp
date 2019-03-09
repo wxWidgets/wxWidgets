2d45276265bfdfe1c55439fecd2178df4f697116
Cătălin Răceanu <catalinraceanu2@gmail.com>
2019-03-10 01:36:18 +0200

    Do not explicitly set selection to -1 when deleting all pages

--- src/qt/notebook.cpp
+++ src/qt/notebook.cpp
@@ -167,12 +167,20 @@ wxSize wxNotebook::CalcSizeFromPage(const wxSize& sizePage) const
 
 bool wxNotebook::DeleteAllPages()
 {
+    // Nothing to do if the notebook was not created yet,
+    // and return true just like other ports do.
+    if ( !m_qtTabWidget )
+        return true;
+
+    // Block signals to not receive selection changed updates
+    // which are sent by Qt after the selected page was deleted.
+    const QSignalBlocker blocker(m_qtTabWidget);
+
+    // Pages will be deleted one by one in the base class.
+    // There's no need to explicitly clear() the Qt control.
     if ( !wxNotebookBase::DeleteAllPages() )
         return false;
 
-    m_qtTabWidget->blockSignals(true);
-    m_qtTabWidget->clear();
-    m_qtTabWidget->blockSignals(false);
     return true;
 }
 
