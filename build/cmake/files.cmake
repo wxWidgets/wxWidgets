32e3846e2774fb5b5d2c7ea0d1a735b839ae1040
Vadim Zeitlin <vadim@wxwidgets.org>
2019-06-29 19:29:14 +0200

    Remove private headers from the public headers list
    Private headers should not be installed and so must not be included in
    the HDR variables in the file lists.

--- build/cmake/files.cmake
+++ build/cmake/files.cmake
@@ -277,13 +277,6 @@ set(QT_HDR
     wx/generic/activityindicator.h
     ${QT_PLATFORM_HDR}
     wx/qt/treectrl.h
-    wx/qt/private/converter.h
-    wx/qt/private/winevent.h
-    wx/qt/private/timer.h
-    wx/qt/private/pointer.h
-    wx/qt/private/treeitemdelegate.h
-    wx/qt/private/treeitemfactory.h
-    wx/qt/private/utils.h
 )
 
 set(QT_SRC
