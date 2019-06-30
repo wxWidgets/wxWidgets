6ae5d4c9ae3b84b45c5291ff01e668cbbb3799a0
Stefano D. Mtangoo <stefano@hosannahighertech.co.tz>
2019-06-30 12:50:05 +0300

    Fix style to comply with wxWidgets standards

--- include/wx/stc/completionitem.h
+++ include/wx/stc/completionitem.h
@@ -7,19 +7,19 @@
 class WXDLLIMPEXP_STC wxCompletionItem
 {
 public:
-    wxCompletionItem(const wxString &name, int icon, const wxString &doc = wxString(), wxClientData *data = NULL);
+    wxCompletionItem(const wxString& name, int icon, const wxString& doc = wxString(), wxClientData* data = NULL);
     virtual ~wxCompletionItem();
 
-    const wxString &GetName();
+    const wxString& GetName();
     int GetIcon();
-    const wxString &GetDocumentation();
-    wxClientData *GetClientObject();
+    const wxString& GetDocumentation();
+    wxClientData* GetClientObject();
 
 protected:
     wxString m_name;
     int m_icon;
     wxString m_doc;
-    wxClientData *m_data;
+    wxClientData* m_data;
 };
 
 #endif //_WX_STC_COMPLETIONITEM_H_
--- src/stc/completionitem.cpp
+++ src/stc/completionitem.cpp
@@ -1,11 +1,11 @@
 #include "wx/stc/completionitem.h"
 
-wxCompletionItem::wxCompletionItem(const wxString &name, int icon, const wxString &doc, wxClientData *data)
+wxCompletionItem::wxCompletionItem(const wxString& name, int icon, const wxString& doc, wxClientData* data)
     : m_name(name), m_icon(icon), m_doc(doc), m_data(data)
 {
 }
 wxCompletionItem::~wxCompletionItem() {}
-const wxString &wxCompletionItem::GetName() { return m_name; }
+const wxString& wxCompletionItem::GetName() { return m_name; }
 int wxCompletionItem::GetIcon() { return m_icon; }
-const wxString &wxCompletionItem::GetDocumentation() { return m_doc; }
-wxClientData *wxCompletionItem::GetClientObject() { return m_data; }
+const wxString& wxCompletionItem::GetDocumentation() { return m_doc; }
+wxClientData* wxCompletionItem::GetClientObject() { return m_data; }
