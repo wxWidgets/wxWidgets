24aa514995acae33f61db52fe296561cd993aa35
Vadim Zeitlin <vadim@wxwidgets.org>
2019-07-26 12:41:56 +0200

    Remove redundant "break" statements in wxX11 code
    This was already done in 01c1dde264490a74ca8db1951280d5d26392030b (among
    other changes) in master.
    
    Closes #11745.

--- src/x11/settings.cpp
+++ src/x11/settings.cpp
@@ -43,18 +43,13 @@ wxFont wxSystemSettingsNative::GetFont(wxSystemFont index)
     switch (index)
     {
         case wxSYS_SYSTEM_FIXED_FONT:
-        {
             return wxFont(12, wxMODERN, wxNORMAL, wxNORMAL, FALSE);
-            break;
-        }
+
         case wxSYS_DEVICE_DEFAULT_FONT:
         case wxSYS_SYSTEM_FONT:
         case wxSYS_DEFAULT_GUI_FONT:
         default:
-        {
             return wxFont(12, wxSWISS, wxNORMAL, wxNORMAL, FALSE);
-            break;
-        }
     }
 
     return wxFont();
