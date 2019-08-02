11f24369fd03d578b70cb24124980983be9987f8
Graham Dawes <graham.dawes@geoteric.com>
2019-08-02 16:43:09 +0100

    Prevent selection by dragging in wxTreeCtrl in wxQt
    This prevented the user from dragging the items in wxTreeCtrl, which is
    something that's supposed to be possible.
    
    To allow selection by dragging, we'd need to extend wx API with a method
    indicating that dragging is not used in this particular control.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/1458

--- src/qt/treectrl.cpp
+++ src/qt/treectrl.cpp
@@ -489,6 +489,11 @@ private:
         EmitEvent(tree_event);
     }
 
+    virtual QItemSelectionModel::SelectionFlags selectionCommand(const QModelIndex &index, const QEvent *event) const wxOVERRIDE
+    {
+        return state() == DragSelectingState ? QItemSelectionModel::NoUpdate : QTreeWidget::selectionCommand(index, event);
+    }
+
     virtual void dropEvent(QDropEvent* event) wxOVERRIDE
     {
         endDrag(event->pos());
