a113aca4732b4a820d71a40f6070f41391b5b27e
oneeyeman1 <ikorot01@gmail.com>
2019-08-31 18:01:35 -0500

    Fix OSX implementation

--- src/osx/cocoa/desktopenv.mm
+++ src/osx/cocoa/desktopenv.mm
@@ -26,35 +26,6 @@ wxDesktopEnv::~wxDesktopEnv()
 
 bool wxDesktopEnv::MoveFileToRecycleBin(const wxString &fileName)
 {
-    bool result = false;
-    if( wxDesktopEnvBase::MoveFileToRecycleBin( fileName ) )
-    {
-        if( wxDirExists( fileName ) )
-            result = MoveDirectoryToTrash( fileName );
-        else if( wxFileExists( fileName ) )
-            result = MoveFileToTrash( fileName );
-    }
-    return result;
-}
-
-bool wxDesktopEnv::MoveDirectoryToTrash(const wxString &dirName)
-{
-    bool ret = true;
-    wxString temp = "file:///" + dirName;
-    NSURL *url = [NSURL URLWithString:wxCFStringRef( temp ).AsNSString()];
-    NSArray *files = [NSArray arrayWithObject:url];
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-    BOOL result = [[NSFileManager defaultManager] trashItemAtURL:url resultingItemURL:nil error:nil];
-    if( result == NO )
-        ret = false;
-#else
-    OSStatus status = FSPathMoveObjectToTrashSync( dirName.c_str(), NULL, kFSFileOperationDefaultOptions );
-#endif
-    return ret;
-}
-
-bool wxDesktopEnv::MoveFileToTrash(const wxString &fileName)
-{
     bool ret = true;
     wxString temp = "file:///" + fileName;
     NSURL *url = [NSURL URLWithString:wxCFStringRef( temp ).AsNSString()];
