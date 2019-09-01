95cc7e0f2d20989c578832c28b7b386bad4b2f54
oneeyeman1 <ikorot01@gmail.com>
2019-08-31 23:46:04 -0500

    Fix formatting

--- src/gtk/desktopenv.cpp
+++ src/gtk/desktopenv.cpp
@@ -43,7 +43,7 @@ bool wxDesktopEnv::MoveFileToRecycleBin(const wxString &fileName)
         if( !res || ( error && error->code == G_IO_ERROR_NOT_SUPPORTED ) )
            wxLogSysError( _( "Failed to move file '%s' to Recycle Bin" ), fileName.c_str() );
         else
-	    {
+        {
             result = true;
             g_object_unref( f );
         }
