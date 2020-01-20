724621929d4f061d2a35dfcb0f92f7e5c8b6594e
Vadim Zeitlin <vadim@wxwidgets.org>
2020-01-20 12:42:18 +0100

    Improve the check for TLW in focus handling code
    Use IsTopNavigationDomain() instead of IsTopLevel() to do the right
    thing for MDI child frames too.

--- src/common/containr.cpp
+++ src/common/containr.cpp
@@ -711,8 +711,15 @@ bool wxSetFocusToChild(wxWindow *win, wxWindow **childLastFocused)
                 else
                     deepestVisibleWindow = NULL;
 
-                if ( (*childLastFocused)->IsTopLevel() )
+                // We shouldn't be looking for the child to focus beyond the
+                // TLW boundary. And we use IsTopNavigationDomain() here
+                // instead of IsTopLevel() because wxMDIChildFrame is also TLW
+                // from this point of view.
+                if ( (*childLastFocused)->
+                        IsTopNavigationDomain(wxWindow::Navigation_Tab) )
+                {
                     break;
+                }
 
                 *childLastFocused = (*childLastFocused)->GetParent();
             }
