1a9da25c3376efd617a532ef369e984c99f0b84b
Paul Cornett <paulcor@users.noreply.github.com>
2020-02-03 09:33:09 -0800

    STL build fix for MSVS after 948ddc6e0f

--- include/wx/dynarray.h
+++ include/wx/dynarray.h
@@ -391,7 +391,7 @@ public:
 
     void Insert(const T* pItem, size_t uiIndex)
     {
-        base::insert(this->begin() + uiIndex, pItem);
+        base::insert(this->begin() + uiIndex, const_cast<T*>(pItem));
     }
 
     void Empty() { DoEmpty(); base::clear(); }
