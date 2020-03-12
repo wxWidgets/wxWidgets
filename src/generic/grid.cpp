40cbea298dfd287449537859ce43db0328411980
ali kettab <aliket1435@gmail.com>
2020-03-12 22:04:41 +0100

    Make wxGrid::SetLabelXXX() work when using native header as well
    Allow changing label colours and font of the native column header too.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/1762
    
    Closes #18605.

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -6497,7 +6497,14 @@ bool wxGrid::UseNativeColHeader(bool native)
     CreateColumnWindow();
 
     if ( m_useNativeHeader )
+    {
         SetNativeHeaderColCount();
+
+        wxHeaderCtrl* const colHeader = GetGridColHeader();
+        colHeader->SetBackgroundColour( GetLabelBackgroundColour() );
+        colHeader->SetForegroundColour( GetLabelTextColour() );
+        colHeader->SetFont( GetLabelFont() );
+    }
 
     CalcWindowSizes();
 
@@ -7980,6 +7987,10 @@ void wxGrid::SetLabelTextColour( const wxColour& colour )
     if ( m_labelTextColour != colour )
     {
         m_labelTextColour = colour;
+
+        if ( IsUsingNativeHeader() )
+            GetGridColHeader()->SetForegroundColour( colour );
+
         if ( ShouldRefresh() )
         {
             m_rowLabelWin->Refresh();
@@ -7991,6 +8002,10 @@ void wxGrid::SetLabelTextColour( const wxColour& colour )
 void wxGrid::SetLabelFont( const wxFont& font )
 {
     m_labelFont = font;
+
+    if ( IsUsingNativeHeader() )
+        GetGridColHeader()->SetFont( font );
+
     if ( ShouldRefresh() )
     {
         m_rowLabelWin->Refresh();
