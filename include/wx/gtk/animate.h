868154ee905002ad64410126e1f5c2e1a6969f82
Robin Dunn <>
2020-03-30 15:01:21 -0700

    native wxAnimationCtrl derives from the generic class

--- include/wx/gtk/animate.h
+++ include/wx/gtk/animate.h
@@ -89,7 +89,7 @@ private:
 // Resize to animation size if this is set
 #define wxAN_FIT_ANIMATION       0x0010
 
-class WXDLLIMPEXP_ADV wxAnimationCtrl: public wxAnimationCtrlBase
+class WXDLLIMPEXP_ADV wxAnimationCtrl: public wxGenericAnimationCtrl
 {
 public:
     wxAnimationCtrl() { Init(); }
