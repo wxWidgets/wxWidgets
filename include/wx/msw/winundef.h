wxWidgets/wxWidgets

commit 948ddc6e0f76fb69ddc17859c29a821290a7969f

Eliminate -Wcast-qual warnings with GCC and Clang
Use const_cast, mutable, and various other changes to avoid -Wcast-qual

include/wx/msw/winundef.h
@@ -263,12 +263,12 @@
    #if wxUSE_UNICODE_WINDOWS_H
    inline int StartDoc(HDC h, CONST DOCINFOW* info)
    {
-      return StartDocW(h, (DOCINFOW*) info);
+      return StartDocW(h, const_cast<DOCINFOW*>(info));
    }
    #else
    inline int StartDoc(HDC h, CONST DOCINFOA* info)
    {
-      return StartDocA(h, (DOCINFOA*) info);
+      return StartDocA(h, const_cast<DOCINFOA*>(info);
    }
    #endif
 #endif

commit adb9ba9984c1c96e8b271e23ab9c0d611b2ecfac

Fix broken non-Unicode MSW build
Add a missing parenthesis in a non-Unicode StartDoc() definition (broken in 948ddc6).

include/wx/msw/winundef.h
@@ -268,7 +268,7 @@
    #else
    inline int StartDoc(HDC h, CONST DOCINFOA* info)
    {
-      return StartDocA(h, const_cast<DOCINFOA*>(info);
+      return StartDocA(h, const_cast<DOCINFOA*>(info));
    }
    #endif
 #endif