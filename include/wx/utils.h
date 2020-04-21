e54abac9ffd6a1eacad1e7e6690d564a00cc2f11
Arrigo Marchiori <ardovm@yahoo.it>
2020-04-21 12:19:59 +0200

    allow wx/utils.h to be included on its own

--- include/wx/utils.h
+++ include/wx/utils.h
@@ -15,6 +15,7 @@
 // headers
 // ----------------------------------------------------------------------------
 
+#include "wx/defs.h"
 #include "wx/windowid.h"
 #include "wx/object.h"
 #include "wx/list.h"
