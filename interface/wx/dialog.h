bbc88cedf118e98157651cf7264345addd4623ef
Dummy <dummy@trac.wxwidgets.org>
2020-04-28 18:52:49 +0200

    Fix wrong documentation of EVT_CLOSE for the dialogs
    Clicking OK/Cancel buttons closes the dialog (by default), but doesn't
    generate wxEVT_CLOSE event, so don't write that it does.
    
    Closes #18743.

--- interface/wx/dialog.h
+++ interface/wx/dialog.h
@@ -140,8 +140,7 @@ enum wxDialogLayoutAdaptationMode
         The dialog is being closed by the user or programmatically (see wxWindow::Close).
         The user may generate this event clicking the close button
         (typically the 'X' on the top-right of the title bar) if it's present
-        (see the @c wxCLOSE_BOX style) or by clicking a button with the
-        @c wxID_CANCEL or @c wxID_OK ids.
+        (see the @c wxCLOSE_BOX style).
     @event{EVT_INIT_DIALOG(func)}
         Process a @c wxEVT_INIT_DIALOG event. See wxInitDialogEvent.
     @endEventTable
