wxWidgets/wxWidgets

commit 67c1c412c6239a582bb8d0f34115b4b2e99c7e8e

Implement support for copying wxGrid cells to clipboard
This is useful for read-only grids, in which an editor can't be shown to
copy the value of a cell, and also for copying an entire selection block
and not just a single cell.

Closes https://github.com/wxWidgets/wxWidgets/pull/1824

Closes #13562.

src/generic/grid.cpp
@@ -49,6 +49,10 @@
 #include "wx/headerctrl.h"
 #include "wx/hashset.h"
 
+#if wxUSE_CLIPBOARD
+    #include "wx/clipbrd.h"
+#endif // wxUSE_CLIPBOARD
+
 #include "wx/generic/gridsel.h"
 #include "wx/generic/gridctrl.h"
 #include "wx/generic/grideditors.h"
@@ -5873,6 +5877,81 @@ void wxGrid::OnKeyDown( wxKeyEvent& event )
                         m_selection->ExtendCurrentBlock(selStart, selEnd, event);
                 }
                 break;
+
+#if wxUSE_CLIPBOARD
+            case WXK_INSERT:
+            case 'C':
+                if ( event.GetModifiers() == wxMOD_CONTROL )
+                {
+                    // Coordinates of the selected block to copy to clipboard.
+                    wxGridBlockCoords sel;
+
+                    // Check if we have any selected blocks and if we don't
+                    // have too many of them.
+                    const wxGridBlocks blocks = GetSelectedBlocks();
+                    wxGridBlocks::iterator iter = blocks.begin();
+                    if ( iter == blocks.end() )
+                    {
+                        // No selection, copy just the current cell.
+                        if ( m_currentCellCoords == wxGridNoCellCoords )
+                        {
+                            // But we don't even have it -- nothing to do then.
+                            event.Skip();
+                            break;
+                        }
+
+                        sel = wxGridBlockCoords(GetGridCursorRow(),
+                                                GetGridCursorCol(),
+                                                GetGridCursorRow(),
+                                                GetGridCursorCol());
+                    }
+                    else // We do have at least one selected block.
+                    {
+                        sel = *blocks.begin();
+
+                        if ( ++iter != blocks.end() )
+                        {
+                            // As we use simple text format, we can't copy more
+                            // than one block to clipboard.
+                            wxLogWarning
+                            (
+                                _("Copying more than one selected block "
+                                  "to clipboard is not supported.")
+                            );
+                            break;
+                        }
+                    }
+
+                    wxClipboardLocker lockClipboard;
+                    if ( !lockClipboard )
+                    {
+                        // Error message should have been already given and we
+                        // don't have much to add.
+                        break;
+                    }
+
+                    wxString buf;
+                    for ( int row = sel.GetTopRow(); row <= sel.GetBottomRow(); row++ )
+                    {
+                        bool first = true;
+
+                        for ( int col = sel.GetLeftCol(); col <= sel.GetRightCol(); col++ )
+                        {
+                            if ( first )
+                                first = false;
+                            else
+                                buf += '\t';
+
+                            buf += GetCellValue(row, col);
+                        }
+
+                        buf += wxTextFile::GetEOL();
+                    }
+
+                    wxTheClipboard->SetData(new wxTextDataObject(buf));
+                }
+                break;
+#endif // wxUSE_CLIPBOARD
 
             default:
                 event.Skip();

commit 37b2918c9cea32c395fbcdeaa2eb580098193016

Don't consume all 'C'/Insert key presses in wxGrid
Skip the key events other than Ctrl-C/Ins which are used for copying
grid contents to the clipboard, notably Alt-C which should still be
usable for opening the menus.

This fixes a problems introduced in 67c1c412c6 (Implement support for
copying wxGrid cells to clipboard, 2020-04-26), see #13562.

src/generic/grid.cpp
@@ -5955,8 +5955,9 @@ void wxGrid::OnKeyDown( wxKeyEvent& event )
                     }
 
                     wxTheClipboard->SetData(new wxTextDataObject(buf));
+                    break;
                 }
-                break;
+                wxFALLTHROUGH;
 #endif // wxUSE_CLIPBOARD
 
             default: