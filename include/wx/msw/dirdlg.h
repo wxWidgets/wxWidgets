83aa1a19a522c7aa01bfb788a2c6c35ff03d0fbf
PB <PBforDev@gmail.com>
2020-06-07 16:24:49 +0200

    Implement MSW support for wxDD_MULTIPLE and wxDD_SHOW_HIDDEN

--- include/wx/msw/dirdlg.h
+++ include/wx/msw/dirdlg.h
@@ -24,13 +24,22 @@ public:
 
     void SetPath(const wxString& path) wxOVERRIDE;
 
+    // can be used only when wxDD_MULTIPLE flag is not set
+    wxString GetPath() const wxOVERRIDE;
+
+    // should be used only when wxDD_MULTIPLE flag is set
+    void GetPaths(wxArrayString& paths) const wxOVERRIDE;
+
     virtual int ShowModal() wxOVERRIDE;
 
 private:
+    // Used for wxDD_MULTIPLE
+    wxArrayString m_paths;
+
     // The real implementations of ShowModal(), used for Windows versions
     // before and since Vista.
     int ShowSHBrowseForFolder(WXHWND owner);
-    int ShowIFileDialog(WXHWND owner);
+    int ShowIFileOpenDialog(WXHWND owner);
 
     wxDECLARE_DYNAMIC_CLASS_NO_COPY(wxDirDialog);
 };
