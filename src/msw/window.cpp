wxWidgets/wxWidgets

commit 3bcbc8fe8eb8bd69c71c4ca9c263ec3e014f026b

Implement dismissal for unfocused wxPopupTransientWindow
Popups not using wxPU_CONTAINS_CONTROLS were not automatically dismissed
at all any longer, as the only auto-dismissal mechanism related on
getting WM_ACTIVATE, which they never did, so implement a different
logic for dismissing them: do it on any change of focus and also any
mouse press (but not move and not key press neither).

This will allow not using wxPU_CONTAINS_CONTROLS for popups that don't
need focus, but still must disappear on their own.

src/msw/window.cpp
@@ -3844,6 +3844,42 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
             }
     }
 
+    // Special hook for dismissing the current popup if it's active. It's a bit
+    // ugly to have to do this here, but the only alternatives seem to be
+    // installing a WH_CBT hook in wxPopupTransientWindow code, which is not
+    // really much better.
+#if wxUSE_POPUPWIN
+    if ( wxCurrentPopupWindow )
+    {
+        switch ( message )
+        {
+            case WM_NCLBUTTONDOWN:
+            case WM_NCLBUTTONUP:
+            case WM_NCLBUTTONDBLCLK:
+            case WM_NCRBUTTONDOWN:
+            case WM_NCRBUTTONUP:
+            case WM_NCRBUTTONDBLCLK:
+            case WM_NCMBUTTONDOWN:
+            case WM_NCMBUTTONUP:
+            case WM_NCMBUTTONDBLCLK:
+
+            case WM_LBUTTONDOWN:
+            case WM_LBUTTONUP:
+            case WM_LBUTTONDBLCLK:
+            case WM_RBUTTONDOWN:
+            case WM_RBUTTONUP:
+            case WM_RBUTTONDBLCLK:
+            case WM_MBUTTONDOWN:
+            case WM_MBUTTONUP:
+            case WM_MBUTTONDBLCLK:
+
+            case WM_SETFOCUS:
+            case WM_KILLFOCUS:
+                wxCurrentPopupWindow->MSWDismissUnfocusedPopup();
+        }
+    }
+#endif // wxUSE_POPUPWIN
+
     if ( !processed )
         return false;
 

include/wx/msw/popupwin.h
@@ -40,6 +40,10 @@ public:
     // Return the top level window parent of this popup or null.
     wxWindow* MSWGetOwner() const { return m_owner; }
 
+    // This is a way to notify non-wxPU_CONTAINS_CONTROLS windows about the
+    // events that should result in their dismissal.
+    virtual void MSWDismissUnfocusedPopup() { }
+
 private:
     wxWindow* m_owner;
 

include/wx/popupwin.h
@@ -151,6 +151,9 @@ public:
                                   WXWPARAM wParam,
                                   WXLPARAM lParam) wxOVERRIDE;
 
+    // Override to dismiss the popup.
+    virtual void MSWDismissUnfocusedPopup() wxOVERRIDE;
+
 private:
     void DismissOnDeactivate();
 

src/msw/popupwin.cpp
@@ -210,6 +210,19 @@ void wxPopupTransientWindow::DismissOnDeactivate()
     }
 }
 
+void wxPopupTransientWindow::MSWDismissUnfocusedPopup()
+{
+    // When we use wxPU_CONTAINS_CONTROLS, we can react to the popup
+    // deactivation in MSWHandleMessage(), but if we don't have focus, we don't
+    // get any events ourselves, so we rely on wxWindow to forward them to us.
+    if ( !HasFlag(wxPU_CONTAINS_CONTROLS) )
+    {
+        // It doesn't seem necessary to use CallAfter() here, as dismissing
+        // this window shouldn't affect the focus, as it never has it anyhow.
+        DismissAndNotify();
+    }
+}
+
 bool
 wxPopupTransientWindow::MSWHandleMessage(WXLRESULT *result,
                                          WXUINT message,

commit 1a4eff90ae9a187786a603786609dea39e2b0490

Make opening wxComboCtrl popup under MSW work again
This was recently broken in 3bcbc8fe8e (Implement dismissal for
unfocused wxPopupTransientWindow, 2020-07-09) as the changes in it
resulted in the popup being dismissed as soon as it was opened with a
mouse click.

Fix this by changing several things:

- Check for wxCurrentPopupWindow before processing the message, not
  after, as the message handler itself could create a new popup and we
  definitely don't want to dismiss it immediately after its creation.

- Check for mouse DOWN events only, not UP and DBLCLK ones, as otherwise
  it might be possible that UP matching the same DOWN whose handler
  showed the popup would dismiss it. As for DBLCLK, it's just
  unnecessary, as it should be always preceded by a DOWN message anyhow.

- Don't dismiss the popup if the message is sent to it or one of its
  children, as in this case the popup itself is supposed to deal with it
  (as wxComboCtrl popup does) and we don't want to prevent it from doing
  it.

With these changes both wxComboCtrl and wxTipWindow seem to work as
expected.

Closes #18844.

src/msw/window.cpp
@@ -2952,6 +2952,33 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
     // for most messages we should return 0 when we do process the message
     rc.result = 0;
 
+    // Special hook for dismissing the current popup if it's active. It's a bit
+    // ugly to have to do this here, but the only alternatives seem to be
+    // installing a WH_CBT hook in wxPopupTransientWindow code, which is not
+    // really much better.
+#if wxUSE_POPUPWIN
+    // Note that we let the popup window, or its child, have the event if it
+    // happens inside it -- it's supposed to react to it and we don't want to
+    // dismiss it before it can do it.
+    if ( wxCurrentPopupWindow && !wxCurrentPopupWindow->IsDescendant(this) )
+    {
+        switch ( message )
+        {
+            case WM_NCLBUTTONDOWN:
+            case WM_NCRBUTTONDOWN:
+            case WM_NCMBUTTONDOWN:
+
+            case WM_LBUTTONDOWN:
+            case WM_RBUTTONDOWN:
+            case WM_MBUTTONDOWN:
+
+            case WM_SETFOCUS:
+            case WM_KILLFOCUS:
+                wxCurrentPopupWindow->MSWDismissUnfocusedPopup();
+        }
+    }
+#endif // wxUSE_POPUPWIN
+
     switch ( message )
     {
         case WM_CREATE:
@@ -3843,42 +3870,6 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
                 processed = (*i->second)(this, message, wParam, lParam);
             }
     }
-
-    // Special hook for dismissing the current popup if it's active. It's a bit
-    // ugly to have to do this here, but the only alternatives seem to be
-    // installing a WH_CBT hook in wxPopupTransientWindow code, which is not
-    // really much better.
-#if wxUSE_POPUPWIN
-    if ( wxCurrentPopupWindow )
-    {
-        switch ( message )
-        {
-            case WM_NCLBUTTONDOWN:
-            case WM_NCLBUTTONUP:
-            case WM_NCLBUTTONDBLCLK:
-            case WM_NCRBUTTONDOWN:
-            case WM_NCRBUTTONUP:
-            case WM_NCRBUTTONDBLCLK:
-            case WM_NCMBUTTONDOWN:
-            case WM_NCMBUTTONUP:
-            case WM_NCMBUTTONDBLCLK:
-
-            case WM_LBUTTONDOWN:
-            case WM_LBUTTONUP:
-            case WM_LBUTTONDBLCLK:
-            case WM_RBUTTONDOWN:
-            case WM_RBUTTONUP:
-            case WM_RBUTTONDBLCLK:
-            case WM_MBUTTONDOWN:
-            case WM_MBUTTONUP:
-            case WM_MBUTTONDBLCLK:
-
-            case WM_SETFOCUS:
-            case WM_KILLFOCUS:
-                wxCurrentPopupWindow->MSWDismissUnfocusedPopup();
-        }
-    }
-#endif // wxUSE_POPUPWIN
 
     if ( !processed )
         return false;