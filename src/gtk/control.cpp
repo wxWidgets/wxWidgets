72bc61c501038200488caef792d67f8dc9536701
Vadim Zeitlin <vadim@wxwidgets.org>
2020-08-04 00:52:45 +0200

    Don't call gtk_widget_set_size_request() unnecessarily
    It doesn't seem useful to reset the size request if it's not set anyhow,
    so just skip doing it and then restoring it later, in this case.
    
    See 6ed7e27bf2 (Fix GetBestSize() for GTK3 after size has been set,
    2016-02-06).

--- src/gtk/control.cpp
+++ src/gtk/control.cpp
@@ -350,9 +350,14 @@ wxSize wxControl::GTKGetPreferredSize(GtkWidget* widget) const
     if ( wasHidden )
         gtk_widget_show(widget);
 
-    gtk_widget_set_size_request(widget, -1, -1);
+    // Temporarily unset the requested size if it's set.
+    if ( w != -1 || h != -1 )
+        gtk_widget_set_size_request(widget, -1, -1);
+
     gtk_widget_get_preferred_size(widget, NULL, &req);
-    gtk_widget_set_size_request(widget, w, h);
+
+    if ( w != -1 || h != -1 )
+        gtk_widget_set_size_request(widget, w, h);
 
     if ( wasHidden )
         gtk_widget_hide(widget);
