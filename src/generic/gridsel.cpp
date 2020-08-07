475bde5cab9b148ad49ad10fde53d70f31c7b6ca
Daniel Kulp <dkulp@apache.org>
2020-08-07 09:59:17 -0400

    Fix SELECTED loop

--- src/generic/gridsel.cpp
+++ src/generic/gridsel.cpp
@@ -71,10 +71,10 @@ void wxGridSelection::EndSelecting()
         wxRect r;
         wxGridCellCoords coords1, coords2;
 
-        // deselect all blocks and update the screen
-        while ( ( n = m_selection.size() ) > 0)
+        // Send selection events for all the selected blocks.
+        const size_t count = m_selection.size();
+        for ( size_t n = 0; n < count; n++ )
         {
-            n--;
             const wxGridBlockCoords& block = m_selection[n];
             coords1 = block.GetTopLeft();
             coords2 = block.GetBottomRight();
