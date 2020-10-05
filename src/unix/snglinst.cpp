633165fd13d2fdb0a530affdd5c78873d84d4023
Václav Slavík <vaclav@slavik.io>
2020-10-05 17:03:45 +0200

    Log stale lock files in verbose mode only
    Actually do what the comment says is intended. wxLogInfo() and
    wxLogVerbose() differ in that only the latter must be explicitly
    enabled with --verbose.

--- src/unix/snglinst.cpp
+++ src/unix/snglinst.cpp
@@ -298,7 +298,7 @@ bool wxSingleInstanceCheckerImpl::Create(const wxString& name)
                     // message is that the previous instance of the program
                     // crashed), don't show it by default, i.e. unless the
                     // program is running with --verbose command line option.
-                    wxLogInfo(_("Deleted stale lock file '%s'."),
+                    wxLogVerbose(_("Deleted stale lock file '%s'."),
                                  name.c_str());
 
                     // retry now
