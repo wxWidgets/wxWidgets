f0ee18f1aa2bc0aa27cee62884cd9379f2978711
PB <pbfordev@gmail.com>
2020-10-13 18:49:53 +0200

    Remove BCC mentions from documentation

--- docs/doxygen/mainpages/introduction.h
+++ docs/doxygen/mainpages/introduction.h
@@ -94,9 +94,9 @@ primary supported compiler is GNU g++.
 supports 32 or 64 bit Intel Macs running macOS 10.10 or later. The port can be
 built either with g++ or clang.
 
-Other platforms (e.g. iOS - with a minimum requirement of iOS 13), compilers (Borland C++
-under Windows, Sun CC, HP-UX aCC, IBM xlC or SGI mipsPro under Unix) and ports (
-wxQT, wxGTK1, wxX11, wxDFB...) are also supported but to a lesser extent.
+Other platforms (e.g. iOS - with a minimum requirement of iOS 13), compilers 
+(Sun CC, HP-UX aCC, IBM xlC or SGI mipsPro under Unix) and ports (wxQT, wxGTK1, 
+wxX11, wxDFB...) are also supported but to a lesser extent.
 Please see the @subpage page_port "platform details page" for more information.
 
 There are no CPU speed requirements but the faster (and more) CPU(s) you have,
--- docs/doxygen/mainpages/platdetails.h
+++ docs/doxygen/mainpages/platdetails.h
@@ -96,8 +96,8 @@ and candy of newer interfaces like GTK.
 wxMSW is a port of wxWidgets for the Windows platforms (Windows XP and later
 are supported). wxMSW provides native look and feel for each Windows version.
 This port can be compiled with several compilers including Microsoft Studio
-VC++ 2003 or later, Borland 5.5, MinGW32, Cygwin as well as cross-compilation
-with a Linux-hosted MinGW32 tool chain.
+VC++ 2003 or later, MinGW32, Cygwin as well as cross-compilation with a 
+Linux-hosted MinGW32 tool chain.
 
 @subpage plat_msw_install "Build and Install Instructions"
 
