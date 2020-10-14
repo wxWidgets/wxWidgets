6ba1b8c78cfb24c292371611e5b293b3036a9abf
PB <pbfordev@gmail.com>
2020-10-14 20:10:30 +0200

    Fix the bug causing GUI tests crashes

--- src/common/event.cpp
+++ src/common/event.cpp
@@ -595,10 +595,7 @@ wxMouseEvent::wxMouseEvent(wxEventType commandType)
 void wxMouseEvent::Assign(const wxMouseEvent& event)
 {
     wxEvent::operator=(event);
-
-    // Borland C++ 5.82 doesn't compile an explicit call to an implicitly
-    // defined operator=() so need to do it this way:
-    *static_cast<wxMouseState *>(this) = event;
+    wxMouseState::operator=(event);
 
     m_x = event.m_x;
     m_y = event.m_y;
@@ -804,10 +801,7 @@ wxKeyEvent& wxKeyEvent::operator=(const wxKeyEvent& evt)
     if ( &evt != this )
     {
         wxEvent::operator=(evt);
-
-        // Borland C++ 5.82 doesn't compile an explicit call to an
-        // implicitly defined operator=() so need to do it this way:
-        *static_cast<wxKeyboardState *>(this) = evt;
+        wxKeyboardState::operator=(evt);
 
         DoAssignMembers(evt);
     }
