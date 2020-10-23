df2f13662567ae6a5142a95fefbff45348bd1ee3
mgrouch <mgrushinskiy@gmail.com>
2020-10-23 17:52:51 -0400

    for debian

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -117,7 +117,7 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/libgtk-3-0_${GTK_VER}_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/gir1.2-gtk-3.0_${GTK_VER}_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/libgtk-3-dev_${GTK_VER}_${PKG_ARCH}.deb;
-    wget http://http.us.debian.org/debian/pool/main/libx/libxcrypt/libcrypt1_4.4.17-1_${PKG_ARCH}.deb;
+    wget http://http.us.debian.org/debian/pool/main/libx/libxcrypt/libcrypt1_4.4.16-1_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/gcc-10/libgcc-s1_10.2.0-15_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/glibc/libc6_2.31-4_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/libn/libnss-nis/libnss-nis_3.1-4_${PKG_ARCH}.deb;
@@ -125,7 +125,7 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     wget http://http.us.debian.org/debian/pool/main/g/glib2.0/libglib2.0-0_2.66.1-2_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/j/json-glib/libjson-glib-1.0-0_1.6.0-1_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/libx/libxcomposite/libxcomposite1_0.4.5-1_${PKG_ARCH}.deb;
-    dpkg -i libcrypt1_4.4.17-1_${PKG_ARCH}.deb;
+    dpkg -i libcrypt1_4.4.16-1_${PKG_ARCH}.deb;
     dpkg -i libgcc-s1_10.2.0-15_${PKG_ARCH}.deb;
     dpkg -i libnss-nis_3.1-4_${PKG_ARCH}.deb;
     dpkg -i libnss-nisplus_1.3-4_${PKG_ARCH}.deb;
