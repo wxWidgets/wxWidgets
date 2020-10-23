291f5b1477d0e1c6b4d843e31044f46d4cf51a57
mgrouch <mgrushinskiy@gmail.com>
2020-10-23 17:04:05 -0400

    for debian

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -117,6 +117,14 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/libgtk-3-0_${GTK_VER}_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/gir1.2-gtk-3.0_${GTK_VER}_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/g/gtk+3.0/libgtk-3-dev_${GTK_VER}_${PKG_ARCH}.deb;
+    wget http://http.us.debian.org/debian/pool/main/g/glibc/libc6_2.31-4_${PKG_ARCH}.deb;
+    wget http://http.us.debian.org/debian/pool/main/g/glib2.0/libglib2.0-0_2.66.1-2_${PKG_ARCH}.deb;
+    wget http://http.us.debian.org/debian/pool/main/j/json-glib/libjson-glib-1.0-0_1.6.0-1_${PKG_ARCH}.deb;
+    wget http://http.us.debian.org/debian/pool/main/libx/libxcomposite/libxcomposite1_0.4.5-1_${PKG_ARCH}.deb;
+    dpkg -i libc6_2.31-4_${PKG_ARCH}.deb;
+    dpkg -i libglib2.0-0_2.66.1-2_${PKG_ARCH}.deb;
+    dpkg -i libjson-glib-1.0-0_1.6.0-1_${PKG_ARCH}.deb;
+    dpkg -i libxcomposite1_0.4.5-1_${PKG_ARCH}.deb;
     dpkg -i libgdk-pixbuf2.0-bin_${GDK_PIX_VER}_${PKG_ARCH}.deb;
     dpkg -i libgdk-pixbuf2.0-common_${GDK_PIX_VER}_all.deb;
     dpkg -i libgdk-pixbuf2.0-0_${GDK_PIX_VER}_${PKG_ARCH}.deb;
