9877b518bd8f988e54f58ae54bb8244cae73cc0e
mgrouch <mgrushinskiy@gmail.com>
2020-10-23 00:00:16 -0400

    for debian

--- build/tools/msvs/buildall.bat
+++
@@ -1,5 +0,0 @@
-start cmd /k Call officialbuild.bat vc14x
-start cmd /k Call officialbuild.bat vc120
-start cmd /k Call officialbuild.bat vc110
-start cmd /k Call officialbuild.bat vc100
-start cmd /E:ON /V:ON /T:0E /K Call officialbuild.bat vc90
