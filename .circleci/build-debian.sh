d7b7ba86bdf69dc41cc6dfdd99969cfeeaf57840
mgrouch <mgrushinskiy@gmail.com>
2020-10-23 19:11:47 -0400

    for debian

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -125,12 +125,11 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     wget http://http.us.debian.org/debian/pool/main/g/glib2.0/libglib2.0-0_2.66.1-2_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/j/json-glib/libjson-glib-1.0-0_1.6.0-1_${PKG_ARCH}.deb;
     wget http://http.us.debian.org/debian/pool/main/libx/libxcomposite/libxcomposite1_0.4.5-1_${PKG_ARCH}.deb;
-    dpkg -i libc6_2.31-4_${PKG_ARCH}.deb;
     dpkg -i libgcc-s1_10.2.0-15_${PKG_ARCH}.deb;
     dpkg -i libnss-nis_3.1-4_${PKG_ARCH}.deb;
     dpkg -i libnss-nisplus_1.3-4_${PKG_ARCH}.deb;
+    dpkg -i libc6_2.31-4_${PKG_ARCH}.deb libcrypt1_4.4.17-1_${PKG_ARCH}.deb;
     dpkg -i libglib2.0-0_2.66.1-2_${PKG_ARCH}.deb;
-    dpkg -i libcrypt1_4.4.17-1_${PKG_ARCH}.deb;
     dpkg -i libjson-glib-1.0-0_1.6.0-1_${PKG_ARCH}.deb;
     dpkg -i libxcomposite1_0.4.5-1_${PKG_ARCH}.deb;
     dpkg -i libgdk-pixbuf2.0-bin_${GDK_PIX_VER}_${PKG_ARCH}.deb;
