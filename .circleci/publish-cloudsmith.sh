ff751c85c6bed8809a24b7115ca91d3ee000d0a7
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 23:17:44 -0400

    for debian

--- .circleci/publish-cloudsmith.sh
+++ .circleci/publish-cloudsmith.sh
@@ -8,6 +8,6 @@ for pkg_file in dist/*.$EXT; do
   cloudsmith push deb $REPO/$DISTRO $pkg_file
   RESULT=$?
   if [ $RESULT -eq 144 ]; then
-     echo " skipping already deployed $pkg_file"
+     echo "skipping already deployed $pkg_file"
   fi
 done
