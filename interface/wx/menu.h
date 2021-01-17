f4172b0b46c6e9d821ec9c1d42ea81006ac88e85
Gary Allen <gvcallen@gmail.com>
2021-01-17 15:07:28 +0200

    Allow overriding wxMenu::MSWCommand() in wxMSW
    This can be useful to implement custom handling of WM_COMMAND for the
    popup menus, so make this function virtual and document it to indicate
    that it is now part of the public API.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/2170

--- interface/wx/menu.h
+++ interface/wx/menu.h
@@ -932,6 +932,33 @@ public:
     bool IsEnabled(int id) const;
 
     /**
+        Allows handling native MSW menu command messages.
+
+        This is a low-level function which allows handling MSW @c WM_COMMAND
+        messages generated when menu items are selected. It is particularly
+        useful for the popup menus, as specific handling can then be defined in
+        a wxMenu-derived class directly, instead of in the wxWindow-derived
+        owner class.
+
+        The base class version of this function generates @c wxEVT_MENU command
+        events.
+
+        @param param
+            The MSW command parameter.
+
+        @param id
+            The id of the command.
+
+        @return
+            @true if the command was handled, @false otherwise.
+
+        @onlyfor{wxMSW}
+
+        @since 3.1.5
+    */
+    virtual bool MSWCommand(WXUINT param, WXWORD id);
+
+    /**
         Inserts the given @a item at position 0, i.e.\ before all the other
         existing items.
 
