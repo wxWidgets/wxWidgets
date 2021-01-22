a1cca242a44bc784ff3a56251117a167665fefb3
Vadim Zeitlin <vadim@wxwidgets.org>
2021-01-22 22:34:01 +0100

    Use CHECK() instead of CPPUNIT_ASSERT in wxSelectionStore test
    No real changes, just simplify the code. Notably we don't need explicit
    "u" suffix on the literal constants any longer as CATCH is smart enough
    to compare signed and unsigned integers correctly without it.

--- tests/misc/selstoretest.cpp
+++ tests/misc/selstoretest.cpp
@@ -65,44 +65,44 @@ const unsigned SelStoreTestCase::NUM_ITEMS = 10; // NB: must be even
 void SelStoreTestCase::SelectItem()
 {
     m_store.SelectItem(0);
-    CPPUNIT_ASSERT_EQUAL( 1u, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( m_store.IsSelected(0) );
+    CHECK( m_store.GetSelectedCount() == 1 );
+    CHECK( m_store.IsSelected(0) );
 
     m_store.SelectItem(NUM_ITEMS - 1);
-    CPPUNIT_ASSERT_EQUAL( 2u, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( m_store.IsSelected(NUM_ITEMS - 1) );
+    CHECK( m_store.GetSelectedCount() == 2 );
+    CHECK( m_store.IsSelected(NUM_ITEMS - 1) );
 
     m_store.SelectItem(0, false);
-    CPPUNIT_ASSERT_EQUAL( 1u, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( !m_store.IsSelected(0) );
+    CHECK( m_store.GetSelectedCount() == 1 );
+    CHECK( !m_store.IsSelected(0) );
 }
 
 void SelStoreTestCase::SelectRange()
 {
     m_store.SelectRange(0, NUM_ITEMS/2);
-    CPPUNIT_ASSERT_EQUAL( NUM_ITEMS/2 + 1, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( m_store.IsSelected(0) );
-    CPPUNIT_ASSERT( !m_store.IsSelected(NUM_ITEMS - 1) );
+    CHECK( m_store.GetSelectedCount() == NUM_ITEMS/2 + 1 );
+    CHECK( m_store.IsSelected(0) );
+    CHECK( !m_store.IsSelected(NUM_ITEMS - 1) );
 
     m_store.SelectRange(NUM_ITEMS/2, NUM_ITEMS - 1);
-    CPPUNIT_ASSERT_EQUAL( NUM_ITEMS, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( m_store.IsSelected(0) );
-    CPPUNIT_ASSERT( m_store.IsSelected(NUM_ITEMS - 1) );
+    CHECK( m_store.GetSelectedCount() == NUM_ITEMS );
+    CHECK( m_store.IsSelected(0) );
+    CHECK( m_store.IsSelected(NUM_ITEMS - 1) );
 
     m_store.SelectRange(1, NUM_ITEMS - 2, false);
-    CPPUNIT_ASSERT_EQUAL( 2u, m_store.GetSelectedCount() );
-    CPPUNIT_ASSERT( m_store.IsSelected(0) );
-    CPPUNIT_ASSERT( !m_store.IsSelected(NUM_ITEMS/2) );
-    CPPUNIT_ASSERT( m_store.IsSelected(NUM_ITEMS - 1) );
+    CHECK( m_store.GetSelectedCount() == 2 );
+    CHECK( m_store.IsSelected(0) );
+    CHECK( !m_store.IsSelected(NUM_ITEMS/2) );
+    CHECK( m_store.IsSelected(NUM_ITEMS - 1) );
 }
 
 void SelStoreTestCase::SetItemCount()
 {
     m_store.SelectRange(1, NUM_ITEMS - 2);
-    CPPUNIT_ASSERT_EQUAL( NUM_ITEMS - 2, m_store.GetSelectedCount() );
+    CHECK( m_store.GetSelectedCount() == NUM_ITEMS - 2 );
 
     m_store.SetItemCount(NUM_ITEMS/2);
-    CPPUNIT_ASSERT_EQUAL( NUM_ITEMS/2 - 1, m_store.GetSelectedCount() );
+    CHECK( m_store.GetSelectedCount() == NUM_ITEMS/2 - 1 );
 
 
     m_store.Clear();
@@ -112,22 +112,22 @@ void SelStoreTestCase::SetItemCount()
     m_store.SelectItem(NUM_ITEMS/2 - 1);
     m_store.SelectItem(NUM_ITEMS/2 + 1);
     m_store.SetItemCount(NUM_ITEMS/2);
-    CPPUNIT_ASSERT_EQUAL( 1u, m_store.GetSelectedCount() );
+    CHECK( m_store.GetSelectedCount() == 1 );
 }
 
 void SelStoreTestCase::Clear()
 {
-    CPPUNIT_ASSERT(m_store.IsEmpty());
-    CPPUNIT_ASSERT_EQUAL( 0u, m_store.GetSelectedCount() );
+    CHECK(m_store.IsEmpty());
+    CHECK( m_store.GetSelectedCount() == 0 );
 
     m_store.SelectItem(0);
 
-    CPPUNIT_ASSERT(!m_store.IsEmpty());
+    CHECK(!m_store.IsEmpty());
 
     m_store.Clear();
 
-    CPPUNIT_ASSERT(m_store.IsEmpty());
-    CPPUNIT_ASSERT_EQUAL( 0u, m_store.GetSelectedCount() );
+    CHECK(m_store.IsEmpty());
+    CHECK( m_store.GetSelectedCount() == 0 );
 }
 
 void SelStoreTestCase::Iterate()
@@ -135,16 +135,16 @@ void SelStoreTestCase::Iterate()
     m_store.SelectRange(NUM_ITEMS/2 - 1, NUM_ITEMS/2 + 1);
 
     wxSelectionStore::IterationState cookie;
-    CPPUNIT_ASSERT_EQUAL(NUM_ITEMS/2 - 1, m_store.GetFirstSelectedItem(cookie));
-    CPPUNIT_ASSERT_EQUAL(NUM_ITEMS/2, m_store.GetNextSelectedItem(cookie));
-    CPPUNIT_ASSERT_EQUAL(NUM_ITEMS/2 + 1, m_store.GetNextSelectedItem(cookie));
+    CHECK(NUM_ITEMS/2 - 1 == m_store.GetFirstSelectedItem(cookie));
+    CHECK(NUM_ITEMS/2 == m_store.GetNextSelectedItem(cookie));
+    CHECK(NUM_ITEMS/2 + 1 == m_store.GetNextSelectedItem(cookie));
 
-    CPPUNIT_ASSERT_EQUAL(wxSelectionStore::NO_SELECTION, m_store.GetNextSelectedItem(cookie));
+    CHECK(wxSelectionStore::NO_SELECTION == m_store.GetNextSelectedItem(cookie));
 
 
     m_store.SelectRange(0, NUM_ITEMS - 1);
     m_store.SelectItem(0, false);
-    CPPUNIT_ASSERT_EQUAL(1, m_store.GetFirstSelectedItem(cookie));
+    CHECK(1 == m_store.GetFirstSelectedItem(cookie));
 }
 
 void SelStoreTestCase::ItemsAddDelete()
@@ -154,26 +154,26 @@ void SelStoreTestCase::ItemsAddDelete()
     m_store.SelectItem(NUM_ITEMS - 1);
 
     m_store.OnItemsInserted(NUM_ITEMS/2 + 1, 1);
-    CPPUNIT_ASSERT(m_store.IsSelected(0));
-    CPPUNIT_ASSERT(m_store.IsSelected(NUM_ITEMS/2));
-    CPPUNIT_ASSERT(m_store.IsSelected(NUM_ITEMS));
-    CPPUNIT_ASSERT_EQUAL(3, m_store.GetSelectedCount());
+    CHECK(m_store.IsSelected(0));
+    CHECK(m_store.IsSelected(NUM_ITEMS/2));
+    CHECK(m_store.IsSelected(NUM_ITEMS));
+    CHECK(m_store.GetSelectedCount() == 3);
 
-    CPPUNIT_ASSERT(m_store.OnItemsDeleted(NUM_ITEMS/2 - 1, 2));
-    CPPUNIT_ASSERT(m_store.IsSelected(0));
-    CPPUNIT_ASSERT(m_store.IsSelected(NUM_ITEMS - 2));
-    CPPUNIT_ASSERT_EQUAL(2, m_store.GetSelectedCount());
+    CHECK(m_store.OnItemsDeleted(NUM_ITEMS/2 - 1, 2));
+    CHECK(m_store.IsSelected(0));
+    CHECK(m_store.IsSelected(NUM_ITEMS - 2));
+    CHECK(m_store.GetSelectedCount() == 2);
 
     m_store.OnItemsInserted(0, 2);
-    CPPUNIT_ASSERT(m_store.IsSelected(2));
-    CPPUNIT_ASSERT(m_store.IsSelected(NUM_ITEMS));
-    CPPUNIT_ASSERT_EQUAL(2, m_store.GetSelectedCount());
+    CHECK(m_store.IsSelected(2));
+    CHECK(m_store.IsSelected(NUM_ITEMS));
+    CHECK(m_store.GetSelectedCount() == 2);
 
     m_store.OnItemDelete(0);
 
     m_store.SelectRange(0, NUM_ITEMS - 1);
-    CPPUNIT_ASSERT(m_store.OnItemsDeleted(0, NUM_ITEMS/2));
-    CPPUNIT_ASSERT_EQUAL(NUM_ITEMS/2, m_store.GetSelectedCount());
-    CPPUNIT_ASSERT(m_store.IsSelected(0));
-    CPPUNIT_ASSERT(m_store.IsSelected(NUM_ITEMS/2));
+    CHECK(m_store.OnItemsDeleted(0, NUM_ITEMS/2));
+    CHECK(m_store.GetSelectedCount() == NUM_ITEMS/2);
+    CHECK(m_store.IsSelected(0));
+    CHECK(m_store.IsSelected(NUM_ITEMS/2));
 }
