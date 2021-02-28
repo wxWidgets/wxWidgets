4afeeb52c526de8e06827717707c0cac7ca2ceff
Maarten Bent <MaartenBent@users.noreply.github.com>
2021-02-28 20:06:03 +0100

    Mention GCC/Clang support in wxWebViewEdge documentation
    And add a comment explaining the name of the EventRegistrationToken include guard.

--- include/wx/msw/wrl/EventToken.h
+++ include/wx/msw/wrl/EventToken.h
@@ -7,6 +7,9 @@
 // Licence:     wxWindows licence
 ///////////////////////////////////////////////////////////////////////////////
 
+// Note: this include guard uses the same name as winrt/EventToken.h in the
+// Windows SDK to prevent a struct type redefinition when both headers are
+// included.
 #ifndef __eventtoken_h__
 #define __eventtoken_h__
 
