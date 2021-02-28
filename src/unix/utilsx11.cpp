810b34241110367a5737e0ff6bd1fffa5d7286a0
Vadim Zeitlin <vadim@wxwidgets.org>
2021-02-28 23:53:25 +0100

    Avoid useless check for GTK_CHECK_VERSION
    Just use it inside the existing check for GTK itself.
    
    No real changes.

--- src/unix/utilsx11.cpp
+++ src/unix/utilsx11.cpp
@@ -48,13 +48,11 @@
 #endif
 GdkWindow* wxGetTopLevelGDK();
 GtkWidget* wxGetTopLevelGTK();
-#endif
 
-#ifdef GTK_CHECK_VERSION
 #if GTK_CHECK_VERSION(3,4,0)
 #define wxHAS_GETKEYSTATE_GTK
 #endif //GTK+ 3.4
-#endif
+#endif // GTK
 
 // Only X11 backend is supported for wxGTK here (GTK < 2 has no others)
 #if !defined(__WXGTK__) || \
