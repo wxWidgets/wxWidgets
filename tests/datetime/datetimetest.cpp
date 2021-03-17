c82e13068aeb101adc2d928d9f795f299a6a4d76
Paul Cornett <paulcor@users.noreply.github.com>
2021-03-17 10:30:34 -0700

    Fix comment typo

--- tests/datetime/datetimetest.cpp
+++ tests/datetime/datetimetest.cpp
@@ -865,7 +865,7 @@ void DateTimeTestCase::TestTimeFormat()
     CPPUNIT_ASSERT( dt.ParseFormat("12:23:45.000", "%H:%M:%S.%l") );
     CPPUNIT_ASSERT_EQUAL( 0, dt.GetMillisecond() );
 
-    // test another format modifier not testes above.
+    // test another format modifier not tested above.
     CPPUNIT_ASSERT( dt.ParseFormat("23", "%e") );
     CPPUNIT_ASSERT_EQUAL( 23, dt.GetDay() );
 
