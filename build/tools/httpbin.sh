003b474d7f4c1b42e482a5b6c0a16cbffcf801da
Vadim Zeitlin <vadim@wxwidgets.org>
2021-04-05 15:37:50 +0200

    Always use Python 3 for httpbin installation
    Trying to use Python 2.7.6 under Trusty is hopeless, as pythonhosted.org
    is going to drop support for non-SNI clients soon, see
    https://status.python.org/incidents/hzmjhqsdjqgb and
    https://github.com/pypa/pypi-support/issues/978

--- build/tools/httpbin.sh
+++ build/tools/httpbin.sh
@@ -7,36 +7,6 @@ httpbin_launch() {
 
     case "$(uname -s)" in
         Linux)
-            dist_codename=$(lsb_release --codename --short)
-            ;;
-
-        Darwin)
-            dist_codename='macOS'
-            ;;
-    esac
-
-    case "$dist_codename" in
-        trusty)
-            # Current decorator (>= 5) is incompatible with Python 2 which we
-            # still use in some builds, so explicitly use a version which
-            # is known to work. Just specifying "decorator==4.4.2" doesn't work
-            # neither for some reason ("no matching distribution found"), so
-            # use an explicit URL.
-            pip_decorator_arg='https://files.pythonhosted.org/packages/ed/1b/72a1821152d07cf1d8b6fce298aeb06a7eb90f4d6d41acec9861e7cc6df0/decorator-4.4.2-py2.py3-none-any.whl'
-
-            # We need to disable SSL certificate checking under Trusty because
-            # Python version there is too old to support SNI.
-            pip_options='--trusted-host files.pythonhosted.org'
-            ;;
-
-        macOS)
-            pip_decorator_arg='decorator==4.4.2'
-            ;;
-
-        *)
-            # Elsewhere just use Python 3.
-            PY3=3
-
             # Ensure that we have at least some version of pip.
             if ! python3 -m pip; then
                 sudo apt-get -q -o=Dpkg::Use-Pty=0 install python3-pip
@@ -47,15 +17,19 @@ httpbin_launch() {
             # than python3, so upgrade it to fix this.
             #
             # However don't upgrade to a version which is too new because then
-            # it may not support Python version that we actually have.
-            python3 -m pip install --user --upgrade pip==20.0.1
+            # it may not support Python version that we actually have (this one
+            # still works with 3.4, 20.0.1 is the last one to support 3.5).
+            python3 -m pip install --user --upgrade pip==19.1.1
+            ;;
+
+        Darwin)
             ;;
     esac
 
-    echo "Installing using `python$PY3 -m pip --version`"
+    echo "Installing httpbin using `python3 -m pip --version`"
 
-    python$PY3 -m pip install $pip_decorator_arg httpbin --user $pip_options
-    python$PY3 -m httpbin.core 2>&1 >httpbin.log &
+    python3 -m pip install httpbin --user
+    python3 -m httpbin.core 2>&1 >httpbin.log &
     WX_TEST_WEBREQUEST_URL="http://localhost:5000"
 
     export WX_TEST_WEBREQUEST_URL
