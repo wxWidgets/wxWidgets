c968e49b13c8a07a4313d8ed606e2c5f8288cc23
Maarten Bent <MaartenBent@users.noreply.github.com>
2021-04-05 21:26:04 +0200

    CMake: Remove iOS workaround
    Not needed anymore since the fix in 60c620c28c.
    
    Closes https://github.com/wxWidgets/wxWidgets/pull/2308

--- build/cmake/functions.cmake
+++ build/cmake/functions.cmake
@@ -211,7 +211,7 @@ function(wx_set_target_properties target_name is_base)
         endif()
     else()
         set(cross_target "")
-        if (CMAKE_CROSSCOMPILING AND NOT IPHONE)
+        if (CMAKE_CROSSCOMPILING)
             set(cross_target "-${CMAKE_SYSTEM_NAME}")
         endif ()
 
@@ -219,7 +219,7 @@ function(wx_set_target_properties target_name is_base)
             PROPERTIES
                 OUTPUT_NAME wx_${lib_toolkit}${lib_unicode}${lib_flavour}${lib_suffix}-${lib_version}${cross_target}
                 # NOTE: wx-config can not be used to connect the libraries with the debug suffix.
-                #OUTPUT_NAME_DEBUG wx_${lib_toolkit}${lib_unicode}d${lib_flavour}${lib_suffix}-${lib_version}
+                #OUTPUT_NAME_DEBUG wx_${lib_toolkit}${lib_unicode}d${lib_flavour}${lib_suffix}-${lib_version}${cross_target}
                 OUTPUT_NAME_DEBUG wx_${lib_toolkit}${lib_unicode}${lib_flavour}${lib_suffix}-${lib_version}${cross_target}
             )
     endif()
