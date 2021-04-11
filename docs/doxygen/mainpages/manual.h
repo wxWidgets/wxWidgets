8620c6e9fde85e2662af19c89e8df738750bb35d
Vadim Zeitlin <vadim@wxwidgets.org>
2021-04-11 17:27:12 +0200

    Update dates and version numbers for 3.1.5 release
    Also invalidate SHA-1 sums for the release files.

--- docs/doxygen/mainpages/manual.h
+++ docs/doxygen/mainpages/manual.h
@@ -14,7 +14,7 @@
 @author Julian Smart, Vadim Zeitlin, Robin Dunn, Stefan Csomor,
         Bryan Petty, Francesco Montorsi, Robert Roebling et al
 
-@date July 22, 2020
+@date April 14, 2021
 
 Welcome to wxWidgets, a stable and powerful open source framework for
 developing native cross-platform GUI applications in C++!
