11967af49f58027d51977b3f5793deba0a660d2f
Vadim Zeitlin <vadim@wxwidgets.org>
2021-04-18 13:09:51 +0200

    Make wxSpinCtrl::GetTextValue() pure virtual
    It is now implemented in all ports.

--- include/wx/spinctrl.h
+++ include/wx/spinctrl.h
@@ -35,7 +35,7 @@ public:
     wxSpinCtrlBase() {}
 
     // accessor functions that derived classes are expected to have
-    virtual wxString GetTextValue() const { return wxString(); }
+    virtual wxString GetTextValue() const = 0;
     // T GetValue() const
     // T GetMin() const
     // T GetMax() const
