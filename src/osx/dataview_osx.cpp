wxWidgets/wxWidgets

commit 36ea7ff4d61e174a2d116819f98e66f22a7f5b8a

Autosize the right columns in macOS wxDataViewCtrl
This is related to 7555d1b245 (Fix expansion of the last column in macOS
wxDataViewCtrl, 2021-01-26) which somehow didn't apply the patch fully,
so finish applying it now.

Closes #14939.

src/osx/dataview_osx.cpp
@@ -302,10 +302,7 @@ void wxOSXDataViewModelNotifier::AdjustAutosizedColumns()
   unsigned count = m_DataViewCtrlPtr->GetColumnCount();
   for ( unsigned col = 0; col < count; col++ )
   {
-      wxDataViewColumn *column = m_DataViewCtrlPtr->GetColumnPtr(col);
-
-      if ( column->GetWidthVariable() == wxCOL_WIDTH_AUTOSIZE )
-        m_DataViewCtrlPtr->GetDataViewPeer()->FitColumnWidthToContent(col);
+    m_DataViewCtrlPtr->GetDataViewPeer()->FitColumnWidthToContent(col);
   }
 }
 

src/osx/cocoa/dataview.mm
@@ -2340,7 +2340,7 @@ void wxCocoaDataViewControl::FitColumnWidthToContent(unsigned int pos)
 
     if ( pos == noOfColumns - 1 )
         [m_OutlineView sizeLastColumnToFit];
-    else
+    else if ( GetColumn(pos)->GetWidthVariable() == wxCOL_WIDTH_AUTOSIZE )
         [column setWidth:calculator.GetMaxWidth() + m_expanderWidth];
 
     if ( !(GetDataViewCtrl()->GetWindowStyle() & wxDV_VARIABLE_LINE_HEIGHT) )

commit e5c0d8d2158370a3a0a5a3b875bb99aee51be3c1

Fix performance regression in macOS wxDataViewCtrl
Fixes severe degradation of performance caused by
36ea7ff4d61e174a2d116819f98e66f22a7f5b8a, which forces calculation of
fitting size even for non-autosized columns.

The purpose of the change was to update row heights as well, but doing
doesn't make sense when the row height is fixed. Avoid calling
FitColumnWidthToContent() unless it actually is needed, i.e. the column
is autosized or variable row heights are used).

src/osx/dataview_osx.cpp
@@ -302,7 +302,9 @@ void wxOSXDataViewModelNotifier::AdjustAutosizedColumns()
   unsigned count = m_DataViewCtrlPtr->GetColumnCount();
   for ( unsigned col = 0; col < count; col++ )
   {
-    m_DataViewCtrlPtr->GetDataViewPeer()->FitColumnWidthToContent(col);
+    wxDataViewColumn *column = m_DataViewCtrlPtr->GetColumnPtr(col);
+    if ( column->GetWidthVariable() == wxCOL_WIDTH_AUTOSIZE || m_DataViewCtrlPtr->HasFlag(wxDV_VARIABLE_LINE_HEIGHT) )
+        m_DataViewCtrlPtr->GetDataViewPeer()->FitColumnWidthToContent(col);
   }
 }
 