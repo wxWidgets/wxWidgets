a1065a89af3ea02807a719936f1c51a38043a3bc
Vadim Zeitlin <vadim@wxwidgets.org>
2021-06-16 19:12:32 +0100

    Fix handling of ampersands in wxCheckListBox under MSW
    They were incorrectly interpreted as mnemonics when drawing
    wxCheckListBox items, which didn't make sense and was inconsistent with
    the other ports and even wxListBox in wxMSW itself.
    
    It also affected wxRearrangeCtrl under MSW, which uses wxCheckListBox
    for its implementation.
    
    Closes #19201.

--- include/wx/msw/ownerdrw.h
+++ include/wx/msw/ownerdrw.h
@@ -21,6 +21,12 @@ public:
 
     virtual bool OnDrawItem(wxDC& dc, const wxRect& rc,
                             wxODAction act, wxODStatus stat) wxOVERRIDE;
+
+protected:
+    // get the type of the text to draw in OnDrawItem(), by default is
+    // DST_PREFIXTEXT but can be overridden to return DST_TEXT when not using
+    // mnemonics
+    virtual int MSWGetTextType() const;
 };
 
 #endif // wxUSE_OWNER_DRAWN
