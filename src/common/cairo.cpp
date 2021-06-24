204db7e76a8359ad76366de273e146d8566ac3e2
Artur Wieczorek <artwik@wp.pl>
2021-06-24 19:01:27 +0200

    Add missing reference to dynamically loaded Cairo API
    (cherry picked from commit 36f84d0257fd1aba8e33cccd35789820384faadc)

--- src/common/cairo.cpp
+++ src/common/cairo.cpp
@@ -183,6 +183,8 @@
        (cairo_t *cr, const cairo_font_options_t* options), (cr, options) ) \
     m( cairo_get_font_options, \
        (cairo_t* cr, cairo_font_options_t* options), (cr, options) ) \
+    m( cairo_user_to_device_distance, \
+       (cairo_t* cr, double *dx, double* dy), (cr, dx, dy) )
 
 #ifdef __WXMAC__
 #define wxCAIRO_PLATFORM_METHODS(m) \
