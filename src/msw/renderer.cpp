wxWidgets/wxWidgets

commit 90ba137f2059355f9dd85de932653dd6a4e317e6

Work around text extent differences resulting in clipped text
Under at least some versions of Windows 10 with wxDVC themed text can be
clipped horizontally because of discrepancies between the text extent
as drawn by DrawThemeTextEx() and calculated with GetTextExtent()
earlier.

Work around the issue by always trying to use GetThemeTextExtent() in
DrawItemText(), not just for alignment of multi-line strings, and adjust
for any width differences similar to the existing adjustment for height.

See #18487.

src/msw/renderer.cpp
@@ -1104,13 +1104,16 @@ void wxRendererXP::DrawItemText(wxWindow* win,
         such alignment use DT_TOP (0), which does work for multi-lines,
         and deal with the actual desired vertical alignment ourselves with
         the help of GetThemeTextExtent().
-        */
-        bool useTopDrawing =
-            s_GetThemeTextExtent
-            && ( align & (wxALIGN_BOTTOM | wxALIGN_CENTRE_VERTICAL) ) != 0
-            && text.Contains(wxS('\n'));
 
-        if ( useTopDrawing )
+        [TODO] Ideally text measurement should only be needed for the above
+        mentioned situations but because there can be a difference between
+        the extent from GetThemeTextExtent() and the rect received by this
+        function could have involved other text measurements (e.g. with wxDVC,
+        see #18487), use it in all cases for now.
+        */
+        bool useTopDrawing = false;
+
+        if ( s_GetThemeTextExtent != NULL )
         {
             /*
             Get the actual text extent using GetThemeTextExtent() and adjust
@@ -1152,6 +1155,30 @@ void wxRendererXP::DrawItemText(wxWindow* win,
             if ( SUCCEEDED(hr) )
             {
                 /*
+                Compensate for rare cases where the horizontal extents differ
+                slightly. Don't use the width of the passed rect here to deal
+                with horizontal alignment as it results in the text always
+                fitting and ellipsization then can't occur. Instead check for
+                width differences by comparing with the extent as calculated
+                by wxDC.
+                */
+                const int textWidthDc = dc.GetMultiLineTextExtent(text).x;
+                const int widthDiff = textWidthDc - rcExtent.right;
+                if ( widthDiff )
+                {
+                    if ( align & wxALIGN_CENTRE_HORIZONTAL )
+                    {
+                        const int widthOffset = widthDiff / 2;
+                        rc.left += widthOffset;
+                        rc.right -= widthOffset;
+                    }
+                    else if ( align & wxALIGN_RIGHT )
+                        rc.left += widthDiff;
+                    else // left aligned
+                        rc.right -= widthDiff;
+                }
+
+                /*
                 For height compare with the height of the passed rect and use
                 the difference for handling vertical alignment. This has
                 consequences for particularly multi-line text: it will now
@@ -1163,8 +1190,12 @@ void wxRendererXP::DrawItemText(wxWindow* win,
                 necessity) confines rendering to a cell's bounds.
                 */
                 const int heightDiff = rect.GetHeight() - rcExtent.bottom;
-                if ( heightDiff )
+                if ( heightDiff
+                     && (align & (wxALIGN_BOTTOM | wxALIGN_CENTRE_VERTICAL))
+                     && text.Contains(wxS('\n')) )
                 {
+                    useTopDrawing = true;
+
                     if ( align & wxALIGN_CENTRE_VERTICAL )
                     {
                         const int heightOffset = heightDiff / 2;
@@ -1176,10 +1207,6 @@ void wxRendererXP::DrawItemText(wxWindow* win,
                     else // top aligned
                         rc.bottom -= heightDiff;
                 }
-            }
-            else
-            {
-                useTopDrawing = false;
             }
         }
 

commit 318c1c2211a3c9a10f73830d5ccb9ecafbd7a551

Fix recent regression with drawing themed multi-line text
Remove conditions from MSW themed text drawing that prevent multi-lines
from being rendered properly and instead always use DT_TOP alignment
for multi-lines.

This fixes a regression occurring since 90ba137f20 (Work around text
extent differences resulting in clipped text, 2021-07-26) with themed
Vista+ :

Non-top aligned multi-line text gets rendered as a single line because
of DT_SINGLELINE being used if the height of the drawing rect and
multi-line text happen to be equal. This occurs on the "Variable line
height" page of the dataview sample, which calculates its cell height
by multiplying the text extent by the number of lines in the text (a
requirement with wxMSW, not wxGTK).

Removing the conditions also exposes top aligned multi-line themed text
to drawing beyond the drawing rect, which actually makes it behave more
like wxGTK and wxOSX as well as wxMSW non-themed drawing as they try to
render text fully without regard for the bounds of the drawing rect.

src/msw/renderer.cpp
@@ -1184,18 +1184,15 @@ void wxRendererXP::DrawItemText(wxWindow* win,
                 consequences for particularly multi-line text: it will now
                 always try to fit vertically while a rect received from wxDVC
                 may have its extent based on calculations for a single line
-                only and therefore couldn't show more than one line.
-                As a result of the expanded height clipping may be needed
-                which at least already happens with wxDVC which (out of
-                necessity) confines rendering to a cell's bounds.
+                only and therefore couldn't show more than one line. This is
+                consistent with other major platforms where no clipping to
+                the rect takes places either, including non-themed MSW.
                 */
-                const int heightDiff = rect.GetHeight() - rcExtent.bottom;
-                if ( heightDiff
-                     && (align & (wxALIGN_BOTTOM | wxALIGN_CENTRE_VERTICAL))
-                     && text.Contains(wxS('\n')) )
+                if ( text.Contains(wxS('\n')) )
                 {
                     useTopDrawing = true;
 
+                    const int heightDiff = rect.GetHeight() - rcExtent.bottom;
                     if ( align & wxALIGN_CENTRE_VERTICAL )
                     {
                         const int heightOffset = heightDiff / 2;