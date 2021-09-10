32769bf0ce5a144515ff118aba65b8cfc725f620
oneeyeman1 <ikorot01@gmail.com>
2021-09-10 08:53:25 -0500

    Fixing license name

--- include/wx/osx/desktopenv.h
+++ include/wx/osx/desktopenv.h
@@ -4,7 +4,7 @@
 // Author:      Igor Korot
 // Created:     2021-09-05
 // Copyright:   (c) 2021 wxWidgets development team
-// Licence:     wxWidgets licence
+// Licence:     wxWindows licence
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef _WX_OSX_DESKTOPENV_H_
