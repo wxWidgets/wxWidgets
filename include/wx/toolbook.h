5d1c79d21831517b0b51f50aa4e7c9a726248e6f
Vadim Zeitlin <vadim@wxwidgets.org>
2021-11-12 17:51:17 +0000

    Remove unnecessary wxToolbook::SetImageList() override
    This wasn't doing anything and is just unnecessary.

--- include/wx/toolbook.h
+++ include/wx/toolbook.h
@@ -81,7 +81,6 @@ public:
                             int imageId = NO_IMAGE) wxOVERRIDE;
     virtual int SetSelection(size_t n) wxOVERRIDE { return DoSetSelection(n, SetSelection_SendEvent); }
     virtual int ChangeSelection(size_t n) wxOVERRIDE { return DoSetSelection(n); }
-    virtual void SetImageList(wxImageList *imageList) wxOVERRIDE;
 
     virtual bool DeleteAllPages() wxOVERRIDE;
     virtual int HitTest(const wxPoint& pt, long *flags = NULL) const wxOVERRIDE;
