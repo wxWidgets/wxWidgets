wxWidgets/wxWidgets

commit 9d86ba11e2a64197b34dcfa81200e66be2ffa913

Add support for wxBitmapBundle to wxGTK wxNotebook
Just use wxWithImages::GetBitmapBundle() directly with wxGtkImage
without passing by wxImageList -- this suffices to have working high DPI
support for notebook page icons.

src/gtk/notebook.cpp
@@ -283,9 +283,10 @@ bool wxNotebook::SetPageImage( size_t page, int image )
     wxCHECK_MSG(page < GetPageCount(), false, "invalid notebook index");
 
     wxGtkNotebookPage* pageData = GetNotebookPage(page);
-    if (image >= 0)
+
+    const wxBitmapBundle bundle = GetBitmapBundle(image);
+    if ( bundle.IsOk() )
     {
-        wxCHECK_MSG(HasImageList(), false, "invalid notebook imagelist");
         if (pageData->m_image == NULL)
         {
             pageData->m_image = wxGtkImage::New();
@@ -293,7 +294,7 @@ bool wxNotebook::SetPageImage( size_t page, int image )
             gtk_box_pack_start(GTK_BOX(pageData->m_box),
                 pageData->m_image, false, false, m_padding);
         }
-        WX_GTK_IMAGE(pageData->m_image)->Set(GetImageList()->GetBitmap(image));
+        WX_GTK_IMAGE(pageData->m_image)->Set(bundle);
     }
     else if (pageData->m_image)
     {
@@ -481,21 +482,13 @@ bool wxNotebook::InsertPage( size_t position,
     gtk_container_set_border_width(GTK_CONTAINER(pageData->m_box), 2);
 #endif
 
-    pageData->m_image = NULL;
-    if (imageId != -1)
+    const wxBitmapBundle bundle = GetBitmapBundle(imageId);
+    if ( bundle.IsOk() )
     {
-        if (HasImageList())
-        {
-            const wxBitmap bitmap = GetImageList()->GetBitmap(imageId);
-            pageData->m_image = wxGtkImage::New();
-            WX_GTK_IMAGE(pageData->m_image)->Set(bitmap);
-            gtk_box_pack_start(GTK_BOX(pageData->m_box),
-                pageData->m_image, false, false, m_padding);
-        }
-        else
-        {
-            wxFAIL_MSG("invalid notebook imagelist");
-        }
+        pageData->m_image = wxGtkImage::New();
+        WX_GTK_IMAGE(pageData->m_image)->Set(bundle);
+        gtk_box_pack_start(GTK_BOX(pageData->m_box),
+            pageData->m_image, false, false, m_padding);
     }
 
     /* set the label text */

commit 26c6273a70b14d35d460b2c1adc663ef49a766c2

Fix crash when setting wxGTK wxNotebook image after creation
The bug was introduced in 9d86ba11e2 (Add support for wxBitmapBundle to
wxGTK wxNotebook, 2021-11-07) after which wxGtkNotebookPage::m_image
remained uninitialized if the page didn't have an image initially, so
fix it by simply initializing this pointer to NULL in this case.

It would arguably be better if wxGtkNotebookPage initialized all its
members in the ctor, but for now just fix the critical bug.

Closes #19332.

src/gtk/notebook.cpp
@@ -490,6 +490,10 @@ bool wxNotebook::InsertPage( size_t position,
         gtk_box_pack_start(GTK_BOX(pageData->m_box),
             pageData->m_image, false, false, m_padding);
     }
+    else
+    {
+        pageData->m_image = NULL;
+    }
 
     /* set the label text */
     pageData->m_label = gtk_label_new(wxGTK_CONV(wxStripMenuCodes(text)));