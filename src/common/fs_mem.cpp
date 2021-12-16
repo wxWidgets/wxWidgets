f84c3a79681df4c0a0ebdeb624f4487683225180
Vadim Zeitlin <vadim@wxwidgets.org>
2021-12-16 21:54:43 +0100

    Fall back to using UTF-8 in wxMemoryFSHandler::AddFile()
    This seems to be better than just losing the data completely if
    converting it to Latin-1 fails.

--- src/common/fs_mem.cpp
+++ src/common/fs_mem.cpp
@@ -180,7 +180,12 @@ void wxMemoryFSHandlerBase::AddFileWithMimeType(const wxString& filename,
                                                 const wxString& textdata,
                                                 const wxString& mimetype)
 {
-    const wxCharBuffer buf(textdata.To8BitData());
+    // We try to use the provided data "as is" if possible, but if not, we fall
+    // back to UTF-8 because it's better to do this than just lose the data
+    // completely.
+    wxCharBuffer buf(textdata.To8BitData());
+    if ( !buf.length() )
+        buf = textdata.utf8_str();
 
     AddFileWithMimeType(filename, buf.data(), buf.length(), mimetype);
 }
