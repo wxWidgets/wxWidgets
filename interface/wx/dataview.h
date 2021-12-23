aacada0ea21b922960741a99a4efca2bf3887a62
Paul Cornett <paulcor@users.noreply.github.com>
2021-12-22 17:12:45 -0800

    wxEVT_DATAVIEW_ITEM_CONTEXT_MENU position should be in client coordinates
    For consistency with wxTreeCtrl and wxListCtrl. See #19188

--- interface/wx/dataview.h
+++ interface/wx/dataview.h
@@ -3824,7 +3824,7 @@ public:
     wxDataViewModel* GetModel() const;
 
     /**
-        Returns the position of a context menu event in screen coordinates.
+        Returns the position of a context menu event in client coordinates.
     */
     wxPoint GetPosition() const;
 
