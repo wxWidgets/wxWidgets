540db9a5813d26c97989e1096ee1c5bff5edc467
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2022-01-01 00:46:08 -0500

    set_target_properties and add_definitions for UWP

--- build/cmake/functions.cmake
+++ build/cmake/functions.cmake
@@ -370,6 +370,11 @@ function(wx_set_target_properties target_name is_base)
         VERSION ${wxSOVERSION}
     )
 
+    if (wxUWP)
+        set_target_properties(${target_name} PROPERTIES VS_WINRT_EXTENSIONS TRUE)
+        add_definitions(-ZW)
+    endif()
+
     wx_set_common_target_properties(${target_name})
 endfunction()
 
