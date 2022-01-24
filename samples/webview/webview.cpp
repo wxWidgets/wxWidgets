wxWidgets/wxWidgets

commit ccb6b10c1fe690b748b85b6ff47b6acd98e34385

Use SVG-based toolbar bitmaps in webview sample
In webview sample toolbar use wxBitmapBundles created from SVG files
instead of XPM bitmaps, as this results in much better appearance in
high DPI.

Closes #2642.

samples/webview/webview.cpp
@@ -52,11 +52,6 @@
 #include "wx/textctrl.h"
 #endif
 
-#if defined(__WXMSW__) || defined(__WXOSX__)
-#include "stop.xpm"
-#include "refresh.xpm"
-#endif
-
 #include "wxlogo.xpm"
 
 
@@ -310,22 +305,31 @@ WebFrame::WebFrame(const wxString& url) :
 
     wxBoxSizer* topsizer = new wxBoxSizer(wxVERTICAL);
 
-    // Create the toolbar
-    m_toolbar = CreateToolBar(wxTB_TEXT);
-    m_toolbar->SetToolBitmapSize(wxSize(32, 32));
+    long toolbarStyle = wxTB_TEXT;
 
-    wxBitmap back = wxArtProvider::GetBitmap(wxART_GO_BACK , wxART_TOOLBAR);
-    wxBitmap forward = wxArtProvider::GetBitmap(wxART_GO_FORWARD , wxART_TOOLBAR);
-    #ifdef __WXGTK__
-        wxBitmap stop = wxArtProvider::GetBitmap("gtk-stop", wxART_TOOLBAR);
-    #else
-        wxBitmap stop = wxBitmap(stop_xpm);
-    #endif
-    #ifdef __WXGTK__
-        wxBitmap refresh = wxArtProvider::GetBitmap("gtk-refresh", wxART_TOOLBAR);
-    #else
-        wxBitmap refresh = wxBitmap(refresh_xpm);
-    #endif
+    wxBitmapBundle back;
+    wxBitmapBundle forward;
+    wxBitmapBundle stop;
+    wxBitmapBundle refresh;
+    wxBitmapBundle wxlogo;
+
+    // Create the toolbar
+#ifdef wxHAS_SVG
+    wxSize sizeBitmap = wxArtProvider::GetNativeSizeHint(wxART_TOOLBAR);
+
+    if ( !sizeBitmap.IsFullySpecified() )
+        sizeBitmap.Set(32, 32);
+
+    back    = wxBitmapBundle::FromSVGFile("webview_back.svg", sizeBitmap);
+    forward = wxBitmapBundle::FromSVGFile("webview_forward.svg", sizeBitmap);
+    stop    = wxBitmapBundle::FromSVGFile("webview_stop.svg", sizeBitmap);
+    refresh = wxBitmapBundle::FromSVGFile("webview_refresh.svg", sizeBitmap);
+    wxlogo  = wxBitmapBundle::FromSVGFile("wxlogo.svg", sizeBitmap);
+#else
+     toolbarStyle |= wxTB_NOICONS;
+#endif
+
+    m_toolbar = CreateToolBar(toolbarStyle);
 
     m_toolbar_back = m_toolbar->AddTool(wxID_ANY, _("Back"), back);
     m_toolbar_forward = m_toolbar->AddTool(wxID_ANY, _("Forward"), forward);
@@ -333,7 +337,7 @@ WebFrame::WebFrame(const wxString& url) :
     m_toolbar_reload = m_toolbar->AddTool(wxID_ANY, _("Reload"),  refresh);
     m_url = new wxTextCtrl(m_toolbar, wxID_ANY, "",  wxDefaultPosition, FromDIP(wxSize(400, -1)), wxTE_PROCESS_ENTER );
     m_toolbar->AddControl(m_url, _("URL"));
-    m_toolbar_tools = m_toolbar->AddTool(wxID_ANY, _("Menu"), wxBitmap(wxlogo_xpm));
+    m_toolbar_tools = m_toolbar->AddTool(wxID_ANY, _("Menu"), wxlogo);
 
     m_toolbar->Realize();
 

build/cmake/samples/CMakeLists.txt
@@ -152,7 +152,8 @@ wx_add_sample(typetest typetest.cpp typetest.h)
 wx_add_sample(uiaction DEPENDS wxUSE_UIACTIONSIMULATOR)
 wx_add_sample(validate validate.cpp validate.h DEPENDS wxUSE_VALIDATORS)
 wx_add_sample(vscroll vstest.cpp)
-wx_add_sample(webview LIBRARIES wxwebview DATA ../help/doc.zip:doc.zip
+wx_add_sample(webview LIBRARIES wxwebview
+    DATA ../help/doc.zip:doc.zip webview_back.svg webview_forward.svg webview_refresh.svg webview_stop.svg wxlogo.svg
     NAME webviewsample DEPENDS wxUSE_WEBVIEW)
 if(TARGET webviewsample AND wxUSE_STC)
     wx_exe_link_libraries(webviewsample wxstc)

samples/webview/Makefile.in
@@ -142,7 +142,7 @@ COND_wxUSE_REGEX_builtin___LIB_REGEX_p = \
 
 ### Targets: ###
 
-all: webview$(EXEEXT) $(__webview_bundle___depname)
+all: webview$(EXEEXT) $(__webview_bundle___depname) data
 
 install: 
 
@@ -190,6 +190,18 @@ webview$(EXEEXT): $(WEBVIEW_OBJECTS) $(__webview___win32rc)
 
 @COND_PLATFORM_MACOSX_1@webview_bundle: $(____webview_BUNDLE_TGT_REF_DEP)
 
+data: 
+	@mkdir -p .
+	@for f in webview_back.svg webview_forward.svg webview_refresh.svg webview_stop.svg wxlogo.svg; do \
+	if test ! -f ./$$f -a ! -d ./$$f ; \
+	then x=yep ; \
+	else x=`find $(srcdir)/$$f -newer ./$$f -print` ; \
+	fi; \
+	case "$$x" in ?*) \
+	cp -pRf $(srcdir)/$$f . ;; \
+	esac; \
+	done
+
 webview_sample_rc.o: $(srcdir)/../../samples/sample.rc
 	$(WINDRES) -i$< -o$@    --define __WX$(TOOLKIT)__ $(__WXUNIV_DEFINE_p_1) $(__DEBUG_DEFINE_p_1)  $(__EXCEPTIONS_DEFINE_p_1) $(__RTTI_DEFINE_p_1) $(__THREAD_DEFINE_p_1)  --include-dir $(srcdir) $(__DLLFLAG_p_1) $(__WIN32_DPI_MANIFEST_p) --include-dir $(srcdir)/../../samples $(__RCDEFDIR_p) --include-dir $(top_srcdir)/include
 
@@ -200,4 +212,4 @@ webview_webview.o: $(srcdir)/webview.cpp
 # Include dependency info, if present:
 @IF_GNU_MAKE@-include ./.deps/*.d
 
-.PHONY: all install uninstall clean distclean webview_bundle
+.PHONY: all install uninstall clean distclean webview_bundle data

samples/webview/makefile.gcc
@@ -219,7 +219,7 @@ $(OBJS):
 
 ### Targets: ###
 
-all: $(OBJS)\webview.exe
+all: $(OBJS)\webview.exe data
 
 clean: 
 	-if exist $(OBJS)\*.o del $(OBJS)\*.o
@@ -232,13 +232,17 @@ $(OBJS)\webview.exe: $(WEBVIEW_OBJECTS) $(OBJS)\webview_sample_rc.o
 	$(CXX) -o $@ @$@.rsp  $(__DEBUGINFO) $(__THREADSFLAG) -L$(LIBDIRNAME)  -Wl,--subsystem,windows -mwindows $(____CAIRO_LIBDIR_FILENAMES_p) $(LDFLAGS)  $(__WXLIB_WEBVIEW_p)  $(__WXLIB_STC_p) -limm32 $(__WXLIB_CORE_p)  $(__WXLIB_BASE_p)  -lwxscintilla$(WXDEBUGFLAG) $(__WXLIB_MONO_p) $(__LIB_SCINTILLA_IF_MONO_p) $(__LIB_TIFF_p) $(__LIB_JPEG_p) $(__LIB_PNG_p)   -lwxzlib$(WXDEBUGFLAG) -lwxregex$(WXUNICODEFLAG)$(WXDEBUGFLAG) -lwxexpat$(WXDEBUGFLAG) $(EXTRALIBS_FOR_BASE) $(__CAIRO_LIB_p) -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lwinspool -lwinmm -lshell32 -lshlwapi -lcomctl32 -lole32 -loleaut32 -luuid -lrpcrt4 -ladvapi32 -lversion -lws2_32 -lwininet -loleacc -luxtheme
 	@-del $@.rsp
 
+data: 
+	if not exist $(OBJS) mkdir $(OBJS)
+	for %%f in (webview_back.svg webview_forward.svg webview_refresh.svg webview_stop.svg wxlogo.svg) do if not exist $(OBJS)\%%f copy .\%%f $(OBJS)
+
 $(OBJS)\webview_sample_rc.o: ./../../samples/sample.rc
 	$(WINDRES) -i$< -o$@    --define __WXMSW__ $(__WXUNIV_DEFINE_p_1) $(__DEBUG_DEFINE_p_1) $(__NDEBUG_DEFINE_p_1) $(__EXCEPTIONS_DEFINE_p_1) $(__RTTI_DEFINE_p_1) $(__THREAD_DEFINE_p_1) $(__UNICODE_DEFINE_p_1) --include-dir $(SETUPHDIR) --include-dir ./../../include $(__CAIRO_INCLUDEDIR_p) --include-dir . $(__DLLFLAG_p_1) --define wxUSE_DPI_AWARE_MANIFEST=$(USE_DPI_AWARE_MANIFEST) --include-dir ./../../samples --define NOPCH
 
 $(OBJS)\webview_webview.o: ./webview.cpp
 	$(CXX) -c -o $@ $(WEBVIEW_CXXFLAGS) $(CPPDEPS) $<
 
-.PHONY: all clean
+.PHONY: all clean data
 
 
 SHELL := $(COMSPEC)

samples/webview/makefile.vc
@@ -422,7 +422,7 @@ $(OBJS):
 
 ### Targets: ###
 
-all: $(OBJS)\webview.exe
+all: $(OBJS)\webview.exe data
 
 clean: 
 	-if exist $(OBJS)\*.obj del $(OBJS)\*.obj
@@ -436,6 +436,10 @@ $(OBJS)\webview.exe: $(WEBVIEW_OBJECTS) $(OBJS)\webview_sample.res
 	link /NOLOGO /OUT:$@  $(__DEBUGINFO_1) /pdb:"$(OBJS)\webview.pdb" $(__DEBUGINFO_2)  $(LINK_TARGET_CPU) /LIBPATH:$(LIBDIRNAME) $(WIN32_DPI_LINKFLAG) /SUBSYSTEM:WINDOWS $(____CAIRO_LIBDIR_FILENAMES_p) $(LDFLAGS) @<<
 	$(WEBVIEW_OBJECTS) $(WEBVIEW_RESOURCES)  $(__WXLIB_WEBVIEW_p)  $(__WXLIB_STC_p) imm32.lib $(__WXLIB_CORE_p)  $(__WXLIB_BASE_p)  wxscintilla$(WXDEBUGFLAG).lib $(__WXLIB_MONO_p) $(__LIB_SCINTILLA_IF_MONO_p) $(__LIB_TIFF_p) $(__LIB_JPEG_p) $(__LIB_PNG_p)   wxzlib$(WXDEBUGFLAG).lib wxregex$(WXUNICODEFLAG)$(WXDEBUGFLAG).lib wxexpat$(WXDEBUGFLAG).lib $(EXTRALIBS_FOR_BASE) $(__CAIRO_LIB_p) kernel32.lib user32.lib gdi32.lib comdlg32.lib winspool.lib winmm.lib shell32.lib shlwapi.lib comctl32.lib ole32.lib oleaut32.lib uuid.lib rpcrt4.lib advapi32.lib version.lib ws2_32.lib wininet.lib
 <<
+
+data: 
+	if not exist $(OBJS) mkdir $(OBJS)
+	for %f in (webview_back.svg webview_forward.svg webview_refresh.svg webview_stop.svg wxlogo.svg) do if not exist $(OBJS)\%f copy .\%f $(OBJS)
 
 $(OBJS)\webview_sample.res: .\..\..\samples\sample.rc
 	rc /fo$@  /d WIN32 $(____DEBUGRUNTIME_3_p_1) /d _CRT_SECURE_NO_DEPRECATE=1 /d _CRT_NON_CONFORMING_SWPRINTFS=1 /d _SCL_SECURE_NO_WARNINGS=1 $(__NO_VC_CRTDBG_p_1)  $(__TARGET_CPU_COMPFLAG_p_1) /d __WXMSW__ $(__WXUNIV_DEFINE_p_1) $(__DEBUG_DEFINE_p_1) $(__NDEBUG_DEFINE_p_1) $(__EXCEPTIONS_DEFINE_p_1) $(__RTTI_DEFINE_p_1) $(__THREAD_DEFINE_p_1) $(__UNICODE_DEFINE_p_1) /i $(SETUPHDIR) /i .\..\..\include $(____CAIRO_INCLUDEDIR_FILENAMES_1_p) /i . $(__DLLFLAG_p_1)  /d _WINDOWS /i .\..\..\samples /d NOPCH .\..\..\samples\sample.rc

samples/webview/refresh.xpm
@@ -1,234 +1,0 @@
-/* XPM */
-static const char * refresh_xpm[] = {
-"24 24 207 2",
-"  	c None",
-". 	c #3162A2",
-"+ 	c #507BB3",
-"@ 	c #6A90C0",
-"# 	c #7296C4",
-"$ 	c #7397C4",
-"% 	c #7094C2",
-"& 	c #648BBD",
-"* 	c #507CB3",
-"= 	c #3061A2",
-"- 	c #8EACD1",
-"; 	c #B3CBE4",
-"> 	c #B7CEE6",
-", 	c #B8CEE6",
-"' 	c #B5CDE5",
-") 	c #B4CBE5",
-"! 	c #B0C9E3",
-"~ 	c #AEC6E1",
-"{ 	c #9FBBDB",
-"] 	c #648DBE",
-"^ 	c #2F60A2",
-"/ 	c #3262A4",
-"( 	c #3364A3",
-"_ 	c #3C6BA8",
-": 	c #A7C0DD",
-"< 	c #C1D6EA",
-"[ 	c #BAD0E7",
-"} 	c #B6CDE5",
-"| 	c #B6CCE6",
-"1 	c #B5CDE6",
-"2 	c #B3CBE5",
-"3 	c #AFC8E3",
-"4 	c #AAC5E2",
-"5 	c #ABC6E2",
-"6 	c #A0BCDB",
-"7 	c #4975AF",
-"8 	c #3666A5",
-"9 	c #89ACD3",
-"0 	c #2C5EA0",
-"a 	c #82A3CB",
-"b 	c #B5CCE4",
-"c 	c #7095C3",
-"d 	c #658CBC",
-"e 	c #678EBE",
-"f 	c #7196C2",
-"g 	c #84A4CC",
-"h 	c #A9C3E0",
-"i 	c #B8CFE7",
-"j 	c #ACC6E3",
-"k 	c #A0BEDE",
-"l 	c #9CBCDD",
-"m 	c #9EBDDE",
-"n 	c #6C93C1",
-"o 	c #7B9FCA",
-"p 	c #ABC7E3",
-"q 	c #2B5DA0",
-"r 	c #4976AE",
-"s 	c #8EADD0",
-"t 	c #4975AE",
-"u 	c #3667A6",
-"v 	c #3666A6",
-"w 	c #4170AB",
-"x 	c #5B83B6",
-"y 	c #9BB9DA",
-"z 	c #97B9DC",
-"A 	c #7EA7D3",
-"B 	c #7CA5D2",
-"C 	c #88AED6",
-"D 	c #8AAFD6",
-"E 	c #A4C1E0",
-"F 	c #406DAB",
-"G 	c #4F7BB2",
-"H 	c #4A77B3",
-"I 	c #4C79B4",
-"J 	c #2E5EA2",
-"K 	c #6F95C3",
-"L 	c #95B6D9",
-"M 	c #7FA8D3",
-"N 	c #76A1D0",
-"O 	c #709DCE",
-"P 	c #2B5DA1",
-"Q 	c #3667A5",
-"R 	c #4D7AB4",
-"S 	c #648DC3",
-"T 	c #4A78B3",
-"U 	c #3262A2",
-"V 	c #2C5FA0",
-"W 	c #6B91C0",
-"X 	c #97B8DB",
-"Y 	c #74A0CF",
-"Z 	c #709ECE",
-"` 	c #ACC7E3",
-" .	c #2D5EA0",
-"..	c #3767A6",
-"+.	c #5985BD",
-"@.	c #78A3D0",
-"#.	c #6C9BCC",
-"$.	c #AEC9E5",
-"%.	c #3263A3",
-"&.	c #3768A7",
-"*.	c #9BB8DA",
-"=.	c #9CB9DA",
-"-.	c #9CBADA",
-";.	c #9FBBDC",
-">.	c #A8C1DF",
-",.	c #2D5FA0",
-"'.	c #22569B",
-").	c #23579C",
-"!.	c #21569A",
-"~.	c #21559B",
-"{.	c #275A9E",
-"].	c #3364A4",
-"^.	c #24589C",
-"/.	c #24579C",
-"(.	c #416FAB",
-"_.	c #C0D3E8",
-":.	c #B8CCE4",
-"<.	c #B5CBE3",
-"[.	c #AFC7E1",
-"}.	c #B0C8E2",
-"|.	c #88A9CF",
-"1.	c #4471AC",
-"2.	c #B9D1E8",
-"3.	c #94B6DA",
-"4.	c #95B7DB",
-"5.	c #A3C1DF",
-"6.	c #A4BEDC",
-"7.	c #2D60A0",
-"8.	c #BBD1E9",
-"9.	c #BCD1E8",
-"0.	c #416EA9",
-"a.	c #3868A7",
-"b.	c #BAD1E9",
-"c.	c #96B7DB",
-"d.	c #98B9DC",
-"e.	c #A2C0DF",
-"f.	c #AAC4DF",
-"g.	c #5F88BA",
-"h.	c #80A8D7",
-"i.	c #CDDCEC",
-"j.	c #7A9FC8",
-"k.	c #4273B1",
-"l.	c #3465A4",
-"m.	c #4371AC",
-"n.	c #B9D1E9",
-"o.	c #B4CCE5",
-"p.	c #9DBCDD",
-"q.	c #8EB2D8",
-"r.	c #91B3D9",
-"s.	c #719AC8",
-"t.	c #4171AC",
-"u.	c #3A6AA8",
-"v.	c #3A75BD",
-"w.	c #9CBBDB",
-"x.	c #608BC1",
-"y.	c #3A6EAD",
-"z.	c #4C80BB",
-"A.	c #3365A4",
-"B.	c #C6DAED",
-"C.	c #6F94C1",
-"D.	c #8CABD1",
-"E.	c #9BBBDD",
-"F.	c #6C9ACD",
-"G.	c #6596CA",
-"H.	c #709DCF",
-"I.	c #7AA4D2",
-"J.	c #6A96C8",
-"K.	c #5885BB",
-"L.	c #4F7EB7",
-"M.	c #497AB6",
-"N.	c #487AB6",
-"O.	c #4275B5",
-"P.	c #4C7FBB",
-"Q.	c #578BC6",
-"R.	c #396CAA",
-"S.	c #789CC6",
-"T.	c #5681B6",
-"U.	c #8AAFD7",
-"V.	c #77A2D1",
-"W.	c #6496CA",
-"X.	c #6294C9",
-"Y.	c #6395CA",
-"Z.	c #5F92C9",
-"`.	c #5C8FC7",
-" +	c #598DC6",
-".+	c #588DC7",
-"++	c #4175B2",
-"@+	c #31619F",
-"#+	c #6089BB",
-"$+	c #77A0CE",
-"%+	c #6E9CCD",
-"&+	c #6696CA",
-"*+	c #6193C9",
-"=+	c #5F91C8",
-"-+	c #5C90C7",
-";+	c #5A8EC6",
-">+	c #558AC3",
-",+	c #4072AF",
-"'+	c #27528A",
-")+	c #2A5285",
-"!+	c #3C6599",
-"~+	c #4B77B0",
-"{+	c #4E7DB6",
-"]+	c #4C7CB7",
-"^+	c #4877B2",
-"/+	c #3B69A2",
-"(+	c #3361A0",
-"                                                ",
-"            . + @ # $ % & *                     ",
-"          = - ; > , ' ) ! ~ { ] ^       / (     ",
-"        _ : < [ } | 1 2 3 4 5 > 6 7   8 9 0     ",
-"        a b a c d e f g h i j k l m n o p q     ",
-"      r s t u v         w x y z A B C D E q     ",
-"      F G H I               J K L M N O 4 P     ",
-"      Q R S T U               V W X Y Z `  .    ",
-"      ..+.                    t L @.Z #.$.0     ",
-"      %.                    &.{ *.=.-.;.>.,.    ",
-"        '.).!.~.~.{.].      %.^./.^./.).'.      ",
-"      (._.:.<.[.}.|.V                     ].    ",
-"      1.2.3.4.5.6.7.                      %.    ",
-"      1.8.X l 9.0.                      a.(     ",
-"      1.b.c.d.e.f.g.              h.i.j.k.l.    ",
-"      m.n.o.p.3.q.r.s.t.u.      v.w.x.y.z.A.    ",
-"      m.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.      ",
-"      (.S.    T.U.V.W.X.Y.Y.X.Z.`. +.+++        ",
-"      ].        @+#+$+%+&+*+=+-+;+>+,+          ",
-"                  '+)+!+~+{+]+^+/+(+            ",
-"                                                ",
-"                                                ",
-"                                                ",
-"                                                "};

samples/webview/stop.xpm
@@ -1,331 +1,0 @@
-/* XPM */
-static const char * stop_xpm[] = {
-"24 24 304 2",
-"  	c None",
-". 	c #920F0F",
-"+ 	c #931212",
-"@ 	c #931111",
-"# 	c #8F0909",
-"$ 	c #981414",
-"% 	c #DC6666",
-"& 	c #F07575",
-"* 	c #EF7272",
-"= 	c #EE7171",
-"- 	c #EE6F6F",
-"; 	c #EE6D6D",
-"> 	c #EE6C6C",
-", 	c #E46969",
-"' 	c #980F0F",
-") 	c #8E0606",
-"! 	c #EA7B7B",
-"~ 	c #E25858",
-"{ 	c #D84040",
-"] 	c #D83F3F",
-"^ 	c #D83E3E",
-"/ 	c #D83D3D",
-"( 	c #D73C3C",
-"_ 	c #D73A3A",
-": 	c #DD4A4A",
-"< 	c #EC7272",
-"[ 	c #950909",
-"} 	c #9A1616",
-"| 	c #EB7C7C",
-"1 	c #E15959",
-"2 	c #D74040",
-"3 	c #D94444",
-"4 	c #D94343",
-"5 	c #D94242",
-"6 	c #D94141",
-"7 	c #D93F3F",
-"8 	c #D73D3D",
-"9 	c #D73939",
-"0 	c #DD4747",
-"a 	c #E96D6D",
-"b 	c #A21818",
-"c 	c #9D1717",
-"d 	c #DC6868",
-"e 	c #E25959",
-"f 	c #D94545",
-"g 	c #D84545",
-"h 	c #DA4545",
-"i 	c #D84242",
-"j 	c #D84141",
-"k 	c #D63D3D",
-"l 	c #D73B3B",
-"m 	c #D63838",
-"n 	c #DC4747",
-"o 	c #D95555",
-"p 	c #9D1414",
-"q 	c #940909",
-"r 	c #E15757",
-"s 	c #DC4848",
-"t 	c #F64949",
-"u 	c #D94747",
-"v 	c #D94646",
-"w 	c #D73F3F",
-"x 	c #EA4141",
-"y 	c #E53D3D",
-"z 	c #D53A3A",
-"A 	c #D43838",
-"B 	c #DB4747",
-"C 	c #E66161",
-"D 	c #920606",
-"E 	c #9D1818",
-"F 	c #EC7C7C",
-"G 	c #DF5353",
-"H 	c #DC4545",
-"I 	c #F15A5A",
-"J 	c #EEB2B2",
-"K 	c #F55757",
-"L 	c #DB4545",
-"M 	c #EA4545",
-"N 	c #F38383",
-"O 	c #F27979",
-"P 	c #DC3D3D",
-"Q 	c #D33838",
-"R 	c #D13535",
-"S 	c #D84343",
-"T 	c #E35C5C",
-"U 	c #980E0E",
-"V 	c #BD4949",
-"W 	c #E25656",
-"X 	c #D74343",
-"Y 	c #F44F4F",
-"Z 	c #EBBFBF",
-"` 	c #E4EBEB",
-" .	c #EBC7C7",
-"..	c #F15D5D",
-"+.	c #DB4141",
-"@.	c #EA4747",
-"#.	c #F09C9C",
-"$.	c #E4EFEF",
-"%.	c #E5DDDD",
-"&.	c #FC5858",
-"*.	c #D53939",
-"=.	c #D13737",
-"-.	c #CE3333",
-";.	c #B93434",
-">.	c #DC4949",
-",.	c #E34545",
-"'.	c #F48F8F",
-").	c #E7E6E6",
-"!.	c #E8E9E9",
-"~.	c #EDCBCB",
-"{.	c #F45A5A",
-"].	c #EA4C4C",
-"^.	c #F2A3A3",
-"/.	c #E9E9E9",
-"(.	c #E7E9E9",
-"_.	c #F0B0B0",
-":.	c #E74848",
-"<.	c #CF3636",
-"[.	c #CE3535",
-"}.	c #D23A3A",
-"|.	c #BC4949",
-"1.	c #DD4848",
-"2.	c #E74444",
-"3.	c #F29E9E",
-"4.	c #EAE9E9",
-"5.	c #EBF0F0",
-"6.	c #F0CECE",
-"7.	c #F79191",
-"8.	c #EAF4F4",
-"9.	c #EBECEC",
-"0.	c #EFC4C4",
-"a.	c #EA4B4B",
-"b.	c #D13939",
-"c.	c #CF3737",
-"d.	c #CD3434",
-"e.	c #D13A3A",
-"f.	c #B83333",
-"g.	c #D73E3E",
-"h.	c #E74646",
-"i.	c #F59E9E",
-"j.	c #EFE8E8",
-"k.	c #EEF1F1",
-"l.	c #EEF5F5",
-"m.	c #F3BABA",
-"n.	c #EA5050",
-"o.	c #D23B3B",
-"p.	c #D03A3A",
-"q.	c #CF3939",
-"r.	c #CC3535",
-"s.	c #CA3131",
-"t.	c #B83232",
-"u.	c #BD4848",
-"v.	c #D83A3A",
-"w.	c #F05D5D",
-"x.	c #F5BEBE",
-"y.	c #F1F2F2",
-"z.	c #F2E6E6",
-"A.	c #FE7A7A",
-"B.	c #CF3434",
-"C.	c #C92121",
-"D.	c #C61212",
-"E.	c #C10707",
-"F.	c #C00505",
-"G.	c #BE0404",
-"H.	c #C30C0C",
-"I.	c #B72222",
-"J.	c #BC4848",
-"K.	c #DC4343",
-"L.	c #D73838",
-"M.	c #E95A5A",
-"N.	c #F8ADAD",
-"O.	c #F2F9F9",
-"P.	c #F3F3F3",
-"Q.	c #F4F2F2",
-"R.	c #F3F7F7",
-"S.	c #F2D5D5",
-"T.	c #F05050",
-"U.	c #C30000",
-"V.	c #BF0000",
-"W.	c #BE0000",
-"X.	c #BD0000",
-"Y.	c #BB0000",
-"Z.	c #C00707",
-"`.	c #B71F1F",
-" +	c #BC4646",
-".+	c #DC4141",
-"++	c #D63636",
-"@+	c #D63737",
-"#+	c #EA5C5C",
-"$+	c #F6B9B9",
-"%+	c #F4FAFA",
-"&+	c #F9C5C5",
-"*+	c #FCACAC",
-"=+	c #F4F0F0",
-"-+	c #F1F6F6",
-";+	c #EB5F5F",
-">+	c #C00101",
-",+	c #B90000",
-"'+	c #BF0707",
-")+	c #B61D1D",
-"!+	c #BC4545",
-"~+	c #DA3E3E",
-"{+	c #D73535",
-"]+	c #D63434",
-"^+	c #EA6363",
-"/+	c #F5BFBF",
-"(+	c #F1F1F1",
-"_+	c #F5F6F6",
-":+	c #FAC9C9",
-"<+	c #DF3C3C",
-"[+	c #D21B1B",
-"}+	c #FBADAD",
-"|+	c #F5F4F4",
-"1+	c #F2F2F2",
-"2+	c #F1D7D7",
-"3+	c #F07373",
-"4+	c #B80000",
-"5+	c #BD0707",
-"6+	c #B61C1C",
-"7+	c #B53B3B",
-"8+	c #E75D5D",
-"9+	c #D63333",
-"0+	c #D53333",
-"a+	c #D42F2F",
-"b+	c #F58686",
-"c+	c #F3D4D4",
-"d+	c #F2F3F3",
-"e+	c #F6D3D3",
-"f+	c #E44343",
-"g+	c #C40000",
-"h+	c #C20000",
-"i+	c #D82C2C",
-"j+	c #F7B7B7",
-"k+	c #F0E9E9",
-"l+	c #FA8F8F",
-"m+	c #BE0A0A",
-"n+	c #B70000",
-"o+	c #B60000",
-"p+	c #C20C0C",
-"q+	c #B41919",
-"r+	c #8B0101",
-"s+	c #C44848",
-"t+	c #EC5F5F",
-"u+	c #D52A2A",
-"v+	c #D22424",
-"w+	c #CE1C1C",
-"x+	c #E96969",
-"y+	c #FBC4C4",
-"z+	c #E44949",
-"A+	c #C40101",
-"B+	c #C10000",
-"C+	c #D53131",
-"D+	c #FBBEBE",
-"E+	c #F09292",
-"F+	c #C00E0E",
-"G+	c #B50000",
-"H+	c #C71212",
-"I+	c #980707",
-"J+	c #890101",
-"K+	c #B52828",
-"L+	c #E63D3D",
-"M+	c #C90202",
-"N+	c #C70000",
-"O+	c #C80505",
-"P+	c #D32424",
-"Q+	c #C61B1B",
-"R+	c #C11111",
-"S+	c #C81313",
-"T+	c #8C0101",
-"U+	c #880000",
-"V+	c #BE2E2E",
-"W+	c #DC2D2D",
-"X+	c #C50000",
-"Y+	c #BC0000",
-"Z+	c #C30D0D",
-"`+	c #CE2424",
-" @	c #850000",
-".@	c #BE2B2B",
-"+@	c #E33434",
-"@@	c #C00000",
-"#@	c #CC1616",
-"$@	c #CB2222",
-"%@	c #910404",
-"&@	c #790000",
-"*@	c #B21D1D",
-"=@	c #E03131",
-"-@	c #B40000",
-";@	c #CD1818",
-">@	c #C61F1F",
-",@	c #830000",
-"'@	c #730000",
-")@	c #B92525",
-"!@	c #E14444",
-"~@	c #E04141",
-"{@	c #DF3F3F",
-"]@	c #DE3C3C",
-"^@	c #DE3A3A",
-"/@	c #DE3737",
-"(@	c #DD3535",
-"_@	c #DD3232",
-":@	c #C42121",
-"<@	c #7E0000",
-"[@	c #720000",
-"}@	c #770000",
-"              . + + + + + + @ @ #               ",
-"            $ % & * * * = - ; > , '             ",
-"          ) ! ~ { { { ] ^ / ( _ : < [           ",
-"        } | 1 2 3 3 4 5 6 7 ^ 8 9 0 a b         ",
-"      c d e 6 f g f h 4 i j 7 k l m n o p       ",
-"    q | r 4 f s t u v h i 6 w x y z A B C D     ",
-"  E F G w 4 H I J K L f i j M N O P Q R S T U   ",
-"  V W / 6 X Y Z `  ...+.j @.#.$.%.&.*.=.-.f ;.  ",
-"  V >.7 { i ,.'.).!.~.{.].^./.(._.:.=.<.[.}.;.  ",
-"  |.1.^ ] 7 { 2.3.4.5.6.7.8.9.0.a.b.c.[.d.e.f.  ",
-"  |.0 ( 8 ^ ] g.h.i.j.k.l.k.m.n.o.p.q.r.s.<.t.  ",
-"  u.H v.l ( / / *.w.x.y.y.z.A.B.C.D.E.F.G.H.I.  ",
-"  J.K.L.9 _ _ _ M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z.`.  ",
-"   +.+++@+L.@+#+$+O.%+&+*+=+-+S.;+>+X.Y.,+'+)+  ",
-"  !+~+{+{+]+^+/+(+_+:+<+[+}+|+1+2+3+Y.,+4+5+6+  ",
-"  7+8+9+0+a+b+c+d+e+f+g+h+i+j+P.k+l+m+n+o+p+q+  ",
-"  r+s+t+u+v+w+x+y+z+A+h+B+X.C+D+E+F+n+G+H+v+I+  ",
-"    J+K+L+M+N+O+P+A+h+B+V.X.Y.Q+R+4+o+S+v+T+    ",
-"      U+V+W+N+X+U.h+B+V.X.Y+Y.,+4+o+Z+`+T+      ",
-"         @.@+@A+h+@@W.X.Y+Y.,+4+G+#@$@%@        ",
-"          &@*@=@V.Y+Y.,+4+o+G+-@;@>@,@          ",
-"            '@)@!@~@{@]@^@/@(@_@:@<@            ",
-"              [@&@&@&@&@&@&@&@&@}@              ",
-"                                                "};

samples/webview/webview.bkl
@@ -17,4 +17,14 @@
         </if>
     </exe>
 
+    <wx-data id="data">
+        <files>
+            webview_back.svg
+            webview_forward.svg
+            webview_refresh.svg
+            webview_stop.svg
+            wxlogo.svg
+        </files>
+    </wx-data>
+
 </makefile>

commit 52845e70f52d56a8fe3bb614c6cbebdd27dc20cd

Fix webview sample toolbar icon loading
The changes in ccb6b10c1fe690b748b85b6ff47b6acd98e34385 broke loading
on macOS. Use the same logic as when searching for doc.zip.

samples/webview/webview.cpp
@@ -320,10 +320,19 @@ WebFrame::WebFrame(const wxString& url) :
     if ( !sizeBitmap.IsFullySpecified() )
         sizeBitmap.Set(32, 32);
 
-    back    = wxBitmapBundle::FromSVGFile("webview_back.svg", sizeBitmap);
-    forward = wxBitmapBundle::FromSVGFile("webview_forward.svg", sizeBitmap);
-    stop    = wxBitmapBundle::FromSVGFile("webview_stop.svg", sizeBitmap);
-    refresh = wxBitmapBundle::FromSVGFile("webview_refresh.svg", sizeBitmap);
+    wxPathList pathlist;
+    pathlist.Add(".");
+    pathlist.Add("..");
+    pathlist.Add("../webview");
+    pathlist.Add("../../../samples/webview");
+    wxFileName svgPath = wxFileName(pathlist.FindValidPath("webview_back.svg"));
+    back    = wxBitmapBundle::FromSVGFile(svgPath.GetFullPath(), sizeBitmap);
+    svgPath.SetName("webview_forward");
+    forward = wxBitmapBundle::FromSVGFile(svgPath.GetFullPath(), sizeBitmap);
+    svgPath.SetName("webview_stop");
+    stop    = wxBitmapBundle::FromSVGFile(svgPath.GetFullPath(), sizeBitmap);
+    svgPath.SetName("webview_refresh");
+    refresh = wxBitmapBundle::FromSVGFile(svgPath.GetFullPath(), sizeBitmap);
     wxlogo  = wxArtProvider::GetBitmapBundle(wxART_WX_LOGO);
 #else
      toolbarStyle |= wxTB_NOICONS;