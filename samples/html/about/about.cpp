bbcaa7b9e0cbce0e5099a27d512c456366c02eff
Alexander Koshelev <akoshelev@tt-solutions.com>
2022-02-11 16:17:44 +0300

    Use wxBitmapBundle in html about sample

--- samples/html/about/about.cpp
+++ samples/html/about/about.cpp
@@ -152,6 +152,7 @@ void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
     html = new wxHtmlWindow(&dlg, wxID_ANY, wxDefaultPosition, wxSize(380, 160), wxHW_SCROLLBAR_NEVER);
     html -> SetBorders(0);
     html -> LoadPage("data/about.htm");
+    html -> SetHTMLBackgroundImage(wxBitmapBundle::FromSVGFile("data/bg.svg", wxSize(65, 45)));
     html -> SetInitialSize(wxSize(html -> GetInternalRepresentation() -> GetWidth(),
                                   html -> GetInternalRepresentation() -> GetHeight()));
 
