b62a1f8f1aebf082e68bcf7f5c0d8d950352d258
Paul Cornett <paulcor@users.noreply.github.com>
2022-02-13 09:03:36 -0800

    wxOverlay implementation for Wayland

--- include/wx/private/overlay.h
+++ include/wx/private/overlay.h
@@ -13,6 +13,17 @@
 
 #include "wx/overlay.h"
 
+#if defined(__WXDFB__)
+    #define wxHAS_NATIVE_OVERLAY 1
+#elif defined(__WXOSX__) && wxOSX_USE_COCOA
+    #define wxHAS_NATIVE_OVERLAY 1
+#elif defined(__WXGTK3__)
+    #define wxHAS_NATIVE_OVERLAY 1
+    #define wxHAS_GENERIC_OVERLAY 1
+#else
+    #define wxHAS_GENERIC_OVERLAY 1
+#endif
+
 class wxOverlay::Impl
 {
 public:
