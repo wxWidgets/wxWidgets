6c9d1291f6f525cf2ff3573a484464ddffa92a81
Jouk <joukj@hrem.nano.tudelft.nl>
2022-02-24 10:11:20 +0100

    Fix build under OpenVMS where mkdtemp() is unavailable
    Just skip the part of the test using this function under OpenVMS.
    
    Closes #22158.

--- tests/filename/filenametest.cpp
+++ tests/filename/filenametest.cpp
@@ -712,6 +712,8 @@ TEST_CASE("wxFileName::Exists", "[filename]")
     // No need for wxFILE_EXISTS_NO_FOLLOW here; wxFILE_EXISTS_SYMLINK implies it
     CHECK( wxFileName::Exists("/proc/self", wxFILE_EXISTS_SYMLINK) );
 #endif // __LINUX__
+#ifndef __VMS
+   // OpenVMS does not have mkdtemp
     wxString name = dirTemp.GetPath() + "/socktmpdirXXXXXX";
     wxString socktempdir = wxString::From8BitData(mkdtemp(name.char_str()));
     wxON_BLOCK_EXIT2(wxRmdir, socktempdir, 0);
@@ -719,7 +721,6 @@ TEST_CASE("wxFileName::Exists", "[filename]")
     wxTCPServer server;
     server.Create(sockfile);
     CHECK( wxFileName::Exists(sockfile, wxFILE_EXISTS_SOCKET) );
-#ifndef __VMS
     wxString fifo = dirTemp.GetPath() + "/fifo";
    if (mkfifo(fifo.c_str(), 0600) == 0)
     {
