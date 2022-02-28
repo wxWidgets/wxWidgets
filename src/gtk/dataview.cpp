26570145df1fd4f1fbd922c5410e98fe8e2d6452
Alexander Koshelev <akoshelev@tt-solutions.com>
2022-02-28 20:22:37 +0300

    fixup! Use wxBitmapBundle in wxHeaderColumn

--- src/gtk/dataview.cpp
+++ src/gtk/dataview.cpp
@@ -3228,6 +3228,34 @@ static void wxGtkTreeCellDataFunc( GtkTreeViewColumn *WXUNUSED(column),
 
 } // extern "C"
 
+namespace
+{
+
+struct BitmapProvider: wxGtkImage::BitmapProvider
+{
+    BitmapProvider(wxDataViewColumn* column) : m_column(column) { }
+
+    virtual double GetScale() const wxOVERRIDE;
+    virtual wxBitmap Get() const wxOVERRIDE;
+    wxDataViewColumn* const m_column;
+};
+
+double BitmapProvider::GetScale() const
+{
+    return m_column->GetOwner()->GetDPIScaleFactor();
+}
+
+wxBitmap BitmapProvider::Get() const
+{
+#ifdef __WXGTK3__
+    if (IsScaled())
+        return GetAtScale(m_column->GetBitmapBundle());
+#endif
+    return wxBitmap();
+}
+
+} // namespace
+
 #include <wx/listimpl.cpp>
 WX_DEFINE_LIST(wxDataViewColumnList)
 
@@ -3267,7 +3295,7 @@ void wxDataViewColumn::Init(wxAlignment align, int flags, int width)
     GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1);
     gtk_widget_show( box );
     // gtk_container_set_border_width((GtkContainer*)box, 2);
-    m_image = wxGtkImage::New();
+    m_image = wxGtkImage::New(new BitmapProvider(this));
     gtk_box_pack_start(GTK_BOX(box), m_image, FALSE, FALSE, 1);
     m_label = gtk_label_new("");
     gtk_box_pack_end( GTK_BOX(box), GTK_WIDGET(m_label), FALSE, FALSE, 1 );
