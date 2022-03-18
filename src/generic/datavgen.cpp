wxWidgets/wxWidgets

commit e5bd3ecec66815f2b41b36f923e6f10f8fe7f53d

Handle item adding to DVC to a non-container node

src/generic/datavgen.cpp
@@ -3094,6 +3094,14 @@ bool wxDataViewMainWindow::ItemAdded(const wxDataViewItem & parent, const wxData
         if ( !parentNode )
             return false;
 
+        // If the parent has not children then just mask it as container and return.
+        // Nodes will be initialized in Expand().
+        if ( !parentNode->HasChildren() )
+        {
+            parentNode->SetHasChildren(true);
+            return true;
+        }
+
         // If the parent has children but child nodes was not initialized then
         // the node is collapsed so just return as nodes will be initialized in
         // Expand().

commit f6c1230f71f666f74d33037d9abfc1e4192e8483

Fix regression when adding items to an open node of wxDVC
This fixes a regression introduced in e5bd3ecec6 (Handle item adding to
DVC to a non-container node, 2022-03-17).

See #22201.

Closes #22205, #22206.

src/generic/datavgen.cpp
@@ -3102,10 +3102,10 @@ bool wxDataViewMainWindow::ItemAdded(const wxDataViewItem & parent, const wxData
             return true;
         }
 
-        // If the parent has children but child nodes was not initialized then
-        // the node is collapsed so just return as nodes will be initialized in
+        // If the parent has children but child nodes was not initialized and
+        // the node is collapsed then just return as nodes will be initialized in
         // Expand().
-        if ( parentNode->GetChildNodes().empty() )
+        if ( !parentNode->IsOpen() && parentNode->GetChildNodes().empty() )
             return true;
 
         parentNode->SetHasChildren(true);