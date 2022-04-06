d273de49a925b76b8e9d7f08f36483424568832e
Vadim Zeitlin <vadim@wxwidgets.org>
2022-04-07 00:00:10 +0200

    Update version to 3.1.7 and rebake the makefiles
    Make it possible to distinguish the latest master from 3.1.6 before
    making any changes.

--- include/wx/osx/config_xcode.h
+++ include/wx/osx/config_xcode.h
@@ -124,9 +124,9 @@
 
 #define PACKAGE_BUGREPORT "wx-dev@googlegroups.com"
 #define PACKAGE_NAME "wxWidgets"
-#define PACKAGE_STRING "wxWidgets 3.1.6"
+#define PACKAGE_STRING "wxWidgets 3.1.7"
 #define PACKAGE_TARNAME "wxwidgets"
-#define PACKAGE_VERSION "3.1.6"
+#define PACKAGE_VERSION "3.1.7"
 
 // for jpeg
 
