wxWidgets/wxWidgets

commit d311c705d7ffc9f97c900dd4ed5d21489577f583

Make native dialogs application-modal in wxMSW
Being able to use other application windows while a modal dialog was
shown was unexpected and could easily result in nasty reentrancies, and
was also inconsistent with the behaviour of ShowModal() for the dialogs
defined in wx, so disable all the windows, not just the parent one,
while showing the native modal dialogs too.

Closes #11887.

src/msw/colordlg.cpp
@@ -177,6 +177,8 @@ int wxColourDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/dirdlg.cpp
@@ -152,6 +152,8 @@ int wxDirDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog();
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/filedlg.cpp
@@ -430,6 +430,8 @@ int wxFileDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/fontdlg.cpp
@@ -92,6 +92,8 @@ int wxFontDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
     // It should be OK to always use GDI simulations

src/msw/msgdlg.cpp
@@ -539,6 +539,8 @@ int wxMessageDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
 #ifdef wxHAS_MSW_TASKDIALOG
     if ( HasNativeTaskDialog() )
     {

src/msw/printdlg.cpp
@@ -29,6 +29,7 @@
     #include "wx/app.h"
     #include "wx/dcprint.h"
     #include "wx/cmndata.h"
+    #include "wx/utils.h"                   // for wxWindowDisabler
 #endif
 
 #include "wx/printdlg.h"
@@ -804,6 +805,8 @@ wxWindowsPrintDialog::~wxWindowsPrintDialog()
 int wxWindowsPrintDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
+
+    wxWindowDisabler disableOthers(this);
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/richmsgdlg.cpp
@@ -17,6 +17,7 @@
 
 #ifndef WX_PRECOMP
     #include "wx/msw/private.h"
+    #include "wx/utils.h"                   // for wxWindowDisabler
 #endif
 
 // This will define wxHAS_MSW_TASKDIALOG if we have support for it in the
@@ -36,6 +37,8 @@ int wxRichMessageDialog::ShowModal()
 
     if ( HasNativeTaskDialog() )
     {
+        wxWindowDisabler disableOthers(this);
+
         // create a task dialog
         WinStruct<TASKDIALOGCONFIG> tdc;
         wxMSWTaskDialogConfig wxTdc(*this);

commit a5a5b1bb1553946dbd2b19cff7bf2fbfc9c0717c

Fix restoring focus after showing native modal dialogs in wxMSW
This fixes a regression introduced in d311c705d7 (Make native dialogs
application-modal in wxMSW, 2022-04-01) which resulted in a focus loss
after showing a modal dialog, as the parent window was still disabled
when the native modal dialog was closed and so focus couldn't be set to
any of its children and was simply lost as the result.

Fix this by not disabling the dialog parent ourselves, but leaving
handling the parent status to the native modal dialog, which already
takes care of disabling the parent it uses when it is shown and
re-enabling it when it is dismissed. Still use wxWindowDisabler to
disable all the other top level windows, if any, to ensure that the
modal dialogs are truly app-modal and not just window-modal, i.e. ensure
that #11887 still remains fixed.

Closes #22285.

src/msw/colordlg.cpp
@@ -177,7 +177,7 @@ int wxColourDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/dirdlg.cpp
@@ -152,7 +152,7 @@ int wxDirDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
     wxWindow* const parent = GetParentForModalDialog();
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/filedlg.cpp
@@ -430,7 +430,7 @@ int wxFileDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/fontdlg.cpp
@@ -92,7 +92,7 @@ int wxFontDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/msgdlg.cpp
@@ -539,7 +539,7 @@ int wxMessageDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
 #ifdef wxHAS_MSW_TASKDIALOG
     if ( HasNativeTaskDialog() )

src/msw/printdlg.cpp
@@ -806,7 +806,7 @@ int wxWindowsPrintDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this);
+    wxWindowDisabler disableOthers(this, GetParent());
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/richmsgdlg.cpp
@@ -37,7 +37,7 @@ int wxRichMessageDialog::ShowModal()
 
     if ( HasNativeTaskDialog() )
     {
-        wxWindowDisabler disableOthers(this);
+        wxWindowDisabler disableOthers(this, GetParent());
 
         // create a task dialog
         WinStruct<TASKDIALOGCONFIG> tdc;