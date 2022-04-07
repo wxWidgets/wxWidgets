wxWidgets/wxWidgets

commit eafa2481175976fca667d242aed547bb19a65b88

Avoid mirroring text when wxDC axes are inverted with GTK3
This was already done for RTL, just extend it to inversion of either axis,
as this is the behavior of MSW and GTK2.
See #22113

src/gtk/dc.cpp
@@ -91,7 +91,14 @@ void wxGTKCairoDCImpl::DoDrawText(const wxString& text, int x, int y)
     if (text.empty())
         return;
 
-    if (m_layoutDir == wxLayout_RightToLeft && text.find('\n') != wxString::npos)
+    cairo_t* cr = static_cast<cairo_t*>(m_graphicContext->GetNativeContext());
+    if (cr == NULL)
+        return;
+
+    double dx = 1, dy = 1;
+    cairo_user_to_device_distance(cr, &dx, &dy);
+    const bool xInverted = dx < 0;
+    if (xInverted && text.find('\n') != wxString::npos)
     {
         // RTL needs each line separately to position text properly.
         // DrawLabel() will split the text and call back for each line.
@@ -105,12 +112,19 @@ void wxGTKCairoDCImpl::DoDrawText(const wxString& text, int x, int y)
     CalcBoundingBox(x, y);
     CalcBoundingBox(x + w, y + h);
 
-    if (m_layoutDir == wxLayout_RightToLeft)
-    {
+    const bool yInverted = dy < 0;
+    if (xInverted || yInverted)
         m_graphicContext->PushState();
+    if (xInverted)
+    {
         // text is not mirrored
         m_graphicContext->Scale(-1, 1);
         x = -x - w;
+    }
+    if (yInverted)
+    {
+        m_graphicContext->Scale(1, -1);
+        y = -y - h;
     }
 
     wxCompositionMode curMode = m_graphicContext->GetCompositionMode();
@@ -123,7 +137,7 @@ void wxGTKCairoDCImpl::DoDrawText(const wxString& text, int x, int y)
 
     m_graphicContext->SetCompositionMode(curMode);
 
-    if (m_layoutDir == wxLayout_RightToLeft)
+    if (xInverted || yInverted)
         m_graphicContext->PopState();
 }
 

commit 4c375ad430e38db849ce88c7de25a71355d887f5

Fix wxDC::DrawRotatedText() with GTK3
Use m_sign[XY] to determine if axes are inverted, as the full device transformation
may have negative scale for things like text rotation. Broken by
eafa248117 (Avoid mirroring text when wxDC axes are inverted with GTK3, 2022-02-13)
See #22278

src/gtk/dc.cpp
@@ -91,13 +91,7 @@ void wxGTKCairoDCImpl::DoDrawText(const wxString& text, int x, int y)
     if (text.empty())
         return;
 
-    cairo_t* cr = static_cast<cairo_t*>(m_graphicContext->GetNativeContext());
-    if (cr == NULL)
-        return;
-
-    double dx = 1, dy = 1;
-    cairo_user_to_device_distance(cr, &dx, &dy);
-    const bool xInverted = dx < 0;
+    const bool xInverted = m_signX < 0 || m_layoutDir == wxLayout_RightToLeft;
     if (xInverted && text.find('\n') != wxString::npos)
     {
         // RTL needs each line separately to position text properly.
@@ -112,7 +106,7 @@ void wxGTKCairoDCImpl::DoDrawText(const wxString& text, int x, int y)
     CalcBoundingBox(x, y);
     CalcBoundingBox(x + w, y + h);
 
-    const bool yInverted = dy < 0;
+    const bool yInverted = m_signY < 0;
     if (xInverted || yInverted)
         m_graphicContext->PushState();
     if (xInverted)