wxWidgets/wxWidgets

commit 24970061faee1fec2f11f1d8895ccc3ea9a7563b

Add wxBitmap(wxImage, wxDC) ctor to all ports
This ctor was previously present only in wxMSW, make it available in all
ports to allow the same code to compile everywhere.

In most of them wxDC argument is simply ignored, but in wxGTK and wxOSX
it is used to assign the appropriate scale factor for the new bitmap.

Enable previously wxMSW-only unit test checking for this.

include/wx/gtk1/bitmap.h
@@ -69,6 +69,7 @@ public:
     wxBitmap( const char* const* bits );
     wxBitmap( const wxString &filename, wxBitmapType type = wxBITMAP_DEFAULT_TYPE );
     wxBitmap( const wxImage& image, int depth = -1, double WXUNUSED(scale) = 1.0 ) { (void)CreateFromImage(image, depth); }
+    wxBitmap( const wxImage& image, const wxDC& WXUNUSED(dc) ) { (void)CreateFromImage(image); }
     explicit wxBitmap(const wxCursor& cursor);
     virtual ~wxBitmap();
 

include/wx/dfb/bitmap.h
@@ -32,7 +32,10 @@ public:
     wxBitmap(const wxString &filename, wxBitmapType type = wxBITMAP_DEFAULT_TYPE);
     wxBitmap(const char* const* bits);
 #if wxUSE_IMAGE
-    wxBitmap(const wxImage& image, int depth = -1, double WXUNUSED(scale) = 1.0);
+    wxBitmap(const wxImage& image, int depth = -1, double scale = 1.0)
+        { InitFromImage(image, depth, scale); }
+    wxBitmap(const wxImage& image, const wxDC& WXUNUSED(dc))
+        { InitFromImage(image, -1, 1.0); }
 #endif
 
     bool Create(const wxIDirectFBSurfacePtr& surface);
@@ -85,6 +88,8 @@ public:
 protected:
     virtual wxGDIRefData *CreateGDIRefData() const;
     virtual wxGDIRefData *CloneGDIRefData(const wxGDIRefData *data) const;
+
+    void InitFromImage(const wxImage& image, int depth, double scale);
 
     bool CreateWithFormat(int width, int height, int dfbFormat);
 

include/wx/gtk/bitmap.h
@@ -75,6 +75,7 @@ public:
     wxBitmap( const wxString &filename, wxBitmapType type = wxBITMAP_DEFAULT_TYPE );
 #if wxUSE_IMAGE
     wxBitmap(const wxImage& image, int depth = wxBITMAP_SCREEN_DEPTH, double scale = 1.0);
+    wxBitmap(const wxImage& image, const wxDC& dc);
 #endif // wxUSE_IMAGE
     wxBitmap(GdkPixbuf* pixbuf, int depth = 0);
     explicit wxBitmap(const wxCursor& cursor);
@@ -147,11 +148,12 @@ public:
     bool HasAlpha() const;
 
 protected:
-#ifndef __WXGTK3__
 #if wxUSE_IMAGE
+    void InitFromImage(const wxImage& image, int depth, double scale);
+#ifndef __WXGTK3__
     bool CreateFromImage(const wxImage& image, int depth);
-#endif // wxUSE_IMAGE
 #endif
+#endif // wxUSE_IMAGE
 
     virtual wxGDIRefData* CreateGDIRefData() const wxOVERRIDE;
     virtual wxGDIRefData* CloneGDIRefData(const wxGDIRefData* data) const wxOVERRIDE;

include/wx/osx/bitmap.h
@@ -114,6 +114,7 @@ public:
 
     // Convert from wxImage:
     wxBitmap(const wxImage& image, int depth = -1, double scale = 1.0);
+    wxBitmap(const wxImage& image, const wxDC& dc);
 
     // Convert from wxIcon
     wxBitmap(const wxIcon& icon) { CopyFromIcon(icon); }
@@ -236,6 +237,9 @@ protected:
     virtual wxGDIRefData *CloneGDIRefData(const wxGDIRefData *data) const wxOVERRIDE;
 
     virtual bool DoCreate(const wxSize& sz, double scale, int depth) wxOVERRIDE;
+
+private:
+    void InitFromImage(const wxImage& image, int depth, double scale);
 };
 
 #endif // _WX_BITMAP_H_

include/wx/qt/bitmap.h
@@ -24,6 +24,7 @@ public:
     wxBitmap(const char* const* bits);
     wxBitmap(const wxString &filename, wxBitmapType type = wxBITMAP_TYPE_XPM);
     wxBitmap(const wxImage& image, int depth = wxBITMAP_SCREEN_DEPTH, double scale = 1.0);
+    wxBitmap(const wxImage& image, const wxDC& dc);
 
     // Convert from wxIcon / wxCursor
     wxBitmap(const wxIcon& icon) { CopyFromIcon(icon); }
@@ -76,6 +77,9 @@ public:
 protected:
     virtual wxGDIRefData *CreateGDIRefData() const wxOVERRIDE;
     virtual wxGDIRefData *CloneGDIRefData(const wxGDIRefData *data) const wxOVERRIDE;
+
+private:
+    void InitFromImage(const wxImage& image, int depth, double WXUNUSED(scale));
 
     wxDECLARE_DYNAMIC_CLASS(wxBitmap);
 };

include/wx/x11/bitmap.h
@@ -84,6 +84,7 @@ public:
 
 #if wxUSE_IMAGE
     wxBitmap( const wxImage& image, int depth = -1, double WXUNUSED(scale) = 1.0 ) { (void)CreateFromImage(image, depth); }
+    wxBitmap( const wxImage& image, const wxDC& WXUNUSED(dc) ) { (void)CreateFromImage(image); }
     wxImage ConvertToImage() const;
     bool CreateFromImage(const wxImage& image, int depth = -1);
 #endif // wxUSE_IMAGE

interface/wx/bitmap.h
@@ -368,6 +368,23 @@ public:
     wxBitmap(const wxImage& img, int depth = wxBITMAP_SCREEN_DEPTH);
 
     /**
+        Creates a bitmap compatible with the given DC from the given image.
+
+        This constructor initializes the bitmap with the data of the given
+        image, which must be valid, but inherits the scaling factor from the
+        given device context instead of simply using the default factor of 1.
+
+        @param img
+            Platform-independent wxImage object.
+        @param dc
+            DC from which the scaling factor is inherited
+
+        @since 3.1.7 (previously this constructor overload was only available
+            in wxMSW port)
+     */
+    wxBitmap(const wxImage& img, const wxDC& dc);
+
+    /**
         Creates bitmap corresponding to the given cursor.
 
         This can be useful to display a cursor as it cannot be drawn directly

src/dfb/bitmap.cpp
@@ -417,7 +417,7 @@ bool wxBitmap::CreateWithFormat(int width, int height, int dfbFormat)
 }
 
 #if wxUSE_IMAGE
-wxBitmap::wxBitmap(const wxImage& imageOrig, int depth, double WXUNUSED(scale))
+void wxBitmap::InitFromImage(const wxImage& imageOrig, int depth, double WXUNUSED(scale))
 {
     wxCHECK_RET( imageOrig.IsOk(), wxT("invalid image") );
 

commit c4dc0353cc45ec736aa2c47bf8f54e5735584edb

Fix wxGTK1 build after adding wxBitmap(wxImage, wxDC) ctor
This fixes a compilation problem introduced in 24970061fa (Add
wxBitmap(wxImage, wxDC) ctor to all ports, 2022-04-12).

include/wx/gtk1/bitmap.h
@@ -69,7 +69,7 @@ public:
     wxBitmap( const char* const* bits );
     wxBitmap( const wxString &filename, wxBitmapType type = wxBITMAP_DEFAULT_TYPE );
     wxBitmap( const wxImage& image, int depth = -1, double WXUNUSED(scale) = 1.0 ) { (void)CreateFromImage(image, depth); }
-    wxBitmap( const wxImage& image, const wxDC& WXUNUSED(dc) ) { (void)CreateFromImage(image); }
+    wxBitmap( const wxImage& image, const wxDC& WXUNUSED(dc) ) { (void)CreateFromImage(image, -1); }
     explicit wxBitmap(const wxCursor& cursor);
     virtual ~wxBitmap();
 