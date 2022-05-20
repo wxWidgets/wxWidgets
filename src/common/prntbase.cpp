4d3bb036f907150cb2954709012bfac4232f7676
Vadim Zeitlin <vadim@wxwidgets.org>
2022-05-21 00:08:11 +0100

    Use GetDefaultBorderFractional() for borders in wxPreviewFrame
    This is slightly more precise when using non-integral scaling factor
    than using GetDefaultBorder().

--- src/common/prntbase.cpp
+++ src/common/prntbase.cpp
@@ -1500,7 +1500,7 @@ public:
         {
             m_needsSeparator = false;
 
-            m_sizer->AddSpacer(2*wxSizerFlags::GetDefaultBorder());
+            m_sizer->AddSpacer(wxRound(2*wxSizerFlags::GetDefaultBorderFractional()));
         }
 
         m_hasContents = true;
@@ -1868,7 +1868,7 @@ void wxPrintPreviewBase::Init(wxPrintout *printout,
     m_currentPage = 1;
     m_currentZoom = 70;
     m_topMargin =
-    m_leftMargin = 2*wxSizerFlags::GetDefaultBorder();
+    m_leftMargin = wxRound(2*wxSizerFlags::GetDefaultBorderFractional());
     m_pageWidth = 0;
     m_pageHeight = 0;
     m_printingPrepared = false;
