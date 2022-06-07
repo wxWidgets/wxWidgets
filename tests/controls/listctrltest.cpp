1a545fe4283a8d51a8e7082c6239f87dbaa098be
Kvaz1r <atomic.alarm@yandex.ru>
2022-06-07 22:12:56 +0300

    Add tests for wxListCtrl ColumnCount

--- tests/controls/listctrltest.cpp
+++ tests/controls/listctrltest.cpp
@@ -48,10 +48,12 @@ private:
         WXUISIM_TEST( ColumnClick );
         WXUISIM_TEST( ColumnDrag );
         CPPUNIT_TEST( SubitemRect );
+        CPPUNIT_TEST( ColumnCount );
     CPPUNIT_TEST_SUITE_END();
 
     void EditLabel();
     void SubitemRect();
+    void ColumnCount();
 #if wxUSE_UIACTIONSIMULATOR
     // Column events are only supported in wxListCtrl currently so we test them
     // here rather than in ListBaseTest
@@ -137,6 +139,19 @@ void ListCtrlTestCase::SubitemRect()
     // Here we can't check for exact equality neither as there can be a margin.
     CHECK(rectLabel.GetLeft() >= rectItem.GetLeft());
     CHECK(rectLabel.GetRight() == rectItem.GetRight());
+}
+
+void ListCtrlTestCase::ColumnCount()
+{
+    CHECK(m_list->GetColumnCount() == 0);
+    m_list->InsertColumn(0, "Column 0");
+    m_list->InsertColumn(1, "Column 1");
+    CHECK(m_list->GetColumnCount() == 2);
+
+    // wxLC_LIST mode
+    tearDown();
+    m_list = new wxListCtrl(wxTheApp->GetTopWindow(), wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLC_LIST);
+    CHECK(m_list->GetColumnCount() == 1);
 }
 
 #if wxUSE_UIACTIONSIMULATOR
