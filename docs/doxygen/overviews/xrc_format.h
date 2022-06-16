4de9af15dfc225166c2f54297a9564ba81c2c48c
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-06-16 06:17:23 -0700

    Support additional wxWizard properties

--- docs/doxygen/overviews/xrc_format.h
+++ docs/doxygen/overviews/xrc_format.h
@@ -2517,6 +2517,22 @@ corresponds to the following tree of labels:
 @hdr3col{property, type, description}
 @row3col{bitmap, @ref overview_xrcformat_type_bitmap,
     Bitmap to display on the left side of the wizard (default: none).}
+@row3col{border, integer, Sets width of border around page area. (default: 0).
+    @since 3.2.0}
+@row3col{bmp_placement, @ref overview_xrcformat_type_style,
+    Sets the flags indicating how the wizard or page bitmap should be expanded
+    and positioned to fit the page height. By default, placement is 0
+    (no expansion is done). See wxWizard::SetBitmapPlacement()
+    @since 3.2.0}
+@row3col{bmp_minwidth, integer,
+    Sets the minimum width for the bitmap that will be constructed to contain
+    the actual wizard or page bitmap if a non-zero bitmap placement flag has
+    been set.
+    @since 3.2.0}
+@row3col{bmp_bg_clr, @ref overview_xrcformat_type_colour,
+    Sets the colour that should be used to fill the area not taken up by the
+    wizard or page bitmap, if a non-zero bitmap placement flag has been set.
+    @since 3.2.0}
 @endTable
 
 A wizard object can have one or more child objects of the wxWizardPage or
