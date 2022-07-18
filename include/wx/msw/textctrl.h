70c5ec6effb73330d025f8c4e9951c1ef582bfc6
Vadim Zeitlin <vadim@wxwidgets.org>
2022-07-18 20:44:02 +0100

    Move wxTextCtrl constructors out of line in wxMSW
    When inheriting from std::streambuf, as is the case by default, and
    creating wxTextCtrl in the application (as is common) and destroying it
    in the library (as is even more so, as it's typically done when the
    parent window is destroyed), having inline constructor but not inline
    destructor apparently results in std::locale, which is part of
    std::streambuf, to be allocated and destroyed using different heaps or
    something similar when using TDM-GCC libstdc++, as destroying
    std::locale inside the library corrupts the heap and results in crashes
    when the next std::locale object is destroyed -- i.e. creating, and then
    destroying, more than one wxTextCtrl results in a crash.
    
    Work around this by moving the constructors out of line, as this is
    sufficient to avoid the problem, even if it's exact causes are not
    exactly clear.
    
    This commit is best viewed with Git --color-moved option.
    
    See #22639.

--- include/wx/msw/textctrl.h
+++ include/wx/msw/textctrl.h
@@ -17,19 +17,14 @@ public:
     // creation
     // --------
 
-    wxTextCtrl() { Init(); }
+    wxTextCtrl();
     wxTextCtrl(wxWindow *parent, wxWindowID id,
                const wxString& value = wxEmptyString,
                const wxPoint& pos = wxDefaultPosition,
                const wxSize& size = wxDefaultSize,
                long style = 0,
                const wxValidator& validator = wxDefaultValidator,
-               const wxString& name = wxASCII_STR(wxTextCtrlNameStr))
-    {
-        Init();
-
-        Create(parent, id, value, pos, size, style, validator, name);
-    }
+               const wxString& name = wxASCII_STR(wxTextCtrlNameStr));
     virtual ~wxTextCtrl();
 
     bool Create(wxWindow *parent, wxWindowID id,
