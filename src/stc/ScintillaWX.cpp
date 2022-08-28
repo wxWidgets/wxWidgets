929f8b00f0ce8e861b62ebf0f7b32663b04342d7
Maarten Bent <MaartenBent@users.noreply.github.com>
2022-08-28 19:33:34 +0200

    Fix appearance of STC call tips in high DPI
    Use correct DPI scale for the bitmap used as the backing store.
    
    See #22734.
    
    Closes #22747.

--- src/stc/ScintillaWX.cpp
+++ src/stc/ScintillaWX.cpp
@@ -115,7 +115,8 @@ public:
 
     void DrawBack(const wxSize& size)
     {
-        m_back = wxBitmap(size);
+        m_back = wxBitmap(ToPhys(size));
+        m_back.SetScaleFactor(GetDPIScaleFactor());
         wxMemoryDC mem(m_back);
         Surface* surfaceWindow = Surface::Allocate(m_swx->technology);
         surfaceWindow->Init(&mem, m_ct->wDraw.GetID());
