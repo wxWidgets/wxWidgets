549be66c7da696d3605cf4ed784458fb1d477a4a
Vadim Zeitlin <vadim@wxwidgets.org>
2022-10-09 16:15:57 +0200

    Avoid warnings about deprecated functions inside glib.h
    This happens when using gcc 4.8 under Ubuntu 18.04 and has to be
    suppressed in the source file including the header as just doing it in
    the header itself, with the usual wxGCC_WARNING_SUPPRESS/RESTORE macros,
    has no effect, i.e. the warning is still given even if we bracket glib.h
    inclusion with them.

--- src/gtk/webview_webkit2.cpp
+++ src/gtk/webview_webkit2.cpp
@@ -11,6 +11,13 @@
 
 #if wxUSE_WEBVIEW && wxUSE_WEBVIEW_WEBKIT2
 
+// In Ubuntu 18.04 compiling glib.h with gcc 4.8 results in the warnings inside
+// it, and disabling them temporarily using wxGCC_WARNING_SUPPRESS/RESTORE
+// doesn't work (i.e. they're still given), so disable them globally here.
+#if wxCHECK_GCC_VERSION(4, 8) && !wxCHECK_GCC_VERSION(4, 9)
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 #include "wx/dir.h"
 #include "wx/dynlib.h"
 #include "wx/filename.h"
