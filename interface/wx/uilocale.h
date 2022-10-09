a1c42b1cebc380f569d4e42c23b6a60d9362d6ec
Vadim Zeitlin <vadim@wxwidgets.org>
2022-10-09 23:56:40 +0200

    Fix copy-paste errors in wxUILocale documentation
    Simply remove the parts that made sense in wxLocale context, but not
    here.

--- interface/wx/uilocale.h
+++ interface/wx/uilocale.h
@@ -218,7 +218,6 @@ public:
 
     /**
         Adds custom, user-defined language to the database of known languages.
-        This database is used in conjunction with the first form of Init().
     */
     static void AddLanguage(const wxLanguageInfo& info);
 
@@ -271,8 +270,8 @@ public:
     static wxString GetLanguageName(int lang);
 
     /**
-        Returns canonical name (see GetCanonicalName()) of the given language
-        or empty string if this language is unknown.
+        Returns canonical name of the given language or empty string if this
+        language is unknown.
 
         See GetLanguageInfo() for a remark about special meaning of @c wxLANGUAGE_DEFAULT.
     */
