e18a0ea49d3fe17c1ff8cac3f11881b9f60ba715
AliKet <AliDev2007@yahoo.fr>
2022-10-19 10:48:15 +0100

    Simplify wxGrid::SetCornerLabelValue() impl.
    Calling Refresh() with no explicit rectangle will refresh
    the entire window anyhow.

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -9122,8 +9122,7 @@ void wxGrid::SetCornerLabelValue( const wxString& s )
         m_table->SetCornerLabelValue( s );
         if ( ShouldRefresh() )
         {
-            wxRect rect = m_cornerLabelWin->GetRect();
-            m_cornerLabelWin->Refresh(true, &rect);
+            m_cornerLabelWin->Refresh();
         }
     }
 }
