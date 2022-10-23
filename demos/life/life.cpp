6a00104cbd1406e317dade547932f1c5f3066e51
Vadim Zeitlin <vadim@wxwidgets.org>
2022-10-23 16:31:27 +0200

    Use wxHAS_IMAGES_IN_RESOURCES instead of checking all platforms
    No real changes, just simplify the preprocessor check.

--- demos/life/life.cpp
+++ demos/life/life.cpp
@@ -35,7 +35,7 @@
 // resources
 // --------------------------------------------------------------------------
 
-#if defined(__WXGTK__) || defined(__WXMAC__) || defined(__WXMGL__) || defined(__WXX11__) || defined(__WXQT__)
+#ifndef wxHAS_IMAGES_IN_RESOURCES
     // application icon
     #include "mondrian.xpm"
 
