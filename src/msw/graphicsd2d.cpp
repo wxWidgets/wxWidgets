c87970b29de1bd4c296988137bd2ca9e86182e63
Artur Wieczorek <7330332+a-wi@users.noreply.github.com>
2022-10-25 10:57:06 +0200

    Fix using wxGCDC (Direct2D) with scrolled wxDC
    Because since 4f9186f (Increase usable scrolling range in wxMSW by a
    factor of 10,000, 2022-04-30) the shift of wxDC origin is not passed
    through HDC so we need to apply it as an internal transform of
    ID2D1RenderTarget. This transform is for internal purposes only and is
    not exposed with GetTransform().
    
    See #22480.
    See #22914.

--- src/msw/graphicsd2d.cpp
+++ src/msw/graphicsd2d.cpp
@@ -4047,7 +4047,9 @@ private:
     wxStack<LayerData> m_layers;
     ID2D1RenderTarget* m_cachedRenderTarget;
     wxCOMPtr<ID2D1GdiInteropRenderTarget> m_gdiRenderTarget;
+    D2D1::Matrix3x2F m_inheritedTransform;
     D2D1::Matrix3x2F m_initTransform;
+    D2D1::Matrix3x2F m_initTransformInv;
     // Clipping box
     bool m_isClipBoxValid;
     double m_clipX1, m_clipY1, m_clipX2, m_clipY2;
@@ -4066,10 +4068,11 @@ wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer,
                            wxWindow* window) :
     wxGraphicsContext(renderer, window), m_direct2dFactory(direct2dFactory),
 #if wxD2D_DEVICE_CONTEXT_SUPPORTED
-    m_renderTargetHolder(new wxD2DDeviceContextResourceHolder(direct2dFactory, hwnd))
+    m_renderTargetHolder(new wxD2DDeviceContextResourceHolder(direct2dFactory, hwnd)),
 #else
-    m_renderTargetHolder(new wxD2DHwndRenderTargetResourceHolder(hwnd, direct2dFactory))
+    m_renderTargetHolder(new wxD2DHwndRenderTargetResourceHolder(hwnd, direct2dFactory)),
 #endif
+    m_inheritedTransform(D2D1::Matrix3x2F::Identity())
 {
     RECT r = wxGetWindowRect(hwnd);
     m_width = r.right - r.left;
@@ -4090,6 +4093,24 @@ wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer,
         const wxSize dcSize = dc->GetSize();
         m_width = dcSize.GetWidth();
         m_height = dcSize.GetHeight();
+
+        // We don't set HDC origin at MSW level in wxDC because this limits it to
+        // 2^27 range and we prefer to handle it ourselves to allow using the full
+        // 2^32 range of int coordinates, but we need to let D2D know about the
+        // origin shift by storing it as an internal transformation
+        // (which is not going to be exposed).
+        double sx, sy;
+        dc->GetUserScale(&sx, &sy);
+        double lsx, lsy;
+        dc->GetLogicalScale(&lsx, &lsy);
+        sx *= lsx;
+        sy *= lsy;
+        wxPoint org = dc->GetDeviceOrigin();
+        m_inheritedTransform = D2D1::Matrix3x2F::Translation(org.x / sx, org.y / sy);
+    }
+    else
+    {
+        m_inheritedTransform = D2D1::Matrix3x2F::Identity();
     }
 
     Init();
@@ -4098,7 +4119,8 @@ wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer,
 #if wxUSE_IMAGE
 wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer, ID2D1Factory* direct2dFactory, wxImage& image) :
     wxGraphicsContext(renderer), m_direct2dFactory(direct2dFactory),
-    m_renderTargetHolder(new wxD2DImageRenderTargetResourceHolder(&image, direct2dFactory))
+    m_renderTargetHolder(new wxD2DImageRenderTargetResourceHolder(&image, direct2dFactory)),
+    m_inheritedTransform(D2D1::Matrix3x2F::Identity())
 {
     m_width = image.GetWidth();
     m_height = image.GetHeight();
@@ -4107,7 +4129,8 @@ wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer, ID2D1Factory* direct2dF
 #endif // wxUSE_IMAGE
 
 wxD2DContext::wxD2DContext(wxGraphicsRenderer* renderer, ID2D1Factory* direct2dFactory, void* nativeContext) :
-    wxGraphicsContext(renderer), m_direct2dFactory(direct2dFactory)
+    wxGraphicsContext(renderer), m_direct2dFactory(direct2dFactory),
+    m_inheritedTransform(D2D1::Matrix3x2F::Identity())
 {
     m_renderTargetHolder = *((wxSharedPtr<wxD2DRenderTargetResourceHolder>*)nativeContext);
     m_width = 0;
@@ -4583,13 +4606,11 @@ wxGraphicsMatrix wxD2DContext::GetTransform() const
     {
         GetRenderTarget()->GetTransform(&transformMatrix);
 
-        if ( m_initTransform.IsInvertible() )
+        // Don't expose internal transformations.
+        if ( !m_initTransformInv.IsIdentity() )
         {
-            D2D1::Matrix3x2F invMatrix = m_initTransform;
-            invMatrix.Invert();
-
             D2D1::Matrix3x2F m;
-            m.SetProduct(transformMatrix, invMatrix);
+            m.SetProduct(transformMatrix, m_initTransformInv);
             transformMatrix = m;
         }
     }
@@ -4768,6 +4789,14 @@ void wxD2DContext::EnsureInitialized()
     {
         m_cachedRenderTarget = m_renderTargetHolder->GetD2DResource();
         GetRenderTarget()->GetTransform(&m_initTransform);
+        m_initTransform = m_initTransform * m_inheritedTransform;
+        wxASSERT(m_initTransform.IsInvertible());
+        m_initTransformInv = m_initTransform;
+        if ( !m_initTransformInv.Invert() )
+        {
+            m_initTransformInv = D2D1::Matrix3x2F::Identity();
+        }
+        GetRenderTarget()->SetTransform(&m_initTransform);
         GetRenderTarget()->SetAntialiasMode(D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
         GetRenderTarget()->SetTextAntialiasMode(D2D1_TEXT_ANTIALIAS_MODE_DEFAULT);
         GetRenderTarget()->BeginDraw();
