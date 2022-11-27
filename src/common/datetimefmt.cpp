afc6cfb88ea12c93cd9d6d1765aecb325ee311ae
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2022-11-27 08:53:23 -0500

    Fix whitespace

--- src/common/datetimefmt.cpp
+++ src/common/datetimefmt.cpp
@@ -1752,7 +1752,7 @@ wxDateTime::ParseDateTime(const wxString& date, wxString::const_iterator *end)
         // Skip spaces, as the ParseTime() function fails on spaces
         while ( endDate != date.end() && wxIsspace(*endDate) )
             ++endDate;
-        
+
         // Skip possible 'T' separator in front of time component
         if ( endDate != date.end() && *endDate == 'T' )
             ++endDate;
