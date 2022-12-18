2cc6d029d0406822f2691db1781c023417be4852
Artur Wieczorek <7330332+a-wi@users.noreply.github.com>
2022-12-18 20:38:29 +0100

    Enable wxPaintDC test under wxGTK3
    Test should pass now.

--- tests/graphics/clippingbox.cpp
+++ tests/graphics/clippingbox.cpp
@@ -4155,11 +4155,7 @@ TEST_CASE("ClippingBoxTestCase::wxPaintDC", "[clip][dc][paintdc]")
 
         SECTION("OneDevRegionAndReset Transform Matrix")
         {
-#ifdef __WXGTK3__
-            WARN("Skipping test known to fail in wxGTK 3");
-#else
             OneDevRegionAndReset(dc, bmp, true, true, wxPoint());
-#endif
         }
 
         SECTION("OneDevRegionAndEmpty")
