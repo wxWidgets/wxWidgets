c613ff79beee4bfab4b483ad8cdf878e0488ade7
Tobias Taschner <TcT2k@users.noreply.github.com>
2023-01-16 12:12:13 +0100

    Fix webview edge on Windows 7
    Disable double buffering on the edge host window. With double buffering
    enabled rendering fails on Windows 7 (possibly Win8). It shouln't
    affect performance as the contents are rendered and hosted by the
    external edge process anyway.

--- src/msw/webview_edge.cpp
+++ src/msw/webview_edge.cpp
@@ -795,6 +795,8 @@ bool wxWebViewEdge::Create(wxWindow* parent,
         return false;
     }
 
+    MSWDisableComposited();
+
     if (!m_impl->Create())
         return false;
     Bind(wxEVT_SIZE, &wxWebViewEdge::OnSize, this);
