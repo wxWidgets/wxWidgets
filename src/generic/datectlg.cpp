725c9a0bc6b5b31860fdc53ce7a0cd8e541236e5
Vadim Zeitlin <vadim@wxwidgets.org>
2023-03-05 01:45:58 +0100

    Use better behaving custom validator in wxDatePickerCtrlGeneric
    Using wxTextValidator with wxFILTER_INCLUDE_CHAR_LIST was both too
    restrictive, as it prevented the user from entering any letters, even if
    the locale format required using them for months (as e.g. nb-NO does)
    and too lax, as it allowed entering any invalid date as long as it
    contained only valid characters.
    
    Fix both of these problems by using a custom validator actually using
    wxDateTime::ParseFormat() for validation instead of just checking that
    the string consists of valid characters (and doing it wrongly).
    
    This validator could be more generally useful, and so could be extracted
    into a public class later, but for now use it only here.
    
    Closes #23310.

--- src/generic/datectlg.cpp
+++ src/generic/datectlg.cpp
@@ -25,9 +25,9 @@
     #include "wx/dialog.h"
     #include "wx/dcmemory.h"
     #include "wx/intl.h"
+    #include "wx/msgdlg.h"
     #include "wx/panel.h"
     #include "wx/textctrl.h"
-    #include "wx/valtext.h"
 #endif
 
 #include "wx/calctrl.h"
@@ -50,6 +50,65 @@
 // ----------------------------------------------------------------------------
 // local classes
 // ----------------------------------------------------------------------------
+
+namespace
+{
+
+class DateValidator : public wxValidator
+{
+public:
+    explicit DateValidator(const wxString& format) : m_format(format) {}
+    DateValidator(const DateValidator& val) = default;
+
+    virtual wxObject *Clone() const override
+    {
+        return new DateValidator(*this);
+    }
+
+    virtual bool Validate(wxWindow *parent) override
+    {
+        // We can only be used with wxComboCtrl, so just a static_cast<> would
+        // be safe, but use checked cast to notice any problems in debug build.
+        const wxString
+            s = wxStaticCast(m_validatorWindow, wxComboCtrl)->GetValue();
+        if ( s.empty() )
+        {
+            // There is no need to tell the user that an empty string is
+            // invalid, this shouldn't be a surprise for them.
+            return true;
+        }
+
+        wxDateTime dt;
+        if ( !dt.ParseFormat(s, m_format) )
+        {
+            wxMessageBox
+            (
+                wxString::Format
+                (
+                    _("\"%s\" is not in the expected date format, "
+                      "please enter it as e.g. \"%s\"."),
+                    s, wxDateTime::Today().Format(m_format)
+                ),
+                _("Invalid date"),
+                wxOK | wxICON_WARNING,
+                parent
+            );
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // We don't transfer any data, this validator is used only for validation.
+    virtual bool TransferFromWindow() override { return true; }
+    virtual bool TransferToWindow() override { return true; }
+
+private:
+    const wxString m_format;
+};
+
+} // anonymous namespace
 
 class wxCalendarComboPopup : public wxCalendarCtrl,
                              public wxComboPopup
@@ -259,20 +318,7 @@ private:
         if ( m_combo )
         {
 #if wxUSE_VALIDATORS
-            wxString allowedChars = wxS("0123456789");
-
-            const wxChar *p2 = m_format.c_str();
-            while ( *p2 )
-            {
-                if ( *p2 == '%')
-                    p2 += 2;
-                else
-                    allowedChars << (*p2++); // append char
-            }
-
-            wxTextValidator tv(wxFILTER_INCLUDE_CHAR_LIST);
-            tv.SetCharIncludes(allowedChars);
-            m_combo->SetValidator(tv);
+            m_combo->SetValidator(DateValidator(m_format));
 #endif // wxUSE_VALIDATORS
 
             if ( GetDate().IsValid() )
