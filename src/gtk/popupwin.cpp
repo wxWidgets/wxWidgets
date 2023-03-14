57a7b220d46ad3096a3a13c3b6e35dd51580c3c0
Carlos Azevedo <carlosazevedonachina@gmail.com>
2023-03-14 19:21:59 +0000

    Fix on wxPopupWindow to allow full use of focusable controls on GTK+
    Presently on GTK+ a POPUP_WINDOW is created when instantiating the
    popup; but having focusable controls on it is not supported by GTK+,
    as evidenced by the sample.
    On the sample the text control on it can be modified by keyboard
    input but will not show a caret while being modified, due to a GTK
    limitation.
    This fix instead creates a TOPLEVEL_WINDOW and hides it from the
    pager and window bar, effectively allowing the focus to now be
    represented with a caret on text controls.
    Since the MSW port already had a style to support controls on the
    popup this fix uses the same on GTK+, with the same meaning.
    During testing it was shown that a TAB press would force the popup
    to center on the screen so code was added to reposition the popup
    when needed.

--- src/gtk/popupwin.cpp
+++ src/gtk/popupwin.cpp
@@ -74,6 +74,22 @@ bool gtk_dialog_delete_callback( GtkWidget *WXUNUSED(widget), GdkEvent *WXUNUSED
 }
 
 //-----------------------------------------------------------------------------
+// "focus-out-event"
+//-----------------------------------------------------------------------------
+
+extern "C" {
+static
+gboolean on_popup_focus_out( GtkWidget *, GdkEventFocus *, wxPopupWindow* win )
+{
+    wxFocusEvent evt( wxEVT_KILL_FOCUS, win->GetId() );
+    evt.SetEventObject( win );
+
+    (void)win->HandleWindowEvent( evt );
+    return TRUE;
+}
+}
+
+//-----------------------------------------------------------------------------
 // wxPopupWindow
 //-----------------------------------------------------------------------------
 
@@ -102,40 +118,77 @@ bool wxPopupWindow::Create( wxWindow *parent, int style )
     // All dialogs should really have this style
     m_windowStyle |= wxTAB_TRAVERSAL;
 
-    m_widget = gtk_window_new( GTK_WINDOW_POPUP );
-    g_object_ref( m_widget );
-
-    gtk_widget_set_name( m_widget, "wxPopupWindow" );
-    // wxPopupWindow is used for different windows as well
-    // gtk_window_set_type_hint( GTK_WINDOW(m_widget), GDK_WINDOW_TYPE_HINT_COMBO );
-
-    // Popup windows can be created without parent, so handle this correctly.
-    if (parent)
+    if ( HasFlag(wxPU_CONTAINS_CONTROLS) )
     {
-        GtkWidget *toplevel = gtk_widget_get_toplevel( parent->m_widget );
-        if (GTK_IS_WINDOW (toplevel))
-            gtk_window_set_transient_for (GTK_WINDOW (m_widget), GTK_WINDOW (toplevel));
+        // On GTK+ popup windows (GTK_WINDOW_POPUP) are not supposed to have controls.
+        // A top level window must be used instead. Hints are use to prevent it being
+        // shown as a separate window on the taskbar or pager.
+        m_widget = gtk_window_new( GTK_WINDOW_TOPLEVEL );
+        gtk_window_set_title( GTK_WINDOW (m_widget), "wxPopUpWindow" );
+        gtk_window_set_resizable( GTK_WINDOW (m_widget), FALSE );
+        gtk_window_set_decorated( GTK_WINDOW (m_widget), FALSE );
+        gtk_window_set_skip_taskbar_hint( GTK_WINDOW (m_widget), TRUE );
+        gtk_window_set_skip_pager_hint( GTK_WINDOW (m_widget), TRUE );
+
+        // Popup windows can be created without parent, so handle this correctly.
+        if (parent)
+        {
+            GtkWidget *toplevel = gtk_widget_get_toplevel( parent->m_widget );
+            if (GTK_IS_WINDOW (toplevel))
+                gtk_window_set_transient_for (GTK_WINDOW (m_widget), GTK_WINDOW (toplevel));
+        }
+
+        gtk_widget_set_events( m_widget, GDK_FOCUS_CHANGE_MASK );
+        g_signal_connect( G_OBJECT (m_widget), "focus-out-event",
+                          G_CALLBACK (on_popup_focus_out), this );
+        g_signal_connect( m_widget, "delete_event",
+                          G_CALLBACK (gtk_dialog_delete_callback), this);
+
+        m_wxwindow = wxPizza::New();
+        gtk_widget_show( m_wxwindow );
+
+        gtk_container_add( GTK_CONTAINER(m_widget), m_wxwindow );
+
+        if (m_parent) m_parent->AddChild( this );
+
+        PostCreation();
     }
+    else
+    {
+        m_widget = gtk_window_new( GTK_WINDOW_POPUP );
+        g_object_ref( m_widget );
 
-    gtk_window_set_resizable (GTK_WINDOW (m_widget), FALSE);
+        gtk_widget_set_name( m_widget, "wxPopupWindow" );
+        // wxPopupWindow is used for different windows as well
+        // gtk_window_set_type_hint( GTK_WINDOW(m_widget), GDK_WINDOW_TYPE_HINT_COMBO );
 
-    g_signal_connect (m_widget, "delete_event",
-                      G_CALLBACK (gtk_dialog_delete_callback), this);
+        // Popup windows can be created without parent, so handle this correctly.
+        if (parent)
+        {
+            GtkWidget *toplevel = gtk_widget_get_toplevel( parent->m_widget );
+            if (GTK_IS_WINDOW (toplevel))
+                gtk_window_set_transient_for (GTK_WINDOW (m_widget), GTK_WINDOW (toplevel));
+        }
 
-    m_wxwindow = wxPizza::New();
-    gtk_widget_show( m_wxwindow );
+        gtk_window_set_resizable (GTK_WINDOW (m_widget), FALSE);
 
-    gtk_container_add( GTK_CONTAINER(m_widget), m_wxwindow );
+        g_signal_connect (m_widget, "delete_event",
+                         G_CALLBACK (gtk_dialog_delete_callback), this);
 
-    if (m_parent) m_parent->AddChild( this );
+        m_wxwindow = wxPizza::New();
+        gtk_widget_show( m_wxwindow );
 
-    PostCreation();
+        gtk_container_add( GTK_CONTAINER(m_widget), m_wxwindow );
 
-    m_time = gtk_get_current_event_time();
+        if (m_parent) m_parent->AddChild( this );
 
-    g_signal_connect (m_widget, "button_press_event",
-                      G_CALLBACK (gtk_popup_button_press), this);
+        PostCreation();
 
+        m_time = gtk_get_current_event_time();
+
+        g_signal_connect (m_widget, "button_press_event",
+                         G_CALLBACK (gtk_popup_button_press), this);
+    }
     return true;
 }
 
@@ -166,7 +219,11 @@ void wxPopupWindow::DoSetSize( int x, int y, int width, int height, int sizeFlag
 
     ConstrainSize();
 
-    if (m_x != old_x || m_y != old_y)
+    // If the popup contains controls always set the position,
+    // as it may become centered when the TAB key is pressed
+    // while the popup has focus. That creates a dissonance
+    // between the actual popup position and the stored position.
+    if (m_x != old_x || m_y != old_y || HasFlag(wxPU_CONTAINS_CONTROLS))
     {
         gtk_window_move(GTK_WINDOW(m_widget), m_x, m_y);
         wxMoveEvent event(wxPoint(m_x, m_y), GetId());
@@ -174,7 +231,8 @@ void wxPopupWindow::DoSetSize( int x, int y, int width, int height, int sizeFlag
         HandleWindowEvent(event);
     }
 
-    if ((m_width != old_width) || (m_height != old_height))
+    // See above the reason for the flag forcing the update
+    if ((m_width != old_width) || (m_height != old_height) || HasFlag(wxPU_CONTAINS_CONTROLS))
     {
         // gtk_window_resize does not work for GTK_WINDOW_POPUP
         gtk_widget_set_size_request( m_widget, m_width, m_height );
