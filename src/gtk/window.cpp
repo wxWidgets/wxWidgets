9ce295f42b8c18468e0204acf05c847303305064
Vadim Zeitlin <vadim@wxwidgets.org>
2023-04-08 16:26:53 +0200

    Improve code in gtk_window_key_press_callback()
    Don't use isalpha() in it as this was potentially incorrect, both
    because the key code could be not in the supported range and because
    isalpha() might return unexpected result in non C locale, and also
    inconsistent with AdjustCharEventKeyCodes().
    
    Just check that the code is in a..z or A..Z range explicitly instead.
    
    See #23379.
    
    (cherry picked from commit 4799785b6dfc2d4490598b447c99435105b8ebba)

--- src/gtk/window.cpp
+++ src/gtk/window.cpp
@@ -1430,9 +1430,12 @@ gtk_window_key_press_callback( GtkWidget *WXUNUSED(widget),
         {
             wxKeyEvent eventChar(wxEVT_CHAR, event);
 
-            if ( event.ControlDown() && isalpha(event.m_keyCode) )
+            // Check for the special case of Ctrl+letter, see comment before
+            // AdjustCharEventKeyCodes().
+            if ( event.ControlDown() &&
+                    ((event.m_keyCode >= 'a' && event.m_keyCode <= 'z') ||
+                     (event.m_keyCode >= 'A' && event.m_keyCode <= 'Z')) )
             {
-                // Ctrl+letter is handled specially by AdjustCharEventKeyCodes().
                 eventChar.m_keyCode = event.m_keyCode;
 #if wxUSE_UNICODE
                 eventChar.m_uniChar = event.m_uniChar;
