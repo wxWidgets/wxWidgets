a1baeda9a2e8842035b7e40909139324d4cdd371
Vadim Zeitlin <vadim@wxwidgets.org>
2023-04-12 16:21:14 +0200

    Use std::unordered_map<> for wxX11 global window maps
    Replace wx hash map with the standard container.

--- include/wx/x11/private.h
+++ include/wx/x11/private.h
@@ -12,7 +12,6 @@
 #define _WX_PRIVATE_H_
 
 #include "wx/defs.h"
-#include "wx/hashmap.h"
 #include "wx/utils.h"
 #if defined( __cplusplus ) && defined( __VMS )
 #pragma message disable nosimpint
@@ -31,6 +30,8 @@
 #include <pango/pango.h>
 #endif
 
+#include <unordered_map>
+
 class WXDLLIMPEXP_FWD_CORE wxMouseEvent;
 class WXDLLIMPEXP_FWD_CORE wxKeyEvent;
 class WXDLLIMPEXP_FWD_CORE wxWindow;
@@ -40,7 +41,7 @@ class WXDLLIMPEXP_FWD_CORE wxWindow;
 // corresponding to the window for this widget
 // ----------------------------------------------------------------------------
 
-WX_DECLARE_HASH_MAP(Window, wxWindow *, wxIntegerHash, wxIntegerEqual, wxWindowHash);
+using wxWindowHash = std::unordered_map<Window, wxWindow*>;
 
 // these hashes are defined in app.cpp
 extern wxWindowHash *wxWidgetHashTable;
