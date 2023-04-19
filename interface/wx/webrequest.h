5dd800d5c631aeadfaa04aecbb20dcea5e277870
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2023-04-19 08:33:12 -0400

    Update interface/wx/webrequest.h
    Co-authored-by: VZ <vz-github@zeitlins.org>

--- interface/wx/webrequest.h
+++ interface/wx/webrequest.h
@@ -598,7 +598,8 @@ public:
     /**
         Returns the content type of the response (if available).
 
-        This can be a value such as "text/html; charset=utf-8".
+        This is the full value of the "Content-Type" header of the response,
+        e.g. a value such as "text/html; charset=utf-8".
 
         @since 3.3.0
     */
