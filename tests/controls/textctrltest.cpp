fd0a52250d8d2dcfc75d16cc0b3ab972ec549c1f
Vadim Zeitlin <vadim@wxwidgets.org>
2023-04-23 17:30:16 +0100

    Remove redundant wxUSE_STD_IOSTREAM test
    It's not necessary to check for it as wxHAS_TEXT_WINDOW_STREAM is only
    set to 1 if standard streams are used.

--- tests/controls/textctrltest.cpp
+++ tests/controls/textctrltest.cpp
@@ -335,7 +335,7 @@ void TextCtrlTestCase::StreamInput()
 
 void TextCtrlTestCase::Redirector()
 {
-#if wxHAS_TEXT_WINDOW_STREAM && wxUSE_STD_IOSTREAM
+#if wxHAS_TEXT_WINDOW_STREAM
 
     wxStreamToTextRedirector redirect(m_text);
 
