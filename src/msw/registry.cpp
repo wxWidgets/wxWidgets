wxWidgets/wxWidgets

commit 52e5561ca539da3ce467996c7bb9b038229acb98

Fix wxRegKey compilation in UTF-8 build
Use a macro to return a valid pointer to a wide string even in UTF-8
build, where we don't have a permanent wchar_t buffer to return a
pointer to.

src/msw/registry.cpp
@@ -108,10 +108,13 @@ GetMSWAccessFlags(wxRegKey::AccessMode mode, wxRegKey::WOW64ViewMode viewMode);
 static wxString GetFullName(const wxRegKey *pKey);
 static wxString GetFullName(const wxRegKey *pKey, const wxString& szValue);
 
-// returns "value" argument of wxRegKey methods converted into a value that can
-// be passed to win32 registry functions; specifically, converts empty string
-// to nullptr
-static inline const wxChar *RegValueStr(const wxString& szValue);
+// Returns a (wide char) pointer to the string contents or null for an empty
+// string.
+//
+// Unfortunately this needs to be a macro to ensure that the temporary buffer
+// returned by t_str() in UTF-8 build lives long enough.
+#define RegValueStr(szValue) \
+    ((szValue).empty() ? nullptr : static_cast<const wchar_t*>(szValue.t_str()))
 
 // Return the user-readable name of the given REG_XXX type constant.
 static wxString GetTypeString(DWORD dwType)
@@ -1587,11 +1590,6 @@ inline void RemoveTrailingSeparator(wxString& str)
 {
   if ( !str.empty() && str.Last() == REG_SEPARATOR )
     str.Truncate(str.Len() - 1);
-}
-
-inline const wxChar *RegValueStr(const wxString& szValue)
-{
-    return szValue.empty() ? nullptr : szValue.t_str();
 }
 
 #endif // wxUSE_REGKEY

commit 5c239e13f1cd7d6730cb750d88df315db73ca92d

Fix wxRegKey compilation in ANSI build too
Fixing it in UTF-8 build in 52e5561ca5 (Fix wxRegKey compilation in
UTF-8 build, 2023-03-24) broke it in ANSI build, fix it too now by using
wxChar instead of always using wchar_t.

src/msw/registry.cpp
@@ -114,7 +114,7 @@ static wxString GetFullName(const wxRegKey *pKey, const wxString& szValue);
 // Unfortunately this needs to be a macro to ensure that the temporary buffer
 // returned by t_str() in UTF-8 build lives long enough.
 #define RegValueStr(szValue) \
-    ((szValue).empty() ? NULL : static_cast<const wchar_t*>(szValue.t_str()))
+    ((szValue).empty() ? NULL : static_cast<const wxChar*>(szValue.t_str()))
 
 // Return the user-readable name of the given REG_XXX type constant.
 static wxString GetTypeString(DWORD dwType)