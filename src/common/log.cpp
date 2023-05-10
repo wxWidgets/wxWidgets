d666d1e22247ceb49962c18e0997321596b806d6
Antti Nietosvaara <antti.nietosvaara@gmail.com>
2023-05-10 15:13:08 +0300

    Fix a memory leak when using non-wx threads
    The wxThreadSpecificInfo object created by wxThreadSpecificInfo::Get()
    was only released by the wxThreadInternal's cleanup, which meant that it
    was leaked if wxThreadSpecificInfo::Get() was called by a thread not
    created by wxThread, e.g. when using std::async or std::thread.
    
    Fix this by simply using C++11 thread_local and removing the entire
    wxThreadSpecificInfo machinery entirely.
    
    See #23535.
    
    Closes #23543.

--- src/common/log.cpp
+++ src/common/log.cpp
@@ -38,7 +38,6 @@
 #include "wx/msgout.h"
 #include "wx/textfile.h"
 #include "wx/thread.h"
-#include "wx/private/threadinfo.h"
 #include "wx/crt.h"
 #include "wx/vector.h"
 
@@ -98,6 +97,10 @@ WX_DEFINE_LOG_CS(TraceMask);
 
 // and this one is used for GetComponentLevels()
 WX_DEFINE_LOG_CS(Levels);
+
+thread_local wxLog* wxPerThreadLogger = nullptr;
+
+thread_local bool wxPerThreadLoggingDisabled = false;
 
 } // anonymous namespace
 
@@ -388,7 +391,7 @@ wxLog::OnLog(wxLogLevel level,
 #if wxUSE_THREADS
     if ( !wxThread::IsMain() )
     {
-        logger = wxThreadInfo.logger;
+        logger = wxPerThreadLogger;
         if ( !logger )
         {
             if ( ms_pLogger )
@@ -506,7 +509,7 @@ wxLog *wxLog::GetActiveTarget()
     if ( !wxThread::IsMain() )
     {
         // check if we have a thread-specific log target
-        wxLog * const logger = wxThreadInfo.logger;
+        wxLog * const logger = wxPerThreadLogger;
 
         // the code below should be only executed for the main thread as
         // CreateLogTarget() is not meant for auto-creating log targets for
@@ -563,11 +566,11 @@ wxLog *wxLog::SetThreadActiveTarget(wxLog *logger)
 {
     wxASSERT_MSG( !wxThread::IsMain(), "use SetActiveTarget() for main thread" );
 
-    wxLog * const oldLogger = wxThreadInfo.logger;
+    wxLog * const oldLogger = wxPerThreadLogger;
     if ( oldLogger )
         oldLogger->Flush();
 
-    wxThreadInfo.logger = logger;
+    wxPerThreadLogger = logger;
 
     return oldLogger;
 }
@@ -779,14 +782,14 @@ void wxLog::FlushThreadMessages()
 /* static */
 bool wxLog::IsThreadLoggingEnabled()
 {
-    return !wxThreadInfo.loggingDisabled;
+    return !wxPerThreadLoggingDisabled;
 }
 
 /* static */
 bool wxLog::EnableThreadLogging(bool enable)
 {
-    const bool wasEnabled = !wxThreadInfo.loggingDisabled;
-    wxThreadInfo.loggingDisabled = !enable;
+    const bool wasEnabled = !wxPerThreadLoggingDisabled;
+    wxPerThreadLoggingDisabled = !enable;
     return wasEnabled;
 }
 
