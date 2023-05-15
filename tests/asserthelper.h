5b9b817aaad551012ca488eee00aeba4567473b5
Vadim Zeitlin <vadim@wxwidgets.org>
2023-05-15 20:42:34 +0100

    Simplify showing colour values in failing unit tests
    Use wxColour::GetAsString() instead of reimplementing it in the test.

--- tests/asserthelper.h
+++ tests/asserthelper.h
@@ -14,21 +14,6 @@
 #include "wx/gdicmn.h"
 #include "wx/font.h"
 
-namespace wxTestPrivate
-{
-    // by default colour components values are output incorrectly because they
-    // are unsigned chars, define a small helper struct which formats them in
-    // a more useful way
-    struct ColourChannel
-    {
-        ColourChannel(unsigned char value) : m_value(value) { }
-
-        unsigned char m_value;
-    };
-
-    std::ostream& operator<<(std::ostream& os, const ColourChannel& cc);
-} // wxTestPrivate namespace
-
 // Operators used to show the values of the corresponding types when comparing
 // them in the unit tests fails.
 std::ostream& operator<<(std::ostream& os, const wxColour& c);
