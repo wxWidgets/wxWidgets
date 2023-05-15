d1fedc141556f88785d31786cbc700adec3c842a
Vadim Zeitlin <vadim@wxwidgets.org>
2023-05-15 20:25:40 +0100

    Get rid of CppUnit boilerplate in wxHyperlinkCtrl unit test
    No real changes, just avoid using compatibility macros in this test.

--- tests/controls/hyperlinkctrltest.cpp
+++ tests/controls/hyperlinkctrltest.cpp
@@ -20,76 +20,61 @@
 #include "testableframe.h"
 #include "asserthelper.h"
 
-class HyperlinkCtrlTestCase : public CppUnit::TestCase
+class HyperlinkCtrlTestCase
 {
 public:
-    HyperlinkCtrlTestCase() { }
+    HyperlinkCtrlTestCase()
+    {
+        m_hyperlink = new wxHyperlinkCtrl(wxTheApp->GetTopWindow(), wxID_ANY,
+                                         "wxWidgets", "http://wxwidgets.org");
+    }
 
-    void setUp() override;
-    void tearDown() override;
+    ~HyperlinkCtrlTestCase()
+    {
+        delete m_hyperlink;
+    }
 
-private:
-    CPPUNIT_TEST_SUITE( HyperlinkCtrlTestCase );
-        CPPUNIT_TEST( Colour );
-        CPPUNIT_TEST( Url );
-        WXUISIM_TEST( Click );
-    CPPUNIT_TEST_SUITE_END();
-
-    void Colour();
-    void Url();
-    void Click();
-
+protected:
     wxHyperlinkCtrl* m_hyperlink;
 
     wxDECLARE_NO_COPY_CLASS(HyperlinkCtrlTestCase);
 };
 
-// register in the unnamed registry so that these tests are run by default
-CPPUNIT_TEST_SUITE_REGISTRATION( HyperlinkCtrlTestCase );
-
-// also include in its own registry so that these tests can be run alone
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( HyperlinkCtrlTestCase, "HyperlinkCtrlTestCase" );
-
-void HyperlinkCtrlTestCase::setUp()
-{
-    m_hyperlink = new wxHyperlinkCtrl(wxTheApp->GetTopWindow(), wxID_ANY,
-                                     "wxWidgets", "http://wxwidgets.org");
-}
-
-void HyperlinkCtrlTestCase::tearDown()
-{
-    wxDELETE(m_hyperlink);
-}
-
-void HyperlinkCtrlTestCase::Colour()
+TEST_CASE_METHOD(HyperlinkCtrlTestCase, "wxHyperlinkCtrl::Colour",
+                 "[hyperlinkctrl]")
 {
 #ifndef __WXGTK__
-    CPPUNIT_ASSERT(m_hyperlink->GetHoverColour().IsOk());
-    CPPUNIT_ASSERT(m_hyperlink->GetNormalColour().IsOk());
-    CPPUNIT_ASSERT(m_hyperlink->GetVisitedColour().IsOk());
+    CHECK(m_hyperlink->GetHoverColour().IsOk());
+    CHECK(m_hyperlink->GetNormalColour().IsOk());
+    CHECK(m_hyperlink->GetVisitedColour().IsOk());
 
     m_hyperlink->SetHoverColour(*wxGREEN);
     m_hyperlink->SetNormalColour(*wxRED);
     m_hyperlink->SetVisitedColour(*wxBLUE);
 
-    CPPUNIT_ASSERT_EQUAL(*wxGREEN, m_hyperlink->GetHoverColour());
-    CPPUNIT_ASSERT_EQUAL(*wxRED, m_hyperlink->GetNormalColour());
-    CPPUNIT_ASSERT_EQUAL(*wxBLUE, m_hyperlink->GetVisitedColour());
+    CHECK( m_hyperlink->GetHoverColour() == *wxGREEN );
+    CHECK( m_hyperlink->GetNormalColour() == *wxRED );
+    CHECK( m_hyperlink->GetVisitedColour() == *wxBLUE );
 #endif
 }
 
-void HyperlinkCtrlTestCase::Url()
+TEST_CASE_METHOD(HyperlinkCtrlTestCase, "wxHyperlinkCtrl::Url",
+                 "[hyperlinkctrl]")
 {
-    CPPUNIT_ASSERT_EQUAL("http://wxwidgets.org", m_hyperlink->GetURL());
+    CHECK( m_hyperlink->GetURL() == "http://wxwidgets.org" );
 
     m_hyperlink->SetURL("http://google.com");
 
-    CPPUNIT_ASSERT_EQUAL("http://google.com", m_hyperlink->GetURL());
+    CHECK( m_hyperlink->GetURL() == "http://google.com" );
 }
 
-void HyperlinkCtrlTestCase::Click()
+TEST_CASE_METHOD(HyperlinkCtrlTestCase, "wxHyperlinkCtrl::Click",
+                 "[hyperlinkctrl]")
 {
 #if wxUSE_UIACTIONSIMULATOR
+    if ( !EnableUITests() )
+        return;
+
     EventCounter hyperlink(m_hyperlink, wxEVT_HYPERLINK);
 
     wxUIActionSimulator sim;
@@ -100,7 +85,7 @@ void HyperlinkCtrlTestCase::Click()
     sim.MouseClick();
     wxYield();
 
-    CPPUNIT_ASSERT_EQUAL(1, hyperlink.GetCount());
+    CHECK( hyperlink.GetCount() == 1 );
 #endif
 }
 
