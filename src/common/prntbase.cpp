cc53811c534b0905a20436b6ad968a3b270d22d2
Stefan Ziegler <sz5000@gmx.de>
2023-06-13 16:33:06 +0200

    new version, mac build seems to be strange

--- src/common/prntbase.cpp
+++ src/common/prntbase.cpp
@@ -640,7 +640,7 @@ std::vector<wxPrintPageRange> wxPrintout::GetPageInfoRanges(int* minPage, int* m
     GetPageInfo(minPage, maxPage, &fromPage, &toPage);
 
     std::vector<wxPrintPageRange> ranges;
-    ranges.emplace_back(fromPage, toPage);
+    ranges.push_back(wxPrintPageRange(fromPage, toPage));
     return ranges;
 }
 
