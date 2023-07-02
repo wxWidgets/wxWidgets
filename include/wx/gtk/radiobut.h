eb6ffd41d56ad64fb7086634a21bb3fc55e0f5c4
Stefan Hansson <newbie13xd@gmail.com>
2023-07-02 18:50:16 +0200

    Create hidden radio button in wxGTK when using wxRB_SINGLE
    This lets us deactivate the shown button by activating the hidden one.
    GTK does not allow deactivating radio buttons by calling them with
    gtk_toggle_button_set_active ( ..., FALSE ), so we need to work around
    it like this.
    
    Tested with GTK 3.24.38.
    
    Closes https://github.com/wxWidgets/wxWidgets/issues/23652
    Related https://github.com/wxWidgets/wxWidgets/issues/17022

--- include/wx/gtk/radiobut.h
+++ include/wx/gtk/radiobut.h
@@ -55,6 +55,8 @@ protected:
 
 private:
     typedef wxControl base_type;
+    // Only used by wxRB_SINGLE
+    GtkWidget *m_hidden_radio_button = nullptr;
 
     wxDECLARE_DYNAMIC_CLASS(wxRadioButton);
 };
