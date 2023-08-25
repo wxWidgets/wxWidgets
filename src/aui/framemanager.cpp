d65126d8837bf150824a179f3b681e6d0622703f
Martin Corino <mcorino@m2c-software.nl>
2023-08-25 09:09:45 +0200

    Don't stop wxWindowDestroyEvent processing in wxAuiManager
    The existing code prevented the other handlers from processing this even
    for no good reason, so don't do it any more.
    
    See #23811, #23815.
    
    (cherry picked from commit cd8a854dce8098d27e9b5c6f8d4a731717728482)

--- src/aui/framemanager.cpp
+++ src/aui/framemanager.cpp
@@ -3954,10 +3954,9 @@ void wxAuiManager::OnDestroy(wxWindowDestroyEvent& event)
         if ( frame )
             frame->ProcessWindowEventLocally(event);
     }
-    else
-    {
-        event.Skip();
-    }
+
+    // Make sure this event get's propagated to other handlers.
+    event.Skip();
 }
 
 void wxAuiManager::OnPaint(wxPaintEvent& WXUNUSED(event))
