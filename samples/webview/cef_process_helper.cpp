db255db61cddbc99fffb8216d0921ccdce677b44
Vadim Zeitlin <vadim@wxwidgets.org>
2023-09-04 23:13:57 +0200

    Use C++11 nullptr in CEF helper process in the sample
    This fixes its compilation under Mac and also avoids triggering the
    check forbidding the use of NULL in the new code.

--- samples/webview/cef_process_helper.cpp
+++ samples/webview/cef_process_helper.cpp
@@ -15,5 +15,5 @@ int main(int argc, char* argv[]) {
   CefMainArgs main_args(argc, argv);
 
   // Execute the secondary process.
-  return CefExecuteProcess(main_args, NULL, NULL);
+  return CefExecuteProcess(main_args, nullptr, nullptr);
 }
