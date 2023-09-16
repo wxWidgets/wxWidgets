5241287a81c9b35d1c25116271e8a3344a97ea7a
Vadim Zeitlin <vadim@wxwidgets.org>
2023-09-16 13:46:57 +0200

    Fix recent wxGTK regression when using wide string entry point
    The changes of 5f17915e63 (Store original command line options passed to
    main(), 2023-09-02) didn't account for the possibility of the
    application using an entry point such as wxEntry(), wxEntryStart() or
    wxInitialize() with wide character argv under Unix -- where this is
    typically not the case but still possible and if any of the wide string
    overloads of these functions were called, wxInitData::arg[cv] remained
    uninitialized resulting in a crash in gtk_init_check() later.
    
    Fix this by ensuring we do initialize them in this case too and also
    reconstruct the original narrow string argvA from wide string arguments
    in this case, which is a bit wasteful but unavoidable if we're not
    provided access to the original main() arguments.

--- include/wx/private/init.h
+++ include/wx/private/init.h
@@ -21,8 +21,13 @@ struct WXDLLIMPEXP_BASE wxInitData
     // Get the single global object.
     static wxInitData& Get();
 
-    // Initialize from ANSI command line arguments.
+    // Initialize from ANSI command line arguments: argv contents should be
+    // static, i.e. remain valid until the end of the program.
     void Initialize(int argc, char** argv);
+
+    // Initialize from wide command line arguments: here we currently make a
+    // copy of the arguments internally, so they don't need to be static.
+    void InitializeFromWide(int argc, wchar_t** argv);
 
     // This function is used instead of the dtor because the global object can
     // be initialized multiple times.
@@ -47,9 +52,10 @@ struct WXDLLIMPEXP_BASE wxInitData
     wchar_t** argvMSW = nullptr;
 #else // !__WINDOWS__
     // Under other platforms we typically need the original, non-Unicode
-    // command line version, so we keep it too. Unlike argv that we allocate,
-    // this pointer doesn't need to be freed.
+    // command line version, so we keep it too. This pointer may or not need to
+    // be freed, as indicated by ownsArgvA flag.
     char** argvA = nullptr;
+    bool ownsArgvA = false;
 #endif // __WINDOWS__
 
     wxDECLARE_NO_COPY_CLASS(wxInitData);
