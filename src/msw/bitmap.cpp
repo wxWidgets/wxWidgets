5ca3157467d57e5f8aad634ce3e1b6dd770b4a1f
PB <pbfordev@gmail.com>
2023-09-30 21:39:42 +0200

    Fix return type of wxBitmap::UseAlpha() under wxMSW
    The method should return bool but was returning void.
    
    This commit is best viewed ignoring whitespace-only changes.
    
    Closes #23903, #23919.

--- src/msw/bitmap.cpp
+++ src/msw/bitmap.cpp
@@ -1222,16 +1222,15 @@ wxDC *wxBitmap::GetSelectedInto() const
 #endif
 }
 
-void wxBitmap::UseAlpha(bool use)
+bool wxBitmap::UseAlpha(bool use)
 {
-    if ( GetBitmapData() )
-    {
-        // Only 32bpp bitmaps can contain alpha channel.
-        if ( use && GetBitmapData()->m_depth < 32 )
-            use = false;
+    // Only 32bpp bitmaps can contain alpha channel.
+    if ( !GetBitmapData() || (use && GetBitmapData()->m_depth < 32) )
+        return false;
 
-        GetBitmapData()->m_hasAlpha = use;
-    }
+    GetBitmapData()->m_hasAlpha = use;
+
+    return true;
 }
 
 bool wxBitmap::HasAlpha() const
