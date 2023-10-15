810f0b882259cc0df34416532da72a3baf3d0d1e
Ulrich Telle <ulrich.telle@gmx.de>
2023-10-15 19:10:07 +0200

    Move static standard C locale into if condition
    This avoids to allocate a standard C locale if not needed.

--- src/osx/core/uilocale.mm
+++ src/osx/core/uilocale.mm
@@ -42,9 +42,9 @@ static NSLocale* gs_currentNSLocale = nullptr;
 
 NSLocale* wxGetCurrentNSLocale()
 {
-    static NSLocale* stdCLocale([[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"] retain]);
     if (!gs_currentNSLocale)
     {
+        static wxCFRef<NSLocale*> stdCLocale([[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"] retain]);
         gs_currentNSLocale = stdCLocale;
     }
     return gs_currentNSLocale;
