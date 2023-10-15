d95d8f57f83f4e448aa69af1962264f1682773de
Ulrich Telle <ulrich.telle@gmx.de>
2023-10-15 18:57:03 +0200

    Remove Cocoa compilation guard

--- include/wx/osx/private/uilocale.h
+++ include/wx/osx/private/uilocale.h
@@ -2,7 +2,7 @@
 // Name:        wx/osx/private/uilocale.h
 // Purpose:     Helper for making pointer to current NSLocale available
 //              for locale-dependent controls under macOS.
-// Author:      Vadim Zeitlin
+// Author:      Ulrich Telle
 // Created:     2023-10-13
 // Copyright:   (c) 2023 Vadim Zeitlin <vadim@wxwidgets.org>
 // Licence:     wxWindows licence
@@ -13,10 +13,8 @@
 
 #if wxUSE_INTL
 
-#ifdef __WXOSX_COCOA__
 // Function returning a pointer to the current NSLocale
 WXDLLIMPEXP_BASE NSLocale* wxGetCurrentNSLocale();
-#endif
 
 #endif
 
