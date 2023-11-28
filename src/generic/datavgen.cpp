0010cd08e4b9d6f15dde90aa946adbb8a5f96c93
Paul Cornett <paulcor@users.noreply.github.com>
2023-11-27 23:08:22 -0800

    Fix generic wxDataViewCtrl drag hint transparency with GTK
    wxGTK requires SetTransparent() be called before Show().
    See #24084
    
    (cherry picked from commit d2140993dec4561272929c206294a01d8492c801)

--- src/generic/datavgen.cpp
+++ src/generic/datavgen.cpp
@@ -1715,12 +1715,12 @@ public:
                                         wxFRAME_NO_TASKBAR |
                                         wxNO_BORDER );
             new wxBitmapCanvas( m_hint, ib, ib.GetSize() );
+            m_hint->SetTransparent(128);
             m_hint->Show();
         }
         else
         {
             m_hint->Move( pos.x - m_dist_x, pos.y + 5  );
-            m_hint->SetTransparent( 128 );
         }
 
         return false;
