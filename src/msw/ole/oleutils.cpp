6c6159823cd385e8d08537e37bc8435ee208820d
Miguel Gimenez <wh11204@users.noreply.github.com>
2023-12-03 15:43:04 +0100

    Add support for VT_UI8 OLE VARIANT type
    Convert it to and from wxULongLong, as just as we already convert
    between VT_I8 and wxLongLong.
    
    See #24115.
    
    (cherry picked from commit 0965698c585df8b4bf744d8eac7056197b1fa2be)

--- src/msw/ole/oleutils.cpp
+++ src/msw/ole/oleutils.cpp
@@ -314,7 +314,12 @@ WXDLLEXPORT bool wxConvertVariantToOle(const wxVariant& variant, VARIANTARG& ole
         oleVariant.vt = VT_I8;
         oleVariant.llVal = variant.GetLongLong().GetValue();
     }
-#endif
+    else if (type == wxT("ulonglong"))
+    {
+        oleVariant.vt = VT_UI8;
+        oleVariant.ullVal = variant.GetULongLong().GetValue();
+    }
+#endif // wxUSE_LONGLONG
     else if (type == wxT("char"))
     {
         oleVariant.vt=VT_I1;            // Signed Char
@@ -483,6 +488,9 @@ wxConvertOleToVariant(const VARIANTARG& oleVariant, wxVariant& variant, long fla
 #if wxUSE_LONGLONG
             case VT_I8:
                 variant = wxLongLong(oleVariant.llVal);
+                break;
+            case VT_UI8:
+                variant = wxULongLong(oleVariant.ullVal);
                 break;
 #endif // wxUSE_LONGLONG
 
