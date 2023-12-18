wxWidgets/wxWidgets

commit 11ed91af8fc79f0a3d41a7c6b0a9b1c447b54598

Add missing default constructor of wxFileDialog in wxMSW
This ctor exists in the other ports and there is no reason for it not to
exist in wxMSW too.

Closes #24113.

include/wx/msw/filedlg.h
@@ -19,6 +19,7 @@ class wxFileDialogMSWData;
 class WXDLLIMPEXP_CORE wxFileDialog: public wxFileDialogBase
 {
 public:
+    wxFileDialog() = default;
     wxFileDialog(wxWindow *parent,
                  const wxString& message = wxASCII_STR(wxFileSelectorPromptStr),
                  const wxString& defaultDir = wxEmptyString,

commit f8a6f0cd39c55aeeb5ed293eb9be47a80dfe9102

Initialize wxFileDialog::m_data in default ctor in wxMSW too
This corrects a problem introduced in 11ed91af8f (Add missing default
constructor of wxFileDialog in wxMSW, 2023-12-03) as m_data remained
uninitialized when default ctor was used.

See #24113.

include/wx/msw/filedlg.h
@@ -79,7 +79,7 @@ private:
 
     // Extra data, possibly null if not needed, use MSWData() to access it if
     // it should be created on demand.
-    wxFileDialogMSWData* m_data;
+    wxFileDialogMSWData* m_data = nullptr;
 
 
     wxDECLARE_DYNAMIC_CLASS(wxFileDialog);

src/msw/filedlg.cpp
@@ -1024,8 +1024,6 @@ wxFileDialog::wxFileDialog(wxWindow *parent,
 {
     // NB: all style checks are done by wxFileDialogBase::Create
 
-    m_data = nullptr;
-
     // Must set to zero, otherwise the wx routines won't size the window
     // the second time you call the file dialog, because it thinks it is
     // already at the requested size.. (when centering)