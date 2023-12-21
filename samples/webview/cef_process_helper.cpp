5a2c45b8f732718cce4221c2593b21c4868e92eb
Maarten Bent <MaartenBent@users.noreply.github.com>
2023-12-21 21:16:05 +0100

    Fix running webview sample with Chromium on macOS

--- samples/webview/cef_process_helper.cpp
+++ samples/webview/cef_process_helper.cpp
@@ -27,6 +27,9 @@
 // and under macOS see the commands executed by the sample makefile.
 
 #include "include/cef_app.h"
+#if defined(__APPLE__)
+    #include "include/wrapper/cef_library_loader.h"
+#endif
 
 // Define simplest possible process entry point just forwarding to CEF.
 #if defined(_WIN32)
@@ -43,6 +46,15 @@ WinMain(HINSTANCE hInstance, HINSTANCE, char*, int)
 
 int main(int argc, char* argv[])
 {
+#if defined(__APPLE__)
+    // Load the CEF framework library at runtime instead of linking directly
+    // as required by the macOS sandbox implementation.
+    CefScopedLibraryLoader library_loader;
+    if (!library_loader.LoadInHelper()) {
+        return 1;
+    }
+#endif
+
     CefMainArgs cefArgs(argc, argv);
 
     return CefExecuteProcess(cefArgs, nullptr, nullptr);
