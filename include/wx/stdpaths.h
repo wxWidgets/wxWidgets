5794f0cc0d699bddc428cfbc1cd4a3eb6c46a920
oneeyeman1 <ikorot01@gmail.com>
2024-01-24 20:04:09 -0600

    Fixing the typo; Fix compilation error on MSW when USE_STDPATH 0

--- include/wx/stdpaths.h
+++ include/wx/stdpaths.h
@@ -257,7 +257,10 @@ public:
     {
         return m_prefix + wxS("/") + basename;
     }
-    virtual wxString GetSharedLibrariesDir() overrde const { return m_prefix; }
+    virtual wxString GetSharedLibrariesDir() const override { return m_prefix; }
+#ifdef __WXMSW__
+    static wxString MSWGetShellDir(int csidl) { return wxEmptyString; }
+#endif
 protected:
     // Ctor is protected because wxStandardPaths::Get() should always be used
     // to access the global wxStandardPaths object of the correct type instead
