e55958b79997f0929f83ae1c6d4601c690721d70
Blake Madden <madindayton@outlook.com>
2024-01-30 18:09:25 -0500

    Use wxRound instead of int cast

--- src/ribbon/panel.cpp
+++ src/ribbon/panel.cpp
@@ -711,8 +711,8 @@ bool wxRibbonPanel::Realize()
                 scale = 2.0;
 
             wxImage img(m_minimised_icon.ConvertToImage());
-            img.Rescale(static_cast<int>(scale * bitmap_size.GetWidth()),
-                        static_cast<int>(scale * bitmap_size.GetHeight()),
+            img.Rescale(wxRound(scale * bitmap_size.GetWidth()),
+                        wxRound(scale * bitmap_size.GetHeight()),
                         wxIMAGE_QUALITY_HIGH);
             m_minimised_icon_resized = wxBitmap(img, -1, scale);
         }
