wxWidgets/wxWidgets

commit 9befda5c26d478b3acfd03acc74ea3426fbfc60c

Only use AutoSystemDpiAware when displays have different DPI
So the native win32 dialogs, like font and colour picker, are not shown blurry unnecessary.

Fixes #24121

include/wx/msw/private/dpiaware.h
@@ -15,6 +15,7 @@
 #endif
 
 #include "wx/dynlib.h"
+#include "wx/display.h"
 
 namespace wxMSWImpl
 {
@@ -35,6 +36,9 @@ public:
     AutoSystemDpiAware()
         : m_prevContext(WXDPI_AWARENESS_CONTEXT_UNAWARE)
     {
+        if ( !Needed() )
+            return;
+
         if ( ms_pfnSetThreadDpiAwarenessContext == (SetThreadDpiAwarenessContext_t)-1)
         {
             wxLoadedDLL dllUser32("user32.dll");
@@ -60,6 +64,15 @@ public:
         {
             ms_pfnSetThreadDpiAwarenessContext(m_prevContext);
         }
+    }
+
+    static bool Needed()
+    {
+        // use system-dpi-aware context when there are displays with different DPI
+        bool diferentDPI = false;
+        for ( unsigned i = 1; i < wxDisplay::GetCount() && !diferentDPI; ++i )
+            diferentDPI = wxDisplay(0u).GetPPI() != wxDisplay(i).GetPPI();
+        return diferentDPI;
     }
 
 private:

commit c72f4efc9c024419bba37aa31983489481302b6c

Fix crash when closing a wxColourDialog under MSW
Check that ms_pfnSetThreadDpiAwarenessContext function pointer was
initialized before using it.

This fixes a regression introduced in 9befda5c26 (Only use
AutoSystemDpiAware when displays have different DPI, 2023-12-10).

See #24121, #24196.

Closes #24288.

include/wx/msw/private/dpiaware.h
@@ -61,7 +61,8 @@ public:
 
     ~AutoSystemDpiAware()
     {
-        if ( ms_pfnSetThreadDpiAwarenessContext )
+        if ( ms_pfnSetThreadDpiAwarenessContext &&
+                ms_pfnSetThreadDpiAwarenessContext != (SetThreadDpiAwarenessContext_t)-1 )
         {
             ms_pfnSetThreadDpiAwarenessContext(m_prevContext);
         }