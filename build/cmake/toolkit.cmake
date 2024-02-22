9b9bdc5a7169c9105a6a478af919bee06e5bcd8e
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-02-22 10:02:39 +0300

    Remove glib-2.0 includes.

--- build/cmake/toolkit.cmake
+++ build/cmake/toolkit.cmake
@@ -135,7 +135,6 @@ if(WXQT)
         find_package(PkgConfig REQUIRED)
 
         pkg_check_modules (GLIB2 REQUIRED glib-2.0)
-        include_directories(${GLIB2_INCLUDE_DIRS})
 
         list(APPEND wxTOOLKIT_INCLUDE_DIRS ${GLIB2_INCLUDE_DIRS})
         list(APPEND wxTOOLKIT_LIBRARIES ${GLIB_LIBRARIES})
