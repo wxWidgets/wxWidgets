wxWidgets/wxWidgets

commit f24b3d54833a1c15ae3bba87dead83bf9a90c41d

Save last wxSplitterWindow position before it was unsplit
Use this position when the window is split again and also save and
restore it from persistent storage in wxPersistentSplitter.

Closes #24329.

interface/wx/splitter.h
@@ -477,6 +477,36 @@ public:
         before showing the top-level window.
     */
     void UpdateSize();
+
+    /**
+        Get the last sash position before the splitter was unsplit.
+
+        The last sash position gets updated each time the window is unsplit. It
+        is kept internally to allow restoring the sash in its previous position
+        when if it is split again.
+
+        @return Point whose x/y component corresponds to the position of the
+            vertical/horizontal sash before the last unsplit or 0 if the
+            splitter was never split in the corresponding direction.
+
+        @since 3.3.0
+    */
+    const wxPoint& GetLastSplitPosition() const;
+
+    /**
+        Sets the last sash position.
+
+        This does not affect the actual sash position while the window is split
+        but determines the initial position of the sash when the window gets
+        split in the future.
+
+        @param pos
+            Point containing the default positions for wxSPLIT_VERTICAL and
+            wxSPLIT_HORIZONTAL modes respectively in its x and y components.
+
+        @since 3.3.0
+    */
+    void SetLastSplitPosition(const wxPoint& pos);
 };
 
 

include/wx/generic/splitter.h
@@ -226,6 +226,15 @@ public:
     // all any more.
     wxDEPRECATED_INLINE( void SetSashSize(int WXUNUSED(width)), return; )
 
+    // Get the sash position that was last used before Unsplit() was called.
+    // Horizontal and vertical components correspond to the split in the
+    // corresponding direction, and are 0 if the splitter hadn't been split in
+    // this direction at all.
+    wxPoint GetLastSplitPosition() const;
+
+    // Set the default initial sash position to use when the splitter is split.
+    void SetLastSplitPosition(const wxPoint& pos);
+
 protected:
     // event handlers
 #if defined(__WXMSW__) || defined(__WXMAC__)
@@ -289,6 +298,7 @@ protected:
     wxPoint     m_ptStart;      // mouse position when dragging started
     int         m_sashStart;    // sash position when dragging started
     int         m_minimumPaneSize;
+    wxPoint     m_lastSplitPosition;
     wxCursor    m_sashCursorWE;
     wxCursor    m_sashCursorNS;
     wxOverlay   m_overlay;

include/wx/persist/splitter.h
@@ -23,6 +23,9 @@
 // Special position value of -1 means the splitter is not split at all.
 #define wxPERSIST_SPLITTER_POSITION wxASCII_STR("Position")
 
+#define wxPERSIST_SPLITTER_DEFAULT_HORIZONTAL wxASCII_STR("LastHorz")
+#define wxPERSIST_SPLITTER_DEFAULT_VERTICAL wxASCII_STR("LastVert")
+
 // ----------------------------------------------------------------------------
 // wxPersistentSplitter: supports saving/restoring splitter position
 // ----------------------------------------------------------------------------
@@ -41,6 +44,14 @@ public:
 
         int pos = splitter->IsSplit() ? splitter->GetSashPosition() : -1;
         SaveValue(wxPERSIST_SPLITTER_POSITION, pos);
+
+        // Save the previously used position too if we have them.
+        const wxPoint lastSplitPos = splitter->GetLastSplitPosition();
+        if ( lastSplitPos.x || lastSplitPos.y )
+        {
+            SaveValue(wxPERSIST_SPLITTER_DEFAULT_HORIZONTAL, lastSplitPos.y);
+            SaveValue(wxPERSIST_SPLITTER_DEFAULT_VERTICAL, lastSplitPos.x);
+        }
     }
 
     virtual bool Restore() override
@@ -53,6 +64,14 @@ public:
             Get()->Unsplit();
         else
             Get()->SetSashPosition(pos);
+
+        // Note that it's possible that default position was not stored, in
+        // which case lastSplitPos will just remain as (0, 0) and that's ok.
+        wxPoint lastSplitPos;
+        RestoreValue(wxPERSIST_SPLITTER_DEFAULT_HORIZONTAL, &lastSplitPos.x);
+        RestoreValue(wxPERSIST_SPLITTER_DEFAULT_VERTICAL, &lastSplitPos.y);
+
+        Get()->SetLastSplitPosition(lastSplitPos);
 
         return true;
     }

src/generic/splitter.cpp
@@ -840,6 +840,18 @@ int wxSplitterWindow::ConvertSashPosition(int sashPosition) const
     }
     else // sashPosition == 0
     {
+        // Use last split position if we have it.
+        if ( m_splitMode == wxSPLIT_VERTICAL )
+        {
+            if ( m_lastSplitPosition.x )
+                return m_lastSplitPosition.x;
+        }
+        else if ( m_splitMode == wxSPLIT_HORIZONTAL )
+        {
+            if ( m_lastSplitPosition.y )
+                return m_lastSplitPosition.y;
+        }
+
         // default, put it in the centre
         return GetWindowSize() / 2;
     }
@@ -869,6 +881,15 @@ bool wxSplitterWindow::Unsplit(wxWindow *toRemove)
         wxFAIL_MSG(wxT("splitter: attempt to remove a non-existent window"));
 
         return false;
+    }
+
+    if (m_splitMode == wxSPLIT_VERTICAL)
+    {
+        m_lastSplitPosition.x = m_sashPosition;
+    }
+    else if (m_splitMode == wxSPLIT_HORIZONTAL)
+    {
+        m_lastSplitPosition.y = m_sashPosition;
     }
 
     OnUnsplit(win);
@@ -1085,6 +1106,17 @@ void wxSplitterWindow::OnUnsplit(wxWindow *winRemoved)
     // call this before calling the event handler which may delete the window
     winRemoved->Show(false);
 }
+
+wxPoint wxSplitterWindow::GetLastSplitPosition() const
+{
+    return m_lastSplitPosition;
+}
+
+void wxSplitterWindow::SetLastSplitPosition(const wxPoint& pos)
+{
+    m_lastSplitPosition = pos;
+}
+
 
 #if defined( __WXMSW__ ) || defined( __WXMAC__)
 

commit be3ea1768a68f2a5897436f9913ba9ef793ce776

Fix documentation of wxSplitterWindow::GetLastSplitPosition()
Correct the documented return type of this function to match its actual
type to fix a problem introduced in the recent f24b3d5483 (Save last
wxSplitterWindow position before it was unsplit, 2024-02-12).

See #24329.

Closes #24347.

interface/wx/splitter.h
@@ -491,7 +491,7 @@ public:
 
         @since 3.3.0
     */
-    const wxPoint& GetLastSplitPosition() const;
+    wxPoint GetLastSplitPosition() const;
 
     /**
         Sets the last sash position.