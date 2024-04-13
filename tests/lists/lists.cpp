635c1f251c987e618509e938e0271daab76e2215
Vadim Zeitlin <vadim@wxwidgets.org>
2024-04-13 17:56:59 +0200

    Use DeleteContents() even with wxUSE_STD_CONTAINERS==1 in test
    There doesn't seem to be any reason to have different branches of test
    code for wxUSE_STD_CONTAINERS on and off, DeleteContents() works when
    it's on too.

--- tests/lists/lists.cpp
+++ tests/lists/lists.cpp
@@ -200,11 +200,7 @@ void ListsTestCase::wxListCtorTest()
         CPPUNIT_ASSERT( list2.GetCount() == 2 );
         CPPUNIT_ASSERT( Baz::GetNumber() == 2 );
 
-#if !wxUSE_STD_CONTAINERS
         list1.DeleteContents(true);
-#else
-        wxClearList(list1);
-#endif
     }
 
     CPPUNIT_ASSERT( Baz::GetNumber() == 0 );
