ced269d29842f92a1ef0d6925042db9baf1d6268
Bill Su <wsu@acm.org>
2024-04-16 00:06:59 -0400

    Fix wxList::Sort() compilation after recent changes
    Add required "this->" to allow standard-conforming compilers to find
    sort().
    
    Closes #24477.

--- include/wx/list.h
+++ include/wx/list.h
@@ -242,7 +242,7 @@ public:
     }
     /* Workaround for broken VC6 std::list::sort() see above */
     void Sort( wxSortCompareFunction compfunc )
-        { sort( wxList_SortFunction<elT>(compfunc ) ); }
+        { this->sort( wxList_SortFunction<elT>(compfunc ) ); }
     ~wxListImpl() { Clear(); }
 
     /* It needs access to our EmptyList */
