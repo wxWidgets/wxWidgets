c3be83de11a2e44e6c5af1de307e75f35cd4e55a
Vadim Zeitlin <vadim@wxwidgets.org>
2024-04-17 18:52:11 +0200

    Always restore wxStaticText original m_foregroundColour
    It's not clear if changing it to the "disabled" colour in dark mode
    if m_hasFgCol was false actually resulted in any problems, but it seems
    safer to always restore it after drawing the control using the disabled
    colour in any case.
    
    This corrects another (possible) problem after the changes of bc72a5d003
    (Fix drawing of disabled wxStaticText in dark mode, 2023-05-18).

--- src/msw/stattext.cpp
+++ src/msw/stattext.cpp
@@ -186,8 +186,7 @@ wxStaticText::MSWHandleMessage(WXLRESULT *result,
             *result = MSWDefWindowProc(WM_PAINT, wParam, lParam);
 
             updateStyle.TurnOn(WS_DISABLED).Apply();
-            if ( m_hasFgCol )
-                m_foregroundColour = colFgOrig;
+            m_foregroundColour = colFgOrig;
 
             return true;
     }
