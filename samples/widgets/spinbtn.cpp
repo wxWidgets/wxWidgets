wxWidgets/wxWidgets

commit a8b4753d1dfa84a41b0aba6ca4c93811abcfa3e2

Reconnect events to the widget after recreating it in the sample
This ensures that the expected events are given even after the widget is
recreated due to a change of some controls on its page (and not because
it's recreated by one of the menu commands in the parent frame).

We also don't need to connect to these events from WidgetsFrame any
longer (and, in fact, doing it would be wrong as we'd connect twice),
which simplifies its code.

samples/widgets/spinbtn.cpp
@@ -416,6 +416,8 @@ void SpinBtnWidgetsPage::CreateSpin()
                                  wxDefaultPosition, wxDefaultSize,
                                  flags);
 
+    NotifyWidgetRecreation(m_spinbtn);
+
     m_spinbtn->SetValue(val);
     m_spinbtn->SetRange(m_min, m_max);
 
@@ -425,11 +427,15 @@ void SpinBtnWidgetsPage::CreateSpin()
                                 flags | textFlags,
                                 m_min, m_max, val);
 
+    NotifyWidgetRecreation(m_spinctrl);
+
     m_spinctrldbl = new wxSpinCtrlDouble(this, SpinBtnPage_SpinCtrlDouble,
                                          wxString::Format("%d", val),
                                          wxDefaultPosition, wxDefaultSize,
                                          flags | textFlags,
                                          m_min, m_max, val, 0.1);
+
+    NotifyWidgetRecreation(m_spinctrldbl);
 
     // Add spacers, labels and spin controls to the sizer.
     m_sizerSpin->Add(0, 0, 1);

samples/widgets/activityindicator.cpp
@@ -145,6 +145,8 @@ void ActivityIndicatorWidgetsPage::RecreateWidget()
                                           wxDefaultPosition, wxDefaultSize,
                                           GetAttrs().m_defaultFlags);
 
+    NotifyWidgetRecreation(m_indicator);
+
     m_sizerIndicator->AddStretchSpacer();
     m_sizerIndicator->Add(m_indicator, wxSizerFlags().Centre());
     m_sizerIndicator->AddStretchSpacer();

samples/widgets/bmpcombobox.cpp
@@ -478,6 +478,8 @@ void BitmapComboBoxWidgetsPage::CreateCombo()
     m_combobox->SetPopupMaxHeight(600);
 #endif
 
+    NotifyWidgetRecreation(m_combobox);
+
     unsigned int count = items.GetCount();
     for ( unsigned int n = 0; n < count; n++ )
     {

samples/widgets/button.cpp
@@ -562,6 +562,8 @@ void ButtonWidgetsPage::CreateButton()
     m_sizerNote->Show(m_chkCommandLink->GetValue());
 #endif
 
+    NotifyWidgetRecreation(m_button);
+
     if ( !showsBitmap && m_chkTextAndBitmap->GetValue() )
     {
         showsBitmap = true;

samples/widgets/checkbox.cpp
@@ -276,6 +276,8 @@ void CheckBoxWidgetsPage::CreateCheckbox()
                               wxDefaultPosition, wxDefaultSize,
                               flags);
 
+    NotifyWidgetRecreation(m_checkbox);
+
     m_sizerCheckbox->Add(0, 0, 1, wxCENTRE);
     m_sizerCheckbox->Add(m_checkbox, 1, wxCENTRE);
     m_sizerCheckbox->Add(0, 0, 1, wxCENTRE);

samples/widgets/choice.cpp
@@ -303,6 +303,8 @@ void ChoiceWidgetsPage::CreateChoice()
                             0, nullptr,
                             flags);
 
+    NotifyWidgetRecreation(m_choice);
+
     m_choice->Set(items);
     m_sizerChoice->Add(m_choice, 0, wxGROW | wxALL, 5);
     m_sizerChoice->Layout();

samples/widgets/clrpicker.cpp
@@ -191,6 +191,8 @@ void ColourPickerWidgetsPage::CreatePicker()
     m_clrPicker = new wxColourPickerCtrl(this, PickerPage_Colour, *wxRED,
                                          wxDefaultPosition, wxDefaultSize,
                                          style);
+
+    NotifyWidgetRecreation(m_clrPicker);
 }
 
 void ColourPickerWidgetsPage::RecreatePicker()

samples/widgets/combobox.cpp
@@ -476,6 +476,8 @@ void ComboboxWidgetsPage::CreateCombo()
     delete m_combobox;
     m_combobox = newCb;
     m_combobox->SetId(ComboPage_Combo);
+
+    NotifyWidgetRecreation(m_combobox);
 }
 
 // ----------------------------------------------------------------------------

commit 5e5a68535c8e8195a657de9c0bdafdf5b4b3aca0

Fix crash on spin page of widgets sample
Attempting to change the style or alignment of the spin controls
in the widgets sample made the sample crash. This was caused by a8b4753 (Reconnect events to the widget after recreating it in the sample, 2024-02-19) which resulted in using windows that were previously destroyed.

Fix this by calling NotifyWidgetRecreation() for each spin control only
after they were all recreated.

samples/widgets/spinbtn.cpp
@@ -415,9 +415,6 @@ void SpinBtnWidgetsPage::CreateSpin()
     m_spinbtn = new wxSpinButton(this, SpinBtnPage_SpinBtn,
                                  wxDefaultPosition, wxDefaultSize,
                                  flags);
-
-    NotifyWidgetRecreation(m_spinbtn);
-
     m_spinbtn->SetValue(val);
     m_spinbtn->SetRange(m_min, m_max);
 
@@ -427,14 +424,14 @@ void SpinBtnWidgetsPage::CreateSpin()
                                 flags | textFlags,
                                 m_min, m_max, val);
 
-    NotifyWidgetRecreation(m_spinctrl);
-
     m_spinctrldbl = new wxSpinCtrlDouble(this, SpinBtnPage_SpinCtrlDouble,
                                          wxString::Format("%d", val),
                                          wxDefaultPosition, wxDefaultSize,
                                          flags | textFlags,
                                          m_min, m_max, val, 0.1);
 
+    NotifyWidgetRecreation(m_spinbtn);
+    NotifyWidgetRecreation(m_spinctrl);
     NotifyWidgetRecreation(m_spinctrldbl);
 
     // Add spacers, labels and spin controls to the sizer.