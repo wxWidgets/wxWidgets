wxWidgets/wxWidgets

commit a8b4753d1dfa84a41b0aba6ca4c93811abcfa3e2

Reconnect events to the widget after recreating it in the sample
This ensures that the expected events are given even after the widget is
recreated due to a change of some controls on its page (and not because
it's recreated by one of the menu commands in the parent frame).

We also don't need to connect to these events from WidgetsFrame any
longer (and, in fact, doing it would be wrong as we'd connect twice),
which simplifies its code.

samples/widgets/widgets.cpp
@@ -257,8 +257,6 @@ protected:
     WidgetsPage *CurrentPage();
 
 private:
-    void ConnectToWidgetEvents();
-
     // the panel containing everything
     wxPanel *m_panel;
 
@@ -773,23 +771,6 @@ WidgetsPage *WidgetsFrame::CurrentPage()
     return wxStaticCast(page, WidgetsPage);
 }
 
-void WidgetsFrame::ConnectToWidgetEvents()
-{
-    auto& app = wxGetApp();
-
-    const Widgets& widgets = CurrentPage()->GetWidgets();
-
-    for ( Widgets::const_iterator it = widgets.begin();
-            it != widgets.end();
-            ++it )
-    {
-        wxWindow* const w = *it;
-        wxCHECK_RET(w, "null widget");
-
-        app.ConnectToWidgetEvents(w);
-    }
-}
-
 WidgetsFrame::~WidgetsFrame()
 {
 #if USE_LOG
@@ -847,7 +828,18 @@ void WidgetsFrame::OnPageChanged(WidgetsBookCtrlEvent& event)
         curPage->SetScrollRate(10, 10);
         curPage->FitInside();
 
-        ConnectToWidgetEvents();
+        auto& app = wxGetApp();
+        for ( const auto w : CurrentPage()->GetWidgets() )
+        {
+            app.ConnectToWidgetEvents(w);
+        }
+
+        // From now on, we're interested in these notifications as we'll need
+        // to reconnect to the widget events if it's recreated (unfortunately
+        // we can't rely getting them on creation as some page don't generate
+        // them -- but neither can we rely on not getting them as some pages do
+        // generate them, hence the use of m_notifyRecreate flag).
+        curPage->EnableRecreationNotifications();
     }
 
     // re-apply the attributes to the widget(s)
@@ -1016,11 +1008,6 @@ void WidgetsFrame::OnSetBorder(wxCommandEvent& event)
     WidgetsPage *page = CurrentPage();
 
     page->RecreateWidget();
-
-    ConnectToWidgetEvents();
-
-    // re-apply the attributes to the widget(s)
-    page->SetUpWidget();
 }
 
 void WidgetsFrame::OnSetVariant(wxCommandEvent& event)
@@ -1480,6 +1467,19 @@ wxCheckBox *WidgetsPage::CreateCheckBoxAndAddToSizer(wxSizer *sizer,
     sizer->AddSpacer(2);
 
     return checkbox;
+}
+
+void WidgetsPage::NotifyWidgetRecreation(wxWindow* widget)
+{
+    if ( !m_notifyRecreate )
+    {
+        // We're in the process of initialization, don't notify yet.
+        return;
+    }
+
+    SetUpWidget();
+
+    wxGetApp().ConnectToWidgetEvents(widget);
 }
 
 /* static */

samples/widgets/activityindicator.cpp
@@ -145,6 +145,8 @@ void ActivityIndicatorWidgetsPage::RecreateWidget()
                                           wxDefaultPosition, wxDefaultSize,
                                           GetAttrs().m_defaultFlags);
 
+    NotifyWidgetRecreation(m_indicator);
+
     m_sizerIndicator->AddStretchSpacer();
     m_sizerIndicator->Add(m_indicator, wxSizerFlags().Centre());
     m_sizerIndicator->AddStretchSpacer();

samples/widgets/bmpcombobox.cpp
@@ -478,6 +478,8 @@ void BitmapComboBoxWidgetsPage::CreateCombo()
     m_combobox->SetPopupMaxHeight(600);
 #endif
 
+    NotifyWidgetRecreation(m_combobox);
+
     unsigned int count = items.GetCount();
     for ( unsigned int n = 0; n < count; n++ )
     {

samples/widgets/button.cpp
@@ -562,6 +562,8 @@ void ButtonWidgetsPage::CreateButton()
     m_sizerNote->Show(m_chkCommandLink->GetValue());
 #endif
 
+    NotifyWidgetRecreation(m_button);
+
     if ( !showsBitmap && m_chkTextAndBitmap->GetValue() )
     {
         showsBitmap = true;

samples/widgets/checkbox.cpp
@@ -276,6 +276,8 @@ void CheckBoxWidgetsPage::CreateCheckbox()
                               wxDefaultPosition, wxDefaultSize,
                               flags);
 
+    NotifyWidgetRecreation(m_checkbox);
+
     m_sizerCheckbox->Add(0, 0, 1, wxCENTRE);
     m_sizerCheckbox->Add(m_checkbox, 1, wxCENTRE);
     m_sizerCheckbox->Add(0, 0, 1, wxCENTRE);

samples/widgets/choice.cpp
@@ -303,6 +303,8 @@ void ChoiceWidgetsPage::CreateChoice()
                             0, nullptr,
                             flags);
 
+    NotifyWidgetRecreation(m_choice);
+
     m_choice->Set(items);
     m_sizerChoice->Add(m_choice, 0, wxGROW | wxALL, 5);
     m_sizerChoice->Layout();

samples/widgets/clrpicker.cpp
@@ -191,6 +191,8 @@ void ColourPickerWidgetsPage::CreatePicker()
     m_clrPicker = new wxColourPickerCtrl(this, PickerPage_Colour, *wxRED,
                                          wxDefaultPosition, wxDefaultSize,
                                          style);
+
+    NotifyWidgetRecreation(m_clrPicker);
 }
 
 void ColourPickerWidgetsPage::RecreatePicker()

samples/widgets/combobox.cpp
@@ -476,6 +476,8 @@ void ComboboxWidgetsPage::CreateCombo()
     delete m_combobox;
     m_combobox = newCb;
     m_combobox->SetId(ComboPage_Combo);
+
+    NotifyWidgetRecreation(m_combobox);
 }
 
 // ----------------------------------------------------------------------------

commit 66076afb6f49b8af2eb4d3892ed36c09d2afec88

Fix creating controls on startup in the widgets sample under Mac
This got broken by a8b4753d1d (Reconnect events to the widget after
recreating it in the sample, 2024-02-18) because checking whether the
children list is empty doesn't work correctly in wxOSX (and wxUniv)
because scrollbars may be present in this list even if there are no
other children yet.

Fix this by checking if we have any "real" children explicitly.

Closes #24528.

Closes #24529.

samples/widgets/widgets.cpp
@@ -821,7 +821,18 @@ void WidgetsFrame::OnPageChanged(WidgetsBookCtrlEvent& event)
     // create the pages on demand, otherwise the sample startup is too slow as
     // it creates hundreds of controls
     WidgetsPage *curPage = CurrentPage();
-    if ( curPage->GetChildren().empty() )
+
+    bool hasChildren = false;
+    for ( const auto child : curPage->GetChildren() )
+    {
+        if ( !IsClientAreaChild(child) )
+        {
+            hasChildren = true;
+            break;
+        }
+    }
+
+    if ( !hasChildren )
     {
         wxWindowUpdateLocker noUpdates(curPage);
         curPage->CreateContent();