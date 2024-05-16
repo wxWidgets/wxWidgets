028967daa89d3ea4db1ff62de084251663ace2c8
Paul Cornett <paulcor@users.noreply.github.com>
2024-05-16 12:24:29 -0700

    Use correct theme color for header button text and arrow with GTK3
    Fixes invisible wxHeaderCtrl sort indicator with some themes,
    such as the Ubuntu 18.04 default "Ambiance"
    
    (cherry picked from commit 90bae2062eab1e187bf57581f21adc84cb4f6e29)

--- src/gtk/renderer.cpp
+++ src/gtk/renderer.cpp
@@ -250,6 +250,11 @@ wxRendererGTK::DrawHeaderButton(wxWindow *win,
         gtk_style_context_set_state(sc, stateTypeToFlags[state]);
         gtk_render_background(sc, cr, rect.x, rect.y, rect.width, rect.height);
         gtk_render_frame(sc, cr, rect.x, rect.y, rect.width, rect.height);
+        if (params)
+        {
+            sc.Fg(params->m_arrowColour, stateTypeToFlags[state]);
+            params->m_labelColour = params->m_arrowColour;
+        }
     }
     else
 #endif // GTK >= 3.20
@@ -259,6 +264,13 @@ wxRendererGTK::DrawHeaderButton(wxWindow *win,
         gtk_style_context_set_state(sc, stateTypeToFlags[state]);
         gtk_render_background(sc, cr, rect.x, rect.y, rect.width, rect.height);
         gtk_render_frame(sc, cr, rect.x, rect.y, rect.width, rect.height);
+        if (params)
+        {
+            GdkRGBA rgba;
+            gtk_style_context_get_color(sc, stateTypeToFlags[state], &rgba);
+            params->m_arrowColour = wxColour(rgba);
+            params->m_labelColour = params->m_arrowColour;
+        }
         gtk_style_context_restore(sc);
     }
 #else
