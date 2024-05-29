d100df35df0e5388ab842997a975521d0dafc8c0
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-05-29 17:23:23 +0300

    wxQt: improve best size calculation for wxTextCtrl.
    The size hint provided by Qt is usually too large for being a min size.

--- src/qt/textctrl.cpp
+++ src/qt/textctrl.cpp
@@ -701,7 +701,11 @@ wxTextCtrl::~wxTextCtrl()
 
 wxSize wxTextCtrl::DoGetBestSize() const
 {
-    return wxTextCtrlBase::DoGetBestSize();
+    if (IsSingleLine())
+        return wxQtConvertSize(m_qtEdit->GetHandle()->sizeHint());
+
+    return wxSize(80,
+                  1 + GetCharHeight() * wxMax(wxMin(GetNumberOfLines(), 10), 2));
 }
 
 int wxTextCtrl::GetLineLength(long lineNo) const
