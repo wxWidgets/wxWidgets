wxWidgets/wxWidgets

commit dce09c9b2ac216e2bbcf7e3aebb25f910096b8b8

Avoid crash on startup in webvew sample under Wayland
The workaround for wxWebViewChromium added in f5e2af9a28 (Make
wxWebViewChromium work with wxGTK3 and X11, 2023-09-02) broke
wxWebViewWebKit under Wayland as calling wxYield() inside
wxEVT_WEBVIEW_CREATED handler results in

Error flushing display: Resource temporarily unavailable

error from GTK and immediate application exit.

This is almost certainly a GTK bug and should be fixed there, see
https://gitlab.gnome.org/GNOME/gtk/-/issues/124, but for now make the
workaround even uglier to avoid it.

samples/webview/webview.cpp
@@ -555,8 +555,11 @@ WebFrame::WebFrame(const wxString& url, int flags, wxWebViewWindowFeatures* wind
             wxWebView::GetBackendVersionInfo(backend).ToString());
 
         // Chromium backend can't be used immediately after creation, so wait
-        // until the browser is created before calling GetUserAgent().
-        m_browser->Bind(wxEVT_WEBVIEW_CREATED, [this](wxWebViewEvent& event) {
+        // until the browser is created before calling GetUserAgent(), but we
+        // can't do it unconditionally neither as doing it with WebViewGTK
+        // triggers https://gitlab.gnome.org/GNOME/gtk/-/issues/124 and just
+        // kills the sample.
+        const auto initShow = [this](){
             wxLogMessage("Web view created, user agent is \"%s\"", m_browser->GetUserAgent());
 
             // We need to synchronize this call with GetUserAgent() one, as
@@ -565,9 +568,22 @@ WebFrame::WebFrame(const wxString& url, int flags, wxWebViewWindowFeatures* wind
             // order and we'd get the wrong user agent string back.
             if (!m_browser->AddScriptMessageHandler("wx"))
                 wxLogError("Could not add script message handler");
+        };
 
-            event.Skip();
-        });
+#if wxUSE_WEBVIEW_CHROMIUM
+        if ( backend == wxWebViewBackendChromium )
+        {
+            m_browser->Bind(wxEVT_WEBVIEW_CREATED, [&initShow](wxWebViewEvent& event) {
+                initShow();
+
+                event.Skip();
+            });
+        }
+        else
+#endif // wxUSE_WEBVIEW_CHROMIUM
+        {
+            initShow();
+        }
 
 #ifndef __WXMAC__
         //We register the wxfs:// protocol for testing purposes

commit 9690779f90980ad6b85e1f2ed28cf8b5ae91ed6f

Fix crash on webview sample startup when using CEF backend
Unfortunately the workaround in dce09c9b2a (Avoid crash on startup in
webvew sample under Wayland, 2024-04-22) broke the sample when using CEF
as we can't capture the local lambda by reference.

Fix this simply by making a copy of it instead -- this is not the most
efficient way to do it, but it doesn't really matter here and it's the
simplest one.

samples/webview/webview.cpp
@@ -573,7 +573,7 @@ WebFrame::WebFrame(const wxString& url, int flags, wxWebViewWindowFeatures* wind
 #if wxUSE_WEBVIEW_CHROMIUM
         if ( backend == wxWebViewBackendChromium )
         {
-            m_browser->Bind(wxEVT_WEBVIEW_CREATED, [&initShow](wxWebViewEvent& event) {
+            m_browser->Bind(wxEVT_WEBVIEW_CREATED, [initShow](wxWebViewEvent& event) {
                 initShow();
 
                 event.Skip();