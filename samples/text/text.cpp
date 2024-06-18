964014cc22f57d981e9fca1a5bc8f7bcdd3a807c
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-06-17 20:06:45 -0400

    Make sample text easier to read with dark mode Fix compile issues with sample changes

--- samples/text/text.cpp
+++ samples/text/text.cpp
@@ -326,11 +326,11 @@ public:
     void OnSetRTF(wxCommandEvent& WXUNUSED(event))
     {
 #ifdef __WXOSX__
-        m_panel->m_text->SetRTFValue(R"({\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
-{\colortbl ;\red79\green129\blue189;\red192\green192\blue192;}
+        m_panel->m_textrich->OSXSetRTFValue(R"({\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
+{\colortbl ;\red79\green129\blue189;\red255\green0\blue0;\red192\green192\blue192;}
 {\*\generator Riched20 10.0.22621}\viewkind4\uc1 
 \pard\sa200\sl276\slmult1\cf1\b\i\f0\fs22\lang9 wxWidgets 3.3\cf0\b0\i0\par
-A \highlight2\ul cross-platform \highlight0\ulnone GUI library which uses \ul\b native\ulnone\b0  controls.\par
+\cf2 A \highlight3\ul cross-platform \highlight0\ulnone GUI library which uses \ul\b native\ulnone\b0  controls.\cf0\par
 })");
 #endif
     }
@@ -449,7 +449,7 @@ enum
     TEXT_REPLACE,
     TEXT_SELECT,
     TEXT_SET,
-    TEXT_SET_RFT,
+    TEXT_SET_RTF,
     TEXT_CHANGE,
 
     // log menu
@@ -522,10 +522,11 @@ bool MyApp::OnInit()
     menuText->Append(TEXT_REPLACE, "&Replace characters 4 to 8 with ABC\tCtrl-R");
     menuText->Append(TEXT_SELECT, "&Select characters 4 to 8\tCtrl-I");
     menuText->Append(TEXT_SET, "&Set the first text zone value\tCtrl-E");
-#ifdef __WXOSX__
-    menuText->Append(TEXT_SET_RTF, "&Set the first text zone value from rich text formatted content");
-#endif
     menuText->Append(TEXT_CHANGE, "&Change the first text zone value\tShift-Ctrl-E");
+#ifdef __WXOSX__
+    menuText->AppendSeparator();
+    menuText->Append(TEXT_SET_RTF, "Set the rich text zone value from rich text formatted content");
+#endif
     menuText->AppendSeparator();
     menuText->Append(TEXT_MOVE_ENDTEXT, "Move cursor to the end of &text");
     menuText->Append(TEXT_MOVE_ENDENTRY, "Move cursor to the end of &entry");
@@ -1548,7 +1549,7 @@ wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(TEXT_GET_LINELENGTH,     MyFrame::OnGetLineLength)
 
     EVT_MENU(TEXT_SET,                MyFrame::OnSetText)
-    EVT_MENU(TEXT_SET_RFT,            MyFrame::OnSetRTF)
+    EVT_MENU(TEXT_SET_RTF,            MyFrame::OnSetRTF)
     EVT_MENU(TEXT_CHANGE,             MyFrame::OnChangeText)
 
     EVT_IDLE(MyFrame::OnIdle)
