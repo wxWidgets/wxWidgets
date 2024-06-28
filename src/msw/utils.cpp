7712b0ce785dbcd5bec4112ead95e5f89291d65d
PB <pbfordev@gmail.com>
2024-06-28 16:51:26 +0200

    Update the Windows versions table in wxGetOsVersion() docs
    Update Windows Server information.
    
    Additionally, add a note to wxGetOsDescription() source to
    keep the table in docs in the sync with the code.
    
    See #24653.
    
    (cherry picked from commit 73e28e2b6534b64805bd2a56dab92ca38e171696)

--- src/msw/utils.cpp
+++ src/msw/utils.cpp
@@ -1107,6 +1107,7 @@ static const int FIRST_WINDOWS11_BUILD = 22000;
 
 } // anonymous namespace
 
+// When adding a new version, update also the table in wxGetOsVersion() docs.
 wxString wxGetOsDescription()
 {
     wxString str;
