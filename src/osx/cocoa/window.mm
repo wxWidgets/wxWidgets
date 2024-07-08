wxWidgets/wxWidgets

commit a0b4c5f14bce83be0932763141b96423e7e18cae

Generate correct events for diagonal trackpad scrolling under Mac
Instead of sending a single event in the direction of the greater
change, send two events, for both horizontal and vertical scroll, if
necessary.

Closes #24516.

src/osx/cocoa/window.mm
@@ -791,17 +791,28 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
             wxevent.m_linesPerAction = 1;
             wxevent.m_columnsPerAction = 1;
 
-            if ( fabs(deltaX) > fabs(deltaY) )
+            // Should we impose some minimum threshhold here?
+            if ( deltaY != 0 )
             {
+                wxevent.m_wheelRotation = (int)deltaY;
+            }
+
+            if ( deltaX != 0 )
+            {
+                // We can send one or two events depending on whether we have
+                // delta in just one or both directions.
+                wxMouseEvent* wxeventPtr = &wxevent;
+                if ( deltaY != 0 )
+                {
+                    wxevents.push_back(wxevent);
+                    wxeventPtr = &wxevents.back();
+                }
+
                 // wx conventions for horizontal are inverted from vertical (originating from native msw behavior)
                 // right and up are positive values, left and down are negative values, while on OSX right and down
                 // are negative and left and up are positive.
-                wxevent.m_wheelAxis = wxMOUSE_WHEEL_HORIZONTAL;
-                wxevent.m_wheelRotation = -(int)deltaX;
-            }
-            else
-            {
-                wxevent.m_wheelRotation = (int)deltaY;
+                wxeventPtr->m_wheelAxis = wxMOUSE_WHEEL_HORIZONTAL;
+                wxeventPtr->m_wheelRotation = -(int)deltaX;
             }
 
         }

commit 35040847c208682ec3d0cac559c06329446573fc

Fix crash when handling diagonal scroll events in wxOSX
Commit a0b4c5f14b (Generate correct events for diagonal trackpad
scrolling under Mac, 2024-06-09) introduced a crash due to the use of a
reference which became dangling after resizing the vector, which could
result in a reallocation.

Fix this by replacing the reference with a pointer and resetting this
pointer after resize.

See #24516, #24601.

Closes #24649.

src/osx/cocoa/window.mm
@@ -596,22 +596,25 @@ wxWidgetCocoaImpl::MouseEvents
 wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
 {
     MouseEvents wxevents(1);
-    auto& wxevent = wxevents.front();
+
+    // This pointer may be invalidated by resizing the vector and has to be
+    // reset in this case.
+    wxMouseEvent* wxevent = &wxevents.front();
 
     int eventType = [nsEvent type];
     UInt32 modifiers = [nsEvent modifierFlags] ;
     
-    SetupCoordinates(wxevent.m_x, wxevent.m_y, nsEvent);
+    SetupCoordinates(wxevent->m_x, wxevent->m_y, nsEvent);
 
     // these parameters are not given for all events
     UInt32 button = [nsEvent buttonNumber];
     UInt32 clickCount = 0;
 
-    wxevent.m_shiftDown = modifiers & NSShiftKeyMask;
-    wxevent.m_rawControlDown = modifiers & NSControlKeyMask;
-    wxevent.m_altDown = modifiers & NSAlternateKeyMask;
-    wxevent.m_controlDown = modifiers & NSCommandKeyMask;
-    wxevent.SetTimestamp( (int)([nsEvent timestamp] * 1000) ) ;
+    wxevent->m_shiftDown = modifiers & NSShiftKeyMask;
+    wxevent->m_rawControlDown = modifiers & NSControlKeyMask;
+    wxevent->m_altDown = modifiers & NSAlternateKeyMask;
+    wxevent->m_controlDown = modifiers & NSCommandKeyMask;
+    wxevent->SetTimestamp( (int)([nsEvent timestamp] * 1000) ) ;
 
     UInt32 mouseChord = 0;
 
@@ -686,21 +689,21 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
         mouseChord = ((mouseChord & ~1U) | 2U);
 
     if(mouseChord & 1U)
-                wxevent.m_leftDown = true ;
+                wxevent->m_leftDown = true ;
     if(mouseChord & 2U)
-                wxevent.m_rightDown = true ;
+                wxevent->m_rightDown = true ;
     if(mouseChord & 4U)
     {
         switch ( button )
         {
             default:
-                wxevent.m_middleDown = true ;
+                wxevent->m_middleDown = true ;
                 break;
             case 3:
-                wxevent.m_aux1Down = true ;
+                wxevent->m_aux1Down = true ;
                 break;
             case 4:
-                wxevent.m_aux2Down = true ;
+                wxevent->m_aux2Down = true ;
                 break;
         }
     }
@@ -714,23 +717,23 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
             switch ( button )
             {
                 case 0 :
-                    wxevent.SetEventType( clickCount > 1 ? wxEVT_LEFT_DCLICK : wxEVT_LEFT_DOWN )  ;
+                    wxevent->SetEventType( clickCount > 1 ? wxEVT_LEFT_DCLICK : wxEVT_LEFT_DOWN )  ;
                     break ;
 
                 case 1 :
-                    wxevent.SetEventType( clickCount > 1 ? wxEVT_RIGHT_DCLICK : wxEVT_RIGHT_DOWN ) ;
+                    wxevent->SetEventType( clickCount > 1 ? wxEVT_RIGHT_DCLICK : wxEVT_RIGHT_DOWN ) ;
                     break ;
 
                 case 2 :
-                    wxevent.SetEventType( clickCount > 1 ? wxEVT_MIDDLE_DCLICK : wxEVT_MIDDLE_DOWN ) ;
+                    wxevent->SetEventType( clickCount > 1 ? wxEVT_MIDDLE_DCLICK : wxEVT_MIDDLE_DOWN ) ;
                     break ;
 
                 case 3 :
-                    wxevent.SetEventType( clickCount > 1 ? wxEVT_AUX1_DCLICK : wxEVT_AUX1_DOWN ) ;
+                    wxevent->SetEventType( clickCount > 1 ? wxEVT_AUX1_DCLICK : wxEVT_AUX1_DOWN ) ;
                     break ;
 
                 case 4 :
-                    wxevent.SetEventType( clickCount > 1 ? wxEVT_AUX2_DCLICK : wxEVT_AUX2_DOWN ) ;
+                    wxevent->SetEventType( clickCount > 1 ? wxEVT_AUX2_DCLICK : wxEVT_AUX2_DOWN ) ;
                     break ;
 
                 default:
@@ -744,23 +747,23 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
             switch ( button )
             {
                 case 0 :
-                    wxevent.SetEventType( wxEVT_LEFT_UP )  ;
+                    wxevent->SetEventType( wxEVT_LEFT_UP )  ;
                     break ;
 
                 case 1 :
-                    wxevent.SetEventType( wxEVT_RIGHT_UP ) ;
+                    wxevent->SetEventType( wxEVT_RIGHT_UP ) ;
                     break ;
 
                 case 2 :
-                    wxevent.SetEventType( wxEVT_MIDDLE_UP ) ;
+                    wxevent->SetEventType( wxEVT_MIDDLE_UP ) ;
                     break ;
 
                 case 3 :
-                    wxevent.SetEventType( wxEVT_AUX1_UP ) ;
+                    wxevent->SetEventType( wxEVT_AUX1_UP ) ;
                     break ;
 
                 case 4 :
-                    wxevent.SetEventType( wxEVT_AUX2_UP ) ;
+                    wxevent->SetEventType( wxEVT_AUX2_UP ) ;
                     break ;
 
                 default:
@@ -773,7 +776,7 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
             float deltaX = 0.0;
             float deltaY = 0.0;
 
-            wxevent.SetEventType( wxEVT_MOUSEWHEEL ) ;
+            wxevent->SetEventType( wxEVT_MOUSEWHEEL ) ;
 
             if ( [nsEvent hasPreciseScrollingDeltas] )
             {
@@ -786,25 +789,29 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
                 deltaY = [nsEvent scrollingDeltaY] * 10;
             }
             
-            wxevent.m_wheelDelta = 10;
-            wxevent.m_wheelInverted = [nsEvent isDirectionInvertedFromDevice];
-            wxevent.m_linesPerAction = 1;
-            wxevent.m_columnsPerAction = 1;
+            wxevent->m_wheelDelta = 10;
+            wxevent->m_wheelInverted = [nsEvent isDirectionInvertedFromDevice];
+            wxevent->m_linesPerAction = 1;
+            wxevent->m_columnsPerAction = 1;
 
             // Should we impose some minimum threshold here?
             if ( deltaY != 0 )
             {
-                wxevent.m_wheelRotation = (int)deltaY;
+                wxevent->m_wheelRotation = (int)deltaY;
             }
 
             if ( deltaX != 0 )
             {
                 // We can send one or two events depending on whether we have
                 // delta in just one or both directions.
-                wxMouseEvent* wxeventPtr = &wxevent;
+                wxMouseEvent* wxeventPtr = wxevent;
                 if ( deltaY != 0 )
                 {
-                    wxevents.push_back(wxevent);
+                    wxevents.push_back(*wxevent);
+
+                    // The push above could have invalidated the pointer, so
+                    // reset it to still point to the first element.
+                    wxevent = &wxevents.front();
                     wxeventPtr = &wxevents.back();
                 }
 
@@ -819,33 +826,33 @@ wxWidgetCocoaImpl::TranslateMouseEvent( NSEvent * nsEvent )
         break ;
 
         case NSMouseEntered :
-            wxevent.SetEventType( wxEVT_ENTER_WINDOW ) ;
+            wxevent->SetEventType( wxEVT_ENTER_WINDOW ) ;
             break;
         case NSMouseExited :
-            wxevent.SetEventType( wxEVT_LEAVE_WINDOW ) ;
+            wxevent->SetEventType( wxEVT_LEAVE_WINDOW ) ;
             break;
         case NSLeftMouseDragged :
         case NSRightMouseDragged :
         case NSOtherMouseDragged :
         case NSMouseMoved :
-            wxevent.SetEventType( wxEVT_MOTION ) ;
+            wxevent->SetEventType( wxEVT_MOTION ) ;
             break;
         
         case NSEventTypeMagnify:
-            wxevent.SetEventType( wxEVT_MAGNIFY );
-            wxevent.m_magnification = [nsEvent magnification];
+            wxevent->SetEventType( wxEVT_MAGNIFY );
+            wxevent->m_magnification = [nsEvent magnification];
             break;
             
         default :
             break ;
     }
 
-    wxevent.m_clickCount = clickCount;
+    wxevent->m_clickCount = clickCount;
     wxWindowMac* peer = GetWXPeer();
     if ( peer )
     {
-        wxevent.SetEventObject(peer);
-        wxevent.SetId(peer->GetId()) ;
+        wxevent->SetEventObject(peer);
+        wxevent->SetId(peer->GetId()) ;
     }
 
     return wxevents;