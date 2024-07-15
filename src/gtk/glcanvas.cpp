4a13663e447d074fb7d9c52e689a6132fa487db7
Vadim Zeitlin <vadim@wxwidgets.org>
2024-07-16 01:37:25 +0200

    Suppress harmless -Wundef in Wayland headers
    Somehow, __STDC_VERSION__ is not defined in Cirrus CI builds under
    FreeBSD, resulting in -Wundef when testing it in wayland-util.h, so
    suppress this warning to let the CI job pass nevertheless.
    
    (cherry picked from commit aba1c66575b0106a473ad6890ffa857685469535)

--- src/gtk/glcanvas.cpp
+++ src/gtk/glcanvas.cpp
@@ -17,12 +17,19 @@
 
 #include "wx/gtk/private/wrapgtk.h"
 #include "wx/gtk/private/backend.h"
+
+// Under some platforms, Wayland headers included from gdkwayland.h result in
+// -Wundef due to __STDC_VERSION__ used there being undefined, suppress this.
+wxGCC_WARNING_SUPPRESS(undef)
+
 #ifdef GDK_WINDOWING_WAYLAND
 #include <gdk/gdkwayland.h>
 #endif
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
 #endif
+
+wxGCC_WARNING_RESTORE(undef)
 
 #ifdef __WXGTK3__
 extern "C" {
