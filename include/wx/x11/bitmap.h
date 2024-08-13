3e8a84ee8673f55e91cffb85df71e6f23a04f515
Vadim Zeitlin <vadim@wxwidgets.org>
2024-08-14 00:55:36 +0200

    Disallow overriding wxBitmap::Create() outside of wxWidgets
    This doesn't really make much sense, this function is only virtual in
    the first place to force wxBitmap (in all ports except for wxMSW) to
    implement wxBitmapBase pure virtual functions, but it's implemented only
    by wxWidgets itself and shouldn't be overridden again, so declare it as
    final in all the ports overriding it and make it non-virtual in wxMSW.
    
    Also, none of the other Create() functions in wxWindow-derived classes
    is virtual, so this improves consistency as well.
    
    Finally, this avoids clang-analyzer-optin.cplusplus.VirtualCall warning
    from clang-tidy due to calling a virtual function from wxBitmap ctor,
    which is the right thing to do only because we really want to call
    wxBitmap's own version and not anything defined in a derived class.
    
    Closes #24759.

--- include/wx/x11/bitmap.h
+++ include/wx/x11/bitmap.h
@@ -65,8 +65,8 @@ public:
 
     static void InitStandardHandlers();
 
-    bool Create(int width, int height, int depth = wxBITMAP_SCREEN_DEPTH);
-    bool Create(const wxSize& sz, int depth = wxBITMAP_SCREEN_DEPTH)
+    bool Create(int width, int height, int depth = wxBITMAP_SCREEN_DEPTH) final;
+    bool Create(const wxSize& sz, int depth = wxBITMAP_SCREEN_DEPTH) final
         { return Create(sz.GetWidth(), sz.GetHeight(), depth); }
     bool Create(int width, int height, const wxDC& WXUNUSED(dc))
         { return Create(width,height); }
