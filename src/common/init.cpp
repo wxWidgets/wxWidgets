ad85670b3683abf619577b869e58922a75403688
Lauri Nurmi <lanurmi@iki.fi>
2024-09-08 22:27:13 +0300

    Extract the actual code from wxTRY blocks into a separate functions
    Leaving wxTRY blocks with only a few lines of code as-is, but refactoring
    those with non-trivial amount of codelines into functions.

--- src/common/init.cpp
+++ src/common/init.cpp
@@ -528,6 +528,28 @@ void wxAddEntryHook(wxEntryHook hook)
     #define wxEntryReal wxEntry
 #endif // !__WINDOWS__
 
+static int DoEntryReal()
+{
+    // app initialization
+    if ( !wxTheApp->CallOnInit() )
+    {
+        // don't call OnExit() if OnInit() failed
+        return wxTheApp->GetErrorExitCode();
+    }
+
+    // ensure that OnExit() is called if OnInit() had succeeded
+    class CallOnExit
+    {
+    public:
+        ~CallOnExit() { wxTheApp->OnExit(); }
+    } callOnExit;
+
+    WX_SUPPRESS_UNUSED_WARN(callOnExit);
+
+    // app execution
+    return wxTheApp->OnRun();
+}
+
 int wxEntryReal(int& argc, wxChar **argv)
 {
     // Do this before trying the hooks as they may use command line arguments.
@@ -555,24 +577,7 @@ int wxEntryReal(int& argc, wxChar **argv)
 
     wxTRY
     {
-        // app initialization
-        if ( !wxTheApp->CallOnInit() )
-        {
-            // don't call OnExit() if OnInit() failed
-            return wxTheApp->GetErrorExitCode();
-        }
-
-        // ensure that OnExit() is called if OnInit() had succeeded
-        class CallOnExit
-        {
-        public:
-            ~CallOnExit() { wxTheApp->OnExit(); }
-        } callOnExit;
-
-        WX_SUPPRESS_UNUSED_WARN(callOnExit);
-
-        // app execution
-        return wxTheApp->OnRun();
+        return DoEntryReal();
     }
     wxCATCH_ALL(
         wxTheApp->OnUnhandledException();
