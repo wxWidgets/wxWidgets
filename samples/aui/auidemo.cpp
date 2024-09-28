40601e2c8c8a8c94e35fa6404bbc3e8e03820838
Maarten Bent <MaartenBent@users.noreply.github.com>
2024-09-28 19:34:50 +0200

    Improve AUI sample settings pane DPI awareness
    Most wxBitmapButtons were missing the FromDIP size.
    And use a wxBitmapBundle with bitmap sizes for scales from 100% to 400%.

--- samples/aui/auidemo.cpp
+++ samples/aui/auidemo.cpp
@@ -298,13 +298,15 @@ public:
 
         //vert->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
 
+        wxSize const elementSize = FromDIP(wxSize(180, 20));
+
         wxBoxSizer* s1 = new wxBoxSizer(wxHORIZONTAL);
         m_border_size = new wxSpinCtrl(this, ID_PaneBorderSize, wxString::Format("%d", frame->GetDockArt()->GetMetric(wxAUI_DOCKART_PANE_BORDER_SIZE)), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, FromDIP(100), frame->GetDockArt()->GetMetric(wxAUI_DOCKART_PANE_BORDER_SIZE));
         s1->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s1->Add(new wxStaticText(this, wxID_ANY, "Pane Border Size:"));
         s1->Add(m_border_size);
         s1->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s1->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s1->SetItemMinSize((size_t)1, elementSize);
         //vert->Add(s1, 0, wxEXPAND | wxLEFT | wxBOTTOM, FromDIP(5));
 
         wxBoxSizer* s2 = new wxBoxSizer(wxHORIZONTAL);
@@ -313,7 +315,7 @@ public:
         s2->Add(new wxStaticText(this, wxID_ANY, "Sash Size:"));
         s2->Add(m_sash_size);
         s2->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s2->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s2->SetItemMinSize((size_t)1, elementSize);
         //vert->Add(s2, 0, wxEXPAND | wxLEFT | wxBOTTOM, FromDIP(5));
 
         wxBoxSizer* s3 = new wxBoxSizer(wxHORIZONTAL);
@@ -322,93 +324,94 @@ public:
         s3->Add(new wxStaticText(this, wxID_ANY, "Caption Size:"));
         s3->Add(m_caption_size);
         s3->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s3->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s3->SetItemMinSize((size_t)1, elementSize);
         //vert->Add(s3, 0, wxEXPAND | wxLEFT | wxBOTTOM, FromDIP(5));
 
         //vert->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
 
 
-        wxBitmap b = CreateColorBitmap(*wxBLACK);
+        wxBitmapBundle const b = CreateColorBitmap(*wxBLACK);
+        wxSize const bitmapSize = FromDIP(wxSize(50, 25));
 
         wxBoxSizer* s4 = new wxBoxSizer(wxHORIZONTAL);
-        m_background_color = new wxBitmapButton(this, ID_BackgroundColor, b, wxDefaultPosition, FromDIP(wxSize(50,25)));
+        m_background_color = new wxBitmapButton(this, ID_BackgroundColor, b, wxDefaultPosition, bitmapSize);
         s4->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s4->Add(new wxStaticText(this, wxID_ANY, "Background Color:"));
         s4->Add(m_background_color);
         s4->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s4->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s4->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s5 = new wxBoxSizer(wxHORIZONTAL);
-        m_sash_color = new wxBitmapButton(this, ID_SashColor, b, wxDefaultPosition, wxSize(50,25));
+        m_sash_color = new wxBitmapButton(this, ID_SashColor, b, wxDefaultPosition, bitmapSize);
         s5->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s5->Add(new wxStaticText(this, wxID_ANY, "Sash Color:"));
         s5->Add(m_sash_color);
         s5->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s5->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s5->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s6 = new wxBoxSizer(wxHORIZONTAL);
-        m_inactive_caption_color = new wxBitmapButton(this, ID_InactiveCaptionColor, b, wxDefaultPosition, wxSize(50,25));
+        m_inactive_caption_color = new wxBitmapButton(this, ID_InactiveCaptionColor, b, wxDefaultPosition, bitmapSize);
         s6->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s6->Add(new wxStaticText(this, wxID_ANY, "Normal Caption:"));
         s6->Add(m_inactive_caption_color);
         s6->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s6->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s6->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s7 = new wxBoxSizer(wxHORIZONTAL);
-        m_inactive_caption_gradient_color = new wxBitmapButton(this, ID_InactiveCaptionGradientColor, b, wxDefaultPosition, wxSize(50,25));
+        m_inactive_caption_gradient_color = new wxBitmapButton(this, ID_InactiveCaptionGradientColor, b, wxDefaultPosition, bitmapSize);
         s7->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s7->Add(new wxStaticText(this, wxID_ANY, "Normal Caption Gradient:"));
         s7->Add(m_inactive_caption_gradient_color);
         s7->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s7->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s7->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s8 = new wxBoxSizer(wxHORIZONTAL);
-        m_inactive_caption_text_color = new wxBitmapButton(this, ID_InactiveCaptionTextColor, b, wxDefaultPosition, wxSize(50,25));
+        m_inactive_caption_text_color = new wxBitmapButton(this, ID_InactiveCaptionTextColor, b, wxDefaultPosition, bitmapSize);
         s8->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s8->Add(new wxStaticText(this, wxID_ANY, "Normal Caption Text:"));
         s8->Add(m_inactive_caption_text_color);
         s8->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s8->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s8->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s9 = new wxBoxSizer(wxHORIZONTAL);
-        m_active_caption_color = new wxBitmapButton(this, ID_ActiveCaptionColor, b, wxDefaultPosition, wxSize(50,25));
+        m_active_caption_color = new wxBitmapButton(this, ID_ActiveCaptionColor, b, wxDefaultPosition, bitmapSize);
         s9->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s9->Add(new wxStaticText(this, wxID_ANY, "Active Caption:"));
         s9->Add(m_active_caption_color);
         s9->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s9->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s9->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s10 = new wxBoxSizer(wxHORIZONTAL);
-        m_active_caption_gradient_color = new wxBitmapButton(this, ID_ActiveCaptionGradientColor, b, wxDefaultPosition, wxSize(50,25));
+        m_active_caption_gradient_color = new wxBitmapButton(this, ID_ActiveCaptionGradientColor, b, wxDefaultPosition, bitmapSize);
         s10->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s10->Add(new wxStaticText(this, wxID_ANY, "Active Caption Gradient:"));
         s10->Add(m_active_caption_gradient_color);
         s10->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s10->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s10->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s11 = new wxBoxSizer(wxHORIZONTAL);
-        m_active_caption_text_color = new wxBitmapButton(this, ID_ActiveCaptionTextColor, b, wxDefaultPosition, wxSize(50,25));
+        m_active_caption_text_color = new wxBitmapButton(this, ID_ActiveCaptionTextColor, b, wxDefaultPosition, bitmapSize);
         s11->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s11->Add(new wxStaticText(this, wxID_ANY, "Active Caption Text:"));
         s11->Add(m_active_caption_text_color);
         s11->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s11->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s11->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s12 = new wxBoxSizer(wxHORIZONTAL);
-        m_border_color = new wxBitmapButton(this, ID_BorderColor, b, wxDefaultPosition, wxSize(50,25));
+        m_border_color = new wxBitmapButton(this, ID_BorderColor, b, wxDefaultPosition, bitmapSize);
         s12->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s12->Add(new wxStaticText(this, wxID_ANY, "Border Color:"));
         s12->Add(m_border_color);
         s12->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s12->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s12->SetItemMinSize((size_t)1, elementSize);
 
         wxBoxSizer* s13 = new wxBoxSizer(wxHORIZONTAL);
-        m_gripper_color = new wxBitmapButton(this, ID_GripperColor, b, wxDefaultPosition, wxSize(50,25));
+        m_gripper_color = new wxBitmapButton(this, ID_GripperColor, b, wxDefaultPosition, bitmapSize);
         s13->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
         s13->Add(new wxStaticText(this, wxID_ANY, "Gripper Color:"));
         s13->Add(m_gripper_color);
         s13->Add(FromDIP(1), FromDIP(1), 1, wxEXPAND);
-        s13->SetItemMinSize((size_t)1, FromDIP(wxSize(180, 20)));
+        s13->SetItemMinSize((size_t)1, elementSize);
 
         wxGridSizer* grid_sizer = new wxGridSizer(2);
         grid_sizer->SetHGap(FromDIP(5));
@@ -434,20 +437,26 @@ public:
 
 private:
 
-    wxBitmap CreateColorBitmap(const wxColour& c)
+    wxBitmapBundle CreateColorBitmap(const wxColour& c)
     {
-        wxImage image;
-        wxSize size = FromDIP(wxSize(25, 14));
-        image.Create(size);
-        for (int x = 0; x < size.x; ++x)
-            for (int y = 0; y < size.y; ++y)
-            {
-                wxColour pixcol = c;
-                if (x == 0 || x == size.x || y == 0 || y == size.y)
-                    pixcol = *wxBLACK;
-                image.SetRGB(x, y, pixcol.Red(), pixcol.Green(), pixcol.Blue());
-            }
-        return wxBitmap(image);
+        wxVector<wxBitmap> bitmaps;
+
+        for (int i = 100; i <= 400; i += 25)
+        {
+            wxSize size(25 * i / 100, 14 * i / 100);
+            wxImage image(size);
+            for (int x = 0; x < size.x; ++x)
+                for (int y = 0; y < size.y; ++y)
+                {
+                    wxColour pixcol = c;
+                    if (x == 0 || x == size.x || y == 0 || y == size.y)
+                        pixcol = *wxBLACK;
+                    image.SetRGB(x, y, pixcol.Red(), pixcol.Green(), pixcol.Blue());
+                }
+            bitmaps.push_back(wxBitmap(image));
+        }
+
+        return wxBitmapBundle::FromBitmaps(bitmaps);
     }
 
     void UpdateColors()
