wxWidgets/wxWidgets

commit 76b3738d137773348815a82da65eeddbe07c2821

Stop using wxAutoBufferedPaintDC in wxAuiToolBar
With all major platforms implementing double-buffering natively, this
is not necessary any more and not using this class allows us not to set
bacgkround style to wxBG_STYLE_PAINT and then not draw the background
bitmap ourselves but let wxCustomBackgroundWindow do it.

src/aui/auibar.cpp
@@ -895,7 +895,6 @@ bool wxAuiToolBar::Create(wxWindow* parent,
     SetExtraStyle(wxWS_EX_PROCESS_IDLE);
     if (style & wxAUI_TB_HORZ_LAYOUT)
         SetToolTextOrientation(wxAUI_TBTOOL_TEXT_RIGHT);
-    SetBackgroundStyle(wxBG_STYLE_PAINT);
 
     return true;
 }
@@ -2445,13 +2444,11 @@ void wxAuiToolBar::UpdateBackgroundBitmap(const wxSize& size)
 
 void wxAuiToolBar::OnPaint(wxPaintEvent& WXUNUSED(evt))
 {
-    wxAutoBufferedPaintDC dc(this);
+    wxPaintDC dc(this);
     wxRect cli_rect(wxPoint(0,0), GetClientSize());
 
 
     bool horizontal = m_orientation == wxHORIZONTAL;
-
-    dc.DrawBitmap(m_backgroundBitmap, 0, 0);
 
     int gripperSize = m_art->GetElementSize(wxAUI_TBART_GRIPPER_SIZE);
     int overflowSize = m_art->GetElementSize(wxAUI_TBART_OVERFLOW_SIZE);

commit 386e60c246f7bf8fcf44863ff138c5b1a869f13a

Fix refreshing wxAuiToolBar after stopping double buffering it
The commit 76b3738 (Stop using wxAutoBufferedPaintDC in wxAuiToolBar,
2024-08-23) broke wxAuiToolBar refreshing as the background was not
drawn in the wxEVT_PAINT handler any more but in wxEVT_ERASE_BACKGROUND
(or its moral equivalent in wxMSW), which wasn't generated because the
various functions changing wxAuiToolBar called Refresh(false), i.e.
explicitly prevented the background from being repainted.

Remove this "false" argument from them to repaint it too and fix the
toolbar refresh.

Closes #24849.

src/aui/auibar.cpp
@@ -1372,7 +1372,7 @@ void wxAuiToolBar::SetToolSticky(int tool_id, bool sticky)
 
     item->m_sticky = sticky;
 
-    Refresh(false);
+    Refresh();
     Update();
 }
 
@@ -1463,7 +1463,7 @@ void wxAuiToolBar::SetGripperVisible(bool visible)
     else
         m_windowStyle &= ~wxAUI_TB_GRIPPER;
     Realize();
-    Refresh(false);
+    Refresh();
 }
 
 
@@ -1479,7 +1479,7 @@ void wxAuiToolBar::SetOverflowVisible(bool visible)
         m_windowStyle |= wxAUI_TB_OVERFLOW;
     else
         m_windowStyle &= ~wxAUI_TB_OVERFLOW;
-    Refresh(false);
+    Refresh();
 }
 
 bool wxAuiToolBar::SetFont(const wxFont& font)
@@ -1518,7 +1518,7 @@ void wxAuiToolBar::SetHoverItem(wxAuiToolBarItem* pitem)
 
     if (former_hover != pitem)
     {
-        Refresh(false);
+        Refresh();
         Update();
     }
 }
@@ -1544,7 +1544,7 @@ void wxAuiToolBar::SetPressedItem(wxAuiToolBarItem* pitem)
 
     if (former_item != pitem)
     {
-        Refresh(false);
+        Refresh();
         Update();
     }
 }
@@ -1578,7 +1578,7 @@ void wxAuiToolBar::RefreshOverflowState()
     if (overflow_state != m_overflowState)
     {
         m_overflowState = overflow_state;
-        Refresh(false);
+        Refresh();
         Update();
     }
 
@@ -1936,7 +1936,7 @@ bool wxAuiToolBar::Realize()
         m_sizer->SetDimension(0, 0, curSize.x, curSize.y);
     }
 
-    Refresh(false);
+    Refresh();
     return true;
 }
 
@@ -2279,7 +2279,7 @@ void wxAuiToolBar::DoIdleUpdate()
 
     if (need_refresh)
     {
-        Refresh(false);
+        Refresh();
     }
 }
 
@@ -2324,7 +2324,7 @@ void wxAuiToolBar::OnSize(wxSizeEvent& WXUNUSED(evt))
     // We need to update the bitmap if the size has changed.
     UpdateBackgroundBitmap(wxSize(x, y));
 
-    Refresh(false);
+    Refresh();
 
     // idle events aren't sent while user is resizing frame (why?),
     // but resizing toolbar here causes havoc,
@@ -2607,7 +2607,7 @@ void wxAuiToolBar::OnLeftDown(wxMouseEvent& evt)
 
                 int res = m_art->ShowDropDown(this, overflow_items);
                 m_overflowState = 0;
-                Refresh(false);
+                Refresh();
                 if (res != -1)
                 {
                     wxCommandEvent event(wxEVT_MENU, res);
@@ -2710,7 +2710,7 @@ void wxAuiToolBar::OnLeftUp(wxMouseEvent& evt)
                 ToggleTool(m_actionItem->m_toolId, toggle);
 
                 // repaint immediately
-                Refresh(false);
+                Refresh();
                 Update();
 
                 e.SetInt(toggle);