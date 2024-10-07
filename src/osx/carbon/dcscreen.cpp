fd0cb4b9fe45134fed84e7bba3a2f93d20d32589
Vadim Zeitlin <vadim@wxwidgets.org>
2024-10-07 15:48:52 +0200

    Remove obsolete wxScreenDC::GetAsBitmap() Carbon implementation
    This doesn't even compile any more with macOS 15 SDK and almost
    certainly doesn't work there neither, so simply remove it -- this means
    that it's not going to work even under previous macOS versions where it
    work before neither, but it doesn't seem worth to maintain this code
    just for the old system versions.
    
    Closes #24724.

--- src/osx/carbon/dcscreen.cpp
+++ src/osx/carbon/dcscreen.cpp
@@ -17,10 +17,6 @@
 
 wxIMPLEMENT_ABSTRACT_CLASS(wxScreenDCImpl, wxWindowDCImpl);
 
-// TODO : for the Screenshot use case, which doesn't work in Quartz
-// we should do a GetAsBitmap using something like
-// http://www.cocoabuilder.com/archive/message/cocoa/2005/8/13/144256
-
 // Create a DC representing the whole screen
 wxScreenDCImpl::wxScreenDCImpl( wxDC *owner ) :
    wxWindowDCImpl( owner )
@@ -41,45 +37,13 @@ wxScreenDCImpl::~wxScreenDCImpl()
     wxDELETE(m_graphicContext);
 }
 
-#if wxOSX_USE_IPHONE
-// Apple has allowed usage of this API as of 15th Dec 2009w
-extern CGImageRef UIGetScreenImage();
-#endif
-
-// TODO Switch to CGWindowListCreateImage for 10.5 and above
-
 wxBitmap wxScreenDCImpl::DoGetAsBitmap(const wxRect *subrect) const
 {
     wxRect rect = subrect ? *subrect : wxRect(0, 0, m_width, m_height);
 
     wxBitmap bmp(rect.GetSize(), 32);
 
-#if !wxOSX_USE_IPHONE
-    CGRect srcRect = CGRectMake(rect.x, rect.y, rect.width, rect.height);
+    // TODO: Implement this using ScreenCaptureKit.
 
-    CGContextRef context = (CGContextRef)bmp.GetHBITMAP();
-
-    CGContextSaveGState(context);
-
-    CGContextTranslateCTM( context, 0,  m_height );
-    CGContextScaleCTM( context, 1, -1 );
-
-    if ( subrect )
-        srcRect = CGRectOffset( srcRect, -subrect->x, -subrect->y ) ;
-
-    CGImageRef image = nullptr;
-    
-    image = CGDisplayCreateImage(kCGDirectMainDisplay);
-
-    wxASSERT_MSG(image, wxT("wxScreenDC::GetAsBitmap - unable to get screenshot."));
-
-    CGContextDrawImage(context, srcRect, image);
-
-    CGImageRelease(image);
-
-    CGContextRestoreGState(context);
-#else
-    // TODO implement using UIGetScreenImage, CGImageCreateWithImageInRect, CGContextDrawImage
-#endif
     return bmp;
 }
