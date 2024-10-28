802da6554560b35d104c2ac97d07ba361ff77d81
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-10-28 10:27:10 -0400

    Use wxGetTranslation instead of wxGETTEXT_IN_CONTEXT

--- src/html/htmprint.cpp
+++ src/html/htmprint.cpp
@@ -709,8 +709,8 @@ bool wxHtmlEasyPrinting::DoPreview(wxHtmlPrintout *printout1, wxHtmlPrintout *pr
     }
 
     wxPreviewFrame *frame = new wxPreviewFrame(preview, m_ParentWindow,
-                                               wxGETTEXT_IN_CONTEXT("HTML easy printer preview title (%s may be the name of the document)",
-                                                                    wxString::Format(_("%s Preview"), m_Name)),
+        wxGetTranslation(wxString::Format(_("%s Preview"), m_Name), wxString(),
+            "HTML easy printer preview title (%s may be the name of the document)"),
                                                wxDefaultPosition,
                                                wxWindow::FromDIP(wxSize(650, 500), m_ParentWindow));
     frame->Centre(wxBOTH);
