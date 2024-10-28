f89cbe6eab0d12261c69b5aec176b5a2a019249a
ali kettab <aliket1435@gmail.com>
2024-10-28 07:21:34 +0100

    wxQt: translate QPainter of wxClientDC into window client area.
    Because Qt considers the menubar and toolbar parts of the client area
    and we need to apply the same fix done for wxPaintDC in this commit
    9652958 (translate QPainter into window client area.) to wxClientDC too.
    
    Closes #24921.

--- src/qt/dcclient.cpp
+++ src/qt/dcclient.cpp
@@ -115,6 +115,8 @@ wxClientDCImpl::wxClientDCImpl( wxDC *owner, wxWindow *win )
 {
      if ( m_ok )
      {
+        m_qtPainter->translate( wxQtConvertPoint(win->GetClientAreaOrigin()) );
+
         m_qtPainter->setClipRect( wxQtConvertRect(win->GetClientRect()),
                                   m_clipping ? Qt::IntersectClip : Qt::ReplaceClip );
     }
