e37adaba307582749c77e398a3c8ec2c40e55c49
Vadim Zeitlin <vadim@wxwidgets.org>
2024-11-16 14:48:01 +0100

    Define wxCharNN as typedefs for charNN_t, not wxUintNN
    This makes more sense, as they're supposed to be character types, not
    integer ones, and fixes the build with libc++ 19 which doesn't provide
    std::char_traits specializations for types other than char, wchar_t and
    charNN_t, so that std::basic_string<wxUint32> can't be compiled with it
    any more without specializing std::char_traits on our own which we'd
    rather avoid.
    
    See #24959.
    
    Closes #24958.

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -1013,20 +1013,23 @@ typedef double wxDouble;
 
 /* Define wxChar16 and wxChar32                                              */
 
+#ifdef __cplusplus
+
 #if SIZEOF_WCHAR_T == 2
     #define wxWCHAR_T_IS_WXCHAR16
     typedef wchar_t wxChar16;
 #else
-    typedef wxUint16 wxChar16;
+    typedef char16_t wxChar16;
 #endif
 
 #if SIZEOF_WCHAR_T == 4
     #define wxWCHAR_T_IS_WXCHAR32
     typedef wchar_t wxChar32;
 #else
-    typedef wxUint32 wxChar32;
+    typedef char32_t wxChar32;
 #endif
 
+#endif /* __cplusplus */
 
 /*
     Helper macro expanding into the given "m" macro invoked with each of the
