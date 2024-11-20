082b04eb6e487b3727a56e9377408510eab19672
Reza Karimi <rezakarimi66@gmail.com>
2024-11-20 17:34:27 +0100

    Fix cursor key behaviour in owner drawn combobox popup
    Remove the code for working around the warning in SetFocus() added back
    in c0c05e7 (Supress SetFocus() warning in wxVListBoxComboPopup,
    2010-05-09) because this warning, whatever was meant by it, doesn't seem
    to happen any more, but not giving focus to the underlying wxVListBox
    unsurprisingly prevents it from handling navigation keys when it is
    shown.
    
    Closes #23776.

--- src/generic/odcombo.cpp
+++ src/generic/odcombo.cpp
@@ -95,18 +95,6 @@ wxVListBoxComboPopup::~wxVListBoxComboPopup()
     Clear();
 }
 
-void wxVListBoxComboPopup::SetFocus()
-{
-    // Suppress SetFocus() warning by simply not calling it. This combo popup
-    // has already been designed with the assumption that SetFocus() may not
-    // do anything useful, so it really doesn't need to be called.
-#ifdef __WXMSW__
-    //
-#else
-    wxVListBox::SetFocus();
-#endif
-}
-
 void wxVListBoxComboPopup::OnDPIChanged(wxDPIChangedEvent& event)
 {
     m_itemHeight = m_combo->GetCharHeight();
