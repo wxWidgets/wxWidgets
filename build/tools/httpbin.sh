946701bb1ee41da19cd6193419286dc8aeb5c09b
Vadim Zeitlin <vadim@wxwidgets.org>
2024-11-27 02:15:08 +0100

    Stop using checkout and other actions for Ubuntu 18.04 CI builds
    Even actions/checkout@v3 doesn't work in the container any longer,
    because none of JS actions can run on Ubuntu 18.04 which doesn't have a
    new enough glibc version any longer, so perform the checkout manually.
    
    Disable ccache for the builds using this OS too for the same reason:
    even though we could install ccache itself manually too, we don't have
    any easy way to save and restore its cache without this action.
    
    Also disable wxWebRequest tests using httpbin which can't be easily
    installed.
    
    The diff to httpbin.sh is best viewed ignoring whitespace-only changes.
    
    (cherry picked from commit 9d1c5b108ce96cc61604a3d8b49a171b8a293fed)

--- build/tools/httpbin.sh
+++ build/tools/httpbin.sh
@@ -3,15 +3,18 @@
 # Do not run it directly.
 
 httpbin_launch() {
-    WX_TEST_WEBREQUEST_URL=0
-    export WX_TEST_WEBREQUEST_URL
+    # If the tests are already disabled, don't do anything.
+    if [ "$WX_TEST_WEBREQUEST_URL" != "0" ]; then
+        WX_TEST_WEBREQUEST_URL=0
+        export WX_TEST_WEBREQUEST_URL
 
-    go version
-    go install github.com/mccutchen/go-httpbin/v2/cmd/go-httpbin@v2
+        go version
+        go install github.com/mccutchen/go-httpbin/v2/cmd/go-httpbin@v2
 
-    echo 'Launching httpbin...'
-    go-httpbin -host 127.0.0.1 -port 8081 2>&1 >httpbin.log &
-    WX_TEST_WEBREQUEST_URL="http://127.0.0.1:8081"
+        echo 'Launching httpbin...'
+        go-httpbin -host 127.0.0.1 -port 8081 2>&1 >httpbin.log &
+        WX_TEST_WEBREQUEST_URL="http://127.0.0.1:8081"
+    fi
 }
 
 httpbin_show_log() {
