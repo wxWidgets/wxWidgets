46712b63b8087f92a7680c1071c27e706543e046
Paul Cornett <paulcor@users.noreply.github.com>
2024-11-27 14:01:47 -0800

    Avoid negative shift count with valid BMP file
    Detected by undefined behavior sanitizer, on a file with 16 bpp and a color
    component of more than 8 bits. Fix by using a different, and more accurate,
    method to scale color components to 8 bits.

--- src/common/imagbmp.cpp
+++ src/common/imagbmp.cpp
@@ -528,7 +528,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
 
     wxInt32         aDword, rmask = 0, gmask = 0, bmask = 0, amask = 0;
     int             rshift = 0, gshift = 0, bshift = 0, ashift = 0;
-    int             rbits = 0, gbits = 0, bbits = 0;
     wxInt8          bbuf[4];
     wxUint8         aByte;
     wxUint16        aWord;
@@ -640,16 +639,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
                 if (rmask & (1 << bit))
                     rshift = bit;
             }
-            // Find number of bits in mask (MSB-LSB+1)
-            for (bit = 0; bit < bpp; bit++)
-            {
-                if (bmask & (1 << bit))
-                    bbits = bit-bshift+1;
-                if (gmask & (1 << bit))
-                    gbits = bit-gshift+1;
-                if (rmask & (1 << bit))
-                    rbits = bit-rshift+1;
-            }
         }
         else if ( bpp == 16 )
         {
@@ -659,9 +648,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
             rshift = 10;
             gshift = 5;
             bshift = 0;
-            rbits = 5;
-            gbits = 5;
-            bbits = 5;
         }
         else if ( bpp == 32 )
         {
@@ -674,9 +660,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
             rshift = 16;
             gshift = 8;
             bshift = 0;
-            rbits = 8;
-            gbits = 8;
-            bbits = 8;
         }
     }
 
@@ -942,15 +925,13 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
                     return false;
                 wxUINT16_SWAP_ON_BE_IN_PLACE(aWord);
                 linepos += 2;
-                /* Use the masks and calculated amount of shift
-                   to retrieve the color data out of the word.  Then
-                   shift it left by (8 - number of bits) such that
-                   the image has the proper dynamic range */
-                temp = (unsigned char)(((aWord & rmask) >> rshift) << (8-rbits));
+
+                // scale color components to 8 bits
+                temp = rmask ? (aWord & rmask) * 255 / rmask : 0;
                 ptr[poffset] = temp;
-                temp = (unsigned char)(((aWord & gmask) >> gshift) << (8-gbits));
+                temp = gmask ? (aWord & gmask) * 255 / gmask : 0;
                 ptr[poffset + 1] = temp;
-                temp = (unsigned char)(((aWord & bmask) >> bshift) << (8-bbits));
+                temp = bmask ? (aWord & bmask) * 255 / bmask : 0;
                 ptr[poffset + 2] = temp;
                 column++;
             }
