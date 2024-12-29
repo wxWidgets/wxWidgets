56ba6d897135ceafe6652147559a2e5a2bce303b
Vadim Zeitlin <vadim@wxwidgets.org>
2024-12-29 15:09:32 +0100

    Improve erasing background of wxMSW wxStaticText with markup
    Don't use our own solid colour but rather delegate to the native control
    which does it correctly, letting the parent background show through.

--- src/msw/stattext.cpp
+++ src/msw/stattext.cpp
@@ -326,8 +326,11 @@ void wxStaticText::WXOnPaint(wxPaintEvent& event)
 
     wxPaintDC dc(this);
 
-    // TODO: support transparent background for static text with markup.
-    dc.Clear();
+    // Erase the background in the same way the native control does it, in
+    // particular this lets the parent background show through.
+    ::SendMessage(GetHwnd(), WM_PRINTCLIENT,
+                  (WPARAM)dc.GetHDC(),
+                  PRF_ERASEBKGND);
 
     const wxRect rect = GetClientRect();
     if ( !IsThisEnabled() )
