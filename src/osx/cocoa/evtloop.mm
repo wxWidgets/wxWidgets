698a0a06685d8761e0222c72a7921c369574283c
Dmitry Makarenko <kryksyh@gmail.com>
2025-01-17 09:52:00 +0300

    Fix wxWindowDisabler not starting modal session under macOS
    If BeginModalSession() is called twice on a new window object located at
    the same address (i.e. allocated on stack), the second call takes a
    shortcut due to modal window being still set to the same value and does
    not start a modal session. Reset m_modalWindow once session is ended.
    
    See #25087.
    
    (cherry picked from commit e4285b4a35db3887347b60ce35e16a172df6c0a3)

--- src/osx/cocoa/evtloop.mm
+++ src/osx/cocoa/evtloop.mm
@@ -514,7 +514,9 @@ void wxGUIEventLoop::EndModalSession()
     wxASSERT_MSG(m_modalSession != NULL, "no modal session active");
     
     wxASSERT_MSG(m_modalNestedLevel > 0, "incorrect modal nesting level");
-    
+
+    m_modalWindow = nullptr;
+
     --m_modalNestedLevel;
     if ( m_modalNestedLevel == 0 )
     {
