7cef03e0cf8b2ee8a6160d960e916b87a28e6da3
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-01-25 05:39:40 -0500

    Minor wording improvements in wxTextCtrl documentation
    See #25119.
    
    (cherry picked from commit fc423ab47fd1d2ec343f8ec16cc8f43777aaba94)

--- interface/wx/textctrl.h
+++ interface/wx/textctrl.h
@@ -1906,8 +1906,8 @@ public:
     void OSXEnableNewLineReplacement(bool enable);
 
     /**
-        Enables the automatic replacement of ASCII quotation marks and
-        apostrophes with their typographic symbols.
+        Enables the automatic replacement of straight (ASCII) quotation marks and
+        apostrophes with smart ("curly") quotes.
 
         This feature is enabled by default.
 
@@ -1917,7 +1917,7 @@ public:
     void OSXEnableAutomaticQuoteSubstitution(bool enable);
 
     /**
-        Enables the automatic conversion of two ASCII hyphens into an m-dash.
+        Enables the automatic conversion of two ASCII hyphens into an em dash.
 
         This feature is enabled by default.
 
