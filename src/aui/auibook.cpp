wxWidgets/wxWidgets

commit ff32a3c64431b25eef9e28617f9eeddfc17e8f3d

Simplify and optimize wxAuiTabContainer::MovePage()
Don't bother removing the page and then adding it back, just swap the
pages in-place.

src/aui/auibook.cpp
@@ -209,14 +209,12 @@ bool wxAuiTabContainer::MovePage(wxWindow* page,
     if (idx == -1)
         return false;
 
-    // get page entry, make a copy of it
-    wxAuiNotebookPage p = GetPage(idx);
+    // Check if we actually have anything to do.
+    if (new_idx == (size_t)idx)
+        return true;
 
-    // remove old page entry
-    RemovePage(page);
-
-    // insert page where it should be
-    InsertPage(page, p, new_idx);
+    // Just swap the pages contents.
+    std::swap(m_pages.at(idx), m_pages.at(new_idx));
 
     return true;
 }

commit 24f5a1415bf4b05a08ef084e3def10d8d9cf8959

Fix wxAuiTabContainer::Move()
This was broken by ff32a3c644 (Simplify and optimize
wxAuiTabContainer::MovePage(), 2024-12-04), we want to move the page
into the new position instead of swapping it with the page there.

Use std::rotate() to do it, which might not necessarily be more
efficient than removing and adding the page back that we did before, but
seems more clear and should hopefully avoid any off by 1 bugs.

src/aui/auibook.cpp
@@ -36,6 +36,7 @@
 #include "wx/osx/private.h"
 #endif
 
+#include <algorithm>
 #include <unordered_set>
 
 wxDEFINE_EVENT(wxEVT_AUINOTEBOOK_PAGE_CLOSE, wxAuiNotebookEvent);
@@ -205,12 +206,14 @@ bool wxAuiTabContainer::MovePage(wxWindow* page,
     if (idx == -1)
         return false;
 
-    // Check if we actually have anything to do.
-    if (new_idx == (size_t)idx)
-        return true;
+    const size_t old_idx = static_cast<size_t>(idx);
 
-    // Just swap the pages contents.
-    std::swap(m_pages.at(idx), m_pages.at(new_idx));
+    const auto b = m_pages.begin();
+    if (old_idx < new_idx)
+        std::rotate(b + old_idx, b + old_idx + 1, b + new_idx + 1);
+    else if (old_idx > new_idx)
+        std::rotate(b + new_idx, b + old_idx, b + old_idx + 1);
+    //else: nothing to do
 
     return true;
 }