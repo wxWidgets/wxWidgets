6459875179dfb9c963f0e2ac81642abf64334468
Vadim Zeitlin <vadim@wxwidgets.org>
2025-02-08 14:36:14 +0100

    Avoid warning about unused wxFatalExit() in wxMSW DLL build
    This function wasn't used when building the library as a DLL, so just
    get rid it of entirely and call ExitProcess() directly to avoid warning
    C4505 about it being unused and removed from MSVC.
    
    See #25145.
    
    (cherry picked from commit f0937ed038d8e2850872e2686de01a2a47a3fc85)

--- src/msw/main.cpp
+++ src/msw/main.cpp
@@ -65,12 +65,6 @@ extern EXCEPTION_POINTERS *wxGlobalSEInformation = NULL;
 // flag telling us whether the application wants to handle exceptions at all
 static bool gs_handleExceptions = false;
 
-static void wxFatalExit()
-{
-    // use the same exit code as abort()
-    ::ExitProcess(3);
-}
-
 unsigned long wxGlobalSEHandler(EXCEPTION_POINTERS *pExcPtrs)
 {
     if ( gs_handleExceptions && wxTheApp )
@@ -108,7 +102,8 @@ void wxSETranslator(unsigned int WXUNUSED(code), EXCEPTION_POINTERS *ep)
             // if wxApp::OnFatalException() had been called we should exit the
             // application -- but we shouldn't kill our host when we're a DLL
 #ifndef WXMAKINGDLL
-            wxFatalExit();
+            // use the same exit code as abort()
+            ::ExitProcess(3);
 #endif // not a DLL
             break;
 
