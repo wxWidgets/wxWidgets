7e21e41f2b6c78cc0803d9e7a287715f5a1c7898
Sergey Fedorov <barracuda@macos-powerpc.org>
2025-02-17 14:16:01 +0800

    uilocale

--- build/cmake/files.cmake
+++ build/cmake/files.cmake
@@ -36,6 +36,7 @@ set(BASE_UNIX_AND_DARWIN_NOTWXMAC_SRC
     ${BASE_UNIX_AND_DARWIN_SRC}
     src/unix/mimetype.cpp
     src/unix/stdpaths.cpp
+    src/unix/uilocale.cpp
 )
 
 set(BASE_UNIX_AND_DARWIN_NOTWXMAC_HDR
@@ -48,7 +49,6 @@ set(BASE_UNIX_SRC
     ${BASE_UNIX_AND_DARWIN_NOTWXMAC_SRC}
     src/unix/fswatcher_inotify.cpp
     src/unix/secretstore.cpp
-    src/unix/uilocale.cpp
 )
 
 set(BASE_UNIX_HDR
@@ -116,7 +116,6 @@ set(BASE_COREFOUNDATION_SRC
     src/osx/core/secretstore.cpp
     src/osx/core/strconv_cf.cpp
     src/osx/cocoa/utils_base.mm
-    src/osx/core/uilocale.mm
 )
 
 set(BASE_COREFOUNDATION_HDR
@@ -139,6 +138,7 @@ set(BASE_COREFOUNDATION_HDR
 
 set(BASE_OSX_SHARED_SRC
     src/osx/core/mimetype.cpp
+    src/osx/core/uilocale.mm
     src/osx/fswatcher_fsevents.cpp
     src/osx/cocoa/stdpaths.mm
     ${BASE_COREFOUNDATION_SRC}
