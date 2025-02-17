wxWidgets/wxWidgets

commit 734face9c52243f88aa7b81be43cd3521ac99e0f

Add support for raw touch events
Add new API and implement for GTK, MSW and OSX ports.

Add a new sample.

See #649.

Closes #17077.

Closes #24610.

Co-Authored-By: Alex Shvartzkop <dudesuchamazing@gmail.com>
Co-Authored-By: Vadim Zeitlin <vadim@wxwidgets.org>

include/wx/qt/private/converter.h
@@ -29,9 +29,20 @@ inline wxPoint wxQtConvertPoint( const QPoint &point )
 {
     return wxPoint( point.x(), point.y() );
 }
+
 inline QPoint wxQtConvertPoint( const wxPoint &point )
 {
     return QPoint( point.x, point.y );
+}
+
+inline wxPoint2DDouble wxQtConvertPointF(const QPointF& point)
+{
+    return wxPoint2DDouble(point.x(), point.y());
+}
+
+inline QPointF wxQtConvertPointF(const wxPoint2DDouble& point)
+{
+    return QPointF(point.m_x, point.m_y);
 }
 
 inline wxRect wxQtConvertRect( const QRect &rect )

build/cmake/samples/CMakeLists.txt
@@ -144,6 +144,7 @@ wx_add_sample(taskbar tbtest.cpp tbtest.h DATA info.svg DEPENDS wxUSE_TASKBARICO
 wx_add_sample(text DEPENDS wxUSE_TEXTCTRL)
 wx_add_sample(thread DEPENDS wxUSE_THREADS)
 wx_add_sample(toolbar RES toolbar.rc DEPENDS wxUSE_TOOLBAR)
+wx_add_sample(touchtest touchtest.cpp)
 wx_add_sample(treectrl treetest.cpp treetest.h DEPENDS wxUSE_TREECTRL)
 wx_add_sample(treelist DEPENDS wxUSE_TREELISTCTRL)
 wx_add_sample(typetest typetest.cpp typetest.h)

include/wx/event.h
@@ -18,11 +18,13 @@
 
 #if wxUSE_GUI
     #include "wx/gdicmn.h"
+    #include "wx/geometry.h"
     #include "wx/cursor.h"
     #include "wx/mousestate.h"
 #endif
 
 #include "wx/dynarray.h"
+#include "wx/itemid.h"
 #include "wx/thread.h"
 #include "wx/tracker.h"
 #include "wx/typeinfo.h"
@@ -685,6 +687,7 @@ class WXDLLIMPEXP_FWD_CORE wxInitDialogEvent;
 class WXDLLIMPEXP_FWD_CORE wxUpdateUIEvent;
 class WXDLLIMPEXP_FWD_CORE wxClipboardTextEvent;
 class WXDLLIMPEXP_FWD_CORE wxHelpEvent;
+class WXDLLIMPEXP_FWD_CORE wxMultiTouchEvent;
 class WXDLLIMPEXP_FWD_CORE wxGestureEvent;
 class WXDLLIMPEXP_FWD_CORE wxPanGestureEvent;
 class WXDLLIMPEXP_FWD_CORE wxZoomGestureEvent;
@@ -796,6 +799,12 @@ wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_SCROLLWIN_PAGEUP, wxScrollWinEv
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_SCROLLWIN_PAGEDOWN, wxScrollWinEvent);
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_SCROLLWIN_THUMBTRACK, wxScrollWinEvent);
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_SCROLLWIN_THUMBRELEASE, wxScrollWinEvent);
+
+    // MultiTouch event types
+wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_TOUCH_BEGIN, wxMultiTouchEvent);
+wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_TOUCH_MOVE, wxMultiTouchEvent);
+wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_TOUCH_END, wxMultiTouchEvent);
+wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_TOUCH_CANCEL, wxMultiTouchEvent);
 
     // Gesture events
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CORE, wxEVT_GESTURE_PAN, wxPanGestureEvent);
@@ -1883,6 +1892,9 @@ public:
     // Is the system set to do page scrolling?
     bool IsPageScroll() const { return ((unsigned int)m_linesPerAction == UINT_MAX); }
 
+    // Check if the event was synthesized from a touch event.
+    bool IsSynthesized() const { return m_synthesized; }
+
     float GetMagnification() const { return m_magnification; }
     virtual wxEvent *Clone() const override { return new wxMouseEvent(*this); }
     virtual wxEventCategory GetEventCategory() const override { return wxEVT_CATEGORY_USER_INPUT; }
@@ -1904,6 +1916,7 @@ public:
     int           m_linesPerAction;
     int           m_columnsPerAction;
     float         m_magnification;
+    bool          m_synthesized;
 
 protected:
     void Assign(const wxMouseEvent& evt);
@@ -1948,6 +1961,42 @@ private:
 
 private:
     wxDECLARE_DYNAMIC_CLASS_NO_ASSIGN(wxSetCursorEvent);
+};
+
+// MultiTouch Event
+
+class wxTouchSequenceId : public wxItemId<void*>
+{
+public:
+    using wxItemId::wxItemId;
+};
+
+class WXDLLIMPEXP_CORE wxMultiTouchEvent : public wxEvent
+{
+public:
+    wxMultiTouchEvent(wxWindowID winid = 0, wxEventType type = wxEVT_NULL)
+        : wxEvent(winid, type)
+    {
+    }
+
+    wxMultiTouchEvent(const wxMultiTouchEvent& event) = default;
+
+    const wxPoint2DDouble& GetPosition() const { return m_pos; }
+    void SetPosition(const wxPoint2DDouble& pos) { m_pos = pos; }
+    bool IsPrimary() const { return m_isPrimary; }
+    void SetPrimary(bool primary) { m_isPrimary = primary; }
+    const wxTouchSequenceId& GetSequenceId() const { return m_sequence; }
+    void SetSequenceId(const wxTouchSequenceId& sequence) { m_sequence = sequence; }
+
+    virtual wxEvent *Clone() const override { return new wxMultiTouchEvent(*this); }
+
+protected:
+    wxPoint2DDouble m_pos;
+    wxTouchSequenceId m_sequence;
+    bool m_isPrimary = false;
+
+    wxDECLARE_DYNAMIC_CLASS_NO_ASSIGN(wxMultiTouchEvent);
+
 };
 
  // Gesture Event
@@ -4222,6 +4271,7 @@ typedef void (wxEvtHandler::*wxContextMenuEventFunction)(wxContextMenuEvent&);
 typedef void (wxEvtHandler::*wxMouseCaptureChangedEventFunction)(wxMouseCaptureChangedEvent&);
 typedef void (wxEvtHandler::*wxMouseCaptureLostEventFunction)(wxMouseCaptureLostEvent&);
 typedef void (wxEvtHandler::*wxClipboardTextEventFunction)(wxClipboardTextEvent&);
+typedef void (wxEvtHandler::*wxMultiTouchEventFunction)(wxMultiTouchEvent&);
 typedef void (wxEvtHandler::*wxPanGestureEventFunction)(wxPanGestureEvent&);
 typedef void (wxEvtHandler::*wxZoomGestureEventFunction)(wxZoomGestureEvent&);
 typedef void (wxEvtHandler::*wxRotateGestureEventFunction)(wxRotateGestureEvent&);
@@ -4305,6 +4355,8 @@ typedef void (wxEvtHandler::*wxFullScreenEventFunction)(wxFullScreenEvent&);
     wxEVENT_HANDLER_CAST(wxMouseCaptureLostEventFunction, func)
 #define wxClipboardTextEventHandler(func) \
     wxEVENT_HANDLER_CAST(wxClipboardTextEventFunction, func)
+#define wxMultiTouchEventHandler(func) \
+    wxEVENT_HANDLER_CAST(wxMultiTouchEventFunction, func)
 #define wxPanGestureEventHandler(func) \
     wxEVENT_HANDLER_CAST(wxPanGestureEventFunction, func)
 #define wxZoomGestureEventHandler(func) \
@@ -4650,6 +4702,19 @@ typedef void (wxEvtHandler::*wxFullScreenEventFunction)(wxFullScreenEvent&);
     EVT_COMMAND_SCROLL_THUMBTRACK(winid, func) \
     EVT_COMMAND_SCROLL_THUMBRELEASE(winid, func) \
     EVT_COMMAND_SCROLL_CHANGED(winid, func)
+
+// Multi-Touch events
+#define EVT_TOUCH_BEGIN(func) wx__DECLARE_EVT0(wxEVT_TOUCH_BEGIN, wxMultiTouchEventHandler(func))
+#define EVT_TOUCH_MOVE(func) wx__DECLARE_EVT0(wxEVT_TOUCH_MOVE, wxMultiTouchEventHandler(func))
+#define EVT_TOUCH_END(func) wx__DECLARE_EVT0(wxEVT_TOUCH_END, wxMultiTouchEventHandler(func))
+#define EVT_TOUCH_CANCEL(func) wx__DECLARE_EVT0(wxEVT_TOUCH_CANCEL, wxMultiTouchEventHandler(func))
+
+// All touch events
+#define EVT_TOUCH_EVENTS(func) \
+    EVT_TOUCH_BEGIN(func) \
+    EVT_TOUCH_MOVE(func) \
+    EVT_TOUCH_END(func) \
+    EVT_TOUCH_CANCEL(func)
 
 // Gesture events
 #define EVT_GESTURE_PAN(winid, func) wx__DECLARE_EVT1(wxEVT_GESTURE_PAN, winid, wxPanGestureEventHandler(func))

include/wx/geometry.h
@@ -294,6 +294,9 @@ public :
     inline void GetFloor( wxInt32 *x , wxInt32 *y ) const;
     inline void GetRounded( wxInt32 *x , wxInt32 *y ) const;
 
+    inline wxPoint GetFloor() const;
+    inline wxPoint GetRounded() const;
+
     inline wxDouble GetVectorLength() const;
      wxDouble GetVectorAngle() const ;
     void SetVectorLength( wxDouble length );
@@ -410,6 +413,16 @@ inline void wxPoint2DDouble::GetRounded( wxInt32 *x , wxInt32 *y ) const
 {
     *x = wxRound( m_x );
     *y = wxRound( m_y );
+}
+
+inline wxPoint wxPoint2DDouble::GetFloor() const
+{
+    return wxPoint( (wxInt32) floor( m_x ), (wxInt32) floor( m_y ) );
+}
+
+inline wxPoint wxPoint2DDouble::GetRounded() const
+{
+    return wxPoint( wxRound( m_x ), wxRound( m_y ) );
 }
 
 inline wxDouble wxPoint2DDouble::GetVectorLength() const

include/wx/gtk/private/event.h
@@ -105,6 +105,24 @@ WindowLeaveCallback(GtkWidget* widget,
                     GdkEventCrossing* event,
                     wxWindowGTK* win);
 
+gboolean
+WindowMotionCallback(GtkWidget* widget,
+                     GdkEventMotion* event,
+                     wxWindowGTK* win,
+                     bool synthesized = false);
+
+gboolean
+WindowButtonPressCallback(GtkWidget* widget,
+                          GdkEventButton* event,
+                          wxWindowGTK* win,
+                          bool synthesized = false);
+
+gboolean
+WindowButtonReleaseCallback(GtkWidget* widget,
+                            GdkEventButton* event,
+                            wxWindowGTK* win,
+                            bool synthesized = false);
+
 } // namespace wxGTKImpl
 
 #endif // _GTK_PRIVATE_EVENT_H_

include/wx/msw/window.h
@@ -390,6 +390,7 @@ public:
     bool HandleRotateGesture(const wxPoint& pt, WXDWORD angleArgument, WXDWORD flags);
     bool HandleTwoFingerTap(const wxPoint& pt, WXDWORD flags);
     bool HandlePressAndTap(const wxPoint& pt, WXDWORD flags);
+    bool HandleTouch(WXWPARAM wParam, WXLPARAM lParam);
 
     bool HandleChar(WXWPARAM wParam, WXLPARAM lParam);
     bool HandleKeyDown(WXWPARAM wParam, WXLPARAM lParam);

include/wx/osx/cocoa/private.h
@@ -188,6 +188,7 @@ public :
     virtual void        TouchesBegan(NSEvent *event);
     virtual void        TouchesMoved(NSEvent *event);
     virtual void        TouchesEnded(NSEvent *event);
+    virtual void        TouchesCancel(NSEvent *event);
 
 #if !wxOSX_USE_NATIVE_FLIPPED
     void                SetFlipped(bool flipped);
@@ -401,6 +402,8 @@ public:
     WXDLLIMPEXP_CORE wxRect wxFromNSRect( NSView* parent, const NSRect& rect );
     WXDLLIMPEXP_CORE NSPoint wxToNSPoint( NSView* parent, const wxPoint& p );
     WXDLLIMPEXP_CORE wxPoint wxFromNSPoint( NSView* parent, const NSPoint& p );
+    WXDLLIMPEXP_CORE NSPoint wxToNSPointF(NSView* parent, const wxPoint2DDouble& p);
+    WXDLLIMPEXP_CORE wxPoint2DDouble wxFromNSPointF(NSView* parent, const NSPoint& p);
 
     NSRect WXDLLIMPEXP_CORE wxOSXGetFrameForControl( wxWindowMac* window , const wxPoint& pos , const wxSize &size ,
         bool adjustForOrigin = true );

commit b8d53a263a9a70da611378c2c5e84c97fb172b78

Fix wxQt compilation after raw touch event changes
Add missing #include "wx/geometry.h".

Broken by 734face (Add support for raw touch events, 2025-01-08).

See #24610.

Closes #25176.

include/wx/qt/private/converter.h
@@ -16,6 +16,7 @@
 #include "wx/kbdstate.h"
 #include "wx/gdicmn.h"
 #include "wx/colour.h"
+#include "wx/geometry.h"
 
 #include <QtCore/QRect>
 #include <QtCore/QSize>