647e63cd5ea75153630a4cf078aa23ed3f0e4bed
Vadim Zeitlin <vadim@wxwidgets.org>
2025-02-24 15:07:48 +0100

    Fix showing labels of newly added wxNotebook pages in wxOSX
    The existing code added new tabs before setting their label, which
    resulted in them being shown initially truncated.
    
    Fix this by adding new tabs with their correct label (and image), as
    this seems to make more sense and also fixes the problem.
    
    Also don't override SetMaximum() in wxCocoaTabView, this doesn't make
    sense as wxNotebook doesn't have a "maximum" property unlike wxSpinCtrl
    or wxGauge, for which this function (and the associated SetMinimum())
    exists, and just do what this function did in SetupTabs() itself, which
    is the function explicitly intended to be overridden in wxNotebook.
    
    Closes #25194.

--- src/osx/cocoa/notebook.mm
+++ src/osx/cocoa/notebook.mm
@@ -224,7 +224,7 @@ public:
             return [slf indexOfTabViewItem:selectedItem]+1;
     }
 
-    void SetMaximum( wxInt32 maximum ) override
+    void SetupTabs( const wxNotebook& notebook) override
     {
         wxNSTabView* slf = (wxNSTabView*) m_osxView;
         int cocoacount = [slf numberOfTabViewItems ];
@@ -232,11 +232,42 @@ public:
         wxTabViewController* controller = [slf delegate];
         [slf setDelegate:nil];
 
+        // Update the existing pages in case their label or image changed.
+        const int maximum = notebook.GetPageCount();
+        for ( int i = 0; i < wxMin(maximum, cocoacount); ++i )
+        {
+            NSTabViewItem* item = [(wxNSTabView*) m_osxView tabViewItemAtIndex:i];
+
+            wxNotebookPage* page = notebook.GetPage(i);
+            [item setView:page->GetHandle() ];
+            wxCFStringRef cf( page->GetLabel() );
+            [item setLabel:cf.AsNSString()];
+
+            const wxBitmapBundle bitmap = notebook.GetPageBitmapBundle(i);
+            if ( bitmap.IsOk() )
+            {
+                [(WXCTabViewImageItem*) item setImage: wxOSXGetImageFromBundle(bitmap)];
+            }
+        }
+
+        // Next also add new pages or delete the no more existing ones.
         if ( maximum > cocoacount )
         {
             for ( int i = cocoacount ; i < maximum ; ++i )
             {
                 NSTabViewItem* item = [[WXCTabViewImageItem alloc] init];
+
+                wxNotebookPage* page = notebook.GetPage(i);
+                [item setView:page->GetHandle() ];
+                wxCFStringRef cf( page->GetLabel() );
+                [item setLabel:cf.AsNSString()];
+
+                const wxBitmapBundle bitmap = notebook.GetPageBitmapBundle(i);
+                if ( bitmap.IsOk() )
+                {
+                    [(WXCTabViewImageItem*) item setImage: wxOSXGetImageFromBundle(bitmap)];
+                }
+
                 [slf addTabViewItem:item];
                 [item release];
             }
@@ -249,29 +280,8 @@ public:
                 [slf removeTabViewItem:item];
             }
         }
+
         [slf setDelegate:controller];
-    }
-
-    void SetupTabs( const wxNotebook& notebook) override
-    {
-        int pcount = notebook.GetPageCount();
-
-        SetMaximum( pcount );
-
-        for ( int i = 0 ; i < pcount ; ++i )
-        {
-            wxNotebookPage* page = notebook.GetPage(i);
-            NSTabViewItem* item = [(wxNSTabView*) m_osxView tabViewItemAtIndex:i];
-            [item setView:page->GetHandle() ];
-            wxCFStringRef cf( page->GetLabel() );
-            [item setLabel:cf.AsNSString()];
-
-            const wxBitmapBundle bitmap = notebook.GetPageBitmapBundle(i);
-            if ( bitmap.IsOk() )
-            {
-                [(WXCTabViewImageItem*) item setImage: wxOSXGetImageFromBundle(bitmap)];
-            }
-        }
     }
 
     int TabHitTest(const wxPoint & pt, long* flags) override
