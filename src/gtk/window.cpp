61e18745877655e9aa3c628dbe3cd1f02b6eaf1c
Vadim Zeitlin <vadim@wxwidgets.org>
2025-02-26 20:10:46 +0100

    Don't do anything in wxGTK wxWindow::Update() under Wayland
    The existing code doesn't work, i.e. doesn't actually update the window
    on screen immediately, but does result in some weird refresh problems
    afterwards, so it seems better to avoid doing anything here.
    
    See #25036, #25204.
    
    (cherry picked from commit 2e2e285a8a039960d532088ecf2c0da392b3aab9)

--- src/gtk/window.cpp
+++ src/gtk/window.cpp
@@ -5330,14 +5330,26 @@ void wxWindowGTK::Update()
 {
     if (m_widget && gtk_widget_get_mapped(m_widget) && m_width > 0 && m_height > 0)
     {
+        GdkWindow* window = GTKGetDrawingWindow();
+        if (window == NULL)
+            window = gtk_widget_get_window(m_widget);
+
+#ifdef GDK_WINDOWING_WAYLAND
+        if (wxGTKImpl::IsWayland(window))
+        {
+            // Using the functions below with Wayland seems to break something
+            // in the update logic, with future updates just getting lost, see
+            // #25036, so don't use it in this case, especially as it doesn't
+            // even seem to work anyhow.
+            return;
+        }
+#endif // GDK_WINDOWING_WAYLAND
+
         GdkDisplay* display = gtk_widget_get_display(m_widget);
         // If window has just been shown, drawing may not work unless pending
         // requests queued for the windowing system are flushed first.
         gdk_display_flush(display);
 
-        GdkWindow* window = GTKGetDrawingWindow();
-        if (window == NULL)
-            window = gtk_widget_get_window(m_widget);
         gdk_window_process_updates(window, true);
 
         gdk_display_flush(display);
