475510db10cdb546d235ad41bb6e0d237964fcbd
ali kettab <aliket1435@gmail.com>
2025-03-01 07:19:48 +0100

    wxQt: QDesktopWidget is gone in Qt6

--- src/qt/dcscreen.cpp
+++ src/qt/dcscreen.cpp
@@ -12,7 +12,9 @@
 #include "wx/qt/dcscreen.h"
 
 #include <QtWidgets/QApplication>
+#if QT_VERSION_MAJOR < 6
 #include <QtWidgets/QDesktopWidget>
+#endif
 #include <QtGui/QPainter>
 #include <QtGui/QPicture>
 #include <QtGui/QPixmap>
@@ -43,6 +45,13 @@ void wxScreenDCImpl::DoGetSize(int *width, int *height) const
 QPixmap *wxScreenDCImpl::GetQPixmap()
 {
     if ( !m_qtPixmap )
-        m_qtPixmap = new QPixmap(QApplication::primaryScreen()->grabWindow(QApplication::desktop()->winId()));
+    {
+        m_qtPixmap = new QPixmap(QApplication::primaryScreen()->grabWindow(
+#if QT_VERSION_MAJOR < 6
+            QApplication::desktop()->winId()
+#endif
+            ));
+    }
+
     return m_qtPixmap;
 }
