d66518388c1f72bdbd478cb57a2c9ff711521d3d
Vadim Zeitlin <vadim@wxwidgets.org>
2025-03-21 23:08:36 +0100

    Update version to 3.2.8
    Run misc/scripts/inc_release followed by bakefile_gen and autoconf.
    
    Also add a new section to the change log.

--- include/wx/version.h
+++ include/wx/version.h
@@ -27,9 +27,9 @@
 /*  NB: this file is parsed by automatic tools so don't change its format! */
 #define wxMAJOR_VERSION      3
 #define wxMINOR_VERSION      2
-#define wxRELEASE_NUMBER     7
+#define wxRELEASE_NUMBER     8
 #define wxSUBRELEASE_NUMBER  0
-#define wxVERSION_STRING   wxT("wxWidgets 3.2.7")
+#define wxVERSION_STRING   wxT("wxWidgets 3.2.8")
 
 /*  nothing to update below this line when updating the version */
 /*  ---------------------------------------------------------------------------- */
