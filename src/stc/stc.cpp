a0ed0aac686322a69a042910d7c0022a5aea451b
Maarten Bent <MaartenBent@users.noreply.github.com>
2025-04-13 14:38:34 +0200

    Update submodules

--- src/stc/stc.cpp
+++ src/stc/stc.cpp
@@ -6051,7 +6051,7 @@ wxStyledTextEvent::wxStyledTextEvent(const wxStyledTextEvent& event):
 
 /*static*/ wxVersionInfo wxStyledTextCtrl::GetLexerVersionInfo()
 {
-    return wxVersionInfo("Lexilla", 5, 4, 2, "Lexilla 5.4.2");
+    return wxVersionInfo("Lexilla", 5, 4, 4, "Lexilla 5.4.4");
 }
 
 
