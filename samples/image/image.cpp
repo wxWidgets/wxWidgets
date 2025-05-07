9def7c87b4c59a559c54494a3b6e1e90a73d5370
Vadim Zeitlin <vadim@wxwidgets.org>
2025-05-07 18:47:42 +0200

    Construct wxCursor from wxBitmap in the image sample
    There is no need to use an intermediate wxImage any more.
    
    We also don't need to copy cursor.png to the build directory, as it is
    now used only during the build and not when running the sample, so don't
    do this.

--- samples/image/image.cpp
+++ samples/image/image.cpp
@@ -51,6 +51,7 @@
 
 #ifndef wxHAS_IMAGES_IN_RESOURCES
     #include "../sample.xpm"
+    #include "cursor_png.c"
 #endif
 
 // ============================================================================
@@ -1165,7 +1166,7 @@ MyFrame::MyFrame()
     m_canvas = new MyCanvas(this);
     wxSize cz = m_canvas->GetDrawingSize();
     m_canvas->SetScrollbars(10, 10, cz.GetWidth() / 10, cz.GetHeight() / 10);
-    m_canvas->SetCursor(wxImage("cursor.png"));
+    m_canvas->SetCursor(wxBITMAP_PNG(cursor));
 
     // slightly wider than the canvas
     SetSize(wxSize(cz.GetWidth() + FromDIP(50), FromDIP(700)));
