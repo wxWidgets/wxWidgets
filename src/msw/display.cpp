wxWidgets/wxWidgets

commit 3d15cc395d598534b7cfaa3b902c9085e06df79e

Fix wxDisplay invalidation on connected displays change
Pointers stored in wxDisplay became dangling when display cache was
invalidated due to a change in the monitors connected to the system,
resulting in fatal crashes when using them.

Fix this by using shared pointers internally to ensure that any existing
wxDisplay objects remain valid, in C++ sense, after display cache
invalidation, even if they switch to a special "disconnected" state
indicating that they don't represent a valid display any longer.

Closes #25396.

Closes #25407.

src/msw/display.cpp
@@ -140,6 +140,10 @@ public:
     virtual wxArrayVideoModes GetModes(const wxVideoMode& mode) const override;
     virtual bool ChangeMode(const wxVideoMode& mode) override;
 
+    // Check if this display is still found in the given vector and update both
+    // its properties and the index to the new values if it is.
+    bool DoRefreshOnDisplayChange(const wxVector<wxDisplayInfo>& displays);
+
 protected:
     // convert a DEVMODE to our wxVideoMode
     static wxVideoMode ConvertToVideoMode(const DEVMODE& dm)
@@ -183,10 +187,21 @@ public:
     virtual int GetFromRect(const wxRect& rect) override;
     virtual int GetFromWindow(const wxWindow *window) override;
 
-    void InvalidateCache() override
+    void UpdateOnDisplayChange() override
     {
-        wxDisplayFactory::InvalidateCache();
+        // Update m_displays first, before calling the base class version which
+        // will call our RefreshOnDisplayChange() that uses it.
         DoRefreshMonitors();
+        wxDisplayFactory::UpdateOnDisplayChange();
+    }
+
+    virtual bool RefreshOnDisplayChange(wxDisplayImpl& impl) const override
+    {
+        // All wxDisplayImpl in a program using this factory are of type
+        // wxDisplayMSW, so the cast is safe.
+        auto& implMSW = static_cast<wxDisplayMSW&>(impl);
+
+        return implMSW.DoRefreshOnDisplayChange(m_displays);
     }
 
     // Declare the second argument as int to avoid problems with older SDKs not
@@ -487,6 +502,22 @@ bool wxDisplayMSW::ChangeMode(const wxVideoMode& mode)
     return false;
 }
 
+bool
+wxDisplayMSW::DoRefreshOnDisplayChange(const wxVector<wxDisplayInfo>& displays)
+{
+    for ( size_t n = 0; n < displays.size(); ++n )
+    {
+        if ( displays[n].hmon == displays[n].hmon )
+        {
+            m_info = displays[n];
+            m_index = n;
+            return true;
+        }
+    }
+
+    Disconnect();
+    return false;
+}
 
 // ----------------------------------------------------------------------------
 // wxDisplayFactoryMSW implementation

include/wx/display.h
@@ -57,7 +57,7 @@ public:
 
     // dtor is not virtual as this is a concrete class not meant to be derived
     // from
-
+    ~wxDisplay();
 
     // return the number of available displays, valid parameters to
     // wxDisplay ctor are from 0 up to this number
@@ -78,6 +78,9 @@ public:
 
     // return true if the object was initialized successfully
     bool IsOk() const { return m_impl != nullptr; }
+
+    // return true if the display has not been unplugged yet
+    bool IsConnected() const;
 
     // get the full display size
     wxRect GetGeometry() const;
@@ -159,7 +162,7 @@ private:
 
 
     // the real implementation
-    wxDisplayImpl *m_impl;
+    wxObjectDataPtr<wxDisplayImpl> m_impl;
 
 
     wxDECLARE_NO_COPY_CLASS(wxDisplay);

include/wx/private/display.h
@@ -26,41 +26,10 @@ public:
 
     // Create the display if necessary using CreateDisplay(), otherwise just
     // get it from cache.
-    wxDisplayImpl* GetDisplay(unsigned n)
-    {
-        // Normally, m_impls should be cleared if the number of displays in the
-        // system changes because InvalidateCache() must be called. However in
-        // some ports (e.g. Mac right now, see #18318), cache invalidation never
-        // happens, so we can end up with m_impls size being out of sync with
-        // the actual number of monitors. Compensate for this here by checking
-        // if the index is invalid and invalidating the cache at least in this
-        // case.
-        //
-        // Note that this is still incorrect because we continue using outdated
-        // information if the first monitor is disconnected, for example. The
-        // only real solution is to ensure that InvalidateCache() is called,
-        // but for now this at least avoids crashes when a new display is
-        // connected.
-        if ( n >= m_impls.size() )
-        {
-            // This strange two-step resize is done to clear all the existing
-            // elements: they may not be valid any longer if the number of
-            // displays has changed.
-            m_impls.resize(0);
-            m_impls.resize(GetCount());
-        }
-        else if ( m_impls[n] )
-        {
-            // Just return the existing display if we have it.
-            return m_impls[n];
-        }
-
-        m_impls[n] = CreateDisplay(n);
-        return m_impls[n];
-    }
+    wxObjectDataPtr<wxDisplayImpl> GetDisplay(unsigned n);
 
     // Return the primary display object, creating it if necessary.
-    wxDisplayImpl* GetPrimaryDisplay();
+    wxObjectDataPtr<wxDisplayImpl> GetPrimaryDisplay();
 
     // get the total number of displays
     virtual unsigned GetCount() = 0;
@@ -78,7 +47,7 @@ public:
     virtual int GetFromWindow(const wxWindow *window);
 
     // Trigger recreation of wxDisplayImpl when they're needed the next time.
-    virtual void InvalidateCache() { ClearImpls(); }
+    virtual void UpdateOnDisplayChange();
 
 protected:
     // create a new display object
@@ -86,12 +55,22 @@ protected:
     // it can return a null pointer if the display creation failed
     virtual wxDisplayImpl *CreateDisplay(unsigned n) = 0;
 
+    // check if the given display is still connected and update its properties
+    // if this is the case (notably its index)
+    //
+    // mark display as disconnected and return false otherwise
+    //
+    // default implementation considers all displays as being not connected any
+    // longer, port-specific implementations should override it to keep the
+    // objects corresponding to the still connected displays valid
+    virtual bool RefreshOnDisplayChange(wxDisplayImpl& impl) const;
+
 private:
     // Delete all the elements of m_impls vector and clear it.
     void ClearImpls();
 
     // On-demand populated vector of wxDisplayImpl objects.
-    wxVector<wxDisplayImpl*> m_impls;
+    wxVector<wxObjectDataPtr<wxDisplayImpl>> m_impls;
 
     wxDECLARE_NO_COPY_CLASS(wxDisplayFactory);
 };
@@ -100,7 +79,7 @@ private:
 // wxDisplayImpl: base class for all wxDisplay implementations
 // ----------------------------------------------------------------------------
 
-class wxDisplayImpl
+class wxDisplayImpl : public wxObjectRefData
 {
 public:
     // virtual dtor for this base class
@@ -144,14 +123,22 @@ public:
     virtual bool ChangeMode(const wxVideoMode& mode) = 0;
 #endif // wxUSE_DISPLAY
 
+    // return true, if this display is still connected physically to system
+    bool IsConnected() const { return m_isConnected; }
+
+    // indicate that this display is not connected to system anymore
+    void Disconnect() { m_isConnected = false; }
+
 protected:
     // create the object providing access to the display with the given index
     wxDisplayImpl(unsigned n) : m_index(n) { }
 
 
     // the index of this display (0 is always the primary one)
-    const unsigned m_index;
+    unsigned m_index;
 
+    // true, if this display is still connected physically to system
+    bool m_isConnected = true;
 
     friend class wxDisplayFactory;
 

interface/wx/display.h
@@ -208,6 +208,26 @@ public:
     static wxSize GetStdPPI();
 
     /**
+        Returns @true if the display has not been unplugged yet.
+
+        This function can return @false if the display configuration has
+        changed since this wxDisplay object has been created and either this
+        display is known not to be connected to the system any more (support
+        for detecting this is currently only implemented in wxMSW) or the
+        display status is now unknown (which happens in all the other ports
+        detecting the display configuration changes, e.g. wxOSX).
+
+        A disconnected object is still usable, but all accessor functions
+        return invalid fallback values (e.g. 0 for the width and height) and so
+        such objects are not really useful any more. It is recommended to
+        recreate them when the display configuration changes, which can be done
+        in wxEVT_DISPLAY_CHANGED handler of any top-level window.
+
+        @since 3.3.0
+    */
+    bool IsConnected() const;
+
+    /**
         Returns @true if the display is the primary display. The primary
         display is the one whose index is 0.
     */

src/common/dpycmn.cpp
@@ -91,6 +91,10 @@ wxDisplay::wxDisplay(const wxWindow* window)
                               : Factory().GetPrimaryDisplay();
 }
 
+// Note that this dtor must be implemented here, where wxDisplayImpl is fully
+// declared, and not inline.
+wxDisplay::~wxDisplay() = default;
+
 // ----------------------------------------------------------------------------
 // static functions forwarded to wxDisplayFactory
 // ----------------------------------------------------------------------------
@@ -119,16 +123,26 @@ wxDisplay::wxDisplay(const wxWindow* window)
 
 /* static */ void wxDisplay::InvalidateCache()
 {
-    Factory().InvalidateCache();
+    Factory().UpdateOnDisplayChange();
 }
 
 // ----------------------------------------------------------------------------
 // functions forwarded to wxDisplayImpl
 // ----------------------------------------------------------------------------
 
+bool wxDisplay::IsConnected() const
+{
+    wxCHECK_MSG( IsOk(), false, wxT("invalid wxDisplay object") );
+
+    return m_impl->IsConnected();
+}
+
 wxRect wxDisplay::GetGeometry() const
 {
     wxCHECK_MSG( IsOk(), wxRect(), wxT("invalid wxDisplay object") );
+
+    if ( !m_impl->IsConnected() )
+        return wxRect();
 
     return m_impl->GetGeometry();
 }
@@ -137,12 +151,18 @@ wxRect wxDisplay::GetClientArea() const
 {
     wxCHECK_MSG( IsOk(), wxRect(), wxT("invalid wxDisplay object") );
 
+    if ( !m_impl->IsConnected() )
+        return wxRect();
+
     return m_impl->GetClientArea();
 }
 
 wxSize wxDisplay::GetPPI() const
 {
     wxCHECK_MSG( IsOk(), wxSize(), wxT("invalid wxDisplay object") );
+
+    if ( !m_impl->IsConnected() )
+        return wxSize();
 
     return m_impl->GetPPI();
 }
@@ -151,12 +171,18 @@ double wxDisplay::GetScaleFactor() const
 {
     wxCHECK_MSG( IsOk(), 0, wxT("invalid wxDisplay object") );
 
+    if ( !m_impl->IsConnected() )
+        return 0.0;
+
     return m_impl->GetScaleFactor();
 }
 
 int wxDisplay::GetDepth() const
 {
     wxCHECK_MSG( IsOk(), 0, wxT("invalid wxDisplay object") );
+
+    if ( !m_impl->IsConnected() )
+        return 0;
 
     return m_impl->GetDepth();
 }
@@ -165,12 +191,15 @@ wxString wxDisplay::GetName() const
 {
     wxCHECK_MSG( IsOk(), wxString(), wxT("invalid wxDisplay object") );
 
+    if ( !m_impl->IsConnected() )
+        return wxString();
+
     return m_impl->GetName();
 }
 
 bool wxDisplay::IsPrimary() const
 {
-    return m_impl && m_impl->IsPrimary();
+    return m_impl && m_impl->IsConnected() && m_impl->IsPrimary();
 }
 
 #if wxUSE_DISPLAY
@@ -179,6 +208,9 @@ wxArrayVideoModes wxDisplay::GetModes(const wxVideoMode& mode) const
 {
     wxCHECK_MSG( IsOk(), wxArrayVideoModes(), wxT("invalid wxDisplay object") );
 
+    if ( !m_impl->IsConnected() )
+        return wxArrayVideoModes();
+
     return m_impl->GetModes(mode);
 }
 
@@ -186,12 +218,18 @@ wxVideoMode wxDisplay::GetCurrentMode() const
 {
     wxCHECK_MSG( IsOk(), wxVideoMode(), wxT("invalid wxDisplay object") );
 
+    if ( !m_impl->IsConnected() )
+        return wxVideoMode();
+
     return m_impl->GetCurrentMode();
 }
 
 bool wxDisplay::ChangeMode(const wxVideoMode& mode)
 {
     wxCHECK_MSG( IsOk(), false, wxT("invalid wxDisplay object") );
+
+    if ( !m_impl->IsConnected() )
+        return false;
 
     return m_impl->ChangeMode(mode);
 }
@@ -216,18 +254,56 @@ bool wxDisplay::ChangeMode(const wxVideoMode& mode)
 // wxDisplayFactory implementation
 // ============================================================================
 
+bool wxDisplayFactory::RefreshOnDisplayChange(wxDisplayImpl& impl) const
+{
+    // We have no means to detect if the display is still connected here, so
+    // assume it isn't any more: this is safer than assuming that it still is.
+    //
+    // Of course, ideally this should be really implemented in port-specific
+    // code.
+    impl.Disconnect();
+
+    return false;
+}
+
 void wxDisplayFactory::ClearImpls()
 {
-    for ( size_t n = 0; n < m_impls.size(); ++n )
-    {
-        // It can be null, that's ok.
-        delete m_impls[n];
-    }
-
     m_impls.clear();
 }
 
-wxDisplayImpl* wxDisplayFactory::GetPrimaryDisplay()
+wxObjectDataPtr<wxDisplayImpl> wxDisplayFactory::GetDisplay(unsigned n)
+{
+    // Normally, m_impls should be cleared if the number of displays in the
+    // system changes because InvalidateCache() must be called. However in
+    // some ports, cache invalidation never happens, so we can end up with
+    // m_impls size being out of sync with the actual number of monitors.
+    // Compensate for this here by checking if the index is invalid and
+    // invalidating the cache at least in this case.
+    //
+    // Note that this is still incorrect because we continue using outdated
+    // information if the first monitor is disconnected, for example. The
+    // only real solution is to ensure that InvalidateCache() is called,
+    // but for now this at least avoids crashes when a new display is
+    // connected.
+    if ( n >= m_impls.size() )
+    {
+        // This strange two-step resize is done to clear all the existing
+        // elements: they may not be valid any longer if the number of
+        // displays has changed.
+        m_impls.resize(0);
+        m_impls.resize(GetCount());
+    }
+
+    // Return the existing display if we have it or create a new one.
+    auto& impl = m_impls[n];
+
+    if ( !impl )
+        impl = CreateDisplay(n);
+
+    return impl;
+}
+
+wxObjectDataPtr<wxDisplayImpl> wxDisplayFactory::GetPrimaryDisplay()
 {
     // Just use dumb linear search -- there seems to be the most reliable way
     // to do this in general. In particular, primary monitor is not guaranteed
@@ -235,14 +311,14 @@ wxDisplayImpl* wxDisplayFactory::GetPrimaryDisplay()
     const unsigned count = GetCount();
     for ( unsigned n = 0; n < count; ++n )
     {
-        wxDisplayImpl* const d = GetDisplay(n);
+        auto const d = GetDisplay(n);
         if ( d && d->IsPrimary() )
             return d;
     }
 
     // This is not supposed to happen, but what else can we do if it
     // somehow does?
-    return nullptr;
+    return wxObjectDataPtr<wxDisplayImpl>();
 }
 
 int wxDisplayFactory::GetFromRect(const wxRect& r)
@@ -285,6 +361,29 @@ int wxDisplayFactory::GetFromWindow(const wxWindow *window)
         return wxNOT_FOUND;
 
     return GetFromRect(window->GetScreenRect());
+}
+
+void wxDisplayFactory::UpdateOnDisplayChange()
+{
+    // Prepare the new vector by copying the still valid displays into it.
+    //
+    // Preserve the invariant that m_impls size is always equal to the number
+    // of displays.
+    wxVector<wxObjectDataPtr<wxDisplayImpl>> impls(GetCount());
+
+    for ( auto& impl : m_impls )
+    {
+        // Object may be empty if not accessed yet.
+        // Try to update display state or mark it as disconnected.
+        if ( impl && RefreshOnDisplayChange(*impl) )
+        {
+            // If display is still connected put it at the new index position:
+            // it must have been updated by RefreshOnDisplayChange().
+            impls[impl->GetIndex()] = impl;
+        }
+    }
+
+    m_impls = std::move(impls);
 }
 
 // ============================================================================

src/msw/window.cpp
@@ -5116,6 +5116,10 @@ bool wxWindowMSW::HandleDisplayChange()
     wxDisplayChangedEvent event;
     event.SetEventObject(this);
 
+    // trigger display cache update to ensure the user receives the most
+    // recent display properties on event handling after change.
+    wxDisplay::InvalidateCache();
+
     return HandleWindowEvent(event);
 }
 

commit 859421b9d54dc35247a403042362466eb10a3bc6

Fix checking for display connected status in wxMSW
This fixes a typo in HMONITOR comparison inadvertently introduced during
refactoring in 3d15cc395d (Fix wxDisplay invalidation on connected
displays change, 2025-05-15), see #25407.

Also add some comments to this function.

See #25396.

src/msw/display.cpp
@@ -505,16 +505,24 @@ bool wxDisplayMSW::ChangeMode(const wxVideoMode& mode)
 bool
 wxDisplayMSW::DoRefreshOnDisplayChange(const wxVector<wxDisplayInfo>& displays)
 {
+    // Try to find this display in the list of the currently available ones.
     for ( size_t n = 0; n < displays.size(); ++n )
     {
-        if ( displays[n].hmon == displays[n].hmon )
+        if ( m_info.hmon == displays[n].hmon )
         {
+            // We did find it, update it just in case its characteristics have
+            // changed.
             m_info = displays[n];
+
+            // And, importantly, also update its index which could have changed
+            // due to a previous monitor disconnection.
             m_index = n;
+
             return true;
         }
     }
 
+    // This display is not connected any more, so mark it as such.
     Disconnect();
     return false;
 }