e816a11473d7c68eb8ddfe43f3497274954fc235
مهدي شينون (Mehdi Chinoune) <mehdi.chinoune@hotmail.com>
2025-05-29 04:38:00 +0000

    Fix making symlinks/copies for wxrc and wx-config
    When DESTDIR is defined.

--- build/cmake/install.cmake
+++ build/cmake/install.cmake
@@ -69,7 +69,7 @@ else()
     install(DIRECTORY DESTINATION "bin")
     install(CODE "execute_process( \
         COMMAND ${CMAKE_COMMAND} -E ${COPY_CMD} \
-        \"${CMAKE_INSTALL_PREFIX}/lib/wx/config/${wxBUILD_FILE_ID}\" \
+        \"\$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/wx/config/${wxBUILD_FILE_ID}\" \
         \"\$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/wx-config\" \
         )"
     )
