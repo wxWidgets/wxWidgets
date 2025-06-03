wxWidgets/wxWidgets

commit cd07eabd543160708ba6a39c54e1325514d04a56

Add support for "Do not show this again" checkbox to wxInfoBar
Closes #25394.

src/generic/infobar.cpp
@@ -35,6 +35,8 @@
 
 #include "wx/artprov.h"
 #include "wx/scopeguard.h"
+#include "wx/valgen.h"
+#include "wx/checkbox.h"
 
 #ifdef __WXGTK3__
     #include "wx/gtk/private/wrapgtk.h"
@@ -45,6 +47,7 @@
 
 wxBEGIN_EVENT_TABLE(wxInfoBarGeneric, wxInfoBarBase)
     EVT_BUTTON(wxID_ANY, wxInfoBarGeneric::OnButton)
+    EVT_BUTTON(wxID_CLOSE, wxInfoBarGeneric::OnButton)
 wxEND_EVENT_TABLE()
 
 // ============================================================================
@@ -64,11 +67,12 @@ void wxInfoBarGeneric::Init()
     m_effectDuration = 0;
 }
 
-bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid)
+bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid, long style)
 {
     // calling Hide() before Create() ensures that we're created initially
     // hidden
     Hide();
+    SetWindowStyle(style);
     if ( !wxWindow::Create(parent, winid) )
         return false;
 
@@ -115,8 +119,14 @@ bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid)
     if(!m_hasFgCol)
         m_text->SetForegroundColour(colFg);
 
-    m_button = wxBitmapButton::NewCloseButton(this, wxID_ANY);
+    m_button = wxBitmapButton::NewCloseButton(this, wxID_CLOSE);
     m_button->SetToolTip(_("Hide this notification message."));
+
+    m_checkbox =
+        new wxCheckBox(this, wxID_ANY, wxEmptyString, wxDefaultPosition,
+            wxDefaultSize, 0, wxGenericValidator(&m_checked));
+    m_checkbox->SetForegroundColour(
+        wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
 
     // center the text inside the sizer with an icon to the left of it and a
     // button at the very right
@@ -124,10 +134,22 @@ bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid)
     // NB: AddButton() relies on the button being the last control in the sizer
     //     and being preceded by a spacer
     wxSizer * const sizer = new wxBoxSizer(wxHORIZONTAL);
-    sizer->Add(m_icon, wxSizerFlags().Centre().Border());
-    sizer->Add(m_text, wxSizerFlags().Proportion(1).Centre());
-    sizer->AddSpacer(0); // This spacer only exists for compatibility.
-    sizer->Add(m_button, wxSizerFlags().Centre().Border());
+    wxSizer * const defaultControlSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* const firstRowSizer = new wxBoxSizer(wxHORIZONTAL);
+    firstRowSizer->Add(m_icon, wxSizerFlags{}.Centre().Border());
+    firstRowSizer->Add(m_text, wxSizerFlags{}.Proportion(1).Centre());
+    firstRowSizer->AddSpacer(0); // This spacer only exists for compatibility.
+    firstRowSizer->Add(m_button, wxSizerFlags{}.Centre().Border());
+
+    wxBoxSizer* const secondRowSizer = new wxBoxSizer(wxHORIZONTAL);
+    secondRowSizer->Add(m_checkbox, wxSizerFlags{}.CentreVertical().Border());
+
+    defaultControlSizer->Add(firstRowSizer, wxSizerFlags{ 1 }.Expand());
+    defaultControlSizer->Add(secondRowSizer);
+    defaultControlSizer->Show(m_checkbox, !m_checkbox->GetLabel().empty(), true);
+
+    sizer->Add(defaultControlSizer, wxSizerFlags{ 1 }.Expand());
+
     SetSizer(sizer);
 
     return true;
@@ -223,6 +245,8 @@ void wxInfoBarGeneric::UpdateParent()
 
 void wxInfoBarGeneric::DoHide()
 {
+    TransferDataFromWindow();
+
     HideWithEffect(GetHideEffect(), GetEffectDuration());
 
     UpdateParent();
@@ -230,6 +254,8 @@ void wxInfoBarGeneric::DoHide()
 
 void wxInfoBarGeneric::DoShow()
 {
+    TransferDataToWindow();
+
     // re-layout the parent first so that the window expands into an already
     // unoccupied by the other controls area: for this we need to change our
     // internal visibility flag to force Layout() to take us into account (an
@@ -464,6 +490,18 @@ void wxInfoBarGeneric::RemoveButton(wxWindowID btnid)
 void wxInfoBarGeneric::OnButton(wxCommandEvent& WXUNUSED(event))
 {
     DoHide();
+}
+
+void wxInfoBarGeneric::ShowCheckBox(const wxString& checkBoxText, bool checked)
+{
+    wxASSERT_MSG( HasFlag(wxINFOBAR_CHECKBOX),
+                  "wxINFOBAR_CHECKBOX style should be set if calling ShowCheckBox()!");
+    m_checked = checked;
+    if (m_checkbox != nullptr)
+    {
+        m_checkbox->SetLabel(checkBoxText);
+        GetSizer()->Show(m_checkbox, !checkBoxText.empty(), true);
+    }
 }
 
 #endif // wxUSE_INFOBAR

src/gtk/infobar.cpp
@@ -75,17 +75,6 @@ public:
 // local functions
 // ----------------------------------------------------------------------------
 
-namespace
-{
-
-inline bool UseNative()
-{
-    // native GtkInfoBar widget is only available in GTK+ 2.18 and later
-    return wx_is_at_least_gtk2(18);
-}
-
-} // anonymous namespace
-
 extern "C"
 {
 
@@ -108,10 +97,19 @@ static void wxgtk_infobar_close(GtkInfoBar * WXUNUSED(infobar),
 // wxInfoBar implementation
 // ============================================================================
 
-bool wxInfoBar::Create(wxWindow *parent, wxWindowID winid)
+bool wxInfoBar::UseNative() const
 {
+    // Native GtkInfoBar widget is only available in GTK+ 2.18 and later.
+    // Also, if the checkbox is included, then we need to use
+    // the generic version.
+    return wx_is_at_least_gtk2(18) && !HasFlag(wxINFOBAR_CHECKBOX);
+}
+
+bool wxInfoBar::Create(wxWindow *parent, wxWindowID winid, long style)
+{
+    SetWindowStyle(style);
     if ( !UseNative() )
-        return wxInfoBarGeneric::Create(parent, winid);
+        return wxInfoBarGeneric::Create(parent, winid, style);
 
     m_impl = new wxInfoBarGTKImpl;
 

docs/doxygen/overviews/xrc_format.h
@@ -1394,11 +1394,21 @@ page.
      this property is of class "button" has name (can be one of standard
      button ID) and has optional label property. If no buttons are added
      to the info bar, the default "Close" button will be shown.}
+@row3col{checkboxlabel, @ref overview_xrcformat_type_text,
+     Shows a checkbox at the bottom of the infobar with the specified label.
+     Note that specifying this attribute automatically turns on @c
+     wxINFOBAR_CHECKBOX style (optional). @since 3.3.0.}
+@row3col{checked, @ref overview_xrcformat_type_bool,
+     If showing the checkbox, indicates whether it should be checked by
+     default (optional). @since 3.3.0.}
 @endTable
 
 Example:
 @code
 <object class="wxInfoBar">
+    <style>wxINFOBAR_CHECKBOX</style>
+    <checkboxlabel>Do not show this again</checkboxlabel>
+    <checked>1</checked>
     <effectduration>1000</effectduration>
     <showeffect>wxSHOW_EFFECT_EXPAND</showeffect>
     <hideeffect>wxSHOW_EFFECT_SLIDE_TO_RIGHT</hideeffect>

include/wx/generic/infobar.h
@@ -13,10 +13,16 @@
 class WXDLLIMPEXP_FWD_CORE wxBitmapButton;
 class WXDLLIMPEXP_FWD_CORE wxStaticBitmap;
 class WXDLLIMPEXP_FWD_CORE wxStaticText;
+class WXDLLIMPEXP_FWD_CORE wxCheckBox;
 
 // ----------------------------------------------------------------------------
 // wxInfoBar
 // ----------------------------------------------------------------------------
+
+enum
+{
+    wxINFOBAR_CHECKBOX = 0x0010
+};
 
 class WXDLLIMPEXP_CORE wxInfoBarGeneric : public wxInfoBarBase
 {
@@ -25,13 +31,13 @@ public:
     // hidden
     wxInfoBarGeneric() { Init(); }
 
-    wxInfoBarGeneric(wxWindow *parent, wxWindowID winid = wxID_ANY)
+    wxInfoBarGeneric(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0)
     {
         Init();
-        Create(parent, winid);
+        Create(parent, winid, style);
     }
 
-    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY);
+    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0);
 
 
     // implement base class methods
@@ -75,6 +81,14 @@ public:
     // get the currently used effect animation duration
     int GetEffectDuration() const { return m_effectDuration; }
 
+    // Whether the checkbox was checked at the time of the window
+    // being closed.
+    // This should be called in a client's handler for the
+    // wxID_CLOSE button being clicked.
+    bool IsCheckBoxChecked() const { return m_checked; }
+
+    // Sets whether the checkbox should be shown.
+    void ShowCheckBox(const wxString& checkBoxText, bool checked);
 
     // overridden base class methods
     // -----------------------------
@@ -100,6 +114,8 @@ private:
     // common part of all ctors
     void Init();
 
+    virtual bool UseNative() const { return false; }
+
     // handler for the close button
     void OnButton(wxCommandEvent& event);
 
@@ -120,9 +136,10 @@ private:
 
 
     // different controls making up the bar
-    wxStaticBitmap *m_icon;
-    wxStaticText *m_text;
-    wxBitmapButton *m_button;
+    wxStaticBitmap *m_icon = nullptr;
+    wxStaticText *m_text = nullptr;
+    wxBitmapButton *m_button = nullptr;
+    wxCheckBox *m_checkbox = nullptr;
 
     // the effects to use when showing/hiding and duration for them: by default
     // the effect is determined by the info bar automatically depending on its
@@ -130,6 +147,8 @@ private:
     wxShowEffect m_showEffect,
                  m_hideEffect;
     int m_effectDuration;
+
+    bool m_checked = false;
 
     wxDECLARE_EVENT_TABLE();
     wxDECLARE_NO_COPY_CLASS(wxInfoBarGeneric);

include/wx/gtk/infobar.h
@@ -16,21 +16,21 @@
 // wxInfoBar for GTK+
 // ----------------------------------------------------------------------------
 
-// notice that the native GTK+ implementation is only available since
-// (relatively recent) 2.18 so we inherit from the generic one to be able to
+// notice that the native GTK+ implementation is only available since 2.18,
+// so we inherit from the generic one to be able to
 // fall back to it if GTK+ version is determined to be too old during run-time
 class WXDLLIMPEXP_CORE wxInfoBar : public wxInfoBarGeneric
 {
 public:
     wxInfoBar() { Init(); }
 
-    wxInfoBar(wxWindow *parent, wxWindowID winid = wxID_ANY)
+    wxInfoBar(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0)
     {
         Init();
-        Create(parent, winid);
+        Create(parent, winid, style);
     }
 
-    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY);
+    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0);
 
     virtual ~wxInfoBar();
 
@@ -61,6 +61,8 @@ protected:
 
 private:
     void Init() { m_impl = nullptr; }
+
+    virtual bool UseNative() const override;
 
     // add a button with the given id/label and return its widget
     GtkWidget *GTKAddButton(wxWindowID btnid,

interface/wx/infobar.h
@@ -7,6 +7,18 @@
 /////////////////////////////////////////////////////////////////////////////
 
 /**
+    Styles to pass to the constructor or Create().
+
+    @since 3.3.0
+ */
+enum
+{
+    /// Include support for showing a customizable checkbox()
+    /// at the bottom of a wxInfoBar.
+    wxINFOBAR_CHECKBOX = 0x0080
+};
+
+/**
     An info bar is a transient window shown at top or bottom of its parent
     window to display non-critical information to the user.
 
@@ -86,7 +98,7 @@ public:
 
         @see Create()
      */
-    wxInfoBar(wxWindow *parent, wxWindowID winid = wxID_ANY);
+    wxInfoBar(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0);
 
     /**
         Create the info bar window.
@@ -106,8 +118,10 @@ public:
         @param winid
             The id of the info bar window, usually unused as currently no
             events are generated by this class.
+        @param style
+            Optional styles to apply to the control.
      */
-    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY);
+    bool Create(wxWindow *parent, wxWindowID winid = wxID_ANY, long style = 0);
 
     /**
         Add a button to be shown in the info bar.
@@ -271,6 +285,33 @@ public:
 
     /// Return the effect animation duration currently used.
     int GetEffectDuration() const;
+
+    /**
+        Return whether the checkbox was checked at the time of the window being
+        closed.
+
+        This function is typically used in the button handler for the @c
+        wxEVT_BUTTON message for @c wxID_CLOSE, which would typically call
+        Dismiss() to hide the info bar and then call this function to see if
+        the checkbox was checked and save its value to reuse it the next time
+        (e.g. to avoid showing the same message again in a common use case).
+
+        See the dialogs sample for an example.
+
+        @since 3.3.0
+     */
+    bool IsCheckBoxChecked() const;
+
+    /**
+        Sets whether the checkbox should be shown, its label, and whether it is
+        checked by default.
+
+        @note The wxINFOBAR_CHECKBOX style must be used for this control
+        if calling this function.
+
+        @since 3.3.0
+     */
+    void ShowCheckBox(const wxString& checkBoxText, bool checked);
 
     /**
         Overridden base class methods changes the font of the text message.

misc/schema/xrc_schema.rnc
@@ -1083,6 +1083,8 @@ wxInfoBar =
         [xrc:p="o"] element effectduration {_, t_integer }* &
         [xrc:p="o"] element showeffect     {_, t_showeffect }* &
         [xrc:p="o"] element hideeffect     {_, t_showeffect }* &
+        [xrc:p="o"] element checkboxlabel  {_, t_text }* &
+        [xrc:p="o"] element checked        {_, t_bool }* &
         element object {
             attribute class { "button" } &
             attribute name  { t_identifier }? &

samples/dialogs/dialogs.cpp
@@ -721,7 +721,7 @@ MyFrame::MyFrame(const wxString& title)
 
 #if wxUSE_INFOBAR
     // an info bar can be created very simply and used without any extra effort
-    m_infoBarSimple = new wxInfoBar(this);
+    m_infoBarSimple = new wxInfoBar(this, wxID_ANY, wxINFOBAR_CHECKBOX);
 
     // or it can also be customized by
     m_infoBarAdvanced = new wxInfoBar(this);
@@ -942,6 +942,13 @@ void MyFrame::LogDialog(wxCommandEvent& WXUNUSED(event))
 void MyFrame::InfoBarSimple(wxCommandEvent& WXUNUSED(event))
 {
     static int s_count = 0;
+    static bool s_dontShowAgain = false;
+
+    if (s_dontShowAgain)
+    {
+        wxMessageBox("You asked to not see this again. Remember?");
+        return;
+    }
 
     wxString msg;
     if ( ++s_count % 2 )
@@ -959,7 +966,21 @@ void MyFrame::InfoBarSimple(wxCommandEvent& WXUNUSED(event))
                    s_count);
     }
 
+    m_infoBarSimple->ShowCheckBox(_("Do not show this again"), false);
     m_infoBarSimple->ShowMessage(msg);
+    // intercept the close button being clicked
+    m_infoBarSimple->Bind(wxEVT_BUTTON,
+        [this](wxCommandEvent& WXUNUSED(event))
+        {
+            // dismiss the message and (if a generic control)
+            // see if the "don't show this again" checkbox
+            // was checked and handle that for next time
+            m_infoBarSimple->Dismiss();
+            if (m_infoBarSimple->IsCheckBoxChecked())
+            {
+                s_dontShowAgain = true;
+            };
+        }, wxID_CLOSE);
 }
 
 void MyFrame::InfoBarAdvanced(wxCommandEvent& WXUNUSED(event))

commit c1fa7055f644d6f6820cde0071b9c468a1635693

Fix wxInfoBar buttons regression after custom checkbox addition
Update the functions dealing with buttons to take into account the extra
sizer added in cd07eabd54 (Add support for "Do not show this again"
checkbox to wxInfoBar, 2025-05-12) which broke the existing logic.

See #25394, #25461.

Closes #25489.

Closes #25493.

src/generic/infobar.cpp
@@ -138,7 +138,6 @@ bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid, long style)
     firstRowSizer->Add(m_icon, wxSizerFlags{}.Centre().Border());
     firstRowSizer->Add(m_text, wxSizerFlags{}.Proportion(1).Centre());
     firstRowSizer->AddSpacer(0); // This spacer only exists for compatibility.
-    firstRowSizer->Add(m_button, wxSizerFlags{}.Centre().Border());
 
     wxBoxSizer* const secondRowSizer = new wxBoxSizer(wxHORIZONTAL);
     secondRowSizer->Add(m_checkbox, wxSizerFlags{}.CentreVertical().Border());
@@ -148,6 +147,7 @@ bool wxInfoBarGeneric::Create(wxWindow *parent, wxWindowID winid, long style)
     defaultControlSizer->Show(m_checkbox, !m_checkbox->GetLabel().empty(), true);
 
     sizer->Add(defaultControlSizer, wxSizerFlags{ 1 }.Expand());
+    sizer->Add(m_button, wxSizerFlags{}.Centre().Border());
 
     SetSizer(sizer);
 
@@ -355,9 +355,10 @@ size_t wxInfoBarGeneric::GetButtonCount() const
     {
         const wxSizerItem * const item = node->GetData();
 
-        // if we reached the spacer separating the buttons from the text
-        // break the for-loop.
-        if ( item->IsSpacer() )
+        // If we reached the spacer separating the buttons from the text
+        // break the for-loop. Same for if we get to the sizer holding
+        // the icon and text.
+        if ( item->IsSpacer() || item->IsSizer() )
             break;
 
         // if the standard button is shown, there must be no other ones
@@ -389,12 +390,12 @@ wxWindowID wxInfoBarGeneric::GetButtonId(size_t idx) const
     {
         const wxSizerItem * const item = node->GetData();
 
-        if ( item->IsSpacer() )
+        if ( item->IsSpacer() || item->IsSizer() )
             foundSpacer = true;
 
         if ( foundSpacer )
         {
-            if ( !item->IsSpacer() )
+            if ( !item->IsSpacer() && !item->IsSizer() )
             {
                 if ( count == idx )
                 {
@@ -433,7 +434,7 @@ bool wxInfoBarGeneric::HasButtonId(wxWindowID btnid) const
 
         // if we reached the spacer separating the buttons from the text
         // then the wanted ID is not inside.
-        if ( item->IsSpacer() )
+        if ( item->IsSpacer() || item->IsSizer() )
             return false;
 
         // check if we found our button
@@ -462,7 +463,7 @@ void wxInfoBarGeneric::RemoveButton(wxWindowID btnid)
         // if we reached the spacer separating the buttons from the text
         // preceding them without finding our button, it must mean it's not
         // there at all
-        if ( item->IsSpacer() )
+        if ( item->IsSpacer() || item->IsSizer() )
         {
             wxFAIL_MSG( wxString::Format("button with id %d not found", btnid) );
             return;

build/cmake/tests/gui/CMakeLists.txt
@@ -48,6 +48,7 @@ set(TEST_GUI_SRC
     controls/htmllboxtest.cpp
     controls/hyperlinkctrltest.cpp
     controls/itemcontainertest.cpp
+    controls/infobar.cpp
     controls/label.cpp
     controls/listbasetest.cpp
     controls/listbooktest.cpp

tests/Makefile.in
@@ -217,6 +217,7 @@ TEST_GUI_OBJECTS =  \
 	test_gui_htmllboxtest.o \
 	test_gui_hyperlinkctrltest.o \
 	test_gui_itemcontainertest.o \
+	test_gui_infobar.o \
 	test_gui_label.o \
 	test_gui_listbasetest.o \
 	test_gui_listbooktest.o \
@@ -1067,6 +1068,9 @@ test_gui_hyperlinkctrltest.o: $(srcdir)/controls/hyperlinkctrltest.cpp $(TEST_GU
 
 test_gui_itemcontainertest.o: $(srcdir)/controls/itemcontainertest.cpp $(TEST_GUI_ODEP)
 	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/itemcontainertest.cpp
+
+test_gui_infobar.o: $(srcdir)/controls/infobar.cpp $(TEST_GUI_ODEP)
+	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/infobar.cpp
 
 test_gui_label.o: $(srcdir)/controls/label.cpp $(TEST_GUI_ODEP)
 	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/label.cpp

tests/controls/infobar.cpp
@@ -1,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+// Name:        tests/controls/infobar.cpp
+// Purpose:     wxInfoBar  tests
+// Author:      Blake Madden
+// Created:     2025-6-02
+// Copyright:   (c) 2025 Blake Madden
+///////////////////////////////////////////////////////////////////////////////
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+
+#include "testprec.h"
+
+#ifndef WX_PRECOMP
+    #include "wx/app.h"
+#endif // WX_PRECOMP
+
+#include "wx/infobar.h"
+
+#include <memory>
+
+TEST_CASE("wxInfoBar::Buttons", "[wxInfoBar]")
+{
+    const std::unique_ptr<wxInfoBar>
+        info(new wxInfoBar(wxTheApp->GetTopWindow(), wxID_ANY, wxINFOBAR_CHECKBOX));
+
+    CHECK(info->GetButtonCount() == 0);
+
+    const int buttonId = wxID_HIGHEST + 1000;
+    info->AddButton(buttonId, "test");
+
+    CHECK(info->GetButtonCount() == 1);
+    CHECK(info->GetButtonId(0) == buttonId);
+    CHECK(info->HasButtonId(buttonId));
+
+    info->RemoveButton(buttonId);
+    CHECK(info->GetButtonCount() == 0);
+}

tests/descrip.mms
@@ -578,6 +578,9 @@ test_gui_itemcontainertest.obj : [.controls]itemcontainertest.cpp
 test_gui_label.obj : [.controls]label.cpp 
 	$(CXXC) /object=[]$@ $(TEST_GUI_CXXFLAGS) [.controls]label.cpp
 
+test_gui_infobar.obj : [.controls]infobar.cpp 
+	$(CXXC) /object=[]$@ $(TEST_GUI_CXXFLAGS) [.controls]infobar.cpp
+
 test_gui_listbasetest.obj : [.controls]listbasetest.cpp 
 	$(CXXC) /object=[]$@ $(TEST_GUI_CXXFLAGS) [.controls]listbasetest.cpp
 

tests/makefile.gcc
@@ -1013,6 +1013,9 @@ $(OBJS)\test_gui_hyperlinkctrltest.o: ./controls/hyperlinkctrltest.cpp
 $(OBJS)\test_gui_itemcontainertest.o: ./controls/itemcontainertest.cpp
 	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
 
+$(OBJS)\test_gui_infobar.o: ./controls/infobar.cpp
+	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
+
 $(OBJS)\test_gui_label.o: ./controls/label.cpp
 	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
 

tests/makefile.vc
@@ -1314,6 +1314,9 @@ $(OBJS)\test_gui_hyperlinkctrltest.obj: .\controls\hyperlinkctrltest.cpp
 $(OBJS)\test_gui_itemcontainertest.obj: .\controls\itemcontainertest.cpp
 	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\itemcontainertest.cpp
 
+$(OBJS)\test_gui_infobar.obj: .\controls\infobar.cpp
+	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\infobar.cpp
+
 $(OBJS)\test_gui_label.obj: .\controls\label.cpp
 	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\label.cpp
 

tests/test.bkl
@@ -230,6 +230,7 @@
             controls/htmllboxtest.cpp
             controls/hyperlinkctrltest.cpp
             controls/itemcontainertest.cpp
+            controls/infobar.cpp
             controls/label.cpp
             controls/listbasetest.cpp
             controls/listbooktest.cpp

tests/test_gui.vcxproj
@@ -932,6 +932,7 @@
     <ClCompile Include="controls\htmllboxtest.cpp" />
     <ClCompile Include="controls\hyperlinkctrltest.cpp" />
     <ClCompile Include="controls\itemcontainertest.cpp" />
+    <ClCompile Include="controls\infobar.cpp" />
     <ClCompile Include="controls\label.cpp" />
     <ClCompile Include="controls\listbasetest.cpp" />
     <ClCompile Include="controls\listbooktest.cpp" />

tests/test_gui.vcxproj.filters
@@ -131,6 +131,9 @@
     <ClCompile Include="image\image.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="controls\infobar.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
     <ClCompile Include="controls\itemcontainertest.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>