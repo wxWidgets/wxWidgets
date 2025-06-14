18da5b8269706472f6c0fa09dcbae605a477da19
Vadim Zeitlin <vadim@wxwidgets.org>
2025-06-15 01:57:27 +0200

    Add persistence support for groups of wxRadioButton controls
    Allow to easily save and restore the selected radio button.
    
    Closes #25530.

--- tests/allheaders.h
+++ tests/allheaders.h
@@ -268,6 +268,7 @@
 #include <wx/persist/checkbox.h>
 #include <wx/persist/combobox.h>
 #include <wx/persist/dataview.h>
+#include <wx/persist/radiobut.h>
 #include <wx/persist/splitter.h>
 #include <wx/persist/toplevel.h>
 #include <wx/persist/treebook.h>
