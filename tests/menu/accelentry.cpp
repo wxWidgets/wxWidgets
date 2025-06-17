wxWidgets/wxWidgets

commit 4cc13e1099fe34d3a0964df187495b6ab79b88a8

Use vector in wxMSW wxAcceleratorTable code
No real changes, just replace manually-allocated buffer with a vector.

src/msw/accel.cpp
@@ -31,6 +31,8 @@
 #include "wx/msw/private.h"
 #include "wx/msw/private/keyboard.h"
 
+#include <vector>
+
 wxIMPLEMENT_DYNAMIC_CLASS(wxAcceleratorTable, wxObject);
 
 // ----------------------------------------------------------------------------
@@ -95,7 +97,7 @@ wxAcceleratorTable::wxAcceleratorTable(int n, const wxAcceleratorEntry entries[]
 {
     m_refData = new wxAcceleratorRefData;
 
-    ACCEL* arr = new ACCEL[n];
+    std::vector<ACCEL> arr(n);
     for ( int i = 0; i < n; i++ )
     {
         int flags = entries[i].GetFlags();
@@ -115,8 +117,7 @@ wxAcceleratorTable::wxAcceleratorTable(int n, const wxAcceleratorEntry entries[]
         arr[i].cmd = (WORD)entries[i].GetCommand();
     }
 
-    M_ACCELDATA->m_hAccel = ::CreateAcceleratorTable(arr, n);
-    delete[] arr;
+    M_ACCELDATA->m_hAccel = ::CreateAcceleratorTable(&arr[0], n);
 
     M_ACCELDATA->m_ok = (M_ACCELDATA->m_hAccel != 0);
 }

commit 750b2a47ae774b03fd823843bd63be6159f19a21

Fix creating wxAcceleratorTable with 0 entries
This was broken in 4cc13e1099 (Use vector in wxMSW wxAcceleratorTable
code, 2023-05-15), make it work again now and add a unit test ensuring
that it doesn't get broken again.

Also document the current behaviour.

Closes #25517.

interface/wx/accel.h
@@ -200,7 +200,8 @@ public:
         Initializes the accelerator table from an array of wxAcceleratorEntry.
 
         @param n
-            Number of accelerator entries.
+            Number of accelerator entries. If this parameter is 0, invalid
+            accelerator table is created, i.e. IsOk() will return @false.
         @param entries
             The array of entries.
 

src/msw/accel.cpp
@@ -91,6 +91,13 @@ wxAcceleratorTable::wxAcceleratorTable(const wxString& resource)
 // Create from an array
 wxAcceleratorTable::wxAcceleratorTable(int n, const wxAcceleratorEntry entries[])
 {
+    if ( n == 0 )
+    {
+        // This is valid but useless.
+        return;
+    }
+
+    wxCHECK_RET( n > 0, "Invalid number of accelerator entries" );
 
     std::vector<ACCEL> arr(n);
     for ( int i = 0; i < n; i++ )

tests/menu/accelentry.cpp
@@ -105,3 +105,17 @@ TEST_CASE( "wxAcceleratorEntry::StringTests", "[accelentry]" )
         CHECK( !a.FromString("bloordyblop") );
     }
 }
+
+TEST_CASE( "wxAcceleratorTable::Create", "[accelentry]" )
+{
+    CHECK( !wxAcceleratorTable(0, nullptr).IsOk() );
+
+    const wxAcceleratorEntry entries[] =
+    {
+        wxAcceleratorEntry(wxACCEL_CTRL, 'A'),
+        wxAcceleratorEntry(wxACCEL_ALT, 'B'),
+        wxAcceleratorEntry(wxACCEL_SHIFT, 'C')
+    };
+
+    CHECK( wxAcceleratorTable(WXSIZEOF(entries), entries).IsOk() );
+}