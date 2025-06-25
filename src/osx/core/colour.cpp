c7e5ac12e69b6b51605c6e2543f29e638cc41f91
Vadim Zeitlin <vadim@wxwidgets.org>
2025-06-26 00:24:07 +0200

    Fix memory leak in wxColour::Set() in wxOSX
    Don't overwrite the existing m_refData in wxColour::InitRGBA()
    implementation, as this could result in leaking it.
    
    Ensure that we are its unique owners, which allocates it if necessary,
    and then assign to the existing object instead.
    
    Closes #25569.

--- src/osx/core/colour.cpp
+++ src/osx/core/colour.cpp
@@ -31,6 +31,8 @@ public:
 
     wxCGColorRefData(const wxCGColorRefData& other);
 
+    void Init(CGFloat components[4]);
+
     virtual bool IsOk() const  override{ return m_cgColour != nullptr; }
 
     virtual CGFloat Red() const override { return m_red; }
@@ -43,16 +45,12 @@ public:
     virtual wxColourRefData* Clone() const override { return new wxCGColorRefData(*this); }
 
 private:
-    void Init(CGFloat components[4]);
-
     wxCFRef<CGColorRef> m_cgColour;
 
     CGFloat m_red;
     CGFloat m_blue;
     CGFloat m_green;
     CGFloat m_alpha;
-
-    wxDECLARE_NO_ASSIGN_CLASS(wxCGColorRefData);
 };
 
 wxCGColorRefData::wxCGColorRefData(CGFloat r, CGFloat g, CGFloat b, CGFloat a)
@@ -141,7 +139,7 @@ wxCGColorRefData::wxCGColorRefData(CGColorRef col)
     }
 }
 
-#define M_COLDATA static_cast<wxColourRefData*>(m_refData)
+#define M_COLDATA static_cast<wxCGColorRefData*>(m_refData)
 
 #if wxOSX_USE_COCOA_OR_CARBON
 wxColour::wxColour(const RGBColor& col)
@@ -230,8 +228,10 @@ WX_NSImage wxColour::OSXGetNSPatternImage() const
 
 void wxColour::InitRGBA(ChannelType r, ChannelType g, ChannelType b, ChannelType a)
 {
+    AllocExclusive();
+
     CGFloat components[4] = { (CGFloat)(r / 255.0), (CGFloat)(g / 255.0), (CGFloat)(b / 255.0), (CGFloat)(a / 255.0) };
-    m_refData = new wxCGColorRefData(components);
+    M_COLDATA->Init(components);
 }
 
 bool wxColour::operator==(const wxColour& other) const
