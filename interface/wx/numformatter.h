8d070ba795c2fa0deba9db5d9a435c586ec67388
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-07-08 19:12:56 -0400

    Just make RemoveTrailingZeroes public

--- interface/wx/numformatter.h
+++ interface/wx/numformatter.h
@@ -136,39 +136,6 @@ public:
     static wxChar GetDecimalSeparator();
 
     /**
-        Get the thousands separator if grouping of the digits is used by the
-        current UI locale.
-
-        The value returned in @a sep should be only used if the function
-        returns @true, otherwise no thousands separator should be used at all.
-
-        @param sep
-            Points to the variable receiving the thousands separator character
-            if it is used by the current UI locale. May be @NULL if only the
-            function return value is needed.
-     */
-    static bool GetThousandsSeparatorIfUsed(wxChar *sep);
-
-    /**
-        Add thousands separators to a string representing a number
-        that currently doesn't have any.
-
-        @param[in,out] str The string to add thousands separators to.
-
-        @since 3.3.1
-     */
-    static void AddThousandsSeparators(wxString& str);
-
-    /**
-        Remove all thousands separators from a string representing a number.
-
-        @param[in,out] str The string to remove thousands separators from.
-
-        @since 3.3.1
-     */
-    static void RemoveThousandsSeparators(wxString& str);
-
-    /**
         Remove trailing zeroes and, if there is nothing left after it, the
         decimal separator itself from a string representing a floating point
         number.
