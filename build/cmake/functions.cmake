e6c7c16c4597271577299a85fd7ef00035b77104
Maarten Bent <MaartenBent@users.noreply.github.com>
2025-07-15 21:44:44 +0200

    CMake: Always use MSVC naming with MSVC compiler
    Fixes #25592

--- build/cmake/functions.cmake
+++ build/cmake/functions.cmake
@@ -14,7 +14,9 @@ include(CMakePrintHelpers)
 
 # Use the MSVC/makefile naming convention, or the configure naming convention,
 # this is the same check as used in FindwxWidgets.
-if(WIN32 AND NOT CYGWIN AND NOT MSYS AND NOT CMAKE_CROSSCOMPILING)
+if(MSVC)
+    set(WIN32_MSVC_NAMING 1)
+elseif(WIN32 AND NOT CYGWIN AND NOT MSYS AND NOT CMAKE_CROSSCOMPILING)
     set(WIN32_MSVC_NAMING 1)
 else()
     set(WIN32_MSVC_NAMING 0)
