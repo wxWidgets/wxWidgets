fdbd2570336076a3b2cecb6d0e8b4e181d4880f6
Maarten Bent <MaartenBent@users.noreply.github.com>
2025-08-21 21:17:59 +0200

    Calculate bounding box in more wxSVGFileDC drawing functions

--- src/common/dcsvg.cpp
+++ src/common/dcsvg.cpp
@@ -1007,6 +1007,9 @@ void wxSVGFileDCImpl::DoDrawArc(wxCoord x1, wxCoord y1, wxCoord x2, wxCoord y2, 
         GetRenderMode(m_renderingMode), GetPenPattern(m_pen));
 
     write(s);
+
+    double r = wxMax(r1, r2);
+    CalcBoundingBox(xc - r, yc - r, xc + r, yc + r);
 }
 
 void wxSVGFileDCImpl::DoDrawEllipticArc(wxCoord x, wxCoord y, wxCoord w, wxCoord h, double sa, double ea)
@@ -1101,6 +1104,8 @@ void wxSVGFileDCImpl::DoDrawEllipticArc(wxCoord x, wxCoord y, wxCoord w, wxCoord
     wxString arcLine = wxString::Format(wxS("%s\" %s %s/>\n"),
         arcPath, GetRenderMode(m_renderingMode), GetPenPattern(m_pen));
     write(arcLine);
+
+    CalcBoundingBox(x, y, x + w, y + h);
 }
 
 void wxSVGFileDCImpl::DoGradientFillLinear(const wxRect& rect,
@@ -1410,6 +1415,8 @@ void wxSVGFileDCImpl::DoDrawBitmap(const wxBitmap& bmp, wxCoord x, wxCoord y,
                                    bool WXUNUSED(useMask))
 {
     NewGraphicsIfNeeded();
+
+    CalcBoundingBox(x, y, x + bmp.GetWidth(), y + bmp.GetHeight());
 
     // If we don't have any bitmap handler yet, use the default one.
     if ( !m_bmp_handler )
