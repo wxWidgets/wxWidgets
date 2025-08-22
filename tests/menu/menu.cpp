wxWidgets/wxWidgets

commit 0dfe37a6438ffdd20d1685e41192e28e23a64063

Add wxMSW-specific wxBitmapBundle accessors to wxMenuItem
Provide GetBitmapBundle(bool checked) and GetDisabledBitmapBundle() too
in wxMSW, for consistency with the existing MSW-only member functions
returning bitmaps.

See #25705.

Closes #25685.

include/wx/msw/menuitem.h
@@ -83,6 +83,11 @@ public:
         DoSetBitmap(bmp, bChecked);
     }
 
+    wxBitmapBundle GetBitmapBundle(bool bChecked) const
+    {
+        return bChecked ? m_bitmap : m_bmpUnchecked;
+    }
+
     wxBitmap GetBitmap(bool bChecked) const;
 
 #if wxUSE_OWNER_DRAWN
@@ -91,6 +96,8 @@ public:
         m_bmpDisabled = bmpDisabled;
         SetOwnerDrawn(true);
     }
+
+    wxBitmapBundle GetDisabledBitmapBundle() const { return m_bmpDisabled; }
 
     wxBitmap GetDisabledBitmap() const;
 

interface/wx/menuitem.h
@@ -179,6 +179,9 @@ public:
     /**
         Returns the checked or unchecked bitmap.
 
+        Prefer using GetBitmapBundle() overload taking @a checked parameter in
+        the new code.
+
         This overload only exists in wxMSW, avoid using it in portable code.
     */
     wxBitmap GetBitmap(bool checked) const;
@@ -196,11 +199,32 @@ public:
     wxBitmapBundle GetBitmapBundle() const;
 
     /**
+        Returns the bitmap bundle containing the checked or unchecked bitmap
+        for this item.
+
+        @onlyfor{wxmsw}
+
+        @since 3.3.2
+     */
+    wxBitmapBundle GetBitmapBundle(bool checked) const;
+
+    /**
         Returns the bitmap used for disabled items.
+
+        @see GetDisabledBitmapBundle()
 
         @onlyfor{wxmsw}
     */
     virtual wxBitmap GetDisabledBitmap() const;
+
+    /**
+        Returns the bitmap bundle used for disabled items.
+
+        @onlyfor{wxmsw}
+
+        @since 3.3.2
+    */
+    wxBitmap GetDisabledBitmapBundle() const;
 
     /**
         Returns the font associated with the menu item.

src/msw/menuitem.cpp
@@ -663,7 +663,7 @@ void wxMenuItem::SetItemLabel(const wxString& txt)
 
 wxBitmap wxMenuItem::GetBitmap(bool bChecked) const
 {
-    wxBitmap bmp = GetBitmapFromBundle(bChecked ? m_bitmap : m_bmpUnchecked);
+    wxBitmap bmp = GetBitmapFromBundle(GetBitmapBundle(bChecked));
 #if wxUSE_IMAGE
     if ( bmp.IsOk() )
     {

commit 8d349dbddc504ac10daff8392617c7ab547f82b5

Make it possible to call wxMenuItem::GetBitmapBundle() again
The changes of 0dfe37a643 (Add wxMSW-specific wxBitmapBundle accessors
to wxMenuItem, 2025-08-18) broke this as wxMSW-specific function taking
bool hid the base class function.

Just make the wxMSW function compatible with the base class one by
defaulting "bChecked" to false. Alternatively, we could use using
declaration to bring the base class function in scope, but this doesn't
seem to have any advantages and this change seems more clear.

In any case, add a unit test checking that the calls to these functions
at least compile.

See #25685, #25705.

Closes #25719.

include/wx/msw/menuitem.h
@@ -83,6 +83,7 @@ public:
         DoSetBitmap(bmp, bChecked);
     }
 
+    using wxMenuItemBase::GetBitmapBundle;
     wxBitmapBundle GetBitmapBundle(bool bChecked) const
     {
         return bChecked ? m_bitmap : m_bmpUnchecked;

tests/menu/menu.cpp
@@ -849,4 +849,20 @@ TEST_CASE( "wxMenuItemAccelEntry", "[menu][accelentry]" )
     }
 }
 
+TEST_CASE("wxMenuItemAccessors", "[menu]")
+{
+    wxMenu menu;
+
+    wxMenuItem* const item = menu.AppendCheckItem(wxID_ANY, "Check");
+
+    // Just check that we can call various functions.
+    CHECK( !item->GetBitmapBundle().IsOk() );
+    CHECK( !item->GetBitmap().IsOk() );
+
+#ifdef __WXMSW__
+    CHECK( !item->GetBitmapBundle(true).IsOk() );
+    CHECK( !item->GetBitmap(true).IsOk() );
+#endif // __WXMSW__
+}
+
 #endif