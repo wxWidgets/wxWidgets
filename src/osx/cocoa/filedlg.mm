wxWidgets/wxWidgets

commit 426f745c6b069e9cd71a47a2272f5d6e07a9c6df

Fix showing extra controls in wxFileDialog under macOS
These controls were not shown due to a forgotten colon in the test for
setAccessoryViewDisclosed support added in e765756555 (OSX file dialog
extensions (#2592), 2021-11-26).

Fix this simply by adding the colon, which is part of the method name.

Closes #25717.

src/osx/cocoa/filedlg.mm
@@ -513,7 +513,7 @@ void wxFileDialog::SetupExtraControls(WXWindow nativeWindow)
         [panel setAccessoryView:accView];
 
         wxCLANG_WARNING_SUPPRESS(undeclared-selector)
-        if ([panel respondsToSelector:@selector(setAccessoryViewDisclosed)])
+        if ([panel respondsToSelector:@selector(setAccessoryViewDisclosed:)])
         {
             [(id)panel setAccessoryViewDisclosed:YES];
         }

commit 32ea27c34f4b88eb6d407c223d0b91c5c884554b

Do not call setAccessoryViewDisclosed: for NSSavePanel
This results in an immediate application exit (thanks Apple) and is
unnecessary anyhow because save panels, unlike open ones, show the
accessory view by default.

This fixes fatal problem introduced in the recent 426f745c6b (Fix
showing extra controls in wxFileDialog under macOS, 2025-08-22),
see #25717.

Co-authored-by: Stefan Csomor <csomor@advancedconcepts.ch>

src/osx/cocoa/filedlg.mm
@@ -512,8 +512,12 @@ void wxFileDialog::SetupExtraControls(WXWindow nativeWindow)
         [accView removeFromSuperview];
         [panel setAccessoryView:accView];
 
+        // We need to explicitly show the accessory view for "Open" file
+        // dialogs, but doing it for "Save" dialogs is not only unnecessary but
+        // results in an immediate application abort (see #25717).
         wxCLANG_WARNING_SUPPRESS(undeclared-selector)
-        if ([panel respondsToSelector:@selector(setAccessoryViewDisclosed:)])
+        if (!HasFlag(wxFD_SAVE) &&
+                [panel respondsToSelector:@selector(setAccessoryViewDisclosed:)])
         {
             [(id)panel setAccessoryViewDisclosed:YES];
         }