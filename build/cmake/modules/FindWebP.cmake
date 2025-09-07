4d369c807102fc8f34f3aa6cab15434db814e3f8
Chris Mayo <aklhfex@gmail.com>
2025-09-07 19:02:29 +0100

    CMake: Use QUIET with find_package(PkgConfig) for libwebp

--- build/cmake/modules/FindWebP.cmake
+++ build/cmake/modules/FindWebP.cmake
@@ -1,7 +1,7 @@
 find_package(WebP CONFIG)
 
 if(NOT WebP_FOUND)
-    find_package(PkgConfig)
+    find_package(PkgConfig QUIET)
     if(PKG_CONFIG_FOUND)
         pkg_check_modules(WebP libwebp)
     endif()
