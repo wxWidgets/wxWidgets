831653954c9b86cbe383c6b9ed2cfc00f8a9bf11
Tijs Van Buggenhout <tijs.van.buggenhout@axsguard.com>
2025-09-18 15:59:13 +0200

    fixup! Add wxCocoaPowerModule and CocoaPowerEventsObserver
    Enable Power Events on request.
    
    Create a unique wxCocoaPowerEventsObserver when system power resources
    are acquired.
    
    Wrap the observer in a C++ class for handling via std::unique_ptr
    (sizeof) and automatic init/release of resources.
    
    Signed-off-by: Tijs Van Buggenhout <tijs.van.buggenhout@axsguard.com>

--- src/osx/cocoa/power.mm
+++ src/osx/cocoa/power.mm
@@ -25,94 +25,13 @@
 
 #include <IOKit/pwr_mgt/IOPMLib.h>
 
-// ----------------------------------------------------------------------------
-// wxPowerResource
-// ----------------------------------------------------------------------------
+#import <Cocoa/Cocoa.h>
 
 static wxAtomicInt g_powerResourceSystemRefCount = 0;
-static NSObject* g_processInfoActivity = nil;
 
-bool UpdatePowerResourceUsage(wxPowerResourceKind kind, const wxString& reason)
-{
-    if ( g_powerResourceSystemRefCount >= 1 )
-    {
-        wxCFStringRef cfreason(reason);
-        if( reason.IsEmpty())
-            cfreason = wxString("User Activity");
-
-        if ( !g_processInfoActivity )
-        {
-            NSActivityOptions
-                options = NSActivityUserInitiated |
-                          NSActivityIdleSystemSleepDisabled;
-
-            if ( kind == wxPOWER_RESOURCE_SCREEN )
-                options |= NSActivityIdleDisplaySleepDisabled;
-
-            g_processInfoActivity = [[NSProcessInfo processInfo]
-                                     beginActivityWithOptions:options
-                                     reason:cfreason.AsNSString()];
-            [g_processInfoActivity retain];
-            return true;
-        }
-    }
-    else if ( g_powerResourceSystemRefCount == 0 )
-    {
-        if ( g_processInfoActivity )
-        {
-            [[NSProcessInfo processInfo]
-             endActivity:(id)g_processInfoActivity];
-            g_processInfoActivity = nil;
-
-            return true;
-        }
-    }
-
-    return true;
-}
-
-bool
-wxPowerResource::Acquire(wxPowerResourceKind kind,
-                         const wxString& reason,
-                         wxPowerBlockKind blockKind)
-{
-    if ( blockKind == wxPOWER_DELAY )
-    {
-        // We don't support this mode under macOS because it's not needed there.
-        return true;
-    }
-
-    wxAtomicInc(g_powerResourceSystemRefCount);
-
-    bool success = UpdatePowerResourceUsage(kind, reason);
-    if (!success)
-        wxAtomicDec(g_powerResourceSystemRefCount);
-
-    return success;
-}
-
-void wxPowerResource::Release(wxPowerResourceKind kind)
-{
-    switch ( kind )
-    {
-        case wxPOWER_RESOURCE_SCREEN:
-        case wxPOWER_RESOURCE_SYSTEM:
-            if ( g_powerResourceSystemRefCount > 0 )
-            {
-                wxAtomicDec(g_powerResourceSystemRefCount);
-            }
-            else
-            {
-                wxFAIL_MSG("Power resource was not acquired");
-            }
-
-            UpdatePowerResourceUsage(kind, "");
-            break;
-    }
-}
-
-#import <Cocoa/Cocoa.h>
-#import "wx/power.h" // Assuming you have wxPowerEvent defined or a custom event
+// ----------------------------------------------------------------------------
+// wxCocoaPowerModule
+// ----------------------------------------------------------------------------
 
 @interface wxCocoaPowerEventsObserver : NSObject
 @end
@@ -161,31 +80,143 @@ void wxPowerResource::Release(wxPowerResourceKind kind)
 @end
 
 namespace {
-// add power observers on init and remove on exit
-class wxCocoaPowerModule : public wxModule
+// wrapper around obj-c interface, usable as std::unique_ptr
+class wxCocoaPowerEventsObserverWrapper
 {
-wxDECLARE_DYNAMIC_CLASS(wxCocoaPowerModule);
-
 public:
-    bool OnInit() override
+    wxCocoaPowerEventsObserverWrapper()
+        : m_powerEventsObserver( [[wxCocoaPowerEventsObserver alloc] init] )
     {
-         m_powerObserver = [[wxCocoaPowerEventsObserver alloc] init];
-         return true;
     }
 
-    void OnExit() override
+    ~wxCocoaPowerEventsObserverWrapper()
     {
-         if (m_powerObserver != nullptr)
-         {
-             [m_powerObserver release];
-             m_powerObserver = nullptr;
-         }
+        [m_powerEventsObserver release];
     }
 
 private:
-    wxCocoaPowerEventsObserver* m_powerObserver;
+    wxCocoaPowerEventsObserver* m_powerEventsObserver;
+
+    wxDECLARE_NO_COPY_CLASS(wxCocoaPowerEventsObserverWrapper);
 };
 
-wxIMPLEMENT_DYNAMIC_CLASS(wxCocoaPowerModule, wxModule)
+// the global power events observer
+static std::unique_ptr<wxCocoaPowerEventsObserverWrapper> g_powerEventsObserver;
 
+// add power observers on init and remove on exit
+class wxCocoaPowerModule : public wxModule
+{
+public:
+    bool OnInit() override { return true; }
+    void OnExit() override
+    {
+        // There should be no other threads by now, so no locking is needed.
+        if ( g_powerResourceSystemRefCount )
+        {
+            if ( g_powerEventsObserver )
+            {
+                delete g_powerEventsObserver.release();
+            }
+
+            g_powerResourceSystemRefCount = 0;
+        }
+    }
+
+private:
+    wxDECLARE_NO_COPY_CLASS(wxCocoaPowerModule);
+};
 }; // anonymous namespace
+
+// ----------------------------------------------------------------------------
+// wxPowerResource
+// ----------------------------------------------------------------------------
+
+static NSObject* g_processInfoActivity = nil;
+
+bool UpdatePowerResourceUsage(wxPowerResourceKind kind, const wxString& reason)
+{
+    if ( g_powerResourceSystemRefCount >= 1 )
+    {
+        wxCFStringRef cfreason(reason);
+        if( reason.IsEmpty())
+            cfreason = wxString("User Activity");
+
+        if ( !g_processInfoActivity )
+        {
+            NSActivityOptions
+                options = NSActivityUserInitiated |
+                          NSActivityIdleSystemSleepDisabled;
+
+            if ( kind == wxPOWER_RESOURCE_SCREEN )
+                options |= NSActivityIdleDisplaySleepDisabled;
+
+            g_processInfoActivity = [[NSProcessInfo processInfo]
+                                     beginActivityWithOptions:options
+                                     reason:cfreason.AsNSString()];
+            [g_processInfoActivity retain];
+            return true;
+        }
+    }
+    else if ( g_powerResourceSystemRefCount == 0 )
+    {
+        if ( g_processInfoActivity )
+        {
+            [[NSProcessInfo processInfo]
+             endActivity:(id)g_processInfoActivity];
+            g_processInfoActivity = nil;
+
+            return true;
+        }
+    }
+
+    return true;
+}
+
+bool
+wxPowerResource::Acquire(wxPowerResourceKind kind,
+                         const wxString& reason,
+                         wxPowerBlockKind blockKind)
+{
+    if ( blockKind == wxPOWER_DELAY )
+    {
+        if ( kind == wxPOWER_RESOURCE_SYSTEM )
+        {
+            wxAtomicInc(g_powerResourceSystemRefCount);
+
+            if ( !g_powerEventsObserver )
+            {
+                g_powerEventsObserver.reset( new wxCocoaPowerEventsObserverWrapper() );
+            }
+        }
+
+        return true;
+    }
+
+    wxAtomicInc(g_powerResourceSystemRefCount);
+
+    bool success = UpdatePowerResourceUsage(kind, reason);
+    if (!success)
+        wxAtomicDec(g_powerResourceSystemRefCount);
+
+    return success;
+}
+
+void wxPowerResource::Release(wxPowerResourceKind kind)
+{
+    switch ( kind )
+    {
+        case wxPOWER_RESOURCE_SCREEN:
+        case wxPOWER_RESOURCE_SYSTEM:
+            if ( g_powerResourceSystemRefCount > 0 )
+            {
+                wxAtomicDec(g_powerResourceSystemRefCount);
+            }
+            else
+            {
+                wxFAIL_MSG("Power resource was not acquired");
+            }
+
+            UpdatePowerResourceUsage(kind, "");
+            break;
+    }
+}
