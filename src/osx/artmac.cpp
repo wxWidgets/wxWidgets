007a2c65cfe46185541d307123a1b56dbd645c18
Stefan Csomor <csomor@advancedconcepts.ch>
2025-10-05 18:09:20 +0200

    add native artprovider for macOS and iOS
    use named system images

--- src/osx/artmac.cpp
+++ src/osx/artmac.cpp
@@ -34,14 +34,9 @@ protected:
     virtual wxIconBundle CreateIconBundle(const wxArtID& id,
                                           const wxArtClient& client) override;
 #endif
-#if wxOSX_USE_COCOA_OR_IPHONE
     virtual wxBitmapBundle CreateBitmapBundle(const wxArtID& id,
                                   const wxArtClient& client,
-                                  const wxSize& size) override
-    {
-        return wxOSXCreateSystemBitmapBundle(id, client, size);
-    }
-#endif
+                                  const wxSize& size) override;
 };
 
 /* static */ void wxArtProvider::InitNativeProvider()
@@ -51,57 +46,60 @@ protected:
 
 #if wxOSX_USE_COCOA_OR_CARBON
 
+// under cocoa we still have access to the old native icons, so map them
+
 // ----------------------------------------------------------------------------
 // helper macros
 // ----------------------------------------------------------------------------
 
-#define CREATE_STD_ICON(iconId, xpmRc) \
+#define CREATE_STD_ICON(iconId) \
     { \
         wxIconBundle icon(wxT(iconId), wxBITMAP_TYPE_ICON_RESOURCE); \
         return icon; \
     }
 
 // Macro used in CreateBitmap to get wxICON_FOO icons:
-#define ART_MSGBOX(artId, iconId, xpmRc) \
+#define ART_MSGBOX(artId, iconId) \
     if ( id == artId ) \
     { \
-        CREATE_STD_ICON(#iconId, xpmRc) \
+        CREATE_STD_ICON(#iconId) \
     }
-
-static wxIconBundle wxMacArtProvider_CreateIconBundle(const wxArtID& id)
-{
-    ART_MSGBOX(wxART_ERROR,       wxICON_ERROR,       error)
-    ART_MSGBOX(wxART_INFORMATION, wxICON_INFORMATION, info)
-    ART_MSGBOX(wxART_WARNING,     wxICON_WARNING,     warning)
-    ART_MSGBOX(wxART_QUESTION,    wxICON_QUESTION,    question)
-
-    ART_MSGBOX(wxART_FOLDER,      wxICON_FOLDER,      folder)
-    ART_MSGBOX(wxART_FOLDER_OPEN, wxICON_FOLDER_OPEN, folder_open)
-    ART_MSGBOX(wxART_NORMAL_FILE, wxICON_NORMAL_FILE, deffile)
-    ART_MSGBOX(wxART_EXECUTABLE_FILE, wxICON_EXECUTABLE_FILE, exefile)
-
-    ART_MSGBOX(wxART_CDROM,       wxICON_CDROM,       cdrom)
-    ART_MSGBOX(wxART_FLOPPY,      wxICON_FLOPPY,      floppy)
-    ART_MSGBOX(wxART_HARDDISK,    wxICON_HARDDISK,    harddisk)
-    ART_MSGBOX(wxART_REMOVABLE,   wxICON_REMOVABLE,   removable)
-    ART_MSGBOX(wxART_PRINT,       wxICON_PRINT,       print)
-
-    ART_MSGBOX(wxART_DELETE,      wxICON_DELETE,      delete)
-
-    ART_MSGBOX(wxART_GO_BACK,     wxICON_GO_BACK,     back)
-    ART_MSGBOX(wxART_GO_FORWARD,  wxICON_GO_FORWARD,  forward)
-    ART_MSGBOX(wxART_GO_HOME,     wxICON_GO_HOME,     home)
-
-    ART_MSGBOX(wxART_HELP_SETTINGS, wxICON_HELP_SETTINGS, htmoptns)
-    ART_MSGBOX(wxART_HELP_PAGE,   wxICON_HELP_PAGE,   htmpage)
-    ART_MSGBOX(wxART_HELP_FOLDER,   wxICON_HELP_FOLDER,   htmlfoldr)
-
-    return wxNullIconBundle;
-}
 
 // ----------------------------------------------------------------------------
 // CreateIconBundle
 // ----------------------------------------------------------------------------
+
+static wxIconBundle wxMacArtProvider_CreateIconBundle(const wxArtID& id)
+{
+    ART_MSGBOX(wxART_ERROR,       wxICON_ERROR)
+    ART_MSGBOX(wxART_INFORMATION, wxICON_INFORMATION)
+    ART_MSGBOX(wxART_WARNING,     wxICON_WARNING)
+    ART_MSGBOX(wxART_QUESTION,    wxICON_QUESTION)
+
+    ART_MSGBOX(wxART_FOLDER,      wxICON_FOLDER)
+    ART_MSGBOX(wxART_FOLDER_OPEN, wxICON_FOLDER_OPEN)
+    ART_MSGBOX(wxART_NORMAL_FILE, wxICON_NORMAL_FILE)
+    ART_MSGBOX(wxART_EXECUTABLE_FILE, wxICON_EXECUTABLE_FILE)
+
+    ART_MSGBOX(wxART_CDROM,       wxICON_CDROM)
+    ART_MSGBOX(wxART_FLOPPY,      wxICON_FLOPPY)
+    ART_MSGBOX(wxART_HARDDISK,    wxICON_HARDDISK)
+    ART_MSGBOX(wxART_REMOVABLE,   wxICON_REMOVABLE)
+    ART_MSGBOX(wxART_PRINT,       wxICON_PRINT)
+
+    ART_MSGBOX(wxART_DELETE,      wxICON_DELETE)
+
+    ART_MSGBOX(wxART_GO_BACK,     wxICON_GO_BACK)
+    ART_MSGBOX(wxART_GO_FORWARD,  wxICON_GO_FORWARD)
+    ART_MSGBOX(wxART_GO_HOME,     wxICON_GO_HOME)
+
+    ART_MSGBOX(wxART_HELP_SETTINGS, wxICON_HELP_SETTINGS)
+    ART_MSGBOX(wxART_HELP_PAGE,   wxICON_HELP_PAGE)
+    ART_MSGBOX(wxART_HELP_FOLDER,   wxICON_HELP_FOLDER)
+
+    return wxNullIconBundle;
+}
+
 
 wxIconBundle wxMacArtProvider::CreateIconBundle(const wxArtID& id, const wxArtClient& client)
 {
@@ -114,6 +112,61 @@ wxIconBundle wxMacArtProvider::CreateIconBundle(const wxArtID& id, const wxArtCl
 }
 
 #endif
+
+// ----------------------------------------------------------------------------
+// CreateBitmapBundle
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// helper macros
+// ----------------------------------------------------------------------------
+
+#define ART_BITMAP(artId, symbolname) \
+    if ( id == artId ) \
+    { \
+        return wxOSXCreateSystemBitmapBundle(symbolname); \
+    }
+
+static wxBitmapBundle wxMacArtProvider_CreateBitmapBundle(const wxArtID& id)
+{
+    ART_BITMAP(wxART_ERROR,         "xmark.circle" )
+    ART_BITMAP(wxART_INFORMATION,   "info.circle" )
+    ART_BITMAP(wxART_WARNING,       "exclamationmark.triangle" )
+    ART_BITMAP(wxART_QUESTION,      "questionmark.circle" )
+
+    ART_BITMAP(wxART_FOLDER,         "folder")
+    ART_BITMAP(wxART_FOLDER_OPEN,    "folder")
+    ART_BITMAP(wxART_NORMAL_FILE,    "document")
+    ART_BITMAP(wxART_EXECUTABLE_FILE,"app")
+
+    ART_BITMAP(wxART_CDROM,          "opticalsdrive")
+    ART_BITMAP(wxART_FLOPPY,         "opticalsdrive")
+    ART_BITMAP(wxART_HARDDISK,       "internaldrive")
+    ART_BITMAP(wxART_REMOVABLE,      "externaldrive")
+    ART_BITMAP(wxART_PRINT,          "printer")
+
+    ART_BITMAP(wxART_DELETE,         "minus.circle")
+
+    ART_BITMAP(wxART_GO_BACK,        "arrow.backward.circle")
+    ART_BITMAP(wxART_GO_FORWARD,     "arrow.fordward.circle")
+    ART_BITMAP(wxART_GO_HOME,        "house")
+
+    ART_BITMAP(wxART_HELP_SETTINGS,  "gear.badge.questionmark")
+    ART_BITMAP(wxART_HELP_PAGE,      "questionmark.text.page")
+    ART_BITMAP(wxART_HELP_FOLDER,    "questionmark.folder")
+
+    return wxBitmapBundle();
+}
+
+wxBitmapBundle wxMacArtProvider::CreateBitmapBundle(const wxArtID& id, const wxArtClient& client, const wxSize& /* size */)
+{
+    // On the Mac folders in lists are always drawn closed, so if an open
+    // folder icon is asked for we will ask for a closed one in its place
+    if ( client == wxART_LIST && id == wxART_FOLDER_OPEN )
+        return wxMacArtProvider_CreateBitmapBundle(wxART_FOLDER);
+
+    return wxMacArtProvider_CreateBitmapBundle(id);
+}
 
 // ----------------------------------------------------------------------------
 // wxArtProvider::GetNativeDIPSizeHint()
