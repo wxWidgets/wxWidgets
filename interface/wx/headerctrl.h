wxWidgets/wxWidgets

commit 00bd397cf29075d745da68d7fc6bc04d41c20b54

Fix parameter name in wxHeaderCtrl::ShowSortIndicator()
Use more useful "ascending".

interface/wx/headerctrl.h
@@ -662,7 +662,7 @@ public:
             If @true or @false show the sort indicator corresponding to
             ascending or descending sort order respectively.
      */
-    void ShowSortIndicator(unsigned int idx, bool sortOrder = true);
+    void ShowSortIndicator(unsigned int idx, bool ascending = true);
 
     /**
         Remove the sort indicator from the column being used as sort key.

commit bd14cf2dbf5d32dfee63e4bd8821471bd1186f6f

Fix wxHeaderCtrl documentation after recent parameter change
Correct a problem introduced in 00bd397cf2 (Fix parameter name in
wxHeaderCtrl::ShowSortIndicator(), 2025-10-28) which wrongly renamed the
parameter but not its documentation.

See #25934.

interface/wx/headerctrl.h
@@ -658,7 +658,7 @@ public:
             The column to set the sort indicator for.
             If @c -1 is given, then the currently shown sort indicator
             will be removed.
-        @param sortOrder
+        @param ascending
             If @true or @false show the sort indicator corresponding to
             ascending or descending sort order respectively.
      */