40ca92d6bbba0ded2e63f1d76c96babf2486537c
Eran Ifrah <eran@codelite.org>
2025-10-31 16:14:41 +0200

    Fix OnEditUpdate to Propagate wxEVT_STC_UPDATEUI Event
    Insert a call to `event.Skip()` at the start of `wxStyledTextCtrlMiniMap::OnEditUpdate`
    so the event continues its propagation.
    
    * src/stc/minimap.cpp
    
    Signed-off-by: Eran Ifrah <eran@codelite.org>

--- src/stc/minimap.cpp
+++ src/stc/minimap.cpp
@@ -544,6 +544,7 @@ void wxStyledTextCtrlMiniMap::OnEditPaint(wxPaintEvent& event)
 
 void wxStyledTextCtrlMiniMap::OnEditUpdate(wxStyledTextEvent& event)
 {
+    event.Skip();
     if ( event.GetUpdated() & wxSTC_UPDATE_V_SCROLL )
     {
         auto const editFirst = m_edit->GetFirstVisibleLine();
