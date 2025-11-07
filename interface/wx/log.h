bc41f9102ad9eabb706701a36701fb8e1b887570
Vadim Zeitlin <vadim@wxwidgets.org>
2025-11-07 18:41:38 +0100

    Fix wxLogWarning() documentation
    Don't claim that they "don't interrupt the program work", whatever it
    means.
    
    Closes #25916.

--- interface/wx/log.h
+++ interface/wx/log.h
@@ -1517,8 +1517,11 @@ void wxVLogVerbose(const char* formatString, va_list argPtr);
 /** @addtogroup group_funcmacro_log */
 ///@{
 /**
-    For warnings - they are also normally shown to the user, but don't
-    interrupt the program work.
+    The function to use for warning messages.
+
+    Warnings are less severe than errors but still should normally be
+    brought to the user's attention. The default processing is to show them
+    in a message box.
 
     @header{wx/log.h}
 */
