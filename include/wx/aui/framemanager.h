e8fc56ed552f4ea10815ffb34b9a8112691f7a95
iolandaasavei <iolanda.asavei@hexagon.com>
2025-11-19 23:32:43 +0200

    [api-change] Disable default AUI dock-hint window
    MERGED REVISIONS: r1787 [r2552]
    AUTHORS: svn-alexandrub (Alexandru Branescu)
    
    Added wxAUI_NB_NO_HINT_WINDOW and wxAUI_MGR_NO_HINT_WINDOW so the AUI manager stops showing its built-in docking hint window on mac and linux, avoiding the visual artifact when dragging panes since we use our own hint display.
    
    Co-authored-by: Alexandru Branescu <alexandru.branescu@hexagon.com>
    
    refs Task RM-7651
    
    [api-change] AUI manager
    
    MERGED REVISIONS: r1782 [r2551]
    AUTHORS: svn-alexandrub (Alexandru Branescu)
    
    Declared selected functions as virtual to allow overriding in derived classes.
    
    Co-authored-by: Alexandru Branescu
    refs RM-7651

--- include/wx/aui/framemanager.h
+++ include/wx/aui/framemanager.h
@@ -48,6 +48,9 @@ enum wxAuiManagerOption
     wxAUI_MGR_HINT_FADE                = 1 << 6,
     wxAUI_MGR_NO_VENETIAN_BLINDS_FADE  = 1 << 7,
     wxAUI_MGR_LIVE_RESIZE              = 1 << 8,
+// Bricsys added: disable hint window
+    wxAUI_MGR_NO_HINT_WINDOW           = 1 << 9,
+// end Bricsys added
 
     wxAUI_MGR_DEFAULT = wxAUI_MGR_ALLOW_FLOATING |
                         wxAUI_MGR_TRANSPARENT_HINT |
@@ -527,8 +530,8 @@ protected:
     wxAuiDockUIPart* GetPanePart(wxWindow* pane);
     int GetDockPixelOffset(wxAuiPaneInfo& test);
     void OnFloatingPaneMoveStart(wxWindow* window);
-    void OnFloatingPaneMoving(wxWindow* window, wxDirection dir );
-    void OnFloatingPaneMoved(wxWindow* window, wxDirection dir);
+    virtual void OnFloatingPaneMoving(wxWindow* window, wxDirection dir );
+    virtual void OnFloatingPaneMoved(wxWindow* window, wxDirection dir);
     void OnFloatingPaneActivated(wxWindow* window);
     void OnFloatingPaneClosed(wxWindow* window, wxCloseEvent& evt);
     void OnFloatingPaneResized(wxWindow* window, const wxRect& rect);
