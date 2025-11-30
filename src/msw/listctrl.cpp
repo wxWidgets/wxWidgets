wxWidgets/wxWidgets

commit bde6b7a8b855f2347a7a09915da08d7c42fa64cc

Fix wxListCtrl::GetSubItemRect(n, 0) when columns are reordered
Sum up all the preceding column widths to find the starting position of
the first column, which may not be 0 if the columns have been reordered.

src/msw/listctrl.cpp
@@ -1382,11 +1382,23 @@ bool wxListCtrl::GetSubItemRect(long item, long subItem, wxRect& rect, int code)
 
     wxCopyRECTToRect(rectWin, rect);
 
-    // there is no way to retrieve the first sub item bounding rectangle using
-    // wxGetListCtrlSubItemRect() as 0 means the whole item, so we need to
-    // truncate it at first column ourselves
+    // We can't use wxGetListCtrlSubItemRect() for the 0th subitem as 0 means
+    // the entire row for this function, so we need to calculate it ourselves.
     if ( subItem == 0 && code == wxLIST_RECT_BOUNDS )
+    {
+        // Because the columns can be reordered, we need to sum the widths of
+        // all preceding columns to get the correct x position.
+        rect.x = 0;
+        for ( auto col : GetColumnsOrder() )
+        {
+            if ( col == 0 )
+                break;
+
+            rect.x += GetColumnWidth(col);
+        }
+
         rect.width = GetColumnWidth(0);
+    }
 
     return true;
 }

commit b2ada524dce55596bf9d88e8fcaf6b8622ee5274

Fix incorrect wxListCtrl::GetSubItemRect(n, 0)
This was broken in bde6b7a8b8.
When the list is scrolled the x-coordinate will be negative.
So don't start counting from 0, but start from the original x coordinate of the full rectangle.

src/msw/listctrl.cpp
@@ -1388,16 +1388,15 @@ bool wxListCtrl::GetSubItemRect(long item, long subItem, wxRect& rect, int code)
     {
         // Because the columns can be reordered, we need to sum the widths of
         // all preceding columns to get the correct x position.
-        rect.x = 0;
         for ( auto col : GetColumnsOrder() )
         {
-            if ( col == 0 )
+            if ( col == subItem )
                 break;
 
             rect.x += GetColumnWidth(col);
         }
 
-        rect.width = GetColumnWidth(0);
+        rect.width = GetColumnWidth(subItem);
     }
 
     return true;