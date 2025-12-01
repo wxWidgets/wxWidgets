wxWidgets/wxWidgets

commit 365b07b75fb0b3e58f31468eda26727a57409141

Fix wxPaintDC invalidation in wxMSW wxNotebook in dark mode
Claling GetTabRect() while painting wxNotebook could result in another
WM_PAINT sent by the native control when using multiple tab rows and
after handling this nested WM_PAINT wxMSW code called EndPaint() on the
DC used by wxPaintDC that we had created before calling GetTabRect().

This is almost certainly wrong and we should use reference counting
instead of assuming that nothing can use the HDC once we return from
WM_PAINT handler as the code seems to be doing currently, but for now
just avoid the problem by calling GetTabRect() before creating wxPaintDC
instead to ensure that wxPaintDC remains valid while we draw on it.

Closes #25700.

src/msw/notebook.cpp
@@ -1255,8 +1255,17 @@ DrawNotebookTab(wxWindow* win,
 
 } // anonymous namespace
 
-void wxNotebook::MSWNotebookPaint(wxDC& dc)
+void wxNotebook::MSWNotebookPaint()
 {
+    // This is tricky: GetTabRect() may result in a nested WM_PAINT when the
+    // native control decides to generate it from its Tab_CalcPaintMetrics()
+    // for some reason (this happens at least when using multiple tab rows), so
+    // we need to call it before creating wxPaintDC as otherwise the current
+    // paint DC would be invalidated by EndPaint() while we use it, see #25700.
+    wxRect rectTabArea = GetTabRect(0);
+
+    // Now create and use the DC.
+    wxPaintDC dc(this);
     dc.Clear();
 
     const wxDirection tabOrient = GetTabOrientation();
@@ -1282,7 +1291,6 @@ void wxNotebook::MSWNotebookPaint(wxDC& dc)
         return;
 
     // Start by erasing the tabs area background.
-    wxRect rectTabArea = GetTabRect(0);
     rectTabArea = ExpandSelectedTab(rectTabArea, tabOrient);
     if ( tabOrient == wxTOP || tabOrient == wxBOTTOM )
         rectTabArea.SetRight(sizeWindow.x);
@@ -1370,15 +1378,15 @@ void wxNotebook::OnPaint(wxPaintEvent& event)
         return;
     }
 
-    wxPaintDC dc(this);
-
     if ( wxMSWDarkMode::IsActive() )
     {
         // We can't use default painting in dark mode, it just doesn't work
         // there, whichever theme we use, so draw everything ourselves.
-        MSWNotebookPaint(dc);
+        MSWNotebookPaint();
         return;
     }
+
+    wxPaintDC dc(this);
 
     RECT rc;
     ::GetClientRect(GetHwnd(), &rc);

include/wx/msw/notebook.h
@@ -183,8 +183,9 @@ protected:
   void OnEraseBackground(wxEraseEvent& event);
   void OnPaint(wxPaintEvent& event);
 
-  // Paint the notebook ourselves on the provided DC.
-  void MSWNotebookPaint(wxDC& dc);
+  // Paint the notebook ourselves using wxPaintDC (so this can be only called
+  // from wxEVT_PAINT handler).
+  void MSWNotebookPaint();
 
 
   // true if we have already subclassed our updown control

commit ce14074307f0e30aa6d4b6d520fe4f301d26d601

Avoid assert when painting empty wxNotebook when using dark mode
Don't call GetTabRect(0) if the notebook is empty as this would trigger
an assert inside this function.

This fixes a regression introduced in 365b07b75f (Fix wxPaintDC
invalidation in wxMSW wxNotebook in dark mode, 2025-08-18), see #25700.

Closes #26007.

src/msw/notebook.cpp
@@ -1262,7 +1262,12 @@ void wxNotebook::MSWNotebookPaint()
     // for some reason (this happens at least when using multiple tab rows), so
     // we need to call it before creating wxPaintDC as otherwise the current
     // paint DC would be invalidated by EndPaint() while we use it, see #25700.
-    wxRect rectTabArea = GetTabRect(0);
+    wxRect rectTabArea;
+
+    // This is more than just an optimization: calling GetTabRect(0) for an
+    // empty control is not allowed and would assert.
+    if ( GetPageCount() > 0 )
+        rectTabArea = GetTabRect(0);
 
     // Now create and use the DC.
     wxPaintDC dc(this);