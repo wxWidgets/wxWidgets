a4a5387f4bb8aa167a72131d724ac0c94b492cc6
iolandaasavei <iolanda.asavei@hexagon.com>
2025-12-03 23:39:19 +0200

    [api-change] Handled GTK child notifications during window reparenting
    MERGED REVISIONS: r3535
    AUTHORS: svn-alexandrub (Alexandru Branescu)
    
    On Linux/GTK, recursively notify a Gtk window's children when a reparenting operation occurs. Qt subwindows are not automatically reparented by GTK, so this ensures they receive the proper notifications and update their X window parent accordingly.
    
    Co-authored-by: Alexandru Branescu <alexandru.branescu@hexagon.com>
    RM-20245

--- include/wx/gtk/window.h
+++ include/wx/gtk/window.h
@@ -162,6 +162,13 @@ public:
     void GTKSendPaintEvents(const GdkRegion* region);
 #endif
 
+    // Bricsys change (Linux): recursively notify our children when a
+    // reparenting action takes place. We use it to reparent
+    // Qt subwindows to the new X window parent (doesn't happen directly)
+    virtual void NotifyWindowOnReparent();
+    // end Bricsys change
+
+
     // The methods below are required because many native widgets
     // are composed of several subwidgets and setting a style for
     // the widget means setting it for all subwidgets as well.
