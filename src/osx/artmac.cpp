3035b169d4790a318a44e6062d58e9a0d48865c9
Daniel Kulp <dan@kulp.com>
2025-12-16 21:57:33 +0100

    Use native images for wxART_GO_UP/DOWN too in wxOSX
    These images really need to be consistent with those for
    wxART_GO_BACK/FORWARD which had been already changed to use system
    symbols in 20ff462703 (OSX make iOS and macOS share more drawing code
    (#25860), 2025-10-07).
    
    Closes #26034.

--- src/osx/artmac.cpp
+++ src/osx/artmac.cpp
@@ -149,6 +149,8 @@ static wxBitmapBundle wxMacArtProvider_CreateBitmapBundle(const wxArtID& id, con
 
     ART_BITMAP(wxART_GO_BACK,        "arrow.backward.circle")
     ART_BITMAP(wxART_GO_FORWARD,     "arrow.forward.circle")
+    ART_BITMAP(wxART_GO_UP,          "arrow.up.circle")
+    ART_BITMAP(wxART_GO_DOWN,        "arrow.down.circle")
     ART_BITMAP(wxART_GO_HOME,        "house")
 
     ART_BITMAP(wxART_HELP_SETTINGS,  "gear.badge.questionmark")
