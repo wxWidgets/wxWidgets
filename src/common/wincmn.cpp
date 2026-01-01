aaa151f5c72fc27a84b17c0763af84fdbaef634c
Glen Gordon <18452241+glenrgordon@users.noreply.github.com>
2026-01-01 13:19:52 -0600

    Return wxACC_NOT_IMPLEMENTED from wxWindowAccessible::GetParent()
    If a widget has a custom accessibility implementation derived from
    wxWindowAccessible, there's no guarantee that the parent overrides the
    nullptr-returning base CreateAccessible.
    
    Returning wxACC_NOT_IMPLEMENTED follows the pattern of other
    accessibility methods to defer to the platform's default implementation,
    and fixes wx wxWindowAccessible::GetParent() when called on custom
    accessibility implementations like wxCheckListBoxAccessible.
    
    Closes #26063.

--- src/common/wincmn.cpp
+++ src/common/wincmn.cpp
@@ -3955,7 +3955,7 @@ wxAccStatus wxWindowAccessible::GetParent(wxAccessible** parent)
         if (*parent)
             return wxACC_OK;
         else
-            return wxACC_FAIL;
+            return wxACC_NOT_IMPLEMENTED;
     }
 }
 
