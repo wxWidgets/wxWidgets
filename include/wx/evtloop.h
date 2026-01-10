wxWidgets/wxWidgets

commit 894a04f914093bc82e299c3bf77635be0680edf3

Fix using wxConsoleEventLoop in non-GUI applications under macOS
This class incorrectly inherited from wxEventLoopManual which is not
used in wxBase under macOS, so replace the test for __WXOSX__ with the
check for __WXDARWIN_OSX__ which works correctly even without any GUI
toolkit definition.

include/wx/evtloop.h
@@ -227,7 +227,7 @@ private:
     wxDECLARE_NO_COPY_CLASS(wxEventLoopBase);
 };
 
-#if defined(__WINDOWS__) || defined(__WXMAC__) || defined(__WXDFB__) || (defined(__UNIX__) && !defined(__WXOSX__))
+#if defined(__WINDOWS__) || defined(__WXDFB__) || (defined(__UNIX__) && !defined(__DARWIN__))
 
 // this class can be used to implement a standard event loop logic using
 // Pending() and Dispatch()
@@ -275,9 +275,9 @@ private:
 // integration with MFC) but currently this is not done for all ports yet (e.g.
 // wxX11) so fall back to the old wxGUIEventLoop definition below for them
 
+// Include wxCFEventLoop declaration which is needed by both the console and
+// GUI event loops under macOS.
 #if defined(__DARWIN__)
-    // CoreFoundation-based event loop is currently in wxBase so include it in
-    // any case too (although maybe it actually shouldn't be there at all)
     #include "wx/osx/core/evtloop.h"
 #endif
 

include/wx/unix/evtloop.h
@@ -21,7 +21,7 @@ class wxFDIODispatcher;
 class wxWakeUpPipeMT;
 
 class WXDLLIMPEXP_BASE wxConsoleEventLoop
-#ifdef __WXOSX__
+#ifdef __DARWIN__
 : public wxCFEventLoop
 #else
 : public wxEventLoopManual

commit d07273a012c4398f56f31d393a5b1c21da48dac4

Fix wxEventLoopManual implementation check for wxGTK under macOS
This was broken by the recent 894a04f914 (Fix using wxConsoleEventLoop
in non-GUI applications under macOS, 2026-01-07) which resulted in the
test around this class declaration in the header and its implementation
in the source file getting out of sync.

Fix this by introducing wxHAS_EVENTLOOP_MANUAL symbol, defining it in
the header if this class is used and checking for it in the source file
to make sure this doesn't happen again.

Note that wxHAS_EVENTLOOP_MANUAL is intentionally not documented as it's
not clear if there is ever any legitimate need to use wxEventLoopManual
directly in the application code, so it's better to not encourage it.

Closes #26089.

include/wx/evtloop.h
@@ -229,6 +229,8 @@ private:
 
 #if defined(__WINDOWS__) || defined(__WXDFB__) || (defined(__UNIX__) && !defined(__DARWIN__))
 
+#define wxHAS_EVENTLOOP_MANUAL
+
 // this class can be used to implement a standard event loop logic using
 // Pending() and Dispatch()
 //

src/common/evtloopcmn.cpp
@@ -212,8 +212,8 @@ wxEventLoopBase::AddSourceForFD(int fd,
 }
 
 #endif // wxUSE_EVENTLOOP_SOURCE
-// wxEventLoopManual is unused in the other ports
-#if defined(__WINDOWS__) || defined(__WXDFB__) || ( ( defined(__UNIX__) && !defined(__WXOSX__) ) && wxUSE_BASE)
+
+#ifdef wxHAS_EVENTLOOP_MANUAL
 
 // ============================================================================
 // wxEventLoopManual implementation
@@ -400,5 +400,4 @@ void wxEventLoopManual::DoStop(int rc)
     WakeUp();
 }
 
-#endif // __WINDOWS__ || __WXMAC__ || __WXDFB__
-
+#endif // wxHAS_EVENTLOOP_MANUAL