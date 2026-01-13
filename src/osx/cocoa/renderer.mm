aad0766b3cc9fce2ad722622973f6e3f954fe066
RobertRoeb <79007492+RobertRoeb@users.noreply.github.com>
2026-01-13 11:14:54 +0100

    Update src/osx/cocoa/renderer.mm
    Co-authored-by: VZ <vz-github@zeitlins.org>

--- src/osx/cocoa/renderer.mm
+++ src/osx/cocoa/renderer.mm
@@ -606,7 +606,7 @@ wxRendererMac::DrawItemSelectionRect(wxWindow * win,
     // data columns and only draws the border where the actual columns begins.
     // wxWidgets generic controls don't do this, so add a bit of distance to
     // the border
-    int distanceFromBorder = 8; 
+    constexpr int distanceFromBorder = 8; 
 
     wxDCPenChanger setPen(dc, *wxTRANSPARENT_PEN);
     wxDCBrushChanger setBrush(dc, selBrush);
