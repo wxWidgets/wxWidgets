wxWidgets/wxWidgets

commit 3e32a9abe1e05f0cddb17c791a59977bed8e8cff

Don't bother resetting wxStaticBitmap image when destroying it
This results in sending a WM_PAINT to the control parent which may be
rather unexpected if it happens while it is itself being destroyed, see
the fix for #25499 in the parent commit, so don't do it when Free() is
called from dtor.

And it is also unnecessary to do it when Free() is called from
DoUpdateImage(), as it calls MSWReplaceImageHandle() with another,
valid, handle right next anyhow.

src/msw/statbmp.cpp
@@ -161,8 +161,6 @@ void wxStaticBitmap::Free()
 {
     m_bitmap.UnRef();
 
-    MSWReplaceImageHandle(0);
-
     if ( m_ownsCurrentHandle )
     {
         ::DeleteObject(m_currentHandle);

commit d662d5f6a06b9ba802fc0a4f17019164d35275f5

Fix wrong destruction of wxBitmap in wxMSW wxStaticBitmap
Due to a regression introduced in 3e32a9abe1 (Don't bother resetting
wxStaticBitmap image when destroying it, 2025-06-12), which was part
of #25518, the handle of wxBitmap passed to wxStaticBitmap::SetBitmap()
was destroyed when another bitmap was passed to SetBitmap() later.

Fix this by ensuring that we don't overwrite the value of the current
handle (to be deleted) prematurely in DoUpdateImage().

Add a simple unit test for wxStaticBitmap checking that the problem is
really fixed and, under MSW, also that it doesn't leak bitmap handles.

Closes #26106.

src/msw/statbmp.cpp
@@ -231,6 +231,7 @@ void wxStaticBitmap::DoUpdateImage(const wxSize& sizeOld, bool wasIcon)
 
     // For the icons we just use its HICON directly, but for bitmaps we create
     // our own temporary bitmap and need to delete its handle manually later.
+    WXHANDLE currentHandle = 0;
     if ( !m_icon.IsOk() )
     {
         wxBitmap bitmap = m_bitmapBundle.GetBitmapFor(this);
@@ -244,8 +245,8 @@ void wxStaticBitmap::DoUpdateImage(const wxSize& sizeOld, bool wasIcon)
         {
             // For bitmap with alpha channel create temporary DIB with
             // not-premultiplied alpha values.
-            m_currentHandle = wxDIB(bitmap.ConvertToImage(),
-                                    wxDIB::PixelFormat_NotPreMultiplied)
+            currentHandle = wxDIB(bitmap.ConvertToImage(),
+                                  wxDIB::PixelFormat_NotPreMultiplied)
                 .Detach();
             m_ownsCurrentHandle = true;
         }
@@ -266,14 +267,14 @@ void wxStaticBitmap::DoUpdateImage(const wxSize& sizeOld, bool wasIcon)
             // Just use the HBITMAP as is, but also make a copy of the bitmap
             // to ensure that HBITMAP remains valid for as long as we need it
             m_bitmap = bitmap;
-            m_currentHandle = bitmap.GetHandle();
+            currentHandle = bitmap.GetHandle();
         }
     }
 
     const bool isIcon = m_icon.IsOk();
     if ( isIcon )
     {
-        m_currentHandle = m_icon.GetHandle();
+        currentHandle = m_icon.GetHandle();
     }
 
     if ( isIcon != wasIcon )
@@ -287,17 +288,21 @@ void wxStaticBitmap::DoUpdateImage(const wxSize& sizeOld, bool wasIcon)
     // Update the handle used by the native control.
     const WPARAM imageType = m_icon.IsOk() ? IMAGE_ICON : IMAGE_BITMAP;
 
-    const HGDIOBJ currentHandle = (HGDIOBJ)m_currentHandle;
     const HGDIOBJ oldHandle = (HGDIOBJ)
         ::SendMessage(GetHwnd(), STM_SETIMAGE, imageType, (LPARAM)currentHandle);
 
     // detect if this is still the handle we passed before or
     // if the static-control made a copy of the bitmap!
-    if ( oldHandle != 0 && oldHandle != currentHandle )
+    if ( oldHandle != 0 && oldHandle != m_currentHandle )
     {
         // the static control made a copy and we are responsible for deleting it
         ::DeleteObject(oldHandle);
     }
+
+    // Note that m_currentHandle must be changed after comparing its previous
+    // value with oldHandle above and before possibly freeing it in the call to
+    // Free() below.
+    m_currentHandle = currentHandle;
 
     // Also check if we need to keep our current handle, it may be unnecessary
     // if the native control doesn't actually use it.

tests/Makefile.in
@@ -239,6 +239,7 @@ TEST_GUI_OBJECTS =  \
 	test_gui_slidertest.o \
 	test_gui_spinctrldbltest.o \
 	test_gui_spinctrltest.o \
+	test_gui_statbmptest.o \
 	test_gui_styledtextctrltest.o \
 	test_gui_textctrltest.o \
 	test_gui_textentrytest.o \
@@ -1137,6 +1138,9 @@ test_gui_spinctrldbltest.o: $(srcdir)/controls/spinctrldbltest.cpp $(TEST_GUI_OD
 
 test_gui_spinctrltest.o: $(srcdir)/controls/spinctrltest.cpp $(TEST_GUI_ODEP)
 	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/spinctrltest.cpp
+
+test_gui_statbmptest.o: $(srcdir)/controls/statbmptest.cpp $(TEST_GUI_ODEP)
+	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/statbmptest.cpp
 
 test_gui_styledtextctrltest.o: $(srcdir)/controls/styledtextctrltest.cpp $(TEST_GUI_ODEP)
 	$(CXXC) -c -o $@ $(TEST_GUI_CXXFLAGS) $(srcdir)/controls/styledtextctrltest.cpp

tests/controls/statbmptest.cpp
@@ -1,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////
+// Name:        tests/controls/statbmptest.cpp
+// Purpose:     wxStaticBitmap unit test
+// Author:      Vadim Zeitlin
+// Created:     2026-01-22
+// Copyright:   (c) 2026 Vadim Zeitlin
+///////////////////////////////////////////////////////////////////////////////
+
+#include "testprec.h"
+
+#if wxUSE_STATBMP
+
+#ifndef WX_PRECOMP
+    #include "wx/app.h"
+#endif // WX_PRECOMP
+
+#include "wx/statbmp.h"
+
+#include "wx/dcmemory.h"
+
+#ifdef __WINDOWS__
+    #include "wx/msw/private/resource_usage.h"
+#endif // __WINDOWS__
+
+static void TestStaticBitmap(wxWindow* window, double scale)
+{
+    // Create the bitmaps using the window scale factor to make sure they
+    // are used directly by wxStaticBitmap instead, without any rescaling
+    // (which would create temporary bitmaps and hide the manifestation of
+    // the bug that this test was written to trigger, see #26106).
+    wxBitmap bmp1;
+    bmp1.CreateWithDIPSize(16, 16, scale);
+    wxBitmap bmp2;
+    bmp2.CreateWithDIPSize(32, 32, scale);
+
+    wxIcon icon;
+    icon.CopyFromBitmap(bmp1);
+
+    wxStaticBitmap statbmp(window, wxID_ANY, bmp1);
+    REQUIRE( statbmp.GetBitmap().IsOk() );
+
+    statbmp.SetBitmap(bmp2);
+    REQUIRE( statbmp.GetBitmap().IsOk() );
+
+    // Check that the bitmap is valid by selecting it into wxMemoryDC:
+    // unlike IsOk() check, this would fail if it had been destroyed.
+    REQUIRE_NOTHROW( wxMemoryDC(bmp1) );
+
+    statbmp.SetIcon(icon);
+    REQUIRE_NOTHROW( wxMemoryDC(bmp2) );
+
+    statbmp.SetBitmap(bmp2);
+    REQUIRE_NOTHROW( wxMemoryDC(bmp1) );
+}
+
+TEST_CASE("wxStaticBitmap::Set", "[wxStaticBitmap][bitmap]")
+{
+    auto* const window = wxTheApp->GetTopWindow();
+
+    TestStaticBitmap(window, window->GetDPIScaleFactor());
+}
+
+#ifdef __WINDOWS__
+
+TEST_CASE("wxStaticBitmap::ResourceLeak", "[wxStaticBitmap]")
+{
+    auto* const window = wxTheApp->GetTopWindow();
+    auto const scale = window->GetDPIScaleFactor();
+
+    wxGUIObjectUsage usageBefore;
+    for ( int n = 0; n < 100; ++n )
+    {
+        TestStaticBitmap(window, scale);
+
+        // First use of GDI allocates some resources, so consider the state
+        // after the first iteration as the baseline: if there are any leaks,
+        // we'll see them in the subsequent iterations too and the test below
+        // will fail.
+        if ( n == 0 )
+            usageBefore = wxGetCurrentlyUsedResources();
+    }
+
+    auto const usageAfter = wxGetCurrentlyUsedResources();
+    CHECK( usageAfter.numGDI == usageBefore.numGDI );
+}
+
+#endif // __WINDOWS__
+
+#endif // wxUSE_STATBMP

tests/makefile.gcc
@@ -212,6 +212,7 @@ TEST_GUI_OBJECTS =  \
 	$(OBJS)\test_gui_slidertest.o \
 	$(OBJS)\test_gui_spinctrldbltest.o \
 	$(OBJS)\test_gui_spinctrltest.o \
+	$(OBJS)\test_gui_statbmptest.o \
 	$(OBJS)\test_gui_styledtextctrltest.o \
 	$(OBJS)\test_gui_textctrltest.o \
 	$(OBJS)\test_gui_textentrytest.o \
@@ -1083,6 +1084,9 @@ $(OBJS)\test_gui_spinctrldbltest.o: ./controls/spinctrldbltest.cpp
 	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\test_gui_spinctrltest.o: ./controls/spinctrltest.cpp
+	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
+
+$(OBJS)\test_gui_statbmptest.o: ./controls/statbmptest.cpp
 	$(CXX) -c -o $@ $(TEST_GUI_CXXFLAGS) $(CPPDEPS) $<
 
 $(OBJS)\test_gui_styledtextctrltest.o: ./controls/styledtextctrltest.cpp

tests/makefile.vc
@@ -227,6 +227,7 @@ TEST_GUI_OBJECTS =  \
 	$(OBJS)\test_gui_slidertest.obj \
 	$(OBJS)\test_gui_spinctrldbltest.obj \
 	$(OBJS)\test_gui_spinctrltest.obj \
+	$(OBJS)\test_gui_statbmptest.obj \
 	$(OBJS)\test_gui_styledtextctrltest.obj \
 	$(OBJS)\test_gui_textctrltest.obj \
 	$(OBJS)\test_gui_textentrytest.obj \
@@ -1383,6 +1384,9 @@ $(OBJS)\test_gui_spinctrldbltest.obj: .\controls\spinctrldbltest.cpp
 
 $(OBJS)\test_gui_spinctrltest.obj: .\controls\spinctrltest.cpp
 	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\spinctrltest.cpp
+
+$(OBJS)\test_gui_statbmptest.obj: .\controls\statbmptest.cpp
+	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\statbmptest.cpp
 
 $(OBJS)\test_gui_styledtextctrltest.obj: .\controls\styledtextctrltest.cpp
 	$(CXX) /c /nologo /TP /Fo$@ $(TEST_GUI_CXXFLAGS) .\controls\styledtextctrltest.cpp

tests/test.bkl
@@ -252,6 +252,7 @@
             controls/slidertest.cpp
             controls/spinctrldbltest.cpp
             controls/spinctrltest.cpp
+            controls/statbmptest.cpp
             controls/styledtextctrltest.cpp
             controls/textctrltest.cpp
             controls/textentrytest.cpp

tests/test_gui.vcxproj
@@ -954,6 +954,7 @@
     <ClCompile Include="controls\slidertest.cpp" />
     <ClCompile Include="controls\spinctrldbltest.cpp" />
     <ClCompile Include="controls\spinctrltest.cpp" />
+    <ClCompile Include="controls\statbmptest.cpp" />
     <ClCompile Include="controls\styledtextctrltest.cpp" />
     <ClCompile Include="controls\textctrltest.cpp" />
     <ClCompile Include="controls\textentrytest.cpp" />

tests/test_gui.vcxproj.filters
@@ -248,6 +248,9 @@
     <ClCompile Include="controls\spinctrltest.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="controls\statbmptest.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
     <ClCompile Include="controls\styledtextctrltest.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>