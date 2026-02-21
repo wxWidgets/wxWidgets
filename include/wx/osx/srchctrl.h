e662ef63a8b4991f22b1a25f0dd15a85e165eed0
iolandaasavei <iolanda.asavei@hexagon.com>
2026-02-21 13:38:43 +0200

    Mac: Make wxSearchCtrl::Create() virtual
    MERGED REVISIONS: r2575
    AUTHORS: svn-antont (Anton Triest)
    
    dev. info: Issue #8935
      @mac
      - make wxSearchCtrl::Create() virtual so it can be overriden in derived class (useful for subclass in XRC)
      - needed only on Mac at this time
    user info:
    
    refs RM-69973

--- include/wx/osx/srchctrl.h
+++ include/wx/osx/srchctrl.h
@@ -31,13 +31,15 @@ public:
 
     virtual ~wxSearchCtrl();
 
-    bool Create(wxWindow *parent, wxWindowID id,
+    // Bricsys change: we subclass wxSearchCtrl in XRC and override Create() on Mac
+    virtual bool Create(wxWindow *parent, wxWindowID id,
                 const wxString& value = wxEmptyString,
                 const wxPoint& pos = wxDefaultPosition,
                 const wxSize& size = wxDefaultSize,
                 long style = 0,
                 const wxValidator& validator = wxDefaultValidator,
                 const wxString& name = wxASCII_STR(wxSearchCtrlNameStr));
+    // end Bricsys change
 
 #if wxUSE_MENUS
     // get/set search button menu
