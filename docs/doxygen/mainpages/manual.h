aa1a25e7b511c0a4e341a3c722e6eac2aa159ab6
Vadim Zeitlin <vadim@wxwidgets.org>
2026-02-28 13:50:12 +0100

    Update release files for 3.2.10
    Follow the instructions in docs/contributing/how-to-release.md

--- docs/doxygen/mainpages/manual.h
+++ docs/doxygen/mainpages/manual.h
@@ -14,7 +14,7 @@
 @author Julian Smart, Vadim Zeitlin, Robin Dunn, Stefan Csomor,
         Bryan Petty, Francesco Montorsi, Robert Roebling et al
 
-@date December 12, 2015
+@date March 3, 2026
 
 Welcome to wxWidgets, a stable and powerful open source framework for
 developing native cross-platform GUI applications in C++!
