wxWidgets/wxWidgets

commit 8ed1469743b505185905f8303a33418b6e4e5042

Fix crash on wxGLCanvasEGL destruction
The "unmap" signal handler was called from wxWindow dtor and accessed
the already destroyed wxGLCanvasEGL object, resulting in a crash.

Avoid this by disconnecting this handler when the object is being
destroyed.

Closes #26248.

src/unix/glegl.cpp
@@ -685,6 +685,19 @@ void wxGLCanvasEGL::OnRealized()
 
 wxGLCanvasEGL::~wxGLCanvasEGL()
 {
+    // Our "unmap" signal handler would be called from the base class dtor, so
+    // disconnect it to avoid accessing this object when it's already
+    // half-destroyed (and it is useless anyhow now as all it does is to call
+    // DestroyWaylandSubsurface() which we already do just below).
+    if ( m_canvas && m_canvas->m_widget )
+    {
+        g_signal_handlers_disconnect_by_func(
+            m_canvas->m_widget,
+            reinterpret_cast<gpointer>(gtk_glcanvas_unmap_callback),
+            this
+        );
+    }
+
     if ( m_surface )
         eglDestroySurface(m_display, m_surface);
 #ifdef GDK_WINDOWING_WAYLAND

commit 583d7d6c339578d34f704d69f3f6de61a47055f9

Fix wxGTK build with GTK versions without Wayland support
Add missing GDK_WINDOWING_WAYLAND compile-time check to fix the problem
introduced in 8ed1469743 (Fix crash on wxGLCanvasEGL destruction,
2026-02-26), see #26248.

Note that we probably don't need a run-time check here because
g_signal_handlers_disconnect_by_func() would just do nothing if the
handler is not connected.

Closes #26269.

src/unix/glegl.cpp
@@ -685,6 +685,7 @@ void wxGLCanvasEGL::OnRealized()
 
 wxGLCanvasEGL::~wxGLCanvasEGL()
 {
+#ifdef GDK_WINDOWING_WAYLAND
     // Our "unmap" signal handler would be called from wxWindow dtor, so
     // disconnect it to avoid accessing this object when it's already destroyed
     // by wxGLCanvas dtor (and it is useless anyhow now as all it does is to
@@ -697,6 +698,7 @@ wxGLCanvasEGL::~wxGLCanvasEGL()
             this
         );
     }
+#endif // GDK_WINDOWING_WAYLAND
 
     if ( m_surface )
         eglDestroySurface(m_display, m_surface);