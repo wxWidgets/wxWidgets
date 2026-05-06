cfb1e7c6f384fb93614532817337a6735abcc07b
Razvan Macovei <razvan.macovei@octave.com>
2026-05-06 09:58:28 +0300

    wxWidgets builds ARM by default on Windows (GH-65)
    - On ARM64 host machines, when -DCMAKE_GENERATOR_PLATFORM is not explicitly set, CMake defaults to building for the host's native architecture (ARM64)
    - For x64 host machines, it would default to x64
    - Before the build was host-dependent rather than having a consistent target, now it targets x64 explicitly
    
    refs RM-63674

--- build/bricsys/build_wx.py
+++ build/bricsys/build_wx.py
@@ -179,6 +179,7 @@ def main():
         f'-DwxBUILD_INSTALL=ON '
     )
     if PLATFORM == 'windows':
+        configure_command += f'-DCMAKE_GENERATOR_PLATFORM=x64 '
         configure_command += f'-DwxUSE_LIBTIFF=builtin '
         configure_command += f'-DCMAKE_CONFIGURATION_TYPES="Debug;RelWithDebInfo" '
     elif PLATFORM == 'mac':
