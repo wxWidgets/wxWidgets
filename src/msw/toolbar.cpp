072bda8fabcb95c5a1a353138b21718bd3845d2d
Steve Cornett <21205494+stevecor@users.noreply.github.com>
2026-05-10 08:04:55 -0700

    Remove a redundant Refresh() from wxToolBar code
    This is not needed any more after the changes in the parent commit.
    
    See #26424.

--- src/msw/toolbar.cpp
+++ src/msw/toolbar.cpp
@@ -2074,8 +2074,6 @@ void wxToolBar::OnSysColourChanged(wxSysColourChangedEvent& event)
     int nrows = m_maxRows;
     m_maxRows = 0;      // otherwise SetRows() wouldn't do anything
     SetRows(nrows);
-
-    Refresh();
 }
 
 void wxToolBar::OnMouseEvent(wxMouseEvent& event)
