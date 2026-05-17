1a60a910d885464269518b2403f221afacd42fec
Stefan Csomor <csomor@advancedconcepts.ch>
2026-05-17 23:03:07 +0200

    Add defines for macOS < 10.12

--- src/osx/carbon/utilscocoa.mm
+++ src/osx/carbon/utilscocoa.mm
@@ -296,6 +296,24 @@ CGContextRef WXDLLIMPEXP_CORE wxOSXCreateBitmapContextFromImage( WXImage nsimage
 namespace
 {
 #if wxOSX_USE_COCOA
+
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12
+    #define NSCompositingOperationClear             NSCompositeClear
+    #define NSCompositingOperationCopy              NSCompositeCopy
+    #define NSCompositingOperationSourceOver        NSCompositeSourceOver
+    #define NSCompositingOperationSourceIn          NSCompositeSourceIn
+    #define NSCompositingOperationSourceOut         NSCompositeSourceOut
+    #define NSCompositingOperationSourceAtop        NSCompositeSourceAtop
+    #define NSCompositingOperationDestinationOver   NSCompositeDestinationOver
+    #define NSCompositingOperationDestinationIn     NSCompositeDestinationIn
+    #define NSCompositingOperationDestinationOut    NSCompositeDestinationOut
+    #define NSCompositingOperationDestinationAtop   NSCompositeDestinationAtop
+    #define NSCompositingOperationExclusion         NSCompositeExclusion
+    #define NSCompositingOperationPlusLighter       NSCompositePlusLighter
+    #define NSCompositingOperationDifference        NSCompositeDifference
+    #define NSCompositingOperationSourceOver        NSCompositeSourceOver
+#endif
+
     NSCompositingOperation wxOSXNSCompositionFromWXComposition( wxCompositionMode composition )
     {
         NSCompositingOperation mode = NSCompositingOperationSourceOver;
