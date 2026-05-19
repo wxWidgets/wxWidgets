1d10d0d838a8b26de605e5a1045732b2daf248c3
Alexandru Branescu <alexandru.branescu@hexagon.com>
2026-05-19 12:50:44 +0300

    fixup! Commented out treatment backslash as escape symbol for following '%' and '$' chars.
    Fix warning on Linux.
    Fix -Wcomment warning: nested '/*' inside block comment.
    
    refs RM-73848

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -574,11 +574,11 @@ wxString wxExpandEnvVars(const wxString& str)
         }
         break;
 
-/** Bricsys change
-/* GV: case below is commented out for _WIN32 platform since backslashes in paths
-/* are treated as escape symbol for following '%' or'$'  that prevents expansion
-/* of environment variables located after such backslashes.
-*/
+/* Bricsys change
+ * GV: case below is commented out for _WIN32 platform since backslashes in paths
+ * are treated as escape symbol for following '%' or'$'  that prevents expansion
+ * of environment variables located after such backslashes.
+ */
 #ifndef __WINDOWS__
       case wxT('\\'):
         // backslash can be used to suppress special meaning of % and $
