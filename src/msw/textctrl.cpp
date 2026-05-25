b1722286466ffdea9f0f9a5caeb79150f019d398
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2026-05-25 06:47:33 -0400

    Get charset from CHARFORMAT in wxTextCtrl instead of hardcoding ANSI_CHARSET

--- src/msw/textctrl.cpp
+++ src/msw/textctrl.cpp
@@ -3611,7 +3611,7 @@ bool wxTextCtrl::GetStyle(long position, wxTextAttr& style)
 
     LOGFONT lf;
     lf.lfWidth = 0;
-    lf.lfCharSet = ANSI_CHARSET; // FIXME: how to get correct charset?
+    lf.lfCharSet = (cf.dwMask & CFM_CHARSET) ? cf.bCharSet : DEFAULT_CHARSET;
     lf.lfClipPrecision = 0;
     lf.lfEscapement = 0;
     wxStrlcpy(lf.lfFaceName, cf.szFaceName, WXSIZEOF(lf.lfFaceName));
