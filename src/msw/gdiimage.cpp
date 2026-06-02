wxWidgets/wxWidgets

commit b45b02c22747ad5e4bd9fc5de9f6afa88121531b

Fix loading of standard icons recently broken in wxMSW
The changes of 96eaa04e9bc (Stop scaling icons loaded from resources in
wxMSW, 2026-04-10) broke loading standard icons because they must be
loaded with LR_SHARED flag and LoadImage() simply fails to load them
without it.

But there is no point in using LoadImage() for the standard icons anyhow
as it behaves in exactly the same way as LoadIcon(), so just restore the
code from before the commit above, including setting the scale factor
for the standard icons.

Still preserve the change of that commit for the application-defined
icons, which is what counts.

See #26369, #26370.

src/msw/gdiimage.cpp
@@ -618,6 +618,8 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         desiredHeight = 0;
     }
 
+    bool iconMayBeScaled = false;
+
     // try to load the icon from this program first to allow overriding the
     // standard icons (although why one would want to do it considering that
     // we already have wxApp::GetStdIcon() is unclear)
@@ -635,13 +637,15 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         else // We may still succeed in loading the standard icon.
         {
             const auto& entry = stdIcons[nStdIcon];
-            hicon = (HICON)::LoadImage((HINSTANCE)nullptr, entry.id, IMAGE_ICON,
-                                       desiredWidth, desiredHeight,
-                                       LR_DEFAULTSIZE | LR_DEFAULTCOLOR);
+            hicon = ::LoadIcon((HINSTANCE)nullptr, entry.id);
             if ( !hicon )
             {
-                wxLogLastError(wxString::Format("LoadImage(%s)", entry.name));
+                wxLogLastError(wxString::Format("LoadIcon(%s)", entry.name));
             }
+
+            // LoadIcon() may scale the icon in high DPI, so get its actual
+            // size below.
+            iconMayBeScaled = true;
         }
     }
 
@@ -649,17 +653,22 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         return false;
 
     wxSize size;
+    double scale = 1.0;
     if ( hasSize )
     {
         size.x = desiredWidth;
         size.y = desiredHeight;
     }
-    else
+    else if ( iconMayBeScaled )
     {
+        // LoadIcon() returns icons of scaled size, so we must use the correct
+        // scaling factor of them.
         size = wxGetHiconSize(hicon);
+        if ( const wxWindow* win = wxApp::GetMainTopWindow() )
+            scale = win->GetDPIScaleFactor();
     }
 
-    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y);
+    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y, scale);
 }
 
 #if wxUSE_PNG_RESOURCE_HANDLER

commit 42c12b2d8fae842b454a9af18f6c0539b3699b0f

Fix size of the icons loaded by wxICOResourceHandler
Changes of b45b02c227 (Fix loading of standard icons recently broken in
wxMSW, 2026-05-02) broke computation of the icon size which was left
empty for the non-scaled icons.

Fix this by always computing this size.

See #26370.

src/msw/gdiimage.cpp
@@ -618,7 +618,7 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         desiredHeight = 0;
     }
 
-    bool iconMayBeScaled = false;
+    double scale = 1.0;
 
     // try to load the icon from this program first to allow overriding the
     // standard icons (although why one would want to do it considering that
@@ -643,9 +643,10 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
                 wxLogLastError(wxString::Format("LoadIcon(%s)", entry.name));
             }
 
-            // LoadIcon() may scale the icon in high DPI, so get its actual
-            // size below.
-            iconMayBeScaled = true;
+            // LoadIcon() may scale the icon in high DPI, ensure we use correct
+            // scale factor for it.
+            if ( const wxWindow* win = wxApp::GetMainTopWindow() )
+                scale = win->GetDPIScaleFactor();
         }
     }
 
@@ -653,19 +654,14 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         return false;
 
     wxSize size;
-    double scale = 1.0;
     if ( hasSize )
     {
         size.x = desiredWidth;
         size.y = desiredHeight;
     }
-    else if ( iconMayBeScaled )
+    else
     {
-        // LoadIcon() returns icons of scaled size, so we must use the correct
-        // scaling factor of them.
         size = wxGetHiconSize(hicon);
-        if ( const wxWindow* win = wxApp::GetMainTopWindow() )
-            scale = win->GetDPIScaleFactor();
     }
 
     return icon->InitFromHICON((WXHICON)hicon, size.x, size.y, scale);