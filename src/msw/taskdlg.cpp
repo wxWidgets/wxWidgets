8b2b9069b31a1b5da1d1fc281bcde22050a6cd8d
Vadim Zeitlin <vadim@wxwidgets.org>
2026-06-07 16:40:53 +0200

    Don't define CLSID_CUIAutomation and IID_IUIAutomation ourselves
    They should be already defined in the SDK headers.

--- src/msw/taskdlg.cpp
+++ src/msw/taskdlg.cpp
@@ -128,14 +128,6 @@ static void DestroyTDPageState(HWND h)
     }
 }
 
-// GUID definitions
-// {ff48dba4-60ef-4201-aa87-54103eef594e}
-const GUID CLSID_CUIAutomation = { 0xff48dba4, 0x60ef, 0x4201, { 0xaa, 0x87, 0x54, 0x10, 0x3e, 0xef, 0x59, 0x4e } };
-
-// Also usually paired with the IID for IUIAutomation:
-// {30cbe57d-d9d0-452a-ab13-7ac5ac4825ee}
-const IID IID_IUIAutomation = { 0x30cbe57d, 0xd9d0, 0x452a, { 0xab, 0x13, 0x7a, 0xc5, 0xac, 0x48, 0x25, 0xee } };
-
 // Function to get IUIAutomation instance
 static IUIAutomation* GetTDAutomation()
 {
