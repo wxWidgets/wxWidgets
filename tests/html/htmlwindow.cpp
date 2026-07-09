ac1cf6550c28e5f999d27aeef4d4b797dfab1f33
Richard <legalize@xmission.com>
2026-07-09 12:01:54 -0600

    Preserve leading wxHTML BR elements
    Keep an initial BR element as a real blank line even when the current
    container only has formatting cells.
    
    Add a wxHtmlWindow regression test covering text preceded by an initial
    BR element.
    
    Fixes #3345

--- tests/html/htmlwindow.cpp
+++ tests/html/htmlwindow.cpp
@@ -39,6 +39,7 @@ private:
     CPPUNIT_TEST_SUITE( HtmlWindowTestCase );
         CPPUNIT_TEST( SelectionToText );
         CPPUNIT_TEST( Title );
+        CPPUNIT_TEST( InitialLineBreak );
 #if wxUSE_UIACTIONSIMULATOR
         WXUISIM_TEST( CellClick );
         WXUISIM_TEST( LinkClick );
@@ -48,6 +49,7 @@ private:
 
     void SelectionToText();
     void Title();
+    void InitialLineBreak();
     void CellClick();
     void LinkClick();
     void AppendToPage();
@@ -114,6 +116,25 @@ void HtmlWindowTestCase::Title()
     m_win->SetPage(TEST_MARKUP);
 
     CPPUNIT_ASSERT_EQUAL("Page", m_win->GetOpenedPageTitle());
+}
+
+void HtmlWindowTestCase::InitialLineBreak()
+{
+    m_win->SetBorders(0);
+    m_win->SetPage("<html><body>TEXT</body></html>");
+
+    wxHtmlContainerCell *plainTextRoot = m_win->GetInternalRepresentation();
+
+    CPPUNIT_ASSERT(plainTextRoot);
+
+    int plainTextHeight = plainTextRoot->GetHeight();
+
+    m_win->SetPage("<html><body><br>TEXT</body></html>");
+
+    wxHtmlContainerCell *rootWithBreak = m_win->GetInternalRepresentation();
+
+    CPPUNIT_ASSERT(rootWithBreak);
+    CPPUNIT_ASSERT(rootWithBreak->GetHeight() > plainTextHeight);
 }
 
 #if wxUSE_UIACTIONSIMULATOR
