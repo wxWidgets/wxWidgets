96c3b0b0b26ee988c52edff1e1e2f4221a4dfc07
Paul Cornett <paulcor@users.noreply.github.com>
2026-07-16 17:07:29 -0700

    Simplify use of rand() in base64 tests
    Just use the result of rand() directly. Avoids -Wdouble-promotion warnings.

--- tests/base64/base64.cpp
+++ tests/base64/base64.cpp
@@ -49,7 +49,7 @@ static void generateRandomData(void* buff, size_t len)
     unsigned char *cbuff = (unsigned char *)buff;
     while(len--)
     {
-        *(cbuff++) = (unsigned char)((rand() / (float)RAND_MAX * 255.) + 1);
+        *(cbuff++) = (unsigned char)rand();
     }
 }
 
@@ -61,7 +61,7 @@ static void generateGibberish(void* buff, size_t len)
     unsigned char *cbuff = (unsigned char *)buff;
     while(len--)
     {
-        *(cbuff++) = cb64[(size_t)((rand() / (float)RAND_MAX * 64.) + 1)];
+        *(cbuff++) = cb64[rand() & 63];
     }
 }
 
@@ -213,7 +213,7 @@ TEST_CASE("Encode Decode PatternC", "[base64]")
 
 TEST_CASE("Encode Decode Random", "[base64]")
 {
-    size_t size = (size_t)(rand() / (float)RAND_MAX * 3000. + 11);
+    size_t size = (rand() & 0xfff) + 11;
     unsigned char *buff = new unsigned char[size];
     generateRandomData(buff, size);
     wxString str = wxBase64Encode(buff, size);
