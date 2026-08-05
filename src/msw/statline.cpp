4b1b00c6b67a696b703ea7cbbe15513e2cb98a51
Steve Cornett <21205494+stevecor@users.noreply.github.com>
2026-08-05 16:39:19 -0700

    MSW: Dark mode switching for wxStaticLine
    Upon dark/light mode switch, update the wxStaticLine border style.

--- src/msw/statline.cpp
+++ src/msw/statline.cpp
@@ -62,15 +62,34 @@ WXDWORD wxStaticLine::MSWGetStyle(long style, WXDWORD *exstyle) const
     WXDWORD msStyle = wxControl::MSWGetStyle(style, exstyle);
 
     // add our default styles
-    msStyle |= SS_NOTIFY | WS_CLIPSIBLINGS;
+    msStyle |= SS_SUNKEN | SS_NOTIFY | WS_CLIPSIBLINGS;
     msStyle |= SS_GRAYRECT ;
 
-    // Sunken 3D border looks too bright in dark mode as it uses white colour,
-    // so we use another style there for less ostentatious appearance.
-    if ( !wxMSWDarkMode::IsActive() )
-        msStyle |= SS_SUNKEN;
-
     return msStyle ;
+}
+
+void wxStaticLine::MSWSetDarkOrLightMode(SetMode setmode)
+{
+    wxStaticLineBase::MSWSetDarkOrLightMode(setmode);
+
+    // Sunken 3D border looks too bright in dark mode as it uses white colour.
+    // Style SS_SUNKEN enables WS_EX_STATICEDGE, so update them both.
+    auto style = ::GetWindowLong(m_hWnd, GWL_STYLE);
+    auto exStyle = ::GetWindowLong(m_hWnd, GWL_EXSTYLE);
+    if ( wxMSWDarkMode::IsActive() )
+    {
+        style &= ~SS_SUNKEN;
+        exStyle &= ~WS_EX_STATICEDGE;
+    }
+    else
+    {
+        style |= SS_SUNKEN;
+        exStyle |= WS_EX_STATICEDGE;
+    }
+    ::SetWindowLong(m_hWnd, GWL_STYLE, style);
+    ::SetWindowLong(m_hWnd, GWL_EXSTYLE, exStyle);
+    ::SetWindowPos(m_hWnd, nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED |
+        SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
 }
 
 #endif // wxUSE_STATLINE
