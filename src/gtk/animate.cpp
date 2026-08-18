ef1537648fbf1b38197eb65a06c2300a2edfc3fd
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2026-08-17 21:01:20 -0400

    Fix memory leak in GTK animation control
    Use wxGtkError to fix missing g_error_free calls.
    
    See #26875.
    
    (cherry picked from commit fa5dfee13d50f4618f9e6b1893d9a820603f08bb)

--- src/gtk/animate.cpp
+++ src/gtk/animate.cpp
@@ -24,6 +24,7 @@
 
 #include "wx/wfstream.h"
 #include "wx/gtk/private.h"
+#include "wx/gtk/private/error.h"
 #include "wx/gtk/private/object.h"
 
 
@@ -96,20 +97,20 @@ bool wxAnimationGTKImpl::Load(wxInputStream &stream, wxAnimationType type)
     }
 
     // create a GdkPixbufLoader
-    GError *error = NULL;
+    wxGtkError error;
     GdkPixbufLoader *loader;
     if (type != wxANIMATION_TYPE_INVALID && type != wxANIMATION_TYPE_ANY)
-        loader = gdk_pixbuf_loader_new_with_type(anim_type, &error);
+        loader = gdk_pixbuf_loader_new_with_type(anim_type, error.Out());
     else
         loader = gdk_pixbuf_loader_new();
 
     wxGtkObject<GdkPixbufLoader> ensureUnrefLoader(loader);
 
     if (!loader ||
-        error != NULL)  // even if the loader was allocated, an error could have happened
+        error)  // even if the loader was allocated, an error could have happened
     {
         wxLogDebug(wxT("Could not create the loader for '%s' animation type: %s"),
-                   anim_type, error->message);
+                   anim_type, error.GetMessage());
         return false;
     }
 
@@ -130,9 +131,9 @@ bool wxAnimationGTKImpl::Load(wxInputStream &stream, wxAnimationType type)
         }
 
         // fetch all data into the loader
-        if (!gdk_pixbuf_loader_write(loader, buf, stream.LastRead(), &error))
+        if (!gdk_pixbuf_loader_write(loader, buf, stream.LastRead(), error.Out()))
         {
-            wxLogDebug(wxT("Could not write to the loader: %s"), error->message);
+            wxLogDebug(wxT("Could not write to the loader: %s"), error.GetMessage());
 
             // gdk_pixbuf_loader_close wants the GError == NULL
             gdk_pixbuf_loader_close(loader, NULL);
@@ -152,9 +153,9 @@ bool wxAnimationGTKImpl::Load(wxInputStream &stream, wxAnimationType type)
     // load complete: gdk_pixbuf_loader_close will now check if the data we
     // wrote inside the pixbuf loader does make sense and will give an error
     // if it doesn't (because of a truncated file, corrupted data or whatelse)
-    if (!gdk_pixbuf_loader_close(loader, &error))
+    if (!gdk_pixbuf_loader_close(loader, error.Out()))
     {
-        wxLogDebug(wxT("Could not close the loader: %s"), error->message);
+        wxLogDebug(wxT("Could not close the loader: %s"), error.GetMessage());
         return false;
     }
 
