6b7a3290e8018014ca7b87c67476d89403c73d3f
Paul Cornett <paulcor@users.noreply.github.com>
2026-08-23 21:07:23 -0700

    Treat diagnostics from wxENABLE_EXTRA_WARNINGS as errors
    Otherwise they override -Werror

--- include/wx/defs.h
+++ include/wx/defs.h
@@ -147,9 +147,9 @@
 /* Enable some warnings not enabled by default if requested. */
 #ifdef wxENABLE_EXTRA_WARNINGS
 #   ifdef __GNUC__
-#       pragma GCC diagnostic warning "-Wcast-qual"
-#       pragma GCC diagnostic warning "-Wdouble-promotion"
-#       pragma GCC diagnostic warning "-Wextra"
+#       pragma GCC diagnostic error "-Wcast-qual"
+#       pragma GCC diagnostic error "-Wdouble-promotion"
+#       pragma GCC diagnostic error "-Wextra"
 #   endif
 #endif
 
