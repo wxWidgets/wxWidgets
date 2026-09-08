39f4f4e67ff2d2b2405c22f505e473d5a2c29146
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2026-09-08 04:05:44 -0400

    Update docs/doxygen/groups/class_ribbon.h
    Co-authored-by: VZ <vz-github@zeitlins.org>

--- docs/doxygen/groups/class_ribbon.h
+++ docs/doxygen/groups/class_ribbon.h
@@ -16,7 +16,9 @@ toolbar. At a more functional level, it is similar to the user interface
 present in recent versions of Microsoft Office.
 
 Since wxWidgets 3.3.4, the ribbon can also be driven from the keyboard using
-Office-style KeyTips, improving accessibility for users who cannot rely on a
+key tips, i.e. popup windows showing the key which can be used to activate
+the associated element, commonly used in other programs with ribbon interface,
+improving accessibility for users who cannot rely on a
 mouse. Pressing @c WXK_F10 (customizable, see
 wxRibbonBar::SetKeyTipsTriggerKey()) badges every element which was assigned
 one, and typing its letters activates that element. See
