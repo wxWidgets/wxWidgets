4b13ab7eb8c28c08b7f6039673acf420ac850677
iolandaasavei <iolanda.asavei@hexagon.com>
2026-09-21 15:50:49 +0300

    Added refcountermt.h to allheaders.h

--- tests/allheaders.h
+++ tests/allheaders.h
@@ -294,6 +294,7 @@
 #include <wx/rawbmp.h>
 #include <wx/rearrangectrl.h>
 #include <wx/recguard.h>
+#include <wx/refcountermt.h>
 #include <wx/regex.h>
 #include <wx/region.h>
 #include <wx/renderer.h>
