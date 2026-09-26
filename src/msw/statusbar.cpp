3f85e80747f5c715263a7118200ff7c93b721097
Mohmed abdel-fattah <memoarfaa@gmail.com>
2026-09-26 11:48:53 +0300

    Update themeId assignment for dark mode support

--- src/msw/statusbar.cpp
+++ src/msw/statusbar.cpp
@@ -671,7 +671,7 @@ void wxStatusBar::MSWGetDarkModeSupport(MSWDarkModeSupport& support) const
     //
     // Note that we should _not_ set the theme name to "Explorer", this ID only
     // works if we do _not_ do it.
-     support.themeId = L"DarkMode_DarkTheme::Status;DarkMode::ExplorerStatusBar";
+    support.themeId = wxMSWDarkMode::HasDarkTheme() ? L"DarkMode_DarkTheme::Status" : L"ExplorerStatusBar";
 }
 
 wxVisualAttributes wxStatusBar::GetDefaultAttributes() const
